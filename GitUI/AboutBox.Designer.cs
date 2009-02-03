2dc078e1da84b4f586cb575c33a0ff5176ca4e14
Henk Westhuis <henk>
2009-02-03 19:56:40 +0100

    Changed version number, improved commit a bit more,

--- GitUI/AboutBox.Designer.cs
+++ GitUI/AboutBox.Designer.cs
@@ -116,7 +116,7 @@
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(271, 17);
             this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "Version 1.35";
+            this.labelVersion.Text = "Version 1.36";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.labelVersion.Click += new System.EventHandler(this.labelVersion_Click);
             // 
