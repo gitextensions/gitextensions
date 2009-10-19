dec9e7a6a86ed87570ad0b31e61628d3ba8886ee
Troels Thomsen <troethom@gmail.com>
2009-10-20 00:46:40 +0800

    Implemented simple command logger class (using a string builder as inner writer).

--- GitCommands/GitCommands/CommandLogger.cs
+++ GitCommands/GitCommands/CommandLogger.cs
@@ -1,6 +1,20 @@
-﻿namespace GitCommands
+﻿using System.Text;
+
+namespace GitCommands
 {
     public class CommandLogger
     {
+        // TODO: Replace the string builder with a rolling collection (keep X latest commands)
+        private readonly StringBuilder _commands = new StringBuilder();
+
+        public void Log(string command)
+        {
+            _commands.AppendLine(command);
+        }
+
+        public override string ToString()
+        {
+            return _commands.ToString();
+        }
     }
 }
