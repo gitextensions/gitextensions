b940628fb143c2833e9234801bf4d28108ee3529
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2009-11-15 06:07:06 +0800

    FormBranchSmall: fixed spelling mistake

--- GitUI/FormBranchSmall.cs
+++ GitUI/FormBranchSmall.cs
@@ -30,7 +30,7 @@ namespace GitUI
                 }
                 else
                 {
-                    new FormProcess(GitCommands.GitCommands.BranchCmd(BName.Text, Revision.Guid, ChechoutAfterCreate.Checked));
+                    new FormProcess(GitCommands.GitCommands.BranchCmd(BName.Text, Revision.Guid, CheckoutAfterCreate.Checked));
 
                     Close();
                 }
