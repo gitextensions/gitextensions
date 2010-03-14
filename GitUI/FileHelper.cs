edf91f7a656c5d3ee3e8bda6d596efc1e192dc90
Jacob Stanley <jystic@jystic.com>
2010-03-15 01:49:32 +0800

    Added asynchronous loading of files for diffs and general viewing.
    This makes the UI appear more responsive because the loading doesn't take place on the UI thread. Also removed some repetition from FileViewer in the process.

--- GitUI/FileHelper.cs
+++ GitUI/FileHelper.cs
@@ -1,37 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace GitUI
 {
     public static class FileHelper
     {
+        private static readonly IEnumerable<string> BinaryExtensions = new[]
+        {
+            ".avi",
+            ".bmp",
+            ".dat",
+            ".dll",
+            ".doc",
+            ".docx",
+            ".dwg",
+            ".exe",
+            ".gif",
+            ".ico",
+            ".jpg",
+            ".jpeg",
+            ".mpg",
+            ".mpeg",
+            ".pdf",
+            ".png",
+            ".pdb",
+            ".tif",
+            ".tiff",
+            ".vsd",
+            ".vsdx",
+            ".xls",
+            ".xlsx",
+        };
+
+        private static readonly IEnumerable<string> ImageExtensions = new[]
+        {
+            ".bmp",
+            ".gif",
+            ".ico",
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".tif",
+            ".tiff",
+        };
+
         public static bool IsBinaryFile(string fileName)
         {
-            return (fileName.EndsWith(".png", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".jpg", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".bmp", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".jpeg", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".ico", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".tif", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".tiff", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".exe", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".gif", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".mpg", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".mpeg", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".dll", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".pdf", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".doc", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".vsd", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".docx", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".xls", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".xlsx", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".vsdx", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".pdb", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".dwg", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".pdb", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".dat", StringComparison.CurrentCultureIgnoreCase) ||
-                    fileName.EndsWith(".avi", StringComparison.CurrentCultureIgnoreCase));
+            return HasMatchingExtension(BinaryExtensions, fileName);
+        }
+
+        public static bool IsImage(string fileName)
+        {
+            return HasMatchingExtension(ImageExtensions, fileName);
+        }
+
+        private static bool HasMatchingExtension(IEnumerable<string> extensions, string fileName)
+        {
+            foreach (string extension in extensions)
+            {
+                if (fileName.EndsWith(extension, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
