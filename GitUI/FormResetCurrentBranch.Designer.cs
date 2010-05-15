5963fb2ae616bb552fb3aebfbd83f2f42c5d9455
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2010-05-16 04:59:58 +0800

    Localized FormResetCurrentBranch.
    -Add a Japanese resource file to FormResetCurrentBranch
    -Modify FormResetCurrentBranch to use the resource files

--- GitUI/FormResetCurrentBranch.Designer.cs
+++ GitUI/FormResetCurrentBranch.Designer.cs
@@ -45,51 +45,52 @@
             // 
             // BranchInfo
             // 
+            resources.ApplyResources(this.BranchInfo, "BranchInfo");
             this.BranchInfo.AutoSize = true;
             this.BranchInfo.Location = new System.Drawing.Point(13, 13);
             this.BranchInfo.Name = "BranchInfo";
             this.BranchInfo.Size = new System.Drawing.Size(60, 13);
             this.BranchInfo.TabIndex = 0;
-            this.BranchInfo.Text = "Reset X to:";
             // 
             // Commit
             // 
+            resources.ApplyResources(this.Commit, "Commit");
             this.Commit.AutoSize = true;
             this.Commit.Location = new System.Drawing.Point(31, 34);
             this.Commit.Name = "Commit";
             this.Commit.Size = new System.Drawing.Size(44, 13);
             this.Commit.TabIndex = 1;
-            this.Commit.Text = "Commit:";
             // 
             // Author
             // 
+            resources.ApplyResources(this.Author, "Author");
             this.Author.AutoSize = true;
             this.Author.Location = new System.Drawing.Point(31, 57);
             this.Author.Name = "Author";
             this.Author.Size = new System.Drawing.Size(41, 13);
             this.Author.TabIndex = 2;
-            this.Author.Text = "Author:";
             // 
             // Message
             // 
+            resources.ApplyResources(this.Message, "Message");
             this.Message.AutoSize = true;
             this.Message.Location = new System.Drawing.Point(31, 106);
             this.Message.Name = "Message";
             this.Message.Size = new System.Drawing.Size(53, 13);
             this.Message.TabIndex = 3;
-            this.Message.Text = "Message:";
             // 
             // Date
             // 
+            resources.ApplyResources(this.Date, "Date");
             this.Date.AutoSize = true;
             this.Date.Location = new System.Drawing.Point(31, 82);
             this.Date.Name = "Date";
             this.Date.Size = new System.Drawing.Size(68, 13);
             this.Date.TabIndex = 4;
-            this.Date.Text = "Commit date:";
             // 
             // groupBox1
             // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.Hard);
             this.groupBox1.Controls.Add(this.Mixed);
             this.groupBox1.Controls.Add(this.Soft);
@@ -98,21 +99,20 @@
             this.groupBox1.Size = new System.Drawing.Size(475, 100);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Reset type";
             // 
             // Hard
             // 
+            resources.ApplyResources(this.Hard, "Hard");
             this.Hard.AutoSize = true;
             this.Hard.Location = new System.Drawing.Point(7, 68);
             this.Hard.Name = "Hard";
             this.Hard.Size = new System.Drawing.Size(451, 17);
             this.Hard.TabIndex = 2;
-            this.Hard.Text = "Hard: reset working dir and index (discard ALL local changes, even uncommitted ch" +
-                "anges)";
             this.Hard.UseVisualStyleBackColor = true;
             // 
             // Mixed
             // 
+            resources.ApplyResources(this.Mixed, "Mixed");
             this.Mixed.AutoSize = true;
             this.Mixed.Checked = true;
             this.Mixed.Location = new System.Drawing.Point(7, 44);
@@ -120,41 +120,41 @@
             this.Mixed.Size = new System.Drawing.Size(250, 17);
             this.Mixed.TabIndex = 1;
             this.Mixed.TabStop = true;
-            this.Mixed.Text = "Mixed: leave working dir untouched, reset index";
             this.Mixed.UseVisualStyleBackColor = true;
             // 
             // Soft
             // 
+            resources.ApplyResources(this.Soft, "Soft");
             this.Soft.AutoSize = true;
             this.Soft.Location = new System.Drawing.Point(7, 20);
             this.Soft.Name = "Soft";
             this.Soft.Size = new System.Drawing.Size(233, 17);
             this.Soft.TabIndex = 0;
-            this.Soft.Text = "Soft: leave working dir and index untouched";
             this.Soft.UseVisualStyleBackColor = true;
             // 
             // Ok
             // 
+            resources.ApplyResources(this.Ok, "Ok");
             this.Ok.Location = new System.Drawing.Point(335, 239);
             this.Ok.Name = "Ok";
             this.Ok.Size = new System.Drawing.Size(75, 23);
             this.Ok.TabIndex = 6;
-            this.Ok.Text = "Ok";
             this.Ok.UseVisualStyleBackColor = true;
             this.Ok.Click += new System.EventHandler(this.Ok_Click);
             // 
             // Cancel
             // 
+            resources.ApplyResources(this.Cancel, "Cancel");
             this.Cancel.Location = new System.Drawing.Point(416, 239);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(75, 23);
             this.Cancel.TabIndex = 7;
-            this.Cancel.Text = "Cancel";
             this.Cancel.UseVisualStyleBackColor = true;
             this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
             // 
             // FormResetCurrentBranch
             // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(498, 269);
@@ -172,7 +172,6 @@
             this.MinimizeBox = false;
             this.Name = "FormResetCurrentBranch";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Reset current branch";
             this.Load += new System.EventHandler(this.FormResetCurrentBranch_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
