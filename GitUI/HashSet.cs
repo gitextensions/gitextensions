51d4375bcea8d5f3a3a793958930280bbf8d9aa2
Nick Mayer <nicholas@the-mayers.net>
2010-06-22 22:30:36 -0500

    Reworked HashSet to use a List, which is more memory efficient.

--- GitUI/HashSet.cs
+++ GitUI/HashSet.cs
@@ -5,14 +5,14 @@ using System.Runtime.Serialization;
 
 namespace GitUI
 {
-    // Taken from http://stackoverflow.com/questions/687034/using-hashset-in-c-2-0-compatible-with-3-5
-    public class HashSet<T> : ICollection<T>, ISerializable, IDeserializationCallback
+    public class HashSet<T> : ICollection<T>
     {
-        private readonly Dictionary<T, object> dict;
+        private readonly List<T> dict;
+        private static readonly HashSet<T> nullSet = new HashSet<T>();
 
         public HashSet()
         {
-            dict = new Dictionary<T, object>();
+            dict = new List<T>();
         }
 
         public HashSet(IEnumerable<T> items)
@@ -29,7 +29,7 @@ namespace GitUI
             }
         }
 
-        public HashSet<T> NullSet { get { return new HashSet<T>(); } }
+        public HashSet<T> NullSet { get { return nullSet; } }
 
         #region ICollection<T> Members
 
@@ -40,7 +40,10 @@ namespace GitUI
                 throw new ArgumentNullException("item");
             }
 
-            dict[item] = null;
+            if (!dict.Contains(item))
+            {
+                dict.Add(item);
+            }
         }
 
         /// <summary>
@@ -54,7 +57,7 @@ namespace GitUI
 
         public bool Contains(T item)
         {
-            return dict.ContainsKey(item);
+            return dict.Contains(item);
         }
 
         /// <summary>
@@ -69,7 +72,7 @@ namespace GitUI
                 throw new ArgumentOutOfRangeException("arrayIndex");
             }
 
-            dict.Keys.CopyTo(array, arrayIndex);
+            dict.CopyTo(array, arrayIndex);
         }
 
         /// <summary>
@@ -115,146 +118,14 @@ namespace GitUI
         {
             foreach (T item in set)
             {
-                if (Contains(item))
-                {
-                    continue;
-                }
-
                 Add(item);
             }
-        }
-
-        public HashSet<T> Subtract(HashSet<T> set)
-        {
-            HashSet<T> subtractSet = new HashSet<T>(this);
-
-            if (null == set)
-            {
-                return subtractSet;
-            }
-
-            foreach (T item in set)
-            {
-                if (!subtractSet.Contains(item))
-                {
-                    continue;
-                }
-
-                subtractSet.dict.Remove(item);
-            }
-
-            return subtractSet;
-        }
-
-        public bool IsSubsetOf(HashSet<T> set)
-        {
-            HashSet<T> setToCompare = set ?? NullSet;
-
-            foreach (T item in this)
-            {
-                if (!setToCompare.Contains(item))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public HashSet<T> Intersection(HashSet<T> set)
-        {
-            HashSet<T> intersectionSet = NullSet;
-
-            if (null == set)
-            {
-                return intersectionSet;
-            }
-
-            foreach (T item in this)
-            {
-                if (!set.Contains(item))
-                {
-                    continue;
-                }
-
-                intersectionSet.Add(item);
-            }
-
-            foreach (T item in set)
-            {
-                if (!Contains(item) || intersectionSet.Contains(item))
-                {
-                    continue;
-                }
-
-                intersectionSet.Add(item);
-            }
-
-            return intersectionSet;
-        }
-
-        public bool IsProperSubsetOf(HashSet<T> set)
-        {
-            HashSet<T> setToCompare = set ?? NullSet;
-
-            // A is a proper subset of a if the b is a subset of a and a != b
-            return (IsSubsetOf(setToCompare) && !setToCompare.IsSubsetOf(this));
-        }
-
-        public bool IsSupersetOf(HashSet<T> set)
-        {
-            HashSet<T> setToCompare = set ?? NullSet;
-
-            foreach (T item in setToCompare)
-            {
-                if (!Contains(item))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public bool IsProperSupersetOf(HashSet<T> set)
-        {
-            HashSet<T> setToCompare = set ?? NullSet;
-
-            // B is a proper superset of a if b is a superset of a and a != b
-            return (IsSupersetOf(setToCompare) && !setToCompare.IsSupersetOf(this));
         }
 
         public List<T> ToList()
         {
             return new List<T>(this);
         }
-
-        #region Implementation of ISerializable
-
-        /// <summary>
-        /// Populates a <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with the data needed to serialize the target object.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> to populate with data. </param><param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext"/>) for this serialization. </param><exception cref="T:System.Security.SecurityException">The caller does not have the required permission. </exception>
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            if (info == null) throw new ArgumentNullException("info");
-            dict.GetObjectData(info, context);
-        }
-
-        #endregion
-
-        #region Implementation of IDeserializationCallback
-
-        /// <summary>
-        /// Runs when the entire object graph has been deserialized.
-        /// </summary>
-        /// <param name="sender">The object that initiated the callback. The functionality for this parameter is not currently implemented. </param>
-        public void OnDeserialization(object sender)
-        {
-            dict.OnDeserialization(sender);
-        }
-
-        #endregion
 
         #region Implementation of IEnumerable
 
@@ -267,7 +138,7 @@ namespace GitUI
         /// <filterpriority>1</filterpriority>
         public IEnumerator<T> GetEnumerator()
         {
-            return dict.Keys.GetEnumerator();
+            return dict.GetEnumerator();
         }
 
         /// <summary>
