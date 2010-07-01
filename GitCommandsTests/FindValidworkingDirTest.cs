d6f75ee77c9d77e00fc8fdbf2eee75abb49acdfe
Henk Westhuis <henk_westhuis@hotmail.com>
2010-07-01 18:48:53 +0200

    Added unit test for FindValidworkingDirTest function

---
+++ GitCommandsTests/FindValidworkingDirTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using GitCommands;
+
+namespace GitCommandsTests
+{
+    /// <summary>
+
+    /// </summary>
+    [TestClass]
+    public class FindValidworkingDirTest
+    {
+        private string GetCurrentDir()
+        {
+            string path = typeof(FindValidworkingDirTest).Assembly.Location;
+
+            return path.Substring(0, path.LastIndexOf('\\'));
+        }
+
+
+        [TestMethod]
+        public void TestWorkingDir()
+        {
+            Settings.WorkingDir = GetCurrentDir();
+            CheckWorkingDir();
+            Settings.WorkingDir = GetCurrentDir() + "\\testfile.txt";
+            CheckWorkingDir();
+            Settings.WorkingDir = GetCurrentDir() + "\\";
+            CheckWorkingDir();
+            Settings.WorkingDir = GetCurrentDir() + "\\\\";
+            CheckWorkingDir();
+            Settings.WorkingDir = GetCurrentDir() + "\\test\\test\\tralala";
+            CheckWorkingDir();
+
+        }
+
+        private static void CheckWorkingDir()
+        {
+            //Should not contain double slashes -> \\
+            Assert.IsFalse(Settings.WorkingDir.Contains("\\\\"));
+
+            //Should end with slash
+            Assert.IsTrue(Settings.WorkingDir.EndsWith("\\"));
+        }
+    }
+}
