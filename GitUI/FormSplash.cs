e0fa62f61964802dcb4ba4d146683a09ae7ebec4
Henk Westhuis <henk_westhuis@hotmail.com>
2010-07-07 18:59:36 +0200

    Issue 95: colored application icons broken.

--- GitUI/FormSplash.cs
+++ GitUI/FormSplash.cs
@@ -10,13 +10,17 @@ using GitCommands;
 
 namespace GitUI
 {
-    public partial class FormSplash : GitExtensionsForm
+    /// <summary>
+    /// DO NOT INHERIT FORM GITEXTENIONSFORM SINCE THE SETTINGS ARE NOT YET LOADED WHEN THIS
+    /// FORM IS SHOWN! TRANSLATIONS AND COLORED APPLICATION ICONS WILL BREAK!!!!
+    /// </summary>
+    public partial class FormSplash : Form
     {
         TranslationString version = new TranslationString("Version {0}");
 
         public FormSplash()
         {
-            InitializeComponent(); Translate();
+            InitializeComponent(); 
         }
 
         public void SetAction(string action)
