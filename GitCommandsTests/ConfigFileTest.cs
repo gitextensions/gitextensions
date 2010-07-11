dc4726dac7df6d7028802e59047762178711cd33
dominiqueplante <dominique.plante@gmail.com>
2010-07-11 11:18:53 -0700

    cleanups
    Signed-off-by: Henk Westhuis <henk_westhuis@hotmail.com>

--- GitCommandsTests/ConfigFileTest.cs
+++ GitCommandsTests/ConfigFileTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using GitCommands;
