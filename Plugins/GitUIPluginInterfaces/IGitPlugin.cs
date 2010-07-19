a4365341bc5cc1c07eb170bc3e96f0e89168d909
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-19 16:03:32 +0200

    Cleanup PluginInterfaces

--- Plugins/GitUIPluginInterfaces/IGitPlugin.cs
+++ Plugins/GitUIPluginInterfaces/IGitPlugin.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GitUIPluginInterfaces
+﻿namespace GitUIPluginInterfaces
 {
     public interface IGitPlugin
     {
         string Description { get; }
 
-        IGitPluginSettingsContainer Settings { get; set;  }
+        IGitPluginSettingsContainer Settings { get; set; }
 
-        void Register(IGitUICommands gitUICommands);
-        void Execute(IGitUIEventArgs gitUICommands);
-    }
-
-    public interface IGitPluginSettingsContainer
-    {
-        void AddSetting(string name, string defaultValue);
-        void SetSetting(string name, string value);
-        string GetSetting(string name);
-        IList<string> GetAvailableSettings();
+        void Register(IGitUICommands gitUiCommands);
+        void Execute(IGitUIEventArgs gitUiCommands);
     }
 }
---
+++ Plugins/GitUIPluginInterfaces/IGitPluginSettingsContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GitUIPluginInterfaces
+{
+    public interface IGitPluginSettingsContainer
+    {
+        void AddSetting(string name, string defaultValue);
+        void SetSetting(string name, string value);
+        string GetSetting(string name);
+        IList<string> GetAvailableSettings();
+    }
+}
