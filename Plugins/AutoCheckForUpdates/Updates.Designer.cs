bf2e9fcddc7dd6f262bb29f318ebff419555674a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-19 15:43:13 +0200

    Cleanup CheckForUpdates project.

--- Plugins/AutoCheckForUpdates/Updates.Designer.cs
+++ Plugins/AutoCheckForUpdates/Updates.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Updates));
             this.Close = new System.Windows.Forms.Button();
             this.UpdateLabel = new System.Windows.Forms.Label();
             this.link = new System.Windows.Forms.LinkLabel();
@@ -37,22 +36,22 @@
             // 
             // Close
             // 
-            this.Close.Location = new System.Drawing.Point(363, 103);
+            this.Close.Location = new System.Drawing.Point(365, 66);
             this.Close.Name = "Close";
             this.Close.Size = new System.Drawing.Size(75, 23);
             this.Close.TabIndex = 0;
             this.Close.Text = "Close";
             this.Close.UseVisualStyleBackColor = true;
-            this.Close.Click += new System.EventHandler(this.Close_Click);
+            this.Close.Click += new System.EventHandler(this.CloseClick);
             // 
             // UpdateLabel
             // 
             this.UpdateLabel.AutoSize = true;
             this.UpdateLabel.Location = new System.Drawing.Point(13, 13);
             this.UpdateLabel.Name = "UpdateLabel";
-            this.UpdateLabel.Size = new System.Drawing.Size(159, 13);
+            this.UpdateLabel.Size = new System.Drawing.Size(114, 13);
             this.UpdateLabel.TabIndex = 1;
-            this.UpdateLabel.Text = "There is a new version available";
+            this.UpdateLabel.Text = "Searching for updates.";
             // 
             // link
             // 
@@ -63,13 +62,13 @@
             this.link.TabIndex = 2;
             this.link.TabStop = true;
             this.link.Text = "version";
-            this.link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_LinkClicked);
+            this.link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkClicked);
             // 
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(16, 37);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(422, 23);
+            this.progressBar1.Size = new System.Drawing.Size(424, 23);
             this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progressBar1.TabIndex = 3;
             // 
@@ -77,17 +76,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 131);
+            this.ClientSize = new System.Drawing.Size(452, 100);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.link);
             this.Controls.Add(this.UpdateLabel);
             this.Controls.Add(this.Close);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Updates";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Check for update";
-            this.Load += new System.EventHandler(this.Updates_Load);
-            this.Shown += new System.EventHandler(this.Updates_Shown);
+            this.Shown += new System.EventHandler(this.UpdatesShown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
