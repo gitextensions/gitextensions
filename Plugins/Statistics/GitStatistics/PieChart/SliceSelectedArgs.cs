d77782c4ad9bc4652bf5d08eae2cefbe32126468
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-20 09:09:14 +0200

    Cleanup PieChartControl

---
+++ Plugins/Statistics/GitStatistics/PieChart/SliceSelectedArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GitStatistics.PieChart
+{
+    public delegate void SliceSelectedHandler(object sender, SliceSelectedArgs e);
+
+    public class SliceSelectedArgs : EventArgs
+    {
+        public object Tag;
+        public string Tooltip;
+        public decimal Value;
+
+        public SliceSelectedArgs(decimal val, string hint)
+        {
+            Value = val;
+            Tooltip = hint;
+        }
+
+        public SliceSelectedArgs(decimal val, string hint, Object t)
+        {
+            Value = val;
+            Tooltip = hint;
+            Tag = t;
+        }
+    }
+}
