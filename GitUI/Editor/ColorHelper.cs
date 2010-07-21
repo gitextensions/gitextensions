7e370c8361441eaf5024ccfa4f7c86507897565a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-21 12:17:56 +0200

    Moving ColorHelper into Editor subfolder.

---
+++ GitUI/Editor/ColorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace GitUI.Editor
+{
+    public static class ColorHelper
+    {
+        public static Color GetForeColorForBackColor(Color backColor)
+        {
+            if ((backColor.R > 130 ||
+                 backColor.B > 130 ||
+                 backColor.G > 130) &&
+                backColor.R + backColor.B + backColor.G > 350)
+                return Color.Black;
+            return Color.White;
+        }
+    }
+}
