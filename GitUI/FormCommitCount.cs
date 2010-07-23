25c863c6820ea92aec340aa28c4fc45c9376d0f7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-23 16:30:50 +0200

    Using CommitCounter.GroupAllCommitsByContributer() for FormCommitCount.

--- GitUI/FormCommitCount.cs
+++ GitUI/FormCommitCount.cs
@@ -1,56 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Threading;
 using System.Windows.Forms;
+using GitCommands.Statistics;
 
 namespace GitUI
 {
     public partial class FormCommitCount : GitExtensionsForm
     {
-        private readonly SynchronizationContext syncContext;
-
         public FormCommitCount()
         {
-            syncContext = SynchronizationContext.Current;
-
-            InitializeComponent(); Translate();
+            InitializeComponent();
+            Translate();
         }
 
-        ~FormCommitCount()
-        {
-            cmd.Kill();
-        }
-
-        private void FormCommitCount_FormClosing(object sender, FormClosingEventArgs e)
+        private void FormCommitCountFormClosing(object sender, FormClosingEventArgs e)
         {
             SavePosition("commit-count");
         }
 
-        private GitCommands.GitCommands cmd = new GitCommands.GitCommands();
-        private void FormCommitCount_Load(object sender, EventArgs e)
+        private void FormCommitCountLoad(object sender, EventArgs e)
         {
             RestorePosition("commit-count");
             Loading.Visible = true;
-            cmd.Exited += new EventHandler(cmd_Exited);
-            //cmd.CmdStartProcess(GitCommands.Settings.GitCommand, "shortlog -s -n");
-            cmd.CmdStartProcess("cmd.exe", "/c \"\"" + GitCommands.Settings.GitCommand + "\" log --all --pretty=short | \"" + GitCommands.Settings.GitCommand + "\" shortlog --all -s -n\"");
-            
-           
-        }
 
-        void SetCount()
-        {
-            CommitCount.Text = cmd.Output.ToString();
+            foreach (var keyValuePair in CommitCounter.GroupAllCommitsByContributer().Item1)
+            {
+                CommitCount.Text += string.Format("{1,6} - {0}\r\n", keyValuePair.Key, keyValuePair.Value);
+            }
+
             Loading.Visible = false;
-        }
-
-        void cmd_Exited(object sender, EventArgs e)
-        {
-            syncContext.Post(_ => SetCount(), null);
         }
     }
 }
