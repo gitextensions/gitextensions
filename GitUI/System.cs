ef320b34752a18d1d50e4dde2684882544d483dd
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-23 09:52:14 +0200

    Moving System extensions into GitCommands project and adding support for System.Tuple.

--- GitUI/System.cs
+++
@@ -1,9 +0,0 @@
-// Copy of some .NET 3.5 delegates and attributes
-// Remove if GitExtensions starts targetting a newer version of .NET
-
-namespace System
-{
-    public delegate void Action();
-
-    public delegate T Func<T>();
-}
