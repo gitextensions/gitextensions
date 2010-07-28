185d346f2aee0c1d1b61f744f9578cc2cdd24f00
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-28 21:02:42 +0200

    Cleanup FormInit

--- GitUI/FormInit.cs
+++ GitUI/FormInit.cs
@@ -1,79 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using System.IO;
+using System.Windows.Forms;
+using GitCommands;
 using ResourceManager.Translation;
 
 namespace GitUI
 {
     public partial class FormInit : GitExtensionsForm
     {
-        TranslationString chooseDirectory = new TranslationString("Please choose a directory.");
-        TranslationString chooseDirectoryNotFile = new TranslationString("Cannot initialize a new repository on a file.\nPlease choose a directory.");
+        private readonly TranslationString _chooseDirectory =
+            new TranslationString("Please choose a directory.");
+
+        private readonly TranslationString _chooseDirectoryNotFile =
+            new TranslationString("Cannot initialize a new repository on a file.\nPlease choose a directory.");
 
         public FormInit(string dir)
         {
-            InitializeComponent(); Translate();
+            InitializeComponent();
+            Translate();
             Directory.Text = dir;
         }
 
         public FormInit()
         {
-            InitializeComponent(); Translate();
+            InitializeComponent();
+            Translate();
 
-            if (!GitCommands.Settings.ValidWorkingDir())
-                Directory.Text = GitCommands.Settings.WorkingDir;
+            if (!Settings.ValidWorkingDir())
+                Directory.Text = Settings.WorkingDir;
         }
 
-        private void Directory_DropDown(object sender, EventArgs e)
+        private void DirectoryDropDown(object sender, EventArgs e)
         {
-            Directory.DataSource = GitCommands.Repositories.RepositoryHistory.Repositories;
+            Directory.DataSource = Repositories.RepositoryHistory.Repositories;
             Directory.DisplayMember = "Path";
         }
 
-        private void Init_Click(object sender, EventArgs e)
+        private void InitClick(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(Directory.Text))
             {
-                MessageBox.Show(chooseDirectory.Text);
+                MessageBox.Show(_chooseDirectory.Text);
                 return;
             }
 
             if (File.Exists(Directory.Text))
             {
-                MessageBox.Show(chooseDirectoryNotFile.Text);
+                MessageBox.Show(_chooseDirectoryNotFile.Text);
                 return;
             }
 
-            GitCommands.Settings.WorkingDir = Directory.Text;
+            Settings.WorkingDir = Directory.Text;
 
-            if (!System.IO.Directory.Exists(GitCommands.Settings.WorkingDir))
-                System.IO.Directory.CreateDirectory(GitCommands.Settings.WorkingDir);
-            
+            if (!System.IO.Directory.Exists(Settings.WorkingDir))
+                System.IO.Directory.CreateDirectory(Settings.WorkingDir);
+
             MessageBox.Show(GitCommands.GitCommands.Init(Central.Checked, Central.Checked), "Initialize new repository");
 
-            GitCommands.Repositories.RepositoryHistory.AddMostRecentRepository(Directory.Text);
-            
+            Repositories.RepositoryHistory.AddMostRecentRepository(Directory.Text);
+
             Close();
         }
 
-        private void Browse_Click(object sender, EventArgs e)
+        private void BrowseClick(object sender, EventArgs e)
         {
-            FolderBrowserDialog browseDialog = new FolderBrowserDialog();
-            
+            var browseDialog = new FolderBrowserDialog();
+
             if (browseDialog.ShowDialog() == DialogResult.OK)
-            {
                 Directory.Text = browseDialog.SelectedPath;
-            }
-        }
-
-        private void FormInit_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
