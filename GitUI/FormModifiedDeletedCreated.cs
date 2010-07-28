bf1d263d1b09f7215bd02216286081546135bd30
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-28 21:00:43 +0200

    Cleanup FormModifiedDeletedCreated

--- GitUI/FormModifiedDeletedCreated.cs
+++ GitUI/FormModifiedDeletedCreated.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 
 namespace GitUI
 {
@@ -12,28 +6,29 @@ namespace GitUI
     {
         public FormModifiedDeletedCreated()
         {
-            InitializeComponent(); Translate();
+            InitializeComponent();
+            Translate();
             Aborted = true;
         }
 
         public bool Aborted { get; set; }
         public bool Delete { get; set; }
 
-        private void Created_Click(object sender, EventArgs e)
+        private void CreatedClick(object sender, EventArgs e)
         {
             Aborted = false;
             Delete = false;
             Close();
         }
 
-        private void Deleted_Click(object sender, EventArgs e)
+        private void DeletedClick(object sender, EventArgs e)
         {
             Aborted = false;
             Delete = true;
             Close();
         }
 
-        private void Abort_Click(object sender, EventArgs e)
+        private void AbortClick(object sender, EventArgs e)
         {
             Aborted = true;
             Close();
