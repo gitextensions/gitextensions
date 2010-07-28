bf8751d42b541ec510461b2c4ec57f94792b915a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-28 21:04:38 +0200

    Cleanup FormDonate

--- GitUI/FormDonate.Designer.cs
+++ GitUI/FormDonate.Designer.cs
@@ -75,7 +75,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(93, 37);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Click += new System.EventHandler(PictureBox1Click);
             // 
             // FormDonate
             // 
