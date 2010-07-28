adc27753ba68b929b5c6b8a2fadcf489a00d9575
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-28 21:04:38 +0200

    Cleanup FormDonate

--- GitUI/FormDonate.cs
+++ GitUI/FormDonate.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace GitUI
 {
     public partial class FormDonate : GitExtensionsForm
     {
+        private const string DonationUrl =
+            @"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WAL2SSDV8ND54&lc=US&item_name=GitExtensions&no_note=1&no_shipping=1&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted";
+
         public FormDonate()
         {
-            InitializeComponent(); Translate();
+            InitializeComponent();
+            Translate();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private static void PictureBox1Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start(@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WAL2SSDV8ND54&lc=US&item_name=GitExtensions&no_note=1&no_shipping=1&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted");
+            Process.Start(DonationUrl);
         }
     }
 }
