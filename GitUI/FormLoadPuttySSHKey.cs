a1f462bc51d8faa98aee4cd32d9d1de733960bbd
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-08-03 18:22:21 +0200

    Cleanup FormLoadPuttySSHKey.

--- GitUI/FormLoadPuttySSHKey.cs
+++ GitUI/FormLoadPuttySSHKey.cs
@@ -1,54 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using System.IO;
+using System.Windows.Forms;
+using GitCommands;
 using GitCommands.Repository;
 
 namespace GitUI
 {
-    public partial class FormLoadPuttySSHKey : GitExtensionsForm
+    public partial class FormLoadPuttySshKey : GitExtensionsForm
     {
-        public FormLoadPuttySSHKey()
+        public FormLoadPuttySshKey()
         {
-            InitializeComponent(); Translate();
+            InitializeComponent();
+            Translate();
         }
 
-        private void PrivateKeypath_DropDown(object sender, EventArgs e)
+        private void PrivateKeypathDropDown(object sender, EventArgs e)
         {
             PrivateKeypath.DataSource = Repositories.RepositoryHistory.Repositories;
             PrivateKeypath.DisplayMember = "Path";
-
         }
 
-        private void LoadSSHKey_Click(object sender, EventArgs e)
+        private void LoadSshKeyClick(object sender, EventArgs e)
         {
-            if (!File.Exists(GitCommands.Settings.Pageant))
-                MessageBox.Show("Cannot load SSH key. PuTTY is not configured properly.", "PuTTY");
-            else
+            if (!File.Exists(Settings.Pageant))
             {
-                Repositories.RepositoryHistory.AddMostRecentRepository(PrivateKeypath.Text);
-                if (!string.IsNullOrEmpty(PrivateKeypath.Text))
-                {
-                    GitCommands.GitCommands.StartPageantWithKey(PrivateKeypath.Text);
-                    Close();
-                }
-                else
-                {
-                    MessageBox.Show("Could not load key.", "PuTTY");
-                }
+                MessageBox.Show("Cannot load SSH key. PuTTY is not configured properly.", "PuTTY");
+                return;
             }
+
+            Repositories.RepositoryHistory.AddMostRecentRepository(PrivateKeypath.Text);
+            if (string.IsNullOrEmpty(PrivateKeypath.Text))
+            {
+                MessageBox.Show("Could not load key.", "PuTTY");
+                return;
+            }
+
+            GitCommands.GitCommands.StartPageantWithKey(PrivateKeypath.Text);
+            Close();
         }
 
-        private void Browse_Click(object sender, EventArgs e)
+        private void BrowseClick(object sender, EventArgs e)
         {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "Private key (*.ppk)|*.ppk";
-            dialog.InitialDirectory = ".";
-            dialog.Title = "Select ssh key file";
+            var dialog = new OpenFileDialog
+                             {
+                                 Filter = "Private key (*.ppk)|*.ppk",
+                                 InitialDirectory = ".",
+                                 Title = "Select ssh key file"
+                             };
             if (dialog.ShowDialog() == DialogResult.OK)
                 PrivateKeypath.Text = dialog.FileName;
         }
