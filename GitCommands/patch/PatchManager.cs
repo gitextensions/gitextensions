7d8e0bda2f11aa4b9b34391a99b779062093fed3
Henk Westhuis <henk_westhuis@hotmail.com>
2010-08-29 12:51:49 +0200

    ﻿WIP: Fix encoding issues

--- GitCommands/patch/PatchManager.cs
+++ GitCommands/patch/PatchManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using GitCommands;
+using System.Text.RegularExpressions;
 
 namespace PatchApply
 {
@@ -297,8 +298,12 @@ namespace PatchApply
                     patch = new Patch();
                     patches.Add(patch);
 
-                    patch.FileNameA = input.Substring(input.LastIndexOf(" a/") + 3, input.LastIndexOf(" b/") - (input.LastIndexOf(" a/") + 3));
-                    patch.FileNameB = input.Substring(input.LastIndexOf(" b/") + 3);
+                    Match match = Regex.Match(input, "[ ][\"]{0,1}[a]/(.*)[\"]{0,1}[ ][\"]{0,1}[b]/(.*)[\"]{0,1}");
+
+                    patch.FileNameA = match.Groups[1].Value;
+                    patch.FileNameB = match.Groups[2].Value;
+                    //patch.FileNameA = input.Substring(input.LastIndexOf(" a/") + 3, input.LastIndexOf(" b/") - (input.LastIndexOf(" a/") + 3));
+                    //patch.FileNameB = input.Substring(input.LastIndexOf(" b/") + 3);
 
                     //The next line tells us what kind of patch
                     //new file mode xxxxxx means new file
