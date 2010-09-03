556ea0877eb42903a16026b2c880685c46ef8a28
Seth Behunin <Seth.A.Behunin@disney.com>
2010-09-03 10:50:24 -0600

    Look up actual file system casing of a file that has been passed into the history form.  This fixes issues where a VS project may have a file that is on disk as c:\myFile.cpp but in the solution it has the file path as c:\MyFile.cpp.
    It only works for Windows unfortunately so it will need to be reworked if this is ever to run on another platform using Mono.

--- GitUI/FormFileHistory.cs
+++ GitUI/FormFileHistory.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
 using GitCommands;
 using PatchApply;
@@ -35,8 +38,22 @@ namespace GitUI
             if (string.IsNullOrEmpty(fileName))
                 return;
 
-            if (fileName.StartsWith(Settings.WorkingDir, StringComparison.InvariantCultureIgnoreCase))
-                fileName = fileName.Substring(Settings.WorkingDir.Length);
+            // we will need this later to look up proper casing for the file
+            string fullFilePath = fileName;
+
+            if (!fileName.StartsWith(Settings.WorkingDir, StringComparison.InvariantCultureIgnoreCase))
+                fullFilePath = Path.Combine(Settings.WorkingDir, fileName);
+
+            // grab the 8.3 file path
+            StringBuilder shortPath = new StringBuilder(4096);
+            GetShortPathName(fullFilePath, shortPath, shortPath.Capacity);
+
+            // use 8.3 file path to get properly cased full file path
+            StringBuilder longPath = new StringBuilder(4096);
+            GetLongPathName(shortPath.ToString(), longPath, longPath.Capacity);
+
+            // remove the working dir and now we have a properly cased file name.
+            fileName = longPath.ToString().Substring(Settings.WorkingDir.Length);
 
             FileName = fileName;
 
@@ -49,7 +66,12 @@ namespace GitUI
                 FileChanges.AllowGraphWithFilter = true;
             }
         }
+ 
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern uint GetShortPathName(string lpszLongPath, StringBuilder lpszShortPath, int cchBuffer);
 
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetLongPathName(string lpszShortPath, StringBuilder lpszLongPath, int cchBuffer);
 
         private void DiffExtraDiffArgumentsChanged(object sender, EventArgs e)
         {
