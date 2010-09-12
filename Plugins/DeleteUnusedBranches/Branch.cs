f579abf2adac4706679f467bb45c7c4f1486b85e
Henk Westhuis <henk_westhuis@hotmail.com>
2010-09-12 13:59:02 +0200

    Added "Delete obsolete branches" plugin

---
+++ Plugins/DeleteUnusedBranches/Branch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleteUnusedBranches
+{
+    public class Branch
+    {
+        public Branch(string name, DateTime date, bool delete)
+        {
+            Name = name;
+            Date = date;
+            Delete = delete;
+        }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public bool Delete { get; set; }
+        public string Result { get; set; }
+    }
+}
---
+++ Plugins/DeleteUnusedBranches/DeleteUnusedBranchesPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitUIPluginInterfaces;
+
+namespace DeleteUnusedBranches
+{
+    public class DeleteUnusedBranchesPlugin : IGitPlugin
+    {
+        public string Description
+        {
+            get { return "Delete obsolete branches"; }
+        }
+
+        public IGitPluginSettingsContainer Settings { get; set; }
+
+        public void Register(IGitUICommands gitUiCommands)
+        {
+            Settings.AddSetting("Delete obsolete branches older than (days)", "30");
+        }
+
+        public void Execute(IGitUIEventArgs gitUiCommands)
+        {
+            int days;
+            if (!int.TryParse(Settings.GetSetting("Delete obsolete branches older than (days)"), out days))
+                days = 30;
+
+            new DeleteUnusedBranchesForm(days, gitUiCommands.GitCommands).ShowDialog();
+        }
+    }
+}
