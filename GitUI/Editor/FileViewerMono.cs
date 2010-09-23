27e9ad8da12cb632e70d332a430f036ac69c9228
Henk Westhuis <henk_westhuis@hotmail.com>
2010-09-23 20:14:10 +0200

    Repaired blame control

--- GitUI/Editor/FileViewerMono.cs
+++ GitUI/Editor/FileViewerMono.cs
@@ -21,6 +21,30 @@ namespace GitUI.Editor
             TextEditor.TextChanged += new EventHandler(TextEditor_TextChanged);
             TextEditor.VScroll += new EventHandler(TextEditor_VScroll);
             TextEditor.WordWrap = false;
+            TextEditor.DoubleClick += new EventHandler(TextEditor_DoubleClick);
+            TextEditor.MouseDown += new MouseEventHandler(TextEditor_MouseDown);
+            TextEditor.BackColor = Color.White;
+        }
+
+        public event SelectedLineChangedHandler SelectedLineChanged;
+
+        void OnSelectedLineChanged(int selectedLine)
+        {
+            if (SelectedLineChanged != null)
+                SelectedLineChanged(this, selectedLine);
+        }
+
+        void TextEditor_MouseDown(object sender, MouseEventArgs e)
+        {
+            OnSelectedLineChanged(TextEditor.GetLineFromCharIndex(TextEditor.GetCharIndexFromPosition(new Point(e.X, e.Y))));
+        }
+
+        public new event EventHandler DoubleClick;
+
+        void TextEditor_DoubleClick(object sender, EventArgs e)
+        {
+            if (DoubleClick != null)
+                DoubleClick(sender, e);
         }
 
         void TextEditor_VScroll(object sender, EventArgs e)
@@ -59,22 +83,14 @@ namespace GitUI.Editor
 
         public void SetText(string text)
         {
-            TextEditor.Text = text;
-        }
+            TextEditor.Clear();
 
-        public void SetHighlighting(string syntax)
-        {
-            
+            TextEditor.Text = text;
         }
 
         public string GetSelectedText()
         {
             return TextEditor.SelectedText;
-        }
-
-        public void AddPatchHighlighting()
-        {
-            
         }
 
         public int ScrollPos
@@ -85,7 +101,7 @@ namespace GitUI.Editor
             }
             set
             {
-                
+                //TextEditor.ScrollBars   
             }
         }
 
@@ -160,6 +176,110 @@ namespace GitUI.Editor
             }
         }
 
+
+        public void AddPatchHighlighting()
+        {
+            //I modified the code below to add syntax highlighting to the RTFEditor. This 
+            //does work on Windows, but it doesn't work on Linux...:(
+            /*
+            TextEditor.SuspendLayout();
+            int lineNumber = 0;
+            foreach(string line in TextEditor.Lines)
+            {
+                if (line.Length == 0)
+                    continue;
+
+                if (line[0] == '+')
+                {
+                    var color = Settings.DiffAddedColor;
+                    TextEditor.Select(TextEditor.GetFirstCharIndexFromLine(lineNumber), line.Length);
+                    TextEditor.SelectionColor = ColorHelper.GetForeColorForBackColor(color);
+                    TextEditor.SelectionBackColor = color;
+
+                }
+                if (line[0] == '-')
+                {
+                    var color = Settings.DiffRemovedColor;
+                    TextEditor.Select(TextEditor.GetFirstCharIndexFromLine(lineNumber), line.Length);
+                    TextEditor.SelectionColor = ColorHelper.GetForeColorForBackColor(color);
+                    TextEditor.SelectionBackColor = color;
+                }
+                if (line[0] == '@')
+                {
+                    var color = Settings.DiffSectionColor;
+                    TextEditor.Select(TextEditor.GetFirstCharIndexFromLine(lineNumber), line.Length);
+                    TextEditor.SelectionColor = ColorHelper.GetForeColorForBackColor(color);
+                    TextEditor.SelectionBackColor = color;
+                }
+                lineNumber++;
+            }
+            AddExtraPatchHighlighting();
+            TextEditor.ResumeLayout();
+            TextEditor.Refresh();
+        }
+
+        private void AddExtraPatchHighlighting()
+        {
+            for (var line = 0; line + 3 < TextEditor.Lines.GetLength(0); line++)
+            {
+                var lineSegment1 = TextEditor.Lines[line];
+                var lineSegment2 = TextEditor.Lines[line+1];
+                var lineSegment3 = TextEditor.Lines[line+2];
+                var lineSegment4 = TextEditor.Lines[line+3];
+
+                if (lineSegment1[0] != ' ' ||
+                    lineSegment2[0] != '-' ||
+                    lineSegment3[0] != '+' ||
+                    lineSegment4[0] != ' ')
+                    continue;
+
+                var beginOffset = 0;
+                var endOffset = lineSegment3.Length;
+                var reverseOffset = 0;
+
+                for (; beginOffset < endOffset; beginOffset++)
+                {
+                    if (lineSegment3.Length > beginOffset &&
+                        lineSegment2.Length > beginOffset &&
+                        !lineSegment3[beginOffset].Equals('+') &&
+                        !lineSegment2[beginOffset].Equals('-') &&
+                        !lineSegment3[beginOffset].Equals(lineSegment2[beginOffset]))
+                        break;
+                }
+
+                for (; endOffset > beginOffset; endOffset--)
+                {
+                    reverseOffset = lineSegment3.Length - endOffset;
+
+                    if (!lineSegment3[lineSegment3.Length - 1 - reverseOffset].Equals('+') &&
+                        !lineSegment2[lineSegment2.Length - 1 - reverseOffset].Equals('-') &&
+                        !lineSegment3[lineSegment3.Length - 1 - reverseOffset].Equals(lineSegment2[lineSegment2.Length - 1 - reverseOffset]))
+                        break;
+                }
+
+                Color color;
+                if (lineSegment3.Length - beginOffset - reverseOffset > 0)
+                {
+                    color = Settings.DiffAddedExtraColor;
+                    TextEditor.Select(TextEditor.GetFirstCharIndexFromLine(line + 2) + beginOffset, lineSegment3.Length - beginOffset - reverseOffset);
+                    TextEditor.SelectionColor = ColorHelper.GetForeColorForBackColor(color);
+                    TextEditor.SelectionBackColor = color;
+                }
+
+                if (lineSegment2.Length - beginOffset - reverseOffset > 0)
+                {
+                    color = Settings.DiffRemovedExtraColor;
+                    TextEditor.Select(TextEditor.GetFirstCharIndexFromLine(line + 1) + beginOffset, lineSegment2.Length - beginOffset - reverseOffset);
+                    TextEditor.SelectionColor = ColorHelper.GetForeColorForBackColor(color);
+                    TextEditor.SelectionBackColor = color;
+                }
+                TextEditor.Select(0, 0);
+            }*/
+        }
+
+        public void SetHighlighting(string  s)
+        {
+        }
 
         #endregion
     }
