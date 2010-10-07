9c7042653f4c10ce9b2ff1095d279a5d4da75191
Seth Behunin <Seth.A.Behunin@disney.com>
2010-10-07 14:47:33 -0600

    Detect and display proper "pluralness" of stash label.

--- GitUI/FormBrowse.cs
+++ GitUI/FormBrowse.cs
@@ -254,7 +254,8 @@ namespace GitUI
             }
 
 
-            toolStripSplitStash.Text = string.Format("{0} saved stashes", GitCommands.GitCommands.GetStashes().Count);
+            int stashCount = GitCommands.GitCommands.GetStashes().Count;
+            toolStripSplitStash.Text = string.Format("{0} saved {1}", stashCount, stashCount != 1 ? "stashes" : "stash");
             //Only show status strip when there are status items on it.
             //There is always a close (x) button, do not count first item.
             if (statusStrip.Items.Count > 1)
