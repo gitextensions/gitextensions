9cbd7b9d230db4e73872e64508cc9a7aba18a1ab
Kevin Moore <kevin@thinkpixellab.com>
2010-10-18 14:12:50 -0700

    Whitespace and using clean-up

--- GitPlugin/Commands/ToolbarCommand.cs
+++ GitPlugin/Commands/ToolbarCommand.cs
@@ -1,46 +1,45 @@
+using System.IO;
 // Copyright (C) 2006-2008 Jim Tilander. See COPYING for and README for more details.
-using System;
 using EnvDTE;
 using EnvDTE80;
-using System.IO;
 
 namespace GitPlugin.Commands
 {
-        class ToolbarCommand<ItemCommandT> : CommandBase
-            where ItemCommandT : ItemCommandBase, new()
+    class ToolbarCommand<ItemCommandT> : CommandBase
+        where ItemCommandT : ItemCommandBase, new()
+    {
+        public override void OnCommand(DTE2 application, OutputWindowPane pane)
         {
-            public override void OnCommand(DTE2 application, OutputWindowPane pane)
-            {
-                new ItemCommandT().OnCommand(application, pane);
-            }
-
-            static public string ResolveFileNameWithCase(string fullpath)
-			{
-				string dirname = Path.GetDirectoryName(fullpath);
-				string basename = Path.GetFileName(fullpath).ToLower();
-				DirectoryInfo info = new DirectoryInfo(dirname);
-				FileInfo[] files = info.GetFiles();
-
-				foreach(FileInfo file in files)
-				{
-					if(file.Name.ToLower() == basename)
-					{
-						return file.FullName;
-					}
-				}
-				
-				// Should never happen...
-				return fullpath;
-			}
-
-
-
-
-
-            public override bool IsEnabled(DTE2 application)
-            {
-                return new ItemCommandT().IsEnabled(application);
-            }
+            new ItemCommandT().OnCommand(application, pane);
         }
+
+        static public string ResolveFileNameWithCase(string fullpath)
+        {
+            string dirname = Path.GetDirectoryName(fullpath);
+            string basename = Path.GetFileName(fullpath).ToLower();
+            DirectoryInfo info = new DirectoryInfo(dirname);
+            FileInfo[] files = info.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                if (file.Name.ToLower() == basename)
+                {
+                    return file.FullName;
+                }
+            }
+
+            // Should never happen...
+            return fullpath;
+        }
+
+
+
+
+
+        public override bool IsEnabled(DTE2 application)
+        {
+            return new ItemCommandT().IsEnabled(application);
+        }
+    }
 
 }
