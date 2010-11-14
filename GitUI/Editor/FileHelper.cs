ae32022fe443990effa3e597be658b9122b14359
cocytus <bem-github@farlig.org>
2010-11-14 03:47:30 +0100

    FileHelper: IsBinaryFile now reads and honors binary file specs in .gitattributes.

--- GitUI/Editor/FileHelper.cs
+++ GitUI/Editor/FileHelper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace GitUI.Editor
 {
@@ -47,7 +51,31 @@ namespace GitUI.Editor
 
         public static bool IsBinaryFile(string fileName)
         {
-            return HasMatchingExtension(BinaryExtensions, fileName);
+            return HasMatchingExtension(BinaryExtensions, fileName) || IsBinaryAccordingToGitAttributes(fileName);
+        }
+
+        private static bool IsBinaryAccordingToGitAttributes(string fileName)
+        {
+            string gitAttributesPath = Path.Combine(GitCommands.Settings.WorkingDir, ".gitattributes");
+            if (File.Exists(gitAttributesPath))
+            {
+                string[] lines = File.ReadAllLines(gitAttributesPath);
+                foreach (var parts in lines.Select(line => line.Trim().Split(' ')))
+                {
+                    if (parts.Length < 2 || parts[0][0] == '#')
+                        continue;
+                    if (parts.Contains("binary") || parts.Contains("-text"))
+                        if (Regex.IsMatch(fileName, CreateRegexFromFilePattern(parts[0])))
+                            return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static string CreateRegexFromFilePattern(string pattern)
+        {
+            return pattern.Replace(".", "\\.").Replace("*", ".*").Replace("?", ".");
         }
 
         public static bool IsImage(string fileName)
