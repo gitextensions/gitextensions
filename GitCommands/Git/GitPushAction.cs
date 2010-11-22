78e5ad82c4057403e364b057d25a22bbd9d3d1f3
Patrick Earl <patearl@patearl.net>
2010-11-21 21:28:10 -0700

    Initial implementation of multi-branch push.

---
+++ GitCommands/Git/GitPushAction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitCommands
+{
+	public class GitPushAction
+	{
+		private string _localBranch;
+		private string _remoteBranch;
+		private bool _force;
+		private bool _delete;
+
+		/// <summary>
+		/// Push a local branch to a remote one, optionally forcing a non-fast-forward commit.
+		/// </summary>
+		/// <param name="fromBranch"></param>
+		/// <param name="toBranch"></param>
+		/// <param name="force"></param>
+		public GitPushAction(string fromBranch, string toBranch, bool force)
+		{
+			_localBranch = fromBranch;
+			_remoteBranch = toBranch;
+			_force = force;
+		}
+
+		/// <summary>
+		/// Push a delete of a remote branch.
+		/// </summary>
+		/// <param name="deleteBranch"></param>
+		public GitPushAction(string deleteBranch)
+		{
+			_remoteBranch = deleteBranch;
+			_delete = true;
+		}
+
+		public string Format()
+		{
+			if (_delete)
+				return ":" + _remoteBranch;
+
+			return (_force ? "+" : "") + _localBranch + ":" + _remoteBranch;
+		}
+	}
+}
