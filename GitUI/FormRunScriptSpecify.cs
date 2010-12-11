68bd502b790259d068d0edabc6b571690b94f55d
Radek Miazio <moomeen@gmail.com>
2010-12-11 20:22:10 +0100

    form for specifying branch when ambigious

---
+++ GitUI/FormRunScriptSpecify.cs
@@ -0,0 +1,31 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI
+{
+    public partial class FormRunScriptSpecify : Form
+    {
+        public FormRunScriptSpecify(List<GitCommands.GitHead> options, string label)
+        {
+            InitializeComponent();
+            specifyLabel.Text = "Specify '" + label+"':";
+            foreach (GitCommands.GitHead head in options)
+            {
+                branchesListView.Items.Add(head.Name);
+            }
+        }
+        public string ret { get; private set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ret = branchesListView.SelectedItems[0].Text;
+            this.Close();
+        }
+    }
+}
