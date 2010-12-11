96b58c9975dcd54f26d2b8a75ef2ef5270e00ad2
Henk Westhuis <henk_westhuis@hotmail.com>
2010-12-11 14:12:32 +0100

    Added icons to merge conflict dialog

--- GitUI/FormModifiedDeletedCreated.Designer.cs
+++ GitUI/FormModifiedDeletedCreated.Designer.cs
@@ -35,16 +35,20 @@
             this.Base = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.questionImage = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.questionImage)).BeginInit();
             this.SuspendLayout();
             // 
             // Label
             // 
             this.Label.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Label.Location = new System.Drawing.Point(3, 0);
+            this.Label.Location = new System.Drawing.Point(67, 0);
             this.Label.Name = "Label";
-            this.Label.Size = new System.Drawing.Size(488, 70);
+            this.Label.Size = new System.Drawing.Size(420, 59);
             this.Label.TabIndex = 0;
             this.Label.Text = "Use created or deleted file?";
             // 
@@ -53,7 +57,7 @@
             this.Local.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Local.Location = new System.Drawing.Point(3, 3);
             this.Local.Name = "Local";
-            this.Local.Size = new System.Drawing.Size(156, 21);
+            this.Local.Size = new System.Drawing.Size(157, 21);
             this.Local.TabIndex = 1;
             this.Local.Text = "Local";
             this.Local.UseVisualStyleBackColor = true;
@@ -62,9 +66,9 @@
             // Remote
             // 
             this.Remote.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Remote.Location = new System.Drawing.Point(165, 3);
+            this.Remote.Location = new System.Drawing.Point(166, 3);
             this.Remote.Name = "Remote";
-            this.Remote.Size = new System.Drawing.Size(156, 21);
+            this.Remote.Size = new System.Drawing.Size(157, 21);
             this.Remote.TabIndex = 2;
             this.Remote.Text = "Remote";
             this.Remote.UseVisualStyleBackColor = true;
@@ -84,7 +88,7 @@
             // Base
             // 
             this.Base.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Base.Location = new System.Drawing.Point(327, 3);
+            this.Base.Location = new System.Drawing.Point(329, 3);
             this.Base.Name = "Base";
             this.Base.Size = new System.Drawing.Size(158, 21);
             this.Base.TabIndex = 4;
@@ -97,14 +101,15 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.Label, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(494, 103);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(496, 98);
             this.tableLayoutPanel1.TabIndex = 5;
             // 
             // tableLayoutPanel2
@@ -117,18 +122,44 @@
             this.tableLayoutPanel2.Controls.Add(this.Remote, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.Base, 2, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 73);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 68);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(488, 27);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(490, 27);
             this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.Label, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.questionImage, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(490, 59);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // questionImage
+            // 
+            this.questionImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.questionImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.questionImage.Location = new System.Drawing.Point(0, 0);
+            this.questionImage.Margin = new System.Windows.Forms.Padding(0);
+            this.questionImage.Name = "questionImage";
+            this.questionImage.Size = new System.Drawing.Size(64, 59);
+            this.questionImage.TabIndex = 6;
+            this.questionImage.TabStop = false;
             // 
             // FormModifiedDeletedCreated
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 103);
+            this.ClientSize = new System.Drawing.Size(496, 98);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.Abort);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -139,6 +170,8 @@
             this.Text = "Solve mergeconflict";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.questionImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -152,5 +185,7 @@
         private System.Windows.Forms.Label Label;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox questionImage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
     }
 }
