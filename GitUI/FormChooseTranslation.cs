9c41db5973fb4d30554b898f22634b6e621512b8
Henk Westhuis <henk_westhuis@hotmail.com>
2010-12-19 10:21:00 +0100

    Added dialog to choose user interface language at start-up.
    This dialog is only shown once, when there is no language set.

---
+++ GitUI/FormChooseTranslation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GitCommands;
+
+namespace GitUI
+{
+    public partial class FormChooseTranslation : GitExtensionsForm
+    {
+        public FormChooseTranslation()
+        {
+            InitializeComponent();
+            Translate();
+        }
+
+        private void dutch_Click(object sender, EventArgs e)
+        {
+            Settings.Translation = "Dutch";
+            Close();
+        }
+
+        private void brittish_Click(object sender, EventArgs e)
+        {
+            Settings.Translation = "English";
+            Close();
+        }
+
+        private void italian_Click(object sender, EventArgs e)
+        {
+            Settings.Translation = "Italiano";
+            Close();
+        }
+
+        private void japanese_Click(object sender, EventArgs e)
+        {
+            Settings.Translation = "Japanese";
+            Close();
+        }
+
+        private void spanish_Click(object sender, EventArgs e)
+        {
+            Settings.Translation = "Spanish";
+            Close();
+        }
+
+        private void FormChooseTranslation_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Settings.Translation))
+                Settings.Translation = "English";
+        }
+    }
+}
