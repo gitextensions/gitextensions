8f2bef1a17c1b00f988997db7022fa2d801bf003
Bjørn Moe <bem-github@farlig.org>
2010-12-27 19:01:07 +0100

    LoadPlugins: Removed empty try-catch.

--- GitExtensions/LoadPlugins.cs
+++ GitExtensions/LoadPlugins.cs
@@ -28,53 +28,47 @@ namespace GitExtensions
         {
             //Github.GithubPlugin ghp = new Github.GithubPlugin();
 
-            try
-            {
-                var file = new FileInfo(Application.ExecutablePath);
+            var file = new FileInfo(Application.ExecutablePath);
 
-                // Only search for plugins in the plugins folder. This increases performance a little bit.
-                // In DEBUG search for plugins in the root folder to make debugging plugins easier.
+            // Only search for plugins in the plugins folder. This increases performance a little bit.
+            // In DEBUG search for plugins in the root folder to make debugging plugins easier.
 #if DEBUG
-                var plugins = file.Directory.GetFiles("*.dll", SearchOption.AllDirectories);
+            var plugins = file.Directory.GetFiles("*.dll", SearchOption.AllDirectories);
 #else
-                var plugins =
-                    Directory.Exists(Path.Combine(file.Directory.FullName, "Plugins"))
-                        ? file.Directory.GetFiles("Plugins\\*.dll", SearchOption.AllDirectories)
-                        : new FileInfo[] { };
+            var plugins =
+                Directory.Exists(Path.Combine(file.Directory.FullName, "Plugins"))
+                    ? file.Directory.GetFiles("Plugins\\*.dll", SearchOption.AllDirectories)
+                    : new FileInfo[] { };
 #endif
 
-                foreach (var pluginFile in plugins)
+            foreach (var pluginFile in plugins)
+            {
+                try
                 {
-                    try
+                    var types = Assembly.LoadFile(pluginFile.FullName).GetTypes();
+                    foreach (var type in types)
                     {
-                        var types = Assembly.LoadFile(pluginFile.FullName).GetTypes();
-                        foreach (var type in types)
-                        {
-                            if (!typeof(IGitPlugin).IsAssignableFrom(type) || type.IsInterface)
-                                continue;
+                        if (!typeof(IGitPlugin).IsAssignableFrom(type) || type.IsInterface)
+                            continue;
 
-                            var gitPlugin = Activator.CreateInstance(type) as IGitPlugin;
-                            if (gitPlugin == null)
-                                continue;
+                        var gitPlugin = Activator.CreateInstance(type) as IGitPlugin;
+                        if (gitPlugin == null)
+                            continue;
 
-                            gitPlugin.Settings = new GitPluginSettingsContainer(gitPlugin.Description);
-                            gitPlugin.Register(GitUICommands.Instance);
+                        gitPlugin.Settings = new GitPluginSettingsContainer(gitPlugin.Description);
+                        gitPlugin.Register(GitUICommands.Instance);
 
-                            if (gitPlugin is IGitHostingPlugin)
-                                RepoHosts.GitHosters.Add(gitPlugin as IGitHostingPlugin);
+                        if (gitPlugin is IGitHostingPlugin)
+                            RepoHosts.GitHosters.Add(gitPlugin as IGitHostingPlugin);
                           
-                            LoadedPlugins.Plugins.Add(gitPlugin);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(string.Format("Failed to load plugin {0} : \r\n{1}", pluginFile, ex.Message));
-                        Trace.WriteLine(ex.Message);
+                        LoadedPlugins.Plugins.Add(gitPlugin);
                     }
                 }
-            }
-            catch
-            {
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("Failed to load plugin {0} : \r\n{1}", pluginFile, ex.Message));
+                    Trace.WriteLine(ex.Message);
+                }
             }
         }
     }
