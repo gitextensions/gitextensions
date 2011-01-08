add111da81887248b61d3650b849a087655fb23f
Bjørn Moe <bem-github@farlig.org>
2011-01-08 17:10:09 +0100

    GitUIPluginInterfaces: IPullRequest Fetcher/Information added

---
+++ Plugins/GitUIPluginInterfaces/IPullRequestFetcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitUIPluginInterfaces
+{
+    public interface IPullRequestsFetcher
+    {
+        List<IPullRequestInformation> Fetch();
+        string Data { get; }
+        string DisplayData { get; }
+    }
+}
---
+++ Plugins/GitUIPluginInterfaces/IPullRequestInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitUIPluginInterfaces
+{
+    public interface IPullRequestInformation
+    {
+        string Title { get; }
+        string Body { get; }
+        string Owner { get; }
+        DateTime Created { get; }
+
+        string DiffData { get; }
+
+        IHostedGitRepo BaseRepo { get; }
+        IHostedGitRepo HeadRepo { get; }
+        string BaseSha { get; }
+        string HeadSha { get; }
+
+        string BaseRef { get; }
+        string HeadRef { get; }
+
+        string Id { get; }
+        string DetailedInfo { get; }
+    }
+}
