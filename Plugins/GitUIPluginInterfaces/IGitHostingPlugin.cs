a637ecb9b8e90050a6baf9bad313c940dde1085c
Bjørn Moe <bem-github@farlig.org>
2011-01-08 17:10:09 +0100

    GitUIPluginInterfaces: IPullRequest Fetcher/Information added

--- Plugins/GitUIPluginInterfaces/IGitHostingPlugin.cs
+++ Plugins/GitUIPluginInterfaces/IGitHostingPlugin.cs
@@ -14,6 +14,7 @@ namespace GitUIPluginInterfaces
         bool ConfigurationOk { get; }
 
         bool CurrentWorkingDirRepoIsRelevantToMe { get; }
+        List<IPullRequestsFetcher> GetPullRequestTargetsForCurrentWorkingDirRepo();
     }
 
     public interface IHostedGitRepo
