1a90aee7131c092d35b71bab0b6ad2c9893c16f6
Bjørn Moe <bem-github@farlig.org>
2011-01-09 01:02:01 +0100

    GitUIPluginInterfaces: Added repo hosted discussion interface stuff.

--- Plugins/GitUIPluginInterfaces/IPullRequestInformation.cs
+++ Plugins/GitUIPluginInterfaces/IPullRequestInformation.cs
@@ -23,5 +23,28 @@ namespace GitUIPluginInterfaces
 
         string Id { get; }
         string DetailedInfo { get; }
+
+        void Close();
+
+        IPullRequestDiscussion Discussion { get; }
+    }
+
+    public interface IDiscussionEntry
+    {
+        string Author { get; }
+        DateTime Created { get; }
+        string Body { get; }
+    }
+
+    public interface ICommitDiscussionEntry : IDiscussionEntry
+    {
+        string Sha { get; }
+    }
+
+    public interface IPullRequestDiscussion
+    {
+        List<IDiscussionEntry> Entries { get; }
+        void Post(string data);
+        void ForceReload();
     }
 }
