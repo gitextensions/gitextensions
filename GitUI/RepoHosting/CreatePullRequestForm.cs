490f5ecac14ab54c9dc2b7fca87dac03136fcc8e
Bjørn Moe <bem-github@farlig.org>
2011-01-11 19:40:57 +0100

    GitUI: Added CreatePullRequestForm

---
+++ GitUI/RepoHosting/CreatePullRequestForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GitUIPluginInterfaces;
+
+namespace GitUI.RepoHosting
+{
+    public partial class CreatePullRequestForm : Form
+    {
+        IGitHostingPlugin _repoHost;
+
+        public CreatePullRequestForm(IGitHostingPlugin repoHost)
+        {
+            InitializeComponent();
+        }
+
+        private void CreatePullRequestForm_Load(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _fetchers = _repoHost.GetPullRequestTargetsForCurrentWorkingDirRepo();
+
+            _selectedOwner.Items.Clear();
+            foreach (var fetcher in _fetchers)
+                _selectedOwner.Items.Add(fetcher);
+
+            if (_selectedOwner.Items.Count > 0)
+            {
+                _selectedOwner.SelectedIndex = 0;
+                _selectedOwner_SelectedIndexChanged(null, null);
+            }
+        }
+    }
+}
