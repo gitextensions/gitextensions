ce0d8df271c7510c8d7df8e585bf8a1c149f003d
showell <showell@nearinfinity.com>
2011-01-23 16:36:25 -0500

    Refactored Patch and PatchManager so the behavior is distributed to the correct entity. Add unit tests for patch and patch manager.

--- GitCommands/patch/PatchManager.cs
+++ GitCommands/patch/PatchManager.cs
@@ -265,52 +265,6 @@ namespace PatchApply
             return header + wholepatch;
         }
 
-
-        public string LoadFile(string fileName)
-        {
-            try
-            {
-                using (var streamReader = new StreamReader(DirToPatch + fileName, Settings.Encoding))
-                {
-                    string retval = "";
-                    string line;
-                    while ((line = streamReader.ReadLine()) != null)
-                    {
-                        retval += line + "\n";
-                    }
-
-                    if (retval.Length > 0 && retval[retval.Length - 1] == '\n')
-                        retval = retval.Remove(retval.Length - 1, 1);
-
-                    return retval;
-                }
-            }
-            catch
-            {
-                return "";
-            }
-        }
-
-        public void SavePatch()
-        {
-            foreach (Patch patch in _patches)
-            {
-                if (!patch.Apply)
-                    continue;
-                string path = DirToPatch + patch.FileNameA;
-                if (patch.Type == Patch.PatchType.DeleteFile)
-                {
-                    File.Delete(path);
-                }
-                else
-                {
-                    Directory.CreateDirectory(path.Substring(0, path.LastIndexOfAny("\\/".ToCharArray())));
-                    using(TextWriter tw = new StreamWriter(DirToPatch + patch.FileNameA, false))
-                        tw.Write(patch.FileTextB);
-                }
-            }
-        }
-
         public string GetMD5Hash(string input)
         {
             IEnumerable<byte> bs = GetUTF8EncodedBytes(input);
@@ -329,163 +283,7 @@ namespace PatchApply
             bs = x.ComputeHash(bs);
             return bs;
         }
-
-
-        public void ApplyPatch(Patch patch)
-        {
-            patch.FileTextB = "";
-            patch.Rate = 100;
-
-            if (patch.Type == Patch.PatchType.DeleteFile)
-            {
-                handleDeletePatchType(patch);
-                return;
-            }
-
-            if (patch.Text == null)
-                return;
-
-            string[] patchLines = patch.Text.Split('\n');
-
-            if (patch.Type == Patch.PatchType.NewFile)
-            {
-                handleNewFilePatchType(patch, patchLines);
-                return;
-            }
-
-            if (patch.Type == Patch.PatchType.ChangeFile)
-            {
-                handleChangeFilePatchType(patch, patchLines);
-                return;
-            }
-        }
-
-        private void handleChangeFilePatchType(Patch patch, IEnumerable<string> patchLines)
-        {
-            var fileLines = new List<string>();
-            foreach (string s in LoadFile(patch.FileNameA).Split('\n'))
-            {
-                fileLines.Add(s);
-            }
-
-            int lineNumber = 0;
-            foreach (string line in patchLines)
-            {
-                //Parse first line
-                //@@ -1,4 +1,4 @@
-                if (line.StartsWith("@@") && line.LastIndexOf("@@") > 0)
-                {
-                    string pos = line.Substring(3, line.LastIndexOf("@@") - 3).Trim();
-                    string[] addrem = pos.Split('+', '-');
-                    string[] oldLines = addrem[1].Split(',');
-
-                    lineNumber = Int32.Parse(oldLines[0]) - 1;
-                    continue;
-                }
-
-                if (line.StartsWith(" "))
-                {
-                    //Do some extra checks
-                    if (line.Length > 0)
-                    {
-                        if (fileLines.Count > lineNumber && fileLines[lineNumber].CompareTo(line.Substring(1)) != 0)
-                            patch.Rate -= 20;
-                    }
-                    else
-                    {
-                        if (fileLines.Count > lineNumber && fileLines[lineNumber] != "")
-                            patch.Rate -= 20;
-                    }
-
-                    lineNumber++;
-                }
-                if (line.StartsWith("-"))
-                {
-                    if (line.Length > 0)
-                    {
-                        if (fileLines.Count > lineNumber && fileLines[lineNumber].CompareTo(line.Substring(1)) != 0)
-                            patch.Rate -= 20;
-                    }
-                    else
-                    {
-                        if (fileLines.Count > lineNumber && fileLines[lineNumber] != "")
-                            patch.Rate -= 20;
-                    }
-
-                    patch.Bookmarks.Add(lineNumber);
-
-                    if (fileLines.Count > lineNumber)
-                        fileLines.RemoveAt(lineNumber);
-                    else
-                        patch.Rate -= 20;
-                }
-                if (line.StartsWith("+"))
-                {
-                    string insertLine = "";
-                    if (line.Length > 1)
-                        insertLine = line.Substring(1);
-
-                    //Is the patch allready applied?
-                    if (fileLines.Count > lineNumber && fileLines[lineNumber].CompareTo(insertLine) == 0)
-                    {
-                        patch.Rate -= 20;
-                    }
-
-                    fileLines.Insert(lineNumber, insertLine);
-                    patch.Bookmarks.Add(lineNumber);
-
-                    lineNumber++;
-                }
-            }
-            foreach (string patchedLine in fileLines)
-            {
-                patch.FileTextB += patchedLine + "\n";
-            }
-            if (patch.FileTextB.Length > 0 && patch.FileTextB[patch.FileTextB.Length - 1] == '\n')
-                patch.FileTextB = patch.FileTextB.Remove(patch.FileTextB.Length - 1, 1);
-
-            if (patch.Rate != 100)
-                patch.Apply = false;
-        }
-
-        private void handleNewFilePatchType(Patch patch, IEnumerable<string> patchLines)
-        {
-            foreach (string line in patchLines)
-            {
-                if (line.Length > 0 && line.StartsWith("+"))
-                {
-                    if (line.Length > 4 && line.StartsWith("+ï»¿"))
-                        patch.AppendText(line.Substring(4));
-                    else
-                        if (line.Length > 1)
-                            patch.FileTextB += line.Substring(1);
-
-                    patch.FileTextB += "\n";
-                }
-            }
-            if (patch.FileTextB.Length > 0 && patch.FileTextB[patch.FileTextB.Length - 1] == '\n')
-                patch.FileTextB = patch.FileTextB.Remove(patch.FileTextB.Length - 1, 1);
-            patch.Rate = 100;
-
-            if (File.Exists(DirToPatch + patch.FileNameB))
-            {
-                patch.Rate -= 40;
-                patch.Apply = false;
-            }
-        }
-
-        private void handleDeletePatchType(Patch patch)
-        {
-            patch.FileTextB = "";
-            patch.Rate = 100;
-
-            if (!File.Exists(DirToPatch + patch.FileNameA))
-            {
-                patch.Rate -= 40;
-                patch.Apply = false;
-            }
-        }
-
+        
         public void LoadPatch(string text, bool applyPatch)
         {
             using (var stream = new StringReader(text))
@@ -502,7 +300,7 @@ namespace PatchApply
             }
         }
 
-        public void LoadPatchStream(TextReader reader, bool applyPatch)
+        private void LoadPatchStream(TextReader reader, bool applyPatch)
         {
             _patches = patchProcessor.CreatePatchesFromReader(reader);
 
@@ -512,7 +310,7 @@ namespace PatchApply
             foreach (Patch patchApply in _patches)
             {
                 if (patchApply.Apply)
-                    ApplyPatch(patchApply);
+                    patchApply.ApplyPatch();
             }
         }
 
