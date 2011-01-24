0261eae2d505e497dcde1bf83badd7fa5f994704
showell <showell@nearinfinity.com>
2011-01-23 20:11:06 -0500

    Renamed methods in Patch. Removed unused method LineToChar in PatchManager. Major refactoring of the PatchProcessor.
    Renamed the patch manager unit tests.

--- GitCommands/patch/PatchProcessor.cs
+++ GitCommands/patch/PatchProcessor.cs
@@ -12,101 +12,62 @@ namespace PatchApply
             var patches = new List<Patch>();
             Patch patch = null;
             bool gitPatch = false;
-            string input = textReader.ReadLine();
-            while (input != null)
+            string input;
+            while ((input = textReader.ReadLine()) != null)
             {
-                //diff --git a/FileA b/FileB
-                //new patch found
-                if (input.StartsWith("diff --git "))
+                if (IsStartOfANewPatch(input))
                 {
                     gitPatch = true;
                     patch = new Patch();
                     patches.Add(patch);
-
-                    Match match = Regex.Match(input,
-                                              "[ ][\\\"]{0,1}[a]/(.*)[\\\"]{0,1}[ ][\\\"]{0,1}[b]/(.*)[\\\"]{0,1}");
-
-                    patch.FileNameA = match.Groups[1].Value;
-                    patch.FileNameB = match.Groups[2].Value;
-                    //patch.FileNameA = input.Substring(input.LastIndexOf(" a/") + 3, input.LastIndexOf(" b/") - (input.LastIndexOf(" a/") + 3));
-                    //patch.FileNameB = input.Substring(input.LastIndexOf(" b/") + 3);
-
-                    //The next line tells us what kind of patch
-                    //new file mode xxxxxx means new file
-                    //delete file mode xxxxxx means delete file
-                    //index means -> no new and no delete, edit
+                    ExtractPatchFilenames(input, patch);
                     if ((input = textReader.ReadLine()) != null)
                     {
-                        //WTF! No change
-                        if (input.StartsWith("diff --git "))
+                        if (IsStartOfANewPatch(input))
                         {
-                            //No change? lets continue to the next line
                             continue;
                         }
 
-                        //new file!
-                        if (input.StartsWith("new file mode "))
-                            patch.Type = Patch.PatchType.NewFile;
-                        else if (input.StartsWith("deleted file mode "))
-                            patch.Type = Patch.PatchType.DeleteFile;
-                        else
-                            patch.Type = Patch.PatchType.ChangeFile;
+                        SetPatchType(input, patch);
 
-                        //we need to move to the line that says 'index'
-                        //because we are not sure if we are there yet because
-                        //we might point at the new or delete line lines
-                        if (!input.StartsWith("index "))
-                            if ((input = textReader.ReadLine()) == null)
+                        if (!IsIndexLine(input))
+                        {
+                            if (textReader.ReadLine() == null)
+                            {
                                 break;
+                            }
+                        }
                     }
 
-                    //The next lines tells us more about the change itself
-                    //Read the next
                     if ((input = textReader.ReadLine()) != null)
                     {
-                        //Binary files a/FileA and /dev/null differ
-                        //means the file is deleted but the changes are not listed explicid
-                        if (input.StartsWith("Binary files a/") && input.EndsWith(" and /dev/null differ"))
+                       if (IsUnlistedBinaryFileDelete(input))
                         {
                             patch.File = Patch.FileType.Binary;
 
-                            //Check if the type was set correctly
                             if (patch.Type != Patch.PatchType.DeleteFile)
-                                throw new Exception("Change not parsed correct: " + input);
+                                throw new FormatException("Change not parsed correct: " + input);
 
                             patch = null;
-
-                            if ((input = textReader.ReadLine()) == null)
-                                break;
-
-                            //Continue loop, we do not get more info about this change
                             continue;
                         }
 
-                        //Binary files a/FileA and /dev/null differ
-                        //means the file is deleted but the changes are not listed explicid
-                        if (input.StartsWith("Binary files /dev/null and b/") && input.EndsWith(" differ"))
+                        if (IsUnlistedBinaryNewFile(input))
                         {
                             patch.File = Patch.FileType.Binary;
 
-                            //Check if the type was set correctly
                             if (patch.Type != Patch.PatchType.NewFile)
-                                throw new Exception("Change not parsed correct: " + input);
+                                throw new FormatException("Change not parsed correct: " + input);
 
                             //TODO: NOT SUPPORTED!
                             patch.Apply = false;
 
                             patch = null;
 
-                            if ((input = textReader.ReadLine()) == null)
-                                break;
-
                             continue;
                         }
 
-                        //GIT binary patch
-                        //means the file is binairy 
-                        if (input.StartsWith("GIT binary patch"))
+                        if (IsBinaryPatch(input))
                         {
                             patch.File = Patch.FileType.Binary;
 
@@ -114,9 +75,6 @@ namespace PatchApply
                             patch.Apply = false;
 
                             patch = null;
-
-                            if ((input = textReader.ReadLine()) == null)
-                                break;
 
                             continue;
                         }
@@ -127,83 +85,109 @@ namespace PatchApply
 
                 if (!gitPatch || patch != null)
                 {
-                    //The previous check checked only if the file was binary
-                    //--- /dev/null
-                    //means there is no old file, so this should be a new file
-                    if (input.StartsWith("--- /dev/null"))
-                    {
-                        if (!gitPatch)
-                        {
-                            patch = new Patch();
-                            patches.Add(patch);
-                        }
-
-                        if (gitPatch && patch.Type != Patch.PatchType.NewFile)
-                            throw new Exception("Change not parsed correct: " + input);
-
-                        //This line is parsed, NEXT!
-                        if ((input = textReader.ReadLine()) == null)
-                            break;
-
-                    }
-
-                    //line starts with --- means, old file name
-                    if (input.StartsWith("--- a/") && !input.StartsWith("--- /dev/null"))
-                    {
-                        if (!gitPatch)
-                        {
-                            patch = new Patch();
-                            patches.Add(patch);
-                        }
-
-                        if (gitPatch && patch.FileNameA != (input.Substring(6).Trim()))
-                            throw new Exception("Old filename not parsed correct: " + input);
-
-                        patch.FileNameA = (input.Substring(6).Trim());
-
-                        //This line is parsed, NEXT!
-                        if ((input = textReader.ReadLine()) == null)
-                            break;
-
-                    }
-
-                    //If there is no 'newfile', reset files
-                    if (input.StartsWith("+++ /dev/null"))
-                    {
-                        if (gitPatch && patch.Type != Patch.PatchType.DeleteFile)
-                            throw new Exception("Change not parsed correct: " + input);
-
-                        //This line is parsed, NEXT!
-                        if ((input = textReader.ReadLine()) == null)
-                            break;
-                    }
-
-
-                    //line starts with +++ means, new file name
-                    //we expect a new file now!
-                    if (input.StartsWith("+++ ") && !input.StartsWith("+++ /dev/null"))
-                    {
-                        Match regexMatch = Regex.Match(input, "[+]{3}[ ][\\\"]{0,1}[b]/(.*)[\\\"]{0,1}");
-
-                        if (gitPatch && patch.FileNameB != (regexMatch.Groups[1].Value.Trim()))
-                            throw new Exception("New filename not parsed correct: " + input);
-
-                        patch.FileNameB = (regexMatch.Groups[1].Value.Trim());
-
-                        //This line is parsed, NEXT!
-                        if ((input = textReader.ReadLine()) == null)
-                            break;
-                    }
+                    ValidateInput(input, patch, gitPatch);
                 }
 
                 if (patch != null)
                     patch.AppendTextLine(input);
-
-                if ((input = textReader.ReadLine()) == null)
-                    break;
             }
 
             return patches;
+        }
+
+        private static bool IsIndexLine(string input)
+        {
+            return input.StartsWith("index ");
+        }
+
+        private static void ValidateInput(string input, Patch patch, bool gitPatch)
+        {
+            //The previous check checked only if the file was binary
+            //--- /dev/null
+            //means there is no old file, so this should be a new file
+            if (IsOldFileMissing(input))
+            {
+                if (gitPatch && patch.Type != Patch.PatchType.NewFile)
+                    throw new FormatException("Change not parsed correct: " + input);
+            }
+
+            //line starts with --- means, old file name
+            if (HasOldFileName(input))
+            {
+                if (gitPatch && patch.FileNameA != (input.Substring(6).Trim()))
+                    throw new FormatException("Old filename not parsed correct: " + input);
+            }
+
+            if (IsNewFileMissing(input))
+            {
+                if (gitPatch && patch.Type != Patch.PatchType.DeleteFile)
+                    throw new FormatException("Change not parsed correct: " + input);
+            }
+
+
+            //line starts with +++ means, new file name
+            //we expect a new file now!
+            if (input.StartsWith("+++ ") && !IsNewFileMissing(input))
+            {
+                Match regexMatch = Regex.Match(input, "[+]{3}[ ][\\\"]{0,1}[b]/(.*)[\\\"]{0,1}");
+
+                if (gitPatch && patch.FileNameB != (regexMatch.Groups[1].Value.Trim()))
+                    throw new FormatException("New filename not parsed correct: " + input);
+            }
+        }
+
+        private static bool IsNewFileMissing(string input)
+        {
+            return input.StartsWith("+++ /dev/null");
+        }
+
+        private static bool HasOldFileName(string input)
+        {
+            return input.StartsWith("--- a/");
+        }
+
+        private static bool IsOldFileMissing(string input)
+        {
+            return input.StartsWith("--- /dev/null");
+        }
+
+        private static bool IsBinaryPatch(string input)
+        {
+            return input.StartsWith("GIT binary patch");
+        }
+
+        private static bool IsUnlistedBinaryFileDelete(string input)
+        {
+            return input.StartsWith("Binary files a/") && input.EndsWith(" and /dev/null differ");
+        }
+
+        private static bool IsUnlistedBinaryNewFile(string input)
+        {
+            return input.StartsWith("Binary files /dev/null and b/") && input.EndsWith(" differ");
+        }
+
+        private static void ExtractPatchFilenames(string input, Patch patch)
+        {
+            Match match = Regex.Match(input,
+                                      "[ ][\\\"]{0,1}[a]/(.*)[\\\"]{0,1}[ ][\\\"]{0,1}[b]/(.*)[\\\"]{0,1}");
+
+            patch.FileNameA = match.Groups[1].Value;
+            patch.FileNameB = match.Groups[2].Value;
+        }
+
+        private static bool IsStartOfANewPatch(string input)
+        {
+            return input.StartsWith("diff --git ");
+        }
+
+        private static void SetPatchType(string input, Patch patch)
+        {
+            if (input.StartsWith("new file mode "))
+                patch.Type = Patch.PatchType.NewFile;
+            else if (input.StartsWith("deleted file mode "))
+                patch.Type = Patch.PatchType.DeleteFile;
+            else
+                patch.Type = Patch.PatchType.ChangeFile;
         }
     }
 }
