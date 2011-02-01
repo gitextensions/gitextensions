f42f025006d68734ba141931a3d17fc0a23e228e
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-02-01 19:39:38 +0100

    Plugins/Statistics/GitImpact: Added first draft of an Impact Graph dialog

---
+++ Plugins/Statistics/GitImpact/FormImpact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using GitCommands.Statistics;
+
+namespace GitImpact
+{
+    public partial class FormImpact : Form
+    {
+        public FormImpact()
+        {
+            InitializeComponent();
+            Impact.UpdateData();
+        }
+    }
+}
---
+++ Plugins/Statistics/GitImpact/GitImpactPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using GitUIPluginInterfaces;
+
+namespace GitImpact
+{
+    public class GitImpactPlugin : IGitPlugin
+    {
+        #region IGitPlugin Members
+
+        public string Description
+        {
+            get { return "Impact Graph"; }
+        }
+
+        public IGitPluginSettingsContainer Settings { get; set; }
+
+        public void Register(IGitUICommands gitUiCommands)
+        {
+        }
+
+        public void Execute(GitUIBaseEventArgs gitUiCommands)
+        {
+            if (string.IsNullOrEmpty(gitUiCommands.GitWorkingDir))
+                return;
+
+            FormImpact form = new FormImpact();
+            form.ShowDialog();
+        }
+
+        #endregion
+    }
+}
