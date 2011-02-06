14ce1ea219010f5eac64b494bff3898672213cfc
Henk Westhuis <henk_westhuis@hotmail.com>
2011-02-06 14:00:42 +0100

    Dispose GitCommandsInstance instead of using kill

--- GitCommands/Statistics/ImpactLoader.cs
+++ GitCommands/Statistics/ImpactLoader.cs
@@ -67,7 +67,7 @@ namespace GitCommands.Statistics
             }
             if (git != null)
             {
-                git.Kill();
+                git.Dispose();
                 git = null;
             }
         }
