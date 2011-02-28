f8c39165e93b66548e47b9201ba956e676035aed
mdk <mdk@lostindetails.com>
2011-02-28 21:48:28 +0100

    Added new Form 'FormHotkeys'

---
+++ GitUI/Hotkey/FormHotkeys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI.Hotkey
+{
+  public partial class FormHotkeys : GitExtensionsForm
+  {
+    public FormHotkeys()
+    {
+      InitializeComponent();
+    }
+  }
+}
