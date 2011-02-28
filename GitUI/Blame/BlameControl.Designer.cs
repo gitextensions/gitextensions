cb09d64f9bccb93007bb3bce353b0a47a280c19c
Henk Westhuis <henk_westhuis@hotmail.com>
2011-02-28 20:19:45 +0100

    Use --porcelain for git blame command to get more info
    TODO: review and refactor code

--- GitUI/Blame/BlameControl.Designer.cs
+++ GitUI/Blame/BlameControl.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.commitInfo = new GitUI.CommitInfo();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.BlameCommitter = new GitUI.Editor.FileViewer();
             this.BlameFile = new GitUI.Editor.FileViewer();
-            this.commitInfo = new GitUI.CommitInfo();
+            this.blameTooltip = new System.Windows.Forms.ToolTip(this.components);
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -59,6 +61,16 @@
             this.splitContainer1.SplitterDistance = 142;
             this.splitContainer1.TabIndex = 7;
             // 
+            // commitInfo
+            // 
+            this.commitInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.commitInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.commitInfo.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.commitInfo.Location = new System.Drawing.Point(0, 0);
+            this.commitInfo.Name = "commitInfo";
+            this.commitInfo.Size = new System.Drawing.Size(735, 142);
+            this.commitInfo.TabIndex = 5;
+            // 
             // splitContainer2
             // 
             this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -80,29 +92,34 @@
             // BlameCommitter
             // 
             this.BlameCommitter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BlameCommitter.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.BlameCommitter.IgnoreWhitespaceChanges = false;
             this.BlameCommitter.IsReadOnly = false;
             this.BlameCommitter.Location = new System.Drawing.Point(0, 0);
             this.BlameCommitter.Name = "BlameCommitter";
+            this.BlameCommitter.NumberOfVisibleLines = 3;
+            this.BlameCommitter.ScrollPos = 0;
+            this.BlameCommitter.ShowEntireFile = false;
+            this.BlameCommitter.ShowLineNumbers = true;
             this.BlameCommitter.Size = new System.Drawing.Size(158, 493);
             this.BlameCommitter.TabIndex = 5;
+            this.BlameCommitter.TreatAllFilesAsText = false;
             // 
             // BlameFile
             // 
             this.BlameFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BlameFile.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.BlameFile.IgnoreWhitespaceChanges = false;
             this.BlameFile.IsReadOnly = false;
             this.BlameFile.Location = new System.Drawing.Point(0, 0);
             this.BlameFile.Name = "BlameFile";
+            this.BlameFile.NumberOfVisibleLines = 3;
+            this.BlameFile.ScrollPos = 0;
+            this.BlameFile.ShowEntireFile = false;
+            this.BlameFile.ShowLineNumbers = true;
             this.BlameFile.Size = new System.Drawing.Size(569, 493);
             this.BlameFile.TabIndex = 0;
-            // 
-            // commitInfo
-            // 
-            this.commitInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.commitInfo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.commitInfo.Location = new System.Drawing.Point(0, 0);
-            this.commitInfo.Name = "commitInfo";
-            this.commitInfo.Size = new System.Drawing.Size(735, 142);
-            this.commitInfo.TabIndex = 5;
+            this.BlameFile.TreatAllFilesAsText = false;
             // 
             // BlameControl
             // 
@@ -128,5 +145,6 @@
         private System.Windows.Forms.SplitContainer splitContainer2;
         private GitUI.Editor.FileViewer BlameCommitter;
         private GitUI.Editor.FileViewer BlameFile;
+        private System.Windows.Forms.ToolTip blameTooltip;
     }
 }
