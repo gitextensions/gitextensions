648daf0a77a6f3c718dad0b0d19d8b69728e83b1
mdk <mdk@lostindetails.com>
2011-03-01 00:22:57 +0100

    Started HotkeySettingsManager

---
+++ GitUI/Hotkey/HotkeySettingsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitUI.Hotkey
+{
+  class HotkeySettingsManager<T>
+  {
+    public void SaveSettings(HotkeyMapping[] hotkeys)
+    {
+      var settings = CreateSettings(hotkeys);
+    }
+
+    private HotkeySettings CreateSettings(HotkeyMapping[] hotkeys)
+    {
+      return new HotkeySettings()
+      {
+        Name = typeof(T).FullName,
+        Mappings = hotkeys
+      };
+    }
+
+    public HotkeyMapping[] LoadSettings()
+    {
+      return null;
+    }
+  }
+}
