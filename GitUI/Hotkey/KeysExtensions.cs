2fd9c2bafc148071c00b0ff5a254faa2c1a1f69b
mdk <mdk@lostindetails.com>
2011-03-01 23:48:26 +0100

    Added KeysExtension which add helper methods to Keys enum

---
+++ GitUI/Hotkey/KeysExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI.Hotkey
+{
+  public static class KeysExtensions
+  {
+    /// <summary>
+    /// Strips the modifier from KeyData
+    /// </summary>
+    /// <param name="keyData"></param>
+    /// <returns></returns>
+    public static Keys GetKeyCode(this Keys keyData)
+    {
+      return keyData & Keys.KeyCode;
+    }
+
+    public static bool IsModifierKey(this Keys key)
+    {
+      return key == Keys.ShiftKey ||
+             key == Keys.ControlKey ||
+             key == Keys.Alt;
+    }
+
+    public static Keys[] GetModifiers(this Keys key)
+    {
+      // Retrieve the modifiers, mask away the rest
+      Keys modifier = key & Keys.Modifiers;
+
+      List<Keys> modifierList = new List<Keys>();
+      Action<Keys> addIfContains = m => { if (m == (m & modifier))  modifierList.Add(m); };
+
+      addIfContains(Keys.Control);
+      addIfContains(Keys.Shift);
+      addIfContains(Keys.Alt);
+
+      return modifierList.ToArray();      
+    }
+
+    public static string ToText(this Keys key)
+    {
+      return string.Join("+", 
+        new [] { key.GetKeyCode() }
+        .Union(key.GetModifiers())
+        .Select(k => k.ToString())
+        .ToArray());
+    }
+  }
+}
