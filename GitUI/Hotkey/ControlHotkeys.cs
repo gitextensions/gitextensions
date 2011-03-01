80ea3a65651f23e5ead5dec09f88037a4c401e18
mdk <mdk@lostindetails.com>
2011-03-01 23:54:58 +0100

    Added ControlHotkeys

---
+++ GitUI/Hotkey/ControlHotkeys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI.Hotkey
+{
+  public partial class ControlHotkeys : UserControl
+  {
+    public ControlHotkeys()
+    {
+      InitializeComponent();
+    }
+  }
+}
