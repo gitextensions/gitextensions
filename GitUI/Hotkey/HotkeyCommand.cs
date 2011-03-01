0781e9dd82afa034edf76a656bc338703a7ef1d7
mdk <mdk@lostindetails.com>
2011-03-02 00:04:27 +0100

    Extended ControlHotkeys

---
+++ GitUI/Hotkey/HotkeyCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+
+namespace GitUI.Hotkey
+{
+  [Serializable]
+  public class HotkeyCommand
+  {
+    [XmlAttribute]
+    public int CommandCode { get; set; }
+
+    [XmlAttribute]
+    public string Name { get; set; }
+
+    [XmlAttribute]
+    public Keys KeyData { get; set; }
+
+    public HotkeyCommand()
+    {
+    }
+    public HotkeyCommand(int commandCode, string name)
+    {
+      this.CommandCode = commandCode;
+      this.Name = name;
+    }
+
+    public static HotkeyCommand[] FromEnum(Type enumType)
+    {
+      return Enum.GetValues(enumType).Cast<object>().Select(c => new HotkeyCommand((int)c, c.ToString())).ToArray();
+    }
+  }
+}
