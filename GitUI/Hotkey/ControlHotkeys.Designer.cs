830a31bef2d28e157fdb9740f464c33bd4e6a8e6
mdk <mdk@lostindetails.com>
2011-03-03 20:26:08 +0100

    ControlHotkeys has TabIndices set and ComboBox binds to 'Name' propertyes

--- GitUI/Hotkey/ControlHotkeys.Designer.cs
+++ GitUI/Hotkey/ControlHotkeys.Designer.cs
@@ -34,6 +34,8 @@
       this.cmbSettings = new System.Windows.Forms.ComboBox();
       this.lHotkeyableItems = new System.Windows.Forms.Label();
       this.listMappings = new System.Windows.Forms.ListView();
+      this.columnCommand = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+      this.columnKey = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
       this.txtHotkey = new GitUI.Hotkey.TextboxHotkey();
       this.SuspendLayout();
       // 
@@ -41,7 +43,7 @@
       // 
       this.lHotkey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.lHotkey.AutoSize = true;
-      this.lHotkey.Location = new System.Drawing.Point(3, 355);
+      this.lHotkey.Location = new System.Drawing.Point(3, 550);
       this.lHotkey.Name = "lHotkey";
       this.lHotkey.Size = new System.Drawing.Size(52, 17);
       this.lHotkey.TabIndex = 1;
@@ -50,10 +52,10 @@
       // bApply
       // 
       this.bApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.bApply.Location = new System.Drawing.Point(152, 380);
+      this.bApply.Location = new System.Drawing.Point(152, 575);
       this.bApply.Name = "bApply";
       this.bApply.Size = new System.Drawing.Size(66, 26);
-      this.bApply.TabIndex = 2;
+      this.bApply.TabIndex = 4;
       this.bApply.Text = "Apply";
       this.bApply.UseVisualStyleBackColor = true;
       this.bApply.Click += new System.EventHandler(this.bApply_Click);
@@ -61,10 +63,10 @@
       // bClear
       // 
       this.bClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.bClear.Location = new System.Drawing.Point(224, 380);
+      this.bClear.Location = new System.Drawing.Point(224, 575);
       this.bClear.Name = "bClear";
       this.bClear.Size = new System.Drawing.Size(66, 26);
-      this.bClear.TabIndex = 3;
+      this.bClear.TabIndex = 5;
       this.bClear.Text = "Clear";
       this.bClear.UseVisualStyleBackColor = true;
       this.bClear.Click += new System.EventHandler(this.bClear_Click);
@@ -73,11 +75,13 @@
       // 
       this.cmbSettings.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
+      this.cmbSettings.DisplayMember = "Name";
+      this.cmbSettings.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
       this.cmbSettings.FormattingEnabled = true;
       this.cmbSettings.Location = new System.Drawing.Point(3, 23);
       this.cmbSettings.Name = "cmbSettings";
       this.cmbSettings.Size = new System.Drawing.Size(287, 24);
-      this.cmbSettings.TabIndex = 4;
+      this.cmbSettings.TabIndex = 0;
       this.cmbSettings.SelectedIndexChanged += new System.EventHandler(this.cmbSettings_SelectedIndexChanged);
       // 
       // lHotkeyableItems
@@ -94,28 +98,41 @@
       this.listMappings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                   | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
+      this.listMappings.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnCommand,
+            this.columnKey});
       this.listMappings.Location = new System.Drawing.Point(6, 53);
       this.listMappings.Name = "listMappings";
-      this.listMappings.Size = new System.Drawing.Size(284, 293);
-      this.listMappings.TabIndex = 6;
+      this.listMappings.Size = new System.Drawing.Size(284, 488);
+      this.listMappings.TabIndex = 1;
       this.listMappings.UseCompatibleStateImageBehavior = false;
+      this.listMappings.View = System.Windows.Forms.View.Details;
       this.listMappings.SelectedIndexChanged += new System.EventHandler(this.listMappings_SelectedIndexChanged);
+      // 
+      // columnCommand
+      // 
+      this.columnCommand.Text = "Command";
+      this.columnCommand.Width = 120;
+      // 
+      // columnKey
+      // 
+      this.columnKey.Text = "Key";
+      this.columnKey.Width = 120;
       // 
       // txtHotkey
       // 
       this.txtHotkey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
       this.txtHotkey.KeyData = System.Windows.Forms.Keys.None;
-      this.txtHotkey.Location = new System.Drawing.Point(56, 352);
+      this.txtHotkey.Location = new System.Drawing.Point(56, 547);
       this.txtHotkey.Name = "txtHotkey";
       this.txtHotkey.Size = new System.Drawing.Size(234, 22);
-      this.txtHotkey.TabIndex = 0;
+      this.txtHotkey.TabIndex = 3;
       this.txtHotkey.Text = "None";
       // 
       // ControlHotkeys
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
       this.Controls.Add(this.listMappings);
       this.Controls.Add(this.lHotkeyableItems);
       this.Controls.Add(this.cmbSettings);
@@ -124,7 +141,7 @@
       this.Controls.Add(this.lHotkey);
       this.Controls.Add(this.txtHotkey);
       this.Name = "ControlHotkeys";
-      this.Size = new System.Drawing.Size(295, 409);
+      this.Size = new System.Drawing.Size(295, 604);
       this.Load += new System.EventHandler(this.ControlHotkeys_Load);
       this.ResumeLayout(false);
       this.PerformLayout();
@@ -140,5 +157,7 @@
     private System.Windows.Forms.ComboBox cmbSettings;
     private System.Windows.Forms.Label lHotkeyableItems;
     private System.Windows.Forms.ListView listMappings;
+    private System.Windows.Forms.ColumnHeader columnCommand;
+    private System.Windows.Forms.ColumnHeader columnKey;
   }
 }
