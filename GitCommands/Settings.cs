4a6db0923205be4fe12fc09271e0f3308e34ae46
Henk Westhuis <henk_westhuis@hotmail.com>
2011-03-04 19:59:36 +0100

    Fixed crash when viewing binary file in blame

--- GitCommands/Settings.cs
+++ GitCommands/Settings.cs
@@ -13,8 +13,8 @@ namespace GitCommands
     public static class Settings
     {
         //Constants
-        public static readonly string GitExtensionsVersionString = "2.13";
-        public static readonly int GitExtensionsVersionInt = 213;
+        public static readonly string GitExtensionsVersionString = "2.14";
+        public static readonly int GitExtensionsVersionInt = 214;
 
         //semi-constants
         public static char PathSeparator = '\\';
