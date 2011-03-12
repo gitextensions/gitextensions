fda17f93402e304f43e2273561be43363bc44727
Henk Westhuis <henk_westhuis@hotmail.com>
2011-03-12 17:07:12 +0100

    Updated changelog and version info

--- GitExtensions/Properties/AssemblyInfo.cs
+++ GitExtensions/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.14")]
+[assembly: AssemblyVersion("2.15")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
