50fa37ec33c227c4343d30769baf7b866702a12c
Henk Westhuis <henk_westhuis@hotmail.com>
2011-04-05 18:57:47 +0200

    Set RichTextBox LanguageOption to RichTextBoxLanguageOptions.UIFonts

--- GitUI/FormDonate.Designer.cs
+++ GitUI/FormDonate.Designer.cs
@@ -42,7 +42,7 @@ namespace GitUI
             // richTextBox1
             // 
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.LanguageOption = RichTextBoxLanguageOptions.AutoKeyboard;
+            this.richTextBox1.LanguageOption = RichTextBoxLanguageOptions.UIFonts;
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
