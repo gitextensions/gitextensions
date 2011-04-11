75902973fa8791db668b87c24c409caef4fa0dd3
Henk Westhuis <henk_westhuis@hotmail.com>
2011-04-11 19:14:09 +0200

    Diff is shown in reverse in stash and commit view
    Commit detail dialog shows all changes in reverse when started from file history

--- GitUI/AboutBox.cs
+++ GitUI/AboutBox.cs
@@ -49,7 +49,7 @@ namespace GitUI
         }
 
         //Contributers list 
-        private const string contributers = "Steffen Forkmann, Jacob Stanley, Nick Mayer, Kevin Moore, Davide, dominiqueplante, Grzegorz Pachocki, Seth Behunin, bleis-tift, Chris Meaney, Adrian Codrington, Troels Thomsen, Wilbert van Dolleweerd, Tobias Bieniek, Radoslaw Miazio, Stan Angeloff, Matt McCormick, Bjørn Moe, William Swanson, Daniel Locantore, Harald Deischinger, Radek Miazio, Stefan Rueckl, Emanuel Henrique do Prado, Lukasz Byczynski, Steffen M. Colding-Jørgensen, alexeik, arBmind, Gianni Rosa Gallina, mausch, xaro, Xharze, Kim Christensen, gor, Charles Brossollet, Patrick Earl, Cheng Huang, ultonis, Michael Frenzel, showell, mdk, superlongman, Marc Murray, rferriz, Jacek Pasternak, miloja";
+        private const string contributers = "Steffen Forkmann, Jacob Stanley, Nick Mayer, Kevin Moore, Davide, dominiqueplante, Grzegorz Pachocki, Seth Behunin, bleis-tift, Chris Meaney, Adrian Codrington, Troels Thomsen, Wilbert van Dolleweerd, Tobias Bieniek, Radoslaw Miazio, Stan Angeloff, Matt McCormick, Bjørn Moe, William Swanson, Daniel Locantore, Harald Deischinger, Radek Miazio, Stefan Rueckl, Emanuel Henrique do Prado, Lukasz Byczynski, Steffen M. Colding-Jørgensen, alexeik, arBmind, Gianni Rosa Gallina, mausch, xaro, Xharze, Kim Christensen, gor, Charles Brossollet, Patrick Earl, Cheng Huang, ultonis, Michael Frenzel, showell, Daniel Doubrovkine, mdk, superlongman, Marc Murray, rferriz, Jacek Pasternak, miloja";
         private string[] contributersList;
         private Random random = new Random();
 
