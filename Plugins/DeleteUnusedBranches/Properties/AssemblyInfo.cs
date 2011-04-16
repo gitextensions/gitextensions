06313e1c483780e6f1b8bf6c10b9e15f3d54c01c
Henk Westhuis <henk_westhuis@hotmail.com>
2011-04-16 09:40:40 +0200

    Fixed issue 423: "GitExtensions has stopped working" at startup

--- Plugins/DeleteUnusedBranches/Properties/AssemblyInfo.cs
+++ Plugins/DeleteUnusedBranches/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.20")]
+[assembly: AssemblyVersion("2.21")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
