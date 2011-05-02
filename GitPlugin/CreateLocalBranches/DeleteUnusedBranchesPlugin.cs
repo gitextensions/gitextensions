411c7d84beb2cdea3a7b9609ae97796e851ae538
Henk Westhuis <henk.westhuis@ultimo.net>
2011-05-02 12:20:04 +0200

    Created "CreateLocalBranches" plugin

---
+++ GitPlugin/CreateLocalBranches/DeleteUnusedBranchesPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitUIPluginInterfaces;
+
+namespace CreateLocalBranches
+{
+	public class CreateLocalBranchesPlugin : IGitPlugin
+    {
+        public string Description
+        {
+            get { return "Create local tracking branches"; }
+        }
+
+        public IGitPluginSettingsContainer Settings { get; set; }
+
+        public void Register(IGitUICommands gitUiCommands)
+        {
+        }
+
+        public void Execute(GitUIBaseEventArgs gitUiCommands)
+        {
+			foreach (string reference in gitUiCommands.GitCommands.RunGit("branch -a").Split('\n'))
+			{
+				try
+				{
+					if (string.IsNullOrEmpty(reference)) continue;
+
+					string branchName = reference.Trim('*', ' ', '\n', '\r');
+
+					if (branchName.StartsWith("remotes"))
+						gitUiCommands.GitCommands.RunGit(string.Concat("branch --track ", branchName.Replace("remotes/origin/", ""), " ", branchName));
+				}
+				catch
+				{ 
+				}
+			}
+
+        }
+    }
+}
