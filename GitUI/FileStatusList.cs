785c2d621ecc4c320c1456b1715a1b11057a4fe2
kirichenko <Vasily.Kirichenko@kaspersky.com>
2011-07-06 13:41:10 +0400

    add auto focus to GitExtensionsUserControl

--- GitUI/FileStatusList.cs
+++ GitUI/FileStatusList.cs
@@ -119,8 +119,13 @@ namespace GitUI
 
         void FileStatusListBox_MouseMove(object sender, MouseEventArgs e)
         {
-            if (!Focused)
-                Focus();
+            ListBox listBox = sender as ListBox;
+
+            if (listBox != null)
+            {
+                if (!listBox.Focused)
+                    listBox.Focus();
+            }
 
             //DRAG
             // If the mouse moves outside the rectangle, start the drag.
@@ -148,7 +153,6 @@ namespace GitUI
             }
 
             //TOOLTIP
-            ListBox listBox = sender as ListBox;
             if (listBox != null)
             {
                 Point point = new Point(e.X, e.Y);
