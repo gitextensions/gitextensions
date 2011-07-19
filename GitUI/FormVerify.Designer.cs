c2abd8334f7f926953799544a60e9491ee9c0c73
Airat Salikhov <asalikhov@iiko.ru>
2011-07-19 20:33:06 +0400

    Parse and display data with grid

--- GitUI/FormVerify.Designer.cs
+++ GitUI/FormVerify.Designer.cs
@@ -45,7 +45,11 @@
             this.TagSelectedObject = new System.Windows.Forms.Button();
             this.Remove = new System.Windows.Forms.Button();
             this.SaveObjects = new System.Windows.Forms.Button();
-            this.Raw = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnAuthor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnSubject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnHash = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnRaw = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -175,13 +179,23 @@
             // 
             // Warnings
             // 
+            this.Warnings.AllowUserToAddRows = false;
+            this.Warnings.AllowUserToDeleteRows = false;
+            this.Warnings.AllowUserToOrderColumns = true;
             this.Warnings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.Warnings.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Raw});
+            this.columnDate,
+            this.columnAuthor,
+            this.columnSubject,
+            this.columnHash,
+            this.columnRaw});
             this.Warnings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Warnings.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.Warnings.Location = new System.Drawing.Point(0, 0);
             this.Warnings.MultiSelect = false;
             this.Warnings.Name = "Warnings";
+            this.Warnings.ReadOnly = true;
+            this.Warnings.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.Warnings.Size = new System.Drawing.Size(859, 325);
             this.Warnings.TabIndex = 0;
             this.Warnings.DoubleClick += new System.EventHandler(this.WarningsDoubleClick);
@@ -263,11 +277,42 @@
             this.SaveObjects.UseVisualStyleBackColor = true;
             this.SaveObjects.Click += new System.EventHandler(this.SaveObjectsClick);
             // 
-            // Raw
+            // columnDate
             // 
-            this.Raw.HeaderText = "Raw";
-            this.Raw.Name = "Raw";
-            this.Raw.ReadOnly = true;
+            this.columnDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.columnDate.DataPropertyName = "Date";
+            this.columnDate.HeaderText = "Date";
+            this.columnDate.Name = "columnDate";
+            this.columnDate.ReadOnly = true;
+            this.columnDate.Width = 56;
+            // 
+            // columnAuthor
+            // 
+            this.columnAuthor.DataPropertyName = "Author";
+            this.columnAuthor.HeaderText = "Author";
+            this.columnAuthor.Name = "columnAuthor";
+            this.columnAuthor.ReadOnly = true;
+            // 
+            // columnSubject
+            // 
+            this.columnSubject.DataPropertyName = "Subject";
+            this.columnSubject.HeaderText = "Subject";
+            this.columnSubject.Name = "columnSubject";
+            this.columnSubject.ReadOnly = true;
+            // 
+            // columnHash
+            // 
+            this.columnHash.DataPropertyName = "Hash";
+            this.columnHash.HeaderText = "Hash";
+            this.columnHash.Name = "columnHash";
+            this.columnHash.ReadOnly = true;
+            // 
+            // columnRaw
+            // 
+            this.columnRaw.DataPropertyName = "Raw";
+            this.columnRaw.HeaderText = "Raw";
+            this.columnRaw.Name = "columnRaw";
+            this.columnRaw.ReadOnly = true;
             // 
             // FormVerify
             // 
@@ -311,6 +356,10 @@
         private System.Windows.Forms.CheckBox ShowOnlyCommits;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView Warnings;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Raw;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnAuthor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnSubject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnHash;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnRaw;
     }
 }
---
+++ GitUI/FormVerify.LostObject.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using GitCommands;
+
+namespace GitUI
+{
+    partial class FormVerify
+    {
+        private sealed class LostObject
+        {
+            /// <summary>
+            /// {0} - lost object's hash.
+            /// %aN - committer name.
+            /// %s  - subject.
+            /// %ct - committer date, UNIX timestamp (easy to parse format).
+            /// </summary>
+            private const string LogCommandArgumentsFormat = "log -n1 --pretty=format:\"%aN, %s, %ct\" {0}";
+            private const string LogPattern = @"^([^,]+), (.+), (\d+)$";
+            private const string Sha1HashPattern = @"[a-f\d]{40}";
+            private const string RawDataPattern = "^((dangling|missing|unreachable) (commit|blob|tree)|warning in tree) (" + Sha1HashPattern + ")(.)*$";
+
+            private static readonly Regex Sha1HashRegex = new Regex("^" + Sha1HashPattern + "$", RegexOptions.Compiled);
+            private static readonly Regex RawDataRegex = new Regex(RawDataPattern, RegexOptions.Compiled);
+            private static readonly Regex LogRegex = new Regex(LogPattern, RegexOptions.Compiled);
+
+            private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+            private readonly string hash;
+            private readonly string raw;
+
+            /// <summary>
+            /// Raw data returned by fsck-objects command.
+            /// </summary>
+            public string Raw { get { return raw; } }
+
+            /// <summary>
+            /// Sha1 hash
+            /// </summary>
+            public string Hash { get { return hash; } }
+
+            public string Author { get; private set; }
+            public string Subject { get; private set; }
+            public DateTime? Date { get; private set; }
+
+            private LostObject(string hash, string raw)
+            {
+                this.hash = hash;
+                this.raw = raw;
+            }
+
+            public static LostObject TryParse(string raw)
+            {
+                if (string.IsNullOrEmpty(raw))
+                    throw new ArgumentException("Raw source must be non-empty string", raw);
+
+                var patterMatch = RawDataRegex.Match(raw);
+
+                // show failed assertion for unsupported cases (for developers)
+                // if you get this message, 
+                //     you can implement this format parsing
+                //     or post an issue to https://github.com/spdr870/gitextensions/issues
+                Debug.Assert(patterMatch.Success, "Lost object's extracted diagnostics format not implemented", raw);
+
+                // skip unsupported raw data format (for end users)
+                if (!patterMatch.Success)
+                    return null;
+
+                Debug.Assert(patterMatch.Groups[4].Success);
+                var hash = patterMatch.Groups[4].Value;
+
+                var result = new LostObject(hash, raw);
+
+                var objectInfo = GetLostObjectLog(hash);
+                var logPatternMatch = LogRegex.Match(objectInfo);
+                if (logPatternMatch.Success)
+                {
+                    result.Author = logPatternMatch.Groups[1].Value;
+                    result.Subject = logPatternMatch.Groups[2].Value;
+                    result.Date = UnixEpoch.AddSeconds(long.Parse(logPatternMatch.Groups[3].Value));
+                }
+                return result;
+            }
+
+            private static string GetLostObjectLog(string hash)
+            {
+                if (string.IsNullOrEmpty(hash) || !Sha1HashRegex.IsMatch(hash))
+                    throw new ArgumentOutOfRangeException("hash", hash, "Hash must be a valid SHA1 hash.");
+
+                return GitCommandHelpers.RunCmd(Settings.GitCommand, string.Format(LogCommandArgumentsFormat, hash));
+            }
+        }
+    }
+}
