f46624b40b1163aad8d579b7e8c227590bc272d0
Henk Westhuis <henk.westhuis@ultimo.net>
2011-07-25 10:14:09 +0200

    Updated version info

--- Plugins/AutoCheckForUpdates/Properties/AssemblyInfo.cs
+++ Plugins/AutoCheckForUpdates/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.23")]
-[assembly: AssemblyFileVersion("2.23")]
+[assembly: AssemblyVersion("2.24")]
+[assembly: AssemblyFileVersion("2.24")]
