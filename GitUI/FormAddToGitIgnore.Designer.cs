52c8a02451c3795c9d5748e03c8cf569c916e9a7
Henk Westhuis <henk_westhuis@hotmail.com>
2011-08-10 12:39:58 -0700

    Moved controls on ForAddToGitIgnore a little to allow translation

--- GitUI/FormAddToGitIgnore.Designer.cs
+++ GitUI/FormAddToGitIgnore.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.filesWillBeIgnored = new System.Windows.Forms.Label();
+            this.noMatchPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NumMatches = new System.Windows.Forms.Label();
             this.Preview = new System.Windows.Forms.ListBox();
             this.AddToIngore = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.FilePattern = new System.Windows.Forms.TextBox();
-            this.noMatchPanel = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.filesWillBeIgnored = new System.Windows.Forms.Label();
-            this.NumMatches = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.noMatchPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -57,42 +57,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Preview";
             // 
-            // Preview
+            // filesWillBeIgnored
             // 
-            this.Preview.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Preview.FormattingEnabled = true;
-            this.Preview.ItemHeight = 15;
-            this.Preview.Location = new System.Drawing.Point(12, 24);
-            this.Preview.Name = "Preview";
-            this.Preview.Size = new System.Drawing.Size(528, 184);
-            this.Preview.TabIndex = 1;
-            // 
-            // AddToIngore
-            // 
-            this.AddToIngore.Location = new System.Drawing.Point(451, 16);
-            this.AddToIngore.Name = "AddToIngore";
-            this.AddToIngore.Size = new System.Drawing.Size(91, 25);
-            this.AddToIngore.TabIndex = 7;
-            this.AddToIngore.Text = "Ignore";
-            this.AddToIngore.UseVisualStyleBackColor = true;
-            this.AddToIngore.Click += new System.EventHandler(this.AddToIngoreClick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Enter a file pattern to ignore:";
-            // 
-            // FilePattern
-            // 
-            this.FilePattern.Location = new System.Drawing.Point(175, 16);
-            this.FilePattern.Name = "FilePattern";
-            this.FilePattern.Size = new System.Drawing.Size(270, 23);
-            this.FilePattern.TabIndex = 6;
-            this.FilePattern.TextChanged += new System.EventHandler(this.FilePattern_TextChanged);
+            this.filesWillBeIgnored.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.filesWillBeIgnored.AutoSize = true;
+            this.filesWillBeIgnored.Location = new System.Drawing.Point(452, 214);
+            this.filesWillBeIgnored.Name = "filesWillBeIgnored";
+            this.filesWillBeIgnored.Size = new System.Drawing.Size(78, 13);
+            this.filesWillBeIgnored.TabIndex = 11;
+            this.filesWillBeIgnored.Text = "file(s) matched";
             // 
             // noMatchPanel
             // 
@@ -105,6 +78,17 @@
             this.noMatchPanel.TabIndex = 2;
             this.noMatchPanel.Visible = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(25, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "No existing files match that pattern.";
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::GitUI.Properties.Resources.Conflict;
@@ -115,37 +99,52 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(25, 5);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(195, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "No existing files match that pattern.";
-            // 
-            // filesWillBeIgnored
-            // 
-            this.filesWillBeIgnored.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.filesWillBeIgnored.AutoSize = true;
-            this.filesWillBeIgnored.Location = new System.Drawing.Point(452, 214);
-            this.filesWillBeIgnored.Name = "filesWillBeIgnored";
-            this.filesWillBeIgnored.Size = new System.Drawing.Size(86, 15);
-            this.filesWillBeIgnored.TabIndex = 11;
-            this.filesWillBeIgnored.Text = "file(s) matched";
-            // 
             // NumMatches
             // 
-            this.NumMatches.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.NumMatches.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.NumMatches.AutoSize = true;
-            this.NumMatches.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.NumMatches.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NumMatches.Location = new System.Drawing.Point(439, 214);
             this.NumMatches.Name = "NumMatches";
             this.NumMatches.Size = new System.Drawing.Size(14, 15);
             this.NumMatches.TabIndex = 10;
             this.NumMatches.Text = "0";
+            // 
+            // Preview
+            // 
+            this.Preview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Preview.FormattingEnabled = true;
+            this.Preview.Location = new System.Drawing.Point(12, 21);
+            this.Preview.Name = "Preview";
+            this.Preview.Size = new System.Drawing.Size(528, 187);
+            this.Preview.TabIndex = 1;
+            // 
+            // AddToIngore
+            // 
+            this.AddToIngore.Location = new System.Drawing.Point(476, 13);
+            this.AddToIngore.Name = "AddToIngore";
+            this.AddToIngore.Size = new System.Drawing.Size(91, 25);
+            this.AddToIngore.TabIndex = 7;
+            this.AddToIngore.Text = "Ignore";
+            this.AddToIngore.UseVisualStyleBackColor = true;
+            this.AddToIngore.Click += new System.EventHandler(this.AddToIngoreClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter a file pattern to ignore:";
+            // 
+            // FilePattern
+            // 
+            this.FilePattern.Location = new System.Drawing.Point(266, 16);
+            this.FilePattern.Name = "FilePattern";
+            this.FilePattern.Size = new System.Drawing.Size(202, 21);
+            this.FilePattern.TabIndex = 6;
+            this.FilePattern.TextChanged += new System.EventHandler(this.FilePattern_TextChanged);
             // 
             // FormAddToGitIgnore
             // 
@@ -165,7 +164,7 @@
             this.groupBox1.PerformLayout();
             this.noMatchPanel.ResumeLayout(false);
             this.noMatchPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
