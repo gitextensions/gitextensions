e0f47daa83dac37a70bccb2570f7daad7b1d1043
Ralph Haußmann <ralph@scanmyfood.de>
2011-10-02 20:40:44 +0200

    Translation + GUI: German translation of FormResetCurrentBranch
    German translation
    GUI fixes for german translation

--- GitUI/FormResetCurrentBranch.Designer.cs
+++ GitUI/FormResetCurrentBranch.Designer.cs
@@ -47,7 +47,7 @@
             this._NO_TRANSLATE_BranchInfo.AutoSize = true;
             this._NO_TRANSLATE_BranchInfo.Location = new System.Drawing.Point(13, 13);
             this._NO_TRANSLATE_BranchInfo.Name = "_NO_TRANSLATE_BranchInfo";
-            this._NO_TRANSLATE_BranchInfo.Size = new System.Drawing.Size(87, 13);
+            this._NO_TRANSLATE_BranchInfo.Size = new System.Drawing.Size(83, 15);
             this._NO_TRANSLATE_BranchInfo.TabIndex = 0;
             this._NO_TRANSLATE_BranchInfo.Text = "##Reset {0} to:";
             // 
@@ -56,7 +56,7 @@
             this._NO_TRANSLATE_Commit.AutoSize = true;
             this._NO_TRANSLATE_Commit.Location = new System.Drawing.Point(31, 34);
             this._NO_TRANSLATE_Commit.Name = "_NO_TRANSLATE_Commit";
-            this._NO_TRANSLATE_Commit.Size = new System.Drawing.Size(81, 13);
+            this._NO_TRANSLATE_Commit.Size = new System.Drawing.Size(85, 15);
             this._NO_TRANSLATE_Commit.TabIndex = 1;
             this._NO_TRANSLATE_Commit.Text = "##Commit: {0}";
             // 
@@ -65,7 +65,7 @@
             this._NO_TRANSLATE_Author.AutoSize = true;
             this._NO_TRANSLATE_Author.Location = new System.Drawing.Point(31, 57);
             this._NO_TRANSLATE_Author.Name = "_NO_TRANSLATE_Author";
-            this._NO_TRANSLATE_Author.Size = new System.Drawing.Size(79, 13);
+            this._NO_TRANSLATE_Author.Size = new System.Drawing.Size(78, 15);
             this._NO_TRANSLATE_Author.TabIndex = 2;
             this._NO_TRANSLATE_Author.Text = "##Author: {0}";
             // 
@@ -74,7 +74,7 @@
             this._NO_TRANSLATE_Message.AutoSize = true;
             this._NO_TRANSLATE_Message.Location = new System.Drawing.Point(31, 106);
             this._NO_TRANSLATE_Message.Name = "_NO_TRANSLATE_Message";
-            this._NO_TRANSLATE_Message.Size = new System.Drawing.Size(88, 13);
+            this._NO_TRANSLATE_Message.Size = new System.Drawing.Size(87, 15);
             this._NO_TRANSLATE_Message.TabIndex = 3;
             this._NO_TRANSLATE_Message.Text = "##Message: {0}";
             // 
@@ -83,7 +83,7 @@
             this._NO_TRANSLATE_Date.AutoSize = true;
             this._NO_TRANSLATE_Date.Location = new System.Drawing.Point(31, 82);
             this._NO_TRANSLATE_Date.Name = "_NO_TRANSLATE_Date";
-            this._NO_TRANSLATE_Date.Size = new System.Drawing.Size(106, 13);
+            this._NO_TRANSLATE_Date.Size = new System.Drawing.Size(111, 15);
             this._NO_TRANSLATE_Date.TabIndex = 4;
             this._NO_TRANSLATE_Date.Text = "##Commit date: {0}";
             // 
@@ -94,7 +94,7 @@
             this.groupBox1.Controls.Add(this.Soft);
             this.groupBox1.Location = new System.Drawing.Point(16, 133);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(475, 100);
+            this.groupBox1.Size = new System.Drawing.Size(510, 100);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Reset type";
@@ -104,7 +104,7 @@
             this.Hard.AutoSize = true;
             this.Hard.Location = new System.Drawing.Point(7, 68);
             this.Hard.Name = "Hard";
-            this.Hard.Size = new System.Drawing.Size(455, 17);
+            this.Hard.Size = new System.Drawing.Size(500, 19);
             this.Hard.TabIndex = 2;
             this.Hard.Text = "Hard: reset working dir and index (discard ALL local changes, even uncommitted ch" +
                 "anges)";
@@ -116,7 +116,7 @@
             this.Mixed.Checked = true;
             this.Mixed.Location = new System.Drawing.Point(7, 44);
             this.Mixed.Name = "Mixed";
-            this.Mixed.Size = new System.Drawing.Size(256, 17);
+            this.Mixed.Size = new System.Drawing.Size(276, 19);
             this.Mixed.TabIndex = 1;
             this.Mixed.TabStop = true;
             this.Mixed.Text = "Mixed: leave working dir untouched, reset index";
@@ -127,7 +127,7 @@
             this.Soft.AutoSize = true;
             this.Soft.Location = new System.Drawing.Point(7, 20);
             this.Soft.Name = "Soft";
-            this.Soft.Size = new System.Drawing.Size(237, 17);
+            this.Soft.Size = new System.Drawing.Size(257, 19);
             this.Soft.TabIndex = 0;
             this.Soft.Text = "Soft: leave working dir and index untouched";
             this.Soft.UseVisualStyleBackColor = true;
@@ -136,7 +136,7 @@
             // 
             this.Ok.Location = new System.Drawing.Point(335, 239);
             this.Ok.Name = "Ok";
-            this.Ok.Size = new System.Drawing.Size(75, 25);
+            this.Ok.Size = new System.Drawing.Size(91, 25);
             this.Ok.TabIndex = 6;
             this.Ok.Text = "OK";
             this.Ok.UseVisualStyleBackColor = true;
@@ -144,9 +144,9 @@
             // 
             // Cancel
             // 
-            this.Cancel.Location = new System.Drawing.Point(416, 239);
+            this.Cancel.Location = new System.Drawing.Point(432, 239);
             this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(75, 25);
+            this.Cancel.Size = new System.Drawing.Size(91, 25);
             this.Cancel.TabIndex = 7;
             this.Cancel.Text = "Cancel";
             this.Cancel.UseVisualStyleBackColor = true;
@@ -154,9 +154,9 @@
             // 
             // FormResetCurrentBranch
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 269);
+            this.ClientSize = new System.Drawing.Size(538, 269);
             this.Controls.Add(this.Cancel);
             this.Controls.Add(this.Ok);
             this.Controls.Add(this.groupBox1);
