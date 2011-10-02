d27ad2b0fe6c6cdf3bddf9597fe704391d9f0a71
Ralph Haußmann <ralph@scanmyfood.de>
2011-10-02 11:16:26 +0200

    Translation + GUI: German translation of FormCheckout
    Added TranslationStrings: Complete form can be translated
    German translation

--- GitUI/FormCheckout.cs
+++ GitUI/FormCheckout.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using ResourceManager.Translation;
 
 namespace GitUI
 {
     public partial class FormCheckout : GitExtensionsForm
     {
+        private readonly TranslationString _noRevisionSelectedMsgBox =
+            new TranslationString("Select 1 revision to checkout.");
+        private readonly TranslationString _noRevisionSelectedMsgBoxCaption =
+            new TranslationString("Checkout");
+
         public FormCheckout()
         {
             InitializeComponent();
@@ -18,7 +24,7 @@ namespace GitUI
             {
                 if (RevisionGrid.GetRevisions().Count != 1)
                 {
-                    MessageBox.Show("Select 1 revision to checkout.", "Checkout");
+                    MessageBox.Show(_noRevisionSelectedMsgBox.Text, _noRevisionSelectedMsgBoxCaption.Text);
                     return;
                 }
 
