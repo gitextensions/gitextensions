57d0b71718a709455e396e2628d1a5f9738c253e
Ralph Haußmann <ralph@scanmyfood.de>
2011-10-02 14:56:42 +0200

    Translation + GUI: German translation of FormCommandlineHelp
    GUI fixes for german translation
    German translation

--- GitUI/FormCommandlineHelp.Designer.cs
+++ GitUI/FormCommandlineHelp.Designer.cs
@@ -37,15 +37,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(252, 351);
+            this.label1.Size = new System.Drawing.Size(276, 405);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // FormCommandlineHelp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 398);
+            this.ClientSize = new System.Drawing.Size(417, 447);
             this.Controls.Add(this.label1);
             this.Name = "FormCommandlineHelp";
             this.Text = "Commandline usage";
