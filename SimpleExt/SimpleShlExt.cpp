e58740aee099d63544bc580537e96eb1bc0f3e51
Arkadiy Shapkin <dragon.artec3d@gmail.com>
2011-11-15 04:01:40 +0400

    Unified SimpleExt project settings
    C++ code reformated
    Removed unused files
    Mixed Platforms configuration removed

--- SimpleExt/SimpleShlExt.cpp
+++ SimpleExt/SimpleShlExt.cpp
@@ -2,7 +2,7 @@
 
 #include "stdafx.h"
 #include "resource.h"
-#include "SimpleExt.h"
+#include "Generated/SimpleExt.h"
 #include "SimpleShlExt.h"
 //#include "afx.h"
 
@@ -17,16 +17,16 @@
 STDMETHODIMP CSimpleShlExt::Initialize (
     LPCITEMIDLIST pidlFolder, LPDATAOBJECT pDataObj, HKEY hProgID )
 {
-FORMATETC fmt = { CF_HDROP, NULL, DVASPECT_CONTENT, -1, TYMED_HGLOBAL };
-STGMEDIUM stg = { TYMED_HGLOBAL };
-HDROP     hDrop;
+    FORMATETC fmt = { CF_HDROP, NULL, DVASPECT_CONTENT, -1, TYMED_HGLOBAL };
+    STGMEDIUM stg = { TYMED_HGLOBAL };
+    HDROP     hDrop;
 
     // Look for CF_HDROP data in the data object.
     if ( pDataObj == NULL || FAILED( pDataObj->GetData ( &fmt, &stg ) ))
-        {
+    {
         // Nope! Return an "invalid argument" error back to Explorer.
         return E_INVALIDARG;
-        }
+    }
 
     // Get a pointer to the actual data.
     hDrop = (HDROP) GlobalLock ( stg.hGlobal );
@@ -36,15 +36,15 @@ HDROP     hDrop;
         return E_INVALIDARG;
 
     // Sanity check - make sure there is at least one filename.
-UINT uNumFiles = DragQueryFile ( hDrop, 0xFFFFFFFF, NULL, 0 );
-HRESULT hr = S_OK;
+    UINT uNumFiles = DragQueryFile ( hDrop, 0xFFFFFFFF, NULL, 0 );
+    HRESULT hr = S_OK;
 
     if ( 0 == uNumFiles )
-        {
+    {
         GlobalUnlock ( stg.hGlobal );
         ReleaseStgMedium ( &stg );
         return E_INVALIDARG;
-        }
+    }
 
     // Get the name of the first file and store it in our member variable m_szFile.
     if ( 0 == DragQueryFile ( hDrop, 0, m_szFile, MAX_PATH ) )
@@ -66,43 +66,44 @@ STDMETHODIMP CSimpleShlExt::QueryContextMenu  (
 
     //InsertMenu (hmenu, uMenuIndex, MF_BYPOSITION, uidFirstCmd, _T("GitEx") );
 
-	int id = 1;
+    int id = 1;
 
-	HMENU popupMenu = CreateMenu();
+    HMENU popupMenu = CreateMenu();
 
-	id = PopulateMenu(popupMenu, uidFirstCmd + id);
+    id = PopulateMenu(popupMenu, uidFirstCmd + id);
 
-	InsertMenu(hmenu, uMenuIndex++, MF_STRING | MF_BYPOSITION | MF_POPUP, (int)popupMenu, "Git Extensions");	
+    InsertMenu(hmenu, uMenuIndex++, MF_STRING | MF_BYPOSITION | MF_POPUP, (int)popupMenu, "Git Extensions");	
     return MAKE_HRESULT ( SEVERITY_SUCCESS, FACILITY_NULL, id-uidFirstCmd );
 }
 
 void CSimpleShlExt::AddMenuItem(HMENU hMenu, LPSTR text, int id, UINT position)
 {
-	MENUITEMINFO mii;
-	memset(&mii, 0, sizeof(mii));
-	mii.cbSize = sizeof(mii);
-	mii.fMask = 0x00000010|0x00000002|0x00000001;//MIIM_STRING | MIIM_FTYPE;
-	mii.wID	= id;
-	mii.fType = 0x00000000;
-	mii.dwTypeData	= text;
-	mii.fState = (UINT)0x00000000;
-	InsertMenuItem(hMenu, position, TRUE, &mii);
+    MENUITEMINFO mii;
+    memset(&mii, 0, sizeof(mii));
+    mii.cbSize = sizeof(mii);
+    mii.fMask = 0x00000010|0x00000002|0x00000001;//MIIM_TYPE|MIIM_ID|MIIM_STATE;
+    mii.wID	= id;
+    mii.fType = 0x00000000;
+    mii.dwTypeData	= text;
+    mii.fState = (UINT)0x00000000;
 
-	//InsertMenu(hMenu, position, MF_BYPOSITION, id, _T("test"));
+    InsertMenuItem(hMenu, position, TRUE, &mii);
+
+    //InsertMenu(hMenu, position, MF_BYPOSITION, id, _T("test"));
 }
 
 int CSimpleShlExt::PopulateMenu(HMENU hMenu, int id)
 {
     AddMenuItem(hMenu, "Add files", id, AddFilesId=0);
     AddMenuItem(hMenu, "Apply patch", ++id, ApplyPatchId=1);
-	AddMenuItem(hMenu, "Browse", ++id, BrowseId=2);
+    AddMenuItem(hMenu, "Browse", ++id, BrowseId=2);
     AddMenuItem(hMenu, "Create branch", ++id, CreateBranchId=3);
     AddMenuItem(hMenu, "Checkout branch", ++id, CheckoutBranchId=4);
     AddMenuItem(hMenu, "Checkout revision", ++id, CheckoutRevisionId=5);
     AddMenuItem(hMenu, "Clone", ++id, CloneId=6);
     AddMenuItem(hMenu, "Commit", ++id, CommitId=7);
-	AddMenuItem(hMenu, "File history", ++id, FileHistoryId=8);
-   	AddMenuItem(hMenu, "Reset file changes", ++id, ResetFileChangesId=9);
+    AddMenuItem(hMenu, "File history", ++id, FileHistoryId=8);
+    AddMenuItem(hMenu, "Reset file changes", ++id, ResetFileChangesId=9);
     AddMenuItem(hMenu, "Pull", ++id, PullId=10);
     AddMenuItem(hMenu, "Push", ++id, PushId=11);
     AddMenuItem(hMenu, "Settings", ++id, SettingsId=12);
@@ -114,7 +115,7 @@ int CSimpleShlExt::PopulateMenu(HMENU hMenu, int id)
 STDMETHODIMP CSimpleShlExt::GetCommandString (
     UINT_PTR idCmd, UINT uFlags, UINT* pwReserved, LPSTR pszName, UINT cchMax )
 {
-USES_CONVERSION;
+    USES_CONVERSION;
 
     // Check idCmd, it must be 0 since we have only one menu item.
     if ( 0 != idCmd )
@@ -123,49 +124,49 @@ USES_CONVERSION;
     // If Explorer is asking for a help string, copy our string into the
     // supplied buffer.
     if ( uFlags & GCS_HELPTEXT )
-        {
+    {
         LPCTSTR szText = _T("Git shell extensions");
 
         if ( uFlags & GCS_UNICODE )
-            {
+        {
             // We need to cast pszName to a Unicode string, and then use the
             // Unicode string copy API.
             lstrcpynW ( (LPWSTR) pszName, T2CW(szText), cchMax );
-            }
+        }
         else
-            {
+        {
             // Use the ANSI string copy API to return the help string.
             lstrcpynA ( pszName, T2CA(szText), cchMax );	
-            }
+        }
 
         return S_OK;
-        }
+    }
 
     return E_INVALIDARG;
 }
 
 void CSimpleShlExt::RunGitEx(const char * command)
 {
-	CString szFile = m_szFile;
-	CString szCommandName = command;
-	CString args;
+    CString szFile = m_szFile;
+    CString szCommandName = command;
+    CString args;
 
-	args += command;
-	args += " \"";
-	args += m_szFile;
-	args += "\"";
+    args += command;
+    args += " \"";
+    args += m_szFile;
+    args += "\"";
 
-	CString dir = "";
+    CString dir = "";
 
-	if (dir.GetLength() == 0)
-		dir = GetRegistryValue(HKEY_CURRENT_USER, "SOFTWARE\\GitExtensions\\GitExtensions", "InstallDir");
-	if (dir.GetLength() == 0)
-		dir = GetRegistryValue(HKEY_USERS, "SOFTWARE\\GitExtensions\\GitExtensions", "InstallDir");
-	if (dir.GetLength() == 0)
-		dir = GetRegistryValue(HKEY_LOCAL_MACHINE, "SOFTWARE\\GitExtensions\\GitExtensions", "InstallDir");
+    if (dir.GetLength() == 0)
+        dir = GetRegistryValue(HKEY_CURRENT_USER, "SOFTWARE\\GitExtensions\\GitExtensions", "InstallDir");
+    if (dir.GetLength() == 0)
+        dir = GetRegistryValue(HKEY_USERS, "SOFTWARE\\GitExtensions\\GitExtensions", "InstallDir");
+    if (dir.GetLength() == 0)
+        dir = GetRegistryValue(HKEY_LOCAL_MACHINE, "SOFTWARE\\GitExtensions\\GitExtensions", "InstallDir");
 
-	ShellExecute(NULL, "open", "GitExtensions.exe", args, dir, SW_SHOWNORMAL); 
-	//system(szMsg);
+    ShellExecute(NULL, "open", "GitExtensions.exe", args, dir, SW_SHOWNORMAL); 
+    //system(szMsg);
 }
 
 STDMETHODIMP CSimpleShlExt::InvokeCommand ( LPCMINVOKECOMMANDINFO pCmdInfo )
@@ -174,78 +175,78 @@ STDMETHODIMP CSimpleShlExt::InvokeCommand ( LPCMINVOKECOMMANDINFO pCmdInfo )
     if ( pCmdInfo == NULL ||0 != HIWORD( pCmdInfo->lpVerb ) )
         return E_INVALIDARG;
 
-	int invokeId = LOWORD( pCmdInfo->lpVerb) - 1;
+    int invokeId = LOWORD( pCmdInfo->lpVerb) - 1;
 
     // Get the command index - the only valid one is 0.
-	if (invokeId == AddFilesId)
+    if (invokeId == AddFilesId)
     {
-		RunGitEx(_T("addfiles"));
+        RunGitEx(_T("addfiles"));
         return S_OK;
     } else
     // Get the command index - the only valid one is 0.
-	if (invokeId == ApplyPatchId)
+    if (invokeId == ApplyPatchId)
     {
-		RunGitEx(_T("applypatch"));
+        RunGitEx(_T("applypatch"));
         return S_OK;
     } else
-	if (invokeId == BrowseId)
+    if (invokeId == BrowseId)
     {
-		RunGitEx(_T("browse"));
+        RunGitEx(_T("browse"));
         return S_OK;
     } else
-	if (invokeId == CreateBranchId)
+    if (invokeId == CreateBranchId)
     {
-		RunGitEx(_T("branch"));
+        RunGitEx(_T("branch"));
         return S_OK;
     } else
-	if (invokeId == CheckoutBranchId)
+    if (invokeId == CheckoutBranchId)
     {
-		RunGitEx(_T("checkoutbranch"));
+        RunGitEx(_T("checkoutbranch"));
         return S_OK;
     } else
-	if (invokeId == CheckoutRevisionId)
+    if (invokeId == CheckoutRevisionId)
     {
-		RunGitEx(_T("checkoutrevision"));
+        RunGitEx(_T("checkoutrevision"));
         return S_OK;
     } else
-	if (invokeId == CloneId)
+    if (invokeId == CloneId)
     {
-		RunGitEx(_T("clone"));
+        RunGitEx(_T("clone"));
         return S_OK;
     } else
-	if (invokeId == CommitId)
+    if (invokeId == CommitId)
     {
-		RunGitEx(_T("commit"));
+        RunGitEx(_T("commit"));
         return S_OK;
     } else
-	if (invokeId == FileHistoryId)
+    if (invokeId == FileHistoryId)
     {
-		RunGitEx(_T("filehistory"));
+        RunGitEx(_T("filehistory"));
         return S_OK;
     } else
-	if (invokeId == PullId)
+    if (invokeId == PullId)
     {
-		RunGitEx(_T("pull"));
+        RunGitEx(_T("pull"));
         return S_OK;
     } else
-	if (invokeId == PushId)
+    if (invokeId == PushId)
     {
-		RunGitEx(_T("push"));
+        RunGitEx(_T("push"));
         return S_OK;
     } else
-	if (invokeId == SettingsId)
+    if (invokeId == SettingsId)
     {
-		RunGitEx(_T("settings"));
+        RunGitEx(_T("settings"));
         return S_OK;
     } else
-	if (invokeId == ViewDiffId)
+    if (invokeId == ViewDiffId)
     {
-		RunGitEx(_T("viewdiff"));
+        RunGitEx(_T("viewdiff"));
         return S_OK;
     } else
-	if (invokeId == ResetFileChangesId)
+    if (invokeId == ResetFileChangesId)
     {
-		RunGitEx(_T("revert"));
+        RunGitEx(_T("revert"));
         return S_OK;
     }
     return E_INVALIDARG;
