eb88c2dd82b90a785619c49a027f4c24e6fdbee7
Arkadiy Shapkin <dragon.artec3d@gmail.com>
2011-12-07 03:25:53 +0400

    ProcessSubmodulePatch minor formating bug fixed
    Submodules diff changed to some way as in commit window

--- GitUI/FormBrowse.cs
+++ GitUI/FormBrowse.cs
@@ -716,7 +716,6 @@ namespace GitUI
                     }
                 }
 
-
                 // Refresh tree
                 GitTree.Nodes.Clear();
                 //restore selected file and scroll position when new selection is done
@@ -752,7 +751,6 @@ namespace GitUI
                     if (lastMatchedNode != matchedNode)
                         FileText.ResetCurrentScrollPos();
                     GitTree.SelectedNode = lastMatchedNode;
-
                 }
             }
             finally
@@ -760,8 +758,6 @@ namespace GitUI
                 GitTree.ResumeLayout();
             }
         }
-
-
 
         private void FillDiff()
         {
@@ -1010,8 +1006,6 @@ namespace GitUI
                     if (!CommitInfoTabControl.TabPages.Contains(Tree))
                         CommitInfoTabControl.TabPages.Insert(1, Tree);
                 }
-
-
 
                 FillFileTree();
                 FillDiff();
@@ -1439,6 +1433,9 @@ namespace GitUI
 
             if (patch == null)
                 return string.Empty;
+
+            if (file.IsSubmodule)
+                return GitCommandHelpers.ProcessSubmodulePatch(patch.Text);
 
             return patch.Text;
         }
