c44d2ec420339a290e8e114358a0e16019c0d792
lynxstv <lynxstv@gmail.com>
2011-12-09 16:13:29 +0400

    Add EncodingHelper
    Signed-off-by: lynxstv <lynxstv@gmail.com>

---
+++ GitCommands/Git/EncodingHelper.cs
@@ -0,0 +1,43 @@
+﻿namespace GitCommands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Encoding Helper
+    /// </summary>
+    public class EncodingHelper
+    {
+        public static string GetString(byte[] output, byte[] error, Encoding encoding)
+        {
+            if (encoding == null)
+            {
+                throw new ArgumentNullException("encoding");
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            if (output != null && output.Length > 0)
+            {
+                sb.Append(encoding.GetString(output));
+            }
+            if (error != null && error.Length > 0 && output != null && output.Length > 0)
+            {
+                sb.AppendLine();
+            }
+            if (error != null && error.Length > 0)
+            {
+                sb.Append(encoding.GetString(error));
+            }
+            return sb.ToString();
+        }
+
+        public static byte[] ConvertTo(Encoding encoding, string filename)
+        {
+            byte[] bytesunicode = Encoding.Unicode.GetBytes(filename);
+            return Encoding.Convert(Encoding.Unicode, encoding, bytesunicode);
+        }
+    }
+}
