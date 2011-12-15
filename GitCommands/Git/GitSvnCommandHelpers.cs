924af18c28422f53331d88b14f3136e9163b0793
lynxstv <lynxstv@gmail.com>
2011-12-15 16:48:27 +0400

    Add GitSvnCommandHelpers with CloneCmd

---
+++ GitCommands/Git/GitSvnCommandHelpers.cs
@@ -0,0 +1,30 @@
+﻿namespace GitCommands.Git
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// git svn commands:
+    /// svn clone
+    /// svn fetch
+    /// svn rebase
+    /// svn dcommit
+    /// </summary>
+    public static class GitSvnCommandHelpers
+    {
+        private static string SvnPrefix = "svn";
+
+        public static string CloneCmd(string fromSvn, string toPath)
+        {
+            toPath = GitCommandHelpers.FixPath(toPath);
+            StringBuilder sb = new StringBuilder();
+            sb.Append(SvnPrefix);
+            sb.Append(" clone ");
+            sb.AppendFormat("\"{0}\"", fromSvn.Trim());
+            sb.AppendFormat("\"{0}\"", toPath.Trim());
+            return sb.ToString();
+        }
+    }
+}
