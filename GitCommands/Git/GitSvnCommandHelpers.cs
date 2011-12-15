d0efc5ff65e9bcdd69e0d9a7ffda1f5ee59e9c5a
lynxstv <lynxstv@gmail.com>
2011-12-15 18:15:44 +0400

    Add FormSvnClone with authors file

--- GitCommands/Git/GitSvnCommandHelpers.cs
+++ GitCommands/Git/GitSvnCommandHelpers.cs
@@ -1,4 +1,4 @@
-﻿namespace GitCommands.Git
+﻿namespace GitCommands
 {
     using System;
     using System.Collections.Generic;
@@ -16,14 +16,20 @@
     {
         private static string SvnPrefix = "svn";
 
-        public static string CloneCmd(string fromSvn, string toPath)
+        public static string CloneCmd(string fromSvn, string toPath, string authorsFile)
         {
             toPath = GitCommandHelpers.FixPath(toPath);
             StringBuilder sb = new StringBuilder();
             sb.Append(SvnPrefix);
             sb.Append(" clone ");
             sb.AppendFormat("\"{0}\"", fromSvn.Trim());
+            sb.Append(' ');
             sb.AppendFormat("\"{0}\"", toPath.Trim());
+            if (authorsFile != null && authorsFile.Trim().Length!=0)
+            {
+                sb.Append(" --authors-file ");
+                sb.AppendFormat("\"{0}\"", authorsFile.Trim());
+            }
             return sb.ToString();
         }
     }
---
+++ GitUI/FormSvnClone.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace GitUI
+{
+    partial class FormSvnClone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSvnClone));
+            this.svnRepositoryComboBox = new System.Windows.Forms.ComboBox();
+            this.destinationComboBox = new System.Windows.Forms.ComboBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.subdirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.authorsFileTextBox = new System.Windows.Forms.TextBox();
+            this.authorsFileBrowseButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // svnRepositoryComboBox
+            // 
+            this.svnRepositoryComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.svnRepositoryComboBox.FormattingEnabled = true;
+            this.svnRepositoryComboBox.Location = new System.Drawing.Point(160, 13);
+            this.svnRepositoryComboBox.Name = "svnRepositoryComboBox";
+            this.svnRepositoryComboBox.Size = new System.Drawing.Size(634, 21);
+            this.svnRepositoryComboBox.TabIndex = 1;
+            // 
+            // destinationComboBox
+            // 
+            this.destinationComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.destinationComboBox.FormattingEnabled = true;
+            this.destinationComboBox.Location = new System.Drawing.Point(160, 42);
+            this.destinationComboBox.Name = "destinationComboBox";
+            this.destinationComboBox.Size = new System.Drawing.Size(531, 21);
+            this.destinationComboBox.TabIndex = 2;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseButton.Location = new System.Drawing.Point(697, 41);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(97, 25);
+            this.browseButton.TabIndex = 3;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Svn repository to clone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Destination";
+            // 
+            // subdirectoryTextBox
+            // 
+            this.subdirectoryTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.subdirectoryTextBox.Location = new System.Drawing.Point(160, 71);
+            this.subdirectoryTextBox.Name = "subdirectoryTextBox";
+            this.subdirectoryTextBox.Size = new System.Drawing.Size(531, 20);
+            this.subdirectoryTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subdirectory to create";
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.Location = new System.Drawing.Point(719, 369);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 5;
+            this.okButton.Text = "Clone";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // authorsFileTextBox
+            // 
+            this.authorsFileTextBox.Location = new System.Drawing.Point(160, 97);
+            this.authorsFileTextBox.Name = "authorsFileTextBox";
+            this.authorsFileTextBox.Size = new System.Drawing.Size(531, 20);
+            this.authorsFileTextBox.TabIndex = 8;
+            // 
+            // authorsFileBrowseButton
+            // 
+            this.authorsFileBrowseButton.Location = new System.Drawing.Point(697, 94);
+            this.authorsFileBrowseButton.Name = "authorsFileBrowseButton";
+            this.authorsFileBrowseButton.Size = new System.Drawing.Size(97, 25);
+            this.authorsFileBrowseButton.TabIndex = 9;
+            this.authorsFileBrowseButton.Text = "Browse";
+            this.authorsFileBrowseButton.UseVisualStyleBackColor = true;
+            this.authorsFileBrowseButton.Click += new System.EventHandler(this.authorsFileBrowseButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Authors file";
+            // 
+            // FormSvnClone
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 404);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.authorsFileBrowseButton);
+            this.Controls.Add(this.authorsFileTextBox);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.subdirectoryTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.destinationComboBox);
+            this.Controls.Add(this.svnRepositoryComboBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormSvnClone";
+            this.Text = "Svn Clone";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox svnRepositoryComboBox;
+        private System.Windows.Forms.ComboBox destinationComboBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox subdirectoryTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TextBox authorsFileTextBox;
+        private System.Windows.Forms.Button authorsFileBrowseButton;
+        private System.Windows.Forms.Label label4;
+    }
+}
