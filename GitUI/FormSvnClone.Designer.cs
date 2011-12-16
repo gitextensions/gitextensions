0244e7e8bf7638aaf08393ee7267fca81990e54b
lynxstv <lynxstv@gmail.com>
2011-12-16 11:56:16 +0400

    Russian translation for FormSvnClone

--- GitUI/FormSvnClone.Designer.cs
+++ GitUI/FormSvnClone.Designer.cs
@@ -29,44 +29,44 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSvnClone));
-            this.svnRepositoryComboBox = new System.Windows.Forms.ComboBox();
-            this.destinationComboBox = new System.Windows.Forms.ComboBox();
+            this._NO_TRANSLATE_svnRepositoryComboBox = new System.Windows.Forms.ComboBox();
+            this._NO_TRANSLATE_destinationComboBox = new System.Windows.Forms.ComboBox();
             this.browseButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.subdirectoryTextBox = new System.Windows.Forms.TextBox();
+            this._NO_TRANSLATE_subdirectoryTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.okButton = new System.Windows.Forms.Button();
-            this.authorsFileTextBox = new System.Windows.Forms.TextBox();
+            this._NO_TRANSLATE_authorsFileTextBox = new System.Windows.Forms.TextBox();
             this.authorsFileBrowseButton = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // svnRepositoryComboBox
+            // _NO_TRANSLATE_svnRepositoryComboBox
             // 
-            this.svnRepositoryComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this._NO_TRANSLATE_svnRepositoryComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.svnRepositoryComboBox.FormattingEnabled = true;
-            this.svnRepositoryComboBox.Location = new System.Drawing.Point(160, 13);
-            this.svnRepositoryComboBox.Name = "svnRepositoryComboBox";
-            this.svnRepositoryComboBox.Size = new System.Drawing.Size(320, 21);
-            this.svnRepositoryComboBox.TabIndex = 1;
+            this._NO_TRANSLATE_svnRepositoryComboBox.FormattingEnabled = true;
+            this._NO_TRANSLATE_svnRepositoryComboBox.Location = new System.Drawing.Point(160, 13);
+            this._NO_TRANSLATE_svnRepositoryComboBox.Name = "_NO_TRANSLATE_svnRepositoryComboBox";
+            this._NO_TRANSLATE_svnRepositoryComboBox.Size = new System.Drawing.Size(320, 21);
+            this._NO_TRANSLATE_svnRepositoryComboBox.TabIndex = 1;
             // 
-            // destinationComboBox
+            // _NO_TRANSLATE_destinationComboBox
             // 
-            this.destinationComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this._NO_TRANSLATE_destinationComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.destinationComboBox.FormattingEnabled = true;
-            this.destinationComboBox.Location = new System.Drawing.Point(160, 42);
-            this.destinationComboBox.Name = "destinationComboBox";
-            this.destinationComboBox.Size = new System.Drawing.Size(217, 21);
-            this.destinationComboBox.TabIndex = 2;
-            this.destinationComboBox.DropDown += new System.EventHandler(this.destinationComboBox_DropDown);
+            this._NO_TRANSLATE_destinationComboBox.FormattingEnabled = true;
+            this._NO_TRANSLATE_destinationComboBox.Location = new System.Drawing.Point(160, 41);
+            this._NO_TRANSLATE_destinationComboBox.Name = "_NO_TRANSLATE_destinationComboBox";
+            this._NO_TRANSLATE_destinationComboBox.Size = new System.Drawing.Size(217, 21);
+            this._NO_TRANSLATE_destinationComboBox.TabIndex = 2;
+            this._NO_TRANSLATE_destinationComboBox.DropDown += new System.EventHandler(this.destinationComboBox_DropDown);
             // 
             // browseButton
             // 
             this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.browseButton.Location = new System.Drawing.Point(383, 41);
+            this.browseButton.Location = new System.Drawing.Point(383, 40);
             this.browseButton.Name = "browseButton";
             this.browseButton.Size = new System.Drawing.Size(97, 25);
             this.browseButton.TabIndex = 3;
@@ -79,61 +79,61 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.Size = new System.Drawing.Size(118, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Svn repository to clone";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Location = new System.Drawing.Point(13, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Destination";
             // 
-            // subdirectoryTextBox
+            // _NO_TRANSLATE_subdirectoryTextBox
             // 
-            this.subdirectoryTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this._NO_TRANSLATE_subdirectoryTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.subdirectoryTextBox.Location = new System.Drawing.Point(160, 71);
-            this.subdirectoryTextBox.Name = "subdirectoryTextBox";
-            this.subdirectoryTextBox.Size = new System.Drawing.Size(217, 20);
-            this.subdirectoryTextBox.TabIndex = 4;
+            this._NO_TRANSLATE_subdirectoryTextBox.Location = new System.Drawing.Point(160, 69);
+            this._NO_TRANSLATE_subdirectoryTextBox.Name = "_NO_TRANSLATE_subdirectoryTextBox";
+            this._NO_TRANSLATE_subdirectoryTextBox.Size = new System.Drawing.Size(217, 21);
+            this._NO_TRANSLATE_subdirectoryTextBox.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Location = new System.Drawing.Point(12, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.Size = new System.Drawing.Size(115, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Subdirectory to create";
             // 
             // okButton
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.Location = new System.Drawing.Point(405, 139);
+            this.okButton.Location = new System.Drawing.Point(383, 137);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.Size = new System.Drawing.Size(97, 25);
             this.okButton.TabIndex = 5;
             this.okButton.Text = "Clone";
             this.okButton.UseVisualStyleBackColor = true;
             this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
-            // authorsFileTextBox
+            // _NO_TRANSLATE_authorsFileTextBox
             // 
-            this.authorsFileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this._NO_TRANSLATE_authorsFileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.authorsFileTextBox.Location = new System.Drawing.Point(160, 97);
-            this.authorsFileTextBox.Name = "authorsFileTextBox";
-            this.authorsFileTextBox.Size = new System.Drawing.Size(217, 20);
-            this.authorsFileTextBox.TabIndex = 8;
+            this._NO_TRANSLATE_authorsFileTextBox.Location = new System.Drawing.Point(160, 97);
+            this._NO_TRANSLATE_authorsFileTextBox.Name = "_NO_TRANSLATE_authorsFileTextBox";
+            this._NO_TRANSLATE_authorsFileTextBox.Size = new System.Drawing.Size(217, 21);
+            this._NO_TRANSLATE_authorsFileTextBox.TabIndex = 8;
             // 
             // authorsFileBrowseButton
             // 
             this.authorsFileBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.authorsFileBrowseButton.Location = new System.Drawing.Point(383, 94);
+            this.authorsFileBrowseButton.Location = new System.Drawing.Point(383, 95);
             this.authorsFileBrowseButton.Name = "authorsFileBrowseButton";
             this.authorsFileBrowseButton.Size = new System.Drawing.Size(97, 25);
             this.authorsFileBrowseButton.TabIndex = 9;
@@ -144,9 +144,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 100);
+            this.label4.Location = new System.Drawing.Point(12, 100);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.Size = new System.Drawing.Size(62, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "Authors file";
             // 
@@ -156,24 +156,23 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(492, 174);
+            this.Controls.Add(this._NO_TRANSLATE_authorsFileTextBox);
+            this.Controls.Add(this._NO_TRANSLATE_subdirectoryTextBox);
+            this.Controls.Add(this._NO_TRANSLATE_destinationComboBox);
+            this.Controls.Add(this._NO_TRANSLATE_svnRepositoryComboBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.authorsFileBrowseButton);
-            this.Controls.Add(this.authorsFileTextBox);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.subdirectoryTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.browseButton);
-            this.Controls.Add(this.destinationComboBox);
-            this.Controls.Add(this.svnRepositoryComboBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(700, 208);
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(500, 208);
             this.Name = "FormSvnClone";
-            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Svn Clone";
             this.ResumeLayout(false);
@@ -183,15 +182,15 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox svnRepositoryComboBox;
-        private System.Windows.Forms.ComboBox destinationComboBox;
+        private System.Windows.Forms.ComboBox _NO_TRANSLATE_svnRepositoryComboBox;
+        private System.Windows.Forms.ComboBox _NO_TRANSLATE_destinationComboBox;
         private System.Windows.Forms.Button browseButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox subdirectoryTextBox;
+        private System.Windows.Forms.TextBox _NO_TRANSLATE_subdirectoryTextBox;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button okButton;
-        private System.Windows.Forms.TextBox authorsFileTextBox;
+        private System.Windows.Forms.TextBox _NO_TRANSLATE_authorsFileTextBox;
         private System.Windows.Forms.Button authorsFileBrowseButton;
         private System.Windows.Forms.Label label4;
     }
