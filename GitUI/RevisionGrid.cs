d310898ba28cd911d1999556e234b7da3c411689
Basewq <chromaticcore@gmail.com>
2012-01-25 19:08:47 +1300

    Fix issue #780 (continued 2): Relative author/commit date in the commit history graph is incorrect.

--- GitUI/RevisionGrid.cs
+++ GitUI/RevisionGrid.cs
@@ -1811,8 +1811,38 @@ namespace GitUI
             if (!Settings.RelativeDate)
                 return string.Format("{0} {1}", time.ToShortDateString(), time.ToLongTimeString());
 
-            var span = DateTime.Now - time;
 
+            var span = DateTime.Now - time;
+            
+            #region Relative time output note
+            /*
+            To summarise, the output always rounds down the relative time. eg. 2.9 days = "2 days ago"
+            The following table describes the output in detail:
+            
+            displayed  |                 |  time unit for
+             output    |  time interval  |  time interval
+           ------------+-----------------+-------------------
+            0 seconds     [0.0, 1.0)        seconds
+            1 second      [1.0, 2.0)        seconds
+            n seconds     [2.0, 60.0)       seconds
+
+            1 minute      [1.0, 2.0)        minutes
+            n minutes     [2.0, 60.0)       minutes
+
+            1 hour        [1.0, 2.0)        hours
+            n hours       [2.0, 24.0)       hours
+
+            1 day         [1.0, 2.0)        days
+            n days        [2.0, 30.0)       days
+
+            1 month       [30.0, 60.0)      days
+            n months      [60.0, 365.0)     days
+
+            1 year        [365.0, 730.0)    days
+            n years       [730.0, inf)      days
+            
+             */
+            #endregion
             if (span.TotalMinutes < 1.0)
             {
                 if (span.Seconds == 1)
@@ -1845,18 +1875,18 @@ namespace GitUI
                     return string.Format(Strings.GetNDaysAgoText(), span.Days);
             }
 
-            if (span.TotalDays < 345.0)     // output "n month"/"n months" up to 11.5 months (exclusive)
+            if (span.TotalDays < 365.0)
             {
-                if (span.Days < 45)    // less than 1.5 months = "1 month"
+                if (span.Days < 60)
                     return string.Format(Strings.Get1MonthAgoText(), "1");
-                else
-                    return string.Format(Strings.GetNMonthsAgoText(), (int)Math.Round(span.TotalDays / 30.0));
+                else    // 30.417 = 365 days / 12 months - note that the if statement only bothers with 30 days for "1 month ago" because span.Days is int.
+                    return string.Format(Strings.GetNMonthsAgoText(), (int)(span.TotalDays / 30.417));  // round down
             }
 
-            if (span.TotalDays < 547.5)  // less than 1.5 years = "1 year"
+            if (span.TotalDays < 730.0)  // less than 2.0 years = "1 year"
                 return string.Format(Strings.Get1YearAgoText(), "1");
             else
-                return string.Format(Strings.GetNYearsAgoText(), (int)Math.Round(span.TotalDays / 365.0));
+                return string.Format(Strings.GetNYearsAgoText(), (int)(span.TotalDays / 365.0));        // round down
         }
 
         private void UpdateGraph(GitRevision rev)
