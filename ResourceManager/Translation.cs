bc788ff1659994b285486274baf7166198050c49
Arkadiy Shapkin <dragon.artec3d@gmail.com>
2012-02-20 01:10:47 +0400

    Translation text sorting fixed.
    Russian translation errors fixed.

--- ResourceManager/Translation.cs
+++ ResourceManager/Translation.cs
@@ -15,6 +15,17 @@ namespace ResourceManager.Translation
 
         public List<TranslationCategory> translationCategories;
 
+        public TranslationCategory FindOrAddTranslationCategory(string translationCategory)
+        {
+            TranslationCategory tc = GetTranslationCategory(translationCategory);
+            if (tc == null)
+            {
+                tc = new TranslationCategory(translationCategory);
+                AddTranslationCategory(tc);
+            }
+            return tc;
+        }
+
         public void AddTranslationCategory(TranslationCategory translationCategory)
         {
             if (string.IsNullOrEmpty(translationCategory.Name))
@@ -36,6 +47,16 @@ namespace ResourceManager.Translation
         public List<TranslationCategory> GetTranslationCategories()
         {
             return translationCategories;
+        }
+
+        public void Sort()
+        {
+            translationCategories.Sort((tc1, tc2) => tc1.Name.CompareTo(tc2.Name));
+            foreach(TranslationCategory tc in translationCategories)
+                tc.GetTranslationItems().Sort((tc1, tc2) => {
+                    var val = tc1.Name.CompareTo(tc2.Name);
+                    if (val == 0) val = tc1.Property.CompareTo(tc2.Property);
+                    return val; });
         }
 
     }
