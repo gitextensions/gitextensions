fea3c87d0c247385ce8ee8ad69ec11ae672bc783
Phillip Cohen <pc@phillipcohen.net>
2012-04-08 16:43:45 -0500

    Cleaned up this form to make it simpler and less threatening.

--- GitUI/FormPuttyError.cs
+++ GitUI/FormPuttyError.cs
@@ -4,34 +4,24 @@ namespace GitUI
 {
     public partial class FormPuttyError : GitExtensionsForm
     {
+        public bool RetryProcess;
+
         public FormPuttyError()
         {
             InitializeComponent(); Translate();
         }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
+     
         private void LoadSSHKey_Click(object sender, EventArgs e)
         {
             if (BrowseForPrivateKey.BrowseAndLoad(this))
                 CloseForm(true);
-        }
-
-        public bool RetryProcess;
+        }        
 
         private void Abort_Click(object sender, EventArgs e)
         {
             CloseForm(false);
         }
-
-        private void Retry_Click(object sender, EventArgs e)
-        {
-            CloseForm(true);
-        }
-
+      
         private void CloseForm(bool shouldRetry)
         {
             RetryProcess = shouldRetry;
