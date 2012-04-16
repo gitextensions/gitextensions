c59367bd839b4381fe4881b90846832994ff48ad
Ralph Haußmann <ralph@scanmyfood.de>
2012-04-16 10:39:09 +0200

    Commit validation RegEx added

--- GitUI/FormCommitTemplateSettings.cs
+++ GitUI/FormCommitTemplateSettings.cs
@@ -38,6 +38,7 @@ namespace GitUI
             _NO_TRANSLATE_numericMaxFirstLineLength.Value = Settings.CommitValidationMaxCntCharsFirstLine;
             _NO_TRANSLATE_numericMaxLineLength.Value = Settings.CommitValidationMaxCntCharsPerLine;
             checkBoxSecondLineEmpty.Checked = Settings.CommitValidationSecondLineMustBeEmpty;
+            _NO_TRANSLATE_textBoxCommitValidationRegex.Text = Settings.CommitValidationRegEx;
 
             _commitTemplates = CommitTemplateItem.DeserializeCommitTemplates(Settings.CommitTemplates);
 
@@ -64,6 +65,7 @@ namespace GitUI
             Settings.CommitValidationMaxCntCharsFirstLine = Convert.ToInt32(_NO_TRANSLATE_numericMaxFirstLineLength.Value);
             Settings.CommitValidationMaxCntCharsPerLine = Convert.ToInt32(_NO_TRANSLATE_numericMaxLineLength.Value);
             Settings.CommitValidationSecondLineMustBeEmpty = checkBoxSecondLineEmpty.Checked;
+            Settings.CommitValidationRegEx = _NO_TRANSLATE_textBoxCommitValidationRegex.Text;
 
             string serializedCommitTemplates = CommitTemplateItem.SerializeCommitTemplates(_commitTemplates);
             if (null == serializedCommitTemplates)
