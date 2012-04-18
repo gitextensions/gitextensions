3a2d85bf708bdd7eea283cc19fb81be439406fa5
Janusz Białobrzewski <janusz@biu.pl>
2012-04-18 20:47:06 +0200

    removed duplicate entry

--- GitUI/FormCommit.Designer.cs
+++ GitUI/FormCommit.Designer.cs
@@ -112,7 +112,6 @@ namespace GitUI
             this.Amend = new System.Windows.Forms.Button();
             this.Reset = new System.Windows.Forms.Button();
             this.UnstagedSubmoduleContext = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.commitSubmoduleChanges = new System.Windows.Forms.ToolStripMenuItem();
             this.resetSubmoduleChanges = new System.Windows.Forms.ToolStripMenuItem();
             this.updateSubmoduleMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator13 = new System.Windows.Forms.ToolStripSeparator();
