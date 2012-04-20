b31ca2949c707a846fd320839f62b8fa2b66afaf
Ralph Haussmann <ralph@scanmyfood.de>
2012-04-20 17:07:53 +0200

    Remove assignment of FileText.Encoding and DiffText.Encoding and relevant resources
    Changes in FormBrowse.resx introduced
    in 279b3c6f0018c0cf68b9e2d1d28e95868f68b12e
    can't be compiled by resgen2.
    <data name="DiffText.Encoding"> and <data name="FileText.Encoding">
    lead to error: "Invalid ResX input."
    There are several changes introduced in commit 279b3c6f00 which
    are not related to authors intention "Add clone SVN menu item".
    I don't think FileText.Encoding and DiffText.Encoding is
    needed.
    But i also don't know why resgen2 fails.

--- GitUI/FormBrowse.Designer.cs
+++ GitUI/FormBrowse.Designer.cs
@@ -704,7 +704,6 @@ namespace GitUI
             // 
             this.FileText.DisableFocusControlOnHover = false;
             this.FileText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.FileText.Encoding = ((System.Text.Encoding)(resources.GetObject("FileText.Encoding")));
             this.FileText.Font = new System.Drawing.Font("Tahoma", 9.75F);
             this.FileText.IgnoreWhitespaceChanges = false;
             this.FileText.IsReadOnly = true;
@@ -837,7 +836,6 @@ namespace GitUI
             // 
             this.DiffText.DisableFocusControlOnHover = false;
             this.DiffText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DiffText.Encoding = ((System.Text.Encoding)(resources.GetObject("DiffText.Encoding")));
             this.DiffText.Font = new System.Drawing.Font("Tahoma", 9.75F);
             this.DiffText.IgnoreWhitespaceChanges = false;
             this.DiffText.IsReadOnly = true;
