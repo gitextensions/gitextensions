b8bfa37442baf4f57d4ce37a1213d22e15d61d9a
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2012-04-27 04:43:00 +0400

    Mouse wheel redirector added

---
+++ GitUI/MouseWheelRedirector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using GitCommands;
+
+namespace GitUI
+{
+    public sealed class MouseWheelRedirector : IMessageFilter
+    {
+        private static readonly MouseWheelRedirector instance = new MouseWheelRedirector();
+
+        private MouseWheelRedirector()
+        {
+        }
+
+        private bool _active = false;
+        public static bool Active
+        {
+            get { return instance._active; }
+            set
+            {
+                if (instance._active != value && !Settings.FocusControlOnHover)
+                {
+                    instance._active = value;
+                    if (instance._active)
+                        Application.AddMessageFilter(instance);
+                    else
+                        Application.RemoveMessageFilter(instance);
+                }
+            }
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x20a)
+            {
+                // WM_MOUSEWHEEL, find the control at screen position m.LParam
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                IntPtr hWnd = WindowFromPoint(pos);
+                if (hWnd != IntPtr.Zero && hWnd != m.HWnd && Control.FromHandle(hWnd) != null)
+                {
+                    Control control = Control.FromHandle(hWnd);
+                    while (control != null && !(control is GitExtensionsControl))
+                        control = control.Parent;
+                    if (control != null)
+                    {
+                        SendMessage(hWnd, m.Msg, m.WParam, m.LParam);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // P/Invoke declarations
+        [DllImport("user32.dll")]
+        private static extern IntPtr WindowFromPoint(Point pt);
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+    }
+}
