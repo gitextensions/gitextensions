gitextensions/gitextensions

commit c020861fc306bafc313d2ad08bdbea168c3625af

Modeless GitLogForm

GitUI/GitLogForm.Designer.cs
@@ -59,6 +59,7 @@ namespace GitUI
             this.TabControl.SelectedIndex = 0;
             this.TabControl.Size = new System.Drawing.Size(659, 470);
             this.TabControl.TabIndex = 1;
+            this.TabControl.SelectedIndexChanged += new System.EventHandler(this.TabControl_SelectedIndexChanged);
             // 
             // tabPageCommandLog
             // 

GitUI/GitLogForm.cs
@@ -2,13 +2,20 @@
 using System.Windows.Forms;
 using GitCommands;
 using System.Linq;
+using GitCommands.Logging;
+using System.Threading;
 
 namespace GitUI
 {
     public partial class GitLogForm : GitExtensionsForm
     {
+
+        protected readonly SynchronizationContext syncContext;
+
         public GitLogForm()
         {
+            ShowInTaskbar = true;
+            syncContext = SynchronizationContext.Current;
             InitializeComponent();
             Translate();
         }
@@ -22,10 +29,54 @@ namespace GitUI
         {
             RestorePosition("log");
 
-            LogItems.DataSource = Settings.GitLog.Commands();
+            Settings.GitLog.CommandsChanged += (CommandLogger log) =>
+            {
+                RefreshLogItems(log);
+            };
 
-            CommandCacheItems.DataSource = GitCommandCache.CachedCommands().ToList();
+            GitCommandCache.CachedCommandsChanged += () =>
+            {
+                RefreshCommandCacheItems();
+            };
+
+            RefreshLogItems(Settings.GitLog);
+
+            RefreshCommandCacheItems();
+
         }
+
+        protected void RefreshLogItems(CommandLogger log)
+        {
+            SendOrPostCallback method = o =>
+            {
+                if (TabControl.SelectedTab == tabPageCommandLog)
+                {
+                    bool selectLastIndex = LogItems.Items.Count == 0 || LogItems.SelectedIndex == LogItems.Items.Count - 1;
+                    LogItems.DataSource = log.Commands();
+                    if (selectLastIndex && LogItems.Items.Count > 0)
+                        LogItems.SelectedIndex = LogItems.Items.Count - 1;
+                }
+            };
+            syncContext.Post(method, this);
+
+        }
+
+        protected void RefreshCommandCacheItems()
+        {
+            SendOrPostCallback method = o =>
+            {
+                if (TabControl.SelectedTab == tabPageCommandCache)
+                {
+                    bool selectLastIndex = CommandCacheItems.Items.Count == 0 || CommandCacheItems.SelectedIndex == CommandCacheItems.Items.Count - 1;
+                    CommandCacheItems.DataSource = GitCommandCache.CachedCommands();
+                    if (selectLastIndex && CommandCacheItems.Items.Count > 0)
+                        CommandCacheItems.SelectedIndex = CommandCacheItems.Items.Count - 1;
+                }
+
+            };
+            syncContext.Post(method, this);
+        }
+
 
         private void CommandCacheItems_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -48,6 +99,12 @@ namespace GitUI
             string command = LogItems.SelectedItem as string;
 
             LogOutput.Text = command;
+        }
+
+        private void TabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshLogItems(Settings.GitLog);
+            RefreshCommandCacheItems();
         }
     }
 }

GitCommands/Git/GitCommandCache.cs
@@ -14,6 +14,8 @@ namespace GitCommands
         //Cache limit
         const int cacheLimit = 40;
 
+        public delegate void CachedCommandsChangedHandler();
+
         /// <summary>
         /// Simple dictionary to store cmd/output pairs
         /// </summary>
@@ -25,9 +27,15 @@ namespace GitCommands
         /// </summary>
         private static Queue<string> queue = new Queue<string>(cacheLimit);
 
-        public static IEnumerable<string> CachedCommands()
+        private static CachedCommandsChangedHandler _CachedCommandsChanged;
+
+
+        public static string[] CachedCommands()
         {
-            return commandCache.Keys;
+            lock (queue)
+            {
+                return queue.ToArray();
+            }
         }
 
         public static bool TryGet(string cmd, out string cmdOutput)
@@ -38,8 +46,8 @@ namespace GitCommands
                 cmdOutput = null;
                 return false;
             }
-
-            return commandCache.TryGetValue(cmd, out cmdOutput);
+            lock (queue)
+                return commandCache.TryGetValue(cmd, out cmdOutput);
         }
 
         public static void Add(string cmd, string cmdOutput)
@@ -48,12 +56,47 @@ namespace GitCommands
             if (string.IsNullOrEmpty(cmd))
                 return;
 
-            commandCache[cmd] = cmdOutput;
-            queue.Enqueue(cmd);
+            lock (queue)
+            {
+                commandCache[cmd] = cmdOutput;
+                queue.Enqueue(cmd);
 
-            //Limit cache to X commands
-            if (queue.Count >= cacheLimit)
-                commandCache.Remove(queue.Dequeue());
+                //Limit cache to X commands
+                if (queue.Count >= cacheLimit)
+                    commandCache.Remove(queue.Dequeue());
+            }
+            FireCachedCommandsChanged();
+        }
+
+        public static event CachedCommandsChangedHandler CachedCommandsChanged
+        {
+            add
+            {
+                lock (queue)
+                {
+                    _CachedCommandsChanged += value;
+                }
+            }
+            remove
+            {
+                lock (queue)
+                {
+                    _CachedCommandsChanged -= value;
+                }
+            }
+        }
+
+        private static void FireCachedCommandsChanged()
+        {
+            CachedCommandsChangedHandler handler;
+            lock (queue)
+            {
+                handler = _CachedCommandsChanged;
+            }
+            if (handler != null)
+            {
+                handler();
+            }
         }
 
     }

GitCommands/Logging/CommandLogger.cs
@@ -6,33 +6,36 @@ namespace GitCommands.Logging
 {
     public class CommandLogger
     {
+        public delegate void CommandsChangedHandler(CommandLogger sender);
+
         private const int LogLimit = 100;
-        private Queue<string> _logQueue = new Queue<string>(LogLimit);
+        private Queue<string> _logQueue = new Queue<string>(LogLimit);       
+        private CommandsChangedHandler _CommandsChanged;
 
         public string[] Commands()
         {
-            return _logQueue.ToArray();
+            lock (_logQueue)
+            {
+                return _logQueue.ToArray();
+            }
         }
 
         public void Log(string command)
         {
-            try
+            lock (_logQueue)
             {
                 if (_logQueue.Count >= LogLimit)
                     _logQueue.Dequeue();
 
                 _logQueue.Enqueue(command);
             }
-            catch //This should NEVER happen... but it did happen (issue 271)
-            {
-                _logQueue = new Queue<string>(LogLimit);
-            }
+            FireCommandsChanged();
         }
 
         public override string ToString()
         {
             var stringBuilder = new StringBuilder();
-            var logQueueArray = _logQueue.ToArray();
+            var logQueueArray = Commands();
 
             for (int n = logQueueArray.Length; n > 0; n--)
             {
@@ -41,5 +44,37 @@ namespace GitCommands.Logging
 
             return stringBuilder.ToString();
         }
+
+        public event CommandsChangedHandler CommandsChanged
+        {
+            add
+            {
+                lock (_logQueue)
+                {
+                    _CommandsChanged += value;
+                }
+            }
+            remove
+            {
+                lock (_logQueue)
+                {
+                    _CommandsChanged -= value;
+                }
+            }
+        }
+
+        protected void FireCommandsChanged()
+        {
+            CommandsChangedHandler handler;
+            lock (_logQueue)
+            {
+                handler = _CommandsChanged;
+            }
+            if (handler != null)
+            {
+                handler (this);
+            }
+        }
+
     }
 }

GitUI/FormBrowse.cs
@@ -1136,7 +1136,7 @@ namespace GitUI
 
         private void GitcommandLogToolStripMenuItemClick(object sender, EventArgs e)
         {
-            new GitLogForm().ShowDialog(this);
+            new GitLogForm().Show();
         }
 
 

commit 0d050d154d4ae48ef517e84e4367a0a14a500dbb

GitLogForm may lead to memory leak
Form subscribes to static-rooted event and cannot be collected after close.
caused by c020861fc306bafc313d2ad08bdbea168c3625af

GitUI/GitLogForm.Designer.cs
@@ -192,6 +192,7 @@ namespace GitUI
             this.Name = "GitLogForm";
             this.Text = "Log";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GitLogFormFormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.GitLogForm_FormClosed); 
             this.Load += new System.EventHandler(this.GitLogFormLoad);
             this.TabControl.ResumeLayout(false);
             this.tabPageCommandLog.ResumeLayout(false);

GitUI/GitLogForm.cs
@@ -17,21 +17,24 @@ namespace GitUI
             Translate();
         }
 
+        private void GitLogFormLoad(object sender, EventArgs e)
+        {
+            RestorePosition("log");
+
+            SubscribeToEvents();
+            RefreshLogItems();
+
+            RefreshCommandCacheItems();
+        }
+
         private void GitLogFormFormClosing(object sender, FormClosingEventArgs e)
         {
             SavePosition("log");
         }
 
-        private void GitLogFormLoad(object sender, EventArgs e)
+        private void GitLogForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            RestorePosition("log");
-
-            Settings.GitLog.CommandsChanged += () => syncContext.Post(_ => RefreshLogItems(), null);
-            GitCommandCache.CachedCommandsChanged += () => syncContext.Post(_ => RefreshCommandCacheItems(), null);
-
-            RefreshLogItems();
-
-            RefreshCommandCacheItems();
+            UnsubscribeFromEvents();
         }
 
         private void RefreshLogItems()
@@ -85,6 +88,28 @@ namespace GitUI
         {
             TopMost = !TopMost;
             alwaysOnTopCheckBox.Checked = TopMost;
+        }
+
+        private void UnsubscribeFromEvents()
+        {
+            Settings.GitLog.CommandsChanged -= OnCommandsLogChanged;
+            GitCommandCache.CachedCommandsChanged -= OnCachedCommandsLogChanged;
+        }
+
+        private void SubscribeToEvents()
+        {
+            Settings.GitLog.CommandsChanged += OnCommandsLogChanged;
+            GitCommandCache.CachedCommandsChanged += OnCachedCommandsLogChanged;
+        }
+
+        private void OnCommandsLogChanged()
+        {
+            syncContext.Post(_ => RefreshLogItems(), null);
+        }
+
+        private void OnCachedCommandsLogChanged()
+        {
+            syncContext.Post(_ => RefreshCommandCacheItems(), null);
         }
     }
 }