gitextensions/gitextensions

commit d65278ea98d18145bdc55aa7de93cca216d8aa66

Performance: replaced date parsing with numbers parsing
Log formatter can print unix time (number of seconds),
it isn't human readable, but can be parsed much faster

GitCommands/DateTimeUtils.cs
@@ -1,0 +1,30 @@
+﻿using System;
+
+namespace GitCommands
+{
+    public static class DateTimeUtils
+    {
+        /// <summary>
+        /// Midnight 1 January 1970.
+        /// </summary>
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+
+        public static bool TryParseUnixTime(string unixTime, out DateTime result)
+        {
+            long seconds;
+            if (long.TryParse(unixTime, out seconds))
+            {
+                result = UnixEpoch.AddSeconds(seconds);
+                return true;
+            }
+
+            result = default(DateTime);
+            return false;
+        }
+
+        public static DateTime ParseUnixTime(string unixTime)
+        {
+            return UnixEpoch.AddSeconds(long.Parse(unixTime));
+        }
+    }
+}

GitCommands/GitCommands.csproj
@@ -66,6 +66,7 @@
   </ItemGroup>
   <ItemGroup>
     <Compile Include="CommitData.cs" />
+    <Compile Include="DateTimeUtils.cs" />
     <Compile Include="FileHelper.cs" />
     <Compile Include="Git\GitCommand.cs" />
     <Compile Include="Git\EncodingHelper.cs" />

GitCommands/RevisionGraph.cs
@@ -131,9 +131,9 @@ namespace GitCommands
                         /* Tree                    */ "%T%n" +
                         /* Author Name             */ "%aN%n" +
                         /* Author Email            */ "%aE%n" +
-                        /* Author Date             */ "%ai%n" +
+                        /* Author Date             */ "%at%n" +
                         /* Committer Name          */ "%cN%n" +
-                        /* Committer Date          */ "%ci%n" +
+                        /* Committer Date          */ "%ct%n" +
                         /* Commit message encoding */ "%e%n" + //there is a bug: git does not recode commit message when format is given
                         /* Commit Message          */ "%s";
                 }
@@ -305,8 +305,8 @@ namespace GitCommands
                 case ReadStep.AuthorDate:
                     {
                         DateTime dateTime;
-                        DateTime.TryParse(line, out dateTime);
-                        revision.AuthorDate = dateTime;
+                        if (DateTimeUtils.TryParseUnixTime(line, out dateTime))
+                            revision.AuthorDate = dateTime;
                     }
                     break;
 
@@ -317,8 +317,8 @@ namespace GitCommands
                 case ReadStep.CommitterDate:
                     {
                         DateTime dateTime;
-                        DateTime.TryParse(line, out dateTime);
-                        revision.CommitDate = dateTime;
+                        if (DateTimeUtils.TryParseUnixTime(line, out dateTime))
+                            revision.CommitDate = dateTime;
                     }
                     break;
 

GitUI/FormVerify.LostObject.cs
@@ -32,7 +32,6 @@ namespace GitUI
             private static readonly Regex RawDataRegex = new Regex(RawDataPattern, RegexOptions.Compiled);
             private static readonly Regex LogRegex = new Regex(LogPattern, RegexOptions.Compiled | RegexOptions.Singleline);
 
-            private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
             private readonly LostObjectType objectType;
             private readonly string rawType;
             private readonly string hash;
@@ -95,7 +94,7 @@ namespace GitUI
                         result.Author = GitCommandHelpers.ReEncodeStringFromLossless(logPatternMatch.Groups[1].Value);
                         string encodingName = logPatternMatch.Groups[2].Value;
                         result.Subject = GitCommandHelpers.ReEncodeCommitMessage(logPatternMatch.Groups[3].Value, encodingName);
-                        result.Date = UnixEpoch.AddSeconds(long.Parse(logPatternMatch.Groups[4].Value));
+                        result.Date = DateTimeUtils.ParseUnixTime(logPatternMatch.Groups[4].Value);
                     }
                 }
 

commit 2695b2baffe6f9f016ec28157462b46e4be9e753

Parse commit dates with local timezone offset
This commit fixes issue that was introduced in d65278ea98d18145bdc55aa7de93cca216d8aa66

GitCommands/DateTimeUtils.cs
@@ -7,14 +7,14 @@ namespace GitCommands
         /// <summary>
         /// Midnight 1 January 1970.
         /// </summary>
-        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public static bool TryParseUnixTime(string unixTime, out DateTime result)
         {
             long seconds;
             if (long.TryParse(unixTime, out seconds))
             {
-                result = UnixEpoch.AddSeconds(seconds);
+                result = UnixEpoch.AddSeconds(seconds).ToLocalTime();
                 return true;
             }
 
@@ -24,7 +24,7 @@ namespace GitCommands
 
         public static DateTime ParseUnixTime(string unixTime)
         {
-            return UnixEpoch.AddSeconds(long.Parse(unixTime));
+            return UnixEpoch.AddSeconds(long.Parse(unixTime)).ToLocalTime();
         }
     }
 }