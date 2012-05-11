47de16cd9aab10918ca3621db45137d230293f88
mabako <mabako@gmail.com>
2012-05-12 01:50:23 +0200

    load pull requests
    Doesn't load discussions, though.

--- Plugins/Github3/GithubHostedRemote.cs
+++ Plugins/Github3/GithubHostedRemote.cs
@@ -3,32 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GitUIPluginInterfaces.RepositoryHosts;
+using Git.hub;
 
 namespace Github3
 {
     class GithubHostedRemote : IHostedRemote
     {
+        private GithubRepo repo;
+        public GithubHostedRemote(string Name, string Owner, string RemoteRepositoryName)
+        {
+            this.Name = Name;
+            this.Owner = Owner;
+            this.RemoteRepositoryName = RemoteRepositoryName;
+        }
+
         public IHostedRepository GetHostedRepository()
         {
-            return new GithubRepo(Github3.github.getRepository(Owner, Name));
+            if(repo == null)
+                repo = new GithubRepo(Github3.github.getRepository(Owner, RemoteRepositoryName));
+
+            return repo;
         }
 
         /// <summary>
         /// Local name of the remote, 'origin'
         /// </summary>
-        public string Name { get; internal set; }
+        public string Name { get; private set; }
 
         /// <summary>
         /// Owner of the remote repository, in
         /// git@github.com:mabako/Git.hub.git this is 'mabako'
         /// </summary>
-        public string Owner { get; internal set; }
+        public string Owner { get; private set; }
 
         /// <summary>
         /// Name of the remote repository, in
         /// git@github.com:mabako/Git.hub.git this is 'Git.hub'
         /// </summary>
-        public string RemoteRepositoryName { get; internal set; }
+        public string RemoteRepositoryName { get; private set; }
 
         public string Data { get { return Owner + "/" + RemoteRepositoryName; } }
         public string DisplayData { get { return Data; } }
