19b7dd8aac1af5f36496e57bc23f32664bd41dc6
mabako <mabako@gmail.com>
2012-05-12 14:36:15 +0200

    pull request discussions featuring only commits

---
+++ Plugins/Github3/GithubPullRequestDiscussion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GitUIPluginInterfaces.RepositoryHosts;
+using Git.hub;
+
+namespace Github3
+{
+    class GithubPullRequestDiscussion : IPullRequestDiscussion
+    {
+        private PullRequest pullrequest;
+
+        public GithubPullRequestDiscussion(PullRequest pullrequest)
+        {
+            this.pullrequest = pullrequest;
+            Entries = new List<IDiscussionEntry>();
+            ForceReload();
+        }
+
+        public List<IDiscussionEntry> Entries { get; private set; }
+
+        public void Post(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ForceReload()
+        {
+            Entries.Clear();
+
+            foreach (var commit in pullrequest.GetCommits())
+            {
+                Entries.Add(new GithubDiscussionCommit { Sha = commit.Sha, Author = commit.Author.Login, Created = commit.Commit.Author.Date, Body = commit.Commit.Message });
+            }
+        }
+    }
+
+    class GithubDiscussionCommit : ICommitDiscussionEntry
+    {
+        public string Sha { get; internal set; }
+        public string Author { get; internal set; }
+        public DateTime Created { get; internal set; }
+        public string Body { get; internal set; }
+    }
+}
