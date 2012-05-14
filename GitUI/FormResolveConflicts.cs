gitextensions/gitextensions

commit 4570c771f591432eb6c879f1824cf1c2963e3336

Improved settings, improved mergeconflictsolver

GitUI/FormResolveConflicts.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using GitCommands;
 
 namespace GitUI
 {
@@ -31,11 +32,130 @@ namespace GitUI
         private void Initialize()
         {
             ConflictedFiles.DataSource = GitCommands.GitCommands.GetConflictedFiles();
+            InitMergetool();
         }
 
         private void Rescan_Click(object sender, EventArgs e)
         {
             Initialize();
+        }
+
+        private string mergetool;
+        private string mergetoolCmd;
+        private string mergetoolPath;
+
+        private void ConflictedFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (ConflictedFiles.SelectedRows.Count != 1)
+                return;
+
+            DataGridViewRow row = ConflictedFiles.SelectedRows[0];
+
+            string filename = GitCommands.GitCommands.GetConflictedFiles(((GitItem)row.DataBoundItem).FileName);
+
+            bool file1 = File.Exists(GitCommands.Settings.WorkingDir + filename + ".BASE");
+            bool file2 = File.Exists(GitCommands.Settings.WorkingDir + filename + ".LOCAL");
+            bool file3 = File.Exists(GitCommands.Settings.WorkingDir + filename + ".REMOTE");
+
+            string arguments = mergetoolCmd;
+
+            if (file1 && file2 && file3)
+            {
+                arguments = arguments.Replace("$BASE", filename + ".BASE");
+                arguments = arguments.Replace("$LOCAL", filename + ".LOCAL");
+                arguments = arguments.Replace("$REMOTE", filename + ".REMOTE");
+                arguments = arguments.Replace("$MERGED", filename + "");
+
+                GitCommands.GitCommands.RunCmd(mergetoolPath, "" + arguments + "");
+
+                if (MessageBox.Show("Is the mergeconflict solved?", "Merge", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    GitCommands.GitCommands.RunCmd(Settings.GitDir + "git.cmd", "add -- \"" + filename + "\"");
+                    Initialize();
+
+                }
+            }
+            else
+            {
+                FormModifiedDeletedCreated frm = new FormModifiedDeletedCreated();
+                if (file1 && file2 && !file3)
+                {
+                    frm.Label.Text = "Use modified or deleted file?";
+                    frm.Created.Text = "Modified";
+                }
+                else
+                    if (file1 && !file2 && file3)
+                    {
+                        frm.Label.Text = "Use created or deleted file?";
+                    }
+                    else
+                    {
+                        File.Delete(Settings.WorkingDir + filename + ".BASE");
+                        File.Delete(Settings.WorkingDir + filename + ".LOCAL");
+                        File.Delete(Settings.WorkingDir + filename + ".REMOTE");
+
+                        Directory.SetCurrentDirectory(GitCommands.Settings.WorkingDir);
+                        GitCommands.GitCommands.RunRealCmd(GitCommands.Settings.GitDir + "git.cmd", "mergetool \"" + filename + "\"");
+                        Initialize();
+                        return;
+                    }
+
+                frm.ShowDialog();
+
+                if (frm.Aborted)
+                {
+                    File.Delete(Settings.WorkingDir + filename + ".BASE");
+                    File.Delete(Settings.WorkingDir + filename + ".LOCAL");
+                    File.Delete(Settings.WorkingDir + filename + ".REMOTE");
+                    return;
+                }
+                else
+                    if (frm.Delete)
+                        GitCommands.GitCommands.RunCmd(Settings.GitDir + "git.cmd", "rm -- \"" + filename + "\"");
+                    else
+                        if (!frm.Delete)
+                            GitCommands.GitCommands.RunCmd(Settings.GitDir + "git.cmd", "add -- \"" + filename + "\"");
+
+                Initialize();
+            }
+
+            File.Delete(Settings.WorkingDir + filename + ".BASE");
+            File.Delete(Settings.WorkingDir + filename + ".LOCAL");
+            File.Delete(Settings.WorkingDir + filename + ".REMOTE");
+        }
+
+        private void InitMergetool()
+        {
+            GitCommands.GitCommands gitCommands = new GitCommands.GitCommands();
+
+            mergetool = GitCommands.GitCommands.GetSetting("merge.tool");
+            if (string.IsNullOrEmpty(mergetool))
+                mergetool = gitCommands.GetGlobalSetting("merge.tool");
+
+            if (string.IsNullOrEmpty(mergetool))
+            {
+                MessageBox.Show("There is no mergetool configured. Please go to settings and set a mergetool!");
+                return;
+            }
+
+            mergetoolCmd = GitCommands.GitCommands.GetSetting("mergetool." + mergetool + ".cmd");
+            if (string.IsNullOrEmpty(mergetoolCmd))
+                mergetoolCmd = gitCommands.GetGlobalSetting("mergetool." + mergetool + ".cmd");
+
+            mergetoolPath = GitCommands.GitCommands.GetSetting("mergetool." + mergetool + ".path");
+            if (string.IsNullOrEmpty(mergetoolPath))
+                mergetoolPath = gitCommands.GetGlobalSetting("mergetool." + mergetool + ".path");
+
+            if (string.IsNullOrEmpty(mergetool) || mergetool == "kdiff3")
+                mergetoolCmd = mergetoolPath + " \"$BASE\" \"$LOCAL\" \"$REMOTE\" -o \"$MERGED\"";
+
+            mergetoolPath = mergetoolCmd.Substring(0, mergetoolCmd.IndexOf(".exe") + 5).Trim(new char[]{'\"', ' '});
+            mergetoolCmd = mergetoolCmd.Substring(mergetoolCmd.IndexOf(".exe") + 5);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConflictedFiles_DoubleClick(sender, e);
         }
     }
 }

GitCommands/GitCommands/Git/GitCommands.cs
@@ -369,7 +369,37 @@ namespace GitCommands
             }
 
             return unmergedFiles;
-        } 
+        }
+
+        static public string GetConflictedFiles(string filename)
+        {
+            filename = FixPath(filename);
+            string[] unmerged = RunCmd(Settings.GitDir + "git.cmd", "ls-files --unmerged --exclude-standard \"" + filename + "\"").Split('\n');
+
+            foreach (string file in unmerged)
+            {
+                string[] fileline = file.Split(new char[] { ' ', '\t' });
+                if (fileline.Length < 3)
+                    continue;
+                if (fileline[2].Trim() == "1")
+                {
+                    string newFileName = filename + ".BASE";
+                    RunCmd(Settings.GitDir + "git.cmd", "cat-file blob " + fileline[1] + " > " + newFileName + "");
+                }
+                if (fileline[2].Trim() == "2")
+                {
+                    string newFileName = filename + ".LOCAL";
+                    RunCmd(Settings.GitDir + "git.cmd", "cat-file blob " + fileline[1] + " > " + newFileName + "");
+                }
+                if (fileline[2].Trim() == "3")
+                {
+                    string newFileName = filename + ".REMOTE";
+                    RunCmd(Settings.GitDir + "git.cmd", "cat-file blob " + fileline[1] + " > " + newFileName + "");
+                }
+            }
+
+            return filename;
+        }
 
         static public string GetMergeMessage()
         {
@@ -1025,9 +1055,13 @@ namespace GitCommands
             {
                 value = value.Replace("\"", "$QUOTE$");
                 value = FixPath(value);
-                value = value.Replace("$QUOTE$", "\"");
+                value = value.Replace("$QUOTE$", "\\\"");
 
-                GitCommands.RunCmd(Settings.GitDir + "git.cmd", "config --replace-all --global \"" + setting + "\" \"" + value.Trim() + "\"").Trim();
+                GitCommands.RunCmd(Settings.GitDir + "git.cmd", "config --global --replace-all \"" + setting + "\" \"" + value.Trim() + "\"").Trim();
+            }
+            else
+            {
+                GitCommands.RunCmd(Settings.GitDir + "git.cmd", "config --global --unset-all \"" + setting + "\"").Trim();
             }
         }
 
@@ -1059,6 +1093,10 @@ namespace GitCommands
                 value = value.Replace("$QUOTE$", "\"");
 
                 GitCommands.RunCmd(Settings.GitDir + "git.cmd", "config --replace-all --file \"" + configFileName + "\" \"" + setting + "\" \"" + value.Trim() + "\"").Trim();
+            }
+            else
+            {
+                UnSetSetting(setting);
             }
         }
 

GitUI/AboutBox.Designer.cs
@@ -116,7 +116,7 @@
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(271, 17);
             this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "Version 1.30";
+            this.labelVersion.Text = "Version 1.35";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.labelVersion.Click += new System.EventHandler(this.labelVersion_Click);
             // 

GitUI/FormDiff.Designer.cs
@@ -65,6 +65,7 @@
             this.RevisionGrid.currentCheckout = "\nfatal: Not a git repository\n";
             this.RevisionGrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.RevisionGrid.HeadFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.RevisionGrid.LastRow = 0;
             this.RevisionGrid.Location = new System.Drawing.Point(0, 0);
             this.RevisionGrid.Name = "RevisionGrid";
             this.RevisionGrid.NormalFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));

GitUI/FormModifiedDeletedCreated.Designer.cs
@@ -1,0 +1,102 @@
+﻿namespace GitUI
+{
+    partial class FormModifiedDeletedCreated
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormModifiedDeletedCreated));
+            this.Label = new System.Windows.Forms.Label();
+            this.Created = new System.Windows.Forms.Button();
+            this.Deleted = new System.Windows.Forms.Button();
+            this.Abort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(12, 22);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(137, 13);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "Use created or deleted file?";
+            // 
+            // Created
+            // 
+            this.Created.Location = new System.Drawing.Point(13, 56);
+            this.Created.Name = "Created";
+            this.Created.Size = new System.Drawing.Size(75, 23);
+            this.Created.TabIndex = 1;
+            this.Created.Text = "Created";
+            this.Created.UseVisualStyleBackColor = true;
+            this.Created.Click += new System.EventHandler(this.Created_Click);
+            // 
+            // Deleted
+            // 
+            this.Deleted.Location = new System.Drawing.Point(119, 56);
+            this.Deleted.Name = "Deleted";
+            this.Deleted.Size = new System.Drawing.Size(75, 23);
+            this.Deleted.TabIndex = 2;
+            this.Deleted.Text = "Deleted";
+            this.Deleted.UseVisualStyleBackColor = true;
+            this.Deleted.Click += new System.EventHandler(this.Deleted_Click);
+            // 
+            // Abort
+            // 
+            this.Abort.Location = new System.Drawing.Point(226, 56);
+            this.Abort.Name = "Abort";
+            this.Abort.Size = new System.Drawing.Size(75, 23);
+            this.Abort.TabIndex = 3;
+            this.Abort.Text = "Abort";
+            this.Abort.UseVisualStyleBackColor = true;
+            this.Abort.Click += new System.EventHandler(this.Abort_Click);
+            // 
+            // FormModifiedDeletedCreated
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 95);
+            this.Controls.Add(this.Abort);
+            this.Controls.Add(this.Deleted);
+            this.Controls.Add(this.Created);
+            this.Controls.Add(this.Label);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormModifiedDeletedCreated";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Solve mergeconflict";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button Deleted;
+        private System.Windows.Forms.Button Abort;
+        public System.Windows.Forms.Button Created;
+    }
+}

GitUI/FormModifiedDeletedCreated.cs
@@ -1,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI
+{
+    public partial class FormModifiedDeletedCreated : Form
+    {
+        public FormModifiedDeletedCreated()
+        {
+            InitializeComponent();
+            Aborted = true;
+        }
+
+        public bool Aborted { get; set; }
+        public bool Delete { get; set; }
+
+        private void Created_Click(object sender, EventArgs e)
+        {
+            Aborted = false;
+            Delete = false;
+            Close();
+        }
+
+        private void Deleted_Click(object sender, EventArgs e)
+        {
+            Aborted = false;
+            Delete = true;
+            Close();
+        }
+
+        private void Abort_Click(object sender, EventArgs e)
+        {
+            Aborted = true;
+            Close();
+        }
+    }
+}

GitUI/FormModifiedDeletedCreated.resx
@@ -117,15 +117,6 @@
   <resheader name="writer">
     <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </resheader>
-  <metadata name="Namex.UserAddedColumn" type="System.Boolean, mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089">
-    <value>True</value>
-  </metadata>
-  <metadata name="gitItemBindingSource.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
-    <value>17, 17</value>
-  </metadata>
-  <metadata name="colorDialog1.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
-    <value>186, 17</value>
-  </metadata>
   <assembly alias="System.Drawing" name="System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" />
   <data name="$this.Icon" type="System.Drawing.Icon, System.Drawing" mimetype="application/x-microsoft.net.object.bytearray.base64">
     <value>

GitUI/FormResolveConflicts.Designer.cs
@@ -34,9 +34,11 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.label1 = new System.Windows.Forms.Label();
-            this.Mergetool = new System.Windows.Forms.Button();
             this.Rescan = new System.Windows.Forms.Button();
+            this.Mergetool = new System.Windows.Forms.Button();
             this.gitItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.Namex = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.ConflictedFiles)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -60,11 +62,14 @@
             this.ConflictedFiles.DataSource = this.gitItemBindingSource;
             this.ConflictedFiles.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ConflictedFiles.Location = new System.Drawing.Point(0, 0);
+            this.ConflictedFiles.MultiSelect = false;
             this.ConflictedFiles.Name = "ConflictedFiles";
             this.ConflictedFiles.ReadOnly = true;
             this.ConflictedFiles.RowHeadersVisible = false;
+            this.ConflictedFiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.ConflictedFiles.Size = new System.Drawing.Size(441, 226);
             this.ConflictedFiles.TabIndex = 0;
+            this.ConflictedFiles.DoubleClick += new System.EventHandler(this.ConflictedFiles_DoubleClick);
             // 
             // splitContainer1
             // 
@@ -79,6 +84,7 @@
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
             this.splitContainer1.Panel2.Controls.Add(this.Rescan);
             this.splitContainer1.Panel2.Controls.Add(this.Mergetool);
             this.splitContainer1.Size = new System.Drawing.Size(576, 255);
@@ -113,6 +119,16 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Unresolved merge conflicts";
             // 
+            // Rescan
+            // 
+            this.Rescan.Location = new System.Drawing.Point(3, 75);
+            this.Rescan.Name = "Rescan";
+            this.Rescan.Size = new System.Drawing.Size(125, 23);
+            this.Rescan.TabIndex = 1;
+            this.Rescan.Text = "Rescan mergeconflicts";
+            this.Rescan.UseVisualStyleBackColor = true;
+            this.Rescan.Click += new System.EventHandler(this.Rescan_Click);
+            // 
             // Mergetool
             // 
             this.Mergetool.Location = new System.Drawing.Point(3, 4);
@@ -123,19 +139,19 @@
             this.Mergetool.UseVisualStyleBackColor = true;
             this.Mergetool.Click += new System.EventHandler(this.Mergetool_Click);
             // 
-            // Rescan
-            // 
-            this.Rescan.Location = new System.Drawing.Point(3, 29);
-            this.Rescan.Name = "Rescan";
-            this.Rescan.Size = new System.Drawing.Size(125, 23);
-            this.Rescan.TabIndex = 1;
-            this.Rescan.Text = "Rescan mergeconflicts";
-            this.Rescan.UseVisualStyleBackColor = true;
-            this.Rescan.Click += new System.EventHandler(this.Rescan_Click);
-            // 
             // gitItemBindingSource
             // 
             this.gitItemBindingSource.DataSource = typeof(GitCommands.GitItem);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Solve selected conflict";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Namex
             // 
@@ -177,6 +193,8 @@
         private System.Windows.Forms.Button Mergetool;
         private System.Windows.Forms.Button Rescan;
         private System.Windows.Forms.BindingSource gitItemBindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Namex;
     }
 }

commit cd8954208c76bd0c9b3ac48ce3562db5a7268a26

fixed #922: Error during conflict resolve
This issue caused by 4570c771f591432eb6c879f1824cf1c2963e3336

GitUI/FormResolveConflicts.cs
@@ -60,14 +60,14 @@ namespace GitUI
         private readonly TranslationString _binaryFileWarningCaption = new TranslationString("Warning");
 
         private readonly TranslationString _noBaseFileMergeCaption = new TranslationString("Merge");
-        
+
         private readonly TranslationString _chooseBaseFileFailedText = new TranslationString("Choose base file failed.");
         private readonly TranslationString _chooseLocalFileFailedText = new TranslationString("Choose local file failed.");
         private readonly TranslationString _chooseRemoteFileFailedText = new TranslationString("Choose remote file failed.");
 
-        private readonly TranslationString _currentFormatFilter = 
+        private readonly TranslationString _currentFormatFilter =
             new TranslationString("Current format (*.{0})");
-        private readonly TranslationString _allFilesFilter = 
+        private readonly TranslationString _allFilesFilter =
             new TranslationString("All files (*.*)");
 
 
@@ -191,7 +191,7 @@ namespace GitUI
                 if (!(extensionsSeperator > 0) || extensionsSeperator + 1 >= fileName.Length)
                     return false;
 
-                string dir = Path.GetDirectoryName(Application.ExecutablePath) + 
+                string dir = Path.GetDirectoryName(Application.ExecutablePath) +
                     Settings.PathSeparator + "Diff-Scripts" + Settings.PathSeparator;
                 if (Directory.Exists(dir))
                 {
@@ -202,7 +202,7 @@ namespace GitUI
                     {
                         string mergeScript = mergeScripts[0];
                         if (MessageBox.Show(this, string.Format(uskUseCustomMergeScript.Text,
-                            mergeScript.Replace(Settings.PathSeparator + Settings.PathSeparator.ToString(), Settings.PathSeparator.ToString())), 
+                            mergeScript.Replace(Settings.PathSeparator + Settings.PathSeparator.ToString(), Settings.PathSeparator.ToString())),
                             uskUseCustomMergeScriptCaption.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                         {
                             UseMergeWithScript(fileName, mergeScript, baseFileName, remoteFileName, localFileName);
@@ -392,11 +392,12 @@ namespace GitUI
             if (string.IsNullOrEmpty(mergetool) || mergetool == "kdiff3")
                 mergetoolCmd = mergetoolPath + " \"$BASE\" \"$LOCAL\" \"$REMOTE\" -o \"$MERGED\"";
 
-            int idx = mergetoolCmd.IndexOf(".exe");
+            const string executablePattern = ".exe";
+            int idx = mergetoolCmd.IndexOf(executablePattern);
             if (idx >= 0)
             {
-                mergetoolPath = mergetoolCmd.Substring(0, idx + 5).Trim(new[] { '\"', ' ' });
-                mergetoolCmd = mergetoolCmd.Substring(idx + 5);
+                mergetoolPath = mergetoolCmd.Substring(0, idx + executablePattern.Length).Trim(new[] { '\"', ' ' });
+                mergetoolCmd = mergetoolCmd.Substring(idx + executablePattern.Length);
             }
             Cursor.Current = Cursors.Default;
         }
@@ -686,7 +687,7 @@ namespace GitUI
             fileDialog.DefaultExt = GitCommandHelpers.GetFileExtension(fileDialog.FileName);
             fileDialog.Filter = string.Format(_currentFormatFilter.Text, GitCommandHelpers.GetFileExtension(fileDialog.FileName)) + "|*." +
                                 GitCommandHelpers.GetFileExtension(fileDialog.FileName) + "|" + _allFilesFilter.Text + "|*.*";
-            
+
             if (fileDialog.ShowDialog(this) == DialogResult.OK)
             {
                 Settings.Module.HandleConflictsSaveSide(GetFileName(), fileDialog.FileName, side);