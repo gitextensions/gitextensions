527ed53e92b87b90ccfa0a1d00ff9911a04d01f0
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2012-05-18 15:59:30 +0400

    Problem fixed with VS addin

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,11 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2012 Henk Westhuis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
