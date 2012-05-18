72b3bd0c0015474068136c22de22a0df1f71003a
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2012-05-18 16:05:16 +0400

    Problem fixed with VS addin

---
+++ Plugins/FindLargeFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
