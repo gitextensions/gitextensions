8abc2334168c27d09a27f6cb2788deb8a4abe5c8
Janusz Białobrzewski <janusz@biu.pl>
2012-05-18 21:49:27 +0200

    GitCommandsTestsMono

--- GitCommandsTests/ConfigFileTest.cs
+++ GitCommandsTests/ConfigFileTest.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections.Generic;
+﻿#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+#else
+using NUnit.Framework;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestContext = System.Object;
+using TestProperty = NUnit.Framework.PropertyAttribute;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+#endif
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GitCommands.Config;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using GitCommands;
 
