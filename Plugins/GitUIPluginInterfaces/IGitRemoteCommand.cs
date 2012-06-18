3ca5d3e73ba04b715784290e60deafa338cb72b9
Pieter van Ginkel <pvginkel@gmail.com>
2012-06-18 12:19:43 +0200

    Extended the plugin interface.
    * FormBrowse notifies plugins of a refresh;
    * Plugins can force a refresh;
    * FormRemoteProcess has been encapsulated in an IGitRemoteCommand so plugins can execute Git commands with UI feedback.

---
+++ Plugins/GitUIPluginInterfaces/IGitRemoteCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitUIPluginInterfaces
+{
+    public interface IGitRemoteCommand
+    {
+        object OwnerForm { get; set; }
+        string Remote { get; set; }
+        string Title { get; set; }
+        string CommandText { get; set; }
+        bool ErrorOccurred { get; }
+        string CommandOutput { get; }
+
+        event GitRemoteCommandCompletedEventHandler Completed;
+
+        void Execute();
+    }
+
+    public class GitRemoteCommandCompletedEventArgs : EventArgs
+    {
+        public IGitRemoteCommand Command { get; private set; }
+
+        public bool IsError { get; set; }
+
+        public bool Handled { get; set; }
+
+        public GitRemoteCommandCompletedEventArgs(IGitRemoteCommand command, bool isError, bool handled)
+        {
+            Command = command;
+            IsError = isError;
+            Handled = handled;
+        }
+    }
+
+    public delegate void GitRemoteCommandCompletedEventHandler(object sender, GitRemoteCommandCompletedEventArgs e);
+}
