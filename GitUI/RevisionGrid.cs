gitextensions/gitextensions

commit 2dcd584a13eb3957f01ade1d5484a7969b899d09

Added bisect skip option to revisions grid context menu

GitUI/RevisionGrid.cs
@@ -2023,19 +2023,25 @@ namespace GitUI
 
         private void markRevisionAsBadToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (Revisions.RowCount <= LastRow || LastRow < 0)
-                return;
-
-            FormProcess.ShowDialog(this, GitCommandHelpers.ContinueBisectCmd(GitBisectOption.Bad, GetRevision(LastRow).Guid), false);
-            RefreshRevisions();
+            ContinueBisect(GitBisectOption.Bad);
         }
 
         private void markRevisionAsGoodToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            ContinueBisect(GitBisectOption.Good);
+        }
+
+        private void bisectSkipRevisionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ContinueBisect(GitBisectOption.Skip);
+        }
+
+        private void ContinueBisect(GitBisectOption bisectOption)
+        {
             if (Revisions.RowCount <= LastRow || LastRow < 0)
                 return;
 
-            FormProcess.ShowDialog(this, GitCommandHelpers.ContinueBisectCmd(GitBisectOption.Good, GetRevision(LastRow).Guid), false);
+            FormProcess.ShowDialog(this, GitCommandHelpers.ContinueBisectCmd(bisectOption, GetRevision(LastRow).Guid), false);
             RefreshRevisions();
         }
 
@@ -2351,7 +2357,6 @@ namespace GitUI
                     item.DropDown.Items[0].PerformClick();
             }
         }
-
     }
 
     public class FilterBranchHelper

GitUI/RevisionGrid.Designer.cs
@@ -112,6 +112,7 @@ namespace GitUI
             this.showAuthorDateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.showRelativeDateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.showGitNotesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bisectSkipRevisionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.Revisions)).BeginInit();
             this.CreateTag.SuspendLayout();
             this.NoCommits.SuspendLayout();
@@ -210,6 +211,7 @@ namespace GitUI
             this.CreateTag.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.markRevisionAsBadToolStripMenuItem,
             this.markRevisionAsGoodToolStripMenuItem,
+            this.bisectSkipRevisionToolStripMenuItem,
             this.stopBisectToolStripMenuItem,
             this.bisectSeparator,
             this.copyToClipboardToolStripMenuItem,
@@ -239,7 +241,7 @@ namespace GitUI
             this.toolStripSeparator7,
             this.runScriptToolStripMenuItem});
             this.CreateTag.Name = "CreateTag";
-            this.CreateTag.Size = new System.Drawing.Size(226, 568);
+            this.CreateTag.Size = new System.Drawing.Size(224, 612);
             this.CreateTag.Opening += new System.ComponentModel.CancelEventHandler(this.CreateTagOpening);
             // 
             // markRevisionAsBadToolStripMenuItem
@@ -709,6 +711,13 @@ namespace GitUI
             this.showRevisionGraphToolStripMenuItem.Click += new System.EventHandler(this.ShowRevisionGraphToolStripMenuItemClick);
             this.renameBranchToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
             this.renameBranchToolStripMenuItem.Text = "Rename branch";
+            //
+            // bisectSkipRevisionToolStripMenuItem
+            //
+            this.bisectSkipRevisionToolStripMenuItem.Name = "bisectSkipRevisionToolStripMenuItem";
+            this.bisectSkipRevisionToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.bisectSkipRevisionToolStripMenuItem.Text = "Skip revision";
+            this.bisectSkipRevisionToolStripMenuItem.Click += new System.EventHandler(this.bisectSkipRevisionToolStripMenuItem_Click);
             // 
             // RevisionGrid
             // 
@@ -802,5 +811,6 @@ namespace GitUI
         private System.Windows.Forms.ToolStripMenuItem squashCommitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem renameBranchToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hashToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bisectSkipRevisionToolStripMenuItem;
     }
 }

commit 4295cb928d6553e7d1477eb6edfb3eb6a7829a29

Hide bisect skip context menu item when repo isn't in the middle of bisect
This issue is caused by 2dcd584a13eb3957f01ade1d5484a7969b899d09

GitUI/RevisionGrid.cs
@@ -1543,6 +1543,7 @@ namespace GitUI
             var inTheMiddleOfBisect = Settings.Module.InTheMiddleOfBisect();
             markRevisionAsBadToolStripMenuItem.Visible = inTheMiddleOfBisect;
             markRevisionAsGoodToolStripMenuItem.Visible = inTheMiddleOfBisect;
+            bisectSkipRevisionToolStripMenuItem.Visible = inTheMiddleOfBisect;
             stopBisectToolStripMenuItem.Visible = inTheMiddleOfBisect;
             bisectSeparator.Visible = inTheMiddleOfBisect;
 