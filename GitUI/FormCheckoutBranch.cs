dc6dcbc3fc9ed66fac5de22577bf14cec8af9e17
Janusz Białobrzewski <jbialobr@o2.pl>
2012-08-25 18:21:10 +0200

    Unified FormCheckout local and remote branch

--- GitUI/FormCheckoutBranch.cs
+++ GitUI/FormCheckoutBranch.cs
@@ -2,20 +2,45 @@
 using System.Diagnostics;
 using System.Linq;
 using GitCommands;
+using GitCommands.Git;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using ResourceManager.Translation;
 
 namespace GitUI
 {
     public partial class FormCheckoutBranch : GitExtensionsForm
     {
+
+        #region Translation
+        private readonly TranslationString _applyShashedItemsAgain =
+            new TranslationString("Apply stashed items to working dir again?");
+
+        private readonly TranslationString _applyShashedItemsAgainCaption =
+            new TranslationString("Auto stash");
+
+        private readonly TranslationString _customBranchNameIsEmpty =
+            new TranslationString("Custom branch name is empty.\nEnter valid branch name or select predefined value.");
+        private readonly TranslationString _customBranchNameIsNotValid =
+            new TranslationString("“{0}” is not valid branch name.\nEnter valid branch name or select predefined value.");
+        #endregion
+
         private string _containRevison;
         private bool isDirtyDir;
+        private string _branch = "";
+        private string _remoteName = "";
+        private string _newLocalBranchName = "";
+        private string _localBranchName = "";
+        private readonly string rbResetBranchText;
+        private readonly string rbCreateBranchText;
+
 
         internal FormCheckoutBranch()
         {
             InitializeComponent();
             Translate();
+            rbResetBranchText = rbResetBranch.Text;
+            rbCreateBranchText = rbCreateBranch.Text;
         }
 
         public FormCheckoutBranch(string branch, bool remote)
@@ -56,10 +81,11 @@ namespace GitUI
 
         public DialogResult DoDefaultActionOrShow(IWin32Window owner)
         {
-            if (!Branches.Text.IsNullOrWhiteSpace() && (!isDirtyDir || Settings.UseDefaultCheckoutBranchAction))
-                return OkClick();
-            else
+            if (Branches.Text.IsNullOrWhiteSpace() || Remotebranch.Checked
+                || (isDirtyDir && !Settings.UseDefaultCheckoutBranchAction))
                 return ShowDialog(owner);
+            else
+                return OkClick();
         }
 
 
@@ -97,6 +123,8 @@ namespace GitUI
             }
 
             Branches.Text = null;
+            remoteOptionsPanel.Visible = Remotebranch.Checked;
+            rbCreateBranch.Checked = Settings.CreateLocalBranchForRemote;
         }
 
         private Settings.LocalChanges ChangesMode
@@ -130,24 +158,59 @@ namespace GitUI
 
         private DialogResult OkClick()
         {
+
+            GitCheckoutBranchCmd cmd = new GitCheckoutBranchCmd(Branches.Text.Trim(), Remotebranch.Checked);
+
+            if (Remotebranch.Checked)
+            {
+                if (rbCreateBranchWithCustomName.Checked)
+                {
+                    cmd.NewBranchName = txtCustomBranchName.Text.Trim();
+                    cmd.NewBranchAction = GitCheckoutBranchCmd.NewBranch.Create;
+                    if (cmd.NewBranchName.IsNullOrWhiteSpace())
+                    {
+                        MessageBox.Show(_customBranchNameIsEmpty.Text, Text);
+                        DialogResult = DialogResult.None;
+                        return DialogResult.None;
+                    }
+                    if (!Settings.Module.CheckRefFormat(cmd.NewBranchName))
+                    {
+                        MessageBox.Show(string.Format(_customBranchNameIsNotValid.Text, cmd.NewBranchName), Text);
+                        DialogResult = DialogResult.None;
+                        return DialogResult.None;
+                    }
+                }
+                else if (rbCreateBranch.Checked)
+                {
+                    cmd.NewBranchAction = GitCheckoutBranchCmd.NewBranch.Create;
+                    cmd.NewBranchName = _newLocalBranchName;
+                }
+                else if (rbResetBranch.Checked)
+                {
+                    cmd.NewBranchAction = GitCheckoutBranchCmd.NewBranch.Reset;
+                    cmd.NewBranchName = _localBranchName;
+                }
+                else
+                {
+                    cmd.NewBranchAction = GitCheckoutBranchCmd.NewBranch.DontCreate;
+                    cmd.NewBranchName = null;
+                }
+
+            }
+
             Settings.LocalChanges changes = ChangesMode;
             Settings.CheckoutBranchAction = changes;
             Settings.UseDefaultCheckoutBranchAction = defaultActionChx.Checked;
+            cmd.SetLocalChangesFromSettings(changes);
 
             IWin32Window _owner = Visible ? this : Owner;
 
             if (changes == Settings.LocalChanges.Stash && Settings.Module.IsDirtyDir())
                 GitUICommands.Instance.Stash(_owner);
 
-            if (Remotebranch.Checked)
             {
-                using (var checkoutRemote = new FormCheckoutRemoteBranch(Branches.Text, changes))
-                    return checkoutRemote.ShowDialog(_owner);
-            }
-            else
-            {
-                var command = GitCommandHelpers.CheckoutCmd(Branches.Text, changes);
-                var successfullyCheckedOut = FormProcess.ShowDialog(_owner, command);
+                var successfullyCheckedOut = GitUICommands.Instance.StartCommandLineProcessDialog(cmd, _owner);
+
                 if (successfullyCheckedOut)
                     return DialogResult.OK;
                 else
@@ -168,8 +231,49 @@ namespace GitUI
         private void RemoteBranchCheckedChanged(object sender, EventArgs e)
         {
             BranchTypeChanged();
+            Branches_SelectedIndexChanged(sender, e);
         }
 
+        private void rbCreateBranchWithCustomName_CheckedChanged(object sender, EventArgs e)
+        {
+            txtCustomBranchName.Enabled = rbCreateBranchWithCustomName.Checked;
+            if (rbCreateBranchWithCustomName.Checked)
+                txtCustomBranchName.SelectAll();
+        }
+
+        private static bool LocalBranchExists(string name)
+        {
+            return Settings.Module.GetHeads(false).Any(head => head.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private void Branches_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var _branch = Branches.Text;
+
+            if (_branch.IsNullOrWhiteSpace() || !Remotebranch.Checked)
+            {
+                _remoteName = string.Empty;
+                _localBranchName = string.Empty;
+                _newLocalBranchName = string.Empty;
+            }
+            else
+            {
+                _remoteName = GitModule.GetRemoteName(_branch, Settings.Module.GetRemotes(false));
+                _localBranchName = _branch.Substring(_remoteName.Length + 1);
+                _newLocalBranchName = string.Concat(_remoteName, "_", _localBranchName);
+                int i = 2;
+                while (LocalBranchExists(_newLocalBranchName))
+                {
+                    _newLocalBranchName = string.Concat(_remoteName, "_", _localBranchName, "_", i.ToString());
+                    i++;
+                }
+            }
+            bool existsLocalBranch = LocalBranchExists(_localBranchName);
+
+            rbResetBranch.Text = String.Format(existsLocalBranch ? rbResetBranchText : rbCreateBranchText, _localBranchName);
+            rbCreateBranch.Text = String.Format(rbCreateBranchText, _newLocalBranchName);
+            txtCustomBranchName.Text = _localBranchName;
+        }
         
     }
 }
