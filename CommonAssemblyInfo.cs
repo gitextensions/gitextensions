6021967f5491fc049eb6e2d63f08181a72e3d963
Henk Westhuis <henk_westhuis@hotmail.com>
2012-09-09 02:59:11 -0700

    Updated file version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,5 +19,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.40")]
-[assembly: AssemblyFileVersion("2.40")]
+[assembly: AssemblyVersion("2.41")]
+[assembly: AssemblyFileVersion("2.41")]
