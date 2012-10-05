a7d6cd6bfce14101ba89102acf66ea57e778eae8
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2012-10-05 19:09:34 +0400

    GitModuleControl fixed properties serialization for designer
    viewpatch command added to command line

--- GitUI/FormCommandlineHelp.Designer.cs
+++ GitUI/FormCommandlineHelp.Designer.cs
@@ -35,9 +35,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(362, 570);
+            this.label1.Padding = new System.Windows.Forms.Padding(10);
+            this.label1.Size = new System.Drawing.Size(382, 605);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
@@ -45,7 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(417, 605);
+            this.ClientSize = new System.Drawing.Size(384, 605);
             this.Controls.Add(this.label1);
             this.Name = "FormCommandlineHelp";
             this.Text = "Commandline usage";
