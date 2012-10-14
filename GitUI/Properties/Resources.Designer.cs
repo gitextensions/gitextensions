0ef9363667bc64b439baf4bd6b27db69fac904e1
australiensun <australien@sun>
2012-10-14 16:37:30 +0200

    Add icon for Reset Working Dir Changes in main menu

--- GitUI/Properties/Resources.Designer.cs
+++ GitUI/Properties/Resources.Designer.cs
@@ -781,6 +781,13 @@ namespace GitUI.Properties {
             }
         }
         
+        public static System.Drawing.Bitmap IconResetWorkingDirChanges {
+            get {
+                object obj = ResourceManager.GetObject("IconResetWorkingDirChanges", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
