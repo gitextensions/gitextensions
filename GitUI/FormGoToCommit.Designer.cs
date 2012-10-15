7c989560d0a3c0fc202a57485059b79a1a06cc25
australiensun <australien@sun>
2012-10-15 20:25:54 +0200

    FormGoToCommit: add group box for help text

--- GitUI/FormGoToCommit.Designer.cs
+++ GitUI/FormGoToCommit.Designer.cs
@@ -32,12 +32,14 @@
             this.goButton = new System.Windows.Forms.Button();
             this.commitExpression = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
             // goButton
             // 
+            this.goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.goButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.goButton.Location = new System.Drawing.Point(437, 22);
+            this.goButton.Location = new System.Drawing.Point(381, 10);
             this.goButton.Name = "goButton";
             this.goButton.Size = new System.Drawing.Size(75, 28);
             this.goButton.TabIndex = 1;
@@ -47,26 +49,41 @@
             // 
             // commitExpression
             // 
-            this.commitExpression.Location = new System.Drawing.Point(157, 23);
+            this.commitExpression.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.commitExpression.Location = new System.Drawing.Point(130, 13);
             this.commitExpression.Name = "commitExpression";
-            this.commitExpression.Size = new System.Drawing.Size(274, 23);
+            this.commitExpression.Size = new System.Drawing.Size(245, 23);
             this.commitExpression.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Location = new System.Drawing.Point(12, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(112, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Commit expression:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 151);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Help";
             // 
             // FormGoToCommit
             // 
             this.AcceptButton = this.goButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(524, 69);
+            this.ClientSize = new System.Drawing.Size(464, 217);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.commitExpression);
             this.Controls.Add(this.goButton);
@@ -74,6 +91,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 100);
             this.Name = "FormGoToCommit";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -88,5 +106,6 @@
         private System.Windows.Forms.Button goButton;
         private System.Windows.Forms.TextBox commitExpression;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
