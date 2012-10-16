4f067391913c74d31b19c393802ccf23bf47e64e
australiensun <australien@sun>
2012-10-16 16:34:37 +0200

    FormCherryPickCommitSmall: use new user control for showing the commit

--- GitUI/CommitSummaryUserControl.Designer.cs
+++ GitUI/CommitSummaryUserControl.Designer.cs
@@ -28,18 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Date = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.Label();
+            this.Author = new System.Windows.Forms.Label();
+            this.Commit = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.Location = new System.Drawing.Point(4, 51);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(68, 13);
+            this.Date.TabIndex = 13;
+            this.Date.Text = "Commit date:";
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(4, 75);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(53, 13);
+            this.Message.TabIndex = 12;
+            this.Message.Text = "Message:";
+            // 
+            // Author
+            // 
+            this.Author.AutoSize = true;
+            this.Author.Location = new System.Drawing.Point(4, 26);
+            this.Author.Name = "Author";
+            this.Author.Size = new System.Drawing.Size(41, 13);
+            this.Author.TabIndex = 11;
+            this.Author.Text = "Author:";
+            // 
+            // Commit
+            // 
+            this.Commit.AutoSize = true;
+            this.Commit.Location = new System.Drawing.Point(4, 3);
+            this.Commit.Name = "Commit";
+            this.Commit.Size = new System.Drawing.Size(44, 13);
+            this.Commit.TabIndex = 10;
+            this.Commit.Text = "Commit:";
             // 
             // CommitSummaryUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.Author);
+            this.Controls.Add(this.Commit);
             this.Name = "CommitSummaryUserControl";
-            this.Size = new System.Drawing.Size(596, 223);
+            this.Size = new System.Drawing.Size(377, 96);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Date;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.Label Author;
+        private System.Windows.Forms.Label Commit;
     }
 }
