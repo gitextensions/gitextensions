b6dedd8654b895a21b9ba98c5f2c4e5d9eb7467d
australiensun <australien@sun>
2012-10-16 16:11:41 +0200

    Add user control for showing a simple commit summary.

---
+++ GitUI/CommitSummaryUserControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI
+{
+    public partial class CommitSummaryUserControl : UserControl
+    {
+        public CommitSummaryUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
