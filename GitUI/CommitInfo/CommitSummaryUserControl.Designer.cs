53c3116a873cfdb565e20ce0f5115cf352968b59
australiensun <australien@sun>
2012-11-05 20:38:34 +0100

    CommitSummaryUserControl: improve layout

--- GitUI/CommitInfo/CommitSummaryUserControl.Designer.cs
+++ GitUI/CommitInfo/CommitSummaryUserControl.Designer.cs
@@ -34,6 +34,11 @@
             this.labelCommit = new System.Windows.Forms.Label();
             this.labelTags = new System.Windows.Forms.Label();
             this.labelBranches = new System.Windows.Forms.Label();
+            this.labelMessageCaption = new System.Windows.Forms.Label();
+            this.labelTagsCaption = new System.Windows.Forms.Label();
+            this.labelBranchesCaption = new System.Windows.Forms.Label();
+            this.labelAuthorCaption = new System.Windows.Forms.Label();
+            this.labelCommitCaption = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelDate
@@ -50,52 +55,102 @@
             this.labelMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelMessage.Location = new System.Drawing.Point(4, 74);
+            this.labelMessage.Location = new System.Drawing.Point(4, 108);
             this.labelMessage.Name = "labelMessage";
-            this.labelMessage.Size = new System.Drawing.Size(405, 51);
+            this.labelMessage.Size = new System.Drawing.Size(405, 53);
             this.labelMessage.TabIndex = 12;
-            this.labelMessage.Text = "Message:";
+            this.labelMessage.Text = "...";
             // 
             // labelAuthor
             // 
             this.labelAuthor.AutoSize = true;
-            this.labelAuthor.Location = new System.Drawing.Point(4, 26);
+            this.labelAuthor.Location = new System.Drawing.Point(80, 26);
             this.labelAuthor.Name = "labelAuthor";
-            this.labelAuthor.Size = new System.Drawing.Size(41, 13);
+            this.labelAuthor.Size = new System.Drawing.Size(16, 13);
             this.labelAuthor.TabIndex = 11;
-            this.labelAuthor.Text = "Author:";
+            this.labelAuthor.Text = "...";
             // 
             // labelCommit
             // 
             this.labelCommit.AutoSize = true;
-            this.labelCommit.Location = new System.Drawing.Point(4, 3);
+            this.labelCommit.Location = new System.Drawing.Point(80, 3);
             this.labelCommit.Name = "labelCommit";
-            this.labelCommit.Size = new System.Drawing.Size(44, 13);
+            this.labelCommit.Size = new System.Drawing.Size(16, 13);
             this.labelCommit.TabIndex = 10;
-            this.labelCommit.Text = "Commit:";
+            this.labelCommit.Text = "...";
             // 
             // labelTags
             // 
             this.labelTags.AutoSize = true;
-            this.labelTags.Location = new System.Drawing.Point(4, 50);
+            this.labelTags.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.labelTags.Location = new System.Drawing.Point(80, 49);
             this.labelTags.Name = "labelTags";
-            this.labelTags.Size = new System.Drawing.Size(34, 13);
+            this.labelTags.Size = new System.Drawing.Size(16, 13);
             this.labelTags.TabIndex = 14;
-            this.labelTags.Text = "Tags:";
+            this.labelTags.Text = "...";
             // 
             // labelBranches
             // 
             this.labelBranches.AutoSize = true;
-            this.labelBranches.Location = new System.Drawing.Point(213, 50);
+            this.labelBranches.Location = new System.Drawing.Point(80, 72);
             this.labelBranches.Name = "labelBranches";
-            this.labelBranches.Size = new System.Drawing.Size(55, 13);
+            this.labelBranches.Size = new System.Drawing.Size(16, 13);
             this.labelBranches.TabIndex = 15;
-            this.labelBranches.Text = "Branches:";
+            this.labelBranches.Text = "...";
+            // 
+            // labelMessageCaption
+            // 
+            this.labelMessageCaption.AutoSize = true;
+            this.labelMessageCaption.Location = new System.Drawing.Point(4, 95);
+            this.labelMessageCaption.Name = "labelMessageCaption";
+            this.labelMessageCaption.Size = new System.Drawing.Size(53, 13);
+            this.labelMessageCaption.TabIndex = 16;
+            this.labelMessageCaption.Text = "Message:";
+            // 
+            // labelTagsCaption
+            // 
+            this.labelTagsCaption.AutoSize = true;
+            this.labelTagsCaption.Location = new System.Drawing.Point(4, 49);
+            this.labelTagsCaption.Name = "labelTagsCaption";
+            this.labelTagsCaption.Size = new System.Drawing.Size(34, 13);
+            this.labelTagsCaption.TabIndex = 17;
+            this.labelTagsCaption.Text = "Tags:";
+            // 
+            // labelBranchesCaption
+            // 
+            this.labelBranchesCaption.AutoSize = true;
+            this.labelBranchesCaption.Location = new System.Drawing.Point(4, 72);
+            this.labelBranchesCaption.Name = "labelBranchesCaption";
+            this.labelBranchesCaption.Size = new System.Drawing.Size(55, 13);
+            this.labelBranchesCaption.TabIndex = 18;
+            this.labelBranchesCaption.Text = "Branches:";
+            // 
+            // labelAuthorCaption
+            // 
+            this.labelAuthorCaption.AutoSize = true;
+            this.labelAuthorCaption.Location = new System.Drawing.Point(5, 26);
+            this.labelAuthorCaption.Name = "labelAuthorCaption";
+            this.labelAuthorCaption.Size = new System.Drawing.Size(41, 13);
+            this.labelAuthorCaption.TabIndex = 19;
+            this.labelAuthorCaption.Text = "Author:";
+            // 
+            // labelCommitCaption
+            // 
+            this.labelCommitCaption.AutoSize = true;
+            this.labelCommitCaption.Location = new System.Drawing.Point(5, 3);
+            this.labelCommitCaption.Name = "labelCommitCaption";
+            this.labelCommitCaption.Size = new System.Drawing.Size(70, 13);
+            this.labelCommitCaption.TabIndex = 20;
+            this.labelCommitCaption.Text = "Commit hash:";
             // 
             // CommitSummaryUserControl
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.AutoSize = true;
+            this.Controls.Add(this.labelCommitCaption);
+            this.Controls.Add(this.labelAuthorCaption);
+            this.Controls.Add(this.labelBranchesCaption);
+            this.Controls.Add(this.labelTagsCaption);
+            this.Controls.Add(this.labelMessageCaption);
             this.Controls.Add(this.labelBranches);
             this.Controls.Add(this.labelTags);
             this.Controls.Add(this.labelDate);
@@ -103,7 +158,7 @@
             this.Controls.Add(this.labelAuthor);
             this.Controls.Add(this.labelCommit);
             this.Name = "CommitSummaryUserControl";
-            this.Size = new System.Drawing.Size(412, 126);
+            this.Size = new System.Drawing.Size(412, 161);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,5 +172,10 @@
         private System.Windows.Forms.Label labelCommit;
         private System.Windows.Forms.Label labelTags;
         private System.Windows.Forms.Label labelBranches;
+        private System.Windows.Forms.Label labelMessageCaption;
+        private System.Windows.Forms.Label labelTagsCaption;
+        private System.Windows.Forms.Label labelBranchesCaption;
+        private System.Windows.Forms.Label labelAuthorCaption;
+        private System.Windows.Forms.Label labelCommitCaption;
     }
 }
--- GitUI/CommitInfo/CommitSummaryUserControl.cs
+++ GitUI/CommitInfo/CommitSummaryUserControl.cs
@@ -11,12 +11,15 @@ using GitCommands;
 namespace GitUI
 {
     /// <summary>
-    /// TODO: replace with some better looking HTML control (similar to Commit Tab in main window)
+    /// TODO: replace with some better looking RTF control (similar to Commit Tab in main window)
+    ///       Tried RichTextBox: strangely it does not show any formatting, just plain text.
     /// </summary>
     public partial class CommitSummaryUserControl : UserControl
     {
         private const string tagsCaption = "Tags";
         private const string branchesCaption = "Branches";
+        private readonly Color tagsBackColor = Color.LightSteelBlue;
+        private readonly Color branchesBackColor = Color.LightSalmon;
 
         public CommitSummaryUserControl()
         {
@@ -36,20 +39,28 @@ namespace GitUI
             {
                 _revision = value;
 
+                labelCommitCaption.Text = Strings.GetCommitHashText() + ":";
+                labelAuthorCaption.Text = Strings.GetAuthorText() + ":";
+                labelMessageCaption.Text = Strings.GetMessageText() + ":";
+                labelTagsCaption.Text = tagsCaption + ":";
+                labelBranchesCaption.Text = branchesCaption + ":";
+
                 if (Revision != null)
                 {
-                    labelCommit.Text = string.Format(Strings.GetCommitHashText() + ": {0}", Revision.Guid);
-                    labelAuthor.Text = string.Format(Strings.GetAuthorText() + ": {0}", Revision.Author);
+                    labelCommit.Text = string.Format("{0}", Revision.Guid);
+                    labelAuthor.Text = string.Format("{0}", Revision.Author);
                     labelDate.Text = string.Format(Strings.GetCommitDateText() + ": {0}", Revision.CommitDate);
-                    labelMessage.Text = string.Format(Strings.GetMessageText() + ": {0}", Revision.Message);
+                    labelMessage.Text = string.Format("{0}", Revision.Message);
                     
                     var tagList = Revision.Heads.Where(r => r.IsTag);
                     string tagListStr = string.Join(", ", tagList.Select(h => h.LocalName).ToArray());
-                    labelTags.Text = string.Format(tagsCaption + ": {0}", tagListStr.IsNullOrEmpty() ? "n/a" : tagListStr);
+                    labelTags.Text = string.Format("{0}", tagListStr.IsNullOrEmpty() ? "n/a" : tagListStr);
+                    labelTags.BackColor = tagList.Any() ? tagsBackColor : Control.DefaultBackColor;
 
                     var branchesList = Revision.Heads.Where(r => r.IsHead);
                     string branchesListStr = string.Join(", ", branchesList.Select(h => h.LocalName).ToArray());
-                    labelBranches.Text = string.Format(branchesCaption + ": {0}", branchesListStr.IsNullOrEmpty() ? "n/a" : branchesListStr);
+                    labelBranches.Text = string.Format("{0}", branchesListStr.IsNullOrEmpty() ? "n/a" : branchesListStr);
+                    labelBranches.BackColor = branchesList.Any() ? branchesBackColor : Control.DefaultBackColor;
                 }
                 else
                 {
@@ -58,7 +69,9 @@ namespace GitUI
                     labelDate.Text = string.Format(Strings.GetCommitDateText() + ": {0}", "---");
                     labelMessage.Text = string.Format(Strings.GetMessageText() + ": {0}", "---");
                     labelTags.Text = string.Format(tagsCaption + ": {0}", "---");
+                    labelTags.BackColor = Control.DefaultBackColor;
                     labelBranches.Text = string.Format(branchesCaption + ": {0}", "---");
+                    labelBranches.BackColor = Control.DefaultBackColor;
                 }
             }
         }
