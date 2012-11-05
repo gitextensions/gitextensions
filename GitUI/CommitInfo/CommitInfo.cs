f4aff13cdbaa94d1dbe55ee1ad2d5b9034a8d6fd
australiensun <australien@sun>
2012-11-05 19:34:42 +0100

    GitUI.CommitInfo: move code of class CommandEventArgs to separate file because it hinders the VS designer; fix namespaces.

--- GitUI/CommitInfo/CommitInfo.cs
+++ GitUI/CommitInfo/CommitInfo.cs
@@ -11,18 +11,8 @@ using GitCommands;
 using GitUI.Editor.RichTextBoxExtension;
 using ResourceManager.Translation;
 
-namespace GitUI
+namespace GitUI.CommitInfo
 {
-    public class CommandEventArgs : EventArgs {
-        public CommandEventArgs(string command, string data)
-        {
-            this.Command = command;
-            this.Data = data;
-        }
-        public string Command { get; set; }
-        public string Data { get; set; }
-    }
-
     public partial class CommitInfo : GitModuleControl
     {
         private readonly TranslationString containedInBranches = new TranslationString("Contained in branches:");
