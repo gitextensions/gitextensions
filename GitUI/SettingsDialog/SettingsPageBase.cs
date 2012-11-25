165f0552cf656bd76a3e61ce503d5526c347c663
australiensun <australien@sun>
2012-11-25 11:30:38 +0100

    Add SettingsPage registering logic and event handling stub

---
+++ GitUI/SettingsDialog/SettingsPageBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI.SettingsDialog
+{
+    public abstract class SettingsPageBase : UserControl
+    {
+        //// public abstract string Title { get; }
+    }
+}
--- GitUI/SettingsDialog/SettingsTreeViewUserControl.cs
+++ GitUI/SettingsDialog/SettingsTreeViewUserControl.cs
@@ -11,9 +11,58 @@ namespace GitUI.SettingsDialog
 {
     public partial class SettingsTreeViewUserControl : UserControl
     {
+        private TreeNode _geRootNode;
+        private TreeNode _pluginsRootNode;
+
+        public event EventHandler<SettingsPageSelectedEventArgs> SettingsPageSelected;
+
         public SettingsTreeViewUserControl()
         {
             InitializeComponent();
+
+            AddRootNodes();
         }
+
+        private void AddRootNodes()
+        {
+            _geRootNode = treeView1.Nodes.Add("Git Extensions");
+
+            _pluginsRootNode = treeView1.Nodes.Add("Plugins");
+            var pluginPlaceHolderNode = _pluginsRootNode.Nodes.Add("todo");
+            pluginPlaceHolderNode.NodeFont = new Font(treeView1.Font, FontStyle.Italic);
+            pluginPlaceHolderNode.ForeColor = Color.Gray;
+        }
+
+        public void RegisterSettingsPage(SettingsPageBase settingsPage)
+        {
+            var settingsPageNode = _geRootNode.Nodes.Add(settingsPage.Text);
+            settingsPageNode.Tag = settingsPage;
+        }
+
+        public void RegisteringComplete()
+        {
+            _geRootNode.Nodes.Add("Other settings");
+            treeView1.ExpandAll();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (SettingsPageSelected != null)
+            {
+                if (e.Node.Tag as SettingsPageBase != null)
+                {
+                    SettingsPageSelected(this, new SettingsPageSelectedEventArgs { SettingsPageBase = (SettingsPageBase)(e.Node.Tag) });
+                }
+                else if (e.Node.Text == "Other settings")
+                {
+                    SettingsPageSelected(this, new SettingsPageSelectedEventArgs { SettingsPageBase = null });
+                }
+            }
+        }
+    }
+
+    public class SettingsPageSelectedEventArgs : EventArgs
+    {
+        public SettingsPageBase SettingsPageBase { get; internal set; }
     }
 }
