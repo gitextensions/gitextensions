78fae237f671d8222bcb494ce758be66f83d4256
australiensun <australien@sun>
2012-11-26 22:32:57 +0100

    GitSettingsPage: add empty stub

---
+++ GitUI/SettingsDialog/Pages/GitSettingsPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI.SettingsDialog.Pages
+{
+    public partial class GitSettingsPage : SettingsPageBase
+    {
+        public GitSettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
