cc75968dfa1a58773e35113bcbe8b68bd4a208fc
australiensun <australien@sun>
2012-11-26 23:17:32 +0100

    Fix autoscalemode; Dock Fill; increase tree item height

--- GitUI/SettingsDialog/Pages/BlankSettingsPage.Designer.cs
+++ GitUI/SettingsDialog/Pages/BlankSettingsPage.Designer.cs
@@ -42,8 +42,7 @@
             // 
             // BlankSettingsPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.Controls.Add(this.label1);
             this.Name = "BlankSettingsPage";
             this.Size = new System.Drawing.Size(197, 133);
