e15c05a5d76d8c8b63188a51c416e187e5682b5c
australiensun <australien@sun>
2012-11-28 22:30:31 +0100

    Add SettingsPage stubs for remaining tabs

---
+++ GitUI/SettingsDialog/Pages/HotkeysSettingsPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI.SettingsDialog.Pages
+{
+    public partial class HotkeysSettingsPage : SettingsPageBase
+    {
+        public HotkeysSettingsPage()
+        {
+            InitializeComponent();
+
+            Text = "Hotkeys";
+        }
+    }
+}
---
+++ GitUI/SettingsDialog/Pages/ShellExtensionSettingsPage.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace GitUI.SettingsDialog.Pages
+{
+    partial class ShellExtensionSettingsPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ShellExtensionSettingsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Name = "ShellExtensionSettingsPage";
+            this.Size = new System.Drawing.Size(691, 513);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
