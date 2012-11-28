66aa5381b7a871864dc23d6378c08a4255a2d435
australiensun <australien@sun>
2012-11-28 22:30:31 +0100

    Add SettingsPage stubs for remaining tabs

---
+++ GitUI/SettingsDialog/Pages/ColorsSettingsPage.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace GitUI.SettingsDialog.Pages
+{
+    partial class ColorsSettingsPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ColorsSettingsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Name = "ColorsSettingsPage";
+            this.Size = new System.Drawing.Size(736, 512);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
