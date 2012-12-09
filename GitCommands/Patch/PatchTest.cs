f01b467bda721e433cfad17347468e37bc082ae1
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2012-12-09 09:10:46 -0500

    Adding Patch.TestPatchConstructor

---
+++ GitCommands/Patch/PatchTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestContext = System.Object;
+using TestProperty = NUnit.Framework.PropertyAttribute;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitCommandsTests.Patch
+{
+    [TestClass]
+    class PatchTest
+    {
+        [TestMethod]
+        public void TestPatchConstructor()
+        {
+            PatchApply.Patch patch = new PatchApply.Patch();
+
+            Assert.IsNotNull(patch);
+        }
+    }
+}
