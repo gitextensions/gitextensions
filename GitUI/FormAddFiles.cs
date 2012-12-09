e9faeda5cc59b50d0ba8fc4e9286cb7b29f4328a
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2012-12-10 02:12:58 +0400

    Add files support filename parameter

--- GitUI/FormAddFiles.cs
+++ GitUI/FormAddFiles.cs
@@ -13,22 +13,28 @@ namespace GitUI
         {
         }
 
-        public FormAddFiles(GitUICommands aCommands)
+        public FormAddFiles(GitUICommands aCommands, string addFile)
             : base(aCommands)
         {
             InitializeComponent();
             Translate();
+            Filter.Text = addFile ?? ".";
+        }
+
+        public FormAddFiles(GitUICommands aCommands)
+            : this(aCommands, null)
+        {
         }
 
         private void AddFilesClick(object sender, EventArgs e)
         {
             var argumentFormat = force.Checked ? "add -f \"{0}\"" : "add \"{0}\"";
-            FormProcess.ShowDialog(this, string.Format(argumentFormat, Filter.Text));
+            FormProcess.ShowDialog(this, string.Format(argumentFormat, Filter.Text), false);
         }
 
         private void ShowFilesClick(object sender, EventArgs e)
         {
-            FormProcess.ShowDialog(this, string.Format("add --dry-run \"{0}\"", Filter.Text));
+            FormProcess.ShowDialog(this, string.Format("add --dry-run \"{0}\"", Filter.Text), false);
         }
     }
 }
