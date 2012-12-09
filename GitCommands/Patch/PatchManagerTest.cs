9b3607ce29efb758eb04300346c97a9d3ba7f7b7
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2012-12-09 10:17:49 -0500

    Add test for PatchManager.LoadPatch

--- GitCommands/Patch/PatchManagerTest.cs
+++ GitCommands/Patch/PatchManagerTest.cs
@@ -23,5 +23,33 @@ namespace GitCommandsTests.Patch
             PatchManager manager = new PatchManager();
             Assert.IsNotNull(manager);
         }
+
+        [TestMethod]
+        public void TestLoadPatch()
+        {
+            PatchManager manager = new PatchManager();
+
+            PatchApply.Patch expectedPatch = new PatchApply.Patch();
+            expectedPatch.Type = PatchApply.Patch.PatchType.ChangeFile;
+            expectedPatch.Apply = true;
+            expectedPatch.PatchHeader = "diff --git a/thisisatest.txt b/thisisatest.txt";
+            expectedPatch.PatchIndex = "index 5e4dce2..5eb1e6f 100644";
+            expectedPatch.FileNameA = "thisisatest.txt";
+            expectedPatch.FileNameB = "thisisatest.txt";
+            expectedPatch.AppendTextLine(expectedPatch.PatchHeader);
+            expectedPatch.AppendTextLine(expectedPatch.PatchIndex);
+            expectedPatch.AppendTextLine("--- a/thisisatest.txt");
+            expectedPatch.AppendTextLine("+++ b/thisisatest.txt");
+            expectedPatch.AppendTextLine("@@ -1,2 +1,2 @@");
+            expectedPatch.AppendTextLine("iiiiii");
+            expectedPatch.AppendTextLine("-asdkjaldskjlaksd");
+            expectedPatch.AppendTextLine("+changed again");
+
+            manager.LoadPatch(expectedPatch.Text, false, Encoding.Default);
+
+            PatchApply.Patch createdPatch = manager.Patches.First();
+            Assert.AreEqual(expectedPatch.Text, createdPatch.Text);
+        }
+
     }
 }
