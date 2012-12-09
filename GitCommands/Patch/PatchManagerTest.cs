456f6740284196034078ad8231dc1376462c4e18
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2012-12-09 10:08:53 -0500

    Add PatchManager and Test for its constructor

---
+++ GitCommands/Patch/PatchManagerTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using PatchApply;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestContext = System.Object;
+using TestProperty = NUnit.Framework.PropertyAttribute;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitCommandsTests.Patch
+{
+    [TestClass]
+    class PatchManagerTest
+    {
+        [TestMethod]
+        public void TestPatchManagerConstructor()
+        {
+            PatchManager manager = new PatchManager();
+            Assert.IsNotNull(manager);
+        }
+    }
+}
