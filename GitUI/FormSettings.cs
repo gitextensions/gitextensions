43108b27d50a07e689294c6fa5e89a4a53bacfbd
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2012-12-12 04:15:35 +0400

    Rearranged commands in shell context menu.
    Support by command move menu items to submenu

--- GitUI/FormSettings.cs
+++ GitUI/FormSettings.cs
@@ -577,11 +577,9 @@ namespace GitUI
                 chkUseFastChecks.Checked = Settings.UseFastChecks;
                 chkShowRelativeDate.Checked = Settings.RelativeDate;
 
-                chkCascadedContextMenu.Checked = Settings.ShellCascadeContextMenu;
-
-                for (int i = 0; i < Settings.ShellVisibleMenuItems.Length; i++)
+                for (int i = 0; i < Settings.CascadeShellMenuItems.Length; i++)
                 {
-                    chlMenuEntries.SetItemChecked(i, Settings.ShellVisibleMenuItems[i] == '1');
+                    chlMenuEntries.SetItemChecked(i, Settings.CascadeShellMenuItems[i] == '1');
                 }
 
                 if (string.IsNullOrEmpty(GitCommandHelpers.GetSsh()))
@@ -697,24 +695,21 @@ namespace GitUI
             Settings.IconColor = GetSelectedApplicationIconColor();
             Settings.IconStyle = IconStyle.Text;
 
-            // Shell Extension settings
-            Settings.ShellCascadeContextMenu = chkCascadedContextMenu.Checked;
-
-            String l_ShellVisibleMenuItems = "";
+            String l_CascadeShellMenuItems = "";
 
             for (int i = 0; i < chlMenuEntries.Items.Count; i++)
-			{
+            {
                 if (chlMenuEntries.GetItemChecked(i))
                 {
-                    l_ShellVisibleMenuItems += "1";
+                    l_CascadeShellMenuItems += "1";
                 }
                 else
                 {
-                    l_ShellVisibleMenuItems += "0";
+                    l_CascadeShellMenuItems += "0";
                 }
-			}
+            }
 
-            Settings.ShellVisibleMenuItems = l_ShellVisibleMenuItems;
+            Settings.CascadeShellMenuItems = l_CascadeShellMenuItems;
 
             EnableSettings();
 
