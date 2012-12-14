ef6af6223448348039e09ad973aa66c358596dfd
Donatas Mačiūnas <mdonatas@gmail.com>
2012-12-14 16:19:11 +0200

    Formatting HotkeyCommand.cs

--- GitUI/Hotkey/HotkeyCommand.cs
+++ GitUI/Hotkey/HotkeyCommand.cs
@@ -5,34 +5,34 @@ using System.Xml.Serialization;
 
 namespace GitUI.Hotkey
 {
-  [Serializable]
-  public class HotkeyCommand
-  {
-    #region Properties
-
-    [XmlAttribute]
-    public int CommandCode { get; set; }
-
-    [XmlAttribute]
-    public string Name { get; set; }
-
-    [XmlAttribute]
-    public Keys KeyData { get; set; }
-
-    #endregion
-
-    public HotkeyCommand()
+    [Serializable]
+    public class HotkeyCommand
     {
-    }
-    public HotkeyCommand(int commandCode, string name)
-    {
-      this.CommandCode = commandCode;
-      this.Name = name;
-    }
+        #region Properties
 
-    public static HotkeyCommand[] FromEnum(Type enumType)
-    {
-      return Enum.GetValues(enumType).Cast<object>().Select(c => new HotkeyCommand((int)c, c.ToString())).ToArray();
+        [XmlAttribute]
+        public int CommandCode { get; set; }
+
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlAttribute]
+        public Keys KeyData { get; set; }
+
+        #endregion
+
+        public HotkeyCommand()
+        {
+        }
+        public HotkeyCommand(int commandCode, string name)
+        {
+            this.CommandCode = commandCode;
+            this.Name = name;
+        }
+
+        public static HotkeyCommand[] FromEnum(Type enumType)
+        {
+            return Enum.GetValues(enumType).Cast<object>().Select(c => new HotkeyCommand((int)c, c.ToString())).ToArray();
+        }
     }
-  }
 }
