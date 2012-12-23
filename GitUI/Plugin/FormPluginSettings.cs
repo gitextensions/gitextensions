34a5bba4aec3375ba984456996c7526561c94889
Linquize <linquize@yahoo.com.hk>
2012-12-24 03:11:06 +0400

    Show a cat in Github menu

--- GitUI/Plugin/FormPluginSettings.cs
+++ GitUI/Plugin/FormPluginSettings.cs
@@ -58,6 +58,9 @@ namespace GitUI.Plugin
 
             foreach (var setting in _selectedGitPlugin.Settings.GetAvailableSettings())
             {
+                if (setting.ToLower() == "icon")
+                    continue;
+
                 var label =
                     new Label
                         {
