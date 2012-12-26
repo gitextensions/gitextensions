be38e25ca49281f900947367ed84d249d4c73b1c
australiensun <australien@sun>
2012-12-26 15:15:40 +0100

    Add user control to display up to two help images.

---
+++ GitUI/Help/HelpImageDisplayUserControl.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GitUI.Help
+{
+    partial class HelpImageDisplayUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelHoverText = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(364, 263);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.HelpImageDisplayUserControl_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.HelpImageDisplayUserControl_MouseLeave);
+            // 
+            // labelHoverText
+            // 
+            this.labelHoverText.AutoSize = true;
+            this.labelHoverText.Location = new System.Drawing.Point(3, 269);
+            this.labelHoverText.Name = "labelHoverText";
+            this.labelHoverText.Size = new System.Drawing.Size(38, 15);
+            this.labelHoverText.TabIndex = 1;
+            this.labelHoverText.Text = "label1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelHoverText, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(370, 284);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // HelpImageDisplayUserControl
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "HelpImageDisplayUserControl";
+            this.Size = new System.Drawing.Size(370, 284);
+            this.Load += new System.EventHandler(this.HelpImageDisplayUserControl_Load);
+            this.MouseEnter += new System.EventHandler(this.HelpImageDisplayUserControl_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.HelpImageDisplayUserControl_MouseLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelHoverText;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
