b16457e9e0c62256adccf652c6a7fed2e7283bdf
australiensun <australien@sun>
2012-12-30 19:18:53 +0100

    Add new plugin ReleaseNotesGenerator (stub)

---
+++ Plugins/ReleaseNotesGenerator/ReleaseNotesGeneratorForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ReleaseNotesGenerator
+{
+    partial class ReleaseNotesGeneratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ReleaseNotesGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 232);
+            this.Name = "ReleaseNotesGeneratorForm";
+            this.Text = "ReleaseNotesGeneratorForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
