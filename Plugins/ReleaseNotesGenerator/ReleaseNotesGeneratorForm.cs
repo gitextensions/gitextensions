26874b9b977b0b97deaa5dde4e2c911d679d01e9
australiensun <australien@sun>
2012-12-30 19:18:53 +0100

    Add new plugin ReleaseNotesGenerator (stub)

---
+++ Plugins/ReleaseNotesGenerator/ReleaseNotesGeneratorForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GitUIPluginInterfaces;
+
+namespace ReleaseNotesGenerator
+{
+    public partial class ReleaseNotesGeneratorForm : Form
+    {
+        private readonly IGitPluginSettingsContainer _settings;
+        private readonly GitUIBaseEventArgs _gitUiCommands;
+
+        public ReleaseNotesGeneratorForm(IGitPluginSettingsContainer settings, GitUIBaseEventArgs gitUiCommands)
+        {
+            InitializeComponent();
+
+            _settings = settings;
+            _gitUiCommands = gitUiCommands;
+        }
+    }
+}
