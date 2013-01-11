73dd5e0675bee0e79d7eff970b2491e95d60dcea
Janusz Białobrzewski <jbialobr@o2.pl>
2013-01-11 22:04:16 +0100

    Persist IsExpanded in global settings

--- GitUI/FormPull.Designer.cs
+++ GitUI/FormPull.Designer.cs
@@ -152,7 +152,7 @@
             this.helpImageDisplayUserControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.helpImageDisplayUserControl1.Image1 = null;
             this.helpImageDisplayUserControl1.Image2 = null;
-            this.helpImageDisplayUserControl1.IsExpanded = false;
+            this.helpImageDisplayUserControl1.IsExpanded = true;
             this.helpImageDisplayUserControl1.Location = new System.Drawing.Point(3, 3);
             this.helpImageDisplayUserControl1.MinimumSize = new System.Drawing.Size(30, 50);
             this.helpImageDisplayUserControl1.Name = "helpImageDisplayUserControl1";
