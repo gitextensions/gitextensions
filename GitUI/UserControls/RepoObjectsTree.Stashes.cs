7561fa21c740f44652403c2681f0825ea7f51fe5
jberger <jberger.public@live.com>
2013-01-27 16:45:02 -0600

    RepoObjectsTreeSet minor changes; other minor changes;

--- GitUI/UserControls/RepoObjectsTree.Stashes.cs
+++ GitUI/UserControls/RepoObjectsTree.Stashes.cs
@@ -7,11 +7,13 @@ namespace GitUI.UserControls
     // "stashes"
     public partial class RepoObjectsTree
     {
+        /// <summary>Reloads the stashes.</summary>
         void ReloadStashes(ICollection<GitStash> stashes)
         {
             nodeStashes.Text = string.Format("stashes ({0})", stashes.Count);
         }
 
+        /// <summary>Adds the specified <paramref name="stash"/> to the stashes tree.</summary>
         TreeNode AddStash(TreeNodeCollection nodes, GitStash stash)
         {
             TreeNode treeNode = nodes.Add(stash.Index.ToString(), stash.Name);
