8164d3f5a55551b27da19f311cf9b4cba7aa2d15
jberger <jberger.public@live.com>
2013-01-30 14:57:39 -0600

    doc;

--- ResourceManager/TranslationString.cs
+++ ResourceManager/TranslationString.cs
@@ -3,14 +3,17 @@ using System.Diagnostics;
 
 namespace ResourceManager.Translation
 {
+    /// <summary>Provides translated text.</summary>
     [DebuggerDisplay("{Text}")]
     public class TranslationString : Component
     {
+        /// <summary>Creates a new <see cref="TranslationString"/> with the specified <paramref name="text"/>.</summary>
         public TranslationString(string text)
         {
             Text = text;
         }
 
+        /// <summary>Gets the translated text.</summary>
         public string Text { get; private set; }
     }
 }
