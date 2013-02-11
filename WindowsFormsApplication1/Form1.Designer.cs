f2e9036fef51507d8ae1a7782066efad962692ab
jberger <jberger.public@live.com>
2013-02-11 13:34:56 -0600

    debugging

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusFiller = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusFiller,
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 70);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(706, 22);
+            this.statusStrip.TabIndex = 5;
+            // 
+            // statusFiller
+            // 
+            this.statusFiller.Name = "statusFiller";
+            this.statusFiller.Size = new System.Drawing.Size(224, 17);
+            this.statusFiller.Spring = true;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(14, 17);
+            this.toolStripStatusLabel1.Text = "X";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 92);
+            this.Controls.Add(this.statusStrip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusFiller;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using GitUI;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Test();
+        }
+
+        void Test()
+        {
+            Debug.WriteLine("Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            WarningToolStripItem warner = new WarningToolStripItem(0);
+            statusStrip.Items.Insert(0, warner);
+
+            var mockStatusUPdates = new StatusFeedItem[]
+            {
+                new StatusFeedItem(StatusSeverity.Fail, "1. fail"), 
+                new StatusFeedItem(StatusSeverity.Fail, "2. fail2"), 
+                new StatusFeedItem(StatusSeverity.Info, "3. info"), 
+                new StatusFeedItem(StatusSeverity.Success, "4. success"), 
+                new StatusFeedItem(StatusSeverity.Warn, "5. warn"), 
+                new StatusFeedItem(StatusSeverity.Success, "6. really long text with a lot of text alsdjfksjadfl;jkasdflkjs;dfjslkjfdskljdfklsj;dfljslkdfjskldfjlsjdfkljsdfjslkdjfkl"), 
+            };
+
+            var statusFeedDelay = Observable.Create<StatusFeedItem>(o =>
+            {
+                var els = new EventLoopScheduler();
+                return mockStatusUPdates.ToObservable()//repoObjectsTree.StatusFeed
+                    .ObserveOn(els)
+                    .Do(x => els.Schedule(() => Thread.Sleep(5 * 1000)))
+                    .Subscribe(o);
+            });
+
+            //statusFeedDelay
+            //    .Timestamp()
+            //    .Select(status => new { status.Value, Timestamp = status.Timestamp.Second + (double)status.Timestamp.Millisecond / 1000.0 })
+            //    .Subscribe(status => Debug.WriteLine("{0}: {1}", status.Timestamp, status.Value));
+            //statusFeedDelay
+            //    .SubscribeOn(this)
+            //    .Subscribe(update => statusToolStripItem.Notify(update));
+
+            statusFeedDelay
+                //.ToObservable()
+                .ObserveOn(this)
+                .DelaySubscription(TimeSpan.FromSeconds(3))
+                .Subscribe(s => warner.Notify(s));
+        }
+    }
+}
