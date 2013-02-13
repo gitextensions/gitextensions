ec9e3fd3438b38b65cdfc60dc419709ca8c03aff
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-02-13 18:05:51 +0400

    Support new TortoiseGit merge tool name

--- GitUI/MergeToolsHelper.cs
+++ GitUI/MergeToolsHelper.cs
@@ -156,10 +156,15 @@ namespace GitUI
                     return FindFileInFolders(exeName, kdiff3path, @"KDiff3\",
                                                           regkdiff3path);
                 case "tmerge":
-                    exeName = "TortoiseMerge.exe";
-                    string difftoolPath = FindFileInFolders(exeName, @"TortoiseSVN\bin\");
+                    exeName = "TortoiseGitMerge.exe"; // TortoiseGit 1.8 use new names
+                    string difftoolPath = FindFileInFolders(exeName, @"TortoiseGit\bin\");
                     if (String.IsNullOrEmpty(difftoolPath))
+                    {
+                        exeName = "TortoiseMerge.exe";
                         difftoolPath = FindFileInFolders(exeName, @"TortoiseGit\bin\");
+                        if (String.IsNullOrEmpty(difftoolPath))
+                            difftoolPath = FindFileInFolders(exeName, @"TortoiseSVN\bin\");
+                    }
                     return difftoolPath;
                 case "winmerge":
                     exeName = "winmergeu.exe";
@@ -246,10 +251,15 @@ namespace GitUI
                     exeName = "p4merge.exe";
                     return FindFileInFolders(exeName, p4mergepath, @"Perforce\");
                 case "tortoisemerge":
-                    exeName = "TortoiseMerge.exe";
-                    string path = FindFileInFolders(exeName, @"TortoiseSVN\bin\");
+                    exeName = "TortoiseGitMerge.exe"; // TortoiseGit 1.8 use new names
+                    string path = FindFileInFolders(exeName, @"TortoiseGit\bin\");
                     if (string.IsNullOrEmpty(path))
+                    {
+                        exeName = "TortoiseMerge.exe";
                         path = FindFileInFolders(exeName, @"TortoiseGit\bin\");
+                        if (string.IsNullOrEmpty(path))
+                            path = FindFileInFolders(exeName, @"TortoiseSVN\bin\");
+                    }
                     return path;
                 case "winmerge":
                     string winmergepath = UnquoteString(GetGlobalSetting("mergetool.winmerge.path"));
@@ -292,7 +302,7 @@ namespace GitUI
 
                     return String.Format(command, exeFile);
             }
-			// other commands supported natively by msysgit
+            // other commands supported natively by msysgit
             return "";
         }
 
