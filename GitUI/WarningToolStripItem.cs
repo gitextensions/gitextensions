526ac0426982944649f1b9f71cd6b8d08d7dcf68
jberger <jberger.public@live.com>
2013-02-14 16:26:50 -0600

    individual notification item pretty solid;

--- GitUI/WarningToolStripItem.cs
+++ GitUI/WarningToolStripItem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Windows.Forms;
 using GitUI.Properties;
@@ -79,57 +80,38 @@ namespace GitUI
         void Notify(Notification notification);
     }
 
-    /// <summary><see cref="ToolStripButton"/> which is meant to notify a user.</summary>
-    public class NotificationsToolStripButton : ToolStripDropDownButton, INotifier
+    /// <summary>Provides helpful members for notifications.</summary>
+    internal static class NotifierHelpers
     {
         /// <summary>blank icon</summary>
-        static string blank = Guid.NewGuid().ToString();
+        internal static string blank = Guid.NewGuid().ToString();
         /// <summary>info icon</summary>
-        static string info = Guid.NewGuid().ToString();
+        internal static string info = Guid.NewGuid().ToString();
         /// <summary>success icon</summary>
-        static string success = Guid.NewGuid().ToString();
+        internal static string success = Guid.NewGuid().ToString();
         /// <summary>warning icon</summary>
-        static string warn = Guid.NewGuid().ToString();
+        internal static string warn = Guid.NewGuid().ToString();
         /// <summary>failure icon</summary>
-        static string fail = Guid.NewGuid().ToString();
+        internal static string fail = Guid.NewGuid().ToString();
 
-        readonly ImageList images;
-
-        /// <summary>3 seconds</summary>
-        static readonly double TotalBlinkDuration = 3000;
-        /// <summary>150ms</summary>
-        static readonly int BlinkInterval = 150;
-        readonly Timer _blinkTimer = new Timer { Interval = BlinkInterval };
-        int nTicks;
-        int nBlinks;
-
-        Blinker currentBlinker;
-        Queue<Notification> statusQueue = new Queue<Notification>();
-
-
-
-        /// <summary>Toggles between an 'on' and 'off' action.</summary>
-        void ToggleOnOff(Action on, Action off)
+        /// <summary>Gets the image key for a <see cref="Notification"/>.</summary>
+        public static string GetImageKey(this Notification notification)
         {
-            nTicks++; // increment
-
-            if (nTicks % 2 == 1)
-            {// even -> off
-                off();
-                Debug.WriteLine("off {0}", currentBlinker.Notification);
-            }
-            else
-            {// odd -> on
-                on();
-                nBlinks++;
-                Debug.WriteLine("on {0}", currentBlinker.Notification);
-            }
-
-            if (nBlinks >= currentBlinker.NumberOfBlinks)
-            {// enough blinks -> stop blinking
-                StopBlink();
+            switch (notification.Severity)
+            {
+                case StatusSeverity.Info: return info;
+                case StatusSeverity.Success: return success;
+                case StatusSeverity.Warn: return warn;
+                case StatusSeverity.Fail: return fail;
+                default: throw new ArgumentOutOfRangeException();
             }
         }
+    }
+
+    /// <summary><see cref="ToolStripButton"/> which holds notifications, with most recent items first.</summary>
+    public class NotificationsToolStripButton : ToolStripDropDownButton, INotifier
+    {
+        readonly ImageList images;
 
         public NotificationsToolStripButton()
         {
@@ -139,23 +121,11 @@ namespace GitUI
             //Dock = DockStyle.Left;
 
             images = new ImageList();
-            images.Images.Add(blank, Resources.BlankIcon);
-            images.Images.Add(success, Resources.NotifySuccess);
-            images.Images.Add(warn, Resources.NotifyWarn); // add when needed
-            images.Images.Add(fail, Resources.NotifyError);
-            images.Images.Add(info, Resources.NotifyInfo);
-        }
-
-        /// <summary>Toggles the <see cref="Image"/> on or off during a tick.</summary>
-        void Blink(object o, EventArgs e)
-        {
-            ToggleOnOff(() =>
-            {
-                ImageKey = currentBlinker.ImageKey;
-            }, () =>
-            {
-                ImageKey = blank;
-            });
+            images.Images.Add(NotifierHelpers.blank, Resources.BlankIcon);
+            images.Images.Add(NotifierHelpers.success, Resources.NotifySuccess);
+            images.Images.Add(NotifierHelpers.warn, Resources.NotifyWarn); // add when needed
+            images.Images.Add(NotifierHelpers.fail, Resources.NotifyError);
+            images.Images.Add(NotifierHelpers.info, Resources.NotifyInfo);
         }
 
         /// <summary>Notifies the user about a status update.</summary>
@@ -182,129 +152,7 @@ namespace GitUI
             }
         }
 
-        /// <summary>Shows the status text and starts blinking the icon.</summary>
-        void Start(Notification notification)
-        {
-            Debug.WriteLine("<StartBlink {0}", notification);
 
-            currentBlinker = Blinker.Create(notification);
-
-            // TODO: ?status log?
-
-            ImageKey = currentBlinker.ImageKey;
-            Text = notification.Text;
-            Visible = true;
-
-            nTicks = 0;
-            if (currentBlinker.NumberOfBlinks == 0)
-            {
-                LeaveOn();
-            }
-            else
-            {
-                StartBlink();
-            }
-        }
-
-        void StartBlink()
-        {
-            _blinkTimer.Tick += Blink;
-            _blinkTimer.Interval = 500;
-            _blinkTimer.Start();
-        }
-
-        /// <summary>Stops the blinking.</summary>
-        void StopBlink()
-        {
-            Debug.WriteLine(">StopBlink {0}", currentBlinker != null ? currentBlinker.Notification : "initial" as object);
-            nTicks = 0;
-            nBlinks = 0;
-            _blinkTimer.Stop();
-            _blinkTimer.Tick -= Blink;
-
-            StartNewOrLeaveOn();
-        }
-
-        /// <summary>Leaves the notification on for a certain amount of time.</summary>
-        void LeaveOn()
-        {
-            if (currentBlinker.OnDuration == TimeSpan.Zero)
-            {// leave on until user clicks (OR next notification)
-                Debug.WriteLine(">LeaveOn -- {0}", currentBlinker.Notification);
-                currentBlinker = null;
-                return;
-            }
-            Debug.WriteLine(">LeaveOn 5s {0}", currentBlinker.Notification);
-            _blinkTimer.Interval = (int)currentBlinker.OnDuration.TotalMilliseconds;
-            _blinkTimer.Tick += Close;
-            _blinkTimer.Start();
-        }
-
-        /// <summary>Leaves the status on or starts the next one.</summary>
-        void StartNewOrLeaveOn()
-        {
-            if (statusQueue.Any())
-            {// if any in queue -> start next
-                Start(statusQueue.Dequeue());
-            }
-            else
-            {// (none in queue) -> leave on
-                LeaveOn();
-            }
-        }
-
-        /// <summary>Close the current blinker.</summary>
-        void Close()
-        {
-            Debug.WriteLine(">Close {0}", currentBlinker.Notification);
-            _blinkTimer.Stop();
-            _blinkTimer.Tick -= Close;
-            Visible = false;
-            Text = string.Empty;
-            ImageKey = blank;
-        }
-
-        /// <summary>EventHandler to close the current blinker.</summary>
-        void Close(object sender, EventArgs e)
-        {
-            Close();
-        }
-
-        class Blinker
-        {
-            public Notification Notification { get; private set; }
-            public string ImageKey { get; private set; }
-            public int NumberOfBlinks { get; private set; }
-            public TimeSpan OnDuration { get; private set; }
-
-            public Blinker(Notification status, string imageKey, TimeSpan onDuration, int nBlinks = 0)
-            {
-                Notification = status;
-                ImageKey = imageKey;
-                NumberOfBlinks = nBlinks;
-                OnDuration = onDuration;
-            }
-
-            public static Blinker Create(Notification statusUpdate)
-            {
-                switch (statusUpdate.Severity)
-                {// TODO: blink rate proportional to severity level
-                    case StatusSeverity.Info:
-                        // NO blinks, stay on for 5s
-                        return new Blinker(statusUpdate, info, TimeSpan.FromSeconds(5));
-                    case StatusSeverity.Success:
-                        // NO blinks, stay on for 5s
-                        return new Blinker(statusUpdate, success, TimeSpan.FromSeconds(5));
-                    case StatusSeverity.Warn:
-                        // blink once, stay on until clicked
-                        return new Blinker(statusUpdate, warn, TimeSpan.Zero, 1);
-                    case StatusSeverity.Fail:
-                        // blink twice, stay on until clicked
-                        return new Blinker(statusUpdate, fail, TimeSpan.Zero, 2);
-                    default: throw new ArgumentOutOfRangeException();
-                }
-            }
-        }
 
         protected override void OnParentChanged(ToolStrip oldParent, ToolStrip newParent)
         {
@@ -315,26 +163,39 @@ namespace GitUI
             }
         }
 
-        void Add(Notification notification)
+        void Update()
         {
-            var control = new NotificationItem(notification);
-            control.Click += OnNotificationClick;
+            Text = notifications.Count.ToString();
         }
 
         List<Notification> notifications = new List<Notification>();
 
-        void OnNotificationClick(object sender, EventArgs e)
+        void Add(Notification notification)
         {
-            NotificationItem control = (NotificationItem)sender;
-            control.Click -= OnNotificationClick;
-            Remove(control);
+            var control = new NotificationItem(notification);
+            control.Click += RemoveNotification;
+            control.Expired += RemoveNotification;
+
+            notifications.Insert(0, notification);
+            DropDownItems.Insert(0, control);
+
+            Update();
         }
 
         /// <summary>Removes a notification from the status feed.</summary>
         void Remove(NotificationItem control)
         {
-            DropDownItems.Remove(control);
             notifications.Remove(control.Notification);
+            DropDownItems.Remove(control);
+            Update();
+        }
+
+        void RemoveNotification(object sender, EventArgs e)
+        {
+            NotificationItem control = (NotificationItem)sender;
+            control.Click -= RemoveNotification;
+            control.Expired -= RemoveNotification;
+            Remove(control);
         }
 
         class NotificationSet
@@ -363,22 +224,45 @@ namespace GitUI
     /// <summary>Single notification item in the status feed.</summary>
     sealed class NotificationItem : ToolStripMenuItem
     {
+        /// <summary>Gets the displayed notification.</summary>
         internal Notification Notification { get; private set; }
+
+        /// <summary>syncronizes the notification's relevancy duration</summary>
+        Timer timer;
 
         public NotificationItem(Notification notification)
         {
-            throw new NotImplementedException("image");
             Text = notification.Text;
+            ImageKey = Notification.GetImageKey();
             Notification = notification;
+            timer = new Timer { Interval = (int)TimeSpan.FromMinutes(1).TotalMilliseconds };
+            timer.Tick += OnExpired;
+            timer.Start();
         }
 
+        /// <summary>Invoked when the notification's relevance expires.</summary>
+        void OnExpired(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timer.Tick -= OnExpired;
+            timer.Dispose();
 
+            Visible = false;
+            var handler = Expired;
+            if (handler != null)
+            {
+                handler(this, null);
+            }
+        }
 
+        /// <summary>Hides the notification.</summary>
         protected override void OnClick(EventArgs e)
         {
-            base.OnClick(e);
             Visible = false;
+            base.OnClick(e);
         }
 
+        /// <summary>Occurs when a notification's relevance has expired.</summary>
+        public event EventHandler Expired;
     }
 }
