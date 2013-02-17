f0396e80d791c179101b14c5f4e915576ab479cf
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-02-17 23:22:20 +0400

    FormChooseCommit window centering fixed
    FormCherryPick layout improved with High DPI

--- GitUI/CommandsDialogs/FormCherryPick.Designer.cs
+++ GitUI/CommandsDialogs/FormCherryPick.Designer.cs
@@ -31,7 +31,6 @@
             this.Pick = new System.Windows.Forms.Button();
             this.btnChooseRevision = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.panelParentsList = new System.Windows.Forms.Panel();
             this.ParentsLabel = new System.Windows.Forms.Label();
             this.ParentsList = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -42,15 +41,26 @@
             this.AutoCommit = new System.Windows.Forms.CheckBox();
             this.BranchInfo = new System.Windows.Forms.Label();
             this.commitSummaryUserControl1 = new GitUI.CommitSummaryUserControl();
+            this.mainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.commitInformationPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelParentsList = new System.Windows.Forms.TableLayoutPanel();
+            this.mainTableLayoutPanel.SuspendLayout();
+            this.commitInformationPanel.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.panelParentsList.SuspendLayout();
             this.SuspendLayout();
             // 
             // Pick
             // 
             this.Pick.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.Pick.Location = new System.Drawing.Point(533, 356);
+            this.Pick.Location = new System.Drawing.Point(586, 35);
+            this.Pick.Margin = new System.Windows.Forms.Padding(4);
             this.Pick.Name = "Pick";
-            this.Pick.Size = new System.Drawing.Size(109, 25);
+            this.tableLayoutPanel3.SetRowSpan(this.Pick, 2);
+            this.Pick.Size = new System.Drawing.Size(136, 31);
             this.Pick.TabIndex = 10;
             this.Pick.Text = "Cherry pick";
             this.Pick.UseVisualStyleBackColor = true;
@@ -60,9 +70,10 @@
             // 
             this.btnChooseRevision.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnChooseRevision.Image = global::GitUI.Properties.Resources.IconSelectRevision;
-            this.btnChooseRevision.Location = new System.Drawing.Point(617, 76);
+            this.btnChooseRevision.Location = new System.Drawing.Point(105, 50);
+            this.btnChooseRevision.Margin = new System.Windows.Forms.Padding(4);
             this.btnChooseRevision.Name = "btnChooseRevision";
-            this.btnChooseRevision.Size = new System.Drawing.Size(25, 24);
+            this.btnChooseRevision.Size = new System.Drawing.Size(31, 30);
             this.btnChooseRevision.TabIndex = 33;
             this.btnChooseRevision.UseVisualStyleBackColor = true;
             this.btnChooseRevision.Click += new System.EventHandler(this.btnChooseRevision_Click);
@@ -71,30 +82,20 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(561, 43);
+            this.label2.Location = new System.Drawing.Point(4, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 30);
+            this.label2.Size = new System.Drawing.Size(132, 46);
             this.label2.TabIndex = 34;
             this.label2.Text = "Choose another\r\nrevision:";
-            // 
-            // panelParentsList
-            // 
-            this.panelParentsList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panelParentsList.Controls.Add(this.ParentsLabel);
-            this.panelParentsList.Controls.Add(this.ParentsList);
-            this.panelParentsList.Location = new System.Drawing.Point(7, 204);
-            this.panelParentsList.Name = "panelParentsList";
-            this.panelParentsList.Size = new System.Drawing.Size(645, 131);
-            this.panelParentsList.TabIndex = 16;
-            this.panelParentsList.Visible = false;
             // 
             // ParentsLabel
             // 
             this.ParentsLabel.AutoSize = true;
-            this.ParentsLabel.Location = new System.Drawing.Point(6, 8);
+            this.ParentsLabel.Location = new System.Drawing.Point(4, 0);
+            this.ParentsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ParentsLabel.Name = "ParentsLabel";
-            this.ParentsLabel.Size = new System.Drawing.Size(207, 15);
+            this.ParentsLabel.Size = new System.Drawing.Size(298, 23);
             this.ParentsLabel.TabIndex = 13;
             this.ParentsLabel.Text = "This commit is a merge, select parent:";
             // 
@@ -111,10 +112,11 @@
             this.columnHeader4});
             this.ParentsList.FullRowSelect = true;
             this.ParentsList.HideSelection = false;
-            this.ParentsList.Location = new System.Drawing.Point(6, 26);
+            this.ParentsList.Location = new System.Drawing.Point(0, 23);
+            this.ParentsList.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
             this.ParentsList.MultiSelect = false;
             this.ParentsList.Name = "ParentsList";
-            this.ParentsList.Size = new System.Drawing.Size(629, 99);
+            this.ParentsList.Size = new System.Drawing.Size(726, 109);
             this.ParentsList.TabIndex = 12;
             this.ParentsList.UseCompatibleStateImageBehavior = false;
             this.ParentsList.View = System.Windows.Forms.View.Details;
@@ -122,7 +124,7 @@
             // columnHeader1
             // 
             this.columnHeader1.Text = "No.";
-            this.columnHeader1.Width = 38;
+            this.columnHeader1.Width = 43;
             // 
             // columnHeader2
             // 
@@ -143,9 +145,10 @@
             // 
             this.checkAddReference.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkAddReference.AutoSize = true;
-            this.checkAddReference.Location = new System.Drawing.Point(12, 370);
+            this.checkAddReference.Location = new System.Drawing.Point(4, 39);
+            this.checkAddReference.Margin = new System.Windows.Forms.Padding(4);
             this.checkAddReference.Name = "checkAddReference";
-            this.checkAddReference.Size = new System.Drawing.Size(253, 19);
+            this.checkAddReference.Size = new System.Drawing.Size(357, 27);
             this.checkAddReference.TabIndex = 14;
             this.checkAddReference.Text = "Add commit reference to commit message";
             this.checkAddReference.UseVisualStyleBackColor = true;
@@ -154,9 +157,10 @@
             // 
             this.AutoCommit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.AutoCommit.AutoSize = true;
-            this.AutoCommit.Location = new System.Drawing.Point(12, 345);
+            this.AutoCommit.Location = new System.Drawing.Point(4, 4);
+            this.AutoCommit.Margin = new System.Windows.Forms.Padding(4);
             this.AutoCommit.Name = "AutoCommit";
-            this.AutoCommit.Size = new System.Drawing.Size(189, 19);
+            this.AutoCommit.Size = new System.Drawing.Size(265, 27);
             this.AutoCommit.TabIndex = 11;
             this.AutoCommit.Text = "Automatically create a commit";
             this.AutoCommit.UseVisualStyleBackColor = true;
@@ -164,9 +168,10 @@
             // BranchInfo
             // 
             this.BranchInfo.AutoSize = true;
-            this.BranchInfo.Location = new System.Drawing.Point(12, 9);
+            this.BranchInfo.Location = new System.Drawing.Point(4, 0);
+            this.BranchInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.BranchInfo.Name = "BranchInfo";
-            this.BranchInfo.Size = new System.Drawing.Size(137, 15);
+            this.BranchInfo.Size = new System.Drawing.Size(194, 23);
             this.BranchInfo.TabIndex = 5;
             this.BranchInfo.Text = "Cherry pick this commit:";
             // 
@@ -175,35 +180,121 @@
             this.commitSummaryUserControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.commitSummaryUserControl1.AutoSize = true;
-            this.commitSummaryUserControl1.Location = new System.Drawing.Point(11, 35);
-            this.commitSummaryUserControl1.MinimumSize = new System.Drawing.Size(440, 160);
+            this.commitSummaryUserControl1.Location = new System.Drawing.Point(4, 4);
+            this.commitSummaryUserControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.commitSummaryUserControl1.MinimumSize = new System.Drawing.Size(550, 200);
             this.commitSummaryUserControl1.Name = "commitSummaryUserControl1";
             this.commitSummaryUserControl1.Revision = null;
-            this.commitSummaryUserControl1.Size = new System.Drawing.Size(545, 160);
+            this.commitSummaryUserControl1.Size = new System.Drawing.Size(572, 200);
             this.commitSummaryUserControl1.TabIndex = 15;
+            // 
+            // mainTableLayoutPanel
+            // 
+            this.mainTableLayoutPanel.AutoSize = true;
+            this.mainTableLayoutPanel.ColumnCount = 1;
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTableLayoutPanel.Controls.Add(this.BranchInfo, 0, 0);
+            this.mainTableLayoutPanel.Controls.Add(this.commitInformationPanel, 0, 1);
+            this.mainTableLayoutPanel.Controls.Add(this.tableLayoutPanel3, 0, 3);
+            this.mainTableLayoutPanel.Controls.Add(this.panelParentsList, 0, 2);
+            this.mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+            this.mainTableLayoutPanel.RowCount = 4;
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainTableLayoutPanel.Size = new System.Drawing.Size(732, 451);
+            this.mainTableLayoutPanel.TabIndex = 35;
+            // 
+            // commitInformationPanel
+            // 
+            this.commitInformationPanel.AutoSize = true;
+            this.commitInformationPanel.ColumnCount = 2;
+            this.commitInformationPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.commitInformationPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.commitInformationPanel.Controls.Add(this.commitSummaryUserControl1, 0, 0);
+            this.commitInformationPanel.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.commitInformationPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.commitInformationPanel.Location = new System.Drawing.Point(3, 26);
+            this.commitInformationPanel.Name = "commitInformationPanel";
+            this.commitInformationPanel.RowCount = 1;
+            this.commitInformationPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.commitInformationPanel.Size = new System.Drawing.Size(726, 208);
+            this.commitInformationPanel.TabIndex = 6;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.btnChooseRevision);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(583, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(140, 202);
+            this.flowLayoutPanel1.TabIndex = 16;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.AutoSize = true;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel3.Controls.Add(this.Pick, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.checkAddReference, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.AutoCommit, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 378);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(726, 70);
+            this.tableLayoutPanel3.TabIndex = 17;
+            // 
+            // panelParentsList
+            // 
+            this.panelParentsList.ColumnCount = 1;
+            this.panelParentsList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.panelParentsList.Controls.Add(this.ParentsLabel, 0, 0);
+            this.panelParentsList.Controls.Add(this.ParentsList, 0, 1);
+            this.panelParentsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelParentsList.Location = new System.Drawing.Point(3, 240);
+            this.panelParentsList.Name = "panelParentsList";
+            this.panelParentsList.RowCount = 2;
+            this.panelParentsList.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.panelParentsList.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.panelParentsList.Size = new System.Drawing.Size(726, 132);
+            this.panelParentsList.TabIndex = 18;
             // 
             // FormCherryPick
             // 
             this.AcceptButton = this.Pick;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(654, 392);
-            this.Controls.Add(this.btnChooseRevision);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.panelParentsList);
-            this.Controls.Add(this.commitSummaryUserControl1);
-            this.Controls.Add(this.checkAddReference);
-            this.Controls.Add(this.AutoCommit);
-            this.Controls.Add(this.Pick);
-            this.Controls.Add(this.BranchInfo);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(732, 451);
+            this.Controls.Add(this.mainTableLayoutPanel);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(670, 430);
+            this.MinimumSize = new System.Drawing.Size(750, 360);
             this.Name = "FormCherryPick";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Cherry pick commit";
             this.Load += new System.EventHandler(this.FormCherryPickCommitSmall_Load);
+            this.mainTableLayoutPanel.ResumeLayout(false);
+            this.mainTableLayoutPanel.PerformLayout();
+            this.commitInformationPanel.ResumeLayout(false);
+            this.commitInformationPanel.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
             this.panelParentsList.ResumeLayout(false);
             this.panelParentsList.PerformLayout();
             this.ResumeLayout(false);
@@ -224,8 +315,12 @@
         private System.Windows.Forms.Label ParentsLabel;
         private System.Windows.Forms.CheckBox checkAddReference;
         private CommitSummaryUserControl commitSummaryUserControl1;
-        private System.Windows.Forms.Panel panelParentsList;
         private System.Windows.Forms.Button btnChooseRevision;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel mainTableLayoutPanel;
+        private System.Windows.Forms.TableLayoutPanel commitInformationPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel panelParentsList;
     }
 }
