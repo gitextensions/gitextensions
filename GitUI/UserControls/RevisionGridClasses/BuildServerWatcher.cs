ced2290d1863f629b6ce7e55156460a037d07845
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-02-19 00:06:49 +0100

    Move all build info drawing logic into a separate class.

--- GitUI/UserControls/RevisionGridClasses/BuildServerWatcher.cs
+++ GitUI/UserControls/RevisionGridClasses/BuildServerWatcher.cs
@@ -29,58 +29,6 @@ namespace GitUI.RevisionGridClasses
             AddBuildStatusColumns();
         }
 
-        public void RevisionsCellPainting(DataGridViewCellPaintingEventArgs e, GitRevision revision, Brush foreBrush, Font rowFont)
-        {
-            switch (e.ColumnIndex)
-            {
-                case 4:
-                    {
-                        if (revision.BuildStatus != null)
-                        {
-                            Image buildStatusImage = null;
-
-                            switch (revision.BuildStatus.Status)
-                            {
-                                case BuildInfo.BuildStatus.Success:
-                                    buildStatusImage = Properties.Resources.IconClean;
-                                    break;
-                                case BuildInfo.BuildStatus.Failure:
-                                    buildStatusImage = Properties.Resources.error;
-                                    break;
-                                case BuildInfo.BuildStatus.Unknown:
-                                    buildStatusImage = Properties.Resources.Conflict;
-                                    break;
-                            }
-
-                            if (buildStatusImage != null)
-                            {
-                                e.Graphics.DrawImage(buildStatusImage, new Rectangle(e.CellBounds.Left, e.CellBounds.Top + 4, 16, 16));
-                            }
-                        }
-                    }
-                    break;
-                case 5:
-                    if (revision.BuildStatus != null)
-                    {
-                        Brush buildStatusForebrush = foreBrush;
-
-                        switch (revision.BuildStatus.Status)
-                        {
-                            case BuildInfo.BuildStatus.Success:
-                                buildStatusForebrush = Brushes.DarkGreen;
-                                break;
-                            case BuildInfo.BuildStatus.Failure:
-                                buildStatusForebrush = Brushes.DarkRed;
-                                break;
-                        }
-
-                        var text = (string)e.FormattedValue;
-                        e.Graphics.DrawString(text, rowFont, buildStatusForebrush, new PointF(e.CellBounds.Left, e.CellBounds.Top + 4));
-                    }
-                    break;
-            }
-        }
-
         private void AddBuildStatusColumns()
         {
             var buildStatusImageColumn = new DataGridViewImageColumn
@@ -206,21 +154,6 @@ namespace GitUI.RevisionGridClasses
             if (cancellationToken != null)
             {
                 cancellationToken.Dispose();
-            }
-        }
-
-        public void RevisionsCellFormatting(DataGridViewCellFormattingEventArgs e, GitRevision revision)
-        {
-            switch (e.ColumnIndex)
-            {
-                case 4:
-                    e.FormattingApplied = false;
-                    break;
-                case 5:
-                    e.Value = revision.BuildStatus != null
-                                  ? revision.BuildStatus.Description
-                                  : string.Empty;
-                    break;
             }
         }
 
