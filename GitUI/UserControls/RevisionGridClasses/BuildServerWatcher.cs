b5a9a541dfad619a676348b9031f198593ab066b
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-02-19 22:14:50 +0100

    Isolate TeamCity interactions into an IBuildServerWatcher-derived class.

--- GitUI/UserControls/RevisionGridClasses/BuildServerWatcher.cs
+++ GitUI/UserControls/RevisionGridClasses/BuildServerWatcher.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using GitCommands;
-using TeamCitySharp;
-using TeamCitySharp.Locators;
 
 namespace GitUI.RevisionGridClasses
 {
@@ -51,13 +46,12 @@ namespace GitUI.RevisionGridClasses
 
         public void LaunchBuildServerInfoFetchOperation()
         {
-            var client = new TeamCityClient("teamcity.codebetter.com");
-            client.Connect(string.Empty, string.Empty, true);
-
             var projectName = revisionGrid.Module.GitWorkingDir.Split(new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries).Last();
-            var fullDayObservable = CreateTeamCityObservable(client, projectName, DateTime.Now.Date);
-            var fullObservable = CreateTeamCityObservable(client, projectName);
-            var fromNowObservable = CreateTeamCityObservable(client, projectName, DateTime.Now);
+            IBuildServerWatcher buildServerWatcher = new TeamCityBuildWatcher(projectName);
+
+            var fullDayObservable = buildServerWatcher.CreateObservable(DateTime.Now.Date);
+            var fullObservable = buildServerWatcher.CreateObservable();
+            var fromNowObservable = buildServerWatcher.CreateObservable(DateTime.Now);
 
             buildStatusCancellationToken =
                 fullDayObservable.Concat(fullObservable)
@@ -84,69 +78,6 @@ namespace GitUI.RevisionGridClasses
                                              }
                                          }
                                      });
-        }
-
-        private IObservable<BuildInfo> CreateTeamCityObservable(TeamCityClient client, string projectName, DateTime? sinceDate = null)
-        {
-            return Observable.Create<BuildInfo>((observer, cancellationToken) =>
-                {
-                    try
-                    {
-                        var project = client.ProjectByName(projectName);
-                        var buildTypes = project.BuildTypes.BuildType;
-                        var builds =
-                            buildTypes.SelectMany(
-                                x =>
-                                client.BuildsByBuildLocator(
-                                    TeamCityBuildLocator2.WithDimensions(BuildTypeLocator.WithId(x.Id), sinceDate: sinceDate, defaultBranch: "any")));
-                        foreach (var build in builds)
-                        {
-                            cancellationToken.ThrowIfCancellationRequested();
-
-                            dynamic buildExpando =
-                                client.CallByUrl<object>(build.Href.Replace("guestAuth/", string.Empty));
-                            var status = BuildInfo.BuildStatus.Unknown;
-                            string statusText = buildExpando.statusText;
-                            object[] revisions = buildExpando.revisions != null
-                                                     ? buildExpando.revisions.revision
-                                                     : null;
-                            string revisionVersion = revisions != null
-                                                         ? ((dynamic)revisions.Single()).version
-                                                         : null;
-
-                            switch ((string)buildExpando.status)
-                            {
-                                case "SUCCESS":
-                                    status = BuildInfo.BuildStatus.Success;
-                                    break;
-                                case "FAILURE":
-                                    status = BuildInfo.BuildStatus.Failure;
-                                    break;
-                            }
-
-                            var buildInfo = new BuildInfo
-                                {
-                                    Id = buildExpando.id,
-                                    StartDate = buildExpando.startDate,
-                                    Status = status,
-                                    Description = statusText,
-                                    CommitHash = revisionVersion
-                                };
-
-                            observer.OnNext(buildInfo);
-                        }
-
-                        observer.OnCompleted();
-                    }
-                    catch (Exception ex)
-                    {
-                        observer.OnError(ex);
-                    }
-
-                    return new Task<IDisposable>(() => Disposable.Empty);
-                })
-                             .SubscribeOn(NewThreadScheduler.Default)
-                             .OnErrorResumeNext(Observable.Empty<BuildInfo>());
         }
 
         public void CancelBuildStatusFetchOperation()
