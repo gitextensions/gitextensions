c2f7bd04897d2f867e780fa50898e4a410662882
jberger <jberger.public@live.com>
2013-02-22 15:32:13 -0600

    added code so the Most Recent notification doesn't change/hide while the mouse is inside the control;
    Case: user hovering over the notification reading and deciding
    to do: don't want to suddenly hide the control.

--- GitUI/Notifications/MostRecentNotification.cs
+++ GitUI/Notifications/MostRecentNotification.cs
@@ -9,11 +9,15 @@ namespace GitUI.Notifications
         /// <remarks>This control privately held within <see cref="NotificationFeed"/>.</remarks></summary>
         sealed class MostRecentNotification : NotificationControl
         {
+            bool isMouseInside = false;
+            bool hasExpired = false;
+
             /// <summary>Updates the text and image for UI, and start expiration timer.</summary>
             public override void Update(Notification notification)
             {
                 base.Update(notification);
                 Visible = true;
+                hasExpired = false;
                 StartExpiration(TimeSpan.FromSeconds(5));
             }
 
@@ -27,6 +31,9 @@ namespace GitUI.Notifications
             /// <summary>Raises the <see cref="NotificationControl.Expired"/> event.</summary>
             protected override void OnExpired(object sender, EventArgs e)
             {
+                hasExpired = true;
+                if (isMouseInside) { return; }// don't change the control while user inside
+
                 base.OnExpired(sender, e);
                 Empty();
             }
@@ -37,7 +44,24 @@ namespace GitUI.Notifications
                 base.OnClick(e);
                 Empty();
             }
-        }
 
+            /// <summary>Sets a flag indicating the user is inside the control.</summary>
+            protected override void OnMouseEnter(EventArgs e)
+            {
+                base.OnMouseEnter(e);
+                isMouseInside = true;
+            }
+
+            /// <summary>If the notification has expired, invokes <see cref="OnExpired"/>.</summary>
+            protected override void OnMouseLeave(EventArgs e)
+            {
+                isMouseInside = false;
+                if (hasExpired)
+                {
+                    OnExpired(null, null);
+                }
+                base.OnMouseLeave(e);
+            }
+        }
     }
 }
