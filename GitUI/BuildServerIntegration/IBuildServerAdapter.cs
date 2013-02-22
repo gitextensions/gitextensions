5530547311d1beaf6843678023584308dfaf5b4f
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-02-22 10:35:19 +0100

    Fix cancellation of Rx queries.
    This reverts commit f5c643aeb100858cd9be038333168a32d66e33a0.

--- GitUI/BuildServerIntegration/IBuildServerAdapter.cs
+++ GitUI/BuildServerIntegration/IBuildServerAdapter.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Reactive.Concurrency;
 using GitCommands;
 
 namespace GitUI.BuildServerIntegration
 {
     public interface IBuildServerAdapter
     {
-        IObservable<BuildInfo> CreateObservable(DateTime? sinceDate = null);
+        IObservable<BuildInfo> CreateObservable(IScheduler scheduler, DateTime? sinceDate = null);
     }
 }
