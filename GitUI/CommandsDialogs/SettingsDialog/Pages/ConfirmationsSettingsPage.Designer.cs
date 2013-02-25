97494257f2f061156f9a6fcf7ec52234c00af6e1
Victor Shih <victor.shih@gmail.com>
2013-02-25 02:24:13 -0800

    Fix grammar, capitalization in Settings dialog pages and translations.

--- GitUI/CommandsDialogs/SettingsDialog/Pages/ConfirmationsSettingsPage.Designer.cs
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/ConfirmationsSettingsPage.Designer.cs
@@ -103,7 +103,7 @@
             this.chkAmmend.Name = "chkAmmend";
             this.chkAmmend.Size = new System.Drawing.Size(196, 27);
             this.chkAmmend.TabIndex = 0;
-            this.chkAmmend.Text = "Ammend last commit";
+            this.chkAmmend.Text = "Amend last commit";
             this.chkAmmend.UseVisualStyleBackColor = true;
             // 
             // chkAutoPopStashAfterPull
