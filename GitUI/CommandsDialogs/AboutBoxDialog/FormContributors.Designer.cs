2b112f34336f08d28e6f419a28dcc4ea4158903d
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-02-26 02:17:25 +0400

    FormContributors resized to fit all coders

--- GitUI/CommandsDialogs/AboutBoxDialog/FormContributors.Designer.cs
+++ GitUI/CommandsDialogs/AboutBoxDialog/FormContributors.Designer.cs
@@ -41,36 +41,44 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 50, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.Size = new System.Drawing.Size(67, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Coders:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 333);
+            this.label2.Location = new System.Drawing.Point(23, 384);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 50, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.Size = new System.Drawing.Size(97, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Translators:";
             // 
             // translatorsLabel
             // 
+            this.translatorsLabel.AutoSize = true;
             this.translatorsLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.translatorsLabel.Location = new System.Drawing.Point(174, 333);
+            this.translatorsLabel.Location = new System.Drawing.Point(185, 384);
+            this.translatorsLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 15);
+            this.translatorsLabel.MinimumSize = new System.Drawing.Size(0, 100);
             this.translatorsLabel.Name = "translatorsLabel";
-            this.translatorsLabel.Size = new System.Drawing.Size(450, 86);
+            this.translatorsLabel.Size = new System.Drawing.Size(623, 100);
             this.translatorsLabel.TabIndex = 2;
             this.translatorsLabel.Text = "Translators";
             // 
             // codersLabel
             // 
+            this.codersLabel.AutoSize = true;
             this.codersLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.codersLabel.Location = new System.Drawing.Point(174, 15);
+            this.codersLabel.Location = new System.Drawing.Point(185, 19);
+            this.codersLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 15);
+            this.codersLabel.MinimumSize = new System.Drawing.Size(0, 350);
             this.codersLabel.Name = "codersLabel";
-            this.codersLabel.Size = new System.Drawing.Size(450, 318);
+            this.codersLabel.Size = new System.Drawing.Size(623, 350);
             this.codersLabel.TabIndex = 3;
             this.codersLabel.Text = "Coders";
             // 
@@ -78,8 +86,8 @@
             // 
             this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.51546F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.48454F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.designersLabel, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.translatorsLabel, 1, 1);
@@ -88,40 +96,46 @@
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(15);
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(19);
             this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 318F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 86F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 11F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(642, 488);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(832, 683);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // designersLabel
             // 
+            this.designersLabel.AutoSize = true;
             this.designersLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.designersLabel.Location = new System.Drawing.Point(174, 419);
+            this.designersLabel.Location = new System.Drawing.Point(185, 499);
+            this.designersLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 15);
+            this.designersLabel.MinimumSize = new System.Drawing.Size(0, 60);
             this.designersLabel.Name = "designersLabel";
-            this.designersLabel.Size = new System.Drawing.Size(450, 54);
+            this.designersLabel.Size = new System.Drawing.Size(623, 150);
             this.designersLabel.TabIndex = 5;
             this.designersLabel.Text = "Designers";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(18, 419);
+            this.label4.Location = new System.Drawing.Point(23, 499);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 50, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.Size = new System.Drawing.Size(107, 23);
             this.label4.TabIndex = 4;
             this.label4.Text = "Logo design:";
             // 
             // FormContributors
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(642, 488);
+            this.ClientSize = new System.Drawing.Size(832, 683);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormContributors";
