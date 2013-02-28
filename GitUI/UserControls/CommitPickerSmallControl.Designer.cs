948b07c7ceae3f0bd4376a143347912c32fd0e8f
australiensun <australien@sun>
2013-03-01 00:24:46 +0100

    fix autoscale mode

--- GitUI/UserControls/CommitPickerSmallControl.Designer.cs
+++ GitUI/UserControls/CommitPickerSmallControl.Designer.cs
@@ -55,8 +55,8 @@
             // 
             // CommitPickerSmallControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.Controls.Add(this.textBoxCommitHash);
             this.Controls.Add(this.buttonPickCommit);
