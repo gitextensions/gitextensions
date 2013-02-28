a6181e8bb197a21e4c5c39048a3484c8218dce8c
australiensun <australien@sun>
2013-03-01 00:06:51 +0100

    GitUICommands: use FormCreateBranchAtRevision instead of FormCreateBranch

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -1880,7 +1880,7 @@ namespace GitUI.CommandsDialogs
 
         private void CreateBranchToolStripMenuItemClick(object sender, EventArgs e)
         {
-            UICommands.StartCreateBranchDialog(this);
+            UICommands.StartCreateBranchDialog(this, RevisionGrid.GetSelectedRevisions().FirstOrDefault());
         }
 
         private void RevisionGridDoubleClick(object sender, EventArgs e)
