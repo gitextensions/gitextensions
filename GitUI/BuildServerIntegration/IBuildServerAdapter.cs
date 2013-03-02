51423827f120c167dcf1e58760748de8f7d0cc8f
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-03-02 22:50:25 +0100

    Start implementing support for build server authentication.

--- GitUI/BuildServerIntegration/IBuildServerAdapter.cs
+++ GitUI/BuildServerIntegration/IBuildServerAdapter.cs
@@ -6,7 +6,13 @@ namespace GitUI.BuildServerIntegration
 {
     public interface IBuildServerAdapter
     {
+        /// <summary>
+        /// Gets a unique key which identifies this build server.
+        /// </summary>
+        string UniqueKey { get; }
+
         IObservable<BuildInfo> GetFinishedBuildsSince(IScheduler scheduler, DateTime? sinceDate = null);
+
         IObservable<BuildInfo> GetRunningBuilds(IScheduler scheduler);
     }
 }
