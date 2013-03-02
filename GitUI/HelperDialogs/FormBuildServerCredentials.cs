57ba6738d1acc4330e7b54b774c0ff2999748975
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-03-02 22:50:25 +0100

    Start implementing support for build server authentication.

---
+++ GitUI/HelperDialogs/FormBuildServerCredentials.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GitUI.HelperDialogs
+{
+    public partial class FormBuildServerCredentials : Form
+    {
+        public FormBuildServerCredentials(string buildServerUniqueKey)
+        {
+            InitializeComponent();
+
+            labelHeader.Text = string.Format(labelHeader.Text, buildServerUniqueKey);
+        }
+
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            UserName = textBoxUserName.Text;
+            Password = textBoxPassword.Text;
+
+            Close();
+        }
+    }
+}
