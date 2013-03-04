f9bad2932724e3c53bfd84ce0d7ee5716ea2ff7f
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-03-04 22:39:12 +0100

    Ensure plugins are given a chance to unload when the app is closed.

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -2382,6 +2382,13 @@ namespace GitUI.CommandsDialogs
                 _dashboard.SaveSplitterPositions();
         }
 
+        protected override void OnClosed(EventArgs e)
+        {
+            SetWorkingDir("");
+
+            base.OnClosed(e);
+        }
+
         private void CloneSvnToolStripMenuItemClick(object sender, EventArgs e)
         {
             UICommands.StartSvnCloneDialog(this, DashboardGitModuleChanged);
