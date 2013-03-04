fadfdfdabd8f4678bc2197b62f65d7225b464a76
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-03-05 00:57:22 +0400

    "Set version" Python script added

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,5 +19,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.44.1")]
-[assembly: AssemblyFileVersion("2.44.1")]
+[assembly: AssemblyVersion("2.45")]
+[assembly: AssemblyFileVersion("2.45")]
