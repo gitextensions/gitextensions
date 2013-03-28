5419cc912b2a2ef7c175eb4381008ebca190853c
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-03-27 21:39:10 -0400

    Nuget on linux now checks for package dir closes #1757

--- .nuget/Nuget.sh
+++ .nuget/Nuget.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-cd packages
-for x in `find ../ -name packages.config`;do mono --runtime=v4.0.30319 ../.nuget/nuget.exe install $x;done
+if [ ! -d 'packages' ]; then
+	mkdir packages
+fi
+
+for x in `find ./ -name packages.config`;do mono --runtime=v4.0.30319 .nuget/NuGet.exe install $x -o packages;done
