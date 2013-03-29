8e91bc897c13f4e59b595325efd3413a62992367
Janusz Białobrzewski <jbialobr@o2.pl>
2013-03-29 20:18:10 +0100

    Use chosen action when performing auto pull in the future.

--- GitCommands/Settings.cs
+++ GitCommands/Settings.cs
@@ -360,7 +360,8 @@ namespace GitCommands
             Merge,
             Rebase,
             Fetch,
-            FetchAll
+            FetchAll,
+            Default
         }
 
         public static PullAction PullMerge
@@ -439,10 +440,10 @@ namespace GitCommands
             set { SetBool("AutoPopStashAfterCheckoutBranch", value); }
         }
 
-        public static bool? AutoPullOnPushRejected
+        public static PullAction? AutoPullOnPushRejectedAction
         {
-            get { return GetBool("AutoPullOnPushRejected", null); }
-            set { SetBool("AutoPullOnPushRejected", value); }
+            get { return GetEnum<PullAction?>("AutoPullOnPushRejectedAction", null); }
+            set { SetEnum<PullAction?>("AutoPullOnPushRejectedAction", value); }
         }
 
         public static bool DontConfirmPushNewBranch
