da60e73795bf43207d31c2732350b2ffee2b1333
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-03-30 00:18:29 +0400

    Support pull latest submodule changes from FormSubmodules dialog window

--- GitUI/CommandsDialogs/FormSubmodules.Designer.cs
+++ GitUI/CommandsDialogs/FormSubmodules.Designer.cs
@@ -54,7 +54,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.SubModuleLocalPath = new System.Windows.Forms.TextBox();
             this.SubModuleCommit = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
             this.SubModuleBranch = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Pull = new System.Windows.Forms.Button();
 #if Mono212Released //waiting for mono 2.12
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
 #endif
@@ -68,6 +71,8 @@
             this.flowLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -85,7 +90,7 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel3);
-            this.splitContainer1.Size = new System.Drawing.Size(782, 355);
+            this.splitContainer1.Size = new System.Drawing.Size(782, 372);
             this.splitContainer1.SplitterDistance = 222;
             this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
@@ -103,12 +108,12 @@
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(222, 355);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(222, 372);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // AddSubmodule
             // 
-            this.AddSubmodule.Location = new System.Drawing.Point(4, 320);
+            this.AddSubmodule.Location = new System.Drawing.Point(4, 337);
             this.AddSubmodule.Margin = new System.Windows.Forms.Padding(4);
             this.AddSubmodule.Name = "AddSubmodule";
             this.AddSubmodule.Size = new System.Drawing.Size(202, 31);
@@ -136,7 +141,7 @@
             this.Submodules.ReadOnly = true;
             this.Submodules.RowHeadersVisible = false;
             this.Submodules.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.Submodules.Size = new System.Drawing.Size(214, 308);
+            this.Submodules.Size = new System.Drawing.Size(214, 325);
             this.Submodules.TabIndex = 0;
             // 
             // nameDataGridViewTextBoxColumn
@@ -172,7 +177,7 @@
             this.tableLayoutPanel3.RowCount = 2;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(555, 355);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(555, 372);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
             // flowLayoutPanel1
@@ -183,7 +188,7 @@
             this.flowLayoutPanel1.Controls.Add(this.SynchronizeSubmodule);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 314);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 331);
             this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(555, 41);
@@ -238,7 +243,7 @@
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(547, 306);
+            this.groupBox1.Size = new System.Drawing.Size(547, 323);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Details";
@@ -259,7 +264,7 @@
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.SubModuleLocalPath, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.SubModuleCommit, 1, 3);
-            this.tableLayoutPanel1.Controls.Add(this.SubModuleBranch, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 1, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 27);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
@@ -272,15 +277,14 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(539, 275);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(539, 292);
             this.tableLayoutPanel1.TabIndex = 12;
             // 
             // SubModuleStatus
             // 
             this.SubModuleStatus.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.SubModuleStatus.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.gitSubmoduleBindingSource, "Status", true));
-            this.SubModuleStatus.Location = new System.Drawing.Point(122, 205);
-            this.SubModuleStatus.Margin = new System.Windows.Forms.Padding(5);
+            this.SubModuleStatus.Location = new System.Drawing.Point(120, 183);
             this.SubModuleStatus.Name = "SubModuleStatus";
             this.SubModuleStatus.ReadOnly = true;
             this.SubModuleStatus.Size = new System.Drawing.Size(214, 30);
@@ -290,7 +294,7 @@
             // 
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(4, 208);
+            this.label6.Location = new System.Drawing.Point(4, 186);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(57, 23);
@@ -301,7 +305,7 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 8);
+            this.label1.Location = new System.Drawing.Point(4, 6);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 23);
@@ -312,7 +316,7 @@
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 48);
+            this.label2.Location = new System.Drawing.Point(4, 42);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 23);
@@ -323,7 +327,7 @@
             // 
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(4, 168);
+            this.label5.Location = new System.Drawing.Point(4, 150);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 23);
@@ -334,8 +338,7 @@
             // 
             this.SubModuleName.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.SubModuleName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.gitSubmoduleBindingSource, "Name", true));
-            this.SubModuleName.Location = new System.Drawing.Point(122, 5);
-            this.SubModuleName.Margin = new System.Windows.Forms.Padding(5);
+            this.SubModuleName.Location = new System.Drawing.Point(120, 3);
             this.SubModuleName.Name = "SubModuleName";
             this.SubModuleName.ReadOnly = true;
             this.SubModuleName.Size = new System.Drawing.Size(214, 30);
@@ -345,7 +348,7 @@
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(4, 128);
+            this.label4.Location = new System.Drawing.Point(4, 114);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 23);
@@ -356,18 +359,17 @@
             // 
             this.SubModuleRemotePath.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.gitSubmoduleBindingSource, "RemotePath", true));
             this.SubModuleRemotePath.Dock = System.Windows.Forms.DockStyle.Top;
-            this.SubModuleRemotePath.Location = new System.Drawing.Point(122, 45);
-            this.SubModuleRemotePath.Margin = new System.Windows.Forms.Padding(5);
+            this.SubModuleRemotePath.Location = new System.Drawing.Point(120, 39);
             this.SubModuleRemotePath.Name = "SubModuleRemotePath";
             this.SubModuleRemotePath.ReadOnly = true;
-            this.SubModuleRemotePath.Size = new System.Drawing.Size(412, 30);
+            this.SubModuleRemotePath.Size = new System.Drawing.Size(416, 30);
             this.SubModuleRemotePath.TabIndex = 9;
             // 
             // label3
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(4, 88);
+            this.label3.Location = new System.Drawing.Point(4, 78);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(89, 23);
@@ -378,40 +380,77 @@
             // 
             this.SubModuleLocalPath.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.gitSubmoduleBindingSource, "LocalPath", true));
             this.SubModuleLocalPath.Dock = System.Windows.Forms.DockStyle.Top;
-            this.SubModuleLocalPath.Location = new System.Drawing.Point(122, 85);
-            this.SubModuleLocalPath.Margin = new System.Windows.Forms.Padding(5);
+            this.SubModuleLocalPath.Location = new System.Drawing.Point(120, 75);
             this.SubModuleLocalPath.Name = "SubModuleLocalPath";
             this.SubModuleLocalPath.ReadOnly = true;
-            this.SubModuleLocalPath.Size = new System.Drawing.Size(412, 30);
+            this.SubModuleLocalPath.Size = new System.Drawing.Size(416, 30);
             this.SubModuleLocalPath.TabIndex = 10;
             // 
             // SubModuleCommit
             // 
             this.SubModuleCommit.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.gitSubmoduleBindingSource, "CurrentCommitGuid", true));
-            this.SubModuleCommit.Dock = System.Windows.Forms.DockStyle.Top;
-            this.SubModuleCommit.Location = new System.Drawing.Point(122, 125);
-            this.SubModuleCommit.Margin = new System.Windows.Forms.Padding(5);
+            this.SubModuleCommit.Location = new System.Drawing.Point(120, 111);
             this.SubModuleCommit.Name = "SubModuleCommit";
             this.SubModuleCommit.ReadOnly = true;
-            this.SubModuleCommit.Size = new System.Drawing.Size(412, 30);
-            this.SubModuleCommit.TabIndex = 11;
+            this.SubModuleCommit.Size = new System.Drawing.Size(416, 30);
+            this.SubModuleCommit.TabIndex = 17;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.AutoSize = true;
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel4.Controls.Add(this.SubModuleBranch, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.flowLayoutPanel2, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(117, 144);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(422, 36);
+            this.tableLayoutPanel4.TabIndex = 18;
             // 
             // SubModuleBranch
             // 
             this.SubModuleBranch.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.gitSubmoduleBindingSource, "Branch", true));
             this.SubModuleBranch.Dock = System.Windows.Forms.DockStyle.Top;
-            this.SubModuleBranch.Location = new System.Drawing.Point(122, 165);
-            this.SubModuleBranch.Margin = new System.Windows.Forms.Padding(5);
+            this.SubModuleBranch.Location = new System.Drawing.Point(3, 3);
             this.SubModuleBranch.Name = "SubModuleBranch";
             this.SubModuleBranch.ReadOnly = true;
-            this.SubModuleBranch.Size = new System.Drawing.Size(412, 30);
-            this.SubModuleBranch.TabIndex = 12;
+            this.SubModuleBranch.Size = new System.Drawing.Size(386, 30);
+            this.SubModuleBranch.TabIndex = 13;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.Controls.Add(this.Pull);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(395, 6);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(24, 24);
+            this.flowLayoutPanel2.TabIndex = 14;
+            // 
+            // Pull
+            // 
+            this.Pull.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Pull.BackgroundImage = global::GitUI.Properties.Resources.Icon_4;
+            this.Pull.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Pull.Location = new System.Drawing.Point(0, 0);
+            this.Pull.Margin = new System.Windows.Forms.Padding(0);
+            this.Pull.MinimumSize = new System.Drawing.Size(24, 24);
+            this.Pull.Name = "Pull";
+            this.Pull.Size = new System.Drawing.Size(24, 24);
+            this.Pull.TabIndex = 22;
+            this.Pull.UseVisualStyleBackColor = true;
+            this.Pull.Click += new System.EventHandler(this.Pull_Click);
             // 
             // FormSubmodules
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(782, 355);
+            this.ClientSize = new System.Drawing.Size(782, 372);
             this.Controls.Add(this.splitContainer1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimumSize = new System.Drawing.Size(710, 350);
@@ -435,6 +474,9 @@
             this.groupBox1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -458,8 +500,6 @@
         private System.Windows.Forms.TextBox SubModuleRemotePath;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox SubModuleLocalPath;
-        private System.Windows.Forms.TextBox SubModuleCommit;
-        private System.Windows.Forms.TextBox SubModuleBranch;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button SynchronizeSubmodule;
         private System.Windows.Forms.Button UpdateSubmodule;
@@ -467,5 +507,10 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox SubModuleCommit;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.TextBox SubModuleBranch;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button Pull;
     }
 }
