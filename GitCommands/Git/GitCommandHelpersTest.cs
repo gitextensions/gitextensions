a2298f1f8fff40337573a3c82c68abc9f95c668b
janusz <janusz@biu.pl>
2013-04-23 22:36:31 +0200

    SettingsContainer

--- GitCommands/Git/GitCommandHelpersTest.cs
+++ GitCommands/Git/GitCommandHelpersTest.cs
@@ -18,7 +18,7 @@ namespace GitExtensionsTest.Git
         [TestMethod]
         public void CanGetRelativeDateString()
         {
-            Settings.CurrentTranslation = "English";
+            AppSettings.CurrentTranslation = "English";
             Assert.AreEqual("0 seconds ago", GitCommandHelpers.GetRelativeDateString(DateTime.Now, DateTime.Now));
             Assert.AreEqual("1 second ago", GitCommandHelpers.GetRelativeDateString(DateTime.Now, DateTime.Now.AddSeconds(-1)));
             Assert.AreEqual("1 minute ago", GitCommandHelpers.GetRelativeDateString(DateTime.Now, DateTime.Now.AddMinutes(-1)));
@@ -42,7 +42,7 @@ namespace GitExtensionsTest.Git
         [TestMethod]
         public void CanGetRelativeNegativeDateString()
         {
-            Settings.CurrentTranslation = "English";
+            AppSettings.CurrentTranslation = "English";
             Assert.AreEqual("-1 second ago", GitCommandHelpers.GetRelativeDateString(DateTime.Now, DateTime.Now.AddSeconds(1)));
             Assert.AreEqual("-1 minute ago", GitCommandHelpers.GetRelativeDateString(DateTime.Now, DateTime.Now.AddMinutes(1)));
             Assert.AreEqual("-1 hour ago", GitCommandHelpers.GetRelativeDateString(DateTime.Now, DateTime.Now.AddMinutes(45)));
