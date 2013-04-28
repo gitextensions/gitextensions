a69b504b724cc2817fa193f0050a381a976498a3
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-04-29 01:23:29 +0400

    Restore old revision grid behavior before revert

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -271,8 +271,8 @@ namespace GitUI.CommandsDialogs
             {
                 if (Settings.PlaySpecialStartupSound)
                 {
-                    using (var cow_moo = Properties.Resources.cow_moo)
-                        new System.Media.SoundPlayer(cow_moo).Play();
+                    using (var cowMoo = Resources.cow_moo)
+                        new System.Media.SoundPlayer(cowMoo).Play();
                 }
             }
             catch // This code is just for fun, we do not want the program to crash because of it.
@@ -285,15 +285,15 @@ namespace GitUI.CommandsDialogs
             this.InvokeAsync(() =>
             {
                 RefreshButton.Image = indexChanged && Settings.UseFastChecks && Module.IsValidGitWorkingDir()
-                                          ? GitUI.Properties.Resources.arrow_refresh_dirty
-                                          : GitUI.Properties.Resources.arrow_refresh;
+                                          ? Resources.arrow_refresh_dirty
+                                          : Resources.arrow_refresh;
             });
         }
 
-        private bool pluginsLoaded;
+        private bool _pluginsLoaded;
         private void LoadPluginsInPluginMenu()
         {
-            if (pluginsLoaded)
+            if (_pluginsLoaded)
                 return;
             foreach (var plugin in LoadedPlugins.Plugins)
             {
@@ -301,7 +301,7 @@ namespace GitUI.CommandsDialogs
                 item.Click += ItemClick;
                 pluginsToolStripMenuItem.DropDownItems.Insert(pluginsToolStripMenuItem.DropDownItems.Count - 2, item);
             }
-            pluginsLoaded = true;
+            _pluginsLoaded = true;
             UpdatePluginMenu(Module.IsValidGitWorkingDir());
         }
 
