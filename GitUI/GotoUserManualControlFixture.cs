1bb608ff548f9ef0737b5b3dbfcc67c03d9c489a
australiensun <australien@sun>
2013-05-19 20:52:54 +0200

    GotoUserManualControlFixture: update test

--- GitUI/GotoUserManualControlFixture.cs
+++ GitUI/GotoUserManualControlFixture.cs
@@ -15,15 +15,37 @@ namespace GitExtensionsTest.GitUI
         public void GetUrlTest()
         {
             var ctrl = new GotoUserManualControl();
-            ctrl.GetUrl().Should().Be("https://gitextensions.readthedocs.org/en/latest/");
 
-            ctrl.ManualSectionSubfolder = "aaa";
+            ctrl.ManualLocation = "https://gitextensions.readthedocs.org/en/latest";
+            ctrl.ManualType = ManualType.StandardHtml;
+
+            ctrl.ManualSectionSubfolder = null;
             ctrl.ManualSectionAnchorName = null;
-            ctrl.GetUrl().Should().Be("https://gitextensions.readthedocs.org/en/latest/aaa");
+            ctrl.GetUrl().Should().Be("https://gitextensions.readthedocs.org/en/latest//"); // both null makes no sense atm
+            
+            ctrl.ManualSectionSubfolder = "merge_conflicts";
+            ctrl.ManualSectionAnchorName = null;
+            ctrl.GetUrl().Should().Be("https://gitextensions.readthedocs.org/en/latest/merge_conflicts/");
 
-            ctrl.ManualSectionSubfolder = "bbb";
-            ctrl.ManualSectionAnchorName = "ccc";
-            ctrl.GetUrl().Should().Be("https://gitextensions.readthedocs.org/en/latest/bbb#ccc");
+            ctrl.ManualSectionSubfolder = "merge_conflicts";
+            ctrl.ManualSectionAnchorName = "merge-conflicts";
+            ctrl.GetUrl().Should().Be("https://gitextensions.readthedocs.org/en/latest/merge_conflicts/#merge-conflicts");
+
+
+            ctrl.ManualLocation = "file:///D:/data2/projects/gitextensions/GitExtensionsDoc/build/singlehtml";
+            ctrl.ManualType = ManualType.SingleHtml;
+
+            ctrl.ManualSectionSubfolder = null;
+            ctrl.ManualSectionAnchorName = null;
+            ctrl.GetUrl().Should().Be("file:///D:/data2/projects/gitextensions/GitExtensionsDoc/build/singlehtml/index.html");
+
+            ctrl.ManualSectionSubfolder = "merge_conflicts";
+            ctrl.ManualSectionAnchorName = null;
+            ctrl.GetUrl().Should().Be("file:///D:/data2/projects/gitextensions/GitExtensionsDoc/build/singlehtml/index.html");
+
+            ctrl.ManualSectionSubfolder = "merge_conflicts";
+            ctrl.ManualSectionAnchorName = "merge-conflicts";
+            ctrl.GetUrl().Should().Be("file:///D:/data2/projects/gitextensions/GitExtensionsDoc/build/singlehtml/index.html#merge-conflicts");
         }
     }
 }
