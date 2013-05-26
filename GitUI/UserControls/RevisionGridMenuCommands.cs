12c7aff31b41ca2e80c1a2a0877dbdb9b167cc7b
australiensun <aepape@gmx.de>
2013-05-26 21:20:49 +0200

    FormBrowse/Navigate: add quick search menu items

--- GitUI/UserControls/RevisionGridMenuCommands.cs
+++ GitUI/UserControls/RevisionGridMenuCommands.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GitUI.Hotkey;
+using System.Windows.Forms;
 
 namespace GitUI.UserControls
 {
@@ -36,6 +37,37 @@ namespace GitUI.UserControls
                 menuCommand.Text = "Go to parent commit";
                 menuCommand.ShortcutKeyDisplayString = _revisionGrid.GetShortcutKeys(GitUI.RevisionGrid.Commands.GoToParent).ToShortcutKeyDisplayString();
                 menuCommand.ExecuteAction = () => _revisionGrid.ExecuteCommand(GitUI.RevisionGrid.Commands.GoToParent);
+
+                resultList.Add(menuCommand);
+            }
+
+            resultList.Add(null);
+
+            {
+                var menuCommand = new MenuCommand();
+                menuCommand.Name = "QuickSearch";
+                menuCommand.Text = "Quick search";
+                menuCommand.ExecuteAction = () => MessageBox.Show("Start typing in revision grid to start quick search.");
+
+                resultList.Add(menuCommand);
+            }
+
+            {
+                var menuCommand = new MenuCommand();
+                menuCommand.Name = "PrevQuickSearch";
+                menuCommand.Text = "Quick search previous";
+                menuCommand.ShortcutKeyDisplayString = _revisionGrid.GetShortcutKeys(GitUI.RevisionGrid.Commands.PrevQuickSearch).ToShortcutKeyDisplayString();
+                menuCommand.ExecuteAction = () => _revisionGrid.ExecuteCommand(GitUI.RevisionGrid.Commands.PrevQuickSearch);
+
+                resultList.Add(menuCommand);
+            }
+
+            {
+                var menuCommand = new MenuCommand();
+                menuCommand.Name = "NextQuickSearch";
+                menuCommand.Text = "Quick search next";
+                menuCommand.ShortcutKeyDisplayString = _revisionGrid.GetShortcutKeys(GitUI.RevisionGrid.Commands.NextQuickSearch).ToShortcutKeyDisplayString();
+                menuCommand.ExecuteAction = () => _revisionGrid.ExecuteCommand(GitUI.RevisionGrid.Commands.NextQuickSearch);
 
                 resultList.Add(menuCommand);
             }
