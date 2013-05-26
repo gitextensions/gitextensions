9c8e1a51998e5efc284a946e5ede53cb67f8153b
australiensun <aepape@gmx.de>
2013-05-26 18:47:01 +0200

    Add FormBrowseMenus.cs to handle the new additional main menus "Navigate" and "View"

---
+++ GitUI/CommandsDialogs/FormBrowseMenus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUI.CommandsDialogs
+{
+    class FormBrowseMenus
+    {
+        MenuStrip _menuStrip;
+        ToolStripMenuItem _navigateToolStripMenuItem;
+        ToolStripMenuItem _viewToolStripMenuItem;
+
+        public FormBrowseMenus(MenuStrip menuStrip)
+        {
+            _menuStrip = menuStrip;
+        }
+
+        /// <summary>
+        /// inserts 
+        /// - Navigate (after Repository)
+        /// - View
+        /// </summary>
+        public void InsertAdditionalMainMenuItems(ToolStripMenuItem insertAfterMenuItem)
+        {
+            RemoveAdditionalMainMenuItems();
+
+            _navigateToolStripMenuItem = new ToolStripMenuItem();
+            // navigateToolStripMenuItem.DropDownItems
+            _navigateToolStripMenuItem.Name = "navigateToolStripMenuItem";
+            // navigateToolStripMenuItem.Size
+            _navigateToolStripMenuItem.Text = "Navigate";
+            _menuStrip.Items.Insert(_menuStrip.Items.IndexOf(insertAfterMenuItem) + 1, _navigateToolStripMenuItem);
+
+            _viewToolStripMenuItem = new ToolStripMenuItem();
+            // navigateToolStripMenuItem.DropDownItems
+            _viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            _viewToolStripMenuItem.Text = "View";
+            _menuStrip.Items.Insert(_menuStrip.Items.IndexOf(_navigateToolStripMenuItem) + 1, _viewToolStripMenuItem);
+        }
+
+        public void RemoveAdditionalMainMenuItems()
+        {
+            _menuStrip.Items.Remove(_navigateToolStripMenuItem);
+            _menuStrip.Items.Remove(_viewToolStripMenuItem);
+        }
+    }
+}
