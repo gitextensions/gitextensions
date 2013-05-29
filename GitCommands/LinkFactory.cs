7fb1bb77b6b2ffb2c7a96aa0145763a61da0c399
australiensun <aepape@gmx.de>
2013-05-29 19:05:33 +0200

    replace gitex with gitext in LinkFactory

--- GitCommands/LinkFactory.cs
+++ GitCommands/LinkFactory.cs
@@ -6,22 +6,22 @@ namespace GitCommands
     {
          public static string CreateTagLink(string tag)
          {
-             return "<a href='gitex://gototag/" + tag + "'>" + WebUtility.HtmlEncode(tag) + "</a>";
+             return "<a href='gitext://gototag/" + tag + "'>" + WebUtility.HtmlEncode(tag) + "</a>";
          }
 
         public static string CreateBranchLink(string noPrefixBranch)
         {
-            return "<a href='gitex://gotobranch/" + noPrefixBranch + "'>" + WebUtility.HtmlEncode(noPrefixBranch) + "</a>";
+            return "<a href='gitext://gotobranch/" + noPrefixBranch + "'>" + WebUtility.HtmlEncode(noPrefixBranch) + "</a>";
         }
 
         public static string CreateCommitLink(string guid)
         {
             if (GitRevision.UnstagedGuid == guid)
-                return "<a href='gitex://gotocommit/" + guid + "'>" + Strings.GetCurrentUnstagedChanges() + "</a>";
+                return "<a href='gitext://gotocommit/" + guid + "'>" + Strings.GetCurrentUnstagedChanges() + "</a>";
             else if (GitRevision.IndexGuid == guid)
-                return "<a href='gitex://gotocommit/" + guid + "'>" + Strings.GetCurrentIndex() + "</a>";
+                return "<a href='gitext://gotocommit/" + guid + "'>" + Strings.GetCurrentIndex() + "</a>";
             else
-                return "<a href='gitex://gotocommit/" + guid + "'>" + guid.Substring(0, 10) + "</a>";
+                return "<a href='gitext://gotocommit/" + guid + "'>" + guid.Substring(0, 10) + "</a>";
         }
     }
 }
