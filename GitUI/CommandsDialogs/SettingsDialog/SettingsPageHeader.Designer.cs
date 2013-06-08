4daeead9cb7e9a8566a659e59fc1ddc4b99c20b8
Janusz Białobrzewski <jbialobr@o2.pl>
2013-06-08 14:55:12 +0200

    Fixed AutoScaleMode

--- GitUI/CommandsDialogs/SettingsDialog/SettingsPageHeader.Designer.cs
+++ GitUI/CommandsDialogs/SettingsDialog/SettingsPageHeader.Designer.cs
@@ -54,10 +54,11 @@
             this.HeaderPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.HeaderPanel.Controls.Add(this.tableLayoutPanel2);
             this.HeaderPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.HeaderPanel.Location = new System.Drawing.Point(0, 3);
+            this.HeaderPanel.Location = new System.Drawing.Point(0, 4);
+            this.HeaderPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.HeaderPanel.Name = "HeaderPanel";
-            this.HeaderPanel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
-            this.HeaderPanel.Size = new System.Drawing.Size(797, 56);
+            this.HeaderPanel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 7);
+            this.HeaderPanel.Size = new System.Drawing.Size(930, 73);
             this.HeaderPanel.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -69,12 +70,13 @@
             this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 3;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(797, 51);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(930, 66);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // linePanel
@@ -82,9 +84,10 @@
             this.linePanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.linePanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.linePanel.Location = new System.Drawing.Point(3, 35);
+            this.linePanel.Location = new System.Drawing.Point(3, 45);
+            this.linePanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.linePanel.Name = "linePanel";
-            this.linePanel.Size = new System.Drawing.Size(791, 3);
+            this.linePanel.Size = new System.Drawing.Size(924, 4);
             this.linePanel.TabIndex = 10;
             // 
             // flowLayoutPanel1
@@ -94,10 +97,10 @@
             this.flowLayoutPanel1.Controls.Add(this.label1);
             this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 4);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(791, 29);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(924, 37);
             this.flowLayoutPanel1.TabIndex = 5;
             // 
             // label1
@@ -105,7 +108,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Location = new System.Drawing.Point(3, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 7;
@@ -131,21 +134,23 @@
             this.tableLayoutPanel1.Controls.Add(this.GlobalRB, 6, 0);
             this.tableLayoutPanel1.Controls.Add(this.EffectiveRB, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.arrows1, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(107, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(107, 4);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(668, 23);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(782, 29);
             this.tableLayoutPanel1.TabIndex = 8;
             // 
             // DistributedRB
             // 
             this.DistributedRB.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.DistributedRB.AutoSize = true;
-            this.DistributedRB.Location = new System.Drawing.Point(297, 3);
+            this.DistributedRB.Location = new System.Drawing.Point(339, 4);
+            this.DistributedRB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.DistributedRB.Name = "DistributedRB";
-            this.DistributedRB.Size = new System.Drawing.Size(190, 17);
+            this.DistributedRB.Size = new System.Drawing.Size(226, 21);
             this.DistributedRB.TabIndex = 12;
             this.DistributedRB.Text = "Distributed with current repository";
             this.DistributedRB.UseVisualStyleBackColor = true;
@@ -154,9 +159,9 @@
             // 
             this.arrow3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.arrow3.AutoSize = true;
-            this.arrow3.Location = new System.Drawing.Point(493, 5);
+            this.arrow3.Location = new System.Drawing.Point(571, 6);
             this.arrow3.Name = "arrow3";
-            this.arrow3.Size = new System.Drawing.Size(23, 13);
+            this.arrow3.Size = new System.Drawing.Size(26, 17);
             this.arrow3.TabIndex = 11;
             this.arrow3.Text = "<<";
             // 
@@ -164,9 +169,9 @@
             // 
             this.arrows2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.arrows2.AutoSize = true;
-            this.arrows2.Location = new System.Drawing.Point(268, 5);
+            this.arrows2.Location = new System.Drawing.Point(307, 6);
             this.arrows2.Name = "arrows2";
-            this.arrows2.Size = new System.Drawing.Size(23, 13);
+            this.arrows2.Size = new System.Drawing.Size(26, 17);
             this.arrows2.TabIndex = 10;
             this.arrows2.Text = "<<";
             // 
@@ -174,9 +179,10 @@
             // 
             this.LocalRB.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.LocalRB.AutoSize = true;
-            this.LocalRB.Location = new System.Drawing.Point(106, 3);
+            this.LocalRB.Location = new System.Drawing.Point(115, 4);
+            this.LocalRB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.LocalRB.Name = "LocalRB";
-            this.LocalRB.Size = new System.Drawing.Size(156, 17);
+            this.LocalRB.Size = new System.Drawing.Size(186, 21);
             this.LocalRB.TabIndex = 9;
             this.LocalRB.Text = "Local for current repository";
             this.LocalRB.UseVisualStyleBackColor = true;
@@ -185,9 +191,10 @@
             // 
             this.GlobalRB.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.GlobalRB.AutoSize = true;
-            this.GlobalRB.Location = new System.Drawing.Point(522, 3);
+            this.GlobalRB.Location = new System.Drawing.Point(603, 4);
+            this.GlobalRB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.GlobalRB.Name = "GlobalRB";
-            this.GlobalRB.Size = new System.Drawing.Size(143, 17);
+            this.GlobalRB.Size = new System.Drawing.Size(176, 21);
             this.GlobalRB.TabIndex = 8;
             this.GlobalRB.Text = "Global for all repositories";
             this.GlobalRB.UseVisualStyleBackColor = true;
@@ -198,9 +205,10 @@
             this.EffectiveRB.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.EffectiveRB.AutoSize = true;
             this.EffectiveRB.Checked = true;
-            this.EffectiveRB.Location = new System.Drawing.Point(3, 3);
+            this.EffectiveRB.Location = new System.Drawing.Point(3, 4);
+            this.EffectiveRB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.EffectiveRB.Name = "EffectiveRB";
-            this.EffectiveRB.Size = new System.Drawing.Size(68, 17);
+            this.EffectiveRB.Size = new System.Drawing.Size(74, 21);
             this.EffectiveRB.TabIndex = 1;
             this.EffectiveRB.TabStop = true;
             this.EffectiveRB.Text = "Effective";
@@ -210,29 +218,30 @@
             // 
             this.arrows1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.arrows1.AutoSize = true;
-            this.arrows1.Location = new System.Drawing.Point(77, 5);
+            this.arrows1.Location = new System.Drawing.Point(83, 6);
             this.arrows1.Name = "arrows1";
-            this.arrows1.Size = new System.Drawing.Size(23, 13);
+            this.arrows1.Size = new System.Drawing.Size(26, 17);
             this.arrows1.TabIndex = 3;
             this.arrows1.Text = "<<";
             // 
             // settingsPagePanel
             // 
             this.settingsPagePanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.settingsPagePanel.Location = new System.Drawing.Point(0, 59);
+            this.settingsPagePanel.Location = new System.Drawing.Point(0, 77);
+            this.settingsPagePanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.settingsPagePanel.Name = "settingsPagePanel";
-            this.settingsPagePanel.Size = new System.Drawing.Size(797, 373);
+            this.settingsPagePanel.Size = new System.Drawing.Size(930, 488);
             this.settingsPagePanel.TabIndex = 3;
             // 
             // SettingsPageHeader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.Controls.Add(this.settingsPagePanel);
             this.Controls.Add(this.HeaderPanel);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SettingsPageHeader";
-            this.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
-            this.Size = new System.Drawing.Size(797, 432);
+            this.Padding = new System.Windows.Forms.Padding(0, 4, 0, 0);
+            this.Size = new System.Drawing.Size(930, 565);
             this.HeaderPanel.ResumeLayout(false);
             this.HeaderPanel.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
