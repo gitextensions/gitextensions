cc02f2bdeace21d1083ef4e831cc1ff9bec03246
australiensun <aepape@gmx.de>
2013-06-14 18:34:27 +0200

    add ReleaseNotesGenerator to Unit tests

---
+++ Plugins/ReleaseNotesGenerator/MainFixture.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ReleaseNotesGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+
+namespace GitExtensionsTest.GitUI
+{
+    [TestFixture]
+    public class ReleaseNoteGeneratorPluginFixture
+    {
+        [Test]
+        public void FormatStringTest()
+        {
+            HtmlFragment.To8DigitString(15).Should().Be("00000015");
+        }
+
+        [Test]
+        [Ignore("Modifies clipboard content / Thread need STA mode")]
+        public void CopyToClipboard()
+        {
+            HtmlFragment.CopyToClipboard("<p>Hallo</p>");
+        }
+    }
+}
