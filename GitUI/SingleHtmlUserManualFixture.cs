cd51e3b0df17b44b84aab3ff36c9de05796145a4
Max Malook <max@malook.de>
2013-06-20 00:03:54 +0200

    update tests recent code changes

---
+++ GitUI/SingleHtmlUserManualFixture.cs
@@ -0,0 +1,19 @@
+﻿using GitUI.UserManual;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace GitExtensionsTest.GitUI
+{
+    [TestFixture]
+    public class SingleHtmlUserManualFixture
+    {
+        [TestCase(null, "file:///D:/data2/projects/gitextensions/GitExtensionsDoc/build/singlehtml/index.html")]
+        [TestCase("merge-conflicts", "file:///D:/data2/projects/gitextensions/GitExtensionsDoc/build/singlehtml/index.html#merge-conflicts")]
+        public void GetUrl(string anchor, string expected)
+        {
+            var sut = new SingleHtmlUserManual(anchor);
+
+            sut.GetUrl().Should().Be(expected);
+        }
+    }
+}
---
+++ GitUI/StandardHtmlUserManualFixture.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using GitUI.UserManual;
+using NUnit.Framework;
+
+namespace GitExtensionsTest.GitUI
+{
+    [TestFixture]
+    public class StandardHtmlUserManualFixture
+    {
+        [TestCase(null, null, "https://gitextensions.readthedocs.org/en/latest//")] // both null makes no sense atm
+        [TestCase("merge_conflicts", null, "https://gitextensions.readthedocs.org/en/latest/merge_conflicts/")]
+        [TestCase("merge_conflicts", "merge-conflicts", "https://gitextensions.readthedocs.org/en/latest/merge_conflicts/#merge-conflicts")]
+        public void GetUrl(string subFolder, string anchor, string expected)
+        {
+            var sut = new StandardHtmlUserManual(subFolder, anchor);
+
+            sut.GetUrl().Should().Be(expected);
+        }
+    }
+}
