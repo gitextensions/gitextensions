24213c25cf67852d0ae0d5dfc075ce8a87c5c029
australiensun <aepape@gmx.de>
2013-06-24 00:52:26 +0200

    Add "Clean working tree..." to context menu of FileTree tab

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -2472,8 +2472,14 @@ namespace GitUI.CommandsDialogs
                 return;
             }
 
-            var gitItem = (GitItem)GitTree.SelectedNode.Tag; // this should not fail, if it still does, user should know
+            var gitItem = (GitItem)GitTree.SelectedNode.Tag;
             UICommands.StartArchiveDialog(this, selectedRevisions.First(), gitItem.FileName);
+        }
+
+        private void fileTreeCleanWorkingTreeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var gitItem = (GitItem)GitTree.SelectedNode.Tag;
+            UICommands.StartCleanupRepositoryDialog(this, gitItem.FileName + "/"); // the trailing / marks a directory
         }
 
         private void DiffContextMenu_Opening(object sender, System.ComponentModel.CancelEventArgs e)
