0218a495fad4edbe17f28c206b37e38095562239
Cahyadi Hendrawan <chen.hendrawan@gmail.com>
2013-06-25 16:03:35 +0800

    Plugin to create pull request for repository hosted in Atlassian Stash.

---
+++ Plugins/Stash/GetUserRequest.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace Stash
+{
+    public class StashUser
+    {
+        public string Slug { get; set; }
+    }
+
+    class GetUserRequest : StashRequestBase
+    {
+        public GetUserRequest(Settings settings)
+            : base(settings)
+        {
+        }
+
+        protected override void WriteRequestBody(HttpWebRequest request)
+        {
+            //do nothing
+        }
+
+        protected override string ApiUrl
+        {
+            get { return "/rest/api/1.0/users?limit=1000"; }
+        }
+
+        protected override string RequestMethod
+        {
+            get { return "GET"; }
+        }
+
+
+    }
+}
