b10de70685ed805d681936e3253e9bd8ca9d12a5
Arkadiy Shapkin <ashapkin@artec-group.com>
2013-08-14 18:56:28 +0400

    Ignore not existed ssh files

--- GitCommands/PathUtil.cs
+++ GitCommands/PathUtil.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using GitCommands.Utils;
 
 namespace GitCommands
 {
-    class PathUtil
+    public static class PathUtil
     {
         /// <summary>
         /// Code guideline: "A directory path should always end with / or \.
@@ -21,6 +23,38 @@ namespace GitCommands
             }
 
             return dirPath;
+        }
+
+        public static bool Equal(string path1, string path2)
+        {
+            path1 = Path.GetFullPath(path1).TrimEnd('\\');
+            path2 = Path.GetFullPath(path2).TrimEnd('\\');
+            StringComparison comprasion = EnvUtils.RunningOnUnix()
+                                              ? StringComparison.InvariantCulture
+                                              : StringComparison.InvariantCultureIgnoreCase;
+
+            return String.Compare(path1, path2, comprasion) == 0;
+        }
+
+        private class PathEqualityComparer : IEqualityComparer<string>
+        {
+            public bool Equals(string path1, string path2)
+            {
+                return Equal(path1, path2);
+            }
+
+            public int GetHashCode(string path)
+            {
+                path = Path.GetFullPath(path).TrimEnd('\\');
+                if (!EnvUtils.RunningOnUnix())
+                    path = path.ToLower();
+                return path.GetHashCode();
+            }
+        }
+
+        public static IEqualityComparer<string> CreatePathEqualityComparer()
+        {
+            return new PathEqualityComparer();
         }
     }
 }
