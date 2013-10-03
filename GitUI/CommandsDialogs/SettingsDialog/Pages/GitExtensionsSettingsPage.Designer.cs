4a64264a8a69d35005b860571bedf3e2a5163221
Ulrich Buchgraber <ulrich.b@gmx.at>
2013-10-03 23:36:25 +0200

    'AutoScroll = true' for GitExtensionsSettingsPage.

--- GitUI/CommandsDialogs/SettingsDialog/Pages/GitExtensionsSettingsPage.Designer.cs
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/GitExtensionsSettingsPage.Designer.cs
@@ -481,6 +481,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox12);
             this.Controls.Add(this.groupBox11);
