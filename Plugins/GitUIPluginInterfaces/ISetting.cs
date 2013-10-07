eae24c3cf2491417ee31d156edfac23b253ed227
Janusz Białobrzewski <jbialobr@o2.pl>
2013-10-07 23:08:55 +0200

    Settings controls draft

---
+++ Plugins/GitUIPluginInterfaces/ISetting.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUIPluginInterfaces
+{
+    public interface ISetting
+    {
+        /// <summary>
+        /// Name of the setting
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Caption of the setting
+        /// </summary>
+        string Caption { get; }
+
+        ISettingControlBinding CreateControlBinding();
+    }
+
+    public interface ISettingControlBinding
+    {
+        /// <summary>
+        /// Creates a control to be placed on FormSettings to edit this setting value
+        /// Control should take care of scalability and resizability of its subcontrols
+        /// </summary>
+        /// <returns></returns>
+        Control GetControl();
+
+        /// <summary>
+        /// Loads setting value from settings to Control
+        /// </summary>
+        /// <param name="settings"></param>
+        void LoadSetting(ISettingsSource settings);
+
+        /// <summary>
+        /// Saves value from Control to settings
+        /// </summary>
+        /// <param name="settings"></param>
+        void SaveSetting(ISettingsSource settings);
+    }
+
+    public abstract class SettingControlBinding<T> : ISettingControlBinding where T : Control
+    {
+        private T _control;
+        private T Control
+        {
+            get
+            {
+                if (_control == null)
+                    throw new NullReferenceException("Control");
+
+                return _control;
+            }
+        }
+
+        public Control GetControl()
+        {
+            if (_control == null)
+                _control = CreateControl();
+
+            return _control;
+        }
+
+        public void LoadSetting(ISettingsSource settings)
+        {
+            LoadSetting(settings, Control);
+        }
+
+        /// <summary>
+        /// Saves value from Control to settings
+        /// </summary>
+        /// <param name="settings"></param>
+        void SaveSetting(ISettingsSource settings)
+        {
+            SaveSetting(settings, Control);
+        }
+
+
+        /// <summary>
+        /// Creates a control to be placed on FormSettings to edit this setting value
+        /// Control should take care of scalability and resizability of its subcontrols
+        /// </summary>
+        /// <returns></returns>
+        abstract T CreateControl();
+
+        /// <summary>
+        /// Loads setting value from settings to Control
+        /// </summary>
+        /// <param name="settings"></param>
+        abstract void LoadSetting(ISettingsSource settings, T control);
+
+        /// <summary>
+        /// Saves value from Control to settings
+        /// </summary>
+        /// <param name="settings"></param>
+        abstract void SaveSetting(ISettingsSource settings, T control);
+
+
+    }
+
+}
---
+++ Plugins/GitUIPluginInterfaces/Settings/BoolSetting.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitUIPluginInterfaces
+{
+    public class BoolSetting: ISetting
+    {
+        public BoolSetting(string aName, bool? aDefaultValue)
+            : this(aName, aName, aDefaultValue)
+        {
+        }
+
+        public BoolSetting(string aName, string aCaption, bool? aDefaultValue)
+        {
+            Name = aName;
+            Caption = aCaption;
+            DefaultValue = aDefaultValue;
+        }
+
+        public string Name { get; private set; }
+        public string Caption { get; private set; }
+        public bool? DefaultValue { get; set; }
+
+        public ISettingControlBinding CreateControlBinding()
+        {
+            return new CheckBoxBinding(this);
+        }
+
+        private class CheckBoxBinding : SettingControlBinding<CheckBox>
+        {
+
+            BoolSetting Setting;
+
+            public CheckBoxBinding(BoolSetting aSetting)
+            {
+                Setting = aSetting;
+            }
+
+            public override CheckBox CreateControl()
+            {
+                return new CheckBox();
+            }
+
+            public override void LoadSetting(ISettingsSource settings, CheckBox control)
+            {
+                //TODO handle three states
+                Setting[settings] = control.Checked;
+            }
+
+            public void SaveSetting(ISettingsSource settings, CheckBox control)
+            {
+                //TODO handle three states
+                control.Checked = Setting[settings].Value;
+            }
+        }
+
+        public bool? this[ISettingsSource settings]
+        {
+            get 
+            {
+                return settings.GetBool(Name, DefaultValue);
+            }
+
+            set 
+            {
+                settings.SetBool(Name, value);
+            }
+        }
+    }
+}
