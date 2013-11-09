0be5f79b7f98609edc548c6197745a1bc7626641
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-11-10 01:49:50 +0400

    Fixed issue #1908: GUI bug in "Reset changes" dialog

--- GitUI/CommandsDialogs/FormResetChanges.Designer.cs
+++ GitUI/CommandsDialogs/FormResetChanges.Designer.cs
@@ -33,23 +33,27 @@
             this.btnCancel = new System.Windows.Forms.Button();
             this.cbDeleteNewFilesAndDirectories = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Location = new System.Drawing.Point(11, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(246, 15);
+            this.label1.Size = new System.Drawing.Size(307, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Are you sure you want to reset your changes?";
             // 
             // btnReset
             // 
             this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnReset.Location = new System.Drawing.Point(272, 66);
+            this.btnReset.Location = new System.Drawing.Point(266, 4);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(81, 25);
+            this.btnReset.Size = new System.Drawing.Size(101, 31);
             this.btnReset.TabIndex = 1;
             this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = true;
@@ -59,21 +63,22 @@
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(359, 66);
+            this.btnCancel.Location = new System.Drawing.Point(375, 4);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 25);
+            this.btnCancel.Size = new System.Drawing.Size(94, 31);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // cbDeleteNewFiles
+            // cbDeleteNewFilesAndDirectories
             // 
-            this.cbDeleteNewFilesAndDirectories.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.cbDeleteNewFilesAndDirectories.AutoSize = true;
-            this.cbDeleteNewFilesAndDirectories.Location = new System.Drawing.Point(12, 66);
-            this.cbDeleteNewFilesAndDirectories.Name = "cbDeleteNewFiles";
-            this.cbDeleteNewFilesAndDirectories.Size = new System.Drawing.Size(230, 19);
+            this.cbDeleteNewFilesAndDirectories.Location = new System.Drawing.Point(13, 72);
+            this.cbDeleteNewFilesAndDirectories.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbDeleteNewFilesAndDirectories.Name = "cbDeleteNewFilesAndDirectories";
+            this.cbDeleteNewFilesAndDirectories.Size = new System.Drawing.Size(291, 24);
             this.cbDeleteNewFilesAndDirectories.TabIndex = 3;
             this.cbDeleteNewFilesAndDirectories.Text = "Also delete new files and/or directories";
             this.cbDeleteNewFilesAndDirectories.UseVisualStyleBackColor = true;
@@ -81,30 +86,44 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 24);
+            this.label2.Location = new System.Drawing.Point(11, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 15);
+            this.label2.Size = new System.Drawing.Size(267, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "This will delete any uncommitted work.";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.btnCancel);
+            this.flowLayoutPanel1.Controls.Add(this.btnReset);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 105);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(473, 39);
+            this.flowLayoutPanel1.TabIndex = 5;
             // 
             // FormResetChanges
             // 
             this.AcceptButton = this.btnCancel;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(446, 101);
+            this.ClientSize = new System.Drawing.Size(473, 144);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cbDeleteNewFilesAndDirectories);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnReset);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormResetChanges";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Reset changes";
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,5 +136,6 @@
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.CheckBox cbDeleteNewFilesAndDirectories;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
