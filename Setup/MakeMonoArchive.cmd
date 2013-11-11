7d021adc22b7f3f24aae3e0aeb0e98f467cb90cf
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2013-11-11 18:54:05 +0400

    Version changed to 2.47.1

--- Setup/MakeMonoArchive.cmd
+++ Setup/MakeMonoArchive.cmd
@@ -6,7 +6,7 @@ rem
 rem Update this version number with every release
 rem
 setlocal
-set version=2.47
+set version=2.47.01
 set zipversion=%version:.=%
 set normal=GitExtensions%zipversion%Mono.zip
 set szip="..\packages\7-Zip.CommandLine.9.20.0\tools\7za"
