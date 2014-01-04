5e4263b1d04e156cbeb2803ab869ef419c751ebc
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2014-01-05 03:29:46 +0400

    Git, KDiff removed from repository

---
+++ Setup/DownloadExternals.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+IF NOT EXIST "%~p0\..\bin\Git-1.8.5.2-preview20131230.exe" (
+    "%~p0\tools\curl.exe" -o ..\bin\Git-1.8.5.2-preview20131230.exe http://msysgit.googlecode.com/files/Git-1.8.5.2-preview20131230.exe
+)
+
+IF NOT EXIST "%~p0\..\bin\KDiff3-32bit-Setup_0.9.97.exe" (
+    "%~p0\tools\curl.exe" -o ..\bin\KDiff3-32bit-Setup_0.9.97.exe http://sourceforge.net/projects/kdiff3/files/kdiff3/0.9.97/KDiff3-32bit-Setup_0.9.97.exe/download -L http://sourceforge.net/ > NUL
+)
