73e85e7d1594754561f3556ffa96c608cfb47767
Arkady Shapkin <arkadiy_s@inbox.ru>
2014-02-23 16:04:22 +0400

    MsysGit updated to version 1.9.0

--- Setup/DownloadExternals.cmd
+++ Setup/DownloadExternals.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-IF NOT EXIST "%~p0\..\bin\Git-1.8.5.2-preview20131230.exe" (
-    "%~p0\tools\curl.exe" -o ..\bin\Git-1.8.5.2-preview20131230.exe http://msysgit.googlecode.com/files/Git-1.8.5.2-preview20131230.exe
+IF NOT EXIST "%~p0\..\bin\Git-1.9.0-preview20140217.exe" (
+    "%~p0\tools\curl.exe" -o ..\bin\Git-1.9.0-preview20140217.exe http://msysgit.googlecode.com/files/Git-1.9.0-preview20140217.exe
 )
 
 IF NOT EXIST "%~p0\..\bin\KDiff3-32bit-Setup_0.9.97.exe" (
