9a4311a7bbd76a90b283061b6185cab6b5cf03f7
Christian Schuster <christian.schuster@rubicon.eu>
2014-05-05 16:43:21 +0200

    Implemented auto completion for commit message window.

---
+++ GitUI/CommandsDialogs/CommitDialog/AutoCompleteRegexProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace GitUI.CommandsDialogs.CommitDialog
+{
+    public static class AutoCompleteRegexProvider
+    {
+        private static readonly Dictionary<string, Regex> _regexes = new Dictionary<string, Regex>(); 
+
+        public static void Initialize ()
+        {
+            foreach (var line in File.ReadLines ("AutoCompleteRegexes.txt"))
+            {
+                var i = line.IndexOf('=');
+                var extensionStr = line.Substring(0, i);
+                var regexStr = line.Substring(i + 1).Trim();
+
+                var extensions = extensionStr.Split(',').Select(s => s.Trim()).Distinct();
+                var regex = new Regex(regexStr, RegexOptions.Compiled);
+
+                foreach (var extension in extensions)
+                    _regexes.Add(extension, regex);
+            }
+        }
+
+        public static Regex GetRegexForExtension (string extension)
+        {
+            return _regexes.ContainsKey(extension) ? _regexes[extension] : null;
+        }
+    }
+}
