042b0f101016180f7fd334250424bdf54f905c80
Linquize <linquize@yahoo.com.hk>
2014-05-29 23:53:45 +0800

    Introduce GitCmdResult struct, RunCmdResult(), RunGitCmdResult() methods

---
+++ Plugins/GitUIPluginInterfaces/GitCmdResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace GitUIPluginInterfaces
+{
+    public struct CmdResult
+    {
+        public string StdOutput;
+        public string StdError;
+        public int ExitCode;
+
+        public string GetString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (this.StdOutput != null && this.StdOutput.Length > 0)
+                sb.Append(this.StdOutput);
+            if (this.StdError != null && this.StdError.Length > 0 && this.StdOutput != null && this.StdOutput.Length > 0)
+                sb.AppendLine();
+            if (this.StdError != null && this.StdError.Length > 0)
+                sb.Append(this.StdError);
+            return sb.ToString();
+        }
+
+    }
+}
