3b3fcab1327ee2524f0b735c309dbf5a8dcf89bd
Arkady Shapkin <arkadiy_s@inbox.ru>
2014-06-01 16:39:06 +0400

    BuildInstallers.CodeBetter.cmd added

---
+++ Setup/BuildInstallers.CodeBetter.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+cd /d "%~p0"
+
+set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+set project=..\GitExtensions.VS2012.sln
+set projectShellEx=..\GitExtensionsShellEx\GitExtensionsShellEx.VS2012.sln
+set projectSshAskPass=..\GitExtSshAskPass\GitExtSshAskPass.VS2012.sln
+set nuget=..\.nuget\nuget.exe
+set SkipShellExtRegistration=1
+set EnableNuGetPackageRestore=true
+
+set msbuildparams=/p:Configuration=Release /t:Rebuild /nologo /v:m
+
+%nuget% install ..\GitUI\packages.config -OutputDirectory ..\packages -Source https://nuget.org/api/v2/
+%nuget% install ..\Plugins\BackgroundFetch\packages.config -OutputDirectory ..\packages -Source https://nuget.org/api/v2/
+%nuget% install ..\Plugins\BuildServerIntegration\TeamCityIntegration\packages.config -OutputDirectory ..\packages -Source https://nuget.org/api/v2/
+
+%msbuild% %project% /p:Platform="Any CPU" %msbuildparams%
+IF ERRORLEVEL 1 EXIT /B 1
+%msbuild% %projectShellEx% /p:Platform=Win32 /p:PlatformToolset=v110 %msbuildparams%
+IF ERRORLEVEL 1 EXIT /B 1
+%msbuild% %projectShellEx% /p:Platform=x64 /p:PlatformToolset=v110 %msbuildparams%
+IF ERRORLEVEL 1 EXIT /B 1
+%msbuild% %projectSshAskPass% /p:Platform=Win32 /p:PlatformToolset=v110 %msbuildparams%
+IF ERRORLEVEL 1 EXIT /B 1
+
+%msbuild% %project% /p:Platform="Any CPU" /p:DefineConstants=__MonoCS__ %msbuildparams%
+IF ERRORLEVEL 1 EXIT /B 1
+
+echo.
+pause
