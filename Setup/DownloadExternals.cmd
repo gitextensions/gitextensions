f0101b6b0e3879c22c2efa2b8959c6e3041de054
Arkady Shapkin <arkadiy_s@inbox.ru>
2014-07-22 21:54:50 +0400

    Installer updated
    git-credential-winstore now optional product
    git-credential-winstore update to version 1.4 beta
    KDiff updated to version 0.9.98

--- Setup/DownloadExternals.cmd
+++ Setup/DownloadExternals.cmd
@@ -7,7 +7,7 @@ IF NOT EXIST "%~p0\..\bin\Git-1.9.4-preview20140611.exe" (
     IF ERRORLEVEL 1 EXIT /B 1
 )
 
-IF NOT EXIST "%~p0\..\bin\KDiff3-32bit-Setup_0.9.97.exe" (
-    "%~p0\tools\curl.exe" -L -k -o ..\bin\KDiff3-32bit-Setup_0.9.97.exe http://sourceforge.net/projects/kdiff3/files/kdiff3/0.9.97/KDiff3-32bit-Setup_0.9.97.exe/download -L http://sourceforge.net/ > NUL
+IF NOT EXIST "%~p0\..\bin\KDiff3-32bit-Setup_0.9.98-3.exe" (
+    "%~p0\tools\curl.exe" -L -k -o ..\bin\KDiff3-32bit-Setup_0.9.98-3.exe https://sourceforge.net/projects/kdiff3/files/kdiff3/0.9.98/KDiff3-32bit-Setup_0.9.98-3.exe/download# -L http://sourceforge.net/ > NUL
     IF ERRORLEVEL 1 EXIT /B 1
 )
