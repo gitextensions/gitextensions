9932f32b162aad1088323a6a09a5c99f01cb0f46
Arkady Shapkin <arkadiy_s@inbox.ru>
2014-08-20 00:44:45 +0400

    Version updated

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,6 +19,6 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.48")]
-[assembly: AssemblyFileVersion("2.48")]
-[assembly: AssemblyInformationalVersion("2.48RC1")]
+[assembly: AssemblyVersion("2.48.01")]
+[assembly: AssemblyFileVersion("2.48.01")]
+[assembly: AssemblyInformationalVersion("2.48")]
