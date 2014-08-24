d3677727035bae06480a1601350322491e3f5528
Arkady Shapkin <arkadiy_s@inbox.ru>
2014-08-24 23:11:37 +0400

    GitExtensionsTest moved from submodule

---
+++ GitExtensionsTest/GitUI/FormUpdateFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using GitCommands.Config;
+
+namespace GitExtensionsTest.GitUI
+{
+    [TestFixture]
+    public class FormUpdateFixture
+    {
+        private string GetReleasesConfigFileText()
+        {
+            ConfigFile configFile = new ConfigFile("", true);
+            configFile.SetValue("Version \"2.47\".ReleaseType", "Major");
+            configFile.SetValue("Version \"2.48\".ReleaseType", "Major");
+            configFile.SetValue("Version \"2.49\".ReleaseType", "ReleaseCandidate");
+            configFile.SetValue("RCVersion \"2.50\".ReleaseType", "ReleaseCandidate");
+
+            return configFile.GetAsString();
+        }
+    }
+}
