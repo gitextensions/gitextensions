50909a507ae4b2cd320ba8afc300b9a996332481
Arkady Shapkin <arkadiy_s@inbox.ru>
2014-08-24 23:11:37 +0400

    GitExtensionsTest moved from submodule

---
+++ GitExtensionsTest/Translation/TranslationTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using GitUI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ResourceManager;
+using ResourceManager.Xliff;
+
+namespace GitExtensionsTest.TranslationTest
+{
+    [TestClass]
+    public class TranslationTest
+    {
+
+        [TestMethod]
+        [STAThread]
+        public void CreateInstanceOfClass()
+        {
+            // just reference to GitUI
+            MouseWheelRedirector.Active = true;
+
+            List<Type> translatableTypes = TranslationUtl.GetTranslatableTypes();
+
+            var testTranslation = new Translation();
+
+            foreach (Type type in translatableTypes)
+            {
+                try
+                {
+                    ITranslate obj = TranslationUtl.CreateInstanceOfClass(type) as ITranslate;
+                    obj.AddTranslationItems(testTranslation);
+                    obj.TranslateItems(testTranslation);
+                }
+                catch (System.Exception)
+                {
+                    Trace.WriteLine("Problem with class: " + type.FullName);
+                    throw;
+                }
+            }
+        }       
+    }
+}
