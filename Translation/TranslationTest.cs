e34dbbb6fcf2366a3fe3fe868e2a7166d81e2bec
Arkady Shapkin <arkadiy_s@inbox.ru>
2014-08-31 21:44:57 +0400

    Squashed 'GitExtensionsTest/' content from commit 08bd3c1
    git-subtree-dir: GitExtensionsTest
    git-subtree-split: 08bd3c1e5f94750d59368a2e09a3994432108e58

---
+++ Translation/TranslationTest.cs
@@ -0,0 +1,50 @@
+﻿#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+
+#endif
+using GitUI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ResourceManager.Translation;
+
+
+namespace GitExtensionsTest.Translation
+{
+    [TestClass]
+    public class TranslationTest
+    {
+
+        [TestMethod]
+        [STAThread]
+        public void CreateInstanceOfClass()
+        {
+            // just reference to GitUI
+            MouseWheelRedirector.Active = true;
+
+            List<Type> translatableTypes = TranslationUtl.GetTranslatableTypes();
+
+            ResourceManager.Translation.Translation testTranslation = new ResourceManager.Translation.Translation();
+
+            foreach (Type type in translatableTypes)
+            {
+                try
+                {
+                    ITranslate obj = TranslationUtl.CreateInstanceOfClass(type) as ITranslate;
+                    obj.AddTranslationItems(testTranslation);
+                    obj.TranslateItems(testTranslation);
+                }
+                catch (System.Exception)
+                {
+                    Trace.WriteLine("Problem with class: " + type.FullName);
+                    throw;
+                }
+            }
+        }       
+    }
+}
