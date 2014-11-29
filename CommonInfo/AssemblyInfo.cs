987ebee48a89cf241f423bea19c733689b000389
Jay Asbury <vbjay.net@gmail.com>
2014-11-29 16:07:13 -0500

    Create CommonInfo Project

---
+++ CommonInfo/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CommonInfo")]
+[assembly: AssemblyDescription("This project generates the git hash info for the CommonGitInfo.cs file.")]
