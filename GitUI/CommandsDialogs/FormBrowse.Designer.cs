6412285405feec2a2a90dec23b477beb5abe38b3
Philippe Miossec <pmiossec@gmail.com>
2014-12-10 00:30:45 +0100

    Enlarge branch dropdown to see entire branch names

--- GitUI/CommandsDialogs/FormBrowse.Designer.cs
+++ GitUI/CommandsDialogs/FormBrowse.Designer.cs
@@ -568,6 +568,7 @@ namespace GitUI.CommandsDialogs
             // 
             this.toolStripBranches.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.toolStripBranches.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.toolStripBranches.DropDownWidth = 300;
             this.toolStripBranches.Name = "toolStripBranches";
             this.toolStripBranches.Size = new System.Drawing.Size(186, 25);
             // 
