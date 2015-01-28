16a177ddeb9aa101800af0ecfbb7bbfbbde74a48
Arkady Shapkin <arkadiy_s@inbox.ru>
2015-01-29 02:12:22 +0300

    Version changed to 2.49a1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,6 +19,6 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.48.04")]
-[assembly: AssemblyFileVersion("2.48.04")]
-[assembly: AssemblyInformationalVersion("2.48.04")]
+[assembly: AssemblyVersion("2.49.00")]
+[assembly: AssemblyFileVersion("2.49.00")]
+[assembly: AssemblyInformationalVersion("2.49a1")]
