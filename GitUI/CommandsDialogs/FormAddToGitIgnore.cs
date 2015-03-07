117debadc1b5fc3451c712b297c5c986ff44918f
Janusz Białobrzewski <jbialobr@o2.pl>
2015-03-07 10:55:26 +0100

    Replace background thread with AsyncLoader.

--- GitUI/CommandsDialogs/FormAddToGitIgnore.cs
+++ GitUI/CommandsDialogs/FormAddToGitIgnore.cs
@@ -15,67 +15,17 @@ namespace GitUI.CommandsDialogs
         private readonly TranslationString _matchingFilesString = new TranslationString("{0} file(s) matched");
         private readonly TranslationString _updateStatusString = new TranslationString("Updating ...");
 
-        private readonly ManualResetEvent _notifyThread;
-        private volatile bool _stopUpdateThread;
-        private volatile string[] _patterns;
+        private readonly AsyncLoader _ignoredFilesLoader;
 
         public FormAddToGitIgnore(GitUICommands aCommands, params string[] filePatterns)
             : base(aCommands)
         {
             InitializeComponent();
+            _ignoredFilesLoader = new AsyncLoader();
             Translate();
-
-            _notifyThread = new ManualResetEvent(false);
-
-            Thread thread = new Thread(UpdateIgnoreItemsThreadProc);
-            thread.IsBackground = true;
-
-            // when form is closed - notify thread for stop activity and wait thread finish work
-            FormClosed += (sender, args) => {
-                _stopUpdateThread = true;
-                _notifyThread.Set();
-                thread.Join();
-                _notifyThread.Close();
-            };
-
-            thread.Start();
 
             if (filePatterns != null)
                 FilePattern.Text = string.Join(Environment.NewLine, filePatterns);
-        }
-
-        void UpdateIgnoreItemsThreadProc()
-        {
-            while (true)
-            {
-                _notifyThread.WaitOne();
-                _notifyThread.Reset();
-
-                // check if requested quit
-                if (_stopUpdateThread)
-                    break;
-
-                var ignoredFiles = Module.GetIgnoredFiles(_patterns);
-
-                // check quit condition after each long work
-                if (_stopUpdateThread)
-                    break;
-
-                // if client request one more refresh - do not update UI and go one more cycle
-                if (_notifyThread.WaitOne(0))
-                    continue;
-
-                // finally - update UI
-                GitUIExtensions.UISynchronizationContext.Post(state => UpdatePreviewPanel((IList<string>)state), ignoredFiles);
-            }
-        }
-
-        protected override void OnRuntimeLoad(EventArgs e)
-        {
-            base.OnRuntimeLoad(e);
-
-            // refresh on start
-            _notifyThread.Set();
         }
 
         private void AddToIngoreClick(object sender, EventArgs e)
@@ -119,10 +69,6 @@ namespace GitUI.CommandsDialogs
 
         private void UpdatePreviewPanel(IList<string> ignoredFiles)
         {
-            // thread can call this method after from closed. prevent access to disposed controls
-            if (_stopUpdateThread)
-                return;
-
             _NO_TRANSLATE_Preview.DataSource = ignoredFiles;
             _NO_TRANSLATE_filesWillBeIgnored.Text = string.Format(_matchingFilesString.Text, _NO_TRANSLATE_Preview.Items.Count);
             _NO_TRANSLATE_Preview.Enabled = true;
@@ -136,13 +82,15 @@ namespace GitUI.CommandsDialogs
 
         private void FilePattern_TextChanged(object sender, EventArgs e)
         {
-            _NO_TRANSLATE_filesWillBeIgnored.Text = _updateStatusString.Text;
-            _NO_TRANSLATE_Preview.DataSource = new List<string> { _updateStatusString.Text };
-            _NO_TRANSLATE_Preview.Enabled = false;
+            _ignoredFilesLoader.Cancel();
+            if (_NO_TRANSLATE_Preview.Enabled)
+            {
+                _NO_TRANSLATE_filesWillBeIgnored.Text = _updateStatusString.Text;
+                _NO_TRANSLATE_Preview.DataSource = new List<string> { _updateStatusString.Text };
+                _NO_TRANSLATE_Preview.Enabled = false;
+            }
 
-            _patterns = GetCurrentPatterns().ToArray();
-
-            _notifyThread.Set();
+            _ignoredFilesLoader.Load(() => Module.GetIgnoredFiles(GetCurrentPatterns()), UpdatePreviewPanel);
         }
 
 
