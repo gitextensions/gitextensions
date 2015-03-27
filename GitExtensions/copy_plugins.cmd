5cf073bc43d7f11829f0ea8d8fde37b880ee9856
Michael Naumov <michael.naumov@volia-software.com>
2015-03-27 19:27:36 +0200

    Skip empty TfsInterop.Common folder

--- GitExtensions/copy_plugins.cmd
+++ GitExtensions/copy_plugins.cmd
@@ -4,6 +4,8 @@ md Plugins\
 echo Microsoft.TeamFoundation.WorkItemTracking.Client.DataStoreLoader.dll > exclude.txt
 echo Microsoft.WITDataStore.dll >> exclude.txt
 for /d %%I in (%~p0\..\Plugins\*, %~p0\..\Plugins\Statistics\*, %~p0\..\Plugins\BuildServerIntegration\*) do (
-  xcopy /y /r %%I\bin\%config%\*.dll Plugins\ /EXCLUDE:exclude.txt
-  xcopy /y /r %%I\bin\%config%\*.pdb Plugins\
+  if not "%%I" == "%~p0\..\Plugins\BuildServerIntegration\TfsInterop.Common" (
+    xcopy /y /r %%I\bin\%config%\*.dll Plugins\ /EXCLUDE:exclude.txt
+    xcopy /y /r %%I\bin\%config%\*.pdb Plugins\
+  )
 )
