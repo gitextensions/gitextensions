29def44ae9ef4916c34ebc59af682bd78b3c4b74
Michael Naumov <michael.naumov@volia-software.com>
2015-03-28 15:47:03 +0200

    Fix warning: CA2213 Disposable fields should be disposed    'FormClone' contains field 'FormClone._branchListLoader' that is of IDisposable type: 'AsyncLoader'. Change the Dispose method on 'FormClone' to call Dispose or Close on this field.   GitUI   FormClone.Designer.cs   15

--- GitUI/CommandsDialogs/FormClone.cs
+++ GitUI/CommandsDialogs/FormClone.cs
@@ -304,5 +304,23 @@ namespace GitUI.CommandsDialogs
         {
             LoadBranches();
         }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            _branchListLoader.Cancel();
+
+            if (disposing)
+                _branchListLoader.Dispose();
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
