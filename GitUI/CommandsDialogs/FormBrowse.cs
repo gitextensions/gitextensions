82b6b219616af862de2946423b55ff241aa9492c
Michael Naumov <michael.naumov@volia-software.com>
2015-03-28 17:26:45 +0200

    Fix warning: CA2213 Disposable fields should be disposed    'FormBrowse' contains field 'FormBrowse._formBrowseMenus' that is of IDisposable type: 'FormBrowseMenus'. Change the Dispose method on 'FormBrowse' to call Dispose or Close on this field. GitUI   FormBrowse.cs   3231

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -3234,7 +3234,7 @@ namespace GitUI.CommandsDialogs
                 _submodulesStatusImagesCTS.Dispose();
                 _pushButton.Dispose();
                 _pullButton.Dispose();
-                _commitButton.Dispose();
+                _formBrowseMenus.Dispose();
             }
 
             if (disposing && (components != null))
