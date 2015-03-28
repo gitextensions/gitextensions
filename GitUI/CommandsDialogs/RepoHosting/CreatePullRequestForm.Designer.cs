03cdcbcb619477b6dab1a2fc23e129826607888a
Michael Naumov <michael.naumov@volia-software.com>
2015-03-28 14:34:42 +0200

    Fix warning: CA2213  Disposable fields should be disposed    'CreatePullRequestForm' contains field 'CreatePullRequestForm._remoteLoader' that is of IDisposable type: 'AsyncLoader'. Change the Dispose method on 'CreatePullRequestForm' to call Dispose or Close on this field.   GitUI   CreatePullRequestForm.Designer.cs   15

--- GitUI/CommandsDialogs/RepoHosting/CreatePullRequestForm.Designer.cs
+++ GitUI/CommandsDialogs/RepoHosting/CreatePullRequestForm.Designer.cs
@@ -7,22 +7,6 @@ namespace GitUI.CommandsDialogs.RepoHosting
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-                _remoteLoader.Cancel();
-
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         #region Windows Form Designer generated code
 
         /// <summary>
