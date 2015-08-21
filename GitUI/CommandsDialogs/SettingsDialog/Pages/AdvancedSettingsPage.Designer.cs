6d6658a6a8911ba0b306bc2773f9e84115a458ad
EbenZhang <zhangyingneng@gmail.com>
2015-08-21 22:40:47 +0800

    add the advanced omit uninteresting diff settings ui for diffviewer

--- GitUI/CommandsDialogs/SettingsDialog/Pages/AdvancedSettingsPage.Designer.cs
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/AdvancedSettingsPage.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.DiffViewerGB = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanelForDiffViewer = new System.Windows.Forms.TableLayoutPanel();
+            this.chkOmitUninterestingDiff = new System.Windows.Forms.CheckBox();
+            this.chkRememberIgnoreWhiteSpacePreference = new System.Windows.Forms.CheckBox();
             this.CheckoutGB = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.chkAlwaysShowCheckoutDlg = new System.Windows.Forms.CheckBox();
@@ -38,8 +42,9 @@
             this.chkCheckForRCVersions = new System.Windows.Forms.CheckBox();
             this.chkAlwaysShowAdvOpt = new System.Windows.Forms.CheckBox();
             this.chkDontSHowHelpImages = new System.Windows.Forms.CheckBox();
-            this.chkRememberIgnoreWhiteSpacePreference = new System.Windows.Forms.CheckBox();
             this.tableLayoutPanel2.SuspendLayout();
+            this.DiffViewerGB.SuspendLayout();
+            this.tableLayoutPanelForDiffViewer.SuspendLayout();
             this.CheckoutGB.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             this.GeneralGB.SuspendLayout();
@@ -52,15 +57,69 @@
             this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.DiffViewerGB, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.CheckoutGB, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.GeneralGB, 0, 1);
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 12);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowCount = 3;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(443, 263);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(443, 337);
             this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // DiffViewerGB
+            // 
+            this.DiffViewerGB.AutoSize = true;
+            this.DiffViewerGB.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.DiffViewerGB.Controls.Add(this.tableLayoutPanelForDiffViewer);
+            this.DiffViewerGB.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DiffViewerGB.Location = new System.Drawing.Point(3, 241);
+            this.DiffViewerGB.Name = "DiffViewerGB";
+            this.DiffViewerGB.Size = new System.Drawing.Size(437, 93);
+            this.DiffViewerGB.TabIndex = 2;
+            this.DiffViewerGB.TabStop = false;
+            this.DiffViewerGB.Text = "Diff Viewer";
+            // 
+            // tableLayoutPanelForDiffViewer
+            // 
+            this.tableLayoutPanelForDiffViewer.AutoSize = true;
+            this.tableLayoutPanelForDiffViewer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanelForDiffViewer.ColumnCount = 1;
+            this.tableLayoutPanelForDiffViewer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelForDiffViewer.Controls.Add(this.chkOmitUninterestingDiff, 0, 0);
+            this.tableLayoutPanelForDiffViewer.Controls.Add(this.chkRememberIgnoreWhiteSpacePreference, 0, 0);
+            this.tableLayoutPanelForDiffViewer.Location = new System.Drawing.Point(6, 21);
+            this.tableLayoutPanelForDiffViewer.Name = "tableLayoutPanelForDiffViewer";
+            this.tableLayoutPanelForDiffViewer.RowCount = 1;
+            this.tableLayoutPanelForDiffViewer.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelForDiffViewer.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelForDiffViewer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelForDiffViewer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelForDiffViewer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelForDiffViewer.Size = new System.Drawing.Size(286, 50);
+            this.tableLayoutPanelForDiffViewer.TabIndex = 1;
+            // 
+            // chkOmitUninterestingDiff
+            // 
+            this.chkOmitUninterestingDiff.AutoSize = true;
+            this.chkOmitUninterestingDiff.Location = new System.Drawing.Point(3, 28);
+            this.chkOmitUninterestingDiff.Name = "chkOmitUninterestingDiff";
+            this.chkOmitUninterestingDiff.Size = new System.Drawing.Size(280, 19);
+            this.chkOmitUninterestingDiff.TabIndex = 6;
+            this.chkOmitUninterestingDiff.Text = "Omit uninteresting changes from combined diff";
+            this.chkOmitUninterestingDiff.UseVisualStyleBackColor = true;
+            // 
+            // chkRememberIgnoreWhiteSpacePreference
+            // 
+            this.chkRememberIgnoreWhiteSpacePreference.AutoSize = true;
+            this.chkRememberIgnoreWhiteSpacePreference.Location = new System.Drawing.Point(3, 3);
+            this.chkRememberIgnoreWhiteSpacePreference.Name = "chkRememberIgnoreWhiteSpacePreference";
+            this.chkRememberIgnoreWhiteSpacePreference.Size = new System.Drawing.Size(269, 19);
+            this.chkRememberIgnoreWhiteSpacePreference.TabIndex = 5;
+            this.chkRememberIgnoreWhiteSpacePreference.Text = "Remember the ignore-white-space preference";
+            this.chkRememberIgnoreWhiteSpacePreference.UseVisualStyleBackColor = true;
             // 
             // CheckoutGB
             // 
@@ -120,7 +179,7 @@
             this.GeneralGB.Dock = System.Windows.Forms.DockStyle.Top;
             this.GeneralGB.Location = new System.Drawing.Point(3, 117);
             this.GeneralGB.Name = "GeneralGB";
-            this.GeneralGB.Size = new System.Drawing.Size(437, 143);
+            this.GeneralGB.Size = new System.Drawing.Size(437, 118);
             this.GeneralGB.TabIndex = 1;
             this.GeneralGB.TabStop = false;
             this.GeneralGB.Text = "General";
@@ -131,7 +190,6 @@
             this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.chkRememberIgnoreWhiteSpacePreference, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.chkCheckForRCVersions, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.chkAlwaysShowAdvOpt, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.chkDontSHowHelpImages, 0, 0);
@@ -142,7 +200,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(275, 100);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(223, 75);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // chkCheckForRCVersions
@@ -175,24 +233,18 @@
             this.chkDontSHowHelpImages.Text = "Don\'t show help images";
             this.chkDontSHowHelpImages.UseVisualStyleBackColor = true;
             // 
-            // chkRememberIgnoreWhiteSpacePreference
-            // 
-            this.chkRememberIgnoreWhiteSpacePreference.AutoSize = true;
-            this.chkRememberIgnoreWhiteSpacePreference.Location = new System.Drawing.Point(3, 78);
-            this.chkRememberIgnoreWhiteSpacePreference.Name = "chkRememberIgnoreWhiteSpacePreference";
-            this.chkRememberIgnoreWhiteSpacePreference.Size = new System.Drawing.Size(269, 19);
-            this.chkRememberIgnoreWhiteSpacePreference.TabIndex = 4;
-            this.chkRememberIgnoreWhiteSpacePreference.Text = "Remember the ignore-white-space preference";
-            this.chkRememberIgnoreWhiteSpacePreference.UseVisualStyleBackColor = true;
-            // 
             // AdvancedSettingsPage
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.Controls.Add(this.tableLayoutPanel2);
             this.Name = "AdvancedSettingsPage";
-            this.Size = new System.Drawing.Size(1303, 465);
+            this.Size = new System.Drawing.Size(1371, 575);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.DiffViewerGB.ResumeLayout(false);
+            this.DiffViewerGB.PerformLayout();
+            this.tableLayoutPanelForDiffViewer.ResumeLayout(false);
+            this.tableLayoutPanelForDiffViewer.PerformLayout();
             this.CheckoutGB.ResumeLayout(false);
             this.CheckoutGB.PerformLayout();
             this.tableLayoutPanel3.ResumeLayout(false);
@@ -218,7 +270,9 @@
         private System.Windows.Forms.CheckBox chkUseLocalChangesAction;
         private System.Windows.Forms.CheckBox chkAlwaysShowAdvOpt;
         private System.Windows.Forms.CheckBox chkCheckForRCVersions;
+        private System.Windows.Forms.GroupBox DiffViewerGB;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelForDiffViewer;
         private System.Windows.Forms.CheckBox chkRememberIgnoreWhiteSpacePreference;
-
+        private System.Windows.Forms.CheckBox chkOmitUninterestingDiff;
     }
 }
