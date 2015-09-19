337858ef7b3ecd310b8d72ab8be635ae7cd26778
Linquize <linquize@yahoo.com.hk>
2015-09-19 21:43:53 +0800

    Git for Windows updated to version 2.5.3. Closes #2925

--- Setup/DownloadExternals.cmd
+++ Setup/DownloadExternals.cmd
@@ -2,8 +2,8 @@
 
 REM Also update in Product.wxs and UI\RequiredSoftwareDlg.wxs
 
-IF NOT EXIST "%~p0\..\bin\Git-1.9.5-preview20141217.exe" (
-    "%~p0\tools\curl.exe" -L -k -o ..\bin\Git-1.9.5-preview20141217.exe https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20141217/Git-1.9.5-preview20141217.exe
+IF NOT EXIST "%~p0\..\bin\Git-2.5.3-64-bit.exe" (
+    "%~p0\tools\curl.exe" -L -k -o ..\bin\Git-2.5.3-64-bit.exe https://github.com/git-for-windows/git/releases/download/v2.5.3.windows.1/Git-2.5.3-64-bit.exe
     IF ERRORLEVEL 1 EXIT /B 1
 )
 
