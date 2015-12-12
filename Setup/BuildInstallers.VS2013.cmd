e3ed90c5ce6b06844736aaca74f7e7ee1fb7a504
hypersw <baltic@hypersw.net>
2015-12-12 03:41:34 +0300

    This allows to build the installers on machines without the global installation of the WiX stuff; now got it portable.

--- Setup/BuildInstallers.VS2013.cmd
+++ Setup/BuildInstallers.VS2013.cmd
@@ -15,6 +15,7 @@ set msbuildparams=/p:Configuration=Release /t:Rebuild /nologo /v:m
 %nuget% install ..\GitUI\packages.config -OutputDirectory ..\packages -Source https://nuget.org/api/v2/
 %nuget% install ..\Plugins\BackgroundFetch\packages.config -OutputDirectory ..\packages -Source https://nuget.org/api/v2/
 %nuget% install ..\Plugins\BuildServerIntegration\TeamCityIntegration\packages.config -OutputDirectory ..\packages -Source https://nuget.org/api/v2/
+%nuget% install packages.config -OutputDirectory ..\packages -Source https://nuget.org/api/v2/
 
 %msbuild% %project% /p:Platform="Any CPU" %msbuildparams%
 IF ERRORLEVEL 1 EXIT /B 1
