e411f700092a76d38c0f676f5c51db78fc2c0178
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2016-04-18 23:57:59 +0300

    Git for Windows updated to version 2.8.1 and KDiff3 switched bach to 32bit. Closes #2953, #2723

--- Setup/DownloadExternals.cmd
+++ Setup/DownloadExternals.cmd
@@ -2,12 +2,12 @@
 
 REM Also update in Product.wxs and UI\RequiredSoftwareDlg.wxs
 
-IF NOT EXIST "%~p0\..\bin\Git-1.9.5-preview20141217.exe" (
-    "%~p0\tools\curl.exe" -L -k -o ..\bin\Git-1.9.5-preview20141217.exe https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20141217/Git-1.9.5-preview20141217.exe
+IF NOT EXIST "%~p0\..\bin\Git-2.8.1-32-bit.exe" (
+    "%~p0\tools\curl.exe" -L -k -o ..\bin\Git-2.8.1-32-bit.exe https://github.com/git-for-windows/git/releases/download/v2.8.1.windows.1/Git-2.8.1-32-bit.exe
     IF ERRORLEVEL 1 EXIT /B 1
 )
 
-IF NOT EXIST "%~p0\..\bin\KDiff3-64bit-Setup_0.9.98-2.exe" (
-    "%~p0\tools\curl.exe" -L -k -o ..\bin\KDiff3-64bit-Setup_0.9.98-2.exe https://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-64bit-Setup_0.9.98-2.exe# -L http://sourceforge.net/ > NUL
+IF NOT EXIST "%~p0\..\bin\KDiff3-32bit-Setup_0.9.98-3.exe" (
+    "%~p0\tools\curl.exe" -L -k -o ..\bin\KDiff3-32bit-Setup_0.9.98-3.exe http://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-32bit-Setup_0.9.98-3.exe -L http://sourceforge.net/ > NUL
     IF ERRORLEVEL 1 EXIT /B 1
 )
