a10b52e87da2b191f7553ecfc6b0161de054cbfc
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2016-04-19 02:26:23 +0300

    Kdiff3 roll back to version 0.9.97

--- Setup/DownloadExternals.cmd
+++ Setup/DownloadExternals.cmd
@@ -7,7 +7,7 @@ IF NOT EXIST "%~p0\..\bin\Git-2.8.1-32-bit.exe" (
     IF ERRORLEVEL 1 EXIT /B 1
 )
 
-IF NOT EXIST "%~p0\..\bin\KDiff3-32bit-Setup_0.9.98-3.exe" (
-    "%~p0\tools\curl.exe" -L -k -o ..\bin\KDiff3-32bit-Setup_0.9.98-3.exe http://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-32bit-Setup_0.9.98-3.exe -L http://sourceforge.net/ > NUL
+IF NOT EXIST "%~p0\..\bin\KDiff3-32bit-Setup_0.9.97.exe" (
+    "%~p0\tools\curl.exe" -L -k -o ..\bin\KDiff3-32bit-Setup_0.9.97.exe http://sourceforge.net/projects/kdiff3/files/kdiff3/0.9.97/KDiff3-32bit-Setup_0.9.97.exe/download -L http://sourceforge.net/ > NUL
     IF ERRORLEVEL 1 EXIT /B 1
 )
