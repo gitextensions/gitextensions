fd0e086d19e121e11e5d8175efc79f890bd5c112
Janusz Białobrzewski <jbialobr@o2.pl>
2016-10-12 20:03:35 +0200

    Restore nuget for conemu

--- Setup/BuildInstallers.Mono.cmd
+++ Setup/BuildInstallers.Mono.cmd
@@ -11,6 +11,7 @@ set EnableNuGetPackageRestore=true
 
 %nuget% install ..\Plugins\BackgroundFetch\packages.config -OutputDirectory ..\packages
 %nuget% install ..\Plugins\BuildServerIntegration\TeamCityIntegration\packages.config -OutputDirectory ..\packages
+%nuget% install ..\GitExtensionsVSIX\packages.config -OutputDirectory ..\packages
 
 call %msbuild% %project% /t:clean
 call %msbuild% %project% /p:TargetFrameworkProfile="" /p:Platform="Any CPU" /p:Configuration=Release /nologo /v:m
