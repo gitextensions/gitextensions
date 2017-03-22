3565e0d01f7146be5776015d2f763c1402dfcca0
Janusz Białobrzewski <jbialobr@o2.pl>
2017-03-22 11:23:31 +0100

    Revert "Merge pull request #2802 from odie2/master"
    This reverts commit b9488d0359b5f08317fec1a0bc001e33618bc434, reversing
    changes made to f8a19ad7c509a039560d16bb8041ad0082c25937.

--- Plugins/ProxySwitcher/ProxySwitcherPlugin.cs
+++ Plugins/ProxySwitcher/ProxySwitcherPlugin.cs
@@ -1,4 +1,4 @@
-using GitUIPluginInterfaces;
+﻿using GitUIPluginInterfaces;
 using ResourceManager;
 
 namespace ProxySwitcher
@@ -15,14 +15,7 @@ namespace ProxySwitcher
             SetNameAndDescription("Proxy Switcher");
             Translate();
         }
-        
-        private StringSetting Username = new StringSetting("Username", "");
-        private StringSetting Password = new StringSetting("Password", "");
-        private StringSetting HttpProxy= new StringSetting("HTTP proxy", "");
-        private StringSetting HttpProxyPort = new StringSetting("HTTP proxy port", "8080");
 
-        #region IGitPlugin Members
-        
         public override System.Collections.Generic.IEnumerable<ISetting> GetSettings()
         {
             yield return Username;
@@ -39,7 +32,5 @@ namespace ProxySwitcher
             }
             return false;
         }
-        
-        #endregion IGitPlugin Members
     }
 }
