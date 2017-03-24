2788b1936958fef29cba7f758d147be30b1de064
Janusz Białobrzewski <jbialobr@o2.pl>
2017-03-24 18:11:56 +0100

    Decode file name from git log output. Closes #3578.

--- GitUI/CommandsDialogs/FormFileHistory.cs
+++ GitUI/CommandsDialogs/FormFileHistory.cs
@@ -175,7 +175,7 @@ namespace GitUI.CommandsDialogs
                 string arg = "log --format=\"%n\" --name-only --follow "+
                     GitCommandHelpers.FindRenamesAndCopiesOpts()
                     + " -- \"" + fileName + "\"";
-                Process p = Module.RunGitCmdDetached(arg);
+                Process p = Module.RunGitCmdDetached(arg, GitModule.LosslessEncoding);
 
                 // the sequence of (quoted) file names - start with the initial filename for the search.
                 var listOfFileNames = new StringBuilder("\"" + fileName + "\"");
@@ -187,6 +187,7 @@ namespace GitUI.CommandsDialogs
                 do
                 {
                     line = p.StandardOutput.ReadLine();
+                    line = GitModule.ReEncodeFileNameFromLossless(line);
 
                     if (!string.IsNullOrEmpty(line) && setOfFileNames.Add(line))
                     {
