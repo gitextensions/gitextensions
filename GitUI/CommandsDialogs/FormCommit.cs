452f7ef9ede11c8647a4a05aa0be7ce74727615d
Valery Petrov <valery.petrov@sidenis.com>
2017-03-31 15:40:18 +0300

    Removed unused usings that fails mono build

--- GitUI/CommandsDialogs/FormCommit.cs
+++ GitUI/CommandsDialogs/FormCommit.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.IO;
 using System.Linq;
