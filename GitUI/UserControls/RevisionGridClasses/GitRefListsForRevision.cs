fb1f48fe44072afc9fd2119c346938fabacb8cc2
RussKie <russkie@screamer>
2017-04-09 22:36:04 +1000

    feat: Ref rename and delete from keyboard
    Allow to rename and delete branches and tags from keyboard.
    Actions:
    * F2 - rename
    * Del - delete
    
    If there is...
    * no valid branch or tag to rename or delete the action is ignored
    * a single branch or tag - the respective action dialog is launched
    * multiple branches or tags then a quick select dialog is launched to
    specify the exact ref to perform action on.
    
    Addresses #3503

--- GitUI/UserControls/RevisionGridClasses/GitRefListsForRevision.cs
+++ GitUI/UserControls/RevisionGridClasses/GitRefListsForRevision.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using GitCommands;
 using GitUIPluginInterfaces;
 
 namespace GitUI.UserControls.RevisionGridClasses
 {
-    class GitRefListsForRevision
+    internal class GitRefListsForRevision
     {
         private readonly IGitRef[] _allBranches;
         private readonly IGitRef[] _localBranches;
@@ -15,45 +16,52 @@ namespace GitUI.UserControls.RevisionGridClasses
         {
             _allBranches = revision.Refs.Where(h => !h.IsTag && (h.IsHead || h.IsRemote)).ToArray();
             _localBranches = _allBranches.Where(b => !b.IsRemote).ToArray();
-            _branchesWithNoIdenticalRemotes = _allBranches.Where(
-                b => !b.IsRemote || !_localBranches.Any(lb => lb.TrackingRemote == b.Remote && lb.MergeWith == b.LocalName)).ToArray();
+            _branchesWithNoIdenticalRemotes = _allBranches.Where(b => !b.IsRemote || 
+                                                                      !_localBranches.Any(lb => lb.TrackingRemote == b.Remote && lb.MergeWith == b.LocalName))
+                                                          .ToArray();
 
             _tags = revision.Refs.Where(h => h.IsTag).ToArray();
         }
 
-        public IGitRef[] AllBranches
-        {
-            get { return _allBranches; }
-        }
 
-        public IGitRef[] LocalBranches
-        {
-            get { return _localBranches; }
-        }
+        public IEnumerable<IGitRef> AllBranches => _allBranches.AsEnumerable();
 
-        public IGitRef[] BranchesWithNoIdenticalRemotes
-        {
-            get { return _branchesWithNoIdenticalRemotes; }
-        }
+        public IEnumerable<IGitRef> AllTags => _tags.AsEnumerable();
 
-        public IGitRef[] AllTags
-        {
-            get { return _tags; }
-        }
+        public IEnumerable<IGitRef> BranchesWithNoIdenticalRemotes => _branchesWithNoIdenticalRemotes.AsEnumerable();
+
 
         public string[] GetAllBranchNames()
         {
-            return AllBranches.Select(b => b.Name).ToArray();
+            return _allBranches.Select(b => b.Name).ToArray();
         }
 
         public string[] GetAllNonRemoteBranchNames()
         {
-            return AllBranches.Where(head => !head.IsRemote).Select(b => b.Name).ToArray();
+            return _allBranches.Where(head => !head.IsRemote).Select(b => b.Name).ToArray();
         }
 
         public string[] GetAllTagNames()
         {
             return AllTags.Select(t => t.Name).ToArray();
+        }
+
+        /// <summary>
+        /// Returns the collection of local branches and tags which can be deleted.
+        /// </summary>
+        /// <returns></returns>
+        public IGitRef[] GetDeletableLocalRefs(string currentBranch)
+        {
+            return _localBranches.Where(b => !b.Name.Equals(currentBranch)).Union(_tags).ToArray();
+        }
+
+        /// <summary>
+        /// Returns the collection of local branches which can be renamed.
+        /// </summary>
+        /// <returns></returns>
+        public IGitRef[] GetRenameableLocalBranches()
+        {
+            return _localBranches.ToArray();
         }
     }
 }
