07c8fc874b639a66dd7674c054500420b8d496f1
Janusz Białobrzewski <jbialobr@o2.pl>
2017-04-11 06:24:28 +0200

    Tests for parsing links.

---
+++ GitExtensionsTest/GitUI/LinkFactoryFixture.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using ResourceManager;
+
+namespace GitExtensionsTest.GitUI
+{
+    [TestFixture]
+    class LinkFactoryFixture
+    {
+        [Test]
+        public void ParseGoToBranchLink()
+        {
+            string expected = "gitext://gotobranch/master";
+            string actual = LinkFactory.ParseLink("master#gitext://gotobranch/master");
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ParseGoToBranchLinkWithHash()
+        {
+            string expected = "gitext://gotobranch/PR#23";
+            string actual = LinkFactory.ParseLink("PR#23#gitext://gotobranch/PR#23");
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ParseHtttpLinkWithHash()
+        {
+            string expected = "https://github.com/gitextensions/gitextensions/pull/3471#end";
+            string actual = LinkFactory.ParseLink("#3471#https://github.com/gitextensions/gitextensions/pull/3471#end");
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
