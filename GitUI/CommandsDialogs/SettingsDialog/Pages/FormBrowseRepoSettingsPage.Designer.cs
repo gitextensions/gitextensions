a50c75136c40093e8287f7b299ab1d11ceaa59fa
Janusz Białobrzewski <jbialobr@o2.pl>
2017-04-25 13:55:35 +0200

    Corrected AutoScaleMode

--- GitUI/CommandsDialogs/SettingsDialog/Pages/FormBrowseRepoSettingsPage.Designer.cs
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/FormBrowseRepoSettingsPage.Designer.cs
@@ -241,8 +241,8 @@
             // 
             // FormBrowseRepoSettingsPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.tableLayoutPanel2);
             this.Name = "FormBrowseRepoSettingsPage";
             this.Size = new System.Drawing.Size(1297, 666);
