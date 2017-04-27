5cf040a30d78de5412d050af5d1bfb87d499df2b
Janusz Białobrzewski <jbialobr@o2.pl>
2017-04-27 22:43:08 +0200

    Merge saved hotkeys into the default hotkeys. Closes #3432.

--- GitUI/Hotkey/HotkeySettings.cs
+++ GitUI/Hotkey/HotkeySettings.cs
@@ -1,28 +1,45 @@
 ﻿using System;
 using System.Xml.Serialization;
 using ResourceManager;
+using System.Linq;
 
 namespace GitUI.Hotkey
 {
-  /// <summary>
-  /// Stores all hotkey mappings of one target
-  /// </summary>
-  [Serializable]
-  public class HotkeySettings
-  {
-    [XmlArray]
-    public HotkeyCommand[] Commands { get; set; }
-
-    [XmlAttribute]
-    public string Name { get; set; }
-
-    public HotkeySettings()
+    /// <summary>
+    /// Stores all hotkey mappings of one target
+    /// </summary>
+    [Serializable]
+    public class HotkeySettings
     {
+        [XmlArray]
+        public HotkeyCommand[] Commands { get; set; }
+
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        public HotkeySettings()
+        {
+        }
+        public HotkeySettings(string name, params HotkeyCommand[] commands)
+        {
+            this.Name = name;
+            this.Commands = commands;
+        }
+
+        public override bool Equals(object obj)
+        {
+            HotkeySettings other = obj as HotkeySettings;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Commands.SequenceEqual(other.Commands);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
-    public HotkeySettings(string name, params HotkeyCommand[] commands)
-    {
-      this.Name = name;
-      this.Commands = commands;
-    }
-  }
 }
