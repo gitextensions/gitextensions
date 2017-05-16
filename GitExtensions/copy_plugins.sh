62525d4eaf3b2d6dfa04e2fdf21c05192a1121af
CDAGaming <cstack2011@yahoo.com>
2017-05-15 22:30:55 -0500

    Fix Non-Existent .mdb Check in exchange for .pdb
    Signed-off-by: CDAGaming <cstack2011@yahoo.com>

--- GitExtensions/copy_plugins.sh
+++ GitExtensions/copy_plugins.sh
@@ -4,6 +4,6 @@ mkdir -p Plugins/
 for i in  $1/Plugins/* $1/Plugins/Statistics/*; do
   if [ -d $i/bin/$config ]; then
     cp -r $i/bin/$config/*.dll Plugins/
-    cp -r $i/bin/$config/*.dll.mdb Plugins/
+    cp -r $i/bin/$config/*.pdb Plugins/
   fi
 done
