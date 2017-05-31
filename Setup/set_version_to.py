e55a4e8d6c00842f3d8515b9742a822ff9d41b22
Janusz Białobrzewski <jbialobr@o2.pl>
2017-05-31 15:08:40 +0200

    Set version in vsixmanifest.

--- Setup/set_version_to.py
+++ Setup/set_version_to.py
@@ -1,4 +1,5 @@
 import argparse, sys
+import re
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -129,3 +130,13 @@ if __name__ == '__main__':
             docoConf[i] = " = '".join(data) + "'\n"
     outfile = open(filename, "w")
     outfile.writelines(docoConf)
+    
+    filename = "..\GitExtensionsVSIX\source.extension.vsixmanifest"
+    vsixManifest = open(filename, "r").readlines()
+    for i in range(len(vsixManifest)):
+        line = vsixManifest[i]
+        if line.find("<Identity Publisher=\"GitExt Team\" Version=") != -1:
+            line = re.sub("<Identity Publisher=\"GitExt Team\" Version=\"[0-9\.]+", "<Identity Publisher=\"GitExt Team\" Version=\"" + '.'.join(verSplitted), line)
+            vsixManifest[i] = line
+    outfile = open(filename, "w")
+    outfile.writelines(vsixManifest)    
