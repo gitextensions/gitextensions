05f7dd700d0429a88af41a4593ba4a5c564d7cac
Janusz Białobrzewski <jbialobr@o2.pl>
2017-07-07 21:33:08 +0200

    Update git to 2.13.2. Closes #3804.

--- Setup/DownloadExternals.cmd
+++ Setup/DownloadExternals.cmd
@@ -4,8 +4,8 @@ REM Also update in Product.wxs and UI\RequiredSoftwareDlg.wxs
 
 md %~p0\cache\ 2> NUL
 
-IF NOT EXIST "%~p0\cache\Git-2.12.2-32-bit.exe" (
-    "%~p0\tools\curl.exe" -L -k -o %~p0\cache\Git-2.12.2-32-bit.exe https://github.com/git-for-windows/git/releases/download/v2.12.2.windows.1/Git-2.12.2-32-bit.exe
+IF NOT EXIST "%~p0\cache\Git-2.13.2-32-bit.exe" (
+    "%~p0\tools\curl.exe" -L -k -o %~p0\cache\Git-2.13.2-32-bit.exe https://github.com/git-for-windows/git/releases/download/v2.13.2.windows.1/Git-2.13.2-32-bit.exe
     IF ERRORLEVEL 1 EXIT /B 1
 )
 
