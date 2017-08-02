e743798be8aaef1890bc2a72e61d75e5411bbd2d
Janusz Białobrzewski <jbialobr@o2.pl>
2017-08-02 11:53:23 +0200

    Enable raising events in order to be notified about process exit.

--- GitCommands/Git/GitCommandHelpers.cs
+++ GitCommands/Git/GitCommandHelpers.cs
@@ -144,17 +144,16 @@ namespace GitCommands
         {
             SetEnvironmentVariable();
 
-            string quotedCmd = fileName;
-            if (quotedCmd.IndexOf(' ') != -1)
-                quotedCmd = quotedCmd.Quote();
-
             var executionStartTimestamp = DateTime.Now;
 
             var startInfo = CreateProcessStartInfo(fileName, arguments, workingDirectory, outputEncoding);
             var startProcess = Process.Start(startInfo);
-
+            startProcess.EnableRaisingEvents = true;
             startProcess.Exited += (sender, args) =>
             {
+                string quotedCmd = fileName;
+                if (quotedCmd.IndexOf(' ') != -1)
+                    quotedCmd = quotedCmd.Quote();
                 var executionEndTimestamp = DateTime.Now;
                 AppSettings.GitLog.Log(quotedCmd + " " + arguments, executionStartTimestamp, executionEndTimestamp);
             };
