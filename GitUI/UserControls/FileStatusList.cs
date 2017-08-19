ee3ba236fe9027cff450520829eafd766d42f02c
Philippe Miossec <pmiossec@gmail.com>
2017-08-19 19:02:54 +0200

    FileStatusList: Fix Focused Property
    (that cause a bad behavior when using previous/next file feature)

--- GitUI/UserControls/FileStatusList.cs
+++ GitUI/UserControls/FileStatusList.cs
@@ -159,7 +159,7 @@ namespace GitUI
         {
             get
             {
-                return FileStatusListView.Focused;
+                return FileStatusListView.Focused || FilterComboBox.Focused;
             }
         }
 
