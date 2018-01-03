4d3457d3183514cbc1661ff52bef4737f6bba221
RussKie <russkie@gmail.com>
2018-01-03 14:57:09 +1100

    Script to generate changelog
    Run the following command to generate the changelog:
    
        PS \gitextensions\Setup> .\Generate-Changelog.ps1 -milestoneNumber XX
    
    where XX is milestone number (as shown by Github).
    
    The script produces `Changelog.v{milestone title}.md`in the same directory

---
+++ Setup/Generate-Changelog.ps1
@@ -0,0 +1,64 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True, Position=1)]
+    [int] $milestoneNumber
+)
+
+pushd $PSScriptRoot
+
+$baseUri = "https://api.github.com/repos/gitextensions/gitextensions"
+
+try{
+    $milestone = Invoke-RestMethod -Method Get -Uri $baseUri/milestones/$milestoneNumber
+    $milestoneTitle = $milestone.title;
+    $milestoneDue = if ($milestone.due_on -eq $null) { "no due date" } else { Get-Date $milestone.due_on -Format "dd MMM yyyy" };
+    $changelogFile = "Changelog.v$milestoneTitle.md";
+    $totalIssueCount = $milestone.closed_issues
+
+    $totalIssues = @();
+    $pageIndex = 1;
+    while ($totalIssueCount -gt 0) {
+        $issues = Invoke-RestMethod -Method Get -Uri "$baseUri/issues?page=$pageIndex&per_page=100&milestone=$($milestone.number)&state=closed" #-Verbose
+        $totalIssues += $issues;
+
+        $totalIssueCount -= $issues.Count;
+        $pageIndex++;
+    }
+
+    $issueFeatures = @();
+    $issueBugs = @();
+    $issueLinks = @();
+
+    $totalIssues | ForEach-Object {
+        $issue = $_;
+
+        if ($issue.labels.name -eq 'bug') {
+            $issueBugs += $issue;
+        }
+        else {
+            $issueFeatures += $issue;
+        }
+        $issueLinks += "[$($issue.number)]:$($issue.html_url)"
+    }
+
+
+    "### Version $milestoneTitle ($milestoneDue)" | Out-File $changelogFile
+    "`r`n#### Features:" | Out-File $changelogFile -Append
+    $issueFeatures | ForEach-Object {
+        $issue = $_;
+        "* $($issue.title) - PR [$($issue.number)]" | Out-File $changelogFile -Append
+    }
+    "`r`n#### Fixes:" | Out-File $changelogFile -Append
+    $issueBugs | ForEach-Object {
+        $issue = $_;
+        "* $($issue.title) - Issue [$($issue.number)]" | Out-File $changelogFile -Append
+    }
+    "`r`n" | Out-File $changelogFile -Append
+    $issueLinks | Out-File $changelogFile -Append
+}
+catch {
+    Write-Error $_;
+    Pause
+    Exit -1;
+}
+
