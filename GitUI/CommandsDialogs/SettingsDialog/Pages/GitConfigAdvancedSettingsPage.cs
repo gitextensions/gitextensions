49c30fe5c57fbec222b2c2ebcf0ac18258ded0f3
Philippe Miossec <pmiossec@gmail.com>
2018-01-06 00:25:24 +0100

    Add setting page to configure some useful/advanced git settings...

---
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/GitConfigAdvancedSettingsPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using GitCommands.Config;
+using GitCommands.Settings;
+using GitCommands.Utils;
+using ResourceManager;
+
+namespace GitUI.CommandsDialogs.SettingsDialog.Pages
+{
+    public partial class GitConfigAdvancedSettingsPage : ConfigFileSettingsPage
+    {
+        public GitConfigAdvancedSettingsPage()
+        {
+            InitializeComponent();
+            Text = "Advanced";
+            Translate();
+        }
+
+        protected override void SettingsToPage()
+        {
+            checkBoxPullRebase.Checked = CurrentSettings.GetValue("pull.rebase").Equals("true");
+            checkBoxFetchPrune.Checked = CurrentSettings.GetValue("fetch.prune").Equals("true");
+            checkBoxRebaseAutostash.Checked = CurrentSettings.GetValue("rebase.autoStash").Equals("true");
+        }
+
+        protected override void PageToSettings()
+        {
+            CurrentSettings.SetValue("pull.rebase", checkBoxPullRebase.Checked ? "true" : "false");
+            CurrentSettings.SetValue("fetch.prune", checkBoxFetchPrune.Checked ? "true" : "false");
+            CurrentSettings.SetValue("rebase.autoStash", checkBoxRebaseAutostash.Checked ? "true" : "false");
+        }
+    }
+}
