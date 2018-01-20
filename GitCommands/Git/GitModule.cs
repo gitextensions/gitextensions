gitextensions/gitextensions

commit 510bd7f73aebfb1f84657b2390a46dfaa142c64c

Compare artificial commits to all other commits (#4157)
* Restructure OpenWithDifftool() so it can handle artificial diff and add unit tests
* Update diff options so 'from' is always first in the list
* Compare artificial commits to all other commits

Handled in #4031, several other PRs are required first, see the issue for merge order.
It is possible to compare a normal commit to any other commit, but staged/unstaged has until know just been comparable to its immediate parent
This commit removes that limitation. In order to do that, the diff handling has been cleaned up some too. There are maybe not less code related to artificial commits, but the normal flow has less special handling

GitCommands/Git/GitModule.cs
@@ -91,6 +91,8 @@ namespace GitCommands
         private readonly IIndexLockManager _indexLockManager;
         private static readonly IGitDirectoryResolver GitDirectoryResolverInstance = new GitDirectoryResolver();
         private readonly IGitTreeParser _gitTreeParser = new GitTreeParser();
+        private readonly IRevisionDiffProvider _revisionDiffProvider = new RevisionDiffProvider();
+
 
         public const string NoNewLineAtTheEnd = "\\ No newline at end of file";
         private const string DiffCommandWithStandardArgs = " -c diff.submodule=short diff --no-color ";
@@ -2201,13 +2203,8 @@ namespace GitCommands
 
             //fix refs slashes
             from = from.ToPosixPath();
-            to = to.ToPosixPath();
-            string commitRange = string.Empty;
-            if (!to.IsNullOrEmpty())
-                commitRange = "\"" + to + "\"";
-            if (!from.IsNullOrEmpty())
-                commitRange = string.Join(" ", commitRange, "\"" + from + "\"");
-
+            to = to == null ? "":to.ToPosixPath();
+            string commitRange = _revisionDiffProvider.Get(from, to);
             if (AppSettings.UsePatienceDiffAlgorithm)
                 extraDiffArguments = string.Concat(extraDiffArguments, " --patience");
 
@@ -2250,7 +2247,7 @@ namespace GitCommands
 
         public string GetDiffFilesText(string from, string to, bool noCache)
         {
-            string cmd = DiffCommandWithStandardArgs + "-M -C --name-status \"" + to + "\" \"" + from + "\"";
+            string cmd = DiffCommandWithStandardArgs + "-M -C --name-status " + _revisionDiffProvider.Get(from, to);
             return noCache ? RunGitCmd(cmd) : this.RunCacheableCmd(AppSettings.GitCommand, cmd, SystemEncoding);
         }
 
@@ -2263,14 +2260,35 @@ namespace GitCommands
 
         public List<GitItemStatus> GetDiffFiles(string from, string to, bool noCache = false)
         {
-            string cmd = DiffCommandWithStandardArgs + "-M -C -z --name-status \"" + to + "\" \"" + from + "\"";
+            noCache = noCache || GitRevision.IsArtificial(from) || GitRevision.IsArtificial(to);
+            string cmd = DiffCommandWithStandardArgs + "-M -C -z --name-status " + _revisionDiffProvider.Get(from, to);
             string result = noCache ? RunGitCmd(cmd) : this.RunCacheableCmd(AppSettings.GitCommand, cmd, SystemEncoding);
-            return GitCommandHelpers.GetAllChangedFilesFromString(this, result, true);
+            var resultCollection = GitCommandHelpers.GetAllChangedFilesFromString(this, result, true);
+            if (from == GitRevision.UnstagedGuid || to == GitRevision.UnstagedGuid)
+            {
+                //For unstaged the untracked must be added too
+                var files = GetUnstagedFilesWithSubmodulesStatus().Where(item => item.IsNew);
+                if (from == GitRevision.UnstagedGuid)
+                {
+                    //The file is seen as "deleted" in 'to' revision
+                    foreach (var item in files)
+                    {
+                        item.IsNew = false;
+                        item.IsDeleted = true;
+                        resultCollection.Add(item);
+                    }
+                }
+                else
+                {
+                    resultCollection.AddRange(files);
+                }
+            }
+            return resultCollection;
         }
 
         public IList<GitItemStatus> GetStashDiffFiles(string stashName)
         {
-            var resultCollection = GetDiffFiles(stashName, stashName + "^", true);
+            var resultCollection = GetDiffFiles(stashName + "^", stashName, true);
 
             // shows untracked files
             string untrackedTreeHash = RunGitCmd("log " + stashName + "^3 --pretty=format:\"%T\" --max-count=1");
@@ -3035,12 +3053,8 @@ namespace GitCommands
         public string OpenWithDifftool(string filename, string oldFileName = "", string revision1 = null, string revision2 = null, string extraDiffArguments = "")
         {
             var output = "";
-            if (!filename.IsNullOrEmpty())
-                filename = filename.Quote();
-            if (!oldFileName.IsNullOrEmpty())
-                oldFileName = oldFileName.Quote();
 
-            string args = string.Join(" ", extraDiffArguments, revision1.QuoteNE(), revision2.QuoteNE(), "--", filename, oldFileName);
+            string args = string.Join(" ", extraDiffArguments, _revisionDiffProvider.Get(revision1, revision2), "--", filename.QuoteNE(), oldFileName.QuoteNE());
             RunGitCmdDetached("difftool --gui --no-prompt " + args);
             return output;
         }

GitCommands/Git/RevisionDiffProvider.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace GitCommands
+namespace GitCommands.Git
 {
-    /// <summary>
-    /// Translate GitRevision including artificial commits to diff options
-    /// Closely related to GitRevision.cs 
-    /// </summary>
-    public class RevisionDiffProvider
+    public interface IRevisionDiffProvider
     {
         /// <summary>
         /// options to git-diff from GE arguments, including artificial commits
+        /// This is an instance class to not have static dependencies in GitModule
         /// </summary>
         /// <param name="from">The first revision</param>
         /// <param name="to">The second "current" revision</param>
         /// <returns></returns>
-        public static string Get(string from, string to)
+        string Get(string from, string to);
+    }
+
+    /// <summary>
+    /// Translate GitRevision including artificial commits to diff options
+    /// Closely related to GitRevision.cs 
+    /// </summary>
+    public sealed class RevisionDiffProvider : IRevisionDiffProvider
+    {
+        private static readonly string StagedOpt = "--cached";
+
+        /// <summary>
+        /// options to git-diff from GE arguments, including artificial commits
+        /// This is an instance class to not have static dependencies in GitModule
+        /// </summary>
+        /// <param name="from">The first revision</param>
+        /// <param name="to">The second "current" revision</param>
+        /// <returns></returns>
+        public string Get(string from, string to)
         {
             string extra = string.Empty;
             from = ArtificialToDiffOptions(from);
@@ -60,7 +75,7 @@ namespace GitCommands
         /// </summary>
         /// <param name="rev"></param>
         /// <returns></returns>
-        private static string ArtificialToDiffOptions(string rev)
+        private string ArtificialToDiffOptions(string rev)
         {
             if (rev.IsNullOrEmpty() || rev == GitRevision.UnstagedGuid) { rev = string.Empty; }
             else if (rev == "^" || rev == GitRevision.UnstagedGuid + "^" || rev == GitRevision.IndexGuid) { rev = StagedOpt; }
@@ -74,7 +89,5 @@ namespace GitCommands
 
             return rev;
         }
-
-        private const string StagedOpt = "--cached";
     }
 }

GitUI/CommandsDialogs/FormArchive.cs
@@ -154,19 +154,19 @@ namespace GitUI.CommandsDialogs
         {
             if (checkBoxPathFilter.Checked)
             {
-                // 1. get all lines from text box which are not empty
-                // 2. wrap lines with ""
+                // 1. get all lines (paths) from text box
+                // 2. wrap lines that are not empty with ""
                 // 3. join together with space as separator
-                return string.Join(" ", textBoxPaths.Lines.Where(a => !a.IsNullOrEmpty()).Select(a => string.Format("\"{0}\"", a)));
+                return string.Join(" ", textBoxPaths.Lines.Select(a => a.QuoteNE()));
             }
             else if (checkboxRevisionFilter.Checked)
             {
 
-                // 1. get all files changed between current revision and selected revision from diff
-                var files = UICommands.Module.GetDiffFiles(this.SelectedRevision.Guid, this.DiffSelectedRevision.Guid);
-                // 2. wrap names with ""
+                // 1. get all changed (and not deleted files) from selected to current revision
+                var files = UICommands.Module.GetDiffFiles(this.DiffSelectedRevision.Guid, this.SelectedRevision.Guid).Where(f => !f.IsDeleted);
+                // 2. wrap file names with ""
                 // 3. join together with space as separator
-                return string.Join(" ", files.Where(f => !f.IsDeleted).Select(f => string.Format("\"{0}\"", f.Name)));
+                return string.Join(" ", files.Select(f => f.Name.QuoteNE()));
             }
             else
             {

GitUI/CommandsDialogs/FormCommit.cs
@@ -1960,11 +1960,11 @@ namespace GitUI.CommandsDialogs
             Clipboard.SetText(fileNames.ToString());
         }
 
-        private void OpenFilesWithDiffTool(IEnumerable<GitItemStatus> items, bool staged)
+        private void OpenFilesWithDiffTool(IEnumerable<GitItemStatus> items, string from, string to)
         {
             foreach (var item in items)
             {
-                string output = Module.OpenWithDifftool(item.Name, null, null, null, staged ? "--cached" : "");
+                string output = Module.OpenWithDifftool(item.Name, null, from, to);
                 if (!string.IsNullOrEmpty(output))
                     MessageBox.Show(this, output);
             }
@@ -1972,7 +1972,7 @@ namespace GitUI.CommandsDialogs
 
         private void OpenWithDifftoolToolStripMenuItemClick(object sender, EventArgs e)
         {
-            OpenFilesWithDiffTool(Unstaged.SelectedItems, staged: false);
+            OpenFilesWithDiffTool(Unstaged.SelectedItems, GitRevision.IndexGuid, GitRevision.UnstagedGuid);
         }
 
         private void ResetPartOfFileToolStripMenuItemClick(object sender, EventArgs e)
@@ -2634,7 +2634,7 @@ namespace GitUI.CommandsDialogs
 
         private void stagedOpenDifftoolToolStripMenuItem9_Click(object sender, EventArgs e)
         {
-            OpenFilesWithDiffTool(Staged.SelectedItems, staged: true);
+            OpenFilesWithDiffTool(Staged.SelectedItems, "HEAD", GitRevision.IndexGuid);
         }
 
         private void openFolderToolStripMenuItem10_Click(object sender, EventArgs e)

GitUI/CommandsDialogs/FormDiff.cs
@@ -119,20 +119,20 @@ namespace GitUI.CommandsDialogs
             if (DiffFiles.SelectedItem == null)
                 return;
 
-            GitUIExtensions.DiffWithRevisionKind diffKind;
+            GitUI.RevisionDiffKind diffKind;
 
             if (sender == aLocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffALocal;
+                diffKind = GitUI.RevisionDiffKind.DiffALocal;
             else if (sender == bLocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffBLocal;
+                diffKind = GitUI.RevisionDiffKind.DiffBLocal;
             else if (sender == parentOfALocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffAParentLocal;
+                diffKind = GitUI.RevisionDiffKind.DiffAParentLocal;
             else if (sender == parentOfBLocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffBParentLocal;
+                diffKind = GitUI.RevisionDiffKind.DiffBParentLocal;
             else
             {
                 Debug.Assert(sender == aBToolStripMenuItem, "Not implemented DiffWithRevisionKind: " + sender);
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffAB;
+                diffKind = GitUI.RevisionDiffKind.DiffAB;
             }
 
             foreach (var selectedItem in DiffFiles.SelectedItems)
@@ -157,12 +157,7 @@ namespace GitUI.CommandsDialogs
 
             if (item.IsTracked)
             {
-                IList<GitRevision> revisions = RevisionGrid.GetSelectedRevisions();
-
-                if (revisions.Count == 0 || GitRevision.IsArtificial(revisions[0].Guid))
-                    UICommands.StartFileHistoryDialog(this, item.Name);
-                else
-                    UICommands.StartFileHistoryDialog(this, item.Name, revisions[0], false);
+                UICommands.StartFileHistoryDialog(this, item.Name, _baseRevision, false);
             }
         }
 
@@ -172,12 +167,7 @@ namespace GitUI.CommandsDialogs
 
             if (item.IsTracked)
             {
-                IList<GitRevision> revisions = RevisionGrid.GetSelectedRevisions();
-
-                if (revisions.Count == 0 || GitRevision.IsArtificial(revisions[0].Guid))
-                    UICommands.StartFileHistoryDialog(this, item.Name, null, false, true);
-                else
-                    UICommands.StartFileHistoryDialog(this, item.Name, revisions[0], true, true);
+                UICommands.StartFileHistoryDialog(this, item.Name, _baseRevision, true, true);
             }
         }
 
@@ -261,7 +251,7 @@ namespace GitUI.CommandsDialogs
 
         private void PickAnotherCommit(GitRevision preSelect, ref string displayStr, ref GitRevision revision)
         {
-            using (var form = new FormChooseCommit(UICommands, preselectCommit: preSelect.Guid))
+            using (var form = new FormChooseCommit(UICommands, preselectCommit: preSelect.Guid, showArtificial: true))
             {
                 if (form.ShowDialog(this) == DialogResult.OK)
                 {

GitUI/CommandsDialogs/FormFileHistory.cs
@@ -328,7 +328,7 @@ namespace GitUI.CommandsDialogs
             {
                 orgFileName = selectedRows[0].Name;
             }
-            FileChanges.OpenWithDifftool(FileName, orgFileName, GitUIExtensions.DiffWithRevisionKind.DiffAB);
+            FileChanges.OpenWithDifftool(FileName, orgFileName, GitUI.RevisionDiffKind.DiffAB);
         }
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
@@ -442,7 +442,7 @@ namespace GitUI.CommandsDialogs
 
         private void diffToolremotelocalStripMenuItem_Click(object sender, EventArgs e)
         {
-            FileChanges.OpenWithDifftool(FileName, string.Empty, GitUIExtensions.DiffWithRevisionKind.DiffBLocal);
+            FileChanges.OpenWithDifftool(FileName, string.Empty, GitUI.RevisionDiffKind.DiffBLocal);
         }
 
         private void toolStripSplitLoad_ButtonClick(object sender, EventArgs e)

GitUI/CommandsDialogs/RevisionDiff.cs
@@ -140,11 +140,7 @@ namespace GitUI.CommandsDialogs
                     break;
 
                 case 2: // diff "first clicked revision" --> "second clicked revision"
-                    bool artificialRevSelected = revisions[0].IsArtificial() || revisions[1].IsArtificial();
-                    if (!artificialRevSelected)
-                        return _diffTwoSelected.Text;
-                    break;
-
+                    return _diffTwoSelected.Text;
             }
             return _diffNotSupported.Text;
         }
@@ -414,12 +410,8 @@ namespace GitUI.CommandsDialogs
 
             if (item.IsTracked)
             {
-                IList<GitRevision> revisions = _revisionGrid.GetSelectedRevisions();
-
-                if (revisions.Count == 0 || GitRevision.IsArtificial(revisions[0].Guid))
-                    UICommands.StartFileHistoryDialog(this, item.Name, null, false, true);
-                else
-                    UICommands.StartFileHistoryDialog(this, item.Name, revisions[0], true, true);
+                GitRevision revision = _revisionGrid.GetSelectedRevisions().FirstOrDefault();
+                UICommands.StartFileHistoryDialog(this, item.Name, revision, true, true);
             }
         }
 
@@ -509,12 +501,8 @@ namespace GitUI.CommandsDialogs
 
             if (item.IsTracked)
             {
-                IList<GitRevision> revisions = _revisionGrid.GetSelectedRevisions();
-
-                if (revisions.Count == 0 || GitRevision.IsArtificial(revisions[0].Guid))
-                    UICommands.StartFileHistoryDialog(this, item.Name);
-                else
-                    UICommands.StartFileHistoryDialog(this, item.Name, revisions[0], false);
+                GitRevision revision = _revisionGrid.GetSelectedRevisions().FirstOrDefault();
+                UICommands.StartFileHistoryDialog(this, item.Name, revision, false);
             }
         }
 
@@ -528,20 +516,19 @@ namespace GitUI.CommandsDialogs
             if (DiffFiles.SelectedItem == null)
                 return;
 
-            GitUIExtensions.DiffWithRevisionKind diffKind;
+            GitUI.RevisionDiffKind diffKind;
 
             if (sender == aLocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffALocal;
+                diffKind = GitUI.RevisionDiffKind.DiffALocal;
             else if (sender == bLocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffBLocal;
+                diffKind = GitUI.RevisionDiffKind.DiffBLocal;
             else if (sender == parentOfALocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffAParentLocal;
+                diffKind = GitUI.RevisionDiffKind.DiffAParentLocal;
             else if (sender == parentOfBLocalToolStripMenuItem)
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffBParentLocal;
+                diffKind = GitUI.RevisionDiffKind.DiffBParentLocal;
             else
             {
-                Debug.Assert(sender == aBToolStripMenuItem, "Not implemented DiffWithRevisionKind: " + sender);
-                diffKind = GitUIExtensions.DiffWithRevisionKind.DiffAB;
+                diffKind = GitUI.RevisionDiffKind.DiffAB;
             }
 
             foreach (var itemWithParent in DiffFiles.SelectedItemsWithParent)
@@ -582,7 +569,6 @@ namespace GitUI.CommandsDialogs
             }
 
             //enable *<->Local items only when local file exists
-            //no simple way to check if A (or A/B parents) is normal, ignore that some menus should be disabled
             foreach (var item in DiffFiles.SelectedItems)
             {
                 bIsNormal = bIsNormal || !(item.IsNew || item.IsDeleted);

GitUI/CommandsDialogs/RevisionFileTree.cs
@@ -363,10 +363,7 @@ See the changes in the commit form.");
             if (gitItem == null)
                 return;
 
-            if (GitRevision.IsArtificial(_revision.Guid))
-                UICommands.StartFileHistoryDialog(this, gitItem.FileName, null, false, true);
-            else
-                UICommands.StartFileHistoryDialog(this, gitItem.FileName, _revision, true, true);
+            UICommands.StartFileHistoryDialog(this, gitItem.FileName, _revision, true, true);
         }
 
         private void collapseAllToolStripMenuItem_Click(object sender, EventArgs e)
@@ -390,10 +387,7 @@ See the changes in the commit form.");
             if (gitItem == null)
                 return;
 
-            if (GitRevision.IsArtificial(_revision.Guid))
-                UICommands.StartFileHistoryDialog(this, gitItem.FileName);
-            else
-                UICommands.StartFileHistoryDialog(this, gitItem.FileName, _revision, false, false);
+            UICommands.StartFileHistoryDialog(this, gitItem.FileName, _revision, false, false);
         }
 
         private void findToolStripMenuItem_Click(object sender, EventArgs e)

commit 3ce202463e278340e9ea90caab5e9c0d5bf909aa

Git treefiles were not set as tracked, stash diff was inverted
Part of #4373
GitItemStatus.IsTracked should be set for all file-revisions handled by Git, i.e. all but working file directory files
This was not set for GetTreeFiles(). This is called for revisions without parents: First/detached commit and stash-untracked.
These files were therefore presented as the difference to the local file if local exists.

In addition, stash-diff was displayed inverted. This was a regression from #4157

GitCommands/Git/GitModule.cs
@@ -2225,8 +2225,8 @@ namespace GitCommands
             }
 
             //fix refs slashes
-            firstRevision = firstRevision == null ? "" : firstRevision.ToPosixPath();
-            secondRevision = secondRevision == null ? "" : secondRevision.ToPosixPath();
+            firstRevision = firstRevision?.ToPosixPath();
+            secondRevision = secondRevision?.ToPosixPath();
             string diffOptions = _revisionDiffProvider.Get(firstRevision, secondRevision, fileName, oldFileName, isTracked);
             if (AppSettings.UsePatienceDiffAlgorithm)
                 extraDiffArguments = string.Concat(extraDiffArguments, " --patience");
@@ -2334,6 +2334,7 @@ namespace GitCommands
             var list = tree
                 .Select(file => new GitItemStatus
                 {
+                    IsTracked = true,
                     IsNew = true,
                     IsChanged = false,
                     IsDeleted = false,

GitUI/CommandsDialogs/FormStash.cs
@@ -139,7 +139,7 @@ namespace GitUI.CommandsDialogs
             }
             else if (stashedItem != null)
             {
-                if (stashedItem.IsNew && !stashedItem.IsTracked)
+                if (stashedItem.IsNew)
                 {
                     if (!stashedItem.IsSubmodule)
                         View.ViewGitItem(stashedItem.Name, stashedItem.TreeGuid);
@@ -153,7 +153,7 @@ namespace GitUI.CommandsDialogs
                     Encoding encoding = this.View.Encoding;
                     View.ViewPatch(() =>
                     {
-                        Patch patch = Module.GetSingleDiff(gitStash.Name, gitStash.Name + "^", stashedItem.Name, stashedItem.OldName, extraDiffArguments, encoding, false);
+                        Patch patch = Module.GetSingleDiff(gitStash.Name + "^", gitStash.Name, stashedItem.Name, stashedItem.OldName, extraDiffArguments, encoding, true, stashedItem.IsTracked);
                         if (patch == null)
                             return String.Empty;
                         if (stashedItem.IsSubmodule)