c2ec7328003b58ca3ef8da6bf0409b7642bc6bfd
aivanov-oneinc <aivanov@oneincsystems.com>
2018-01-31 16:26:26 +0300

    Fix #3520. Implementated feature request (#4405)
    Add new settings thath in fact duplicates and stores values from main
    plugin window. Now default values for "Delete obsoleted branches"
    plugin fields pulled from settings.
    
    By the way. Remove redundant handlers (ClearResults method wrappers for
    specific control).

--- Plugins/DeleteUnusedBranches/DeleteUnusedBranchesPlugin.cs
+++ Plugins/DeleteUnusedBranches/DeleteUnusedBranchesPlugin.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows.Forms;
-using GitUIPluginInterfaces;
+﻿using GitUIPluginInterfaces;
 using ResourceManager;
 
 namespace DeleteUnusedBranches
@@ -14,16 +13,41 @@ namespace DeleteUnusedBranches
 
         private StringSetting MergedInBranch = new StringSetting("Branch where all branches should be merged in", "HEAD");
         private NumberSetting<int> DaysOlderThan = new NumberSetting<int>("Delete obsolete branches older than (days)", 30);
+        private BoolSetting DeleteRemoteBranchesFromFlag = new BoolSetting("Delete obsolete branches from remote", false);
+        private StringSetting RemoteName = new StringSetting("Remote name obsoleted branches should be deleted from", "origin");
+        private BoolSetting UseRegexToFilterBranchesFlag = new BoolSetting("Use regex to filter branches to delete", false);
+        private StringSetting RegexFilter = new StringSetting("Regex to filter branches to delete", "/(feature|develop)/");
+        private BoolSetting RegexCaseInsensitiveFlag = new BoolSetting("Is regex filter case insensitive?", false);
+        private BoolSetting RegexInvertedFlag = new BoolSetting("Search branches that does not match regex", false);
+        private BoolSetting IncludeUnmergedBranchesFlag = new BoolSetting("Delete unmerged branches", false);
 
         public override System.Collections.Generic.IEnumerable<ISetting> GetSettings()
         {
             yield return DaysOlderThan;
             yield return MergedInBranch;
+            yield return DeleteRemoteBranchesFromFlag;
+            yield return RemoteName;
+            yield return UseRegexToFilterBranchesFlag;
+            yield return RegexFilter;
+            yield return RegexCaseInsensitiveFlag;
+            yield return RegexInvertedFlag;
+            yield return IncludeUnmergedBranchesFlag;
         }
 
         public override bool Execute(GitUIBaseEventArgs gitUiArgs)
         {
-            using (var frm = new DeleteUnusedBranchesForm(DaysOlderThan.ValueOrDefault(Settings), MergedInBranch.ValueOrDefault(Settings), gitUiArgs.GitModule, gitUiArgs.GitUICommands, this))
+            var settings = new DeleteUnusedBranchesFormSettings(
+                DaysOlderThan.ValueOrDefault(Settings),
+                MergedInBranch.ValueOrDefault(Settings),
+                DeleteRemoteBranchesFromFlag.ValueOrDefault(Settings),
+                RemoteName.ValueOrDefault(Settings),
+                UseRegexToFilterBranchesFlag.ValueOrDefault(Settings),
+                RegexFilter.ValueOrDefault(Settings),
+                RegexCaseInsensitiveFlag.ValueOrDefault(Settings),
+                RegexInvertedFlag.ValueOrDefault(Settings),
+                IncludeUnmergedBranchesFlag.ValueOrDefault(Settings));
+
+            using (var frm = new DeleteUnusedBranchesForm(settings, gitUiArgs.GitModule, gitUiArgs.GitUICommands, this))
             {
                 frm.ShowDialog(gitUiArgs.OwnerForm);
             }
