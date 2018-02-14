38d1a0814df58e91cdfdb8fa830b06b45b39e655
Drew Noakes <git@drewnoakes.com>
2018-02-14 03:17:25 +0000

    Remove redundant code

--- Plugins/Statistics/GitImpact/ImpactControl.cs
+++ Plugins/Statistics/GitImpact/ImpactControl.cs
@@ -18,7 +18,6 @@ namespace GitImpact
         private const int LinesFontSize = 10;
         private const int WeekFontSize = 8;
 
-
         private readonly object _dataLock = new object();
 
         private ImpactLoader _impactLoader;
@@ -48,7 +47,7 @@ namespace GitImpact
             InitializeComponent();
 
             // Set DoubleBuffer flag for flicker-free drawing
-            this.SetStyle(ControlStyles.AllPaintingInWmPaint |
+            SetStyle(ControlStyles.AllPaintingInWmPaint |
                 ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
 
             MouseWheel += ImpactControl_MouseWheel;
@@ -86,7 +85,7 @@ namespace GitImpact
 
         private void ImpactControl_MouseWheel(object sender, MouseEventArgs e)
         {
-            this._scrollBar.Value = Math.Min(this._scrollBar.Maximum, Math.Max(this._scrollBar.Minimum, this._scrollBar.Value + e.Delta));
+            _scrollBar.Value = Math.Min(_scrollBar.Maximum, Math.Max(_scrollBar.Minimum, _scrollBar.Value + e.Delta));
             // Redraw when we've scrolled
             Invalidate();
         }
@@ -162,28 +161,28 @@ namespace GitImpact
 
         private void InitializeComponent()
         {
-            this._scrollBar = new System.Windows.Forms.HScrollBar();
-            this.SuspendLayout();
+            _scrollBar = new HScrollBar();
+            SuspendLayout();
             //
             // scrollBar
             //
-            this._scrollBar.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this._scrollBar.LargeChange = 0;
-            this._scrollBar.Location = new System.Drawing.Point(0, 133);
-            this._scrollBar.Maximum = 0;
-            this._scrollBar.Name = "_scrollBar";
-            this._scrollBar.Size = new System.Drawing.Size(150, 17);
-            this._scrollBar.SmallChange = 0;
-            this._scrollBar.TabIndex = 0;
-            this._scrollBar.Scroll += this.OnScroll;
+            _scrollBar.Dock = DockStyle.Bottom;
+            _scrollBar.LargeChange = 0;
+            _scrollBar.Location = new Point(0, 133);
+            _scrollBar.Maximum = 0;
+            _scrollBar.Name = "_scrollBar";
+            _scrollBar.Size = new Size(150, 17);
+            _scrollBar.SmallChange = 0;
+            _scrollBar.TabIndex = 0;
+            _scrollBar.Scroll += OnScroll;
             //
             // ImpactControl
             //
-            this.Controls.Add(this._scrollBar);
-            this.Name = "ImpactControl";
-            this.Paint += this.OnPaint;
-            this.Resize += this.OnResize;
-            this.ResumeLayout(false);
+            Controls.Add(_scrollBar);
+            Name = "ImpactControl";
+            Paint += OnPaint;
+            Resize += OnResize;
+            ResumeLayout(false);
 
         }
 
