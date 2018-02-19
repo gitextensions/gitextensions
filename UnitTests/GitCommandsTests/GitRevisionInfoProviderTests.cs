40bb2256208ba3258b9896f38355fbc7baf0cd69
Drew Noakes <git@drewnoakes.com>
2018-02-19 10:14:50 +0000

    Fix GitRevisionInfoProviderTests

--- UnitTests/GitCommandsTests/GitRevisionInfoProviderTests.cs
+++ UnitTests/GitCommandsTests/GitRevisionInfoProviderTests.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using FluentAssertions;
 using GitCommands;
 using GitCommands.Git;
@@ -15,7 +17,6 @@ namespace GitCommandsTests
         private IGitModule _module;
         private GitRevisionInfoProvider _provider;
 
-
         [SetUp]
         public void Setup()
         {
@@ -26,7 +27,7 @@ namespace GitCommandsTests
         [Test]
         public void LoadChildren_should_throw_if_null()
         {
-            ((Action)(() => _provider.LoadChildren(null))).ShouldThrow<ArgumentNullException>();
+            Assert.ThrowsAsync<ArgumentNullException>(() => _provider.LoadChildren(null));
         }
 
         [TestCase(null)]
@@ -37,7 +38,7 @@ namespace GitCommandsTests
             var item = Substitute.For<IGitItem>();
             item.Guid.Returns(guid);
 
-            ((Action)(() => _provider.LoadChildren(item))).ShouldThrow<ArgumentException>();
+            Assert.ThrowsAsync<ArgumentException>(() => _provider.LoadChildren(item));
         }
 
         [Test]
@@ -49,40 +50,41 @@ namespace GitCommandsTests
 
             _provider = new GitRevisionInfoProvider(() => null);
 
-            ((Action)(() => _provider.LoadChildren(item))).ShouldThrow<ArgumentException>();
+            Assert.ThrowsAsync<ArgumentException>(() => _provider.LoadChildren(item));
         }
 
         [Test]
-        public void LoadChildren_should_return_shallow_tree_for_non_GitItem()
+        public async Task LoadChildren_should_return_shallow_tree_for_non_GitItem()
         {
             var guid = Guid.NewGuid().ToString("N");
             var item = Substitute.For<IGitItem>();
             item.Guid.Returns(guid);
 
             var items = new[] { Substitute.For<IGitItem>(), Substitute.For<IGitItem>(), Substitute.For<IGitItem>() };
-            _module.GetTree(guid, false).Returns(items);
 
-            var children = _provider.LoadChildren(item);
+            _module.GetTreeAsync(guid, false).Returns(items);
+
+            var children = await _provider.LoadChildren(item);
 
             children.Should().BeEquivalentTo(items);
-            _module.Received(1).GetTree(guid, false);
+            _module.Received(1).GetTreeAsync(guid, false);
         }
 
         [Test]
-        public void LoadChildren_should_return_shallow_tree_for_GitItem_with_updated_FileName()
+        public async Task LoadChildren_should_return_shallow_tree_for_GitItem_with_updated_FileName()
         {
-            var guid = Guid.NewGuid().ToString("N");
-            var item = new GitItem("", "", guid, "folder");
+            var item = new GitItem("", "", Guid.NewGuid().ToString("N"), "folder");
+            var expectedChildren = new[] { Substitute.For<IGitItem>(), new GitItem("", "", "", "file2"), new GitItem("", "", "", "file3") };
 
-            var items = new[] { Substitute.For<IGitItem>(), new GitItem("", "", "", "file2"), new GitItem("", "", "", "file3") };
-            _module.GetTree(guid, false).Returns(items);
+            _module.GetTreeAsync(item.Guid, full: false).Returns(expectedChildren);
 
-            var children = _provider.LoadChildren(item);
+            var children = await _provider.LoadChildren(item);
 
-            children.Should().BeEquivalentTo(items);
-            ((GitItem)items[1]).FileName.Should().Be(Path.Combine(item.FileName, "file2"));
-            ((GitItem)items[2]).FileName.Should().Be(Path.Combine(item.FileName, "file3"));
-            _module.Received(1).GetTree(guid, false);
+            children.Count().Should().Be(3);
+            children.Should().BeEquivalentTo(expectedChildren);
+            ((GitItem)expectedChildren[1]).FileName.Should().Be(Path.Combine(item.FileName, "file2"));
+            ((GitItem)expectedChildren[2]).FileName.Should().Be(Path.Combine(item.FileName, "file3"));
+            _module.Received(1).GetTreeAsync(item.Guid, false);
         }
     }
 }
