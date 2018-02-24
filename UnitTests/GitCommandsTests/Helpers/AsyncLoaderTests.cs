18e842e1b038078002e2dc89661b8fe54ac12c16
Drew Noakes <git@drewnoakes.com>
2018-02-24 00:23:18 +0000

    Test and fix AsyncLoader (fixes #4517)
    Following #4501 I started looking at the behaviour of AsyncLoader
    and adding some tests. This flagged up a mismatch between the intention
    and behaviour of the class.
    
    This PR adds thorough tests to AsyncLoader, and fixes the bug around
    cancellation during disposal.
    
    It also allows more than one thread to specify DefaultContinuationTaskScheduler
    via the use of ThreadLocal.
    
    Added more documentation as my understanding grew.
    
    Prevent use-after-dispose.

---
+++ UnitTests/GitCommandsTests/Helpers/AsyncLoaderTests.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using GitCommands;
+using NUnit.Framework;
+
+namespace GitCommandsTests.Helpers
+{
+    public sealed class AsyncLoaderTests
+    {
+        private AsyncLoader _loader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+
+            _loader = new AsyncLoader();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _loader.Dispose();
+        }
+
+        [Test]
+        public async Task RegularCase()
+        {
+            var loadSignal = new SemaphoreSlim(0);
+
+            var started = 0;
+            var completed = 0;
+            var task = _loader.Load(
+                () =>
+                {
+                    started++;
+                    loadSignal.Release();
+                    Thread.Sleep(100);
+                },
+                () => completed++);
+
+            Assert.True(await loadSignal.WaitAsync(1000));
+
+            Assert.AreEqual(1, started);
+            Assert.AreEqual(0, completed);
+
+            await task;
+
+            Assert.AreEqual(1, started);
+            Assert.AreEqual(1, completed);
+
+            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+        }
+
+        [Test]
+        public async Task Threads()
+        {
+            var callerThread = Thread.CurrentThread;
+            Thread loadThread = null;
+            Thread continuationThread = null;
+
+            Assert.False(callerThread.IsThreadPoolThread);
+
+            using (var scheduler = new SingleThreadTaskScheduler())
+            using (var loader = new AsyncLoader(scheduler))
+            {
+                await loader.Load(
+                    () => loadThread = Thread.CurrentThread,
+                    () => continuationThread = Thread.CurrentThread);
+            }
+
+            Assert.True(loadThread.IsThreadPoolThread);
+            Assert.AreNotSame(loadThread, callerThread);
+            Assert.AreNotSame(loadThread, continuationThread);
+        }
+
+        [Test]
+        public async Task CancelDuringLoad()
+        {
+            var loadSignal = new SemaphoreSlim(0);
+
+            var started = 0;
+            var completed = 0;
+            var task = _loader.Load(
+                () =>
+                {
+                    started++;
+                    loadSignal.Release();
+                    Thread.Sleep(100);
+                },
+                () => completed++);
+
+            Assert.True(await loadSignal.WaitAsync(1000));
+
+            Assert.AreEqual(1, started);
+            Assert.AreEqual(0, completed);
+
+            _loader.Cancel();
+
+            await task;
+
+            Assert.AreEqual(1, started);
+            Assert.AreEqual(0, completed, "Should not have called the follow-up action");
+
+            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+        }
+
+        [Test]
+        public async Task CancelDuringDelay()
+        {
+            // Deliberately use a long delay as cancellation should cut it short
+            _loader.Delay = TimeSpan.FromMilliseconds(5000);
+
+            var started = 0;
+            var completed = 0;
+            var task = _loader.Load(
+                () => started++,
+                () => completed++);
+
+            await Task.Delay(50);
+
+            Assert.AreEqual(0, started);
+            Assert.AreEqual(0, completed);
+
+            _loader.Cancel();
+
+            await task;
+
+            Assert.AreEqual(0, started);
+            Assert.AreEqual(0, completed);
+
+            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+        }
+
+        [Test]
+        public async Task DisposeCalledDuringOperation()
+        {
+            var loadSignal = new SemaphoreSlim(0);
+
+            var started = 0;
+            var completed = 0;
+            var task = _loader.Load(
+                () =>
+                {
+                    started++;
+                    loadSignal.Release();
+                    Thread.Sleep(100);
+                },
+                () => completed++);
+
+            Assert.True(await loadSignal.WaitAsync(1000));
+
+            Assert.AreEqual(1, started);
+            Assert.AreEqual(0, completed);
+
+            _loader.Dispose();
+
+            await task;
+
+            Assert.AreEqual(1, started);
+            Assert.AreEqual(0, completed, "Should not have called the follow-up action");
+
+            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+        }
+
+        [Test]
+        public async Task DelayedLoad()
+        {
+            _loader.Delay = TimeSpan.FromMilliseconds(100);
+
+            var sw = Stopwatch.StartNew();
+
+            await _loader.Load(
+                () => sw.Stop(),
+                () => { });
+
+            Assert.GreaterOrEqual(sw.Elapsed, _loader.Delay);
+        }
+
+        [Test]
+        public async Task ErrorEventHandled()
+        {
+            var observed = new List<Exception>();
+
+            _loader.LoadingError += (_, e) =>
+            {
+                observed.Add(e.Exception);
+                e.Handled = true;
+            };
+
+            var ex = new Exception();
+
+            await _loader.Load(
+                () => throw ex,
+                Assert.Fail);
+
+            Assert.AreEqual(1, observed.Count);
+            Assert.AreSame(ex, observed[0]);
+        }
+
+        [Test]
+        public void ErrorEventUnhandled()
+        {
+            var observed = new List<Exception>();
+
+            _loader.LoadingError += (_, e) =>
+            {
+                observed.Add(e.Exception);
+                e.Handled = false;
+            };
+
+            var ex = new Exception();
+
+            var oe = Assert.ThrowsAsync<Exception>(() => _loader.Load(
+                () => throw ex,
+                Assert.Fail));
+
+            Assert.AreEqual(1, observed.Count);
+            Assert.AreSame(ex, observed[0]);
+            Assert.AreSame(oe, observed[0]);
+        }
+
+        [Test]
+        public void UseAfterDispose()
+        {
+            var loader = new AsyncLoader();
+
+            // Safe to dispose multiple times
+            loader.Dispose();
+            loader.Dispose();
+            loader.Dispose();
+
+            // Any use after dispose should throw
+            Assert.Throws<ObjectDisposedException>(() => loader.Load(() => { }, () => { }));
+            Assert.Throws<ObjectDisposedException>(() => loader.Load(_ => { }, () => { }));
+            Assert.Throws<ObjectDisposedException>(() => loader.Load(() => 1, i => { }));
+            Assert.Throws<ObjectDisposedException>(() => loader.Load(_ => 1, i => { }));
+            Assert.Throws<ObjectDisposedException>(() => loader.Cancel());
+        }
+    }
+}
---
+++ UnitTests/GitCommandsTests/Helpers/SingleThreadTaskScheduler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitCommandsTests.Helpers
+{
+    /// <summary>
+    /// Simple <see cref="TaskScheduler"/> implementation that owns a single thread
+    /// and schedules all work on it. No tasks may execute inline under this scheduler.
+    /// </summary>
+    /// <remarks>Only really useful for testing purposes.</remarks>
+    internal sealed class SingleThreadTaskScheduler : TaskScheduler, IDisposable
+    {
+        private readonly BlockingCollection<Task> _queue = new BlockingCollection<Task>();
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private readonly Thread _thread;
+
+        public SingleThreadTaskScheduler()
+        {
+            _thread = new Thread(() =>
+            {
+                try
+                {
+                    foreach (var task in _queue.GetConsumingEnumerable(_cts.Token))
+                    {
+                        TryExecuteTask(task);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // ignored
+                }
+            });
+
+            _thread.Start();
+        }
+
+        protected override void QueueTask(Task task) => _queue.Add(task);
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) => false;
+
+        protected override IEnumerable<Task> GetScheduledTasks() => _queue;
+
+        public void Dispose()
+        {
+            _cts.Cancel();
+            _thread.Join();
+            _cts.Dispose();
+            _queue.Dispose();
+        }
+    }
+}
