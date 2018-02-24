b7b548356630b26a5c3cd768ba539cebf9a9ae77
Drew Noakes <git@drewnoakes.com>
2018-02-24 15:14:39 +0000

    Reword test method names

--- UnitTests/GitCommandsTests/AsyncLoaderTests.cs
+++ UnitTests/GitCommandsTests/AsyncLoaderTests.cs
@@ -28,7 +28,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public async Task RegularCase()
+        public async Task Load_should_execute_async_operation_and_callback()
         {
             var loadSignal = new SemaphoreSlim(0);
 
@@ -57,7 +57,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public async Task Threads()
+        public async Task Load_performed_on_thread_pool_and_result_handled_via_callers_context()
         {
             var callerThread = Thread.CurrentThread;
             Thread loadThread = null;
@@ -79,7 +79,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public async Task CancelDuringLoad()
+        public async Task Cancel_during_load_means_callback_not_fired()
         {
             var loadSignal = new SemaphoreSlim(0);
 
@@ -110,7 +110,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public async Task CancelDuringDelay()
+        public async Task Cancel_during_delay_means_load_not_fired()
         {
             // Deliberately use a long delay as cancellation should cut it short
             _loader.Delay = TimeSpan.FromMilliseconds(5000);
@@ -137,7 +137,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public async Task DisposeCalledDuringOperation()
+        public async Task Dispose_during_load_means_callback_not_fired()
         {
             var loadSignal = new SemaphoreSlim(0);
 
@@ -168,7 +168,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public async Task DelayedLoad()
+        public async Task Delay_causes_load_callback_to_be_deferred()
         {
             _loader.Delay = TimeSpan.FromMilliseconds(100);
 
@@ -182,7 +182,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public async Task ErrorEventHandled()
+        public async Task Error_raised_via_event_and_marked_as_handled_does_not_fault_task()
         {
             var observed = new List<Exception>();
 
@@ -203,7 +203,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public void ErrorEventUnhandled()
+        public void Error_raised_via_event_and_not_marked_as_handled_faults_task()
         {
             var observed = new List<Exception>();
 
@@ -225,7 +225,7 @@ namespace GitCommandsTests
         }
 
         [Test]
-        public void UseAfterDispose()
+        public void Using_load_or_cancel_after_dispose_throws()
         {
             var loader = new AsyncLoader();
 
