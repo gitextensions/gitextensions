bf44900cb5233780efd7d71c7065bcadd3ef0cfd
Markas Gedminas <markas.gedminas@devbridge.com>
2018-03-06 15:08:24 +0200

    Force branch selection when opening 'Merge branch' dialog (Ctrl + M)
    (cherry picked from commit 78e12a72c85e1e95379d1e016d3923156e705745)

--- GitUI/CommandsDialogs/FormMergeBranch.cs
+++ GitUI/CommandsDialogs/FormMergeBranch.cs
@@ -46,6 +46,8 @@ namespace GitUI.CommandsDialogs
 
             advanced.Checked = AppSettings.AlwaysShowAdvOpt;
             advanced_CheckedChanged(null, null);
+
+            Branches.Select();
         }
 
 
