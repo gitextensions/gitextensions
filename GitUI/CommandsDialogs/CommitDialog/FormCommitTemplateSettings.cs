a5b92294d2a920355d2532c199bcdd080f55fd66
Drew Noakes <git@drewnoakes.com>
2018-03-15 18:00:50 +0000

    Reuse ShortenTo

--- GitUI/CommandsDialogs/CommitDialog/FormCommitTemplateSettings.cs
+++ GitUI/CommandsDialogs/CommitDialog/FormCommitTemplateSettings.cs
@@ -102,8 +102,7 @@ namespace GitUI.CommandsDialogs.CommitDialog
 
             if (!_commitTemplates[line].Name.IsNullOrEmpty())
             {
-                comboBoxText = _commitTemplates[line].Name.Substring(0, _commitTemplates[line].Name.Length > _maxShownCharsForName ? (_maxShownCharsForName - 3) : _commitTemplates[line].Name.Length);
-                comboBoxText += _commitTemplates[line].Name.Length > _maxShownCharsForName ? "..." : "";
+                comboBoxText = _commitTemplates[line].Name.ShortenTo(_maxShownCharsForName);
             }
             else
             {
