25637bc63a6bf9009efec1087259912d0462722c
Drew Noakes <git@drewnoakes.com>
2018-03-15 17:50:28 +0000

    Remove unused StringExtension methods

--- GitCommands/System.cs
+++ GitCommands/System.cs
@@ -178,49 +178,10 @@ namespace System
             return sb.ToString();
         }
 
-        /// <summary>Split a string, removing empty entries, then trim whitespace.</summary>
-        public static IEnumerable<string> SplitThenTrim(this string value, params string[] separator)
-        {
-            return value
-                .Split(separator, StringSplitOptions.RemoveEmptyEntries)
-                .Select(s => s.Trim());
-        }
-
-        /// <summary>Split a string, removing empty entries, then trim whitespace.</summary>
-        public static IEnumerable<string> SplitThenTrim(this string value, params char[] separator)
-        {
-            return value
-                .Split(separator, StringSplitOptions.RemoveEmptyEntries)
-                .Select(s => s.Trim());
-        }
-
         /// <summary>Split a string, delimited by line-breaks, excluding empty entries.</summary>
         public static string[] SplitLines(this string value)
         {
             return value.Split(NewLineSeparator, StringSplitOptions.RemoveEmptyEntries);
-        }
-
-        /// <summary>Split a string, delimited by line-breaks, excluding empty entries; then trim whitespace.</summary>
-        public static IEnumerable<string> SplitLinesThenTrim(this string value)
-        {
-            return value.SplitThenTrim(NewLineSeparator);
-        }
-
-        /// <summary>Gets the text after the last separator.
-        /// If NO separator OR ends with separator, returns the original value.</summary>
-        public static string SubstringAfterLastSafe(this string value, string separator)
-        {// ex: "origin/master" -> "master"
-            if (value.EndsWith(separator) || !value.Contains(separator))
-            {// "origin/master/" OR "master" -> return original
-                return value;
-            }
-
-            return value.Substring(1 + value.LastIndexOf(separator, StringComparison.InvariantCultureIgnoreCase));
-        }
-
-        public static string SubstringAfterFirst(this string value, string separator)
-        {
-            return value.Substring(1 + value.IndexOf(separator, StringComparison.InvariantCultureIgnoreCase));
         }
 
         /// <summary>
