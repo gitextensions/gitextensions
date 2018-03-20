d7ade4cbf7f5e937836f110be21ec417fe663356
Drew Noakes <git@drewnoakes.com>
2018-03-20 13:48:12 +0000

    Add DeconstructionExtensions

--- GitCommands/System.cs
+++ GitCommands/System.cs
@@ -245,4 +245,52 @@ namespace System
             return force ? " -f " : string.Empty;
         }
     }
+
+    public static class DeconstructionExtensions
+    {
+        /// <summary>
+        /// Supports C# 7 deconstruction of <see cref="KeyValuePair{TKey,TValue}"/>.
+        /// </summary>
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> source, out TKey key, out TValue value)
+        {
+            key = source.Key;
+            value = source.Value;
+        }
+
+        /// <summary>
+        /// Supports C# 7 deconstruction of <see cref="IGrouping{TKey,TElement}"/>.
+        /// </summary>
+        public static void Deconstruct<TKey, TElement>(this IGrouping<TKey, TElement> source, out TKey key, out IEnumerable<TElement> elements)
+        {
+            key = source.Key;
+            elements = source;
+        }
+
+        /// <summary>
+        /// Supports C# 7 deconstruction of <see cref="Tuple{T1}"/>.
+        /// </summary>
+        public static void Deconstruct<T1>(this Tuple<T1> source, out T1 item1)
+        {
+            item1 = source.Item1;
+        }
+
+        /// <summary>
+        /// Supports C# 7 deconstruction of <see cref="Tuple{T1,T2}"/>.
+        /// </summary>
+        public static void Deconstruct<T1, T2>(this Tuple<T1, T2> source, out T1 item1, out T2 item2)
+        {
+            item1 = source.Item1;
+            item2 = source.Item2;
+        }
+
+        /// <summary>
+        /// Supports C# 7 deconstruction of <see cref="Tuple{T1,T2,T3}"/>.
+        /// </summary>
+        public static void Deconstruct<T1, T2, T3>(this Tuple<T1, T2, T3> source, out T1 item1, out T2 item2, out T3 item3)
+        {
+            item1 = source.Item1;
+            item2 = source.Item2;
+            item3 = source.Item3;
+        }
+    }
 }
