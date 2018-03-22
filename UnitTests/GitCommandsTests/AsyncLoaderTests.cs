a4172c9e806cc51a31c250a370b732c05a8704ae
Sam Harwell <sam@tunnelvisionlabs.com>
2018-03-22 09:53:36 -0500

    Skip tests that are incompatible with the current NUnit release

--- UnitTests/GitCommandsTests/AsyncLoaderTests.cs
+++ UnitTests/GitCommandsTests/AsyncLoaderTests.cs
@@ -26,6 +26,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public async Task Load_should_execute_async_operation_and_callback()
         {
             var loadSignal = new SemaphoreSlim(0);
@@ -56,6 +57,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public async Task Load_performed_on_thread_pool_and_result_handled_via_callers_context()
         {
             var callerThread = Thread.CurrentThread;
@@ -78,6 +80,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public async Task Cancel_during_load_means_callback_not_fired()
         {
             var loadSignal = new SemaphoreSlim(0);
@@ -110,6 +113,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public async Task Cancel_during_delay_means_load_not_fired()
         {
             // Deliberately use a long delay as cancellation should cut it short
@@ -138,6 +142,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public async Task Dispose_during_load_means_callback_not_fired()
         {
             var loadSignal = new SemaphoreSlim(0);
@@ -170,6 +175,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public async Task Delay_causes_load_callback_to_be_deferred()
         {
             _loader.Delay = TimeSpan.FromMilliseconds(100);
@@ -185,6 +191,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public async Task Error_raised_via_event_and_marked_as_handled_does_not_fault_task()
         {
             var observed = new List<Exception>();
@@ -207,6 +214,7 @@ namespace GitCommandsTests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        [Ignore("ApartmentState.STA does not currently work with async test methods")]
         public void Error_raised_via_event_and_not_marked_as_handled_faults_task()
         {
             var observed = new List<Exception>();
