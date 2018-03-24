c53a5ee9f3d789f84f8c4cc73005efb7b3462e1d
Sam Harwell <sam@tunnelvisionlabs.com>
2018-03-24 11:03:49 -0500

    Fix AsyncLoaderTests by using JTF as the message pump

--- UnitTests/GitCommandsTests/AsyncLoaderTests.cs
+++ UnitTests/GitCommandsTests/AsyncLoaderTests.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using GitCommands;
+using GitUI;
 using NUnit.Framework;
 
 namespace GitCommandsTests
@@ -26,188 +27,201 @@ namespace GitCommandsTests
         }
 
         [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
-        public async Task Load_should_execute_async_operation_and_callback()
+        public void Load_should_execute_async_operation_and_callback()
         {
-            var loadSignal = new SemaphoreSlim(0);
-
-            var started = 0;
-            var completed = 0;
-            var task = _loader.LoadAsync(
-                () =>
-                {
-                    started++;
-                    loadSignal.Release();
-                    Thread.Sleep(100);
-                },
-                () => completed++);
-
-            Assert.True(await loadSignal.WaitAsync(1000));
-
-            Assert.AreEqual(1, started);
-            Assert.AreEqual(0, completed);
-
-            await task;
-
-            Assert.AreEqual(1, started);
-            Assert.AreEqual(1, completed);
-
-            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
-        }
-
-        [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
-        public async Task Load_performed_on_thread_pool_and_result_handled_via_callers_context()
-        {
-            var callerThread = Thread.CurrentThread;
-            Thread loadThread = null;
-            Thread continuationThread = null;
-
-            Assert.False(callerThread.IsThreadPoolThread);
-
-            using (var loader = new AsyncLoader())
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
             {
-                await loader.LoadAsync(
-                    () => loadThread = Thread.CurrentThread,
-                    () => continuationThread = Thread.CurrentThread);
-            }
+                var loadSignal = new SemaphoreSlim(0);
 
-            Assert.True(loadThread.IsThreadPoolThread);
-            Assert.AreNotSame(loadThread, callerThread);
-            Assert.AreNotSame(loadThread, continuationThread);
+                var started = 0;
+                var completed = 0;
+                var task = _loader.LoadAsync(
+                    () =>
+                    {
+                        started++;
+                        loadSignal.Release();
+                        Thread.Sleep(100);
+                    },
+                    () => completed++);
+
+                Assert.True(await loadSignal.WaitAsync(1000));
+
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(0, completed);
+
+                await task;
+
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(1, completed);
+
+                Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+            });
         }
 
         [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
-        public async Task Cancel_during_load_means_callback_not_fired()
+        public void Load_performed_on_thread_pool_and_result_handled_via_callers_context()
         {
-            var loadSignal = new SemaphoreSlim(0);
-
-            var started = 0;
-            var completed = 0;
-            var task = _loader.LoadAsync(
-                () =>
-                {
-                    started++;
-                    loadSignal.Release();
-                    Thread.Sleep(100);
-                },
-                () => completed++);
-
-            Assert.True(await loadSignal.WaitAsync(1000));
-
-            Assert.AreEqual(1, started);
-            Assert.AreEqual(0, completed);
-
-            _loader.Cancel();
-
-            await task;
-
-            Assert.AreEqual(1, started);
-            Assert.AreEqual(0, completed, "Should not have called the follow-up action");
-
-            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
-        }
-
-        [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
-        public async Task Cancel_during_delay_means_load_not_fired()
-        {
-            // Deliberately use a long delay as cancellation should cut it short
-            _loader.Delay = TimeSpan.FromMilliseconds(5000);
-
-            var started = 0;
-            var completed = 0;
-            var task = _loader.LoadAsync(
-                () => started++,
-                () => completed++);
-
-            await Task.Delay(50);
-
-            Assert.AreEqual(0, started);
-            Assert.AreEqual(0, completed);
-
-            _loader.Cancel();
-
-            await task;
-
-            Assert.AreEqual(0, started);
-            Assert.AreEqual(0, completed);
-
-            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
-        }
-
-        [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
-        public async Task Dispose_during_load_means_callback_not_fired()
-        {
-            var loadSignal = new SemaphoreSlim(0);
-
-            var started = 0;
-            var completed = 0;
-            var task = _loader.LoadAsync(
-                () =>
-                {
-                    started++;
-                    loadSignal.Release();
-                    Thread.Sleep(100);
-                },
-                () => completed++);
-
-            Assert.True(await loadSignal.WaitAsync(1000));
-
-            Assert.AreEqual(1, started);
-            Assert.AreEqual(0, completed);
-
-            _loader.Dispose();
-
-            await task;
-
-            Assert.AreEqual(1, started);
-            Assert.AreEqual(0, completed, "Should not have called the follow-up action");
-
-            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
-        }
-
-        [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
-        public async Task Delay_causes_load_callback_to_be_deferred()
-        {
-            _loader.Delay = TimeSpan.FromMilliseconds(100);
-
-            var sw = Stopwatch.StartNew();
-
-            await _loader.LoadAsync(
-                () => sw.Stop(),
-                () => { });
-
-            Assert.GreaterOrEqual(sw.Elapsed, _loader.Delay - TimeSpan.FromMilliseconds(10));
-        }
-
-        [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
-        public async Task Error_raised_via_event_and_marked_as_handled_does_not_fault_task()
-        {
-            var observed = new List<Exception>();
-
-            _loader.LoadingError += (_, e) =>
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
             {
-                observed.Add(e.Exception);
-                e.Handled = true;
-            };
+                var callerThread = Thread.CurrentThread;
+                Thread loadThread = null;
+                Thread continuationThread = null;
 
-            var ex = new Exception();
+                Assert.False(callerThread.IsThreadPoolThread);
 
-            await _loader.LoadAsync(
-                () => throw ex,
-                Assert.Fail);
+                using (var loader = new AsyncLoader())
+                {
+                    await loader.LoadAsync(
+                        () => loadThread = Thread.CurrentThread,
+                        () => continuationThread = Thread.CurrentThread);
+                }
 
-            Assert.AreEqual(1, observed.Count);
-            Assert.AreSame(ex, observed[0]);
+                Assert.True(loadThread.IsThreadPoolThread);
+                Assert.AreNotSame(loadThread, callerThread);
+                Assert.AreNotSame(loadThread, continuationThread);
+            });
         }
 
         [Test]
-        [Ignore("ApartmentState.STA does not currently work with async test methods")]
+        public void Cancel_during_load_means_callback_not_fired()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                var loadSignal = new SemaphoreSlim(0);
+
+                var started = 0;
+                var completed = 0;
+                var task = _loader.LoadAsync(
+                    () =>
+                    {
+                        started++;
+                        loadSignal.Release();
+                        Thread.Sleep(100);
+                    },
+                    () => completed++);
+
+                Assert.True(await loadSignal.WaitAsync(1000));
+
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(0, completed);
+
+                _loader.Cancel();
+
+                await task;
+
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(0, completed, "Should not have called the follow-up action");
+
+                Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+            });
+        }
+
+        [Test]
+        public void Cancel_during_delay_means_load_not_fired()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                // Deliberately use a long delay as cancellation should cut it short
+                _loader.Delay = TimeSpan.FromMilliseconds(5000);
+
+                var started = 0;
+                var completed = 0;
+                var task = _loader.LoadAsync(
+                    () => started++,
+                    () => completed++);
+
+                await Task.Delay(50);
+
+                Assert.AreEqual(0, started);
+                Assert.AreEqual(0, completed);
+
+                _loader.Cancel();
+
+                await task;
+
+                Assert.AreEqual(0, started);
+                Assert.AreEqual(0, completed);
+
+                Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+            });
+        }
+
+        [Test]
+        public void Dispose_during_load_means_callback_not_fired()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                var loadSignal = new SemaphoreSlim(0);
+
+                var started = 0;
+                var completed = 0;
+                var task = _loader.LoadAsync(
+                    () =>
+                    {
+                        started++;
+                        loadSignal.Release();
+                        Thread.Sleep(100);
+                    },
+                    () => completed++);
+
+                Assert.True(await loadSignal.WaitAsync(1000));
+
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(0, completed);
+
+                _loader.Dispose();
+
+                await task;
+
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(0, completed, "Should not have called the follow-up action");
+
+                Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+            });
+        }
+
+        [Test]
+        public void Delay_causes_load_callback_to_be_deferred()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                _loader.Delay = TimeSpan.FromMilliseconds(100);
+
+                var sw = Stopwatch.StartNew();
+
+                await _loader.LoadAsync(
+                    () => sw.Stop(),
+                    () => { });
+
+                Assert.GreaterOrEqual(sw.Elapsed, _loader.Delay - TimeSpan.FromMilliseconds(10));
+            });
+        }
+
+        [Test]
+        public void Error_raised_via_event_and_marked_as_handled_does_not_fault_task()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                var observed = new List<Exception>();
+
+                _loader.LoadingError += (_, e) =>
+                {
+                    observed.Add(e.Exception);
+                    e.Handled = true;
+                };
+
+                var ex = new Exception();
+
+                await _loader.LoadAsync(
+                    () => throw ex,
+                    Assert.Fail);
+
+                Assert.AreEqual(1, observed.Count);
+                Assert.AreSame(ex, observed[0]);
+            });
+        }
+
+        [Test]
         public void Error_raised_via_event_and_not_marked_as_handled_faults_task()
         {
             var observed = new List<Exception>();
@@ -220,9 +234,11 @@ namespace GitCommandsTests
 
             var ex = new Exception();
 
-            var oe = Assert.ThrowsAsync<Exception>(() => _loader.LoadAsync(
+            var loadTask = ThreadHelper.JoinableTaskFactory.RunAsync(() => _loader.LoadAsync(
                 () => throw ex,
                 Assert.Fail));
+
+            var oe = Assert.Throws<Exception>(() => loadTask.Join());
 
             Assert.AreEqual(1, observed.Count);
             Assert.AreSame(ex, observed[0]);
