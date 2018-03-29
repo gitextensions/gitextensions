496fe4ed2f1afff3339be7f76bd13a9349d384fc
Drew Noakes <git@drewnoakes.com>
2018-03-29 12:10:13 +0100

    Annotate IImageNameProvider and enforce in implementation

--- Gravatar/AvatarImageNameProvider.cs
+++ Gravatar/AvatarImageNameProvider.cs
@@ -1,4 +1,7 @@
-﻿namespace Gravatar
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Gravatar
 {
     public interface IImageNameProvider
     {
@@ -7,7 +10,9 @@
         /// </summary>
         /// <param name="email">The email.</param>
         /// <returns>The image filename.</returns>
-        string Get(string email);
+        /// <exception cref="ArgumentException"><paramref name="email"/> is <c>null</c> or white-space.</exception>
+        [NotNull]
+        string Get([NotNull] string email);
     }
 
     /// <summary>
@@ -15,16 +20,11 @@
     /// </summary>
     public sealed class AvatarImageNameProvider : IImageNameProvider
     {
-        /// <summary>
-        /// Returns the avatar image filename for the given email.
-        /// </summary>
-        /// <param name="email">The email.</param>
-        /// <returns>The avatar image filename, if email is supplied; otherwise <see langword="null"/>.</returns>
-        public string Get(string email)
+        string IImageNameProvider.Get(string email)
         {
             if (string.IsNullOrWhiteSpace(email))
             {
-                return null;
+                throw new ArgumentException("Must be a non-blank value.", nameof(email));
             }
 
             return $"{email}.png";
