gitextensions/gitextensions

commit 83a151b869acbc697c1ec74008fca27c9e23f8bf

Add ArgumentBuilder and tests

GitCommands/ArgumentBuilderExtensions.cs
@@ -1,0 +1,204 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using GitCommands.Git;
+using JetBrains.Annotations;
+
+namespace GitCommands
+{
+    /// <summary>
+    /// Extension methods for working with <see cref="ArgumentBuilder"/>.
+    /// </summary>
+    public static class ArgumentBuilderExtensions
+    {
+        /// <summary>
+        /// Adds <paramref name="ifConditionTrue"/> to the argument list if <paramref name="condition"/>
+        /// is <c>true</c>.
+        /// </summary>
+        /// <remarks>
+        /// If <paramref name="ifConditionTrue"/> is <c>null</c> or white-space, then no change is made
+        /// to the argument list, regardless of the value of <paramref name="condition"/>.
+        /// </remarks>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="condition">Whether or not to add <paramref name="ifConditionTrue"/> to the argument list.</param>
+        /// <param name="ifConditionTrue">The string to add if <paramref name="condition"/> is <c>true</c>.</param>
+        public static void Add(this ArgumentBuilder builder, bool condition, [CanBeNull] string ifConditionTrue)
+        {
+            if (condition)
+            {
+                builder.Add(ifConditionTrue);
+            }
+        }
+
+        /// <summary>
+        /// Adds either <paramref name="ifConditionTrue"/> or <paramref name="ifConditionFalse"/> to the
+        /// argument list, depending upon the value of <paramref name="condition"/>.
+        /// </summary>
+        /// <remarks>
+        /// If <paramref name="ifConditionTrue"/> is <c>null</c> or white-space, then no change is made
+        /// to the argument list, regardless of the value of <paramref name="condition"/>.
+        /// </remarks>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="condition">Whether or not to add <paramref name="ifConditionTrue"/> to the argument list.</param>
+        /// <param name="ifConditionTrue">The string to add if <paramref name="condition"/> is <c>true</c>.</param>
+        /// <param name="ifConditionFalse">The string to add if <paramref name="condition"/> is <c>false</c>.</param>
+        public static void Add(this ArgumentBuilder builder, bool condition, [CanBeNull] string ifConditionTrue, [CanBeNull] string ifConditionFalse)
+        {
+            builder.Add(condition ? ifConditionTrue : ifConditionFalse);
+        }
+
+        /// <summary>
+        /// Adds all non-<c>null</c> <paramref name="values"/> to the argument list.
+        /// </summary>
+        /// <remarks>
+        /// <para>If <paramref name="values"/> is <c>null</c> or empty, then no change is made to the argument list.</para>
+        ///
+        /// <para>Any <c>null</c> values in the enumeration will be skipped, which can be convenient in calling constructions.</para>
+        /// </remarks>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="values">A sequence of strings to add.</param>
+        public static void Add(this ArgumentBuilder builder, [CanBeNull, ItemCanBeNull] IEnumerable<string> values)
+        {
+            if (values == null)
+            {
+                return;
+            }
+
+            foreach (var value in values)
+            {
+                builder.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// Adds all non-<c>null</c> <paramref name="ifConditionTrue"/> values to the argument list if <paramref name="condition"/>
+        /// is <c>true</c>.
+        /// </summary>
+        /// <remarks>
+        /// <para>If <paramref name="ifConditionTrue"/> is <c>null</c> or empty, then no change is made
+        /// to the argument list, regardless of the value of <paramref name="condition"/>.</para>
+        ///
+        /// <para>Any <c>null</c> values in the enumeration will be skipped, which can be convenient in calling constructions.</para>
+        /// </remarks>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="condition">Whether or not to add <paramref name="ifConditionTrue"/> to the argument list.</param>
+        /// <param name="ifConditionTrue">A sequence of strings to add if <paramref name="condition"/> is <c>true</c>.</param>
+        public static void Add(this ArgumentBuilder builder, bool condition, [CanBeNull, ItemCanBeNull] IEnumerable<string> ifConditionTrue)
+        {
+            if (!condition || ifConditionTrue == null)
+            {
+                return;
+            }
+
+            foreach (var value in ifConditionTrue)
+            {
+                builder.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// Adds the git argument syntax for members of the <see cref="ForcePushOptions"/> enum.
+        /// </summary>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="option">The enum member to add to the builder.</param>
+        public static void Add(this ArgumentBuilder builder, ForcePushOptions option)
+        {
+            builder.Add(GetArgument());
+
+            string GetArgument()
+            {
+                switch (option)
+                {
+                    case ForcePushOptions.Force:
+                        return "-f";
+                    case ForcePushOptions.ForceWithLease:
+                        return "--force-with-lease";
+                    case ForcePushOptions.DoNotForce:
+                        return null;
+                    default:
+                        throw new InvalidEnumArgumentException(nameof(option), (int)option, typeof(ForcePushOptions));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the git argument syntax for members of the <see cref="UntrackedFilesMode"/> enum.
+        /// </summary>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="mode">The enum member to add to the builder.</param>
+        public static void Add(this ArgumentBuilder builder, UntrackedFilesMode mode)
+        {
+            builder.Add(GetArgument());
+
+            string GetArgument()
+            {
+                switch (mode)
+                {
+                    case UntrackedFilesMode.Default:
+                        return "--untracked-files";
+                    case UntrackedFilesMode.No:
+                        return "--untracked-files=no";
+                    case UntrackedFilesMode.Normal:
+                        return "--untracked-files=normal";
+                    case UntrackedFilesMode.All:
+                        return "--untracked-files=all";
+                    default:
+                        throw new InvalidEnumArgumentException(nameof(mode), (int)mode, typeof(UntrackedFilesMode));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the git argument syntax for members of the <see cref="IgnoreSubmodulesMode"/> enum.
+        /// </summary>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="mode">The enum member to add to the builder.</param>
+        public static void Add(this ArgumentBuilder builder, IgnoreSubmodulesMode mode)
+        {
+            builder.Add(GetArgument());
+
+            string GetArgument()
+            {
+                switch (mode)
+                {
+                    case IgnoreSubmodulesMode.Default:
+                        return "--ignore-submodules";
+                    case IgnoreSubmodulesMode.None:
+                        return "--ignore-submodules=none";
+                    case IgnoreSubmodulesMode.Untracked:
+                        return "--ignore-submodules=untracked";
+                    case IgnoreSubmodulesMode.Dirty:
+                        return "--ignore-submodules=dirty";
+                    case IgnoreSubmodulesMode.All:
+                        return "--ignore-submodules=all";
+                    default:
+                        throw new InvalidEnumArgumentException(nameof(mode), (int)mode, typeof(IgnoreSubmodulesMode));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the git argument syntax for members of the <see cref="GitBisectOption"/> enum.
+        /// </summary>
+        /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
+        /// <param name="option">The enum member to add to the builder.</param>
+        public static void Add(this ArgumentBuilder builder, GitBisectOption option)
+        {
+            builder.Add(GetArgument());
+
+            string GetArgument()
+            {
+                switch (option)
+                {
+                    case GitBisectOption.Good:
+                        return "good";
+                    case GitBisectOption.Bad:
+                        return "bad";
+                    case GitBisectOption.Skip:
+                        return "skip";
+                    default:
+                        throw new InvalidEnumArgumentException(nameof(option), (int)option, typeof(GitBisectOption));
+                }
+            }
+        }
+    }
+}

UnitTests/GitCommandsTests/ArgumentBuilderExtensionsTests.cs
@@ -1,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GitCommands;
+using GitCommands.Git;
+using NUnit.Framework;
+
+namespace GitCommandsTests
+{
+    public sealed class ArgumentBuilderExtensionsTests
+    {
+        [Test]
+        public void Adds_conditional_parameters()
+        {
+            Test(
+                "foo true",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, "true" }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { false, "true" }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, "" }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, (string)null }
+                });
+
+            void Test(string expected, ArgumentBuilder command)
+            {
+                Assert.AreEqual(expected, command.ToString());
+            }
+        }
+
+        [Test]
+        public void Adds_conditional_if_else_parameters()
+        {
+            Test(
+                "foo true",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, "true", "false" }
+                });
+
+            Test(
+                "foo false",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { false, "true", "false" }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, "", "" }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { false, "", "" }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, null, null }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { false, null, null }
+                });
+
+            void Test(string expected, ArgumentBuilder command)
+            {
+                Assert.AreEqual(expected, command.ToString());
+            }
+        }
+
+        [Test]
+        public void Adds_enumerable_parameters()
+        {
+            Test(
+                "foo a b c",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    new[] { "a", "b", "c" }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    Enumerable.Empty<string>()
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    (IEnumerable<string>)null
+                });
+
+            void Test(string expected, ArgumentBuilder command)
+            {
+                Assert.AreEqual(expected, command.ToString());
+            }
+        }
+
+        [Test]
+        public void Adds_conditional_enumerable_parameters()
+        {
+            Test(
+                "foo a b c",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, new[] { "a", "b", "c" } }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { false, new[] { "a", "b", "c" } }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, Enumerable.Empty<string>() }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { false, Enumerable.Empty<string>() }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { true, (IEnumerable<string>)null }
+                });
+
+            Test(
+                "foo",
+                new ArgumentBuilder
+                {
+                    "foo",
+                    { false, (IEnumerable<string>)null }
+                });
+
+            void Test(string expected, ArgumentBuilder command)
+            {
+                Assert.AreEqual(expected, command.ToString());
+            }
+        }
+
+        [Test]
+        public void Handles_all_enum_members()
+        {
+            Test<ForcePushOptions>();
+            Test<UntrackedFilesMode>();
+            Test<IgnoreSubmodulesMode>();
+            Test<GitBisectOption>();
+
+            void Test<T>()
+            {
+                var method = typeof(ArgumentBuilderExtensions).GetMethod(
+                    nameof(ArgumentBuilderExtensions.Add),
+                    new[]
+                    {
+                        typeof(ArgumentBuilder),
+                        typeof(T)
+                    });
+
+                Assert.NotNull(method);
+
+                foreach (T member in Enum.GetValues(typeof(T)))
+                {
+                    var args = new ArgumentBuilder();
+
+                    Assert.DoesNotThrow(() => method.Invoke(null, new object[] { args, member }));
+                }
+            }
+        }
+    }
+}

UnitTests/GitCommandsTests/Git/GitCommandHelpersTest.cs
@@ -571,16 +571,18 @@ namespace GitCommandsTests.Git
             Assert.AreEqual(
                 "rebase -i --autosquash --preserve-merges --autostash \"branch\"",
                 GitCommandHelpers.RebaseCmd("branch", interactive: true, preserveMerges: true, autosquash: true, autostash: true));
-        }
 
-        [Test]
-        public void RebaseRangeCmd()
-        {
             // TODO quote 'onto'?
 
             Assert.AreEqual(
                 "rebase \"from\" \"branch\" --onto onto",
-                GitCommandHelpers.RebaseRangeCmd("from", "branch", "onto", interactive: false, preserveMerges: false, autosquash: false, autostash: false));
+                GitCommandHelpers.RebaseCmd("branch", interactive: false, preserveMerges: false, autosquash: false, autostash: false, "from", "onto"));
+
+            Assert.Throws<ArgumentException>(
+                () => GitCommandHelpers.RebaseCmd("branch", false, false, false, false, from: null, onto: "onto"));
+
+            Assert.Throws<ArgumentException>(
+                () => GitCommandHelpers.RebaseCmd("branch", false, false, false, false, from: "from", onto: null));
         }
 
         [Test]
@@ -671,6 +673,31 @@ namespace GitCommandsTests.Git
             Assert.AreEqual(
                 "merge --no-ff --strategy=strategy --squash --no-commit --allow-unrelated-histories -m \"message\" --log=1 branch",
                 GitCommandHelpers.MergeBranchCmd("branch", allowFastForward: false, squash: true, noCommit: true, allowUnrelatedHistories: true, message: "message", log: 1, strategy: "strategy"));
+        }
+
+        [Test]
+        public void ApplyDiffPatchCmd()
+        {
+            Assert.AreEqual(
+                "apply \"hello/world.patch\"",
+                GitCommandHelpers.ApplyDiffPatchCmd(false, "hello\\world.patch"));
+            Assert.AreEqual(
+                "apply --ignore-whitespace \"hello/world.patch\"",
+                GitCommandHelpers.ApplyDiffPatchCmd(true, "hello\\world.patch"));
+        }
+
+        [Test]
+        public void ApplyMailboxPatchCmd()
+        {
+            Assert.AreEqual(
+                "am --3way --signoff \"hello/world.patch\"",
+                GitCommandHelpers.ApplyMailboxPatchCmd(false, "hello\\world.patch"));
+            Assert.AreEqual(
+                "am --3way --signoff --ignore-whitespace \"hello/world.patch\"",
+                GitCommandHelpers.ApplyMailboxPatchCmd(true, "hello\\world.patch"));
+            Assert.AreEqual(
+                "am --3way --signoff --ignore-whitespace",
+                GitCommandHelpers.ApplyMailboxPatchCmd(true));
         }
     }
 }

GitCommands/ArgumentBuilder.cs
@@ -1,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace GitCommands
+{
+    /// <summary>
+    /// Builds a command line argument string from zero or more arguments.
+    /// </summary>
+    /// <remarks>
+    /// <para>To retrieve the constructed argument list string, call <see cref="ToString"/>.</para>
+    ///
+    /// <para>Arguments are separated by a single space character.</para>
+    ///
+    /// <para>Adding <c>null</c> or white-space strings has no effect on the output, which can be
+    /// useful in some calling constructions.</para>
+    ///
+    /// <para>This class has been designed to work with C# collection initialiser syntax which makes
+    /// its use quite ergonomic. See the example for more information.</para>
+    ///
+    /// <para>The type accepts strings, however conversion from other types (eg. <see cref="ForcePushOptions"/>
+    /// enum) is achieved via extension methods on <see cref="ArgumentBuilderExtensions"/> by adding a
+    /// method named <c>Add</c> that accepts the required type.</para>
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// var args = new ArgumentBuilder
+    /// {
+    ///     "commit",                   // adds the string unnconditionally
+    ///     { isAmend, "--amend" },     // adds the option only if isAmend == true
+    ///     { isUp, "--up", "--down" }, // selects the option based on the value of isUp
+    /// };
+    /// </code>
+    /// </example>
+    public sealed class ArgumentBuilder : IEnumerable
+    {
+        private readonly StringBuilder _command = new StringBuilder(capacity: 16);
+
+        /// <summary>
+        /// Adds <paramref name="s"/> to the argument list.
+        /// </summary>
+        /// <remarks>
+        /// If <paramref name="s"/> is <c>null</c> or white-space, then no change is made
+        /// to the argument list.
+        /// </remarks>
+        /// <param name="s">The string to add.</param>
+        public void Add([CanBeNull] string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return;
+            }
+
+            if (_command.Length != 0)
+            {
+                _command.Append(' ');
+            }
+
+            _command.Append(s);
+        }
+
+        /// <summary>
+        /// Returns the composed argument list as a string.
+        /// </summary>
+        public override string ToString()
+        {
+            return _command.ToString();
+        }
+
+        /// <summary>
+        /// This method is only implemented to support collection initialiser syntax, and always
+        /// throws if called.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Always thrown.</exception>
+        [ContractAnnotation("=>halt")]
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new InvalidOperationException($"{nameof(IEnumerable)} only implemented to support collection initialiser syntax.");
+        }
+    }
+}

GitCommands/GitCommands.csproj
@@ -97,8 +97,10 @@
       <Link>Properties\CommonAssemblyInfo.cs</Link>
     </Compile>
     <Compile Include="AppTitleGenerator.cs" />
+    <Compile Include="ArgumentBuilderExtensions.cs" />
     <Compile Include="AsyncLoader.cs" />
     <Compile Include="BoolExtensions.cs" />
+    <Compile Include="ArgumentBuilder.cs" />
     <Compile Include="CommitData.cs" />
     <Compile Include="CommitDataManager.cs" />
     <Compile Include="CommitTemplateItem.cs" />

commit 1e48764a7162b4f8fbd512dfb8bf189914a5249f

Allow ArgumentBuilder enum extensions to accept null
Fixes #4754.

Bug was introduced in 83a151b869acbc when we started
throwing for unknown enum members.

GitCommands/ArgumentBuilderExtensions.cs
@@ -98,11 +98,17 @@ namespace GitCommands
         /// <summary>
         /// Adds the git argument syntax for members of the <see cref="ForcePushOptions"/> enum.
         /// </summary>
+        /// <remarks>
+        /// If <paramref name="option"/> is <c>null</c> then no argument is added.
+        /// </remarks>
         /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
-        /// <param name="option">The enum member to add to the builder.</param>
-        public static void Add(this ArgumentBuilder builder, ForcePushOptions option)
+        /// <param name="option">The enum member to add to the builder, or <c>null</c>.</param>
+        public static void Add(this ArgumentBuilder builder, ForcePushOptions? option)
         {
-            builder.Add(GetArgument());
+            if (option.HasValue)
+            {
+                builder.Add(GetArgument());
+            }
 
             string GetArgument()
             {
@@ -123,11 +129,17 @@ namespace GitCommands
         /// <summary>
         /// Adds the git argument syntax for members of the <see cref="UntrackedFilesMode"/> enum.
         /// </summary>
+        /// <remarks>
+        /// If <paramref name="mode"/> is <c>null</c> then no argument is added.
+        /// </remarks>
         /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
-        /// <param name="mode">The enum member to add to the builder.</param>
-        public static void Add(this ArgumentBuilder builder, UntrackedFilesMode mode)
+        /// <param name="mode">The enum member to add to the builder, or <c>null</c>.</param>
+        public static void Add(this ArgumentBuilder builder, UntrackedFilesMode? mode)
         {
-            builder.Add(GetArgument());
+            if (mode.HasValue)
+            {
+                builder.Add(GetArgument());
+            }
 
             string GetArgument()
             {
@@ -150,11 +162,17 @@ namespace GitCommands
         /// <summary>
         /// Adds the git argument syntax for members of the <see cref="IgnoreSubmodulesMode"/> enum.
         /// </summary>
+        /// <remarks>
+        /// If <paramref name="mode"/> is <c>null</c> then no argument is added.
+        /// </remarks>
         /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
-        /// <param name="mode">The enum member to add to the builder.</param>
-        public static void Add(this ArgumentBuilder builder, IgnoreSubmodulesMode mode)
+        /// <param name="mode">The enum member to add to the builder, or <c>null</c>.</param>
+        public static void Add(this ArgumentBuilder builder, IgnoreSubmodulesMode? mode)
         {
-            builder.Add(GetArgument());
+            if (mode.HasValue)
+            {
+                builder.Add(GetArgument());
+            }
 
             string GetArgument()
             {
@@ -179,11 +197,17 @@ namespace GitCommands
         /// <summary>
         /// Adds the git argument syntax for members of the <see cref="GitBisectOption"/> enum.
         /// </summary>
+        /// <remarks>
+        /// If <paramref name="option"/> is <c>null</c> then no argument is added.
+        /// </remarks>
         /// <param name="builder">The <see cref="ArgumentBuilder"/> to add arguments to.</param>
-        /// <param name="option">The enum member to add to the builder.</param>
-        public static void Add(this ArgumentBuilder builder, GitBisectOption option)
+        /// <param name="option">The enum member to add to the builder, or <c>null</c>.</param>
+        public static void Add(this ArgumentBuilder builder, GitBisectOption? option)
         {
-            builder.Add(GetArgument());
+            if (option.HasValue)
+            {
+                builder.Add(GetArgument());
+            }
 
             string GetArgument()
             {

GitCommands/Git/GitCommandHelpers.cs
@@ -729,7 +729,7 @@ namespace GitCommands
             return args.ToString();
         }
 
-        public static string GetAllChangedFilesCmd(bool excludeIgnoredFiles, UntrackedFilesMode untrackedFiles, IgnoreSubmodulesMode ignoreSubmodules = 0)
+        public static string GetAllChangedFilesCmd(bool excludeIgnoredFiles, UntrackedFilesMode untrackedFiles, IgnoreSubmodulesMode? ignoreSubmodules = null)
         {
             var args = new ArgumentBuilder
             {

UnitTests/GitCommandsTests/ArgumentBuilderExtensionsTests.cs
@@ -205,14 +205,14 @@ namespace GitCommandsTests
             Test<IgnoreSubmodulesMode>();
             Test<GitBisectOption>();
 
-            void Test<T>()
+            void Test<T>() where T : struct
             {
                 var method = typeof(ArgumentBuilderExtensions).GetMethod(
                     nameof(ArgumentBuilderExtensions.Add),
                     new[]
                     {
                         typeof(ArgumentBuilder),
-                        typeof(T)
+                        typeof(T?)
                     });
 
                 Assert.NotNull(method);
@@ -221,6 +221,11 @@ namespace GitCommandsTests
                 {
                     var args = new ArgumentBuilder();
 
+                    // Passing null should not modify the arguments
+                    method.Invoke(null, new object[] { args, default(T?) });
+                    Assert.AreEqual("", args.ToString());
+
+                    // Passing a member should not throw (though might not modify the arguments)
                     Assert.DoesNotThrow(() => method.Invoke(null, new object[] { args, member }));
                 }
             }

UnitTests/GitCommandsTests/Git/GitCommandHelpersTest.cs
@@ -641,6 +641,9 @@ namespace GitCommandsTests.Git
             Assert.AreEqual(
                 "status --porcelain -z --untracked-files --ignore-submodules=all",
                 GitCommandHelpers.GetAllChangedFilesCmd(excludeIgnoredFiles: true, UntrackedFilesMode.Default, IgnoreSubmodulesMode.All));
+            Assert.AreEqual(
+                "status --porcelain -z --untracked-files",
+                GitCommandHelpers.GetAllChangedFilesCmd(excludeIgnoredFiles: true, UntrackedFilesMode.Default));
         }
 
         [Test]