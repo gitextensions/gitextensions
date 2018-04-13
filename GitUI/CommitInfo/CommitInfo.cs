a9ee2c6e954da0915341c52318ea2d23795740e4
Drew Noakes <git@drewnoakes.com>
2018-04-13 22:08:59 +0100

    Remove unused parameter

--- GitUI/CommitInfo/CommitInfo.cs
+++ GitUI/CommitInfo/CommitInfo.cs
@@ -446,7 +446,7 @@ namespace GitUI.CommitInfo
                         .ToList();
 
                     thisRevisionTagNames.Sort(new ItemTpComparer(_sortedRefs, "refs/tags/"));
-                    _annotatedTagsInfo = GetAnnotatedTagsInfo(Revision, thisRevisionTagNames, _annotatedTagsMessages);
+                    _annotatedTagsInfo = GetAnnotatedTagsInfo(thisRevisionTagNames, _annotatedTagsMessages);
                 }
 
                 if (_tags != null && string.IsNullOrEmpty(_tagInfo))
@@ -490,7 +490,6 @@ namespace GitUI.CommitInfo
         }
 
         private static string GetAnnotatedTagsInfo(
-            GitRevision revision,
             IEnumerable<string> tagNames,
             IDictionary<string, string> annotatedTagsMessages)
         {
