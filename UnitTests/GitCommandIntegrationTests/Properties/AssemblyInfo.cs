f0320920c63d5d50add590a6a27564eb0451c63f
Szali Szali <szalicil@gmail.com>
2018-04-24 08:23:32 +0200

    added new test project GitCommandIntegrationTests

---
+++ UnitTests/GitCommandIntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitCommandIntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitCommandIntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62c7a5d4-dd49-413d-81d5-01b9a7f03287")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/GitCommandIntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace GitCommandIntegrationTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            true.Should().Be(true);
+        }
+    }
+}
