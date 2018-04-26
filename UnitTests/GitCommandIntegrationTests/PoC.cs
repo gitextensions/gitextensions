8e74852eee7c2e4e76d4c583d420c8860ad887bd
Szali Szali <szalicil@gmail.com>
2018-04-26 06:59:32 +0200

    added "real" tests

---
+++ UnitTests/GitCommandIntegrationTests/PoC.cs
@@ -0,0 +1,52 @@
+﻿using ApprovalTests;
+using GitCommands;
+using GitUI;
+using Microsoft.VisualStudio.Threading;
+using NUnit.Framework;
+
+namespace GitCommandIntegrationTests
+{
+    /// <summary>
+    /// Three simple test cases for git commands copypasted from GitModule.GetRefs.
+    /// They are not intended to be fit for any purpose except demonstration of test execution.
+    /// </summary>
+    [TestFixture]
+    public class PoC
+    {
+        private const string TestRepoName = "poc";
+
+        [Test]
+        public void Listing_both_tags_and_branches_should_list_all_tags_and_branches_with_hash_and_refname_columns()
+        {
+            RunGitCommandOnTestRepo(@"show-ref --dereference");
+        }
+
+        [Test]
+        public void Listing_tags_should_list_all_tags_with_hash_and_refname_columns()
+        {
+            RunGitCommandOnTestRepo(@"show-ref --tags");
+        }
+
+        [Test]
+        public void Listing_branches_should_list_all_branches_with_hash_and_refname_columns()
+        {
+            RunGitCommandOnTestRepo(@"for-each-ref --sort=-committerdate refs/heads/ --format=""%(objectname) %(refname)""");
+        }
+
+        private static void RunGitCommandOnTestRepo(string gitCommandLine)
+        {
+            using (var tempDir = new TestRepoDirectory(TestRepoName))
+            {
+                var gitModule = new GitModule(tempDir.PathToExtractedContent);
+                var output = gitModule.RunGitCmdResult(gitCommandLine, GitModule.SystemEncoding).StdOutput;
+
+                Approvals.Verify(output);
+            }
+        }
+
+        static PoC()
+        {
+            ThreadHelper.JoinableTaskContext = new JoinableTaskContext(); // TODO oh no that's ugly
+        }
+    }
+}
