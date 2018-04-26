8095c230cdb863c6f34f0718dcbffe1b9a543788
Szali Szali <szalicil@gmail.com>
2018-04-26 22:21:07 +0200

    added the same test cases using LibGit2Sharp

---
+++ UnitTests/GitCommandIntegrationTests/CodedRepositoryPoC.cs
@@ -0,0 +1,43 @@
+﻿using ApprovalTests;
+using GitCommands;
+using NUnit.Framework;
+
+namespace GitCommandIntegrationTests
+{
+    /// <summary>
+    /// Three simple test cases for git commands copypasted from GitModule.GetRefs.
+    /// They are not intended to be fit for any purpose except demonstration of test execution.
+    /// </summary>
+    [TestFixture]
+    public class CodedRepositoryPoC
+    {
+        [Test]
+        public void Listing_both_tags_and_branches_should_list_all_tags_and_branches_with_hash_and_refname_columns()
+        {
+            RunGitCommandOnTestRepo(@"show-ref --dereference");
+        }
+
+        [Test]
+        public void Listing_tags_should_list_all_tags_with_hash_and_refname_columns()
+        {
+            RunGitCommandOnTestRepo(@"show-ref --tags");
+        }
+
+        [Test]
+        public void Listing_branches_should_list_all_branches_with_hash_and_refname_columns()
+        {
+            RunGitCommandOnTestRepo(@"for-each-ref --sort=-committerdate refs/heads/ --format=""%(objectname) %(refname)""");
+        }
+
+        private static void RunGitCommandOnTestRepo(string gitCommandLine)
+        {
+            using (ITestRepositoryData tempDir = new CodedTestRepository())
+            {
+                var gitModule = new GitModule(tempDir.ContentPath);
+                var output = gitModule.RunGitCmdResult(gitCommandLine, GitModule.SystemEncoding).StdOutput;
+
+                Approvals.Verify(output);
+            }
+        }
+    }
+}
---
+++ UnitTests/GitCommandIntegrationTests/CodedTestRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace GitCommandIntegrationTests
+{
+    internal class CodedTestRepository : TestRepository
+    {
+        // time must be fixed to get deterministic hashes
+        private static readonly DateTimeOffset When = new DateTimeOffset(2018, 1, 2, 3, 4, 5, 6, TimeSpan.Zero);
+
+        protected override void CreateContent()
+        {
+            LibGit2Sharp.Repository.Init(ContentTargetPath);
+            using (var repository = new LibGit2Sharp.Repository(ContentTargetPath))
+            {
+                CreateRepoFile("A.txt", "A");
+                repository.Index.Add("A.txt");
+
+                var message = "A commit message";
+                var author = new LibGit2Sharp.Signature("GitUITests", "unittests@gitextensions.com", When);
+                var committer = author;
+                var options = new LibGit2Sharp.CommitOptions();
+                repository.Commit(message, author, committer, options);
+            }
+        }
+
+        private void CreateRepoFile(string filename, string content)
+        {
+            File.WriteAllText(Path.Combine(ContentTargetPath, filename), content);
+        }
+    }
+}
