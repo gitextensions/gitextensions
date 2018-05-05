c5f2636dca688b3701ed7b49ad7a04bee49b98ac
Szali Szali <szalicil@gmail.com>
2018-05-05 17:21:23 +0200

    merged CopyToClipboardMenuHelper with CopyContextMenuItem
    it only contained internal logic of the copy to clipboard context menu

--- GitUI/UserControls/RevisionGridClasses/CopyContextMenuItem.cs
+++ GitUI/UserControls/RevisionGridClasses/CopyContextMenuItem.cs
@@ -24,10 +24,10 @@ namespace GitUI.UserControls.RevisionGridClasses
             var r = LatestSelectedRevision;
             if (r != null)
             {
-                CopyToClipboardMenuHelper.AddOrUpdateTextPostfix(hashCopyToolStripMenuItem, r.Guid.ShortenTo(15));
-                CopyToClipboardMenuHelper.AddOrUpdateTextPostfix(messageCopyToolStripMenuItem, r.Subject.ShortenTo(30));
-                CopyToClipboardMenuHelper.AddOrUpdateTextPostfix(authorCopyToolStripMenuItem, r.Author);
-                CopyToClipboardMenuHelper.AddOrUpdateTextPostfix(dateCopyToolStripMenuItem, r.CommitDate.ToString());
+                AddOrUpdateTextPostfix(hashCopyToolStripMenuItem, r.Guid.ShortenTo(15));
+                AddOrUpdateTextPostfix(messageCopyToolStripMenuItem, r.Subject.ShortenTo(30));
+                AddOrUpdateTextPostfix(authorCopyToolStripMenuItem, r.Author);
+                AddOrUpdateTextPostfix(dateCopyToolStripMenuItem, r.CommitDate.ToString());
             }
         }
 
@@ -59,12 +59,64 @@ namespace GitUI.UserControls.RevisionGridClasses
             MenuUtil.SetAsCaptionMenuItem(tagNameCopyToolStripMenuItem, mainContextMenu);
 
             var branchNames = gitRefListsForRevision.GetAllBranchNames();
-            CopyToClipboardMenuHelper.SetCopyToClipboardMenuItems(this, branchNameCopyToolStripMenuItem, branchNames, "branchNameItem");
+            SetCopyToClipboardMenuItems(this, branchNameCopyToolStripMenuItem, branchNames, "branchNameItem");
 
             var tagNames = gitRefListsForRevision.GetAllTagNames();
-            CopyToClipboardMenuHelper.SetCopyToClipboardMenuItems(this, tagNameCopyToolStripMenuItem, tagNames, "tagNameItem");
+            SetCopyToClipboardMenuItems(this, tagNameCopyToolStripMenuItem, tagNames, "tagNameItem");
 
             separatorAfterRefNames.Visible = branchNames.Any() || tagNames.Any();
+        }
+
+        /// <summary>
+        /// ...
+        /// sets also the visibility of captionItem
+        /// ...
+        /// </summary>
+        private static void SetCopyToClipboardMenuItems(
+            ToolStripMenuItem targetMenu,
+            ToolStripMenuItem captionItem,
+            string[] gitNameList,
+            string itemFlag)
+        {
+            // remove previous items
+            targetMenu.DropDownItems.OfType<ToolStripMenuItem>()
+                .Where(item => (item.Tag as string) == itemFlag)
+                .ToArray()
+                .ForEach(item => targetMenu.DropDownItems.Remove(item));
+
+            // insert items
+            var branchNameItemInsertAfter = captionItem;
+            gitNameList.ForEach(branchName =>
+            {
+                var branchNameItem = new ToolStripMenuItem(branchName);
+                branchNameItem.Tag = itemFlag; // to delete items from previous opening
+                branchNameItem.Click += CopyToClipBoard;
+                int insertAfterIndex = targetMenu.DropDownItems.IndexOf(branchNameItemInsertAfter);
+                targetMenu.DropDownItems.Insert(insertAfterIndex + 1, branchNameItem);
+                branchNameItemInsertAfter = branchNameItem;
+            });
+
+            // visibility of caption
+            // TODO: why is the Visible property always false when it is read from?
+            captionItem.Visible = gitNameList.Any();
+        }
+
+        private static void CopyToClipBoard(object sender, EventArgs e)
+        {
+            Clipboard.SetText(sender.ToString());
+        }
+
+        /// <summary>
+        /// adds or updates text in parentheses (...)
+        /// </summary>
+        private static void AddOrUpdateTextPostfix(ToolStripItem target, string postfix)
+        {
+            if (target.Text.EndsWith(")"))
+            {
+                target.Text = target.Text.Substring(0, target.Text.IndexOf("     ("));
+            }
+
+            target.Text += string.Format("     ({0})", postfix);
         }
     }
 }
