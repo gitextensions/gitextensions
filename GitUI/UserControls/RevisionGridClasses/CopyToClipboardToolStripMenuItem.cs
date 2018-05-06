d67223ee0585abb87460059b50e1841291d7c45d
Szali Szali <szalicil@gmail.com>
2018-05-06 09:03:34 +0200

    added CopyToClipboardToolStripMenuItem

---
+++ GitUI/UserControls/RevisionGridClasses/CopyToClipboardToolStripMenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GitUI.UserControls.RevisionGridClasses
+{
+    /// <summary>
+    /// Specialized menu item that has a title and a value which will be copied to the clipboard on click.
+    /// </summary>
+    public class CopyToClipboardToolStripMenuItem : ToolStripMenuItem
+    {
+        private readonly string _value;
+
+        public CopyToClipboardToolStripMenuItem(string text, string value)
+            : base(text)
+        {
+            _value = value;
+            Click += CopyToClipboard;
+        }
+
+        private void CopyToClipboard(object sender, EventArgs e)
+        {
+            Clipboard.SetText(_value);
+        }
+    }
+}
