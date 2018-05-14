dd9f90b07de9ef27d4fdd794a37714a6bfefc448
Szali Szali <szalicil@gmail.com>
2018-05-14 23:22:13 +0200

    #4907 bypass activated event handler for reset
    (from 3.0 branch)

--- GitUI/CommandsDialogs/FormCommit.cs
+++ GitUI/CommandsDialogs/FormCommit.cs
@@ -2047,14 +2047,28 @@ namespace GitUI.CommandsDialogs
 
         private void ResetClick(object sender, EventArgs e)
         {
-            UICommands.StartResetChangesDialog(this, Unstaged.AllItems, false);
+            BypassFormActivatedEventHandler(() => UICommands.StartResetChangesDialog(this, Unstaged.AllItems.ToList(), onlyUnstaged: false));
             Initialize();
         }
 
         private void ResetUnStagedClick(object sender, EventArgs e)
         {
-            UICommands.StartResetChangesDialog(this, Unstaged.AllItems, true);
+            BypassFormActivatedEventHandler(() => UICommands.StartResetChangesDialog(this, Unstaged.AllItems.ToList(), onlyUnstaged: true));
             Initialize();
+        }
+
+        private void BypassFormActivatedEventHandler(Action action)
+        {
+            try
+            {
+                Activated -= FormCommitActivated;
+
+                action();
+            }
+            finally
+            {
+                Activated += FormCommitActivated;
+            }
         }
 
         private void ShowUntrackedFilesToolStripMenuItemClick(object sender, EventArgs e)
