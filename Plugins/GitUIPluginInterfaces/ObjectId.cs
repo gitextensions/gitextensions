27c06b3d48361e15cd5e45958e1f460f3ead72d3
Drew Noakes <git@drewnoakes.com>
2018-05-27 10:34:44 +0100

    Remove unused method

--- Plugins/GitUIPluginInterfaces/ObjectId.cs
+++ Plugins/GitUIPluginInterfaces/ObjectId.cs
@@ -415,16 +415,6 @@ namespace GitUIPluginInterfaces
 
     internal static class StreamExtensions
     {
-        [NotNull]
-        public static byte[] ReadBytes([NotNull] this Stream stream, int count)
-        {
-            var buffer = new byte[count];
-
-            stream.ReadBytes(buffer, offset: 0, count);
-
-            return buffer;
-        }
-
         public static void ReadBytes([NotNull] this Stream stream, [NotNull] byte[] buffer, int offset, int count)
         {
             while (offset != count)
