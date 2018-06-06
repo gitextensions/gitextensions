c0c9c5c7c23f555456c249cbef6497d280af4c0e
Drew Noakes <git@drewnoakes.com>
2018-06-06 19:35:55 +0100

    Extract RevisionGridRefRenderer class

--- GitUI/UserControls/RevisionGrid.cs
+++ GitUI/UserControls/RevisionGrid.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.DirectoryServices;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Reactive.Concurrency;
@@ -1442,17 +1441,17 @@ If this is a central repository (bare repository without a working directory):
                 {
                     if (spi.Conflict_Base == revision.Guid)
                     {
-                        offset = DrawRef(drawRefArgs, offset, "Base", Color.OrangeRed, ArrowType.NotFilled);
+                        offset = RevisionGridRefRenderer.DrawRef(drawRefArgs, offset, "Base", Color.OrangeRed, ArrowType.NotFilled);
                     }
 
                     if (spi.Conflict_Local == revision.Guid)
                     {
-                        offset = DrawRef(drawRefArgs, offset, "Local", Color.OrangeRed, ArrowType.NotFilled);
+                        offset = RevisionGridRefRenderer.DrawRef(drawRefArgs, offset, "Local", Color.OrangeRed, ArrowType.NotFilled);
                     }
 
                     if (spi.Conflict_Remote == revision.Guid)
                     {
-                        offset = DrawRef(drawRefArgs, offset, "Remote", Color.OrangeRed, ArrowType.NotFilled);
+                        offset = RevisionGridRefRenderer.DrawRef(drawRefArgs, offset, "Remote", Color.OrangeRed, ArrowType.NotFilled);
                     }
                 }
 
@@ -1480,7 +1479,7 @@ If this is a central repository (bare repository without a working directory):
                             continue;
                         }
 
-                        var headColor = GetHeadColor(gitRef);
+                        var headColor = RevisionGridRefRenderer.GetHeadColor(gitRef);
 
                         var arrowType = gitRef.Selected
                             ? ArrowType.Filled
@@ -1504,14 +1503,14 @@ If this is a central repository (bare repository without a working directory):
                             name = name + "  " + MultilineMessageIndicator;
                         }
 
-                        offset = DrawRef(drawRefArgs, offset, name, headColor, arrowType, superprojectRef != null, true);
+                        offset = RevisionGridRefRenderer.DrawRef(drawRefArgs, offset, name, headColor, arrowType, superprojectRef != null, true);
                     }
                 }
 
                 for (int i = 0; i < Math.Min(MaxSuperprojectRefs, superprojectRefs.Count); i++)
                 {
                     var gitRef = superprojectRefs[i];
-                    var headColor = GetHeadColor(gitRef);
+                    var headColor = RevisionGridRefRenderer.GetHeadColor(gitRef);
                     var gitRefName = i < (MaxSuperprojectRefs - 1) ? gitRef.Name : "…";
 
                     var arrowType = gitRef.Selected
@@ -1521,11 +1520,11 @@ If this is a central repository (bare repository without a working directory):
                             : ArrowType.None;
                     drawRefArgs.RefsFont = gitRef.Selected ? rowFont : _refsFont;
 
-                    offset = DrawRef(drawRefArgs, offset, gitRefName, headColor, arrowType, true);
+                    offset = RevisionGridRefRenderer.DrawRef(drawRefArgs, offset, gitRefName, headColor, arrowType, true);
                 }
 
                 var text = (string)e.FormattedValue;
-                var bounds = AdjustCellBounds(e.CellBounds, offset);
+                var bounds = RevisionGridRefRenderer.AdjustCellBounds(e.CellBounds, offset);
                 DrawColumnText(e, text, rowFont, foreColor, bounds);
 
                 if (revision.IsArtificial)
@@ -1533,7 +1532,7 @@ If this is a central repository (bare repository without a working directory):
                     // Get offset for "count" text
                     offset += 1 + drawRefArgs.Graphics.MeasureString(text, rowFont).Width;
 
-                    /*offset = */DrawRef(drawRefArgs, offset, revision.Subject, AppSettings.OtherTagColor, ArrowType.None, false, true);
+                    RevisionGridRefRenderer.DrawRef(drawRefArgs, offset, revision.Subject, AppSettings.OtherTagColor, ArrowType.None, false, true);
                 }
             }
             else if (columnIndex == AuthorDataGridViewColumn.Index)
@@ -1711,214 +1710,14 @@ If this is a central repository (bare repository without a working directory):
 
         #endregion
 
-        private static float DrawRef(DrawRefArgs drawRefArgs, float offset, string name, Color headColor, ArrowType arrowType, bool dashedLine = false, bool fill = false)
-        {
-            var textColor = fill ? headColor : Lerp(headColor, Color.White, 0.5f);
-
-            var headBounds = AdjustCellBounds(drawRefArgs.CellBounds, offset);
-            var textSize = drawRefArgs.Graphics.MeasureString(name, drawRefArgs.RefsFont);
-
-            offset += textSize.Width;
-            offset += 9;
-
-            var extraOffset = DrawHeadBackground(
-                drawRefArgs.IsRowSelected,
-                drawRefArgs.Graphics,
-                headColor,
-                headBounds.X,
-                headBounds.Y,
-                RoundToEven(textSize.Width),
-                RoundToEven(textSize.Height),
-                radius: 3, arrowType, dashedLine, fill);
-
-            offset += extraOffset;
-            headBounds.Offset((int)(extraOffset + 1), 0);
-
-            RevisionGridUtils.DrawColumnTextTruncated(drawRefArgs.Graphics, name, drawRefArgs.RefsFont, textColor, headBounds);
-
-            return offset;
-        }
-
-        private const string MultilineMessageIndicator = "[...]";
-
-        private static Rectangle AdjustCellBounds(Rectangle cellBounds, float offset)
-        {
-            return new Rectangle((int)(cellBounds.Left + offset), cellBounds.Top + 4,
-                                 cellBounds.Width - (int)offset, cellBounds.Height);
-        }
-
-        private static Color GetHeadColor(IGitRef gitRef)
-        {
-            if (gitRef.IsTag)
-            {
-                return AppSettings.TagColor;
-            }
-
-            if (gitRef.IsHead)
-            {
-                return AppSettings.BranchColor;
-            }
-
-            if (gitRef.IsRemote)
-            {
-                return AppSettings.RemoteBranchColor;
-            }
-
-            return AppSettings.OtherTagColor;
-        }
-
-        private static float RoundToEven(float value)
-        {
-            var result = (int)value;
-            result <<= 1;
-            result >>= 1;
-            return result < value
-                ? result + 2
-                : result;
-        }
-
-        private enum ArrowType
+        internal enum ArrowType
         {
             None,
             Filled,
             NotFilled
         }
 
-        private static readonly float[] dashPattern = { 4, 4 };
-
-        private static float DrawHeadBackground(bool isSelected, Graphics graphics, Color color,
-            float x, float y, float width, float height, float radius, ArrowType arrowType, bool dashedLine, bool fill)
-        {
-            float additionalOffset = arrowType != ArrowType.None ? height - 6 : 0;
-            width += additionalOffset;
-            var oldMode = graphics.SmoothingMode;
-            graphics.SmoothingMode = SmoothingMode.AntiAlias;
-
-            try
-            {
-                // shade
-                if (fill)
-                {
-                    using (var shadePath = CreateRoundRectPath(x + 1, y + 1, width, height, radius))
-                    {
-                        var shadeBrush = isSelected ? Brushes.Black : Brushes.Gray;
-                        graphics.FillPath(shadeBrush, shadePath);
-                    }
-                }
-
-                using (var forePath = CreateRoundRectPath(x, y, width, height, radius))
-                {
-                    Color fillColor = Lerp(color, Color.White, 0.92F);
-
-                    if (fill)
-                    {
-                        using (var fillBrush = new LinearGradientBrush(new RectangleF(x, y, width, height), fillColor, Lerp(fillColor, Color.White, 0.9F), 90))
-                        {
-                            graphics.FillPath(fillBrush, forePath);
-                        }
-                    }
-                    else if (isSelected)
-                    {
-                        graphics.FillPath(Brushes.White, forePath);
-                    }
-
-                    // frame
-                    using (var pen = new Pen(Lerp(color, Color.White, 0.83F)))
-                    {
-                        if (dashedLine)
-                        {
-                            pen.DashPattern = dashPattern;
-                        }
-
-                        graphics.DrawPath(pen, forePath);
-                    }
-
-                    // arrow if the head is the current branch
-                    if (arrowType != ArrowType.None)
-                    {
-                        DrawArrow(graphics, x, y, height, color, arrowType == ArrowType.Filled);
-                    }
-                }
-            }
-            finally
-            {
-                graphics.SmoothingMode = oldMode;
-            }
-
-            return additionalOffset;
-        }
-
-        private static readonly PointF[] _arrowPoints = new PointF[4];
-
-        private static void DrawArrow(Graphics graphics, float x, float y, float rowHeight, Color color, bool filled)
-        {
-            ThreadHelper.AssertOnUIThread();
-
-            const float horShift = 4;
-            const float verShift = 3;
-
-            float height = rowHeight - (verShift * 2);
-            float width = height / 2;
-
-            _arrowPoints[0] = new PointF(x + horShift, y + verShift);
-            _arrowPoints[1] = new PointF(x + horShift + width, y + verShift + (height / 2));
-            _arrowPoints[2] = new PointF(x + horShift, y + verShift + height);
-            _arrowPoints[3] = new PointF(x + horShift, y + verShift);
-
-            if (filled)
-            {
-                using (var brush = new SolidBrush(color))
-                {
-                    graphics.FillPolygon(brush, _arrowPoints);
-                }
-            }
-            else
-            {
-                using (var pen = new Pen(color))
-                {
-                    graphics.DrawPolygon(pen, _arrowPoints);
-                }
-            }
-        }
-
-        private static GraphicsPath CreateRoundRectPath(float x, float y, float width, float height, float radius)
-        {
-            var path = new GraphicsPath();
-            path.AddLine(x + radius, y, x + width - (radius * 2), y);
-            path.AddArc(x + width - (radius * 2), y, radius * 2, radius * 2, 270, 90);
-            path.AddLine(x + width, y + radius, x + width, y + height - (radius * 2));
-            path.AddArc(x + width - (radius * 2), y + height - (radius * 2), radius * 2, radius * 2, 0, 90);
-            path.AddLine(x + width - (radius * 2), y + height, x + radius, y + height);
-            path.AddArc(x, y + height - (radius * 2), radius * 2, radius * 2, 90, 90);
-            path.AddLine(x, y + height - (radius * 2), x, y + radius);
-            path.AddArc(x, y, radius * 2, radius * 2, 180, 90);
-            path.CloseFigure();
-            return path;
-        }
-
-        private static float Lerp(float start, float end, float amount)
-        {
-            float difference = end - start;
-            float adjusted = difference * amount;
-            return start + adjusted;
-        }
-
-        private static Color Lerp(Color colour, Color to, float amount)
-        {
-            // start colours as lerp-able floats
-            float sr = colour.R, sg = colour.G, sb = colour.B;
-
-            // end colours as lerp-able floats
-            float er = to.R, eg = to.G, eb = to.B;
-
-            // lerp the colours to get the difference
-            byte r = (byte)Lerp(sr, er, amount),
-                 g = (byte)Lerp(sg, eg, amount),
-                 b = (byte)Lerp(sb, eb, amount);
-
-            // return the new colour
-            return Color.FromArgb(r, g, b);
-        }
+        private const string MultilineMessageIndicator = "[...]";
 
         public void ViewSelectedRevisions()
         {
@@ -3125,7 +2924,8 @@ If this is a central repository (bare repository without a working directory):
 
         #region Nested type: Draw ref args
 
-        private struct DrawRefArgs
+        // TODO get rid of this type
+        public struct DrawRefArgs
         {
             public Graphics Graphics;
             public Rectangle CellBounds;
