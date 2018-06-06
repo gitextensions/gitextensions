4b31102b14233c9f445f6350db66f6e5f55bb125
Drew Noakes <git@drewnoakes.com>
2018-06-07 00:05:25 +0100

    Introduce control for bare repo UI

---
+++ GitUI/UserControls/RevisionGridClasses/BareRepoControl.cs
@@ -0,0 +1,17 @@
+﻿using ResourceManager;
+
+namespace GitUI.UserControls.RevisionGridClasses
+{
+    public partial class BareRepoControl : GitModuleControl
+    {
+        private readonly TranslationString _bareRepositoriesAreNotSupported = new TranslationString("Git Extensions does not support viewing bare repositories.");
+
+        public BareRepoControl()
+        {
+            InitializeComponent();
+            Translate();
+
+            label.Text = _bareRepositoriesAreNotSupported.Text;
+        }
+    }
+}
