113471e481c1893a0f724f18f388dafaa6aabfe0
Drew Noakes <git@drewnoakes.com>
2018-06-09 22:11:53 +0100

    Add HslColor and use for better colour adjustments
    Working in HSL colour space gives more visually pleasing results.

--- GitUI/ColorHelper.cs
+++ GitUI/ColorHelper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace GitUI
 {
@@ -12,55 +11,13 @@ namespace GitUI
 
         public static bool IsLightColor(this Color color)
         {
-            if ((color.R > 130 ||
-                 color.B > 130 ||
-                 color.G > 130) &&
-                color.R + color.B + color.G > 350)
-            {
-                return true;
-            }
-
-            return false;
+            return new HslColor(color).L > 0.5;
         }
 
-        public static Color AdjustColor(Color color, int amount = 10)
+        public static Color MakeColorDarker(Color color, double amount = 0.1)
         {
-            return Color.FromArgb(
-                AdjustColorComponent(color.R),
-                AdjustColorComponent(color.G),
-                AdjustColorComponent(color.B));
-
-            int AdjustColorComponent(int v)
-            {
-                var newValue = v + amount;
-
-                return newValue < 0
-                    ? 0
-                    : newValue > 255
-                        ? 255
-                        : newValue;
-            }
-        }
-
-        public static Color MakeColorLighter(Color color, int amount = 10)
-        {
-            return AdjustColor(color, amount);
-        }
-
-        public static Color MakeColorDarker(Color color, int amount = 10)
-        {
-            return AdjustColor(color, -amount);
-        }
-
-        public static int GetColorBrightnessIndex(Color c)
-        {
-            // From: http://www.had2know.com/technology/color-contrast-calculator-web-design.html
-            return ((299 * c.R) + (587 * c.G) + (114 * c.B)) / 1000;
-        }
-
-        public static int GetColorBrightnessDifference(Color a, Color b)
-        {
-            return Math.Abs(GetColorBrightnessIndex(a) - GetColorBrightnessIndex(b));
+            var hsl = new HslColor(color);
+            return hsl.WithBrightness(hsl.L - amount).ToColor();
         }
     }
 }
