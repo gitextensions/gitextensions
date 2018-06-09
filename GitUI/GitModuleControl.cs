2078de2909dfa5d560d07b707ae7530af90eb46c
Drew Noakes <git@drewnoakes.com>
2018-06-10 00:00:25 +0100

    Join null check with assignment

--- GitUI/GitModuleControl.cs
+++ GitUI/GitModuleControl.cs
@@ -37,17 +37,12 @@ namespace GitUI
             }
             set
             {
-                if (value == null)
-                {
-                    throw new ArgumentException("Can not assign null value to UICommandsSource");
-                }
-
                 if (_uiCommandsSource != null)
                 {
                     throw new ArgumentException("UICommandsSource is already set");
                 }
 
-                _uiCommandsSource = value;
+                _uiCommandsSource = value ?? throw new ArgumentException("Can not assign null value to UICommandsSource");
                 OnUICommandsSourceChanged(_uiCommandsSource);
             }
         }
@@ -132,12 +127,7 @@ namespace GitUI
                     }
                 }
 
-                if (cmdsSrc == null)
-                {
-                    throw new InvalidOperationException("The UI Command Source is not available for this control. Are you calling methods before adding it to the parent control?");
-                }
-
-                UICommandsSource = cmdsSrc;
+                UICommandsSource = cmdsSrc ?? throw new InvalidOperationException("The UI Command Source is not available for this control. Are you calling methods before adding it to the parent control?");
             }
         }
 
