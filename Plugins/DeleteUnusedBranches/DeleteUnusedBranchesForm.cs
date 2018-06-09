5eb83d1861a5de1f63687d69519b293b517a755c
Drew Noakes <git@drewnoakes.com>
2018-06-09 18:38:45 +0100

    Typos

--- Plugins/DeleteUnusedBranches/DeleteUnusedBranchesForm.cs
+++ Plugins/DeleteUnusedBranches/DeleteUnusedBranchesForm.cs
@@ -22,7 +22,7 @@ namespace DeleteUnusedBranches
         private readonly TranslationString _dangerousAction = new TranslationString("DANGEROUS ACTION!\nBranches will be deleted on the remote '{0}'. This can not be undone.\nAre you sure you want to continue?");
         private readonly TranslationString _deletingBranches = new TranslationString("Deleting branches...");
         private readonly TranslationString _deletingUnmergedBranches = new TranslationString("Deleting unmerged branches will result in dangling commits. Use with caution!");
-        private readonly TranslationString _chooseBrancesToDelete = new TranslationString("Choose branches to delete. Only branches that are fully merged in '{0}' will be deleted.");
+        private readonly TranslationString _chooseBranchesToDelete = new TranslationString("Choose branches to delete. Only branches that are fully merged in '{0}' will be deleted.");
         private readonly TranslationString _pressToSearch = new TranslationString("Press '{0}' to search for branches to delete.");
         private readonly TranslationString _cancel = new TranslationString("Cancel");
         private readonly TranslationString _searchBranches = new TranslationString("Search branches");
@@ -201,7 +201,7 @@ namespace DeleteUnusedBranches
 
         private void ClearResults(object sender, EventArgs e)
         {
-            instructionLabel.Text = string.Format(_chooseBrancesToDelete.Text, mergedIntoBranch.Text);
+            instructionLabel.Text = string.Format(_chooseBranchesToDelete.Text, mergedIntoBranch.Text);
             lblStatus.Text = string.Format(_pressToSearch.Text, RefreshBtn.Text);
             _branches.Clear();
             _branches.ResetBindings();
