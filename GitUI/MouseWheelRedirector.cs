4f93458f803c890b792f35a5220971def6af1bbf
Drew Noakes <git@drewnoakes.com>
2018-06-09 18:26:43 +0100

    Formatting

--- GitUI/MouseWheelRedirector.cs
+++ GitUI/MouseWheelRedirector.cs
@@ -94,15 +94,8 @@ namespace GitUI
                     Y = y;
                 }
 
-                public static implicit operator Point(POINT p)
-                {
-                    return new Point(p.X, p.Y);
-                }
-
-                public static implicit operator POINT(Point p)
-                {
-                    return new POINT(p.X, p.Y);
-                }
+                public static implicit operator Point(POINT p) => new Point(p.X, p.Y);
+                public static implicit operator POINT(Point p) => new POINT(p.X, p.Y);
             }
         }
     }
