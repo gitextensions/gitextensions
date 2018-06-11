4c6c9b2dc0f1dfedfa0cb77e34da09e647da90ac
Drew Noakes <git@drewnoakes.com>
2018-06-11 13:49:24 +0100

    Refactor into RevisionNodeFlags
    Also allow checked-out outline to surround both circle and square nodes.

--- GitUI/UserControls/RevisionGrid/Graph/Node.cs
+++ GitUI/UserControls/RevisionGrid/Graph/Node.cs
@@ -10,15 +10,15 @@ namespace GitUI.UserControls.RevisionGrid.Graph
         public string ObjectId { get; }
 
         public GitRevision Data { get; set; }
-        public RevisionDataGridView.DataTypes DataTypes { get; set; }
+        public RevisionNodeFlags Flags { get; set; }
 
         public int InLane { get; set; } = int.MaxValue;
         public int Index { get; set; } = int.MaxValue;
 
         public Node(string objectId) => ObjectId = objectId;
 
-        public bool IsCheckedOut => DataTypes.HasFlag(RevisionDataGridView.DataTypes.CheckedOut);
-        public bool IsSpecial => DataTypes.HasFlag(RevisionDataGridView.DataTypes.Special);
+        public bool IsCheckedOut => Flags.HasFlag(RevisionNodeFlags.CheckedOut);
+        public bool HasRef => Flags.HasFlag(RevisionNodeFlags.HasRef);
 
         public override string ToString() => Data?.ToString() ?? $"{ObjectId} ({Index})";
     }
