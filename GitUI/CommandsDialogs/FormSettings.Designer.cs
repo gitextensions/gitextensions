348e6d05de4bcb22636293f421e8ac60de34e427
Drew Noakes <git@drewnoakes.com>
2018-06-11 23:02:56 +0100

    Remove unused event handler

--- GitUI/CommandsDialogs/FormSettings.Designer.cs
+++ GitUI/CommandsDialogs/FormSettings.Designer.cs
@@ -222,7 +222,6 @@ namespace GitUI.CommandsDialogs
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Settings";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormSettings_FormClosing);
             this.Load += new System.EventHandler(this.FormSettings_Load);
             this.Shown += new System.EventHandler(this.FormSettings_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
