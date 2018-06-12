d19d80d0b39b9be20e28636d4258ec313188c1cd
Drew Noakes <git@drewnoakes.com>
2018-06-12 20:49:01 +0100

    Refactor FormContributors

--- GitUI/CommandsDialogs/AboutBoxDialog/FormContributors.cs
+++ GitUI/CommandsDialogs/AboutBoxDialog/FormContributors.cs
@@ -4,92 +4,92 @@ using System.Windows.Forms;
 
 namespace GitUI.CommandsDialogs.AboutBoxDialog
 {
-    public class FormContributors : GitExtensionsForm
+    public sealed class FormContributors : GitExtensionsForm
     {
         private static readonly string[] tabCaptions = { "The Coders", "The Translators", "The Designers" };
 
-        private readonly TextBox[] _textboxes = new TextBox[tabCaptions.Length];
+        private readonly TextBox[] _textBoxes = new TextBox[tabCaptions.Length];
         private readonly TabPage[] _tabPages = new TabPage[tabCaptions.Length];
-        private TabControl _tabControl;
 
         public FormContributors()
         {
             SetupForm();
             Translate();
             this.AdjustForDpiScaling();
-        }
 
-        private static TextBox GetNewTextBox()
-        {
-            return new TextBox
+            void SetupForm()
             {
-                BackColor = Color.White,
-                Dock = DockStyle.Fill,
-                Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0),
-                Margin = new Padding(0),
-                Multiline = true,
-                ReadOnly = true,
-                ScrollBars = ScrollBars.Vertical,
-                TabStop = false
-            };
-        }
+                SuspendLayout();
+                Controls.Clear();
 
-        private static TabPage GetNewTabPage(TextBox tb, string caption)
-        {
-            var tp = new TabPage
-            {
-                Margin = new Padding(0),
-                Text = caption
-            };
-            tp.Controls.Add(tb);
-            return tp;
-        }
+                var tabControl = GetNewTabControl();
+                ////tabControl.SuspendLayout();
 
-        private static TabControl GetNewTabControl()
-        {
-            return new TabControl
-            {
-                Dock = DockStyle.Fill,
-                Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0),
-                SelectedIndex = 0,
-            };
-        }
+                for (var i = 0; i < tabCaptions.Length; i++)
+                {
+                    _textBoxes[i] = GetNewTextBox();
+                    _tabPages[i] = GetNewTabPage(_textBoxes[i], tabCaptions[i]);
+                    tabControl.Controls.Add(_tabPages[i]);
+                }
 
-        private void SetupForm()
-        {
-            SuspendLayout();
-            Controls.Clear();
+                Controls.Add(tabControl);
 
-            _tabControl = GetNewTabControl();
-            _tabControl.SuspendLayout();
+                AutoScaleDimensions = new SizeF(96F, 96F);
+                AutoScaleMode = AutoScaleMode.Dpi;
+                ClientSize = new Size(624, 442);
+                FormBorderStyle = FormBorderStyle.FixedDialog;
+                MaximizeBox = false;
+                MinimizeBox = false;
+                StartPosition = FormStartPosition.CenterParent;
+                Text = "Thanks to...";
 
-            for (int i = 0; i < tabCaptions.Length; i++)
-            {
-                _textboxes[i] = GetNewTextBox();
-                _tabPages[i] = GetNewTabPage(_textboxes[i], tabCaptions[i]);
-                _tabControl.Controls.Add(_tabPages[i]);
+                ResumeLayout(false);
+
+                TextBox GetNewTextBox()
+                {
+                    return new TextBox
+                    {
+                        BackColor = Color.White,
+                        Dock = DockStyle.Fill,
+                        Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0),
+                        Margin = new Padding(0),
+                        Multiline = true,
+                        ReadOnly = true,
+                        ScrollBars = ScrollBars.Vertical,
+                        TabStop = false
+                    };
+                }
+
+                TabPage GetNewTabPage(TextBox tb, string caption)
+                {
+                    var tp = new TabPage
+                    {
+                        Margin = new Padding(0),
+                        Text = caption
+                    };
+                    tp.Controls.Add(tb);
+                    return tp;
+                }
+
+                TabControl GetNewTabControl()
+                {
+                    return new TabControl
+                    {
+                        Dock = DockStyle.Fill,
+                        Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0),
+                        SelectedIndex = 0,
+                    };
+                }
             }
-
-            Controls.Add(_tabControl);
-
-            AutoScaleDimensions = new SizeF(96F, 96F);
-            AutoScaleMode = AutoScaleMode.Dpi;
-            ClientSize = new Size(624, 442);
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            StartPosition = FormStartPosition.CenterParent;
-            Text = "Thanks to...";
-
-            ResumeLayout(false);
         }
 
         public void LoadContributors(string coders, string translators, string designers)
         {
             const string NEWLINES = @"\r\n?|\n";
-            _textboxes[0].Text = Regex.Replace(coders, NEWLINES, " ");
-            _textboxes[1].Text = Regex.Replace(translators, NEWLINES, " ");
-            _textboxes[2].Text = Regex.Replace(designers, NEWLINES, " ");
+
+            _textBoxes[0].Text = Regex.Replace(coders, NEWLINES, " ");
+            _textBoxes[1].Text = Regex.Replace(translators, NEWLINES, " ");
+            _textBoxes[2].Text = Regex.Replace(designers, NEWLINES, " ");
         }
     }
 }
