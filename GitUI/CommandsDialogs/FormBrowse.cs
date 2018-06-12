9f121b3e0133bb778adfbd3fb91a4ed4e5d31c74
Drew Noakes <git@drewnoakes.com>
2018-06-12 20:58:24 +0100

    Use HasFlag

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -1108,8 +1108,7 @@ namespace GitUI.CommandsDialogs
 
         private void PushToolStripMenuItemClick(object sender, EventArgs e)
         {
-            bool isSilent = (ModifierKeys & Keys.Shift) != 0;
-            UICommands.StartPushDialog(this, isSilent);
+            UICommands.StartPushDialog(this, pushOnShow: ModifierKeys.HasFlag(Keys.Shift));
         }
 
         private void PullToolStripMenuItemClick(object sender, EventArgs e)
