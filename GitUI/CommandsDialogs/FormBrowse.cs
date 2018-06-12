06fadabdb8b84fb178a15f3ad3adc79aa337c2a0
Drew Noakes <git@drewnoakes.com>
2018-06-12 23:58:17 +0100

    Enforce SplitContainer and TabControl layout
    Previously the AdjustForDpiScaling method would only walk the control
    tree if not at 96 DPI. This change means we always walk the tree, but
    now set some values that are not specific to DPI issues:
    
    - Splitter width
    - Tab control heading padding
    
    Also fixes bug where we wouldn't walk children of a SplitContainer.

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -141,7 +141,6 @@ namespace GitUI.CommandsDialogs
             CommitInfoTabControl.TabPages[1].ImageIndex = 1;
             CommitInfoTabControl.TabPages[2].ImageIndex = 2;
             CommitInfoTabControl.TabPages[3].ImageIndex = 3;
-            CommitInfoTabControl.Padding = DpiUtil.Scale(new Point(8, 6));
 
             if (!AppSettings.ShowGpgInformation.ValueOrDefault)
             {
