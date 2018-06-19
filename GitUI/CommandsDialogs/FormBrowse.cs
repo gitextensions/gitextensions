3151906a283cb43dc32323460bc93067c1371cf3
Drew Noakes <git@drewnoakes.com>
2018-06-19 11:31:04 +0100

    Rename field

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -2377,11 +2377,11 @@ namespace GitUI.CommandsDialogs
             return spmenu;
         }
 
-        private DateTime _previousUpdateTime;
+        private DateTime _previousSubmoduleUpdateTime;
 
         private void LoadSubmodulesIntoDropDownMenu()
         {
-            TimeSpan elapsed = DateTime.Now - _previousUpdateTime;
+            TimeSpan elapsed = DateTime.Now - _previousSubmoduleUpdateTime;
             if (elapsed.TotalSeconds > 15)
             {
                 UpdateSubmodulesList();
@@ -2470,7 +2470,7 @@ namespace GitUI.CommandsDialogs
 
         private void UpdateSubmodulesList()
         {
-            _previousUpdateTime = DateTime.Now;
+            _previousSubmoduleUpdateTime = DateTime.Now;
 
             // Cancel any previous async activities:
             var cancelToken = _submodulesStatusSequence.Next();
@@ -2623,7 +2623,7 @@ namespace GitUI.CommandsDialogs
                 // time, performance would be extremely slow with many submodules (> 100).
                 toolStripButtonLevelUp.DropDownItems.AddRange(newItems.ToArray());
 
-                _previousUpdateTime = DateTime.Now;
+                _previousSubmoduleUpdateTime = DateTime.Now;
             });
         }
 
