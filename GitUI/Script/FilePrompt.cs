00336ce90bf1a0feb3754d9bfb87a3c4536438a7
oriash93 <oriash93@gmail.com>
2018-06-20 00:41:29 +0300

    Created FilePrompt form

---
+++ GitUI/Script/FilePrompt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GitUI.Script
+{
+    public partial class FilePrompt : Form
+    {
+        public string FileInput { get; private set; } = string.Empty;
+
+        public FilePrompt()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            FileInput = txt_FilePath.Text;
+            Close();
+        }
+
+        private void FilePrompt_Shown(object sender, EventArgs e)
+        {
+            txt_FilePath.Focus();
+        }
+
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {
+            using (var browseDialog = new OpenFileDialog
+            {
+                InitialDirectory = "."
+            })
+            {
+                if (browseDialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    txt_FilePath.Text = browseDialog.FileName;
+                }
+            }
+        }
+    }
+}
