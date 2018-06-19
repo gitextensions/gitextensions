e0349436f718cefb15097a843fe6c0ccc3efb026
oriash93 <oriash93@gmail.com>
2018-06-20 00:42:26 +0300

    Added {UserFile} option to list + localizations

--- GitUI/CommandsDialogs/SettingsDialog/Pages/ScriptsSettingsPage.cs
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/ScriptsSettingsPage.cs
@@ -16,6 +16,7 @@ namespace GitUI.CommandsDialogs.SettingsDialog.Pages
         private readonly TranslationString _scriptSettingsPageHelpDisplayArgumentsHelp = new TranslationString("Arguments help");
         private readonly TranslationString _scriptSettingsPageHelpDisplayContent = new TranslationString(@"User Input:
 {UserInput}
+{UserFile}
 
 Working Dir:
 {WorkingDir}
