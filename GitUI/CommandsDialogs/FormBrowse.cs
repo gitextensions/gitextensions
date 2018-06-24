0a543b438f80cda8d02ab83cc11ec1f70d6ec0eb
Drew Noakes <git@drewnoakes.com>
2018-06-25 00:09:06 +0100

    Remember split view layout setting
    Fixes #3072.

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -349,6 +349,7 @@ namespace GitUI.CommandsDialogs
 
             SetSplitterPositions();
             HideVariableMainMenuItems();
+            RefreshSplitViewLayout();
 
             RevisionGrid.Load();
             _filterBranchHelper.InitToolStripBranchFilter();
@@ -2026,7 +2027,9 @@ namespace GitUI.CommandsDialogs
         private void FindFileInSelectedCommit()
         {
             CommitInfoTabControl.SelectedTab = TreeTabPage;
-            EnabledSplitViewLayout(enabled: true);
+
+            AppSettings.ShowSplitViewLayout = true;
+            RefreshSplitViewLayout();
 
             fileTree.InvokeFindFileDialog();
         }
@@ -2882,7 +2885,8 @@ namespace GitUI.CommandsDialogs
 
         private void toggleSplitViewLayout_Click(object sender, EventArgs e)
         {
-            EnabledSplitViewLayout(RightSplitContainer.Panel2Collapsed);
+            AppSettings.ShowSplitViewLayout = !AppSettings.ShowSplitViewLayout;
+            RefreshSplitViewLayout();
         }
 
         private void toggleBranchTreePanel_Click(object sender, EventArgs e)
@@ -2899,16 +2903,16 @@ namespace GitUI.CommandsDialogs
             RefreshLayoutToggleButtonStates();
         }
 
-        private void EnabledSplitViewLayout(bool enabled)
+        private void RefreshSplitViewLayout()
         {
-            RightSplitContainer.Panel2Collapsed = !enabled;
+            RightSplitContainer.Panel2Collapsed = !AppSettings.ShowSplitViewLayout;
             RefreshLayoutToggleButtonStates();
         }
 
         private void RefreshLayoutToggleButtonStates()
         {
             toggleBranchTreePanel.Checked = !MainSplitContainer.Panel1Collapsed;
-            toggleSplitViewLayout.Checked = !RightSplitContainer.Panel2Collapsed;
+            toggleSplitViewLayout.Checked = AppSettings.ShowSplitViewLayout;
             toggleCommitInfoOnRight.Checked = AppSettings.ShowRevisionInfoNextToRevisionGrid;
         }
 
