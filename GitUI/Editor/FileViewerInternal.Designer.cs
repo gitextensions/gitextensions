4f02e168b46afb49d55ec792442a1cba97a0d5d1
Drew Noakes <git@drewnoakes.com>
2018-06-25 10:17:31 +0100

    Hide vertical rule

--- GitUI/Editor/FileViewerInternal.Designer.cs
+++ GitUI/Editor/FileViewerInternal.Designer.cs
@@ -40,6 +40,7 @@
             this.TextEditor.Name = "TextEditor";
             this.TextEditor.Size = new System.Drawing.Size(757, 519);
             this.TextEditor.TabIndex = 3;
+            this.TextEditor.ShowVRuler = false;
             // 
             // FileViewerInternal
             // 
