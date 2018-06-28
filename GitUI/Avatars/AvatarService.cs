1c9b1061a8738b8b4953e1d2140b0d299fd06d83
Drew Noakes <git@drewnoakes.com>
2018-06-28 11:46:30 +0100

    Use BackupAvatarProvider
    This class provides a default image if an exception is thrown anywhere
    in the provider chain.

--- GitUI/Avatars/AvatarService.cs
+++ GitUI/Avatars/AvatarService.cs
@@ -2,9 +2,7 @@
 {
     public static class AvatarService
     {
-        // TODO get a backup image
         public static IAvatarProvider Default { get; }
-            ////= new BackupAvatarProvider(new AvatarMemoryCache(new AvatarPersistentCache(new AvatarDownloader())), GitUI.Properties.Resources.User);
-            = new AvatarMemoryCache(new AvatarPersistentCache(new AvatarDownloader()));
+            = new BackupAvatarProvider(new AvatarMemoryCache(new AvatarPersistentCache(new AvatarDownloader())), Properties.Resources.User);
     }
 }
