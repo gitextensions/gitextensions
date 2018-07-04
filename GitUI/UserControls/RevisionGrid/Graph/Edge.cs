6b7b3ff002376f0d557ae62d7b625dcc9114fbb5
Drew Noakes <git@drewnoakes.com>
2018-07-04 11:03:59 +0100

    Move nested types to outer scope

---
+++ GitUI/UserControls/RevisionGrid/Graph/Edge.cs
@@ -0,0 +1,18 @@
+﻿namespace GitUI.UserControls.RevisionGrid.Graph
+{
+    internal readonly struct Edge
+    {
+        public readonly int Start;
+        public readonly LaneInfo Data;
+
+        public Edge(LaneInfo data, int start)
+        {
+            Data = data;
+            Start = start;
+        }
+
+        public int End => Data.ConnectLane;
+
+        public override string ToString() => $"{Start}->{End}: {Data}";
+    }
+}
