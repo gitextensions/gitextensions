8200a377a53c708384a8c78d0283b78d99030567
Drew Noakes <git@drewnoakes.com>
2018-07-05 12:16:58 +0100

    Pack flags in GitItemStatus
    Each bool was 4 bytes. This should save 40 bytes per instance.

--- GitCommands/Git/GitItemStatus.cs
+++ GitCommands/Git/GitItemStatus.cs
@@ -15,24 +15,31 @@ namespace GitCommands
         Index
     }
 
-    public class GitItemStatus : IComparable<GitItemStatus>
+    public sealed class GitItemStatus : IComparable<GitItemStatus>
     {
+        [Flags]
+        private enum Flags
+        {
+            IsTracked = 1 << 1,
+            IsDeleted = 1 << 2,
+            IsChanged = 1 << 3,
+            IsNew = 1 << 4,
+            IsIgnored = 1 << 5,
+            IsRenamed = 1 << 6,
+            IsCopied = 1 << 7,
+            IsConflict = 1 << 8,
+            IsAssumeUnchanged = 1 << 9,
+            IsSkipWorktree = 1 << 10,
+            IsSubmodule = 1 << 11,
+        }
+
         private JoinableTask<GitSubmoduleStatus> _submoduleStatus;
+
+        private Flags _flags;
 
         public string Name { get; set; }
         public string OldName { get; set; }
         public string TreeGuid { get; set; }
-        public bool IsTracked { get; set; }
-        public bool IsDeleted { get; set; }
-        public bool IsChanged { get; set; }
-        public bool IsNew { get; set; }
-        public bool IsIgnored { get; set; }
-        public bool IsRenamed { get; set; }
-        public bool IsCopied { get; set; }
-        public bool IsConflict { get; set; }
-        public bool IsAssumeUnchanged { get; set; }
-        public bool IsSkipWorktree { get; set; }
-        public bool IsSubmodule { get; set; }
         public string RenameCopyPercentage { get; set; }
 
         // Staged is three state and has no default status
@@ -51,6 +58,88 @@ namespace GitCommands
                 _staged = value;
             }
         }
+
+        #region Flags
+
+        public bool IsTracked
+        {
+            get => _flags.HasFlag(Flags.IsTracked);
+            set => SetFlag(value, Flags.IsTracked);
+        }
+
+        public bool IsDeleted
+        {
+            get => _flags.HasFlag(Flags.IsDeleted);
+            set => SetFlag(value, Flags.IsDeleted);
+        }
+
+        public bool IsChanged
+        {
+            get => _flags.HasFlag(Flags.IsChanged);
+            set => SetFlag(value, Flags.IsChanged);
+        }
+
+        public bool IsNew
+        {
+            get => _flags.HasFlag(Flags.IsNew);
+            set => SetFlag(value, Flags.IsNew);
+        }
+
+        public bool IsIgnored
+        {
+            get => _flags.HasFlag(Flags.IsIgnored);
+            set => SetFlag(value, Flags.IsIgnored);
+        }
+
+        public bool IsRenamed
+        {
+            get => _flags.HasFlag(Flags.IsRenamed);
+            set => SetFlag(value, Flags.IsRenamed);
+        }
+
+        public bool IsCopied
+        {
+            get => _flags.HasFlag(Flags.IsCopied);
+            set => SetFlag(value, Flags.IsCopied);
+        }
+
+        public bool IsConflict
+        {
+            get => _flags.HasFlag(Flags.IsConflict);
+            set => SetFlag(value, Flags.IsConflict);
+        }
+
+        public bool IsAssumeUnchanged
+        {
+            get => _flags.HasFlag(Flags.IsAssumeUnchanged);
+            set => SetFlag(value, Flags.IsAssumeUnchanged);
+        }
+
+        public bool IsSkipWorktree
+        {
+            get => _flags.HasFlag(Flags.IsSkipWorktree);
+            set => SetFlag(value, Flags.IsSkipWorktree);
+        }
+
+        public bool IsSubmodule
+        {
+            get => _flags.HasFlag(Flags.IsSubmodule);
+            set => SetFlag(value, Flags.IsSubmodule);
+        }
+
+        private void SetFlag(bool isSet, Flags flag)
+        {
+            if (isSet)
+            {
+                _flags |= flag;
+            }
+            else
+            {
+                _flags &= ~flag;
+            }
+        }
+
+        #endregion
 
         [CanBeNull]
         public Task<GitSubmoduleStatus> GetSubmoduleStatusAsync()
