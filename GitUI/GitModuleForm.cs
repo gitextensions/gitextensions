7f635b6f70579cfb8b5d62e3f1fcd2620f19971f
Drew Noakes <git@drewnoakes.com>
2018-08-06 11:53:46 +0100

    Update GitModuleForm class documentation

--- GitUI/GitModuleForm.cs
+++ GitUI/GitModuleForm.cs
@@ -7,8 +7,9 @@ using JetBrains.Annotations;
 
 namespace GitUI
 {
-    /// <summary>Base class for a <see cref="Form"/> requiring
-    /// <see cref="GitModule"/> and <see cref="GitUICommands"/>.</summary>
+    // NOTE do not make this class abstract as it breaks the WinForms designer in VS
+
+    /// <summary>Base <see cref="Form"/> that provides access to <see cref="GitModule"/> and <see cref="GitUICommands"/>.</summary>
     public class GitModuleForm : GitExtensionsForm, IGitUICommandsSource
     {
         /// <inheritdoc />
