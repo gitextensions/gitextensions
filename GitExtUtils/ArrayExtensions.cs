53d5fc6950c4ed2f96b1f65920c3e61b0ea3dcfe
Drew Noakes <git@drewnoakes.com>
2018-08-06 22:19:03 +0100

    Move ArrayExtensions to GitExtUtils
    Add Append method, and unit test.

--- GitExtUtils/ArrayExtensions.cs
+++ GitExtUtils/ArrayExtensions.cs
@@ -12,5 +12,14 @@ namespace GitCommands
             Array.Copy(array, index, sub, 0, length);
             return sub;
         }
+
+        [MustUseReturnValue]
+        public static T[] Append<T>([NotNull] this T[] array, T element)
+        {
+            var larger = new T[array.Length + 1];
+            Array.Copy(array, larger, array.Length);
+            larger[array.Length] = element;
+            return larger;
+        }
     }
 }
