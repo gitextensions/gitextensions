0acc4fe9a5d74e50fe6f633b4256a0f986f476e6
yiabiten <iabiten@gmail.com>
2018-09-16 23:26:21 +0100

    [refctoring] Merge AddItem and AddImmutableItem

--- GitUI/UserControls/RevisionGrid/CopyContextMenuItem.cs
+++ GitUI/UserControls/RevisionGrid/CopyContextMenuItem.cs
@@ -20,9 +20,22 @@ namespace GitUI.UserControls.RevisionGrid
             Image = Images.CopyToClipboard;
             Text = "Copy to clipboard";
 
+            // Action template
+            Action<Func<GitRevision, string>> extractText = (Func<GitRevision, string> extractRevisionText) =>
+            {
+                var gitRevisions = _revisionFunc?.Invoke();
+                var sb = new StringBuilder();
+                foreach (var revision in gitRevisions)
+                {
+                    sb.AppendLine(extractRevisionText(revision));
+                }
+
+                Clipboard.SetText(sb.ToString());
+            };
+
             // Add a dummy copy item, so that the shortcut key works.
             // This item will never be seen by the user, as the submenu is rebuilt on opening.
-            AddItem("Dummy item", r => r.Guid, image: null, Keys.Control | Keys.C, showShortcutKeys: false);
+            AddItem("Dummy item", () => extractText(r => r.Guid), image: null, Keys.Control | Keys.C, showShortcutKeys: false);
 
             DropDownOpening += OnDropDownOpening;
 
@@ -55,7 +68,7 @@ namespace GitUI.UserControls.RevisionGrid
 
                     foreach (var name in branchNames)
                     {
-                        AddImmutableItem(name, Images.Branch);
+                        AddItem(name, () => Clipboard.SetText(name), Images.Branch);
                     }
 
                     DropDownItems.Add(new ToolStripSeparator());
@@ -70,21 +83,20 @@ namespace GitUI.UserControls.RevisionGrid
 
                     foreach (var name in tagNames)
                     {
-                        AddImmutableItem(name, Images.Tag);
+                        AddItem(name, () => Clipboard.SetText(name), Images.Tag);
                     }
 
                     DropDownItems.Add(new ToolStripSeparator());
                 }
 
-                // Add other items
-                AddItem(Strings.CommitHash,  (GitRevision r) => r.Guid, Images.CommitId, Keys.Control | Keys.C);
-                AddItem(Strings.Message,     r => r.Body ?? r.Subject, Images.Message);
-                AddItem(Strings.Author,      r => r.Author, Images.Author);
-                AddItem(Strings.AuthorDate,  r => r.AuthorDate.ToString(), Images.Date);
-                AddItem(Strings.CommitDate,  r => r.CommitDate.ToString(), Images.Date);
+                AddItem(Strings.CommitHash, () => extractText(r => r.Guid), Images.CommitId, Keys.Control | Keys.C);
+                AddItem(Strings.Message, () => extractText(r => r.Body ?? r.Subject), Images.Message);
+                AddItem(Strings.Author, () => extractText(r => r.Author), Images.Author);
+                AddItem(Strings.AuthorDate, () => extractText(r => r.AuthorDate.ToString()), Images.Date);
+                AddItem(Strings.CommitDate, () => extractText(r => r.CommitDate.ToString()), Images.Date);
             }
 
-            void AddImmutableItem(string displayText, Image image, Keys shortcutKeys = Keys.None, bool showShortcutKeys = true)
+            void AddItem(string displayText, Action clickAction, Image image, Keys shortcutKeys = Keys.None, bool showShortcutKeys = true)
             {
                 var item = new ToolStripMenuItem
                 {
@@ -95,31 +107,7 @@ namespace GitUI.UserControls.RevisionGrid
                 };
                 item.Click += delegate
                 {
-                    Clipboard.SetText(displayText);
-                };
-
-                DropDownItems.Add(item);
-            }
-
-            void AddItem(string displayText, Func<GitRevision, string> extractTextFunc, Image image, Keys shortcutKeys = Keys.None, bool showShortcutKeys = true)
-            {
-                var item = new ToolStripMenuItem
-                {
-                    Text = displayText,
-                    ShortcutKeys = shortcutKeys,
-                    ShowShortcutKeys = showShortcutKeys,
-                    Image = image
-                };
-                item.Click += delegate
-                {
-                    var revisions = _revisionFunc?.Invoke();
-                    var sb = new StringBuilder();
-                    foreach (var revision in revisions)
-                    {
-                        sb.AppendLine(extractTextFunc(revision));
-                    }
-
-                    Clipboard.SetText(sb.ToString());
+                    clickAction();
                 };
 
                 DropDownItems.Add(item);
