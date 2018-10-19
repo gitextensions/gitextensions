gitextensions/gitextensions

commit 71919e5dd5f9d1c87af5ee31ec3d3a3d0e2a8cbb

WIP: Implement #5327, solves the main part of #5328, too

GitUI/CommandsDialogs/FormCommit.cs
@@ -817,38 +817,8 @@ namespace GitUI.CommandsDialogs
                 Unstaged.StoreNextIndexToSelect();
             }
 
-            ScheduleGoToLine();
             _selectedDiffReloaded = false;
             RescanChanges();
-
-            return;
-
-            void ScheduleGoToLine()
-            {
-                int selectedDiffLineToSelect = SelectedDiff.GetText().Substring(0, SelectedDiff.GetSelectionPosition()).Count(c => c == '\n');
-                int scrollPosition = SelectedDiff.ScrollPos;
-                string selectedFileName = _currentItem.Name;
-
-                OnStageAreaLoaded += StageAreaLoaded;
-
-                void StageAreaLoaded()
-                {
-                    void TextLoaded(object a, EventArgs b)
-                    {
-                        if (_currentItem != null && _currentItem.Name == selectedFileName)
-                        {
-                            SelectedDiff.GoToLine(selectedDiffLineToSelect);
-                            SelectedDiff.ScrollPos = scrollPosition;
-                        }
-
-                        SelectedDiff.TextLoaded -= TextLoaded;
-                        _selectedDiffReloaded = true;
-                    }
-
-                    SelectedDiff.TextLoaded += TextLoaded;
-                    OnStageAreaLoaded -= StageAreaLoaded;
-                }
-            }
         }
 
         private void ResetSelectedLinesToolStripMenuItemClick(object sender, EventArgs e)

GitUI/CommandsDialogs/FormFileHistory.cs
@@ -287,9 +287,6 @@ namespace GitUI.CommandsDialogs
 
         private void FileChangesSelectionChanged(object sender, EventArgs e)
         {
-            View.SaveCurrentScrollPos();
-            Diff.SaveCurrentScrollPos();
-
             UpdateSelectedFileViewers();
         }
 
@@ -366,11 +363,8 @@ namespace GitUI.CommandsDialogs
             }
             else if (tabControl1.SelectedTab == ViewTab)
             {
-                var scrollPos = View.ScrollPos;
-
                 View.Encoding = Diff.Encoding;
                 View.ViewGitItemRevisionAsync(fileName, revision.ObjectId);
-                View.ScrollPos = scrollPos;
             }
             else if (tabControl1.SelectedTab == DiffTab)
             {

GitUI/CommandsDialogs/RevisionDiffControl.cs
@@ -140,7 +140,6 @@ namespace GitUI.CommandsDialogs
 
         public void DisplayDiffTab()
         {
-            DiffText.SaveCurrentScrollPos();
             var revisions = _revisionGrid.GetSelectedRevisions();
             DiffFiles.SetDiffs(revisions);
             if (_oldDiffItem != null && DiffFiles.Revision?.Guid == _oldRevision)

GitUI/CommandsDialogs/RevisionFileTreeControl.cs
@@ -150,7 +150,6 @@ See the changes in the commit form.");
                 if (tvGitTree.SelectedNode != null)
                 {
                     var node = tvGitTree.SelectedNode;
-                    FileText.SaveCurrentScrollPos();
                     _lastSelectedNodes.Clear();
                     while (node != null)
                     {
@@ -196,12 +195,6 @@ See the changes in the commit form.");
                             lastMatchedNode = matchedNode;
                             currentNodes = matchedNode.Nodes;
                         }
-                    }
-
-                    // if there is no exact match, don't restore scroll position
-                    if (lastMatchedNode != matchedNode)
-                    {
-                        FileText.ResetCurrentScrollPos();
                     }
 
                     tvGitTree.SelectedNode = lastMatchedNode;

GitUI/Editor/Diff/DiffViewerLineNumberControl.cs
@@ -14,12 +14,24 @@ namespace GitUI.Editor.Diff
 
         private Dictionary<int, DiffLineNum> DiffLines { get; set; }
 
-        private int _maxValueOfLineNum;
+        public int MaxValueOfLineNum { get; private set; }
+
         private bool _visible = true;
 
         public DiffViewerLineNumberControl(TextArea textArea) : base(textArea)
         {
             DiffLines = new Dictionary<int, DiffLineNum>();
+        }
+
+        /// <summary>
+        /// returns the according line numbers or null if the caretLine is not mapped
+        /// </summary>
+        /// <param name="caretLine">0-based (in contrast to the displayed line numbers which are 1-based)</param>
+        public DiffLineNum GetLineNum(int caretLine)
+        {
+            DiffLineNum diffLine;
+            DiffLines.TryGetValue(caretLine + 1, out diffLine);
+            return diffLine;
         }
 
         public override int Width
@@ -28,7 +40,7 @@ namespace GitUI.Editor.Diff
             {
                 if (_visible && DiffLines.Any())
                 {
-                    return textArea.TextView.WideSpaceWidth * ((int)Math.Log10(_maxValueOfLineNum) + TextHorizontalMargin + 3);
+                    return textArea.TextView.WideSpaceWidth * ((int)Math.Log10(MaxValueOfLineNum) + TextHorizontalMargin + 3);
                 }
 
                 return 0;
@@ -113,7 +125,7 @@ namespace GitUI.Editor.Diff
         {
             var result = new DiffLineNumAnalyzer().Analyze(diff);
             DiffLines = result.LineNumbers;
-            _maxValueOfLineNum = result.MaxLineNumber;
+            MaxValueOfLineNum = result.MaxLineNumber;
         }
 
         public void Clear(bool forDiff)

GitUI/Editor/FileViewer.cs
@@ -37,7 +37,6 @@ namespace GitUI.Editor
 
         private readonly AsyncLoader _async;
         private readonly IFullPathResolver _fullPathResolver;
-        private int _currentScrollPos = -1;
         private bool _currentViewIsPatch;
         private bool _patchHighlighting;
         private Encoding _encoding;
@@ -338,27 +337,6 @@ namespace GitUI.Editor
             };
         }
 
-        public void SaveCurrentScrollPos()
-        {
-            _currentScrollPos = ScrollPos;
-        }
-
-        public void ResetCurrentScrollPos()
-        {
-            _currentScrollPos = 0;
-        }
-
-        private void RestoreCurrentScrollPos()
-        {
-            if (_currentScrollPos < 0)
-            {
-                return;
-            }
-
-            ScrollPos = _currentScrollPos;
-            ResetCurrentScrollPos();
-        }
-
         public Task ViewFileAsync(string fileName)
         {
             return ShowOrDeferAsync(
@@ -545,7 +523,6 @@ namespace GitUI.Editor
                         ResetForDiff();
                         internalFileViewer.SetText(text, openWithDifftool, isDiff: true);
                         TextLoaded?.Invoke(this, null);
-                        RestoreCurrentScrollPos();
                         return Task.CompletedTask;
                     }));
         }
@@ -581,8 +558,6 @@ namespace GitUI.Editor
             {
                 internalFileViewer.SetText(text, openWithDifftool);
                 TextLoaded?.Invoke(this, null);
-
-                RestoreCurrentScrollPos();
             }
 
             async Task<string> SummariseBinaryFileAsync()
@@ -1261,17 +1236,12 @@ namespace GitUI.Editor
 
         private void goToLineToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (!internalFileViewer.IsGotoLineUIApplicable())
-            {
-                return;
-            }
-
             using (var formGoToLine = new FormGoToLine())
             {
-                formGoToLine.SetMaxLineNumber(internalFileViewer.TotalNumberOfLines);
+                formGoToLine.SetMaxLineNumber(_internalFileViewer.MaxLineNumber);
                 if (formGoToLine.ShowDialog(this) == DialogResult.OK)
                 {
-                    GoToLine(formGoToLine.GetLineNumber() - 1);
+                    GoToLine(formGoToLine.GetLineNumber());
                 }
             }
         }

GitUI/Editor/FileViewerInternal.cs
@@ -22,11 +22,22 @@ namespace GitUI.Editor
         private readonly DiffViewerLineNumberControl _lineNumbersControl;
 
         private DiffHighlightService _diffHighlightService = DiffHighlightService.Instance;
-        private bool _isGotoLineUIApplicable = true;
+
+        private ViewPosition _previousViewPosition;
 
         public Action OpenWithDifftool { get; private set; }
 
-        public FileViewerInternal()
+        private struct ViewPosition
+        {
+            internal string FirstLine; // contains the file names in case of a diff
+            internal int TotalNumberOfLines; // if changed, CaretPosition and FirstVisibleLine must be ignored and the line number must be searched
+            internal TextLocation CaretPosition;
+            internal int FirstVisibleLine;
+            internal bool CaretVisible; // if not, FirstVisibleLine has priority for restoring
+            internal DiffLineNum ActiveLineNum;
+        }
+
+        public FileViewerInternal(Func<GitModule> moduleProvider)
         {
             InitializeComponent();
             InitializeComplete();
@@ -96,6 +107,47 @@ namespace GitUI.Editor
 
         public void SetText(string text, Action openWithDifftool, bool isDiff = false)
         {
+            if (TotalNumberOfLines > 1)
+            {
+                // store the previous view position
+                _previousViewPosition.FirstLine = GetLineText(0);
+                _previousViewPosition.TotalNumberOfLines = TotalNumberOfLines;
+                _previousViewPosition.CaretPosition = TextEditor.ActiveTextAreaControl.Caret.Position;
+                _previousViewPosition.FirstVisibleLine = FirstVisibleLine;
+                _previousViewPosition.CaretVisible
+                    = _previousViewPosition.CaretPosition.Line >= _previousViewPosition.FirstVisibleLine
+                      && _previousViewPosition.CaretPosition.Line < _previousViewPosition.FirstVisibleLine + TextEditor.ActiveTextAreaControl.TextArea.TextView.VisibleLineCount;
+                _previousViewPosition.ActiveLineNum = null;
+                int initialActiveLine = _previousViewPosition.CaretVisible ? _previousViewPosition.CaretPosition.Line : _previousViewPosition.FirstVisibleLine;
+                if (/*a diff was displayed*/!TextEditor.ShowLineNumbers)
+                {
+                    void SetActiveLineNum(int line)
+                    {
+                        _previousViewPosition.ActiveLineNum = _lineNumbersControl.GetLineNum(line);
+                        if (_previousViewPosition.ActiveLineNum != null)
+                        {
+                            if (_previousViewPosition.ActiveLineNum.LeftLineNum == DiffLineNum.NotApplicableLineNum
+                                && _previousViewPosition.ActiveLineNum.RightLineNum == DiffLineNum.NotApplicableLineNum)
+                            {
+                                _previousViewPosition.ActiveLineNum = null;
+                            }
+                        }
+                    }
+
+                    // search downwards for a code line, i.e. a line with line numbers
+                    for (int activeLine = initialActiveLine; activeLine < _previousViewPosition.TotalNumberOfLines && _previousViewPosition.ActiveLineNum == null; ++activeLine)
+                    {
+                        SetActiveLineNum(activeLine);
+                    }
+
+                    // if none found, search upwards
+                    for (int activeLine = initialActiveLine - 1; activeLine >= 0 && _previousViewPosition.ActiveLineNum == null; --activeLine)
+                    {
+                        SetActiveLineNum(activeLine);
+                    }
+                }
+            }
+
             OpenWithDifftool = openWithDifftool;
             _lineNumbersControl.Clear(isDiff);
 
@@ -120,7 +172,6 @@ namespace GitUI.Editor
             }
 
             TextEditor.Text = text;
-            _isGotoLineUIApplicable = !isDiff;
 
             if (isDiff)
             {
@@ -128,6 +179,35 @@ namespace GitUI.Editor
             }
 
             TextEditor.Refresh();
+
+            if (TotalNumberOfLines > 1)
+            {
+                if (TotalNumberOfLines == _previousViewPosition.TotalNumberOfLines)
+                {
+                    FirstVisibleLine = _previousViewPosition.FirstVisibleLine;
+                    TextEditor.ActiveTextAreaControl.Caret.Position = _previousViewPosition.CaretPosition;
+                    if (!_previousViewPosition.CaretVisible)
+                    {
+                        FirstVisibleLine = _previousViewPosition.FirstVisibleLine;
+                    }
+                }
+                else if (isDiff && GetLineText(0) == _previousViewPosition.FirstLine && _previousViewPosition.ActiveLineNum != null)
+                {
+                    // prefer the LeftLineNum because the base revision will not change
+                    int line = _previousViewPosition.ActiveLineNum.LeftLineNum != DiffLineNum.NotApplicableLineNum
+                               ? GetCaretLine(_previousViewPosition.ActiveLineNum.LeftLineNum, rightFile: false)
+                               : GetCaretLine(_previousViewPosition.ActiveLineNum.RightLineNum, rightFile: true);
+                    if (_previousViewPosition.CaretVisible)
+                    {
+                        TextEditor.ActiveTextAreaControl.Caret.Position = new TextLocation(_previousViewPosition.CaretPosition.Column, line);
+                        TextEditor.ActiveTextAreaControl.CenterViewOn(line, treshold: 5);
+                    }
+                    else
+                    {
+                        FirstVisibleLine = line;
+                    }
+                }
+            }
         }
 
         public void SetHighlighting(string syntax)
@@ -266,13 +346,35 @@ namespace GitUI.Editor
 
         public void GoToLine(int lineNumber)
         {
-            TextEditor.ActiveTextAreaControl.Caret.Position = new TextLocation(0, lineNumber);
+            TextEditor.ActiveTextAreaControl.Caret.Position = new TextLocation(0, GetCaretLine(lineNumber, rightFile: true));
         }
 
-        public bool IsGotoLineUIApplicable()
+        private int GetCaretLine(int lineNumber, bool rightFile)
         {
-            return _isGotoLineUIApplicable;
+            if (TextEditor.ShowLineNumbers)
+            {
+                return lineNumber - 1;
+            }
+            else
+            {
+                for (int line = 0; line < TotalNumberOfLines; ++line)
+                {
+                    DiffLineNum diffLineNum = _lineNumbersControl.GetLineNum(line);
+                    if (diffLineNum != null)
+                    {
+                        int diffLine = rightFile ? diffLineNum.RightLineNum : diffLineNum.LeftLineNum;
+                        if (diffLine != DiffLineNum.NotApplicableLineNum && diffLine >= lineNumber)
+                        {
+                            return line;
+                        }
+                    }
+                }
+            }
+
+            return 0;
         }
+
+        public int MaxLineNumber => TextEditor.ShowLineNumbers ? TotalNumberOfLines : _lineNumbersControl.MaxValueOfLineNum;
 
         public int LineAtCaret => TextEditor.ActiveTextAreaControl.Caret.Position.Line;
 

GitUI/Editor/IFileViewer.cs
@@ -60,14 +60,12 @@ namespace GitUI.Editor
         string GetLineText(int line);
         int TotalNumberOfLines { get; }
 
-        // lineNumber is 0 based
-        void GoToLine(int lineNumber);
-
         /// <summary>
-        /// Indicates if the Goto line UI is applicable or not.
-        /// Code-behind goto line function is always availabe, so we can goto next diff section.
+        /// positions to the given line number
         /// </summary>
-        bool IsGotoLineUIApplicable();
+        /// <param name="lineNumber">1..MaxLineNumber</param>
+        void GoToLine(int lineNumber);
+        int MaxLineNumber { get; }
 
         Font Font { get; set; }
 

commit 1818ec0c05c34c08f45013b777bc7265f610b51a

fix stage/unstage/reset command only working once in commit window
the bug was introduced in 71919e5dd5f9d1c87af5ee31ec3d3a3d0e2a8cbb

GitUI/CommandsDialogs/FormCommit.Designer.cs
@@ -1062,6 +1062,7 @@ namespace GitUI.CommandsDialogs
             this.SelectedDiff.TabIndex = 0;
             this.SelectedDiff.TabStop = false;
             this.SelectedDiff.ContextMenuOpening += new System.ComponentModel.CancelEventHandler(this.SelectedDiff_ContextMenuOpening);
+            this.SelectedDiff.TextLoaded += new System.EventHandler(this.SelectedDiff_TextLoaded); 
             //
             // tableLayoutPanel1
             //

GitUI/CommandsDialogs/FormCommit.cs
@@ -520,6 +520,11 @@ namespace GitUI.CommandsDialogs
             _resetSelectedLinesToolStripMenuItem.Enabled = _stageSelectedLinesToolStripMenuItem.Enabled;
         }
 
+        private void SelectedDiff_TextLoaded(object sender, EventArgs e)
+        {
+            _selectedDiffReloaded = true;
+        }
+
         #region Hotkey commands
 
         public static readonly string HotkeySettingsName = "Commit";