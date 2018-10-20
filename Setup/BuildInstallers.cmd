08da442b8423d2fff6388089f8c3f2ab34bc4ed2
RussKie <russkie@gmail.com>
2018-10-20 22:37:35 +1100

    Tweak the build script
    * Separate build, test and package phases
    We want to run tests before attempting to package
    
    * Publish msi only for release branches.

--- Setup/BuildInstallers.cmd
+++ Setup/BuildInstallers.cmd
@@ -5,18 +5,6 @@ cd /d "%~p0"
 SET Configuration=%1
 IF "%Configuration%"=="" SET Configuration=Release
 
-for /f "tokens=*" %%i in ('hMSBuild.bat -only-path -notamd64') do set msbuild="%%i"
-set project=..\GitExtensions.sln
-..\.nuget\nuget.exe update -self
-..\.nuget\nuget.exe restore -Verbosity Quiet %project%
-set msbuildparams=/p:Configuration=%Configuration% /t:Rebuild /nologo /v:m
-
-call BuildGitExtNative.cmd %Configuration% Rebuild
-IF ERRORLEVEL 1 EXIT /B 1
-
-%msbuild% %project% /p:Platform="Any CPU" %msbuildparams%
-IF ERRORLEVEL 1 EXIT /B 1
-
 call MakeInstallers.cmd %Configuration% Rebuild
 IF ERRORLEVEL 1 EXIT /B 1
 
