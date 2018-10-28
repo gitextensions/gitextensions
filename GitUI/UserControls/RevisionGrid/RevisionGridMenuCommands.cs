cb719a553d9e328e1a7edddd2701b91053c90f99
Gerhard Olsson <gerhard.nospam@gmail.com>
2018-10-28 23:21:41 +0100

    Browse: Go to common ancestor w multiple selections (#5638)
    * Browse: Go to common ancestor w multiple selections
    
    Previously just the selected commit was compared to HEAD
    (behavior not changed if only one selected commit)
    
    The command crashed when an artificial commit was selected.
    Artificial commits are now replaced with HEAD, which is normally what you want
    (rather than a popup informing that this is illegal)

--- GitUI/UserControls/RevisionGrid/RevisionGridMenuCommands.cs
+++ GitUI/UserControls/RevisionGrid/RevisionGridMenuCommands.cs
@@ -111,7 +111,7 @@ namespace GitUI.UserControls.RevisionGrid
                 {
                     Name = "GotoMergeBaseCommit",
                     Text = "Go to common ancestor (merge base)",
-                    ToolTipText = "Go to the common ancestor (merge base) commit, which is the best common commit between the currently checked out commit (HEAD) and the currently selected commit",
+                    ToolTipText = "Selects the common ancestor commit (merge base), which is the most recent shared ancestor of the selected commits (or if only one commit is selected, between it and the checked out commit (HEAD))",
                     ShortcutKeyDisplayString = GetShortcutKeyDisplayStringFromRevisionGridIfAvailable(RevisionGridControl.Commands.GoToMergeBaseCommit),
                     ExecuteAction = () => _revisionGrid.ExecuteCommand(RevisionGridControl.Commands.GoToMergeBaseCommit)
                 },
