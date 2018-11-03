gitextensions/gitextensions

commit cd68a511068cd2c0095793d27b2f4c50efb955a6

Performance improvements for object tree rendering

GitUI/BranchTreePanel/RepoObjectsTree.Nodes.Branches.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GitCommands;
 using GitUI.Properties;
 using JetBrains.Annotations;
 using Microsoft.VisualStudio.Threading;
@@ -110,7 +111,7 @@ namespace GitUI.BranchTreePanel
                 TreeViewNode.ImageKey = TreeViewNode.SelectedImageKey = nameof(Images.BranchDocument);
                 if (IsActive)
                 {
-                    TreeViewNode.NodeFont = new Font(TreeViewNode.NodeFont, FontStyle.Bold);
+                    TreeViewNode.NodeFont = new Font(AppSettings.Font, FontStyle.Bold);
                 }
             }
 

GitUI/BranchTreePanel/RepoObjectsTree.Nodes.cs
@@ -220,7 +220,6 @@ namespace GitUI.BranchTreePanel
 
             protected virtual void ApplyStyle()
             {
-                TreeViewNode.NodeFont = AppSettings.Font;
             }
 
             internal virtual void OnSelected()

GitUI/BranchTreePanel/RepoObjectsTree.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GitCommands;
 using GitExtUtils.GitUI;
 using GitUI.CommandsDialogs;
 using GitUI.Properties;
@@ -159,6 +160,7 @@ namespace GitUI.BranchTreePanel
 
             try
             {
+                treeMain.BeginUpdate();
                 var tasks = _rootNodes.Select(r => r.ReloadAsync(token)).ToArray();
                 await Task.WhenAll(tasks).ConfigureAwait(false);
             }
@@ -176,6 +178,8 @@ namespace GitUI.BranchTreePanel
                     _rootNodes.ForEach(t => t.IgnoreSelectionChangedEvent = false);
                     treeMain.SelectedNode.EnsureVisible();
                 }
+
+                treeMain.EndUpdate();
             }
         }
 
@@ -232,6 +236,7 @@ namespace GitUI.BranchTreePanel
             tree.TreeViewNode.SelectedImageKey = tree.TreeViewNode.ImageKey;
             tree.TreeViewNode.Tag = tree;
             treeMain.Nodes.Add(tree.TreeViewNode);
+            treeMain.Font = AppSettings.Font;
             _rootNodes.Add(tree);
         }
 

commit f39bfa63f03dcfbe6f2dc376282167b61fd6103d

Fix RemoteObjectTree branch folder nodes sometimes becoming bold
This bug was introduced in cd68a511068cd2c0095793d27b2f4c50efb955a6 where setting TreeViewNode.NodeFont = AppSettings.Font was removed as an optimization. Problem is that for performance reasons, nodes are recycled, and it's possible for a node that was a LocalBranchNode for the active branch (thus, with bold font) to become a BranchPathNode. This would leave the BranchPathNode with bold font instead of the regular font.

This change brings back the former change, except this time we only update the node's font if required.

GitUI/BranchTreePanel/RepoObjectsTree.Nodes.Branches.cs
@@ -109,10 +109,7 @@ namespace GitUI.BranchTreePanel
             {
                 base.ApplyStyle();
                 TreeViewNode.ImageKey = TreeViewNode.SelectedImageKey = nameof(Images.BranchDocument);
-                if (IsActive)
-                {
-                    TreeViewNode.NodeFont = new Font(AppSettings.Font, FontStyle.Bold);
-                }
+                SetNodeFont(IsActive ? FontStyle.Bold : FontStyle.Regular);
             }
 
             public override bool Equals(object obj)

GitUI/BranchTreePanel/RepoObjectsTree.Nodes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -218,8 +219,37 @@ namespace GitUI.BranchTreePanel
                 return ToString();
             }
 
+            protected void SetNodeFont(FontStyle style)
+            {
+                if (style == FontStyle.Regular)
+                {
+                    // For regular, set to null to use the NativeTreeView font
+                    if (TreeViewNode.NodeFont != null)
+                    {
+                        TreeViewNode.NodeFont.Dispose();
+                        TreeViewNode.NodeFont = null;
+                    }
+                }
+                else
+                {
+                    // If current font doesn't have the input style, get rid of it
+                    if (TreeViewNode.NodeFont != null && !TreeViewNode.NodeFont.Style.HasFlag(style))
+                    {
+                        TreeViewNode.NodeFont.Dispose();
+                        TreeViewNode.NodeFont = null;
+                    }
+
+                    // If non-null, our font is already valid, otherwise create a new one
+                    if (TreeViewNode.NodeFont == null)
+                    {
+                        TreeViewNode.NodeFont = new Font(AppSettings.Font, style);
+                    }
+                }
+            }
+
             protected virtual void ApplyStyle()
             {
+                SetNodeFont(FontStyle.Regular);
             }
 
             internal virtual void OnSelected()