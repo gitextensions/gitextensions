a104bd2714eab5f95b8d1aa743c7548eb9be91ca
Marek Fišera <mara@neptuo.com>
2018-11-14 19:39:39 +0100

    Integration of PluginManager into release process.

--- Setup/Prepare-Release.ps1
+++ Setup/Prepare-Release.ps1
@@ -124,6 +124,54 @@ function Update-Contributors {
     }
 }
 
+function Download-PluginManager([string] $ExtractRootPath)
+{
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $Releases = Invoke-RestMethod -Uri 'https://api.github.com/repos/maraf/GitExtensions.PluginManager/releases'
+
+    $Assets = $Releases[0].assets;
+    foreach ($Asset in $Assets)
+    {
+        if ($Asset.name.StartsWith('GitExtensions.PluginManager'))
+        {
+            $AssetToDownload = $Asset;
+            break;
+        }
+    }
+
+    if (!($AssetToDownload -eq $null))
+    {
+        $AssetUrl = $AssetToDownload.browser_download_url;
+        $Version = $AssetToDownload.tag_name;
+    
+        $DownloadName = [System.IO.Path]::GetFileName($AssetToDownload.name);
+        $DownloadFilePath = [System.IO.Path]::Combine($ExtractRootPath, $DownloadName);
+        $ExtractPath = [System.IO.Path]::Combine($ExtractRootPath, 'Output');
+
+        if (!(Test-Path $DownloadFilePath))
+        {
+            if (!(Test-Path $ExtractRootPath))
+            {
+                New-Item -ItemType directory -Path $ExtractRootPath | Out-Null;
+            }
+
+            if (!(Test-Path $ExtractPath))
+            {
+                New-Item -ItemType directory -Path $ExtractPath | Out-Null;
+            }
+
+            Write-Host ('Downloading "' + $DownloadName + '".');
+
+            Invoke-WebRequest -Uri $AssetUrl -OutFile $DownloadFilePath;
+            Expand-Archive $DownloadFilePath -DestinationPath $ExtractPath -Force
+        }
+        else 
+        {
+            Write-Host ('Download "' + $DownloadName + '" already exists.');
+        }
+    }
+}
+
 
 pushd $PSScriptRoot
 
@@ -147,6 +195,11 @@ try {
     Write-Host Generate the changelog
     Write-Host ----------------------------------------------------------------------
     Generate-Changelog -milestones $milestoneNumbers
+
+    Write-Host ----------------------------------------------------------------------
+    Write-Host Download PluginManager
+    Write-Host ----------------------------------------------------------------------
+    Download-PluginManager -ExtractRootPath '..\Plugins\GitExtensions.PluginManager'
 
     Write-Host ----------------------------------------------------------------------
     Write-Host Compile and package
