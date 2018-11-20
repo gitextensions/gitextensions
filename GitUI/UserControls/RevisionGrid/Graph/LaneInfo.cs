83e8293ca6827052af12d385c3ef180e1bbe3b76
Henk Westhuis <henk.westhuis@ultimo.com>
2018-11-20 20:43:24 +0100

    Add LaneInfo class
    This class can be used to:
    - Make sure a branch keeps its color through the entire history
    - Provide much better tooltips
    - Order lanes in the graphrendering

---
+++ GitUI/UserControls/RevisionGrid/Graph/LaneInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace GitUI.UserControls.RevisionGrid.Graph
+{
+    public class LaneInfo
+    {
+        public LaneInfo(RevisionGraphSegment startSegment)
+        {
+            StartRevision = startSegment.Parent;
+            Color = StartRevision.Objectid.GetHashCode();
+        }
+
+        public int Color { get; private set; }
+
+        public RevisionGraphRevision StartRevision { get; private set; }
+
+        public int StartScore => StartRevision.Score;
+    }
+}
