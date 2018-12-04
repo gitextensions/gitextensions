9bc4321707b36432b644ade89a2a3001deeaf1f4
RussKie <russkie@gmail.com>
2018-12-04 23:23:11 +1100

    Fix external projects assembly info

--- NetSpell.SpellChecker/Properties/AssemblyInfo.cs
+++ NetSpell.SpellChecker/Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) 2003, Paul Welter
 // All rights reserved.
 
+using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 //
 // General Information about an assembly is controlled through the following
@@ -13,3 +15,26 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LoreSoft")]
 [assembly: AssemblyCopyright("Copyright © 2003 Paul Welter, modified by Henk Westhuis for GitExtensions.")]
+
+[assembly: AssemblyProduct("GitExtensions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.99.90")]
+[assembly: AssemblyFileVersion("2.99.90")]
+[assembly: AssemblyInformationalVersion("3.00.rc2")]
+
+// Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
+[assembly: CLSCompliant(isCompliant: false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
