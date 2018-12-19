48130ce394c5b02fdfead953b42b4d298e487879
Dzmitry Mikhalap <dzmitry_mikhalap@epam.com>
2018-12-20 01:20:40 +0300

    Fix scrolling index in PatchGrid for one skipped patch
    (cherry picked from commit 9ddc4a54c7b59124eb285eca3d791b1f453900f0)

--- GitUI/UserControls/PatchGrid.cs
+++ GitUI/UserControls/PatchGrid.cs
@@ -43,7 +43,8 @@ namespace GitUI
             if (patchFiles.Any())
             {
                 int rowsInView = Patches.DisplayedRowCount(false);
-                Patches.FirstDisplayedScrollingRowIndex = Math.Max(0, patchFiles.TakeWhile(pf => !pf.IsNext).Count() - (rowsInView / 2));
+                int currentPatchFileIndex = patchFiles.TakeWhile(pf => !pf.IsNext).Count() - 1;
+                Patches.FirstDisplayedScrollingRowIndex = Math.Max(0, currentPatchFileIndex - (rowsInView / 2));
             }
         }
 
