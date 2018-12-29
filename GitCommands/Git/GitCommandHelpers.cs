7c16c1bd19e8d8ba41e390ebcde08b1d150499ab
RussKie <russkie@gmail.com>
2018-12-29 13:48:49 +1100

    Fix enums
    As per the "enum design guidelines" (https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/enum)
    add default 0-based values.
    
    (cherry picked from commit 15a6cf99c242b6e03a4ee1564eba9b83ccb95713)

--- GitCommands/Git/GitCommandHelpers.cs
+++ GitCommands/Git/GitCommandHelpers.cs
@@ -18,40 +18,40 @@ namespace GitCommands
     public enum UntrackedFilesMode
     {
         /// <summary>Default is <see cref="All"/>; when <see cref="UntrackedFilesMode"/> is NOT used, 'git status' uses <see cref="Normal"/>.</summary>
-        Default = 1,
+        Default = 0,
 
         /// <summary>Show no untracked files.</summary>
-        No = 2,
+        No,
 
         /// <summary>Shows untracked files and directories.</summary>
-        Normal = 3,
+        Normal,
 
         /// <summary>Shows untracked files and directories, and individual files in untracked directories.</summary>
-        All = 4
+        All
     }
 
     /// <summary>Specifies whether to ignore changes to submodules when looking for changes (e.g. via 'git status').</summary>
     public enum IgnoreSubmodulesMode
     {
         /// <summary>Default is <see cref="All"/> (hides all changes to submodules).</summary>
-        Default = 1,
+        Default = 0,
 
         /// <summary>Consider a submodule modified when it either:
         ///  contains untracked or modified files,
         ///  or its HEAD differs from the commit recorded in the superproject.</summary>
-        None = 2,
+        None,
 
         /// <summary>Submodules NOT considered dirty when they only contain <i>untracked</i> content
         ///  (but they are still scanned for modified content).</summary>
-        Untracked = 3,
+        Untracked,
 
         /// <summary>Ignores all changes to the work tree of submodules,
         ///  only changes to the <i>commits</i> stored in the superproject are shown.</summary>
-        Dirty = 4,
+        Dirty,
 
         /// <summary>Hides all changes to submodules
         ///  (and suppresses the output of submodule summaries when the config option status.submodulesummary is set).</summary>
-        All = 5
+        All
     }
 
     /// <summary>Mode for 'git clean'</summary>
