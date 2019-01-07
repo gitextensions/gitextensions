4af22a0988f60c5fa1e3cd42071afdcf37842670
NikolayHD <hidalgo1984@list.ru>
2019-01-07 18:27:39 +0300

    #6029 prevent flickering on Ctrl+Backspace

--- GitExtUtils/GitUI/ControlHotkeyExtensions.cs
+++ GitExtUtils/GitUI/ControlHotkeyExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using GitExtUtils;
@@ -41,8 +42,14 @@ namespace GitUI
                 e.Handled = true;
                 e.SuppressKeyPress = true;
 
+                // Call begin / end update to prevent flickering transient state,
+                // after the word is selected and before it is erased.
+                _beginUpdateMethod.Invoke(sender, parameters: null);
+
                 // emulate keyboard sequence: Ctrl + Shift + Left, Backspace
                 SendKeys.Send("^+{LEFT} {BACKSPACE}");
+
+                _endUpdateMethod.Invoke(sender, parameters: null);
             }
         }
 
@@ -51,5 +58,17 @@ namespace GitUI
             ((Control)sender).Disposed -= HandleDisposed;
             ((Control)sender).KeyDown -= HandleKeyDown;
         }
+
+        private static readonly MethodInfo _beginUpdateMethod =
+            typeof(Control).GetMethod("BeginUpdateInternal",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+
+        // there are 2 EndUpdateInternal methods, we are looking the one with parameterless signature
+        private static readonly MethodInfo _endUpdateMethod =
+            typeof(Control).GetMethod("EndUpdateInternal",
+                BindingFlags.Instance | BindingFlags.NonPublic,
+                binder: null,
+                types: new Type[0],
+                modifiers: new ParameterModifier[0]);
     }
 }
