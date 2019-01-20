96c559bfae2c535cf0ccaa326f483c0d63516501
Gerhard Olsson <gerhardol@users.noreply.github.com>
2019-01-20 23:26:54 +0100

    Cleanup for updating versions to 3.1.0

--- CommonAssemblyInfoExternals.cs
+++ CommonAssemblyInfoExternals.cs
@@ -13,9 +13,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.01.00.0")]
-[assembly: AssemblyFileVersion("3.01.00.0")]
-[assembly: AssemblyInformationalVersion("3.01.00.0")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
 
 // Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
 [assembly: CLSCompliant(isCompliant: false)]
