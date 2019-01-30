3e92682188303697495c5f70f81219c84193e7fa
Antonio Maiorano <amaiorano@gmail.com>
2019-01-29 21:59:59 -0500

    SubmoduleStatusProvider always sets TopProject
    Prior to this change, SubmoduleStatusProvider would only set TopProject if current module's SuperProject was not the TopProject. In other words, when the current module was at least 2 submodules deep from the top module. With this change, we always set the TopProject to the top module. This means that SuperProject == TopProject when current module is 1 submodule deep from the top module. This change makes it easier to retrieve the top project when needed at no extra runtime cost.

--- GitCommands/Submodules/SubmoduleInfoResult.cs
+++ GitCommands/Submodules/SubmoduleInfoResult.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace GitCommands.Submodules
 {
@@ -9,8 +10,16 @@ namespace GitCommands.Submodules
     {
         public IList<SubmoduleInfo> OurSubmodules { get; } = new List<SubmoduleInfo>();
         public IList<SubmoduleInfo> SuperSubmodules { get; } = new List<SubmoduleInfo>();
+
+        // Always set to the top-most module.
+        [NotNull]
         public SubmoduleInfo TopProject { get; set; }
+
+        // Set to the current module's parent, or null if current module is the top one.
+        // If current module's parent is also the top-most module, then SuperProject == TopProject.
+        [CanBeNull]
         public SubmoduleInfo SuperProject { get; set; }
+
         public string CurrentSubmoduleName { get; set; }
     }
 }
