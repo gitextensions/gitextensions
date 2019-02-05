7a2dfce364ba2819be4a776f253b6fdcc6ad2449
Antonio Maiorano <amaiorano@gmail.com>
2019-02-05 08:57:11 -0500

    Add tests for SubmoduleStatusProvider

--- GitCommands/Submodules/SubmoduleInfoResult.cs
+++ GitCommands/Submodules/SubmoduleInfoResult.cs
@@ -8,18 +8,24 @@ namespace GitCommands.Submodules
     /// </summary>
     public class SubmoduleInfoResult
     {
+        // List of SubmoduleInfo for all submodules (recursively) under current module.
         public IList<SubmoduleInfo> OurSubmodules { get; } = new List<SubmoduleInfo>();
+
+        // List of SubmoduleInfo for all submodules under TopProject.
+        // Only populated if current module is a submodule (i.e. is not TopProject)
         public IList<SubmoduleInfo> SuperSubmodules { get; } = new List<SubmoduleInfo>();
 
         // Always set to the top-most module.
         [NotNull]
-        public SubmoduleInfo TopProject { get; set; }
+        public SubmoduleInfo TopProject { get; internal set; }
 
         // Set to the current module's parent, or null if current module is the top one.
         // If current module's parent is also the top-most module, then SuperProject == TopProject.
         [CanBeNull]
-        public SubmoduleInfo SuperProject { get; set; }
+        public SubmoduleInfo SuperProject { get; internal set; }
 
-        public string CurrentSubmoduleName { get; set; }
+        // Name of current module, if it is a submodule. If current module is TopProject, will be null.
+        [CanBeNull]
+        public string CurrentSubmoduleName { get; internal set; }
     }
 }
