3faa346ba1cd1a791758abf410de8b44468bc0d4
crypto_rsa <pkotrc@gmail.com>
2019-02-06 16:06:47 +0100

    Add struct CommandStatus

---
+++ GitCommands/CommandStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace GitCommands
+{
+    public struct CommandStatus
+    {
+        public CommandStatus(bool executed, bool needsGridRefresh)
+        {
+            Executed = executed;
+            NeedsGridRefresh = needsGridRefresh;
+        }
+
+        public static implicit operator CommandStatus(bool executed) => new CommandStatus(executed, false);
+
+        public bool Executed { get; }
+
+        public bool NeedsGridRefresh { get; }
+    }
+}
