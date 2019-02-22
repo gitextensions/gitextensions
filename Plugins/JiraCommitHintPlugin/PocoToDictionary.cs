8dfeb9d6bd40b40c241a03351806616ff2e32794
Henk Westhuis <henk.westhuis@ultimo.com>
2019-02-22 09:16:05 +0100

    #6294: Allow usage of custom fields in jira commit hint plugin

---
+++ Plugins/JiraCommitHintPlugin/PocoToDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace JiraCommitHintPlugin
+{
+    public static class PocoToDictionary
+    {
+        private static readonly MethodInfo AddToDicitonaryMethod = typeof(IDictionary<string, object>).GetMethod("Add");
+        private static readonly Dictionary<Type, Func<object, IDictionary<string, object>>> Converters = new Dictionary<Type, Func<object, IDictionary<string, object>>>();
+        private static readonly ConstructorInfo DictionaryConstructor = typeof(Dictionary<string, object>).GetConstructors().FirstOrDefault(c => c.IsPublic && !c.GetParameters().Any());
+
+        public static IDictionary<string, object> ToDictionary(object obj)
+        {
+            var dictionary = new Dictionary<string, object>();
+            var inputType = obj.GetType();
+            foreach (var property in inputType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy))
+            {
+                if (!property.CanRead || property.GetIndexParameters().Any())
+                {
+                    continue;
+                }
+
+                object value = property.GetValue(obj);
+
+                dictionary[property.Name] = value;
+            }
+
+            return dictionary;
+        }
+    }
+}
