c58e847a04f9fd300a95461be80da3183eea5047
Ivan Grek <kvant@outlook.com>
2019-03-08 14:09:24 +0200

    Add SimpleDialog

---
+++ GitUI/Browsing/Dialogs/ISimpleDialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace GitUI.Browsing.Dialogs
+{
+    internal interface ISimpleDialog
+    {
+        DialogResult ShowOkDialog(string text, string caption, MessageBoxIcon icon);
+
+        DialogResult ShowYesNoDialog(string text, string caption, MessageBoxIcon icon);
+
+        DialogResult ShowFilePromptDialog();
+    }
+}
---
+++ GitUI/Browsing/Dialogs/SimpleDialog.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+using GitUI.Script;
+
+namespace GitUI.Browsing.Dialogs
+{
+    internal sealed class SimpleDialog : ISimpleDialog
+    {
+        private readonly IWin32Window _owner;
+
+        public SimpleDialog(IWin32Window owner)
+        {
+            _owner = owner;
+        }
+
+        public DialogResult ShowOkDialog(string text, string caption, MessageBoxIcon icon)
+        {
+            return MessageBox.Show(_owner, text, caption, MessageBoxButtons.OK, icon);
+        }
+
+        public DialogResult ShowYesNoDialog(string text, string caption, MessageBoxIcon icon)
+        {
+            return MessageBox.Show(_owner, text, caption, MessageBoxButtons.YesNo, icon);
+        }
+
+        public DialogResult ShowFilePromptDialog()
+        {
+            using (var filePromptForm = new FormFilePrompt())
+            {
+                return filePromptForm.ShowDialog(_owner);
+            }
+        }
+    }
+}
