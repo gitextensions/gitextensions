8978608cd99c705cdcafd94bbc9c109660643895
Ivan Grek <kvant@outlook.com>
2019-03-08 14:09:52 +0200

    Add Window container

---
+++ GitUI/Browsing/Dialogs/IWindowContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace GitUI.Browsing.Dialogs
+{
+    /// <summary>
+    /// Window container. At this point, refactoring will be needed later.
+    /// </summary>
+    internal interface IWindowContainer
+    {
+        IWin32Window Window { get; }
+    }
+}
---
+++ GitUI/Browsing/Dialogs/WindowContainer.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace GitUI.Browsing.Dialogs
+{
+    internal sealed class WindowContainer : IWindowContainer
+    {
+        public WindowContainer(IWin32Window window)
+        {
+            Window = window;
+        }
+
+        public IWin32Window Window { get; }
+    }
+}
