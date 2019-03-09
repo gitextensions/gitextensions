67015323be1f44204c294bad272bb1e0b8bf25eb
Ivan Grek <kvant@outlook.com>
2019-03-09 15:24:23 +0200

    Add simple test for ScriptRunner

---
+++ UnitTests/GitUITests/Script/ScriptRunnerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using GitUI.Browsing;
+using GitUI.Browsing.Dialogs;
+using GitUI.Script;
+using GitUIPluginInterfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace GitUITests.Script
+{
+    [TestFixture]
+    internal sealed class ScriptRunnerTests
+    {
+        private IScriptRunner _scriptRunner;
+
+        private readonly Mock<IGitModule> _moduleMock = new Mock<IGitModule>();
+        private readonly Mock<IScriptOptionsParser> _scriptOptionsParserMock = new Mock<IScriptOptionsParser>();
+        private readonly Mock<ISimpleDialog> _simpleDialogMock = new Mock<ISimpleDialog>();
+        private readonly Mock<IScriptManager> _scriptManagerMock = new Mock<IScriptManager>();
+        private readonly Mock<ICanGoToRef> _canGoToRef = new Mock<ICanGoToRef>();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _scriptRunner = new ScriptRunner(
+                _moduleMock.Object,
+                new GitUIEventArgs(new Mock<IWin32Window>().Object, new Mock<IGitUICommands>().Object),
+                _scriptOptionsParserMock.Object,
+                _simpleDialogMock.Object,
+                _scriptManagerMock.Object,
+                _canGoToRef.Object);
+        }
+
+        [Test]
+        public void Build_tool_should_work()
+        {
+            // Arrange
+            var scriptKey = Guid.NewGuid().ToString();
+
+            // Act
+            var result = _scriptRunner.RunScript(scriptKey);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
