d0f987b0e2627a9e760cc2da973ab08203e335c1
Michael Seibt <mstv@gmx.net>
2019-04-11 23:18:22 +0200

    Highlight the new name in the FileStatusList

---
+++ UnitTests/GitUITests/CommandsDialogs/PathFormatterTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using GitUI;
+using NUnit.Framework;
+
+namespace GitUITests.CommandsDialogs
+{
+    public class PathFormatterTests
+    {
+        [TestCase("new.ext", null, "new.ext", null)]
+        [TestCase("new.ext", "", "new.ext", null)]
+        [TestCase("path/new.ext", null, "new.ext", null)]
+        [TestCase("/path/new.ext", null, "new.ext", null)]
+        [TestCase("C:/path/new.ext", null, "new.ext", null)]
+        [TestCase("path\\new.ext", null, "new.ext", null)]
+        [TestCase("C:\\path\\new.ext", null, "new.ext", null)]
+        [TestCase("path/new.ext", "old.ext", "new.ext", " (old.ext)")]
+        [TestCase("path/new.ext", "oldPath/old.ext", "new.ext", " (old.ext)")]
+        [TestCase("path/new.ext", "/oldPath/old.ext", "new.ext", " (old.ext)")]
+        [TestCase("path/new.ext", "C:/oldPath/old.ext", "new.ext", " (old.ext)")]
+        [TestCase("path/new.ext", "C:\\oldPath\\old.ext", "new.ext", " (old.ext)")]
+        [TestCase("path/new.ext", "oldPath\\old.ext", "new.ext", " (old.ext)")]
+        public void Test_FormatTextForFileNameOnly(string name, string oldName, string expectedText, string expectedSuffix)
+        {
+            PathFormatter.FormatTextForFileNameOnly(name, oldName).Should().Be((expectedText, expectedSuffix));
+        }
+
+        [TestCase(null, null)]
+        [TestCase("", null)]
+        [TestCase("filename.ext", " (filename.ext)")]
+        [TestCase("/filename.ext", " (/filename.ext)")]
+        [TestCase("path/filename.ext", " (path/filename.ext)")]
+        [TestCase("nested/path/filename.ext", " (nested/path/filename.ext)")]
+        [TestCase("/nested/path/filename.ext", " (/nested/path/filename.ext)")]
+        [TestCase("path\\filename.ext", " (path\\filename.ext)")]
+        public void Test_FormatOldName(string oldName, string expectedSuffix)
+        {
+            PathFormatter.TestAccessor.FormatOldName(oldName).Should().Be(expectedSuffix);
+        }
+
+        [TestCase(null, null, null)]
+        [TestCase("", null, "")]
+        [TestCase(" ", null, " ")]
+        [TestCase("filename.ext", null, "filename.ext")]
+        [TestCase("/filename.ext", "/", "filename.ext")]
+        [TestCase("path/filename.ext", "path/", "filename.ext")]
+        [TestCase("nested/path/filename.ext", "nested/path/", "filename.ext")]
+        [TestCase("/nested/path/filename.ext", "/nested/path/", "filename.ext")]
+        [TestCase("path\\filename.ext", null, "path\\filename.ext")]
+        public void Test_SplitPathName(string name, string expectedPath, string expectedFileName)
+        {
+            PathFormatter.TestAccessor.SplitPathName(name).Should().Be((expectedPath, expectedFileName));
+        }
+    }
+}
