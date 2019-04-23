cc5fe97fce4150c6165b57f8b84d5989a2f31c64
Ivan Grek <kvant@outlook.com>
2019-04-23 21:30:15 +0300

    Move declaration IUserScriptMenuBuilder to UserScriptMenuBuilder.cs

--- GitUI/Browsing/UserScriptMenuBuilder.cs
+++ GitUI/Browsing/UserScriptMenuBuilder.cs
@@ -6,6 +6,21 @@ using GitUI.Script;
 
 namespace GitUI.Browsing
 {
+    public interface IUserScriptMenuBuilder
+    {
+        /// <summary>
+        /// Build and insert 'Run script' tool
+        /// </summary>
+        /// <param name="tool">Top tool</param>
+        void Build(ToolStrip tool);
+
+        /// <summary>
+        /// Build and insert 'Run script' menu
+        /// </summary>
+        /// <param name="contextMenu">Context menu</param>
+        void Build(ContextMenuStrip contextMenu);
+    }
+
     /// <summary>
     /// Build and insert 'Run script' menu
     /// </summary>
