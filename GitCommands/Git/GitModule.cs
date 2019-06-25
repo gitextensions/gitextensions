gitextensions/gitextensions

commit a94a30af90ddcb8bad08220e171af20fd1f31b19

Further use of ObjectId
Make most GitSubmoduleStatus properties readonly.
Add IGitRef.ObjectId.

GitCommands/Git/GitModule.cs
@@ -2464,7 +2464,7 @@ namespace GitCommands
                     IsChanged = false,
                     IsDeleted = false,
                     Name = file.Name,
-                    TreeGuid = file.Guid,
+                    TreeGuid = file.ObjectId,
                     Staged = StagedStatus.None
                 }).ToList();
 
@@ -2550,7 +2550,7 @@ namespace GitCommands
 
                         Patch patch = GetSingleDiff(firstRevision, secondRevision, item.Name, item.OldName, "", SystemEncoding, true);
                         string text = patch != null ? patch.Text : "";
-                        var submoduleStatus = GitCommandHelpers.GetSubmoduleStatus(text, this, item.Name);
+                        var submoduleStatus = GitCommandHelpers.ParseSubmoduleStatus(text, this, item.Name);
                         if (submoduleStatus.Commit != submoduleStatus.OldCommit)
                         {
                             var submodule = submoduleStatus.GetSubmodule(this);
@@ -2941,7 +2941,7 @@ namespace GitCommands
             foreach (Match match in matches)
             {
                 var refName = match.Groups["refname"].Value;
-                var objectId = match.Groups["objectid"].Value;
+                var objectId = ObjectId.Parse(match.Groups["objectid"].Value);
                 var remoteName = GitRefName.GetRemoteName(refName);
                 var head = new GitRef(this, objectId, refName, remoteName);
 
@@ -3362,7 +3362,8 @@ namespace GitCommands
             return RunCacheableGitCmd($"cat-file blob \"{id}\"", encoding);
         }
 
-        public string GetFileBlobHash(string fileName, ObjectId objectId)
+        [CanBeNull]
+        public ObjectId GetFileBlobHash(string fileName, ObjectId objectId)
         {
             if (objectId == ObjectId.UnstagedId)
             {
@@ -3377,7 +3378,7 @@ namespace GitCommands
                 var lines = RunGitCmd($"ls-files -s \"{fileName}\"").Split(' ', '\t');
                 if (lines.Length >= 2)
                 {
-                    return lines[1];
+                    return ObjectId.Parse(lines[1]);
                 }
             }
             else
@@ -3385,11 +3386,11 @@ namespace GitCommands
                 var lines = RunGitCmd($"ls-tree -r {objectId} \"{fileName}\"").Split(' ', '\t');
                 if (lines.Length >= 3)
                 {
-                    return lines[2];
+                    return ObjectId.Parse(lines[2]);
                 }
             }
 
-            return "";
+            return null;
         }
 
         [CanBeNull]

GitUI/Editor/FileViewer.cs
@@ -512,27 +512,29 @@ namespace GitUI.Editor
             else
             {
                 // Retrieve blob, same as GitItemStatus.TreeGuid
-                string blob = Module.GetFileBlobHash(fileName, objectId);
+                var blob = Module.GetFileBlobHash(fileName, objectId);
                 return ViewGitItemAsync(fileName, blob);
             }
         }
 
-        public Task ViewGitItemAsync(string fileName, string guid)
+        public Task ViewGitItemAsync(string fileName, [CanBeNull] ObjectId objectId)
         {
+            var sha = objectId?.ToString();
+
             return ViewItemAsync(
                 fileName,
                 getImage: GetImage,
                 getFileText: GetFileTextIfBlobExists,
-                getSubmoduleText: () => LocalizationHelpers.GetSubmoduleText(Module, fileName.TrimEnd('/'), guid),
-                openWithDifftool: () => Module.OpenWithDifftool(fileName, firstRevision: guid));
+                getSubmoduleText: () => LocalizationHelpers.GetSubmoduleText(Module, fileName.TrimEnd('/'), sha),
+                openWithDifftool: () => Module.OpenWithDifftool(fileName, firstRevision: sha));
 
-            string GetFileTextIfBlobExists() => guid != "" ? Module.GetFileText(guid, Encoding) : "";
+            string GetFileTextIfBlobExists() => sha != null ? Module.GetFileText(sha, Encoding) : "";
 
             Image GetImage()
             {
                 try
                 {
-                    using (var stream = Module.GetFileStream(guid))
+                    using (var stream = Module.GetFileStream(sha))
                     {
                         return CreateImage(fileName, stream);
                     }
@@ -596,7 +598,8 @@ namespace GitUI.Editor
             }
         }
 
-        private static Image CreateImage(string fileName, Stream stream)
+        [NotNull]
+        private static Image CreateImage([NotNull] string fileName, [NotNull] Stream stream)
         {
             if (IsIcon())
             {

GitUI/GitUIExtensions.cs
@@ -29,7 +29,7 @@ namespace GitUI
             [NotNull] Encoding encoding)
         {
             // Files with tree guid should be presented with normal diff
-            var isTracked = file.IsTracked || (file.TreeGuid.IsNotNullOrWhitespace() && secondRevision != null);
+            var isTracked = file.IsTracked || (file.TreeGuid != null && secondRevision != null);
 
             return module.GetSingleDiff(firstRevision?.ToString(), secondRevision?.ToString(), file.Name, file.OldName, diffArgs, encoding, true, isTracked);
         }
@@ -101,7 +101,7 @@ namespace GitUI
             if (firstRevision == null)
             {
                 // The previous commit does not exist, nothing to compare with
-                if (file.TreeGuid.IsNullOrEmpty())
+                if (file.TreeGuid != null)
                 {
                     if (secondRevision == null)
                     {

GitCommands/Git/GitCommandHelpers.cs
@@ -707,7 +707,7 @@ namespace GitCommands
         {
             Patch patch = module.GetCurrentChanges(fileName, oldFileName, staged, "", module.FilesEncoding);
             string text = patch != null ? patch.Text : "";
-            return GetSubmoduleStatus(text, module, fileName);
+            return ParseSubmoduleStatus(text, module, fileName);
         }
 
         [CanBeNull]
@@ -717,14 +717,21 @@ namespace GitCommands
         }
 
         [CanBeNull]
-        public static GitSubmoduleStatus GetSubmoduleStatus(string text, GitModule module, string fileName)
+        public static GitSubmoduleStatus ParseSubmoduleStatus(string text, GitModule module, string fileName)
         {
             if (string.IsNullOrEmpty(text))
             {
                 return null;
             }
 
-            var status = new GitSubmoduleStatus();
+            string name = null;
+            string oldName = null;
+            bool isDirty = false;
+            ObjectId commitId = null;
+            ObjectId oldCommitId = null;
+            int? addedCommits = null;
+            int? removedCommits = null;
+
             using (var reader = new StringReader(text))
             {
                 string line = reader.ReadLine();
@@ -734,16 +741,16 @@ namespace GitCommands
                     var match = Regex.Match(line, @"diff --git [abic]/(.+)\s[abwi]/(.+)");
                     if (match.Groups.Count > 1)
                     {
-                        status.Name = match.Groups[1].Value;
-                        status.OldName = match.Groups[2].Value;
+                        name = match.Groups[1].Value;
+                        oldName = match.Groups[2].Value;
                     }
                     else
                     {
                         match = Regex.Match(line, @"diff --cc (.+)");
                         if (match.Groups.Count > 1)
                         {
-                            status.Name = match.Groups[1].Value;
-                            status.OldName = match.Groups[1].Value;
+                            name = match.Groups[1].Value;
+                            oldName = match.Groups[1].Value;
                         }
                     }
                 }
@@ -761,38 +768,38 @@ namespace GitCommands
                     }
 
                     char c = line[0];
-                    const string commit = "commit ";
+                    const string commitStr = "commit ";
                     string hash = "";
-                    int pos = line.IndexOf(commit);
+                    int pos = line.IndexOf(commitStr);
                     if (pos >= 0)
                     {
-                        hash = line.Substring(pos + commit.Length);
+                        hash = line.Substring(pos + commitStr.Length);
                     }
 
-                    bool isDirty = hash.EndsWith("-dirty");
+                    bool endsWithDirty = hash.EndsWith("-dirty");
                     hash = hash.Replace("-dirty", "");
                     if (c == '-')
                     {
-                        status.OldCommit = hash;
+                        oldCommitId = ObjectId.Parse(hash);
                     }
                     else if (c == '+')
                     {
-                        status.Commit = hash;
-                        status.IsDirty = isDirty;
+                        commitId = ObjectId.Parse(hash);
+                        isDirty = endsWithDirty;
                     }
 
                     // TODO: Support combined merge
                 }
             }
 
-            if (status.OldCommit != null && status.Commit != null)
+            if (oldCommitId != null && commitId != null)
             {
                 var submodule = module.GetSubmodule(fileName);
-                status.AddedCommits = submodule.GetCommitCount(status.Commit, status.OldCommit);
-                status.RemovedCommits = submodule.GetCommitCount(status.OldCommit, status.Commit);
+                addedCommits = submodule.GetCommitCount(commitId.ToString(), oldCommitId.ToString());
+                removedCommits = submodule.GetCommitCount(oldCommitId.ToString(), commitId.ToString());
             }
 
-            return status;
+            return new GitSubmoduleStatus(name, oldName, isDirty, commitId, oldCommitId, addedCommits, removedCommits);
         }
 
         /// <summary>

GitCommands/Git/GitItemStatus.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
+using GitUIPluginInterfaces;
 using JetBrains.Annotations;
 using Microsoft.VisualStudio.Threading;
 
@@ -39,7 +40,8 @@ namespace GitCommands
 
         public string Name { get; set; }
         public string OldName { get; set; }
-        public string TreeGuid { get; set; }
+        [CanBeNull]
+        public ObjectId TreeGuid { get; set; }
         public string RenameCopyPercentage { get; set; }
 
         // Staged is three state and has no default status

GitCommands/Git/GitRef.cs
@@ -7,23 +7,23 @@ using JetBrains.Annotations;
 
 namespace GitCommands
 {
-    public class GitRef : IGitRef
+    public sealed class GitRef : IGitRef
     {
         private readonly string _mergeSettingName;
         private readonly string _remoteSettingName;
 
         public IGitModule Module { get; }
 
-        public GitRef(IGitModule module, string guid, string completeName)
-            : this(module, guid, completeName, string.Empty)
+        public GitRef(IGitModule module, [CanBeNull] ObjectId objectId, string completeName)
+            : this(module, objectId, completeName, string.Empty)
         {
         }
 
-        public GitRef(IGitModule module, string guid, string completeName, string remote)
+        public GitRef(IGitModule module, [CanBeNull] ObjectId objectId, string completeName, string remote)
         {
             Module = module;
-            Guid = guid;
-            IsSelected = false;
+            ObjectId = objectId;
+            Guid = objectId?.ToString();
             CompleteName = completeName;
             Remote = remote;
 
@@ -41,7 +41,7 @@ namespace GitCommands
         }
 
         public string CompleteName { get; }
-        public bool IsSelected { get; set; }
+        public bool IsSelected { get; set; } = false;
         public bool IsSelectedHeadMergeSource { get; set; }
         public bool IsTag { get; }
         public bool IsHead { get; }
@@ -125,15 +125,15 @@ namespace GitCommands
 
         #region IGitItem Members
 
+        [CanBeNull]
+        public ObjectId ObjectId { get; }
+        [CanBeNull]
         public string Guid { get; }
         public string Name { get; }
 
         #endregion
 
-        public override string ToString()
-        {
-            return CompleteName;
-        }
+        public override string ToString() => CompleteName;
 
         [CanBeNull]
         private string ParseName()

GitCommands/Git/GitSubmoduleStatus.cs
@@ -1,15 +1,29 @@
-﻿namespace GitCommands
+﻿using GitUIPluginInterfaces;
+
+namespace GitCommands
 {
     public sealed class GitSubmoduleStatus
     {
-        public string Name { get; set; }
-        public string OldName { get; set; }
-        public bool IsDirty { get; set; }
-        public string Commit { get; set; }
-        public string OldCommit { get; set; }
+        public string Name { get; }
+        public string OldName { get; }
+        public bool IsDirty { get; }
+        public ObjectId Commit { get; }
+        public ObjectId OldCommit { get; }
+        public int? AddedCommits { get; }
+        public int? RemovedCommits { get; }
+
         public SubmoduleStatus Status { get; set; } = SubmoduleStatus.Unknown;
-        public int? AddedCommits { get; set; }
-        public int? RemovedCommits { get; set; }
+
+        public GitSubmoduleStatus(string name, string oldName, bool isDirty, ObjectId commit, ObjectId oldCommit, int? addedCommits, int? removedCommits)
+        {
+            Name = name;
+            OldName = oldName;
+            IsDirty = isDirty;
+            Commit = commit;
+            OldCommit = oldCommit;
+            AddedCommits = addedCommits;
+            RemovedCommits = removedCommits;
+        }
 
         public GitModule GetSubmodule(GitModule module)
         {
@@ -24,7 +38,7 @@
                 return;
             }
 
-            Status = submodule.CheckSubmoduleStatus(Commit, OldCommit);
+            Status = submodule.CheckSubmoduleStatus(Commit.ToString(), OldCommit.ToString());
         }
 
         public string AddedAndRemovedString()

GitCommands/Remote/GitRemoteManager.cs
@@ -112,7 +112,7 @@ namespace GitCommands.Remote
             var module = GetModule();
             bool IsSettingForBranch(string setting, string branchName)
             {
-                var head = new GitRef(module, string.Empty, setting);
+                var head = new GitRef(module, null, setting);
                 return head.IsHead && head.Name.Equals(branchName, StringComparison.OrdinalIgnoreCase);
             }
 
@@ -120,7 +120,7 @@ namespace GitCommands.Remote
                                    .Select(s => s.Split(':'))
                                    .Where(t => t.Length == 2)
                                    .Where(t => IsSettingForBranch(t[0], branch))
-                                   .Select(t => new GitRef(module, string.Empty, t[1]))
+                                   .Select(t => new GitRef(module, null, t[1]))
                                    .FirstOrDefault(h => h.IsHead);
 
             return remoteHead?.Name;

commit 622a2da0fe25dbf895d5a0777ba6afc528fef8ab

Show changed images in all diff tabs
Previously, images were displayed as binary diffs, except some in
FormCommit:  .png and new worktree images

Other:
* FormCommit always show the worktree commit for index .png
* Show all images in FormCommit
(i.e. show index images and changed other than .png)
* Regression from a94a30af90ddcb8bad08220e171af20fd1f31b19 (ObjectId)
where blob guid were ignored and diff not shown
* Combined guid should never be used to get file blob guid
* openWithDiffTool action were not called from stash and RevisionDiff

GitCommands/Git/GitModule.cs
@@ -3347,11 +3347,9 @@ namespace GitCommands
         [CanBeNull]
         public ObjectId GetFileBlobHash(string fileName, ObjectId objectId)
         {
-            if (objectId == ObjectId.WorkTreeId)
+            if (objectId == ObjectId.WorkTreeId || objectId == ObjectId.CombinedDiffId)
             {
-                // working directory changes
-                Debug.Assert(false, "Tried to get blob for worktree file");
-                return null;
+                throw new ArgumentException($"Tried to get blob for unsupported revision: {objectId} and file: {fileName}");
             }
 
             // TODO use regex for parsing

GitUI/CommandsDialogs/FormCommit.cs
@@ -1204,17 +1204,14 @@ namespace GitUI.CommandsDialogs
         private async Task SetSelectedDiffAsync(GitItemStatus item, bool staged)
         {
             Action openWithDiffTool = () => (staged ? stagedOpenDifftoolToolStripMenuItem9 : openWithDifftoolToolStripMenuItem).PerformClick();
-            if (item.Name.EndsWith(".png"))
+            if (!item.IsTracked || FileHelper.IsImage(item.Name))
             {
-                await SelectedDiff.ViewFileAsync(item.Name, openWithDiffTool);
-            }
-            else if (item.IsTracked)
-            {
-                SelectedDiff.ViewCurrentChanges(item, staged, openWithDiffTool);
+                var guid = staged ? ObjectId.IndexId : ObjectId.WorkTreeId;
+                await SelectedDiff.ViewGitItemRevisionAsync(item.Name, guid, openWithDiffTool);
             }
             else
             {
-                await SelectedDiff.ViewFileAsync(item.Name);
+                SelectedDiff.ViewCurrentChanges(item, staged, openWithDiffTool);
             }
         }
 

GitUI/CommandsDialogs/RevisionDiffControl.cs
@@ -652,7 +652,7 @@ namespace GitUI.CommandsDialogs
             var gitItemStatus = DiffFiles.SelectedItem;
             var revisionId = DiffFiles.Revision?.ObjectId;
 
-            if (gitItemStatus?.Name == null || revisionId == null)
+            if (gitItemStatus?.Name == null || revisionId == null || revisionId == ObjectId.CombinedDiffId)
             {
                 return;
             }

GitUI/Editor/FileViewer.cs
@@ -18,7 +18,6 @@ using GitUI.Hotkey;
 using GitUI.Properties;
 using GitUIPluginInterfaces;
 using JetBrains.Annotations;
-using Microsoft.VisualStudio.Threading;
 using ResourceManager;
 
 namespace GitUI.Editor
@@ -675,22 +674,22 @@ namespace GitUI.Editor
             return str.ToString();
         }
 
-        public Task ViewGitItemRevisionAsync(string fileName, ObjectId objectId)
+        public Task ViewGitItemRevisionAsync(string fileName, ObjectId objectId, [CanBeNull] Action openWithDifftool = null)
         {
             if (objectId == ObjectId.WorkTreeId)
             {
                 // No blob exists for worktree, present contents from file system
-                return ViewFileAsync(fileName);
+                return ViewFileAsync(fileName, openWithDifftool);
             }
             else
             {
                 // Retrieve blob, same as GitItemStatus.TreeGuid
                 var blob = Module.GetFileBlobHash(fileName, objectId);
-                return ViewGitItemAsync(fileName, blob);
+                return ViewGitItemAsync(fileName, blob, openWithDifftool);
             }
         }
 
-        public Task ViewGitItemAsync(string fileName, [CanBeNull] ObjectId objectId)
+        public Task ViewGitItemAsync(string fileName, [CanBeNull] ObjectId objectId, [CanBeNull] Action openWithDifftool = null)
         {
             var sha = objectId?.ToString();
 
@@ -699,9 +698,14 @@ namespace GitUI.Editor
                 getImage: GetImage,
                 getFileText: GetFileTextIfBlobExists,
                 getSubmoduleText: () => LocalizationHelpers.GetSubmoduleText(Module, fileName.TrimEnd('/'), sha),
-                openWithDifftool: () => Module.OpenWithDifftool(fileName, firstRevision: sha));
+                openWithDifftool: openWithDifftool ?? OpenWithDifftool);
 
             string GetFileTextIfBlobExists() => objectId != null ? Module.GetFileText(objectId, Encoding) : "";
+
+            void OpenWithDifftool()
+            {
+                Module.OpenWithDifftool(fileName, firstRevision: sha);
+            }
 
             Image GetImage()
             {

GitUI/GitUIExtensions.cs
@@ -98,12 +98,13 @@ namespace GitUI
             [NotNull] string defaultText,
             [CanBeNull] Action openWithDifftool)
         {
-            if (firstRevision == null)
+            if (firstRevision == null || FileHelper.IsImage(file.Name))
             {
                 // The previous commit does not exist, nothing to compare with
-                if (file.TreeGuid == null)
+                if (file.TreeGuid != null)
                 {
-                    return diffViewer.ViewGitItemAsync(file.Name, file.TreeGuid);
+                    // blob guid exists
+                    return diffViewer.ViewGitItemAsync(file.Name, file.TreeGuid, openWithDifftool);
                 }
 
                 if (secondRevision == null)
@@ -111,7 +112,8 @@ namespace GitUI
                     throw new ArgumentNullException(nameof(secondRevision));
                 }
 
-                return diffViewer.ViewGitItemRevisionAsync(file.Name, secondRevision);
+                // Get blob guid from revision
+                return diffViewer.ViewGitItemRevisionAsync(file.Name, secondRevision, openWithDifftool);
             }
 
             return diffViewer.ViewPatchAsync(() =>