7aa5f0c807874fd94db2f07fc6e47cf703583337
Michael Seibt <mstv@gmx.net>
2019-11-14 21:30:03 +0100

    Make UpdateSubmoduleStructureAndWaitForResult exception-safe
    and cleanup UpdateSubmoduleStatusAndWaitForResult

--- UnitTests/CommonTestUtils/SubmoduleTestHelpers.cs
+++ UnitTests/CommonTestUtils/SubmoduleTestHelpers.cs
@@ -9,20 +9,24 @@ namespace CommonTestUtils
         public static SubmoduleInfoResult UpdateSubmoduleStructureAndWaitForResult(ISubmoduleStatusProvider provider, GitModule module, bool updateStatus = false)
         {
             SubmoduleInfoResult result = null;
-            provider.StatusUpdated += Provider_StatusUpdated;
+            provider.StatusUpdated += ProviderStatusUpdated;
+            try
+            {
+                provider.UpdateSubmodulesStructure(
+                    workingDirectory: module.WorkingDir,
+                    noBranchText: string.Empty,
+                    updateStatus: updateStatus);
 
-            provider.UpdateSubmodulesStructure(
-                workingDirectory: module.WorkingDir,
-                noBranchText: string.Empty,
-                updateStatus: updateStatus);
-
-            AsyncTestHelper.WaitForPendingOperations();
-
-            provider.StatusUpdated -= Provider_StatusUpdated;
+                AsyncTestHelper.WaitForPendingOperations();
+            }
+            finally
+            {
+                provider.StatusUpdated -= ProviderStatusUpdated;
+            }
 
             return result;
 
-            void Provider_StatusUpdated(object sender, SubmoduleStatusEventArgs e)
+            void ProviderStatusUpdated(object sender, SubmoduleStatusEventArgs e)
             {
                 result = e.Info;
             }
@@ -30,22 +34,9 @@ namespace CommonTestUtils
 
         public static void UpdateSubmoduleStatusAndWaitForResult(ISubmoduleStatusProvider provider, GitModule module, IReadOnlyList<GitItemStatus> gitStatus)
         {
-            List<DetailedSubmoduleInfo> result = new List<DetailedSubmoduleInfo>();
-            provider.StatusUpdated += Provider_StatusUpdated;
-
-            provider.UpdateSubmodulesStatus(
-                workingDirectory: module.WorkingDir,
-                gitStatus: gitStatus);
+            provider.UpdateSubmodulesStatus(workingDirectory: module.WorkingDir, gitStatus: gitStatus);
 
             AsyncTestHelper.WaitForPendingOperations();
-
-            provider.StatusUpdated -= Provider_StatusUpdated;
-
-            return;
-
-            void Provider_StatusUpdated(object sender, SubmoduleStatusEventArgs e)
-            {
-            }
         }
     }
 }
