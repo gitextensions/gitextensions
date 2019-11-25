3324c617153f805d29d55a84b8458f9eedafc480
Michael Seibt <mstv@gmx.net>
2019-11-25 23:09:59 +0100

    Prepare for integration tests & repeated test runs

--- Build/Run-Tests.ps1
+++ Build/Run-Tests.ps1
@@ -1,20 +1,30 @@
-$testAssemblies = (Get-ChildItem -Path UnitTests -Filter '*Tests.dll' -Recurse -Exclude 'ApprovalTests.dll').FullName | Where-Object { $_.Contains('\bin\Release') }
+$testAssemblies = @();
+$testAssemblies += (Get-ChildItem -Path UnitTests        -Filter '*Tests.dll' -Recurse -Exclude 'ApprovalTests.dll').FullName | Where-Object { $_.Contains('\bin\Release') }
+$testAssemblies += (Get-ChildItem -Path IntegrationTests -Filter '*Tests.dll' -Recurse -Exclude 'ApprovalTests.dll').FullName | Where-Object { $_.Contains('\bin\Release') }
 $packageConfig = [xml](Get-Content .nuget\packages.config)
 $opencover_version = $packageConfig.SelectSingleNode('/packages/package[@id="OpenCover"]').version
 $opencover_console = "packages\OpenCover.$opencover_version\tools\OpenCover.Console.exe"
 
-&$opencover_console `
-    -register:administrator `
-    -returntargetcode `
-    -hideskipped:All `
-    -filter:"+[*]* -[FluentAssertions*]* -[SmartFormat*]* -[nunit*]*" `
-    -excludebyattribute:*.ExcludeFromCodeCoverage* `
-    -excludebyfile:*\*Designer.cs `
-    -output:"OpenCover.GitExtensions.xml" `
-    -target:"nunit3-console.exe" `
-    -targetargs:"$testAssemblies --workers=1 --timeout=90000"
-$testExitCode = $LastExitCode
-Push-AppveyorArtifact "TestResult.xml"
+$testRunCount = 1
+for ($i=1; $i -le $testRunCount; $i++)
+{
+    Write-Host "[INFO]: Test Run ${i}/${testRunCount}"
+    &$opencover_console `
+        -register:administrator `
+        -returntargetcode `
+        -hideskipped:All `
+        -filter:"+[*]* -[FluentAssertions*]* -[SmartFormat*]* -[nunit*]*" `
+        -excludebyattribute:*.ExcludeFromCodeCoverage* `
+        -excludebyfile:*\*Designer.cs `
+        -output:"OpenCover.GitExtensions.xml" `
+        -target:"nunit3-console.exe" `
+        -targetargs:"$testAssemblies --workers=1 --timeout=90000"
+    $testExitCode += $LastExitCode
+    if ($LastExitCode -ne 0) { Write-Host "[ERROR]: Test run ${i} failed!" }
+    $artifact_name = "TestResult" + $i + "." + $LastExitCode + "err.xml"
+    Copy-Item "TestResult.xml" $artifact_name
+    Push-AppveyorArtifact $artifact_name
+}
 if ($testExitCode -ne 0) { $host.SetShouldExit($testExitCode) }
 
 $codecov_version = $packageConfig.SelectSingleNode('/packages/package[@id="Codecov"]').version
