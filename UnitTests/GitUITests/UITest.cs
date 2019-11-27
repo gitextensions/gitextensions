6d05d3542031cb927d7ab44185454b9184cd9c16
Michael Seibt <mstv@gmx.net>
2019-11-28 00:22:04 +0100

    Add output to UITest.RunForm

--- UnitTests/GitUITests/UITest.cs
+++ UnitTests/GitUITests/UITest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace GitUITests
             Func<T, Task> runTestAsync)
             where T : Form
         {
+            Console.WriteLine($"{nameof(RunForm)} entry");
             var idleCompletionSource = new TaskCompletionSource<VoidResult>();
 
             // Start runAsync before calling showForm.
@@ -27,8 +29,11 @@ namespace GitUITests
             // complete.
             var test = ThreadHelper.JoinableTaskContext.Factory.RunAsync(async () =>
             {
+                Console.WriteLine($"{nameof(RunForm)}.test entry");
+
                 // Wait for the form to be opened by the test thread.
                 await idleCompletionSource.Task;
+                Console.WriteLine($"{nameof(RunForm)}.test idle");
                 var form = Application.OpenForms.OfType<T>().Single();
                 if (!form.Visible)
                 {
@@ -41,37 +46,46 @@ namespace GitUITests
                 {
                     // Wait for potential pending asynchronous tasks triggered by the form.
                     AsyncTestHelper.WaitForPendingOperations(AsyncTestHelper.UnexpectedTimeout);
+                    Console.WriteLine($"{nameof(RunForm)}.test form loaded");
 
                     await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                    Console.WriteLine($"{nameof(RunForm)}.test on main thread");
 
                     await runTestAsync(form);
+                    Console.WriteLine($"{nameof(RunForm)}.test passed");
                 }
                 finally
                 {
                     form.Close(); // also calls form.Dispose()
+                    Console.WriteLine($"{nameof(RunForm)}.test form closed");
                 }
             });
 
             // The following call to showForm will trigger messages.
             // So we can be sure we don't stall waiting for idle if the application was already idle.
             Application.Idle += HandleApplicationIdle;
+            Console.WriteLine($"{nameof(RunForm)} subscribed");
             try
             {
                 try
                 {
                     showForm();
+                    Console.WriteLine($"{nameof(RunForm)} form shown");
                 }
-                catch (Exception)
+                catch (Exception outer)
                 {
                     try
                     {
                         // Wake up and join the asynchronous test operation.
+                        Console.WriteLine($"{nameof(RunForm)} exception {outer.Demystify()}");
                         idleCompletionSource.TrySetResult(default);
                         test.Join();
+                        Console.WriteLine($"{nameof(RunForm)} test joined");
                     }
-                    catch (Exception)
+                    catch (Exception inner)
                     {
                         // ignore
+                        Console.WriteLine($"{nameof(RunForm)} exception {inner.Demystify()}");
                     }
 
                     throw;
@@ -79,16 +93,19 @@ namespace GitUITests
 
                 // Join the asynchronous test operation so any exceptions are rethrown on this thread.
                 test.Join();
+                Console.WriteLine($"{nameof(RunForm)} test joined");
             }
             finally
             {
                 Application.Idle -= HandleApplicationIdle;
+                Console.WriteLine($"{nameof(RunForm)} unsubscribed");
             }
 
             return;
 
             void HandleApplicationIdle(object sender, EventArgs e)
             {
+                Console.WriteLine($"{nameof(HandleApplicationIdle)}");
                 idleCompletionSource.TrySetResult(default);
             }
         }
