gitextensions/gitextensions

commit 3fcb4788b42d9b485880eb756e4bdc0af47d321b

BuildServerIntegration: Adjust polling interval for running bui… (#7452)
depending if some builds are running (refresh interval is 10s)
or not (interval is 120s)

... to reduce calls to the CI server when not needed.

GitUI/BuildServerIntegration/BuildServerWatcher.cs
@@ -27,6 +27,8 @@ namespace GitUI.BuildServerIntegration
 {
     public sealed class BuildServerWatcher : IBuildServerWatcher, IDisposable
     {
+        private const int PollIntervalWhenSomeBuildsAreRunning = 10;
+        private const int PollIntervalWhenNoBuildsAreRunning = 120;
         private readonly CancellationTokenSequence _launchCancellation = new CancellationTokenSequence();
         private readonly object _buildServerCredentialsLock = new object();
         private readonly RevisionGridControl _revisionGrid;
@@ -79,6 +81,7 @@ namespace GitUI.BuildServerIntegration
             var fullObservable = buildServerAdapter.GetFinishedBuildsSince(scheduler);
             var fromNowObservable = buildServerAdapter.GetFinishedBuildsSince(scheduler, DateTime.Now);
 
+            var isBuildRunning = false;
             var cancellationToken = new CompositeDisposable
                     {
                         fullDayObservable.OnErrorResumeNext(fullObservable)
@@ -90,8 +93,12 @@ namespace GitUI.BuildServerIntegration
                                          .ObserveOn(MainThreadScheduler.Instance)
                                          .Subscribe(OnBuildInfoUpdate),
 
-                        runningBuildsObservable.OnErrorResumeNext(Observable.Empty<BuildInfo>()
-                                                                            .DelaySubscription(TimeSpan.FromSeconds(10)))
+                        runningBuildsObservable.Do(_ => isBuildRunning = true)
+                                               .Finally(() =>
+                                               {
+                                                   Thread.Sleep(TimeSpan.FromSeconds(isBuildRunning ? PollIntervalWhenSomeBuildsAreRunning : PollIntervalWhenNoBuildsAreRunning));
+                                                   isBuildRunning = false;
+                                               })
                                                .Retry()
                                                .Repeat()
                                                .ObserveOn(MainThreadScheduler.Instance)

commit 86e28bbd80ce4f52965c78817118a6c5b157ce0c

BuildServerIntegration: Fix freeze due to polling interval adju… (#7485)
Fix regression introduced in 3fcb4788b42d9b485880eb756e4bdc0af47d321b

where application is fozen during a long time
after a revision grid refresh (`Thread.Sleep()` was made in the UI thread :( ).

Use Observable.Defer to introduce dynamic delay
depending if a build running was found (short delay) or not (longer delay)

GitUI/BuildServerIntegration/BuildServerWatcher.cs
@@ -27,8 +27,8 @@ namespace GitUI.BuildServerIntegration
 {
     public sealed class BuildServerWatcher : IBuildServerWatcher, IDisposable
     {
-        private const int PollIntervalWhenSomeBuildsAreRunning = 10;
-        private const int PollIntervalWhenNoBuildsAreRunning = 120;
+        private static readonly TimeSpan ShortPollInterval = TimeSpan.FromSeconds(10);
+        private static readonly TimeSpan LongPollInterval = TimeSpan.FromSeconds(120);
         private readonly CancellationTokenSequence _launchCancellation = new CancellationTokenSequence();
         private readonly object _buildServerCredentialsLock = new object();
         private readonly RevisionGridControl _revisionGrid;
@@ -81,7 +81,9 @@ namespace GitUI.BuildServerIntegration
             var fullObservable = buildServerAdapter.GetFinishedBuildsSince(scheduler);
             var fromNowObservable = buildServerAdapter.GetFinishedBuildsSince(scheduler, DateTime.Now);
 
-            var isBuildRunning = false;
+            bool anyBuilds = false;
+            var delayObservable = Observable.Defer(() => Observable.Empty<BuildInfo>()
+                                                                   .DelaySubscription(anyBuilds ? ShortPollInterval : LongPollInterval));
             var cancellationToken = new CompositeDisposable
                     {
                         fullDayObservable.OnErrorResumeNext(fullObservable)
@@ -93,13 +95,10 @@ namespace GitUI.BuildServerIntegration
                                          .ObserveOn(MainThreadScheduler.Instance)
                                          .Subscribe(OnBuildInfoUpdate),
 
-                        runningBuildsObservable.Do(_ => isBuildRunning = true)
-                                               .Finally(() =>
-                                               {
-                                                   Thread.Sleep(TimeSpan.FromSeconds(isBuildRunning ? PollIntervalWhenSomeBuildsAreRunning : PollIntervalWhenNoBuildsAreRunning));
-                                                   isBuildRunning = false;
-                                               })
+                        runningBuildsObservable.Do(buildInfo => anyBuilds = true)
                                                .Retry()
+                                               .Concat(delayObservable)
+                                               .Finally(() => anyBuilds = false)
                                                .Repeat()
                                                .ObserveOn(MainThreadScheduler.Instance)
                                                .Subscribe(OnBuildInfoUpdate)