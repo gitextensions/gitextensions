f228154bec52e8d7146543452057880a37b99490
RussKie <russkie@gmail.com>
2019-12-16 23:11:14 +1100

    Convert projects to SDK-style
    * Convert all primary C# projects (i.e. not externals) to SDK-style
    * Remove all excluded files

--- Setup/Build.cmd
+++ Setup/Build.cmd
@@ -10,10 +10,10 @@ set msbuild32=hMSBuild -notamd64 -no-cache
 set solution=..\GitExtensions.sln
 ..\.nuget\nuget.exe update -self
 ..\.nuget\nuget.exe restore -Verbosity Quiet %solution%
-set msbuildparams=/p:Configuration=%Configuration% /t:Rebuild /nologo /v:m
+set msbuildparams=/p:Configuration=%Configuration% /t:Rebuild /nologo /v:m /bl:C:\Development\gitextensions\build.binlog
 
-call BuildGitExtNative.cmd %Configuration% Rebuild
-IF ERRORLEVEL 1 EXIT /B 1
+REM call BuildGitExtNative.cmd %Configuration% Rebuild
+REM IF ERRORLEVEL 1 EXIT /B 1
 
 call %msbuild32% %solution% /p:Platform="Any CPU" %msbuildparams%
 IF ERRORLEVEL 1 EXIT /B 1
