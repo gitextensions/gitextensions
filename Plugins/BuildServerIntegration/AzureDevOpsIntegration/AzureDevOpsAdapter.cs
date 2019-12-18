7bc0104156090227cc98ad4d2fb62695d4370967
Philippe Miossec <pmiossec@gmail.com>
2019-12-18 11:03:34 +0100

    Azure devops ci: reduce calls to api when revision grid is refreshed  (#7477)
    * refactoring & cleaning
    
    * Ensure query to get build definition is called once (and not twice due to multi-threading)
    
    * Prevent doing every time calls to retrieve build definitions by putting them in cache.
    
    Build definitions don't change often  and it's acceptable to restart GitExtensions when it's the case
    
    Prevent 2 to 4 api calls at each revision grid refresh keeping only the 2 calls to get the build results (finished and running).

--- Plugins/BuildServerIntegration/AzureDevOpsIntegration/AzureDevOpsAdapter.cs
+++ Plugins/BuildServerIntegration/AzureDevOpsIntegration/AzureDevOpsAdapter.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.Composition;
 using System.Globalization;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using AzureDevOpsIntegration.Settings;
@@ -36,6 +35,15 @@ namespace AzureDevOpsIntegration
         private IntegrationSettings _settings;
         private ApiClient _apiClient;
         private static readonly IBuildDurationFormatter _buildDurationFormatter = new BuildDurationFormatter();
+        private Task<string> _buildDefinitionsTask;
+        private string _projectUrl;
+        private string _buildDefinitions;
+
+        // Static variable used to convey the data between the different instances of the class but that doesn't necessarily require synchronisation because:
+        // * there is only one instance at every given time (link to the revision grid and recreated on revision grid refresh)
+        // * data is created by the first instance and used in readonly by the later instances
+        private static CacheAzureDevOps CacheAzureDevOps = null;
+        private string CacheKey => _projectUrl + "|" + _settings.BuildDefinitionFilter;
 
         public void Initialize(IBuildServerWatcher buildServerWatcher, ISettingsSource config, Func<ObjectId, bool> isCommitInRevisionGrid = null)
         {
@@ -52,14 +60,23 @@ namespace AzureDevOpsIntegration
                 return;
             }
 
-            var projectUrl = _buildServerWatcher.ReplaceVariables(_settings.ProjectUrl);
+            _projectUrl = _buildServerWatcher.ReplaceVariables(_settings.ProjectUrl);
 
-            if (!Uri.IsWellFormedUriString(projectUrl, UriKind.Absolute) || string.IsNullOrWhiteSpace(_settings.ApiToken))
+            if (!Uri.IsWellFormedUriString(_projectUrl, UriKind.Absolute) || string.IsNullOrWhiteSpace(_settings.ApiToken))
             {
                 return;
             }
 
-            _apiClient = new ApiClient(projectUrl, _settings.ApiToken);
+            _apiClient = new ApiClient(_projectUrl, _settings.ApiToken);
+            if (CacheAzureDevOps == null || CacheAzureDevOps.Id != CacheKey)
+            {
+                CacheAzureDevOps = null;
+                _buildDefinitionsTask = _apiClient.GetBuildDefinitionsAsync(_settings.BuildDefinitionFilter);
+            }
+            else
+            {
+                _buildDefinitions = CacheAzureDevOps.BuildDefinitions;
+            }
         }
 
         /// <summary>
@@ -77,7 +94,7 @@ namespace AzureDevOpsIntegration
             return GetBuilds(scheduler, null, true);
         }
 
-        public IObservable<BuildInfo> GetBuilds(IScheduler scheduler, DateTime? sinceDate = null, bool? running = null)
+        private IObservable<BuildInfo> GetBuilds(IScheduler scheduler, DateTime? sinceDate = null, bool? running = null)
         {
             return Observable.Create<BuildInfo>((observer, cancellationToken) => ObserveBuildsAsync(sinceDate, running, observer, cancellationToken));
         }
@@ -92,9 +109,25 @@ namespace AzureDevOpsIntegration
 
             try
             {
-                var builds = await _apiClient.QueryBuildsAsync(_settings.BuildDefinitionFilter, sinceDate, running);
+                if (_buildDefinitions == null)
+                {
+                    _buildDefinitions = await _buildDefinitionsTask;
 
-                Parallel.ForEach(builds, detail => { observer.OnNext(CreateBuildInfo(detail)); });
+                    if (_buildDefinitions == null)
+                    {
+                        observer.OnCompleted();
+                        return;
+                    }
+
+                    CacheAzureDevOps = new CacheAzureDevOps { Id = CacheKey, BuildDefinitions = _buildDefinitions };
+                }
+
+                var builds = await _apiClient.QueryBuildsAsync(_buildDefinitions, sinceDate, running);
+
+                foreach (var build in builds)
+                {
+                    observer.OnNext(CreateBuildInfo(build));
+                }
             }
             catch (OperationCanceledException)
             {
