b59ea9f456c9e27d710ba9d3209685f30a3dfa10
RussKie <russkie@gmail.com>
2020-02-05 00:21:31 +1100

    Optimise SetWindowTheme

--- GitUI/Interops/UxTheme/SetWindowTheme.cs
+++ GitUI/Interops/UxTheme/SetWindowTheme.cs
@@ -4,7 +4,16 @@ namespace System
 {
     internal static partial class NativeMethods
     {
-        [DllImport(Libraries.UxTheme, CharSet = CharSet.Unicode)]
-        public static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+        [DllImport(Libraries.UxTheme, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static unsafe extern int SetWindowTheme(IntPtr hWnd, char* pszSubAppName, char* pszSubIdList);
+
+        public static unsafe int SetWindowTheme(IntPtr hWnd, string subAppName, string subIdList)
+        {
+            fixed (char* pszSubAppName = subAppName)
+            fixed (char* pszSubIdList = subIdList)
+            {
+                return SetWindowTheme(hWnd, pszSubAppName, pszSubIdList);
+            }
+        }
     }
 }
