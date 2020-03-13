15892fb35bdd5a01e94e77352cf4cbcb1c5a707d
RussKie <russkie@gmail.com>
2020-03-13 21:35:30 +1100

    Remove obsolete scripts
    Contributes to #7702

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0Build\Build.ps1""" -restore -build -bl %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0Build\build.ps1""" -restore -build -bl %*"
 exit /b %ErrorLevel%
