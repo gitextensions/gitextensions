2a9bd05998a969e148840aaee2c94f5572b6b706
Jörg Pichler <joergpichler@users.noreply.github.com>
2020-03-27 11:40:54 +0100

    Make DropDownWidth in BranchSelector adjust to longest item (#7903)

--- GitUI/UserControls/BranchSelector.cs
+++ GitUI/UserControls/BranchSelector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GitCommands;
 using GitCommands.Git;
 using GitUIPluginInterfaces;
 using Microsoft.VisualStudio.Threading;
@@ -51,6 +52,8 @@ namespace GitUI.UserControls
                 : LocalBranch.Checked
                     ? GetLocalBranches()
                     : GetRemoteBranches());
+
+            Branches.ResizeDropDownWidth(AppSettings.BranchDropDownMinWidth, AppSettings.BranchDropDownMaxWidth);
 
             if (_containRevisions != null && Branches.Items.Count == 1)
             {
