2eefa67a8905cef890b417b78a9c86494f86186a
Chris Oliver <cado82@gmail.com>
2020-03-30 10:45:28 +0100

    Add root description to Git node in settings tree view (#7904)
    - This fixes an issue with navigating the tree view using the keyboard

---
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/GitRootIntroductionPage.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace GitUI.CommandsDialogs.SettingsDialog.Pages
+{
+    partial class GitRootIntroductionPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 132);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select one of the subnodes to view or edit the Git settings";
+            // 
+            // GitRootIntroductionPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.label1);
+            this.Name = "GitRootIntroductionPage";
+            this.Size = new System.Drawing.Size(473, 229);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+
+    }
+}
