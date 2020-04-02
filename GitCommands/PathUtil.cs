069f3fc34c0098f9808fb972bcd5c14c9a3fc89b
NoahLerner <noahlerner94@gmail.com>
2020-04-02 08:28:13 +0300

    Fix CanBeGitURL and extract to PathUtil

--- GitCommands/PathUtil.cs
+++ GitCommands/PathUtil.cs
@@ -89,6 +89,18 @@ namespace GitCommands
                  || path.StartsWith("file:", StringComparison.CurrentCultureIgnoreCase));
         }
 
+        public static bool CanBeGitURL(string url)
+        {
+            if (url.IsNullOrWhiteSpace())
+            {
+                return false;
+            }
+
+            return PathUtil.IsUrl(url)
+                   || url.EndsWith(".git", StringComparison.CurrentCultureIgnoreCase)
+                   || GitModule.IsValidGitWorkingDir(url);
+        }
+
         [NotNull]
         public static string GetFileName([NotNull] string fileName)
         {
