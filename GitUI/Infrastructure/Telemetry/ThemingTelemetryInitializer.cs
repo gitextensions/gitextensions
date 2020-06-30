7ac64b96a348d5a08c24c42d9e1325fec1c0bea8
NikolayHD <hidalgo1984@list.ru>
2020-07-01 01:27:44 +0300

    review

--- GitUI/Infrastructure/Telemetry/ThemingTelemetryInitializer.cs
+++ GitUI/Infrastructure/Telemetry/ThemingTelemetryInitializer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Drawing;
-using GitExtUtils.GitUI.Theming;
+﻿using GitExtUtils.GitUI.Theming;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 
@@ -15,24 +13,6 @@ namespace GitUI.Infrastructure.Telemetry
             properties["Theme name"] = themeId.Name;
             properties["Theme is builtin"] = themeId.IsBuiltin.ToString();
             properties["Theme use system visual style"] = Theming.ThemeModule.Settings.UseSystemVisualStyle.ToString();
-
-            if (Theming.ThemeModule.Settings.Theme != Theme.Default)
-            {
-                foreach ((AppColor appColor, Color color) in Theming.ThemeModule.Settings.Theme.AppColorValues)
-                {
-                    properties[$"Theme application color {appColor}"] = Format(color);
-                }
-
-                foreach ((KnownColor sysColor, Color color) in Theming.ThemeModule.Settings.Theme.SysColorValues)
-                {
-                    properties[$"Theme system color {sysColor}"] = Format(color);
-                }
-            }
-
-            return;
-
-            static string Format(Color c) =>
-                c.ToArgb().ToString("x8");
         }
     }
 }
