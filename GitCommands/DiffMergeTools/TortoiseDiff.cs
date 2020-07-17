4d3285ade2a5a383cc285e753d814ea20ab94f06
Gerhard Olsson <gerhardol@users.noreply.github.com>
2020-07-18 00:09:55 +0200

    DiffTools: add 'tortoisediff' as a tool alias

---
+++ GitCommands/DiffMergeTools/TortoiseDiff.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace GitCommands.DiffMergeTools
+{
+    /// <summary>
+    /// Alias for name 'tortoisemerge' as Git disables difftools with that name:
+    /// https://github.com/git/git/pull/471#issuecomment-660205096
+    /// </summary>
+    internal class TortoiseDiff : TortoiseGitMerge
+    {
+        /// <inheritdoc />
+        public override string Name => "tortoisediff";
+    }
+}
