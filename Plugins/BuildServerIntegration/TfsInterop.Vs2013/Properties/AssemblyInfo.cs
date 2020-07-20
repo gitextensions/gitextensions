a777eac414273e6afc9c7a24269fcfa9292245c9
RussKie <russkie@gmail.com>
2020-07-20 13:16:21 +1000

    Remove TFS plugins due to incompatibility

--- Plugins/BuildServerIntegration/TfsInterop.Vs2013/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("GitExtensions plugin for integration with TFS 2013 server")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d2c68247-a04a-464a-899d-a906c6828174")]
