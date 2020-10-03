a86f8a09b1a0dd0d0c71f74939a13a34d63f860e
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2020-10-03 17:36:34 +0300

    Update with an extension method per suggestion
    Co-authored-by: Igor Velikorossov <RussKie@users.noreply.github.com>

--- GitUI/OsShellUtil.cs
+++ GitUI/OsShellUtil.cs
@@ -18,7 +18,7 @@ namespace GitUI
 
         public static void SelectPathInFileExplorer(string filePath)
         {
-            Process.Start("explorer.exe", "/select, \"" + filePath + "\"");
+            Process.Start("explorer.exe", $"/select, {filePath.Quote()}");
         }
 
         public static void OpenWithFileExplorer(string filePath)
