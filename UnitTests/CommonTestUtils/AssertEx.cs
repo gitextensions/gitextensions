4e644a674fa45f7d9bf37b824aa4767b2414b118
Drew Noakes <git@drewnoakes.com>
2021-03-08 21:43:29 +1100

    Add AssertEx.SequenceEqual

--- UnitTests/CommonTestUtils/AssertEx.cs
+++ UnitTests/CommonTestUtils/AssertEx.cs
@@ -2,7 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
@@ -11,9 +15,9 @@ namespace CommonTestUtils
 {
     public static class AssertEx
     {
-        public static async Task<Exception> ThrowsAsync(IResolveConstraint expression, AsyncTestDelegate code, string message, params object[] args)
+        public static async Task<Exception?> ThrowsAsync(IResolveConstraint expression, AsyncTestDelegate code, string message, params object?[]? args)
         {
-            Exception caughtException = null;
+            Exception? caughtException = null;
             try
             {
                 await code();
@@ -27,31 +31,86 @@ namespace CommonTestUtils
             return caughtException;
         }
 
-        public static async Task<Exception> ThrowsAsync(IResolveConstraint expression, AsyncTestDelegate code)
+        public static async Task<Exception?> ThrowsAsync(IResolveConstraint expression, AsyncTestDelegate code)
         {
             return await ThrowsAsync(expression, code, string.Empty, null);
         }
 
-        public static async Task<Exception> ThrowsAsync(Type expectedExceptionType, AsyncTestDelegate code, string message, params object[] args)
+        public static async Task<Exception?> ThrowsAsync(Type expectedExceptionType, AsyncTestDelegate code, string message, params object?[]? args)
         {
             return await ThrowsAsync(new ExceptionTypeConstraint(expectedExceptionType), code, message, args);
         }
 
-        public static async Task<Exception> ThrowsAsync(Type expectedExceptionType, AsyncTestDelegate code)
+        public static async Task<Exception?> ThrowsAsync(Type expectedExceptionType, AsyncTestDelegate code)
         {
             return await ThrowsAsync(new ExceptionTypeConstraint(expectedExceptionType), code, string.Empty, null);
         }
 
-        public static async Task<TActual> ThrowsAsync<TActual>(AsyncTestDelegate code, string message, params object[] args)
+        public static async Task<TActual?> ThrowsAsync<TActual>(AsyncTestDelegate code, string message, params object?[]? args)
             where TActual : Exception
         {
-            return (TActual)(await ThrowsAsync(typeof(TActual), code, message, args));
+            return (TActual?)(await ThrowsAsync(typeof(TActual), code, message, args));
         }
 
-        public static async Task<TActual> ThrowsAsync<TActual>(AsyncTestDelegate code)
+        public static async Task<TActual?> ThrowsAsync<TActual>(AsyncTestDelegate code)
             where TActual : Exception
         {
             return await ThrowsAsync<TActual>(code, string.Empty, null);
+        }
+
+        public static void SequenceEqual<T>(
+            IEnumerable<T> expected,
+            IEnumerable<T> actual,
+            IEqualityComparer<T>? comparer = null)
+        {
+            comparer ??= EqualityComparer<T>.Default;
+
+            if (expected is ICollection c1 && actual is ICollection c2)
+            {
+                Assert.AreEqual(c1.Count, c2.Count, "Invalid collection count");
+            }
+
+            int index = 0;
+
+            using var expectedEnumerator = expected.GetEnumerator();
+            using var actualEnumerator = actual.GetEnumerator();
+
+            while (true)
+            {
+                var expectedHasNext = expectedEnumerator.MoveNext();
+                var actualHasNext = actualEnumerator.MoveNext();
+
+                switch (expectedHasNext, actualHasNext)
+                {
+                    case (false, false):
+                    {
+                        // Both sequences end at the same point. We are finished.
+                        return;
+                    }
+
+                    case (false, true):
+                    {
+                        throw new($"Expected sequence ended at index {index} while actual sequence has more items.");
+                    }
+
+                    case (true, false):
+                    {
+                        throw new($"Actual sequence ended at index {index} while expected sequence has more items.");
+                    }
+
+                    case (true, true):
+                    {
+                        if (!comparer.Equals(expectedEnumerator.Current, actualEnumerator.Current))
+                        {
+                            throw new($"Sequences differ at index {index}.\nExpect: {expectedEnumerator.Current}\nActual: {actualEnumerator.Current}");
+                        }
+
+                        break;
+                    }
+                }
+
+                index++;
+            }
         }
     }
 }
