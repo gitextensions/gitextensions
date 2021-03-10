89e7fa8bb0236926f1eec561fbef0d293263dc91
Michael Seibt <mstv@gmx.net>
2021-03-10 22:01:32 +0100

    fixup! Refactor PaintGraphCell

--- GitUI/UserControls/RevisionGrid/Columns/RevisionGraphColumnProvider.cs
+++ GitUI/UserControls/RevisionGrid/Columns/RevisionGraphColumnProvider.cs
@@ -218,7 +218,7 @@ namespace GitUI.UserControls.RevisionGrid.Columns
 
                     DrawItem();
 
-                    // Reset graphics options
+                    // Restore graphics options
                     g.Clip = oldClip;
                     g.SmoothingMode = oldSmoothingMode;
 
