8b8639b30b130d0b8d4fdb3224f4d5e846f802d1
Matyáš Bach <matyasbach@centrum.cz>
2021-03-12 15:02:53 +0100

    Jira plugin configuration - Password renamed to Password/API token (#8836)
    (cherry picked from commit b8e3decffc70b4b98b66290a21655af9191a041c)

--- Plugins/GitUIPluginInterfaces/UserControls/CredentialsControl.Designer.cs
+++ Plugins/GitUIPluginInterfaces/UserControls/CredentialsControl.Designer.cs
@@ -98,7 +98,7 @@
             this.passwordLabel.Name = "passwordLabel";
             this.passwordLabel.Size = new System.Drawing.Size(53, 17);
             this.passwordLabel.TabIndex = 1;
-            this.passwordLabel.Text = "Password";
+            this.passwordLabel.Text = "API token/Password";
             // 
             // CredentialsControl
             // 
