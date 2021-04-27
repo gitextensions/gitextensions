7d6caed802c6d4b5d7425437c153085c18210d9c
Michael Seibt <36601201+mstv@users.noreply.github.com>
2021-04-27 20:52:21 +0200

    Workaround for splitter persistence in FormBrowse (#9125)

--- GitUI/SplitterManager.cs
+++ GitUI/SplitterManager.cs
@@ -29,7 +29,6 @@ namespace GitUI
 
         public void RestoreSplitters()
         {
-            // TODO: Disabled as splitters have become unstable. Refer to #8745 for more details.
             foreach (var splitter in _splitters)
             {
                 splitter.RestoreFromSettings(_settings);
@@ -38,7 +37,6 @@ namespace GitUI
 
         public void SaveSplitters()
         {
-            // TODO: Disabled as splitters have become unstable. Refer to #8745 for more details.
             foreach (var splitter in _splitters)
             {
                 splitter.SaveToSettings(_settings);
