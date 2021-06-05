8d02f286b08a665fd3adb0ed67d2f4411fc1a3fe
RussKie <russkie@gmail.com>
2021-06-05 22:52:28 +1000

    Enable CA1416: Validate platform compatibility
    Related https://github.com/dotnet/roslyn-analyzers/issues/4983#issuecomment-852738069

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows7.0")]
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
