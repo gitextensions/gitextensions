755b70470b12dae02d0c3ed5813169f94d6e3eac
Michael Seibt <mstv@gmx.net>
2021-06-27 00:21:37 +0200

    Release DC retrieved using GetWindowDC

---
+++ GitUI/Interops/User32/ReleaseDC.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    internal static partial class NativeMethods
+    {
+        [DllImport(Libraries.User32)]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+    }
+}
