8e41ffcd9526457660fafb265cb6050625ac152f
Michael Seibt <mstv@gmx.net>
2021-06-27 02:15:12 +0200

    Dispose resources in WindowsJumpListManager.MakeIcon

--- GitUI/WindowsJumpListManager.cs
+++ GitUI/WindowsJumpListManager.cs
@@ -213,8 +213,8 @@ namespace GitUI
         /// <returns>An icon!!</returns>
         private static Icon MakeIcon(Image img, int size, bool keepAspectRatio)
         {
-            var square = new Bitmap(size, size); // create new bitmap
-            Graphics g = Graphics.FromImage(square); // allow drawing to it
+            using Bitmap square = new(size, size); // create new bitmap
+            using Graphics g = Graphics.FromImage(square); // allow drawing to it
 
             int x, y, w, h; // dimensions for new image
 
