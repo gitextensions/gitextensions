d076b78884bca0569a2cbce9f8a721760a3a30de
Gerhard Olsson <6248932+gerhardol@users.noreply.github.com>
2021-07-04 01:24:32 +0200

    FormRebase: Avoid list refs if not advanced (#9326)
    When advanced, reuse the existing Git ref list.

--- GitUI/CommandsDialogs/FormRebase.cs
+++ GitUI/CommandsDialogs/FormRebase.cs
@@ -81,7 +81,9 @@ namespace GitUI.CommandsDialogs
             Currentbranch.Text = selectedHead;
 
             // Offer rebase on refs also for tags (but not stash, notes etc)
-            var refs = Module.GetRefs(RefsFilter.Heads | RefsFilter.Remotes | RefsFilter.Tags).OfType<GitRef>().ToList();
+            List<GitRef> refs = _startRebaseImmediately
+                ? new()
+                : Module.GetRefs(RefsFilter.Heads | RefsFilter.Remotes | RefsFilter.Tags).OfType<GitRef>().ToList();
             Branches.DataSource = refs;
             Branches.DisplayMember = nameof(GitRef.Name);
 
@@ -92,7 +94,7 @@ namespace GitUI.CommandsDialogs
 
             Branches.Select();
 
-            refs = Module.GetRefs(RefsFilter.Heads).OfType<GitRef>().ToList();
+            refs = refs.Where(h => h.IsHead).ToList();
             cboTo.DataSource = refs;
             cboTo.DisplayMember = nameof(GitRef.Name);
 
