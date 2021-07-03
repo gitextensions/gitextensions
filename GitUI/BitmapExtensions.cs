bb11df8ae9dfc262c46f05e1872b2fb346cb0f73
Michael Seibt <36601201+mstv@users.noreply.github.com>
2021-07-04 01:50:44 +0200

    Fix/9166 gdi objects (#9329)

---
+++ GitUI/BitmapExtensions.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+
+using System;
+using System.Drawing;
+
+namespace GitUI
+{
+    public static class BitmapExtensions
+    {
+        public static Icon ToIcon(this Bitmap bitmap)
+        {
+            if (bitmap is null)
+            {
+                throw new ArgumentNullException(nameof(bitmap));
+            }
+
+            IntPtr handle = IntPtr.Zero;
+            try
+            {
+                handle = bitmap.GetHicon();
+                Icon icon = Icon.FromHandle(handle);
+
+                return (Icon)icon.Clone();
+            }
+            finally
+            {
+                if (handle != IntPtr.Zero)
+                {
+                    NativeMethods.DestroyIcon(handle);
+                }
+            }
+        }
+    }
+}
