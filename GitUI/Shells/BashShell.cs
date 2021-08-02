7afcb01d1fa055af2d9f646670039b234ec3f891
Michael Seibt <36601201+mstv@users.noreply.github.com>
2021-08-03 00:29:06 +0200

    BashShell: Try git-bash.exe first (#9430)
    and search also in the 64-bit-programs folder

--- GitUI/Shells/BashShell.cs
+++ GitUI/Shells/BashShell.cs
@@ -7,7 +7,8 @@ namespace GitUI.Shells
 {
     public class BashShell : ShellDescriptor
     {
-        private const string BashExe = "bash.exe"; // Generic bash, should generally be in the git dir
+        private const string GitBashExe = "git-bash.exe"; // Bash with git in the path, should generally be in the git dir
+        private const string BashExe = "bash.exe"; // Fallback to generic bash, should generally be in the git bin dir
         private const string ShExe = "sh.exe";     // Fallback to SH
         public const string ShellName = "bash";
 
@@ -16,17 +17,28 @@ namespace GitUI.Shells
             Name = ShellName;
             Icon = Images.GitForWindows;
 
-            ExecutableName = BashExe;
-            if (PathUtil.TryFindShellPath(ExecutableName, out var exePath))
+            if (PathUtil.TryFindShellPath(GitBashExe, out var exePath))
             {
+                ExecutableName = GitBashExe;
                 ExecutablePath = exePath;
-                ExecutableCommandLine = $"{exePath.Quote()} --login -i";
+
+                // Try to find bash or sh below to set ExecutableCommandLine, as git-bash.exe cannot be connected to the built-in console.
             }
-            else if (PathUtil.TryFindShellPath(ShExe, out exePath))
+
+            foreach (string shellExecutableName in new string[] { BashExe, ShExe })
             {
-                ExecutableName = ShExe;
-                ExecutablePath = exePath;
-                ExecutableCommandLine = $"{exePath.Quote()} --login -i";
+                if (PathUtil.TryFindShellPath(shellExecutableName, out exePath))
+                {
+                    if (ExecutablePath is null)
+                    {
+                        ExecutableName = shellExecutableName;
+                        ExecutablePath = exePath;
+                    }
+
+                    ExecutableCommandLine = $"{exePath.Quote()} --login -i";
+
+                    break;
+                }
             }
         }
 
