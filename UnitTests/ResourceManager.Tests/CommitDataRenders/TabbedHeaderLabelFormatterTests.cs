e6b51dfc1fb27c6ed5a63c0de2bd59f81904e1cf
Gintautas Švedas <svedgintas@gmail.com>
2021-08-06 05:27:32 +0300

    Fixed Commit info varying indents. Fixes #9164 (#9444)

--- UnitTests/ResourceManager.Tests/CommitDataRenders/TabbedHeaderLabelFormatterTests.cs
+++ UnitTests/ResourceManager.Tests/CommitDataRenders/TabbedHeaderLabelFormatterTests.cs
@@ -15,15 +15,15 @@ namespace ResourceManagerTests.CommitDataRenders
             _formatter = new TabbedHeaderLabelFormatter();
         }
 
-        [TestCase(null, 10, ":			")]
-        [TestCase("", 10, ":			")]
-        [TestCase("", 16, ":				")]
+        [TestCase(null, 10, ":		")]
+        [TestCase("", 10, ":		")]
+        [TestCase("", 16, ":			")]
         [TestCase(" ", 10, " :		")]
         [TestCase("a", 10, "a:		")]
-        [TestCase("a", 8, "a:		")]
+        [TestCase("a", 8, "a:	")]
         [TestCase("abc", 1, "abc:")]
-        [TestCase("John Doe <John.Doe@test.com>", 38, "John Doe &lt;John.Doe@test.com&gt;:	")]
-        [TestCase("John Doe <John.Doe@test.com>", 40, "John Doe &lt;John.Doe@test.com&gt;:		")]
+        [TestCase("John Doe <John.Doe@test.com>", 38, "John Doe &lt;John.Doe@test.com&gt;:")]
+        [TestCase("John Doe <John.Doe@test.com>", 40, "John Doe &lt;John.Doe@test.com&gt;:	")]
         public void FormatLabel_should_render_correctly(string given, int desiredLength, string expected)
         {
             _formatter.FormatLabel(given, desiredLength).Should().Be(expected);
