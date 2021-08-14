df412624e74ac1464eea96716e3afcce72d72111
David <davidgiga1993@gmail.com>
2021-08-14 16:09:30 +0200

    Recent repo search (#9380)

--- GitUI/CommandsDialogs/BrowseDialog/DashboardControl/UserRepositoriesListController.cs
+++ GitUI/CommandsDialogs/BrowseDialog/DashboardControl/UserRepositoriesListController.cs
@@ -13,14 +13,20 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
         Task AssignCategoryAsync(Repository repository, string? category);
         string GetCurrentBranchName(string path);
         bool IsValidGitWorkingDir(string path);
-        (IReadOnlyList<RecentRepoInfo> recentRepositories, IReadOnlyList<RecentRepoInfo> favouriteRepositories) PreRenderRepositories(Graphics g);
+        (IReadOnlyList<RecentRepoInfo> recentRepositories, IReadOnlyList<RecentRepoInfo> favouriteRepositories) PreRenderRepositories(Graphics g, string filter);
         bool RemoveInvalidRepository(string path);
+        void ClearCache();
     }
 
     public sealed class UserRepositoriesListController : IUserRepositoriesListController
     {
         private readonly ILocalRepositoryManager _localRepositoryManager;
         private readonly IInvalidRepositoryRemover _invalidRepositoryRemover;
+
+        // Holds the raw, unfiltered list of repositories.
+        // This is done to allow fast filtering of all known repos.
+        private IList<Repository>? _allRecentRepositories;
+        private IList<Repository>? _allFavoriteRepositories;
 
         public UserRepositoriesListController(ILocalRepositoryManager localRepositoryManager, IInvalidRepositoryRemover invalidRepositoryRemover)
         {
@@ -38,6 +44,15 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
             await _localRepositoryManager.AssignCategoryAsync(repository, category);
         }
 
+        /// <summary>
+        /// Clears the repository cache. After this call the repository list will be loaded from disk.
+        /// </summary>
+        public void ClearCache()
+        {
+            _allRecentRepositories = null;
+            _allFavoriteRepositories = null;
+        }
+
         public string GetCurrentBranchName(string path)
         {
             if (!AppSettings.ShowRepoCurrentBranch || GitModule.IsBareRepository(path))
@@ -53,7 +68,7 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
             return GitModule.IsValidGitWorkingDir(path);
         }
 
-        public (IReadOnlyList<RecentRepoInfo> recentRepositories, IReadOnlyList<RecentRepoInfo> favouriteRepositories) PreRenderRepositories(Graphics g)
+        public (IReadOnlyList<RecentRepoInfo> recentRepositories, IReadOnlyList<RecentRepoInfo> favouriteRepositories) PreRenderRepositories(Graphics g, string pattern)
         {
             List<RecentRepoInfo> mostRecentRepos = new();
             List<RecentRepoInfo> lessRecentRepos = new();
@@ -70,11 +85,13 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
                 SortMostRecentRepos = AppSettings.SortMostRecentRepos
             };
 
-            var repositories = ThreadHelper.JoinableTaskFactory.Run(() => RepositoryHistoryManager.Locals.LoadRecentHistoryAsync());
+            _allRecentRepositories ??= ThreadHelper.JoinableTaskFactory.Run(() => RepositoryHistoryManager.Locals.LoadRecentHistoryAsync());
+            var repositories = Filter(_allRecentRepositories, pattern);
             splitter.SplitRecentRepos(repositories, mostRecentRepos, lessRecentRepos);
             var recentRepositories = mostRecentRepos.Union(lessRecentRepos).ToList();
 
-            repositories = ThreadHelper.JoinableTaskFactory.Run(() => RepositoryHistoryManager.Locals.LoadFavouriteHistoryAsync());
+            _allFavoriteRepositories ??= ThreadHelper.JoinableTaskFactory.Run(() => RepositoryHistoryManager.Locals.LoadFavouriteHistoryAsync());
+            repositories = Filter(_allFavoriteRepositories, pattern);
             mostRecentRepos.Clear();
             lessRecentRepos.Clear();
             splitter.SplitRecentRepos(repositories, mostRecentRepos, lessRecentRepos);
@@ -85,5 +102,26 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
 
         public bool RemoveInvalidRepository(string path)
            => _invalidRepositoryRemover.ShowDeleteInvalidRepositoryDialog(path);
+
+        private static IList<Repository> Filter(IList<Repository> repositories, string pattern)
+        {
+            if (pattern.Length == 0)
+            {
+                return repositories;
+            }
+
+            List<Repository> repos = new();
+            for (var i = repositories.Count - 1; i >= 0; i--)
+            {
+                var repo = repositories[i];
+                if (repo.Path.Contains(pattern, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    repos.Add(repo);
+                    continue;
+                }
+            }
+
+            return repos;
+        }
     }
 }
