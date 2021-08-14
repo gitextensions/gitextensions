53969a2670295b0b44ee8d061e6a26e5aaa8aae9
Gerhard Olsson <gerhardol@users.noreply.github.com>
2021-08-15 00:38:44 +0200

    CommitDiff: Do not Join() in Dispose()

--- GitUI/CommitInfo/CommitInfo.cs
+++ GitUI/CommitInfo/CommitInfo.cs
@@ -152,8 +152,7 @@ namespace GitUI.CommitInfo
         {
             if (disposing)
             {
-                _asyncLoadCancellation.CancelCurrent();
-                ThreadHelper.JoinPendingOperations(); // those run with FileAndForget
+                _asyncLoadCancellation.Dispose();
 
                 components?.Dispose();
             }
