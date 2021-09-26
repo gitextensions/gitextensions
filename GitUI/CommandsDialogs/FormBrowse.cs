b14a7488433b50070632b736d50fa0e90a38a700
Igor Velikorossov <russkie@gmail.com>
2021-09-26 19:27:41 +1000

    Move the init logic from OnLoad to InternalInitialize
    If the app is opened with `browse <repo>` we would execute a number of
    instructions that we wouldn't execute if the app was opened in the
    dashboard-mode. Those instructions included initialising the "Push" button,
    the revision grid.
    
    Move these instructions into `InternalInitialize` method, that gets
    executed whenever a dashboard is shown or a repo is loaded. This
    way we unify the flow, and contain repo-init instructions in one place.
    
    At the same time restore the splitter positions when we load the form
    irrespective of the current view.
    
    Addendum to #9106

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -212,6 +212,19 @@ namespace GitUI.CommandsDialogs
                 PuTTYToolStripMenuItem.Visible = false;
             }
 
+            RevisionGrid.IndexWatcher.Changed += (_, args) =>
+            {
+                bool indexChanged = args.IsIndexChanged;
+                this.InvokeAsync(
+                        () =>
+                        {
+                            RefreshButton.Image = indexChanged && AppSettings.UseFastChecks && Module.IsValidGitWorkingDir()
+                                ? Images.ReloadRevisionsDirty
+                                : Images.ReloadRevisions;
+                        })
+                    .FileAndForget();
+            };
+            RevisionGrid.PathFilterChanged += revisionGrid_PathFilterChanged;
             RevisionGrid.RefFilterOptionsChanged += (sender, e) =>
             {
                 if (e.RefFilterOptions.HasFlag(RefFilterOptions.All | RefFilterOptions.Boundary))
@@ -326,6 +339,7 @@ namespace GitUI.CommandsDialogs
 
             _aheadBehindDataProvider = GitVersion.Current.SupportAheadBehindData ? new AheadBehindDataProvider(() => Module.GitExecutable) : null;
 
+            toolStripButtonPush.Initialize(_aheadBehindDataProvider);
             repoObjectsTree.Initialize(_aheadBehindDataProvider, branchFilterAction: ToolStripFilters.SetBranchFilter, RevisionGrid, RevisionGrid, RevisionGrid);
             revisionDiff.Bind(RevisionGrid, fileTree, RequestRefresh);
             fileTree.Bind(RevisionGrid, RequestRefresh);
@@ -683,43 +697,21 @@ namespace GitUI.CommandsDialogs
             HideVariableMainMenuItems();
             RefreshSplitViewLayout();
             LayoutRevisionInfo();
+            SetSplitterPositions();
             InternalInitialize(false);
 
-            if (!Module.IsValidGitWorkingDir())
+            if (Module.IsValidGitWorkingDir())
             {
-                base.OnLoad(e);
-                return;
+                // When loading the app with 'browse <repo>' args, we don't go through UICommands.PostRepositoryChanged event
+                // and need to manually load the revisions in to the grid.
+                RevisionGrid.Load();
+                UpdateSubmodulesStructure();
+                UpdateStashCount();
             }
-
-            RevisionGrid.Load();
-
-            ActiveControl = RevisionGrid;
-            RevisionGrid.IndexWatcher.Reset();
-
-            RevisionGrid.IndexWatcher.Changed += (_, args) =>
-            {
-                bool indexChanged = args.IsIndexChanged;
-                this.InvokeAsync(
-                        () =>
-                        {
-                            RefreshButton.Image = indexChanged && AppSettings.UseFastChecks && Module.IsValidGitWorkingDir()
-                                ? Images.ReloadRevisionsDirty
-                                : Images.ReloadRevisions;
-                        })
-                    .FileAndForget();
-            };
-            RevisionGrid.PathFilterChanged += revisionGrid_PathFilterChanged;
-            UpdateSubmodulesStructure();
-            UpdateStashCount();
-
-            toolStripButtonPush.Initialize(_aheadBehindDataProvider);
-            toolStripButtonPush.DisplayAheadBehindInformation(Module.GetSelectedBranch());
-
-            _formBrowseDiagnosticsReporter.Report();
 
             base.OnLoad(e);
 
-            SetSplitterPositions();
+            _formBrowseDiagnosticsReporter.Report();
         }
 
         protected override void OnActivated(EventArgs e)
@@ -1137,6 +1129,11 @@ namespace GitUI.CommandsDialogs
                     _formBrowseMenus.AddMenuCommandSet(MainMenuItem.ViewMenu, RevisionGrid.MenuCommands.ViewMenuCommands);
 
                     _formBrowseMenus.InsertRevisionGridMainMenuItems(repositoryToolStripMenuItem);
+
+                    toolStripButtonPush.DisplayAheadBehindInformation(Module.GetSelectedBranch());
+
+                    ActiveControl = RevisionGrid;
+                    RevisionGrid.IndexWatcher.Reset();
                 }
                 else
                 {
