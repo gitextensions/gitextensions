2328d79bf047c06750681a4e2e42137f563026b9
NikolayHD <hidalgo1984@list.ru>
2021-10-03 17:22:01 +0300

    wip

---
+++ GitExtUtils/GitUI/ToolStripExProfessionalRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace GitUI
+{
+    internal class ToolStripExProfessionalRenderer : ToolStripProfessionalRenderer
+    {
+        public ToolStripExProfessionalRenderer()
+        {
+            RoundedEdges = false;
+        }
+
+        protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs e)
+        {
+            if (e.ToolStrip.GetMenuItemBackgroundFilter()?.ShouldRenderMenuItemBackground(e) != false)
+            {
+                base.OnRenderMenuItemBackground(e);
+            }
+        }
+
+        protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e)
+        {
+            if (e.ToolStrip is not IToolStripEx { DrawBorder: false })
+            {
+                // render border
+                base.OnRenderToolStripBorder(e);
+            }
+        }
+    }
+}
