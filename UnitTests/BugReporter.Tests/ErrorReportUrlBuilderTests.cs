gitextensions/gitextensions

commit 6890e99096a8262b0f80ed58b283b8c7eb705407

Update issue template and update BugReporter to use the new template (#9638)

BugReporter/BugReportForm.cs
@@ -35,7 +35,7 @@ Send report anyway?");
         private readonly TranslationString _toolTipQuit = new("Quit application without reporting the issue");
         private readonly TranslationString _noReproStepsSuppliedErrorMessage = new(@"Please provide as much as information as possible to help the developers solve this issue.");
 
-        private static readonly IErrorReportMarkDownBodyBuilder ErrorReportBodyBuilder;
+        private static readonly IErrorReportUrlBuilder ErrorReportBodyBuilder;
         private static readonly GitHubUrlBuilder UrlBuilder;
         private SerializableException? _lastException;
         private Report? _lastReport;
@@ -44,7 +44,7 @@ Send report anyway?");
 
         static BugReportForm()
         {
-            ErrorReportBodyBuilder = new ErrorReportMarkDownBodyBuilder();
+            ErrorReportBodyBuilder = new ErrorReportUrlBuilder();
             UrlBuilder = new GitHubUrlBuilder(ErrorReportBodyBuilder);
         }
 

BugReporter/ErrorReportUrlBuilder.cs
@@ -1,0 +1,53 @@
+﻿using System;
+using System.Text;
+using BugReporter.Serialization;
+
+namespace BugReporter
+{
+    public interface IErrorReportUrlBuilder
+    {
+        string Build(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo);
+    }
+
+    public sealed class ErrorReportUrlBuilder : IErrorReportUrlBuilder
+    {
+        public string Build(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo)
+        {
+            if (exception is null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            StringBuilder sb = new();
+
+            sb.Append($"template=bug_report.yml");
+            sb.Append($"&labels={Uri.EscapeDataString("type: NBug")}");
+            sb.Append($"&about={Uri.EscapeDataString(environmentInfo)}");
+
+            StringBuilder exceptionDetails = new();
+            if (!string.IsNullOrEmpty(exceptionInfo))
+            {
+                // Tests may be run on VMs, and AppendLine() could be either \r\n or \n
+                exceptionDetails.Append('\n');
+                exceptionDetails.Append(exceptionInfo);
+                exceptionDetails.Append('\n');
+            }
+
+            exceptionDetails.Append("```\n");
+            exceptionDetails.Append(exception.ToString());
+            exceptionDetails.Append('\n');
+            exceptionDetails.Append("```\n");
+            exceptionDetails.Append('\n');
+
+            if (!string.IsNullOrWhiteSpace(additionalInfo))
+            {
+                exceptionDetails.Append(additionalInfo.Trim());
+                exceptionDetails.Append('\n');
+            }
+
+            sb.Append($"&description={Uri.EscapeDataString(exceptionDetails.ToString())}");
+
+            return sb.ToString();
+        }
+    }
+}

UnitTests/BugReporter.Tests/ErrorReportUrlBuilderTests.cs
@@ -1,0 +1,46 @@
+﻿using System;
+using BugReporter;
+using BugReporter.Serialization;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BugReporterTests
+{
+    [TestFixture]
+    [SetCulture("en-US")]
+    [SetUICulture("en-US")]
+    public sealed class ErrorReportUrlBuilderTests
+    {
+        private ErrorReportUrlBuilder _builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _builder = new ErrorReportUrlBuilder();
+        }
+
+        [Test]
+        public void Build_should_throw_ANE_if_exception_null()
+        {
+            ((Action)(() => _builder.Build(exception: null, exceptionInfo: null, environmentInfo: null, additionalInfo: null)))
+                .Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void Build_should_serialize_exception_into_url()
+        {
+            var exception = new SerializableException(new Exception("OPPS!", new ApplicationException("BAM!", new DivideByZeroException("BOOM!"))));
+            string environmentInfo = @"Git Extensions 3.4.3.9999
+Build d4b0f48
+Git 2.27.0.windows.1
+Microsoft Windows NT 10.0.19041.0
+.NET Framework 4.8.4360.0
+DPI 288dpi (300% scaling)".Replace("\r\n", "\n");
+            string additionalInfo = "I was just trying out a bunch of stuff, not sure what is the cause. Could just as well be an issue with Git. I cannot provide the specific Repo I used. Hope I'm not wasting your time.";
+
+            string url = _builder.Build(exception, exceptionInfo: null, environmentInfo, additionalInfo);
+
+            Assert.AreEqual("template=bug_report.yml&labels=type%3A%20NBug&about=Git%20Extensions%203.4.3.9999%0ABuild%20d4b0f48%0AGit%202.27.0.windows.1%0AMicrosoft%20Windows%20NT%2010.0.19041.0%0A.NET%20Framework%204.8.4360.0%0ADPI%20288dpi%20%28300%25%20scaling%29&description=%60%60%60%0ASystem.Exception%3A%20OPPS%21%0D%0A%20---%3E%20System.ApplicationException%3A%20BAM%21%0D%0A%20---%3E%20System.DivideByZeroException%3A%20BOOM%21%0D%0A%20%20%20---%20End%20of%20inner%20exception%20stack%20trace%20---%0D%0A%20%20%20---%20End%20of%20inner%20exception%20stack%20trace%20---%0A%60%60%60%0A%0AI%20was%20just%20trying%20out%20a%20bunch%20of%20stuff%2C%20not%20sure%20what%20is%20the%20cause.%20Could%20just%20as%20well%20be%20an%20issue%20with%20Git.%20I%20cannot%20provide%20the%20specific%20Repo%20I%20used.%20Hope%20I%27m%20not%20wasting%20your%20time.%0A", url);
+        }
+    }
+}

.github/ISSUE_TEMPLATE/bug_report.yml
@@ -1,0 +1,59 @@
+name: Bug
+description: Report a bug or other issue
+labels: ':eyeglasses: status: needs review'
+body:
+  - type: markdown
+    attributes:
+      value: |
+        Thanks for taking the time to fill out this bug report!
+
+        ⚠️ Review existing issues to see whether someone else has already reported your issue.
+        📖 Please also read [CONTRIBUTING.md](https://github.com/gitextensions/gitextensions/blob/master/CONTRIBUTING.md).
+  - type: textarea
+    id: about
+    attributes:
+      label: Environment
+      description: Use _Help > About_ in Git Extensions to copy this information to the clipboard automatically.
+      value: |
+        - GitExtensions version:
+        - GIT version: 
+        - OS version: 
+        - .NET version: 
+    validations:
+      required: true
+  - type: textarea
+    id: description
+    attributes:
+      label: Issue description
+      description: |
+        Be as specific and detailed as possible to help us triaging your issue. Screenshots and/or animations can be very useful in helping to understand the issue you're facing.
+
+        Tip: You can attach images or log files by clicking this area to highlight it and then dragging files in.
+        Tip: You can use https://www.screentogif.com/ to record animations and videos.
+    validations:
+      required: true
+  - type: textarea
+    id: steps
+    attributes:
+      label: Steps to reproduce
+      description: Take some time to try and reproduce the issue, then explain how to do so here. 
+    validations:
+      required: true
+  - type: textarea
+    id: regression
+    attributes:
+      label: Did this work in previous version of GitExtensions?
+      description: If so, which version?
+    validations:
+      required: false
+  - type: textarea
+    id: diagnostics
+    attributes:
+      label: Diagnostics
+      description: If relevant, as described at https://github.com/gitextensions/gitextensions/wiki/Diagnosing-problems.
+    validations:
+      required: false
+  - type: markdown
+    attributes:
+      value: |
+        ❤️ Git Extensions? Please consider supporting our collective: https://opencollective.com/gitextensions

BugReporter/ErrorReportMarkDownBodyBuilder.cs
@@ -1,88 +1,0 @@
-﻿using System;
-using System.Text;
-using BugReporter.Serialization;
-
-namespace BugReporter
-{
-    public interface IErrorReportMarkDownBodyBuilder
-    {
-        string Build(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo);
-    }
-
-    public sealed class ErrorReportMarkDownBodyBuilder : IErrorReportMarkDownBodyBuilder
-    {
-        public string Build(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo)
-        {
-            if (exception is null)
-            {
-                throw new ArgumentNullException(nameof(exception));
-            }
-
-            StringBuilder sb = new();
-
-            sb.AppendLine(@"<!--
-    :warning: Review existing issues to see whether someone else has already reported your issue.
--->
-
-:warning: The sections below must be filled in and this text must be removed or the issue will be closed.
-
-## Current behaviour
-
-<!-- Be as specific and detailed as possible to help us identify your issue. -->
-
-
-## Expected behaviour
-
-
-## Steps to reproduce
-
-<!-- Take some time to try and reproduce the issue, then explain how to do so here. -->
-
-
-## Error Details");
-            if (!string.IsNullOrEmpty(exceptionInfo))
-            {
-                sb.AppendLine();
-                sb.AppendLine(exceptionInfo);
-            }
-
-            sb.AppendLine("```");
-            sb.AppendLine(exception.ToString());
-            sb.AppendLine("```");
-            sb.AppendLine();
-            sb.AppendLine();
-
-            if (!string.IsNullOrWhiteSpace(additionalInfo))
-            {
-                sb.AppendLine("## Additional information");
-                sb.AppendLine(additionalInfo.Trim());
-                sb.AppendLine();
-                sb.AppendLine();
-            }
-
-            try
-            {
-                sb.AppendLine("## Environment");
-
-                if (!string.IsNullOrWhiteSpace(environmentInfo))
-                {
-                    sb.AppendLine(environmentInfo);
-                }
-                else
-                {
-                    sb.AppendLine("System information is not supplied");
-                }
-            }
-            catch (Exception ex)
-            {
-                sb.AppendLine("Failed to retrieve system information.");
-                sb.AppendLine("Exception:");
-                sb.AppendLine("```");
-                sb.AppendLine(ex.ToString());
-                sb.AppendLine("```");
-            }
-
-            return sb.ToString();
-        }
-    }
-}

BugReporter/GitHubUrlBuilder.cs
@@ -11,11 +11,11 @@ namespace BugReporter
 {
     public sealed class GitHubUrlBuilder
     {
-        private readonly IErrorReportMarkDownBodyBuilder _errorReportMarkDownBodyBuilder;
+        private readonly IErrorReportUrlBuilder _errorReportUrlBuilder;
 
-        public GitHubUrlBuilder(IErrorReportMarkDownBodyBuilder errorReportMarkDownBodyBuilder)
+        public GitHubUrlBuilder(IErrorReportUrlBuilder errorReportUrlBuilder)
         {
-            _errorReportMarkDownBodyBuilder = errorReportMarkDownBodyBuilder;
+            _errorReportUrlBuilder = errorReportUrlBuilder;
         }
 
         /// <summary>
@@ -50,9 +50,8 @@ namespace BugReporter
                 subject = subject.Substring(0, 66) + "...";
             }
 
-            string body = Uri.EscapeDataString(_errorReportMarkDownBodyBuilder.Build(exception, exceptionInfo, environmentInfo, additionalInfo));
-
-            return $"{validatedUri}{separator}title={Uri.EscapeDataString(subject)}&body={body}";
+            string urlEncodedError = _errorReportUrlBuilder.Build(exception, exceptionInfo, environmentInfo, additionalInfo);
+            return $"{validatedUri}{separator}title={Uri.EscapeDataString(subject)}&{urlEncodedError}";
         }
     }
 }

commit 8ac72b0945add55c89f2ad722c212805f49732b6

NBug copy text were url encoded
Regression from 6890e99096a8262b0f80ed58b283b8c7eb705407

BugReporter/BugReportForm.cs
@@ -171,7 +171,7 @@ Send report anyway?");
         {
             Validates.NotNull(_lastException);
 
-            var report = ErrorReportBodyBuilder.Build(_lastException, _exceptionInfo, _environmentInfo, descriptionTextBox.Text);
+            var report = ErrorReportBodyBuilder.CopyText(_lastException, _exceptionInfo, _environmentInfo, descriptionTextBox.Text);
             if (string.IsNullOrWhiteSpace(report))
             {
                 return;

BugReporter/ErrorReportUrlBuilder.cs
@@ -7,6 +7,7 @@ namespace BugReporter
     public interface IErrorReportUrlBuilder
     {
         string Build(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo);
+        string CopyText(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo);
     }
 
     public sealed class ErrorReportUrlBuilder : IErrorReportUrlBuilder
@@ -23,18 +24,39 @@ namespace BugReporter
             sb.Append($"template=bug_report.yml");
             sb.Append($"&labels={Uri.EscapeDataString("type: NBug")}");
             sb.Append($"&about={Uri.EscapeDataString(environmentInfo)}");
+            sb.Append($"&description={Uri.EscapeDataString(GetExceptionDetails(exception, exceptionInfo, additionalInfo).ToString())}");
 
+            return sb.ToString();
+        }
+
+        public string CopyText(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo)
+        {
+            if (exception is null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            StringBuilder sb = new();
+            sb.Append(environmentInfo);
+            sb.Append(GetExceptionDetails(exception, exceptionInfo, additionalInfo).ToString());
+
+            return sb.ToString();
+        }
+
+        private StringBuilder GetExceptionDetails(SerializableException exception, string exceptionInfo, string? additionalInfo)
+        {
             StringBuilder exceptionDetails = new();
             if (!string.IsNullOrEmpty(exceptionInfo))
             {
                 // Tests may be run on VMs, and AppendLine() could be either \r\n or \n
                 exceptionDetails.Append('\n');
-                exceptionDetails.Append(exceptionInfo);
+                exceptionDetails.Append(exceptionInfo.Trim());
+                exceptionDetails.Append('\n');
                 exceptionDetails.Append('\n');
             }
 
             exceptionDetails.Append("```\n");
-            exceptionDetails.Append(exception.ToString());
+            exceptionDetails.Append(exception.ToString().Trim());
             exceptionDetails.Append('\n');
             exceptionDetails.Append("```\n");
             exceptionDetails.Append('\n');
@@ -45,9 +67,7 @@ namespace BugReporter
                 exceptionDetails.Append('\n');
             }
 
-            sb.Append($"&description={Uri.EscapeDataString(exceptionDetails.ToString())}");
-
-            return sb.ToString();
+            return exceptionDetails;
         }
     }
 }

UnitTests/BugReporter.Tests/ErrorReportUrlBuilderTests.cs
@@ -26,10 +26,9 @@ namespace BugReporterTests
                 .Should().Throw<ArgumentNullException>();
         }
 
-        [Test]
-        public void Build_should_serialize_exception_into_url()
+        private (SerializableException exception, string? exceptionInfo, string environmentInfo, string additionalInfo) BuildData()
         {
-            var exception = new SerializableException(new Exception("OPPS!", new ApplicationException("BAM!", new DivideByZeroException("BOOM!"))));
+            SerializableException exception = new(new Exception("OPPS!", new ApplicationException("BAM!", new DivideByZeroException("BOOM!"))));
             string environmentInfo = @"Git Extensions 3.4.3.9999
 Build d4b0f48
 Git 2.27.0.windows.1
@@ -38,9 +37,24 @@ Microsoft Windows NT 10.0.19041.0
 DPI 288dpi (300% scaling)".Replace("\r\n", "\n");
             string additionalInfo = "I was just trying out a bunch of stuff, not sure what is the cause. Could just as well be an issue with Git. I cannot provide the specific Repo I used. Hope I'm not wasting your time.";
 
-            string url = _builder.Build(exception, exceptionInfo: null, environmentInfo, additionalInfo);
+            return (exception, exceptionInfo: null, environmentInfo, additionalInfo);
+        }
 
+        [Test]
+        public void Build_should_serialize_exception_into_url()
+        {
+            (SerializableException exception, string? exceptionInfo, string environmentInfo, string additionalInfo) bd = BuildData();
+            string url = _builder.Build(bd.exception, bd.exceptionInfo, bd.environmentInfo, bd.additionalInfo);
             Assert.AreEqual("template=bug_report.yml&labels=type%3A%20NBug&about=Git%20Extensions%203.4.3.9999%0ABuild%20d4b0f48%0AGit%202.27.0.windows.1%0AMicrosoft%20Windows%20NT%2010.0.19041.0%0A.NET%20Framework%204.8.4360.0%0ADPI%20288dpi%20%28300%25%20scaling%29&description=%60%60%60%0ASystem.Exception%3A%20OPPS%21%0D%0A%20---%3E%20System.ApplicationException%3A%20BAM%21%0D%0A%20---%3E%20System.DivideByZeroException%3A%20BOOM%21%0D%0A%20%20%20---%20End%20of%20inner%20exception%20stack%20trace%20---%0D%0A%20%20%20---%20End%20of%20inner%20exception%20stack%20trace%20---%0A%60%60%60%0A%0AI%20was%20just%20trying%20out%20a%20bunch%20of%20stuff%2C%20not%20sure%20what%20is%20the%20cause.%20Could%20just%20as%20well%20be%20an%20issue%20with%20Git.%20I%20cannot%20provide%20the%20specific%20Repo%20I%20used.%20Hope%20I%27m%20not%20wasting%20your%20time.%0A", url);
+        }
+
+        [Test]
+        public void CopyText_should_format_text()
+        {
+            (SerializableException exception, string? exceptionInfo, string environmentInfo, string additionalInfo) bd = BuildData();
+            string url = _builder.CopyText(bd.exception, bd.exceptionInfo, bd.environmentInfo, bd.additionalInfo);
+
+            Assert.AreEqual("Git Extensions 3.4.3.9999\nBuild d4b0f48\nGit 2.27.0.windows.1\nMicrosoft Windows NT 10.0.19041.0\n.NET Framework 4.8.4360.0\nDPI 288dpi (300% scaling)```\nSystem.Exception: OPPS!\r\n ---> System.ApplicationException: BAM!\r\n ---> System.DivideByZeroException: BOOM!\r\n   --- End of inner exception stack trace ---\r\n   --- End of inner exception stack trace ---\n```\n\nI was just trying out a bunch of stuff, not sure what is the cause. Could just as well be an issue with Git. I cannot provide the specific Repo I used. Hope I'm not wasting your time.\n", url);
         }
     }
 }