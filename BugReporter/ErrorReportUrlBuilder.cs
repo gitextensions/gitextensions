db5c8b092a9d471705d4f4ba8403f2ddb2601fc8
Gerhard Olsson <6248932+gerhardol@users.noreply.github.com>
2021-10-21 12:27:24 +0200

    NBug copy text were url encoded (#9666)

--- BugReporter/ErrorReportUrlBuilder.cs
+++ BugReporter/ErrorReportUrlBuilder.cs
@@ -7,6 +7,7 @@ namespace BugReporter
     public interface IErrorReportUrlBuilder
     {
         string Build(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo);
+        string CopyText(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo);
     }
 
     public sealed class ErrorReportUrlBuilder : IErrorReportUrlBuilder
@@ -23,18 +24,39 @@ namespace BugReporter
             sb.Append($"template=bug_report.yml");
             sb.Append($"&labels={Uri.EscapeDataString("type: NBug")}");
             sb.Append($"&about={Uri.EscapeDataString(environmentInfo)}");
+            sb.Append($"&description={Uri.EscapeDataString(GetExceptionDetails(exception, exceptionInfo, additionalInfo).ToString())}");
 
+            return sb.ToString();
+        }
+
+        public string CopyText(SerializableException exception, string exceptionInfo, string? environmentInfo, string? additionalInfo)
+        {
+            if (exception is null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            StringBuilder sb = new();
+            sb.Append(environmentInfo);
+            sb.Append(GetExceptionDetails(exception, exceptionInfo, additionalInfo).ToString());
+
+            return sb.ToString();
+        }
+
+        private StringBuilder GetExceptionDetails(SerializableException exception, string exceptionInfo, string? additionalInfo)
+        {
             StringBuilder exceptionDetails = new();
             if (!string.IsNullOrEmpty(exceptionInfo))
             {
                 // Tests may be run on VMs, and AppendLine() could be either \r\n or \n
                 exceptionDetails.Append('\n');
-                exceptionDetails.Append(exceptionInfo);
+                exceptionDetails.Append(exceptionInfo.Trim());
+                exceptionDetails.Append('\n');
                 exceptionDetails.Append('\n');
             }
 
             exceptionDetails.Append("```\n");
-            exceptionDetails.Append(exception.ToString());
+            exceptionDetails.Append(exception.ToString().Trim());
             exceptionDetails.Append('\n');
             exceptionDetails.Append("```\n");
             exceptionDetails.Append('\n');
@@ -45,9 +67,7 @@ namespace BugReporter
                 exceptionDetails.Append('\n');
             }
 
-            sb.Append($"&description={Uri.EscapeDataString(exceptionDetails.ToString())}");
-
-            return sb.ToString();
+            return exceptionDetails;
         }
     }
 }
