gitextensions/gitextensions

commit 8066c59a7b286f5b791d29cfee8c2209bb27b6db

PushLocalCmd: path must be absolute (#9678)

GitCommands/Git/Commands/GitCommandHelpers.cs
@@ -204,13 +204,14 @@ namespace GitCommands.Git.Commands
         /// </summary>
         /// <param name="gitRef">The branch to move.</param>
         /// <param name="targetId">The commit to move to.</param>
+        /// <param name="repoDir">Directory to the current repo.</param>
         /// <param name="force">Push the reference also if commits are lost.</param>
         /// <returns>The Git command to execute.</returns>
-        public static ArgumentString PushLocalCmd(string gitRef, ObjectId targetId, bool force = false)
+        public static ArgumentString PushLocalCmd(string gitRef, ObjectId targetId, string repoDir, bool force = false)
         {
             return new GitArgumentBuilder("push")
             {
-                ".",
+                $@"""file://{repoDir}""",
                 $"{targetId}:{gitRef}".QuoteNE(),
                 { force, "--force" }
             };

UnitTests/GitCommands.Tests/Git/Commands/GitCommandHelpersTest.cs
@@ -496,11 +496,11 @@ namespace GitCommandsTests.Git.Commands
                 () => GitCommandHelpers.ResetCmd(ResetMode.ResetIndex, commit: hash, file: "file.txt"));
         }
 
-        [TestCase("mybranch", false, ExpectedResult = @"push . ""1111111111111111111111111111111111111111:mybranch""")]
-        [TestCase("branch2", true, ExpectedResult = @"push . ""1111111111111111111111111111111111111111:branch2"" --force")]
-        public string PushLocalCmd(string gitRef, bool force)
+        [TestCase("mybranch", ".", false, ExpectedResult = @"push ""file://."" ""1111111111111111111111111111111111111111:mybranch""")]
+        [TestCase("branch2", "/my/path", true, ExpectedResult = @"push ""file:///my/path"" ""1111111111111111111111111111111111111111:branch2"" --force")]
+        public string PushLocalCmd(string gitRef, string repoDir, bool force)
         {
-            return GitCommandHelpers.PushLocalCmd(gitRef, ObjectId.WorkTreeId, force).Arguments;
+            return GitCommandHelpers.PushLocalCmd(gitRef, ObjectId.WorkTreeId, repoDir, force).Arguments;
         }
 
         [TestCase(ResetMode.ResetIndex, "tree-ish", null, @"reset ""tree-ish"" --")]

GitUI/HelperDialogs/FormResetAnotherBranch.cs
@@ -93,7 +93,7 @@ namespace GitUI.HelperDialogs
                 return;
             }
 
-            var command = GitCommandHelpers.PushLocalCmd(gitRefToReset.CompleteName, _revision.ObjectId, ForceReset.Checked);
+            var command = GitCommandHelpers.PushLocalCmd(gitRefToReset.CompleteName, _revision.ObjectId, Module.WorkingDir, ForceReset.Checked);
             bool success = FormProcess.ShowDialog(this, arguments: command, Module.WorkingDir, input: null, useDialogSettings: true);
             if (success)
             {

commit d08e763511fb53a343a53acde3a0265b480fbc81

Reset another branch to here path format
Regression from 8066c59a7b286f5b791d29cfee8c2209bb27b6db

GitCommands/Git/Commands/GitCommandHelpers.cs
@@ -211,7 +211,7 @@ namespace GitCommands.Git.Commands
         {
             return new GitArgumentBuilder("push")
             {
-                $@"""file://{repoDir}""",
+                $@"""file://{repoDir.ToPosixPath()}""",
                 $"{targetId}:{gitRef}".QuoteNE(),
                 { force, "--force" }
             };

UnitTests/GitCommands.Tests/Git/Commands/GitCommandHelpersTest.cs
@@ -498,6 +498,7 @@ namespace GitCommandsTests.Git.Commands
 
         [TestCase("mybranch", ".", false, ExpectedResult = @"push ""file://."" ""1111111111111111111111111111111111111111:mybranch""")]
         [TestCase("branch2", "/my/path", true, ExpectedResult = @"push ""file:///my/path"" ""1111111111111111111111111111111111111111:branch2"" --force")]
+        [TestCase("branchx", @"c:\my\path", true, ExpectedResult = @"push ""file://c:/my/path"" ""1111111111111111111111111111111111111111:branchx"" --force")]
         public string PushLocalCmd(string gitRef, string repoDir, bool force)
         {
             return GitCommandHelpers.PushLocalCmd(gitRef, ObjectId.WorkTreeId, repoDir, force).Arguments;