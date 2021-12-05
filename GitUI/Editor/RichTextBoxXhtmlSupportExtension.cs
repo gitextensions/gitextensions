33c494855bb87267da3d450a88eed4e646f3776d
Michael Seibt <mstv@gmx.net>
2021-12-05 19:46:44 +0100

    fixup! Let RTB extension GetPlainText ignore links
    Start comments with capitals

--- GitUI/Editor/RichTextBoxXhtmlSupportExtension.cs
+++ GitUI/Editor/RichTextBoxXhtmlSupportExtension.cs
@@ -1068,13 +1068,13 @@ namespace GitUI.Editor.RichTextBoxExtension
             try
             {
                 //--------------------------------
-                // this is an inefficient method to get text format
-                // but RichTextBox doesn't provide another method to
-                // get something like an array of charformat and paraformat
+                // This is an inefficient method to get text format.
+                // But RichTextBox doesn't provide another method to
+                // get something like an array of charformat and paraformat.
                 //--------------------------------
                 for (int i = from; i < to; i++)
                 {
-                    // try to select one visible character (RichTextBox returns a string, can be empty, may contain more than what is visible)
+                    // Try to select one visible character (RichTextBox returns a string, can be empty, may contain more than what is visible)
                     rtb.Select(i, 1);
                     string selectedText = rtb.SelectedText;
 
