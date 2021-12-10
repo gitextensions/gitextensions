gitextensions/gitextensions

commit 562aa41fff947436101b64f9193ecb62675493b2

BASE diff: Icons for unique changes (#9720)
If a diff has a common BASE commit show icons that
indicates how the files have changed in first (A) and
second (B) compared to BASE.

GitUI/UserControls/FileStatusDiffCalculator.cs
@@ -112,9 +112,6 @@ namespace GitUI
                 return fileStatusDescs;
             }
 
-            // Extra information with limited selection
-            var allAToB = fileStatusDescs[0].Statuses;
-
             // Get base commit, add as parent if unique
             var firstRevHead = GetRevisionOrHead(firstRev, headId);
             var selectedRevHead = GetRevisionOrHead(selectedRev, headId);
@@ -158,32 +155,45 @@ namespace GitUI
                 return fileStatusDescs;
             }
 
-            // Present common files in BASE->B, BASE->A separately
-            // For the following diff:  A->B a,c,d; BASE->B a,b,c; BASE->A a,b,d
-            // (the file a has unique changes, b has the same change and c,d is changed in one of the branches)
-            // The following groups will be shown: A->B a,c,d; BASE->B a,c; BASE->A a,d; Common BASE b
+            var allAToB = fileStatusDescs[0].Statuses;
             var allBaseToB = module.GetDiffFilesWithSubmodulesStatus(baseRevGuid, selectedRev.ObjectId, selectedRev.FirstParentId);
             var allBaseToA = module.GetDiffFilesWithSubmodulesStatus(baseRevGuid, firstRev.ObjectId, firstRev.FirstParentId);
 
             GitItemStatusNameEqualityComparer comparer = new();
-            var commonBaseToAandB = allBaseToB.Intersect(allBaseToA, comparer).Except(allAToB, comparer).ToList();
+            var sameBaseToAandB = allBaseToB.Intersect(allBaseToA, comparer).Except(allAToB, comparer).ToList();
+            var onlyA = allBaseToA.Except(allBaseToB, comparer).ToList();
+            var onlyB = allBaseToB.Except(allBaseToA, comparer).ToList();
+
+            foreach (var l in new[] { allAToB, allBaseToB, allBaseToA })
+            {
+                foreach (var f in l)
+                {
+                    f.DiffStatus = GetDiffStatus(f, l == allAToB);
+                }
+            }
+
+            DiffBranchStatus GetDiffStatus(GitItemStatus f, bool atoBDiff)
+            {
+                // Always show where the change is done
+                // This means that if a file is added in A it is shown as removed in the A->B diff,
+                // but marked with A
+                return sameBaseToAandB.Any(i => i.Name == f.Name) ? DiffBranchStatus.SameChange
+                    : onlyA.Any(i => i.Name == f.Name) ? DiffBranchStatus.OnlyAChange
+                    : onlyB.Any(i => i.Name == f.Name) ? DiffBranchStatus.OnlyBChange
+                    : DiffBranchStatus.UnequalChange;
+            }
 
             GitRevision revBase = new(baseRevGuid);
             fileStatusDescs.Add(new FileStatusWithDescription(
                 firstRev: revBase,
                 secondRev: selectedRev,
-                summary: TranslatedStrings.DiffBaseToB + GetDescriptionForRevision(selectedRev.ObjectId),
-                statuses: allBaseToB.Except(commonBaseToAandB, comparer).ToList()));
+                summary: $"{TranslatedStrings.DiffBaseWith} B {GetDescriptionForRevision(selectedRev.ObjectId)}",
+                statuses: allBaseToB));
             fileStatusDescs.Add(new FileStatusWithDescription(
                 firstRev: revBase,
                 secondRev: firstRev,
-                summary: TranslatedStrings.DiffBaseToB + GetDescriptionForRevision(firstRev.ObjectId),
-                statuses: allBaseToA.Except(commonBaseToAandB, comparer).ToList()));
-            fileStatusDescs.Add(new FileStatusWithDescription(
-                firstRev: revBase,
-                secondRev: selectedRev,
-                summary: TranslatedStrings.DiffCommonBase + GetDescriptionForRevision(baseRevGuid),
-                statuses: commonBaseToAandB));
+                summary: $"{TranslatedStrings.DiffBaseWith} A {GetDescriptionForRevision(firstRev.ObjectId)}",
+                statuses: allBaseToA));
 
             if (!module.GitVersion.SupportRangeDiffTool)
             {
@@ -198,7 +208,7 @@ namespace GitUI
 
             // first and selected has a common merge base and count must be available
             // Only a printout, so no Validates
-            var desc = $"{TranslatedStrings.DiffRange} {baseToFirstCount ?? -1}↓ {baseToSecondCount ?? -1}↑";
+            var desc = $"{TranslatedStrings.DiffRange} {baseToFirstCount ?? -1}↓ {baseToSecondCount ?? -1}↑ BASE {GetDescriptionForRevision(baseRevGuid)}";
 
             FileStatusWithDescription rangeDiff = new(
                 firstRev: first,

GitCommands/Git/DiffBranchStatus.cs
@@ -1,0 +1,13 @@
+﻿namespace GitCommands
+{
+    public enum DiffBranchStatus
+    {
+        Unknown = 0,
+        OnlyAChange,
+        OnlyBChange,
+        SameChange,
+
+        // Concurrent changes, different in first(A) and second(B)
+        UnequalChange
+    }
+}

GitCommands/Git/GitItemStatus.cs
@@ -60,6 +60,7 @@ namespace GitCommands
         public string? RenameCopyPercentage { get; set; }
 
         public StagedStatus Staged { get; set; }
+        public DiffBranchStatus DiffStatus { get; set; } = DiffBranchStatus.Unknown;
 
         #region Flags
 

GitUI/CommandsDialogs/RevisionDiffControl.cs
@@ -30,8 +30,8 @@ namespace GitUI.CommandsDialogs
             new("Are you sure you want to delete the selected file(s)?");
         private readonly TranslationString _deleteFailed = new("Delete file failed");
         private readonly TranslationString _multipleDescription = new("<multiple>");
-        private readonly TranslationString _selectedRevision = new("Second: b/");
-        private readonly TranslationString _firstRevision = new("First: a/");
+        private readonly TranslationString _selectedRevision = new("Second: B ");
+        private readonly TranslationString _firstRevision = new("First: A ");
 
         private readonly TranslationString _resetSelectedChangesText =
             new("Are you sure you want to reset all selected files to {0}?");

GitUI/Properties/Images.Designer.cs
@@ -863,9 +863,89 @@ namespace GitUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap FileStatusAddedOnlyA {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusAddedOnlyA", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusAddedOnlyB {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusAddedOnlyB", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusAddedSame {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusAddedSame", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusAddedUnequal {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusAddedUnequal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap FileStatusCopied {
             get {
                 object obj = ResourceManager.GetObject("FileStatusCopied", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusCopiedOnlyA {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusCopiedOnlyA", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusCopiedOnlyB {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusCopiedOnlyB", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusCopiedSame {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusCopiedSame", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusCopiedUnequal {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusCopiedUnequal", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -883,6 +963,46 @@ namespace GitUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap FileStatusModifiedOnlyA {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusModifiedOnlyA", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusModifiedOnlyB {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusModifiedOnlyB", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusModifiedSame {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusModifiedSame", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusModifiedUnequal {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusModifiedUnequal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap FileStatusRemoved {
             get {
                 object obj = ResourceManager.GetObject("FileStatusRemoved", resourceCulture);
@@ -893,9 +1013,89 @@ namespace GitUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap FileStatusRemovedOnlyA {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRemovedOnlyA", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusRemovedOnlyB {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRemovedOnlyB", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusRemovedSame {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRemovedSame", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusRemovedUnequal {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRemovedUnequal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap FileStatusRenamed {
             get {
                 object obj = ResourceManager.GetObject("FileStatusRenamed", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusRenamedOnlyA {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRenamedOnlyA", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusRenamedOnlyB {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRenamedOnlyB", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusRenamedSame {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRenamedSame", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap FileStatusRenamedUnequal {
+            get {
+                object obj = ResourceManager.GetObject("FileStatusRenamedUnequal", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }

GitUI/Properties/Images.resx
@@ -553,17 +553,77 @@
   <data name="FileStatusAdded" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\Icons\FileStatusAdded.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
+  <data name="FileStatusAddedOnlyA" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusAddedOnlyA.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusAddedOnlyB" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusAddedOnlyB.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusAddedSame" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusAddedSame.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusAddedUnequal" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusAddedUnequal.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
   <data name="FileStatusModified" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\Icons\FileStatusModified.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusModifiedOnlyA" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusModifiedOnlyA.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusModifiedOnlyB" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusModifiedOnlyB.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusModifiedSame" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusModifiedSame.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusModifiedUnequal" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusModifiedUnequal.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
   <data name="FileStatusRemoved" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\Icons\FileStatusRemoved.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
+  <data name="FileStatusRemovedOnlyA" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRemovedOnlyA.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusRemovedOnlyB" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRemovedOnlyB.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusRemovedSame" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRemovedSame.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusRemovedUnequal" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRemovedUnequal.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
   <data name="FileStatusCopied" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\Icons\FileStatusCopied.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
+  <data name="FileStatusCopiedOnlyA" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusCopiedOnlyA.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusCopiedOnlyB" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusCopiedOnlyB.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusCopiedSame" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusCopiedSame.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusCopiedUnequal" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusCopiedUnequal.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
   <data name="FileStatusRenamed" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\Icons\FileStatusRenamed.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusRenamedOnlyA" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRenamedOnlyA.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusRenamedOnlyB" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRenamedOnlyB.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusRenamedSame" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRenamedSame.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="FileStatusRenamedUnequal" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\Icons\FileStatusRenamedUnequal.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
   <data name="FileStatusUnknown" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\Icons\FileStatusUnknown.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>

commit 702b4d08f5350b39cf8bb4532b51f204b8d32c99

range-diff: Handle artificial commits
Regression from #9720/2119320c3401b59c8883f52b06a230a756fb4b18

GitCommands/Git/GitModule.cs
@@ -2391,12 +2391,13 @@ namespace GitCommands
             string extraDiffArguments,
             CancellationToken cancellationToken)
         {
-            if (firstId.IsArtificial
-                || secondId.IsArtificial
-                || (firstBase?.IsArtificial ?? false)
-                || (secondBase?.IsArtificial ?? false))
+            // range-diff is not possible for artificial commits, use HEAD
+            string first = firstId.IsArtificial ? "HEAD" : firstId.ToString();
+            string second = secondId.IsArtificial ? "HEAD" : secondId.ToString();
+
+            if ((firstBase?.IsArtificial is true) || (secondBase?.IsArtificial is true))
             {
-                throw new ArgumentException($"Tried to get range diff for artificial commit: {firstId} and file: {secondId}");
+                throw new ArgumentException($"Cannot get range diff for artificial commit base of A: {firstBase} or base of B: {secondBase}.");
             }
 
             // Supported since Git 2.19 (checks when adding the command)
@@ -2406,7 +2407,7 @@ namespace GitCommands
                 "--find-copies",
                 { AppSettings.UseHistogramDiffAlgorithm, "--histogram" },
                 extraDiffArguments,
-                { firstBase is null || secondBase is null,  $"{firstId}...{secondId}", $"{firstBase}..{firstId} {secondBase}..{secondId}" }
+                { firstBase is null || secondBase is null,  $"{first}...{second}", $"{firstBase}..{first} {secondBase}..{second}" }
             };
 
             ExecutionResult result = await _gitExecutable.ExecuteAsync(

GitUI/GitUIExtensions.cs
@@ -71,7 +71,7 @@ namespace GitUI
                     ? $"{firstId}...{item.SecondRevision.ObjectId}"
                     : $"{item.BaseA}..{firstId} {item.BaseB}..{item.SecondRevision.ObjectId}";
 
-                await fileViewer.ViewTextAsync("range-diff.sh", $"git range-diff {range}");
+                await fileViewer.ViewTextAsync("git-range-diff.sh", $"git range-diff {range}");
 
                 string output = await fileViewer.Module.GetRangeDiffAsync(
                         firstId,

GitUI/UserControls/FileStatusDiffCalculator.cs
@@ -216,8 +216,8 @@ namespace GitUI
                 secondRev: fileStatusDescs[0].SecondRev,
                 summary: fileStatusDescs[0].Summary,
                 statuses: allAToB,
-                baseA: fileStatusDescs[0].BaseA,
-                baseB: fileStatusDescs[0].BaseB);
+                baseA: baseA,
+                baseB: baseB);
 
             return fileStatusDescs;
 