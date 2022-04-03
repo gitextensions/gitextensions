bcd0c2617bdd4c842c9a707888f951af07c65ecd
Michael Seibt <36601201+mstv@users.noreply.github.com>
2022-04-03 11:35:58 +0200

    Increase contrast between graph colors (#9932)

--- GitUI/UserControls/RevisionGrid/Graph/RevisionGraphLaneColor.cs
+++ GitUI/UserControls/RevisionGrid/Graph/RevisionGraphLaneColor.cs
@@ -8,12 +8,11 @@ namespace GitUI.UserControls.RevisionGrid.Graph
     {
         internal static readonly IReadOnlyList<Color> PresetGraphColors = new[]
         {
-            Color.FromArgb(230, 36, 107), // red-pink
+            Color.FromArgb(240, 100, 160), // red-pink
             Color.FromArgb(120, 180, 230), // light blue
             Color.FromArgb(36, 194, 33), // green
-            Color.FromArgb(142, 108, 193), // light violet
-            Color.FromArgb(221, 76, 60), // red
-            Color.FromArgb(60, 120, 220), // dark blue
+            Color.FromArgb(160, 120, 240), // light violet
+            Color.FromArgb(221, 50, 40), // red
             Color.FromArgb(26, 198, 166), // cyan-green
             Color.FromArgb(231, 176, 15) // orange
         };
