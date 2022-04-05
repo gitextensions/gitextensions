bb8ad2538a6e8e7c8692d37c0f253876f6a98f45
Holger Schmidt <leroyz.mailaccount@gmx.net>
2022-04-05 02:28:53 +0200

    documented

--- GitUI/BranchTreePanel/NodeExtensions.cs
+++ GitUI/BranchTreePanel/NodeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace GitUI.BranchTreePanel
 {
@@ -17,9 +18,15 @@ namespace GitUI.BranchTreePanel
         internal static IEnumerable<RepoObjectsTree.NodeBase> HavingChildren(this IEnumerable<RepoObjectsTree.NodeBase> nodes)
             => nodes.Where(node => node.HasChildren());
 
+        /// <summary>Filters <paramref name="nodes"/> for those that are not <see cref="TreeNode.IsExpanded"/>.
+        /// This only makes sense on nodes <see cref="HavingChildren(IEnumerable{RepoObjectsTree.NodeBase})"/>,
+        /// so you'll probably want to use that filter first.</summary>
         internal static IEnumerable<RepoObjectsTree.NodeBase> Expandable(this IEnumerable<RepoObjectsTree.NodeBase> nodes)
             => nodes.Where(node => !node.TreeViewNode.IsExpanded);
 
+        /// <summary>Filters <paramref name="nodes"/> for those that are <see cref="TreeNode.IsExpanded"/>.
+        /// This only makes sense on nodes <see cref="HavingChildren(IEnumerable{RepoObjectsTree.NodeBase})"/>,
+        /// so you'll probably want to use that filter first.</summary>
         internal static IEnumerable<RepoObjectsTree.NodeBase> Collapsible(this IEnumerable<RepoObjectsTree.NodeBase> nodes)
             => nodes.Where(node => node.TreeViewNode.IsExpanded);
     }
