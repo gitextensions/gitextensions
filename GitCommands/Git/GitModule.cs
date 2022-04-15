cb525f94bd026086fd1bff766d5a0e17590af04f
Gerhard Olsson <6248932+gerhardol@users.noreply.github.com>
2022-04-15 23:43:25 +0200

    git-fetch --jobs==0 if no configuration (#9956)
    Override the Git default of one parallel job.
    
    If the user has set fetch.parallel or submodule.fetchJobs the
    the user config value is set (or git default if just one is set).

--- GitCommands/Git/GitModule.cs
+++ GitCommands/Git/GitModule.cs
@@ -1522,9 +1522,13 @@ namespace GitCommands
                 }
             }
 
+            bool jobsConfig = string.IsNullOrWhiteSpace(EffectiveConfigFile.GetValue("fetch.parallel"))
+                && string.IsNullOrWhiteSpace(EffectiveConfigFile.GetValue("submodule.fetchJobs"));
+
             // TODO return ArgumentBuilder and add special case ArgumentBuilder.Add(ArgumentBuilder childBuilder)
             return new ArgumentBuilder
             {
+                { jobsConfig, "--jobs=0" },
                 remote.ToPosixPath()?.Trim().Quote(),
                 branchArguments,
                 { fetchTags == true, "--tags" },
