gitextensions/gitextensions

commit 3cf9009f8279452f7e6d7f0694e8dabf0c3c108f

Set branchname in repo menu async (#10124)
Evaluate the branch names for favorite/recent repo branch names async
(as I/O is required) instead of delaying the opening of the menu and dashboard.

As the I/O is done in parallel, it normally even complete when the user see the menu.
(CPU load spike is hardly noticeable.)

GitUI/CommandsDialogs/BrowseDialog/DashboardControl/UserRepositoriesList.cs
@@ -14,7 +14,9 @@ using GitExtUtils.GitUI;
 using GitUI.Properties;
 using GitUI.UserControls;
 using Microsoft;
+using Microsoft.VisualStudio.Threading;
 using ResourceManager;
+using static System.Windows.Forms.ListViewItem;
 
 namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
 {
@@ -327,7 +329,7 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
 
                 for (var index = 0; index < repos.Count; index++)
                 {
-                    listView1.Items.Add(new ListViewItem(repos[index].Caption)
+                    ListViewItem item = new(repos[index].Caption)
                     {
                         ForeColor = ForeColor,
                         Font = AppSettings.Font,
@@ -335,15 +337,18 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
                         ImageIndex = repoValidityArray[index] ? 1 : 0,
                         UseItemStyleForSubItems = false,
                         Tag = repos[index].Repo,
-                        ToolTipText = repos[index].Repo.Path,
-                        SubItems =
-                        {
-                            {
-                                _controller.GetCurrentBranchName(repos[index].Repo.Path), BranchNameColor, BackColor, _secondaryFont
-                            },
-                            //// NB: we can add a 3rd row as well: { repository.Repo.Category, SystemColors.GrayText, BackColor, _secondaryFont }
-                        }
-                    });
+                        ToolTipText = repos[index].Repo.Path
+                    };
+                    listView1.Items.Add(item);
+
+                    ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                    {
+                        await TaskScheduler.Default;
+                        string branchName = _controller.GetCurrentBranchName(repos[index].Repo.Path);
+                        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                        item.SubItems.Add(new ListViewSubItem(item, branchName, BranchNameColor, BackColor, _secondaryFont));
+                        //// NB: we can add a 3rd row as well: { repository.Repo.Category, SystemColors.GrayText, BackColor, _secondaryFont }
+                    }).FileAndForget();
                 }
             }
         }

GitUI/CommandsDialogs/FormBrowseController.cs
@@ -6,6 +6,7 @@ using GitCommands.Git;
 using GitCommands.Gpg;
 using GitCommands.UserRepositoryHistory;
 using GitUIPluginInterfaces;
+using Microsoft.VisualStudio.Threading;
 
 namespace GitUI.CommandsDialogs
 {
@@ -39,11 +40,9 @@ namespace GitUI.CommandsDialogs
                                           string? caption,
                                           Action<object, GitModuleEventArgs> setGitModule)
         {
-            string branchName = _repositoryCurrentBranchNameProvider.GetCurrentBranchName(repo.Path);
             ToolStripMenuItem item = new(caption)
             {
-                DisplayStyle = ToolStripItemDisplayStyle.ImageAndText,
-                ShortcutKeyDisplayString = branchName
+                DisplayStyle = ToolStripItemDisplayStyle.ImageAndText
             };
 
             menuItemContainer.DropDownItems.Add(item);
@@ -57,6 +56,14 @@ namespace GitUI.CommandsDialogs
             {
                 item.ToolTipText = repo.Path;
             }
+
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                await TaskScheduler.Default;
+                string branchName = _repositoryCurrentBranchNameProvider.GetCurrentBranchName(repo.Path);
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                item.ShortcutKeyDisplayString = branchName;
+            }).FileAndForget();
         }
 
         public async Task<GpgInfo?> LoadGpgInfoAsync(GitRevision? revision)

UnitTests/GitUI.Tests/CommandsDialogs/FormBrowseControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.Threading;
+using System.Windows.Forms;
+using CommonTestUtils;
 using FluentAssertions;
 using GitCommands.Gpg;
 using GitCommands.UserRepositoryHistory;
@@ -9,6 +11,7 @@ using NUnit.Framework;
 
 namespace GitUITests.CommandsDialogs
 {
+    [Apartment(ApartmentState.STA)]
     [TestFixture]
     public sealed class FormBrowseControllerTests
     {
@@ -73,6 +76,9 @@ namespace GitUITests.CommandsDialogs
             Repository repository = new(path);
 
             _controller.AddRecentRepositories(containerMenu, repository, caption, (s, e) => { });
+
+            // await adding branch name in ShortcutKeyDisplayString, done async
+            ThreadHelper.JoinableTaskContext.Factory.Run(() => ThreadHelper.JoinPendingOperationsAsync(default));
 
             ToolStripMenuItem item = (ToolStripMenuItem)containerMenu.DropDownItems[0];
             item.ShortcutKeyDisplayString.Should().Be(branch);

commit faa9b611b54c5defb459f2274bd9d5197e231e37

Dashboard: Capture variable in RunAsync
Regression from 3cf9009f8279452f7e6d7f0694e8dabf0c3c108f

GitUI/CommandsDialogs/BrowseDialog/DashboardControl/UserRepositoriesList.cs
@@ -329,22 +329,23 @@ namespace GitUI.CommandsDialogs.BrowseDialog.DashboardControl
 
                 for (var index = 0; index < repos.Count; index++)
                 {
-                    ListViewItem item = new(repos[index].Caption)
+                    RecentRepoInfo recent = repos[index];
+                    ListViewItem item = new(recent.Caption)
                     {
                         ForeColor = ForeColor,
                         Font = AppSettings.Font,
-                        Group = isFavourite ? GetTileGroup(repos[index].Repo) : _lvgRecentRepositories,
+                        Group = isFavourite ? GetTileGroup(recent.Repo) : _lvgRecentRepositories,
                         ImageIndex = repoValidityArray[index] ? 1 : 0,
                         UseItemStyleForSubItems = false,
-                        Tag = repos[index].Repo,
-                        ToolTipText = repos[index].Repo.Path
+                        Tag = recent.Repo,
+                        ToolTipText = recent.Repo.Path
                     };
                     listView1.Items.Add(item);
 
                     ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
                     {
                         await TaskScheduler.Default;
-                        string branchName = _controller.GetCurrentBranchName(repos[index].Repo.Path);
+                        string branchName = _controller.GetCurrentBranchName(recent.Repo.Path);
                         await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
                         item.SubItems.Add(new ListViewSubItem(item, branchName, BranchNameColor, BackColor, _secondaryFont));
                         //// NB: we can add a 3rd row as well: { repository.Repo.Category, SystemColors.GrayText, BackColor, _secondaryFont }