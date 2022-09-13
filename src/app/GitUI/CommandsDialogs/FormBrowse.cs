356893f5205fc676b95dff379ef46c6f38af339d
Philippe Miossec <pmiossec@gmail.com>
2022-09-13 10:53:08 +0200

    FormBrowse: transform Settings button in split button for easy access of other settings
    Co-authored-by: Michael Seibt <36601201+mstv@users.noreply.github.com>

--- src/app/GitUI/CommandsDialogs/FormBrowse.cs
+++ src/app/GitUI/CommandsDialogs/FormBrowse.cs
@@ -21,6 +21,7 @@ using GitExtUtils.GitUI.Theming;
 using GitUI.Avatars;
 using GitUI.CommandsDialogs.BrowseDialog;
 using GitUI.CommandsDialogs.BrowseDialog.DashboardControl;
+using GitUI.CommandsDialogs.SettingsDialog;
 using GitUI.CommandsDialogs.WorktreeDialog;
 using GitUI.HelperDialogs;
 using GitUI.Infrastructure.Telemetry;
@@ -1476,10 +1477,16 @@ namespace GitUI.CommandsDialogs
 
         private void OnShowSettingsClick(object sender, EventArgs e)
         {
+            // Open settings on the last opened page
+            OpenSettings();
+        }
+
+        private void OpenSettings(SettingsPageReference? initialPage = null)
+        {
             string translation = AppSettings.Translation;
             CommitInfoPosition commitInfoPosition = AppSettings.CommitInfoPosition;
 
-            UICommands.StartSettingsDialog(this);
+            UICommands.StartSettingsDialog(this, initialPage);
 
             HandleSettingsChanged(translation, commitInfoPosition);
         }
@@ -1526,6 +1533,21 @@ namespace GitUI.CommandsDialogs
             _gitStatusMonitor.Active = NeedsGitStatusMonitor() && Module.IsValidGitWorkingDir();
 
             RefreshDefaultPullAction();
+        }
+
+        private void GitextSettingsToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            OpenSettings(SettingsDialog.Pages.GeneralSettingsPage.GetPageReference());
+        }
+
+        private void gitSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UICommands.StartRepoSettingsDialog(this);
+        }
+
+        private void pluginsSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UICommands.StartPluginSettingsDialog(this);
         }
 
         private void TagToolStripMenuItemClick(object sender, EventArgs e)
@@ -2121,7 +2143,7 @@ namespace GitUI.CommandsDialogs
                 case Command.OpenWithDifftool: OpenWithDifftool(); break;
                 case Command.OpenWithDifftoolFirstToLocal: OpenWithDifftoolFirstToLocal(); break;
                 case Command.OpenWithDifftoolSelectedToLocal: OpenWithDifftoolSelectedToLocal(); break;
-                case Command.OpenSettings: EditSettings.PerformClick(); break;
+                case Command.OpenSettings: OpenSettings(); break;
                 case Command.ToggleLeftPanel: toggleLeftPanel.PerformClick(); break;
                 case Command.EditFile: EditFile(); break;
                 case Command.OpenAsTempFile when fileTree.Visible: fileTree.ExecuteCommand(RevisionFileTreeControl.Command.OpenAsTempFile); break;
