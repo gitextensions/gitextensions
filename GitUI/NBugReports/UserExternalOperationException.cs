680f277af59715435f9efe7fd2ce4a9388b7fa29
Michael Seibt <mstv@gmx.net>
2022-11-13 17:07:23 +0100

    Wrap exceptions with UserExternalOperationException (#10421)
    instead of manually showing a MessageBox

--- GitUI/NBugReports/UserExternalOperationException.cs
+++ GitUI/NBugReports/UserExternalOperationException.cs
@@ -8,7 +8,7 @@ namespace GitUI.NBugReports
     public class UserExternalOperationException : ExternalOperationException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="UserExternalOperationException"/> class with a specified parameters
+        /// Initializes a new instance of the <see cref="UserExternalOperationException"/> class with the specified context information
         /// and a reference to the inner exception that is the cause of this exception.
         /// </summary>
         /// <param name="context">The command that led to the exception.</param>
@@ -19,6 +19,16 @@ namespace GitUI.NBugReports
             Context = context;
         }
 
-        public string Context { get; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserExternalOperationException"/> class with
+        /// a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        public UserExternalOperationException(Exception innerException)
+            : base(innerException: innerException)
+        {
+        }
+
+        public string? Context { get; }
     }
 }
