fc98ce2cf3cf762c9326dba87956b5a36d851006
Michael Seibt <mstv@gmx.net>
2022-11-13 14:37:27 +0100

    Add Un/UnregisterButton to ShellExtensionSettingsPage (#10421)

--- GitUI/CommandsDialogs/SettingsDialog/Pages/ShellExtensionSettingsPage.Designer.cs
+++ GitUI/CommandsDialogs/SettingsDialog/Pages/ShellExtensionSettingsPage.Designer.cs
@@ -35,12 +35,18 @@
             this._NO_TRANSLATE_chlMenuEntries = new System.Windows.Forms.CheckedListBox();
             this.cbAlwaysShowAllCommands = new System.Windows.Forms.CheckBox();
             this.tlpnlMain = new System.Windows.Forms.TableLayoutPanel();
-            this.gbShellExtensions = new System.Windows.Forms.GroupBox();
+            this.gbExplorerIntegration = new System.Windows.Forms.GroupBox();
+            this.flpnlExplorerIntegration = new System.Windows.Forms.FlowLayoutPanel();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.UnregisterButton = new System.Windows.Forms.Button();
+            this.gbCascadingMenu = new System.Windows.Forms.GroupBox();
             this.tlpnlCascadingMenu = new System.Windows.Forms.TableLayoutPanel();
             panel1 = new System.Windows.Forms.Panel();
             panel1.SuspendLayout();
             this.tlpnlMain.SuspendLayout();
-            this.gbShellExtensions.SuspendLayout();
+            this.gbExplorerIntegration.SuspendLayout();
+            this.flpnlExplorerIntegration.SuspendLayout();
+            this.gbCascadingMenu.SuspendLayout();
             this.tlpnlCascadingMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,20 +56,20 @@
             panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             panel1.Controls.Add(this.labelPreview);
             panel1.Controls.Add(this.label1);
-            panel1.Location = new System.Drawing.Point(323, 77);
             panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Location = new System.Drawing.Point(746, 77);
             panel1.Margin = new System.Windows.Forms.Padding(2);
             panel1.Name = "panel1";
             panel1.Padding = new System.Windows.Forms.Padding(3);
-            panel1.Size = new System.Drawing.Size(317, 291);
+            panel1.Size = new System.Drawing.Size(741, 484);
             panel1.TabIndex = 0;
             // 
             // labelPreview
             // 
             this.labelPreview.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelPreview.Location = new System.Drawing.Point(0, 15);
+            this.labelPreview.Location = new System.Drawing.Point(3, 18);
             this.labelPreview.Name = "labelPreview";
-            this.labelPreview.Size = new System.Drawing.Size(317, 196);
+            this.labelPreview.Size = new System.Drawing.Size(735, 463);
             this.labelPreview.TabIndex = 1;
             this.labelPreview.Text = "...";
             // 
@@ -71,9 +77,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(3, 3);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 15);
+            this.label1.Size = new System.Drawing.Size(130, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Context menu preview:";
             // 
@@ -84,12 +90,12 @@
             this.lblMenuEntries.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblMenuEntries.Location = new System.Drawing.Point(3, 45);
             this.lblMenuEntries.Name = "lblMenuEntries";
-            this.lblMenuEntries.Size = new System.Drawing.Size(636, 30);
-            this.lblMenuEntries.TabIndex = 2;
+            this.lblMenuEntries.Size = new System.Drawing.Size(1483, 30);
+            this.lblMenuEntries.TabIndex = 4;
             this.lblMenuEntries.Text = "Select items to be shown in the cascaded context menu.\r\n(Unchecked items will be " +
     "shown top level for direct access.)";
             // 
-            // chlMenuEntries
+            // _NO_TRANSLATE_chlMenuEntries
             // 
             this._NO_TRANSLATE_chlMenuEntries.CheckOnClick = true;
             this._NO_TRANSLATE_chlMenuEntries.FormattingEnabled = true;
@@ -114,8 +120,8 @@
             "View changes"});
             this._NO_TRANSLATE_chlMenuEntries.Location = new System.Drawing.Point(3, 78);
             this._NO_TRANSLATE_chlMenuEntries.Name = "_NO_TRANSLATE_chlMenuEntries";
-            this._NO_TRANSLATE_chlMenuEntries.Size = new System.Drawing.Size(315, 110);
-            this._NO_TRANSLATE_chlMenuEntries.TabIndex = 3;
+            this._NO_TRANSLATE_chlMenuEntries.Size = new System.Drawing.Size(315, 94);
+            this._NO_TRANSLATE_chlMenuEntries.TabIndex = 5;
             this._NO_TRANSLATE_chlMenuEntries.SelectedValueChanged += new System.EventHandler(this.chlMenuEntries_SelectedValueChanged);
             // 
             // cbAlwaysShowAllCommands
@@ -125,8 +131,8 @@
             this.cbAlwaysShowAllCommands.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cbAlwaysShowAllCommands.Location = new System.Drawing.Point(3, 3);
             this.cbAlwaysShowAllCommands.Name = "cbAlwaysShowAllCommands";
-            this.cbAlwaysShowAllCommands.Size = new System.Drawing.Size(636, 19);
-            this.cbAlwaysShowAllCommands.TabIndex = 1;
+            this.cbAlwaysShowAllCommands.Size = new System.Drawing.Size(1483, 19);
+            this.cbAlwaysShowAllCommands.TabIndex = 3;
             this.cbAlwaysShowAllCommands.Text = "Always show all commands";
             this.cbAlwaysShowAllCommands.UseVisualStyleBackColor = true;
             // 
@@ -136,29 +142,78 @@
             this.tlpnlMain.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tlpnlMain.ColumnCount = 1;
             this.tlpnlMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpnlMain.Controls.Add(this.gbShellExtensions, 0, 0);
+            this.tlpnlMain.Controls.Add(this.gbExplorerIntegration, 0, 0);
+            this.tlpnlMain.Controls.Add(this.gbCascadingMenu, 0, 1);
             this.tlpnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpnlMain.Location = new System.Drawing.Point(8, 8);
             this.tlpnlMain.Name = "tlpnlMain";
             this.tlpnlMain.RowCount = 1;
             this.tlpnlMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tlpnlMain.Size = new System.Drawing.Size(664, 508);
             this.tlpnlMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 508F));
+            this.tlpnlMain.Size = new System.Drawing.Size(1511, 670);
             this.tlpnlMain.TabIndex = 0;
             // 
-            // gbShellExtensions
+            // gbExplorerIntegration
             // 
-            this.gbShellExtensions.AutoSize = true;
-            this.gbShellExtensions.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.gbShellExtensions.Controls.Add(this.tlpnlCascadingMenu);
-            this.gbShellExtensions.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gbShellExtensions.Location = new System.Drawing.Point(3, 3);
-            this.gbShellExtensions.Name = "gbShellExtensions";
-            this.gbShellExtensions.Padding = new System.Windows.Forms.Padding(8);
-            this.gbShellExtensions.Size = new System.Drawing.Size(658, 502);
-            this.gbShellExtensions.TabIndex = 0;
-            this.gbShellExtensions.TabStop = false;
-            this.gbShellExtensions.Text = "Cascaded Context Menu";
+            this.gbExplorerIntegration.AutoSize = true;
+            this.gbExplorerIntegration.Controls.Add(this.flpnlExplorerIntegration);
+            this.gbExplorerIntegration.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbExplorerIntegration.Location = new System.Drawing.Point(3, 3);
+            this.gbExplorerIntegration.Name = "gbExplorerIntegration";
+            this.gbExplorerIntegration.Padding = new System.Windows.Forms.Padding(8);
+            this.gbExplorerIntegration.Size = new System.Drawing.Size(1505, 63);
+            this.gbExplorerIntegration.TabIndex = 0;
+            this.gbExplorerIntegration.TabStop = false;
+            this.gbExplorerIntegration.Text = "Windows Explorer integration";
+            // 
+            // flpnlExplorerIntegration
+            // 
+            this.flpnlExplorerIntegration.AutoSize = true;
+            this.flpnlExplorerIntegration.Controls.Add(this.RegisterButton);
+            this.flpnlExplorerIntegration.Controls.Add(this.UnregisterButton);
+            this.flpnlExplorerIntegration.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpnlExplorerIntegration.Location = new System.Drawing.Point(8, 24);
+            this.flpnlExplorerIntegration.Name = "flpnlExplorerIntegration";
+            this.flpnlExplorerIntegration.Size = new System.Drawing.Size(1489, 31);
+            this.flpnlExplorerIntegration.TabIndex = 0;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.AutoSize = true;
+            this.RegisterButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.RegisterButton.Location = new System.Drawing.Point(3, 3);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(133, 25);
+            this.RegisterButton.TabIndex = 1;
+            this.RegisterButton.Text = "&Enable shell extension";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // UnregisterButton
+            // 
+            this.UnregisterButton.AutoSize = true;
+            this.UnregisterButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.UnregisterButton.Location = new System.Drawing.Point(142, 3);
+            this.UnregisterButton.Name = "UnregisterButton";
+            this.UnregisterButton.Size = new System.Drawing.Size(136, 25);
+            this.UnregisterButton.TabIndex = 2;
+            this.UnregisterButton.Text = "&Disable shell extension";
+            this.UnregisterButton.UseVisualStyleBackColor = true;
+            this.UnregisterButton.Click += new System.EventHandler(this.UnregisterButton_Click);
+            // 
+            // gbCascadingMenu
+            // 
+            this.gbCascadingMenu.AutoSize = true;
+            this.gbCascadingMenu.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gbCascadingMenu.Controls.Add(this.tlpnlCascadingMenu);
+            this.gbCascadingMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbCascadingMenu.Location = new System.Drawing.Point(3, 72);
+            this.gbCascadingMenu.Name = "gbCascadingMenu";
+            this.gbCascadingMenu.Padding = new System.Windows.Forms.Padding(8);
+            this.gbCascadingMenu.Size = new System.Drawing.Size(1505, 595);
+            this.gbCascadingMenu.TabIndex = 0;
+            this.gbCascadingMenu.TabStop = false;
+            this.gbCascadingMenu.Text = "Cascaded Context Menu";
             // 
             // tlpnlCascadingMenu
             // 
@@ -179,7 +234,7 @@
             this.tlpnlCascadingMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlpnlCascadingMenu.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tlpnlCascadingMenu.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tlpnlCascadingMenu.Size = new System.Drawing.Size(642, 470);
+            this.tlpnlCascadingMenu.Size = new System.Drawing.Size(1489, 563);
             this.tlpnlCascadingMenu.TabIndex = 0;
             // 
             // ShellExtensionSettingsPage
@@ -189,13 +244,17 @@
             this.Controls.Add(this.tlpnlMain);
             this.Name = "ShellExtensionSettingsPage";
             this.Padding = new System.Windows.Forms.Padding(8);
-            this.Size = new System.Drawing.Size(680, 524);
+            this.Size = new System.Drawing.Size(1527, 686);
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             this.tlpnlMain.ResumeLayout(false);
             this.tlpnlMain.PerformLayout();
-            this.gbShellExtensions.ResumeLayout(false);
-            this.gbShellExtensions.PerformLayout();
+            this.gbExplorerIntegration.ResumeLayout(false);
+            this.gbExplorerIntegration.PerformLayout();
+            this.flpnlExplorerIntegration.ResumeLayout(false);
+            this.flpnlExplorerIntegration.PerformLayout();
+            this.gbCascadingMenu.ResumeLayout(false);
+            this.gbCascadingMenu.PerformLayout();
             this.tlpnlCascadingMenu.ResumeLayout(false);
             this.tlpnlCascadingMenu.PerformLayout();
             this.ResumeLayout(false);
@@ -210,8 +269,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelPreview;
         private System.Windows.Forms.CheckBox cbAlwaysShowAllCommands;
+        private System.Windows.Forms.FlowLayoutPanel flpnlExplorerIntegration;
         private System.Windows.Forms.TableLayoutPanel tlpnlCascadingMenu;
         private System.Windows.Forms.TableLayoutPanel tlpnlMain;
-        private System.Windows.Forms.GroupBox gbShellExtensions;
+        private System.Windows.Forms.GroupBox gbCascadingMenu;
+        private System.Windows.Forms.GroupBox gbExplorerIntegration;
+        private Button RegisterButton;
+        private Button UnregisterButton;
     }
 }
