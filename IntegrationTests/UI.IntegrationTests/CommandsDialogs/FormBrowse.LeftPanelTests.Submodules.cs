fe1d5b2d56d634f5d265520fc7eb14e623e09cb4
Igor Velikorossov <russkie@gmail.com>
2022-11-13 22:27:41 +1100

    Disable failing tests after git v2.38.1 disabled file protocol
    Relates to #10387

--- IntegrationTests/UI.IntegrationTests/CommandsDialogs/FormBrowse.LeftPanelTests.Submodules.cs
+++ IntegrationTests/UI.IntegrationTests/CommandsDialogs/FormBrowse.LeftPanelTests.Submodules.cs
@@ -94,6 +94,7 @@ namespace GitExtensions.UITests.CommandsDialogs
             _repo3.Dispose();
         }
 
+        [Ignore("See https://github.com/gitextensions/gitextensions/issues/10387")]
         [Test]
         public void RepoObjectTree_should_show_all_submodules()
         {
