cbc4f6848d69541827589dae493521335f0a780c
Philippe Miossec <pmiossec@gmail.com>
2023-01-23 15:35:07 +0100

    Left panel contextual menu: improve menu items labels and tooltips
    * Different menu entries depending on the type handled (local, remote and tag)
    * Other tooltip fixes around "Fetch & ..." menu items
    
    Fixes #10670

--- GitUI/BranchTreePanel/ContextMenu/MenuItemsStrings.cs
+++ GitUI/BranchTreePanel/ContextMenu/MenuItemsStrings.cs
@@ -1,5 +1,6 @@
 ﻿using GitCommands;
 using GitUI.BranchTreePanel.Interfaces;
+using ICSharpCode.TextEditor.Actions;
 using ResourceManager;
 
 namespace GitUI.BranchTreePanel.ContextMenu
@@ -9,17 +10,17 @@ namespace GitUI.BranchTreePanel.ContextMenu
         // the name of the members act as keys for the generated entries in the translation files (xlf)
 
         /// <see cref="IGitRefActions"/>
-        internal readonly TranslationString Checkout = new("Chec&kout branch...");
-        internal readonly TranslationString Merge = new("&Merge branches...");
-        internal readonly TranslationString Rebase = new("&Rebase...");
+        internal TranslationString Checkout;
+        internal TranslationString Rebase;
+        internal readonly TranslationString Merge = new("&Merge into current branch...");
         internal readonly TranslationString CreateBranch = new("Create &branch...");
         internal readonly TranslationString Reset = new("Re&set current branch to here...");
 
         /// <see cref="ICanRename"/>
-        internal readonly TranslationString Rename = new("R&ename branch...");
+        internal TranslationString Rename = new("R&ename branch...");
 
         /// <see cref="ICanDelete"/>
-        internal readonly TranslationString Delete = new("&Delete branch...");
+        internal TranslationString Delete;
 
         internal Dictionary<MenuItemKey, TranslationString> Tooltips { get; } = new Dictionary<MenuItemKey, TranslationString>();
 
@@ -31,10 +32,14 @@ namespace GitUI.BranchTreePanel.ContextMenu
 
     public class BranchMenuItemsStrings : Translate
     {
+        internal readonly TranslationString Checkout = new("Chec&kout branch...");
+        internal readonly TranslationString Rebase = new("&Rebase current branch on this branch...");
+        internal readonly TranslationString Delete = new("&Delete branch...");
+
         internal readonly TranslationString CheckoutTooltip = new("Checkout this branch");
         internal readonly TranslationString MergeTooltip = new("Merge this branch into current branch");
         internal readonly TranslationString CreateTooltip = new("Create a local branch from this branch");
-        internal readonly TranslationString RebaseTooltip = new("Rebase current branch to this branch");
+        internal readonly TranslationString RebaseTooltip = new("Rebase current branch on this branch");
         internal readonly TranslationString ResetTooltip = new("Reset current branch to here");
         internal readonly TranslationString RenameTooltip = new("Rename this branch");
 
@@ -45,6 +50,10 @@ namespace GitUI.BranchTreePanel.ContextMenu
 
         public void ApplyTo(MenuItemsStrings strings)
         {
+            strings.Checkout = Checkout;
+            strings.Rebase = Rebase;
+            strings.Delete = Delete;
+
             strings.Tooltips[MenuItemKey.GitRefCheckout] = CheckoutTooltip;
             strings.Tooltips[MenuItemKey.GitRefCreateBranch] = CreateTooltip;
             strings.Tooltips[MenuItemKey.GitRefMerge] = MergeTooltip;
