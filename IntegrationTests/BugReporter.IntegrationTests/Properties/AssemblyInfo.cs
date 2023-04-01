4b78d7f26a88ae86497e85b421ea3626cd5e6836
Philippe Miossec <pmiossec@gmail.com>
2023-04-01 14:15:00 +0200

    Tests: Add a trait to all the tests ('Unit' or 'Integration')
    to be able to run them more easily in VS Test Explorer

--- IntegrationTests/BugReporter.IntegrationTests/Properties/AssemblyInfo.cs
+++ IntegrationTests/BugReporter.IntegrationTests/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using ApprovalTests.Reporters.ContinuousIntegration;
 using ApprovalTests.Reporters.TestFrameworks;
 using CommonTestUtils;
 
+[assembly: NUnit.Framework.Category("IntegrationTests")]
 [assembly: ConfigureJoinableTaskFactory]
 [assembly: TestAppSettings]
 [assembly: UseReporter(typeof(NUnitReporter), typeof(AppVeyorReporter), typeof(DiffReporter))]
