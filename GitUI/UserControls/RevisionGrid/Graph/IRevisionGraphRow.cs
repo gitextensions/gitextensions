104eeac824c668f67a29530bed69a7bb988ad894
Michael Seibt <mstv@gmx.net>
2023-04-06 07:01:56 +0200

    Refactor RevisionGraphRow.BuildSegmentLanes

---
+++ GitUI/UserControls/RevisionGrid/Graph/IRevisionGraphRow.cs
@@ -0,0 +1,13 @@
+﻿namespace GitUI.UserControls.RevisionGrid.Graph
+{
+    public interface IRevisionGraphRow
+    {
+        RevisionGraphRevision Revision { get; }
+        IReadOnlyList<RevisionGraphSegment> Segments { get; }
+        int GetCurrentRevisionLane();
+        int GetLaneCount();
+        IEnumerable<RevisionGraphSegment> GetSegmentsForIndex(int index);
+        int GetLaneIndexForSegment(RevisionGraphSegment revisionGraphRevision);
+        void MoveLanesRight(int fromLane);
+    }
+}
