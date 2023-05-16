8d68c03cbf0c860d032cbe8cc0dddce8eb49019a
Philippe Miossec <pmiossec@gmail.com>
2023-05-16 15:09:55 +0200

    Blame: better estimate line number for "blame previous commit"
    by taking into account changes made in the selected blamed commit
    and using the diff content to get the offset that should be applied
    
    Good example to test the behavior:
    
    Select commit 7edc265403b54dd8ea2b8402f8790219d8d42077 and blame FormBrowse.cs
    then blame "previous revision" on line 2281
    ( introduced by the commit 52476f30670ba5338756b606841fb0a346fd6460
     that adds a lot of lines --89-- before this line 2281 )
    
    Before: you stay on line 2281, so far away of previous code analyzed
    After: you end up 89 lines before, on line 2192, next to the previous code analyzed

---
+++ IntegrationTests/UI.IntegrationTests/UserControls/GitBlameParserTest.cs
@@ -0,0 +1,29 @@
+﻿using GitCommands;
+using GitUI.UserControls;
+using GitUIPluginInterfaces;
+
+namespace GitUITests.UserControls;
+
+public sealed class GitBlameParserTest
+{
+    [Test]
+    public void GetOriginalLineInPreviousCommit()
+    {
+        // Simulate "Blame previous revision" on line 2281
+        // when blaming file FormBrowse.cs on commit 7edc265403b54dd8ea2b8402f8790219d8d42077
+        // line content is: internal bool ExecuteCommand(Command cmd)
+
+        string gitExtensionsRepoPath = Path.GetFullPath(@"..\..\..\..\..\..\..");
+        Assert.IsTrue(gitExtensionsRepoPath.EndsWith("gitextensions"));
+
+        GitModule gitModule = new(gitExtensionsRepoPath);
+
+        // Revision corresponding to line 2281
+        GitRevision selectedBlamedRevision = gitModule.GetRevision(ObjectId.Parse("52476f30670ba5338756b606841fb0a346fd6460"));
+
+        int matchingBlameLineNumber = new GitBlameParser(() => gitModule)
+            .GetOriginalLineInPreviousCommit(selectedBlamedRevision, "GitUI/CommandsDialogs/FormBrowse.cs", 2281);
+
+        Assert.AreEqual(2192, matchingBlameLineNumber); // line content is still: internal bool ExecuteCommand(Command cmd)
+    }
+}
