229e5e604c0a19dad30ff407d1ba8fbb9ce29415
Evgeny Stadnik <128707156+e-stadnik@users.noreply.github.com>
2023-08-06 21:30:27 +0300

    update GitlabRemoteParserTests (#11104)

--- UnitTests/Plugins/BuildServerIntegration/GitlabIntegration.Tests/GitlabRemoteParserTests.cs
+++ UnitTests/Plugins/BuildServerIntegration/GitlabIntegration.Tests/GitlabRemoteParserTests.cs
@@ -6,15 +6,28 @@ namespace GitlabIntegrationTests
     [TestFixture]
     internal class GitlabRemoteParserTests
     {
-        [TestCase("https://github.com/owner/repo.git/")]
-        [TestCase("http://github.com/owner/repo.git/")]
-        [TestCase("https://github.com/owner/repo/")]
-        [TestCase("ssh://git@github.com/owner/repo.git/")]
-        [TestCase("git@github.com/owner/repo.git/")]
-        public void TryExtractGitHubDataFromRemoteUrl(string url)
+        [TestCase("https://gitlab.com/owner/repo.git/")]
+        [TestCase("http://gitlab.com/owner/repo.git/")]
+        [TestCase("https://gitlab.com/owner/repo/")]
+        [TestCase("ssh://git@gitlab.com/owner/repo.git/")]
+        [TestCase("git@gitlab.com/owner/repo.git/")]
+        public void TryExtractGitlabDataFromGitlabComRemoteUrl(string url)
         {
             new GitlabRemoteParser().TryExtractGitlabDataFromRemoteUrl(url, out var host, out var owner, out var repository).Should().BeTrue();
-            host.Should().Be("github.com");
+            host.Should().Be("gitlab.com");
+            owner.Should().Be("owner");
+            repository.Should().Be("repo");
+        }
+
+        [TestCase("https://example.com/owner/repo.git/", "example.com")]
+        [TestCase("http://gitlab.example.com/owner/repo.git/", "gitlab.example.com")]
+        [TestCase("https://repo.example.com/owner/repo/", "repo.example.com")]
+        [TestCase("https://192.168.1.11/owner/repo/", "192.168.1.11")]
+        [TestCase("https://repo.192.168.1.11:5896/owner/repo/", "repo.192.168.1.11:5896")]
+        public void TryExtractGitlabDataFromSelfHostedRemoteUrl(string url, string expectedHost)
+        {
+            new GitlabRemoteParser().TryExtractGitlabDataFromRemoteUrl(url, out var host, out var owner, out var repository).Should().BeTrue();
+            host.Should().Be(expectedHost);
             owner.Should().Be("owner");
             repository.Should().Be("repo");
         }
@@ -22,13 +35,13 @@ namespace GitlabIntegrationTests
         [Test]
         public void Should_fail_in_parsing_invalid_url()
         {
-            GitlabRemoteParser gitHubRemoteParser = new();
-            var url = "https://owner@dev.bad.com/owner/project/_git/repo";
-            gitHubRemoteParser.TryExtractGitlabDataFromRemoteUrl(url, out var host, out var owner, out var repository).Should().BeFalse();
+            GitlabRemoteParser gitlabRemoteParser = new();
+            string url = "https://owner@dev.bad.com/owner/project/_git/repo";
+            gitlabRemoteParser.TryExtractGitlabDataFromRemoteUrl(url, out var host, out var owner, out var repository).Should().BeFalse();
             owner.Should().BeNull();
             repository.Should().BeNull();
 
-            gitHubRemoteParser.IsValidRemoteUrl(url).Should().BeFalse();
+            gitlabRemoteParser.IsValidRemoteUrl(url).Should().BeFalse();
         }
     }
 }
