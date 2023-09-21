c0dbe189de321b1c13f32fc4747a62d64803f538
Philippe Miossec <pmiossec@gmail.com>
2023-09-21 14:17:53 +0200

    Rebase: fix accuracy of data in list of commits displayed (#11206)
    * rebase: fix accuracy of data in list of commits displayed
    
    by using "done" file for commits already processed
    and "git-rebase-todo" for what will be **really** done.
    
    because "git-rebase-todo.backup" content is not accurate
    (it contains what was presented to user in interactive rebase
    **before** user choose which actions to do on commits)
    
    it fixes "action" column value that was not accurate
    and commits that was reordered.
    
    Note: for commits that we don't want to keep, the normal behavior is the following:
    * 'dropped' commits are still displayed but with the action 'dropped'
    * removed or commented commits lines are not displayed in the list
    So prefer using 'd'/'dropped'...
    
    fixes #11179
    
    * tests: Add integration tests for `GetInteractiveRebasePatchFiles()`
    
    * refactor ReferenceRepository test util class
    
    * refactor and clean tests

---
+++ IntegrationTests/UI.IntegrationTests/GitCommands/GitModuleTests.cs
@@ -0,0 +1,106 @@
+﻿using System.Text;
+using CommonTestUtils;
+using FluentAssertions;
+using GitCommands;
+using GitCommands.Patches;
+using GitUIPluginInterfaces;
+
+namespace GitCommandsTests
+{
+    [TestFixture]
+    public sealed class GitModuleTests
+    {
+        private ReferenceRepository _refRepo;
+        private GitModule _gitModule;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _refRepo = new();
+            _gitModule = _refRepo.Module;
+            Console.WriteLine("Repo path: " + _gitModule.WorkingDir);
+        }
+
+        [Test]
+        public void RebasePatchesBuiltAccordingToGitRebaseFiles_EnsureRebaseFileFormatWithGit()
+        {
+            string content = "line1" + Environment.NewLine;
+            string mainFile = "main_file.txt";
+            CommitData initialCommit = CreateCommit("main: initial commit", content, mainFile);
+            content += "line2" + Environment.NewLine;
+            CreateCommit("main: commit2", content, mainFile);
+            content += "line3" + Environment.NewLine;
+            CommitData tipOfMainBranchCommit = CreateCommit("main: commit3", content, mainFile);
+
+            // Create a branch with contents that will conflict on rebase
+            _refRepo.CreateBranch("branch2", initialCommit.Hash);
+            _refRepo.CheckoutBranch("branch2");
+            string otherFile = "other_file.txt";
+            CommitData commitDone = CreateCommit("branch2: commit that will apply successfully", "other_line1", otherFile);
+            CommitData commitApplying = CreateCommit("branch2: commit that will fail to rebase", "a content that will create a conflict", mainFile);
+            CommitData commitToDo = CreateCommit("branch2: commit todo", "foobar", otherFile);
+
+            // Real git rebase to ensure git rebase file format has not changed
+            ExecutionResult execResult = _gitModule.GitExecutable.Execute($"rebase {tipOfMainBranchCommit.Hash}", throwOnErrorExit: false);
+            execResult.ExitCode.Should().NotBe(0); // Expecting conflicts!
+
+            IReadOnlyList<PatchFile> patches = _gitModule.GetInteractiveRebasePatchFiles();
+
+            patches.Count.Should().Be(3);
+            VerifyPatch(patches[0], "pick", commitDone, isApplied: true);
+            VerifyPatch(patches[1], "pick", commitApplying, isApplied: false, isNext: true);
+            VerifyPatch(patches[2], "pick", commitToDo, isApplied: false);
+
+            CommitData CreateCommit(string commitMessage, string content, string filename)
+            {
+                string hash = _refRepo.CreateCommit(commitMessage, content, filename);
+                return new CommitData(hash, commitMessage, $"{hash} {commitMessage}", ObjectId.Parse(hash));
+            }
+        }
+
+        [Test]
+        public void RebasePatchesBuiltAccordingToGitRebaseFiles()
+        {
+            CommitData commitDonePicked = CreateCommit("commit done: pick");
+            CommitData commitDoneDelete = CreateCommit("commit done: drop");
+            CommitData commitDoneLineRemoved = CreateCommit("commit done: line removed i.e drop");
+            CommitData commitApplying = CreateCommit("commit in progress");
+            CommitData commitToDoPicked = CreateCommit("commit todo: picked");
+            CommitData commitToDoDelete = CreateCommit("commit todo: drop");
+            CommitData commitToDoLineRemoved = CreateCommit("commit todo: line removed i.e drop");
+
+            // Carefully crafted git rebase files to test different actions (pick, drop, line moved, line removed)
+            Directory.CreateDirectory(Path.Combine(_gitModule.WorkingDirGitDir, "rebase-merge"));
+            _refRepo.CreateRepoFile(".git/rebase-merge/done", @$"drop {commitDoneDelete.RebaseLine}
+pick {commitDonePicked.RebaseLine}
+pick {commitApplying.RebaseLine}");
+            _refRepo.CreateRepoFile(".git/rebase-merge/stopped-sha", commitApplying.Hash);
+            _refRepo.CreateRepoFile(".git/rebase-merge/git-rebase-todo", @$"drop {commitToDoDelete.RebaseLine}
+pick {commitToDoPicked.RebaseLine}");
+
+            IReadOnlyList<PatchFile> patches = _gitModule.GetInteractiveRebasePatchFiles();
+
+            patches.Count.Should().Be(5);
+            VerifyPatch(patches[0], "drop", commitDoneDelete, isApplied: true); // Line moved up "during interactive rebase"
+            VerifyPatch(patches[1], "pick", commitDonePicked, isApplied: true);
+            VerifyPatch(patches[2], "pick", commitApplying, isApplied: false, isNext: true);
+            VerifyPatch(patches[3], "drop", commitToDoDelete, isApplied: false); // Line moved up "during interactive rebase"
+            VerifyPatch(patches[4], "pick", commitToDoPicked, isApplied: false);
+
+            CommitData CreateCommit(string commitMessage)
+            {
+                string hash = _refRepo.CreateCommit(commitMessage);
+                return new CommitData(hash, commitMessage, $"{hash} {commitMessage}", ObjectId.Parse(hash));
+            }
+        }
+
+        private void VerifyPatch(PatchFile patchFile, string action, CommitData commit, bool isApplied, bool isNext = false)
+            => patchFile.Should().BeEquivalentTo(
+                new PatchFile { Action = action, ObjectId = commit.ObjectId, IsApplied = isApplied, IsNext = isNext, Subject = commit.CommitMessage, Author = ReferenceRepository.AuthorFullIdentity },
+                options => options.Excluding(x => x.Date));
+    }
+
+    internal record struct CommitData(string Hash, string CommitMessage, string RebaseLine, ObjectId ObjectId)
+    {
+    }
+}
