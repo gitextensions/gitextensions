0c5553df9077b45259dc19251b0969bd0f1f6372
RussKie <russkie@gmail.com>
2023-09-27 13:19:48 +1000

    VS reformat

--- GitUI/CommandsDialogs/FormCheckoutRevision.Designer.cs
+++ GitUI/CommandsDialogs/FormCheckoutRevision.Designer.cs
@@ -28,121 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.OkCheckout = new System.Windows.Forms.Button();
-            this.Force = new System.Windows.Forms.CheckBox();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.commitPickerSmallControl1 = new GitUI.UserControls.CommitPickerSmallControl();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            OkCheckout = new Button();
+            Force = new CheckBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label2 = new Label();
+            commitPickerSmallControl1 = new UserControls.CommitPickerSmallControl();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(444, 106);
-            this.tableLayoutPanel1.TabIndex = 1;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(444, 106);
+            tableLayoutPanel1.TabIndex = 1;
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.AutoSize = true;
-            this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.71545F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.28455F));
-            this.tableLayoutPanel2.Controls.Add(this.OkCheckout, 1, 0);
-            this.tableLayoutPanel2.Controls.Add(this.Force, 0, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 70);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(438, 31);
-            this.tableLayoutPanel2.TabIndex = 1;
+            tableLayoutPanel2.AutoSize = true;
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 69.71545F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30.28455F));
+            tableLayoutPanel2.Controls.Add(OkCheckout, 1, 0);
+            tableLayoutPanel2.Controls.Add(Force, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 72);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(438, 31);
+            tableLayoutPanel2.TabIndex = 1;
             // 
             // OkCheckout
             // 
-            this.OkCheckout.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.OkCheckout.Image = global::GitUI.Properties.Images.Checkout;
-            this.OkCheckout.Location = new System.Drawing.Point(313, 3);
-            this.OkCheckout.Name = "OkCheckout";
-            this.OkCheckout.Size = new System.Drawing.Size(122, 25);
-            this.OkCheckout.TabIndex = 1;
-            this.OkCheckout.Text = "&Checkout";
-            this.OkCheckout.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.OkCheckout.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.OkCheckout.UseVisualStyleBackColor = true;
-            this.OkCheckout.Click += new System.EventHandler(this.OkClick);
+            OkCheckout.Anchor = AnchorStyles.Right;
+            OkCheckout.Image = Properties.Images.Checkout;
+            OkCheckout.Location = new Point(313, 3);
+            OkCheckout.Name = "OkCheckout";
+            OkCheckout.Size = new Size(122, 25);
+            OkCheckout.TabIndex = 1;
+            OkCheckout.Text = "&Checkout";
+            OkCheckout.TextAlign = ContentAlignment.MiddleRight;
+            OkCheckout.TextImageRelation = TextImageRelation.ImageBeforeText;
+            OkCheckout.UseVisualStyleBackColor = true;
+            OkCheckout.Click += OkClick;
             // 
             // Force
             // 
-            this.Force.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.Force.AutoSize = true;
-            this.Force.Location = new System.Drawing.Point(3, 6);
-            this.Force.Name = "Force";
-            this.Force.Size = new System.Drawing.Size(166, 19);
-            this.Force.TabIndex = 5;
-            this.Force.Text = "&Force (reset local changes)";
-            this.Force.UseVisualStyleBackColor = true;
+            Force.Anchor = AnchorStyles.Left;
+            Force.AutoSize = true;
+            Force.Location = new Point(3, 6);
+            Force.Name = "Force";
+            Force.Size = new Size(166, 19);
+            Force.TabIndex = 5;
+            Force.Text = "&Force (reset local changes)";
+            Force.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.label2);
-            this.flowLayoutPanel1.Controls.Add(this.commitPickerSmallControl1);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(438, 61);
-            this.flowLayoutPanel1.TabIndex = 2;
+            flowLayoutPanel1.Controls.Add(label2);
+            flowLayoutPanel1.Controls.Add(commitPickerSmallControl1);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(3, 3);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(438, 63);
+            flowLayoutPanel1.TabIndex = 2;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 5);
-            this.label2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Checkout this &revision";
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 5);
+            label2.Margin = new Padding(3, 5, 3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Checkout this &revision";
             // 
             // commitPickerSmallControl1
             // 
-            this.commitPickerSmallControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.commitPickerSmallControl1.Location = new System.Drawing.Point(133, 3);
-            this.commitPickerSmallControl1.MinimumSize = new System.Drawing.Size(100, 26);
-            this.commitPickerSmallControl1.Name = "commitPickerSmallControl1";
-            this.commitPickerSmallControl1.Size = new System.Drawing.Size(207, 26);
-            this.commitPickerSmallControl1.TabIndex = 3;
+            commitPickerSmallControl1.AutoSize = true;
+            commitPickerSmallControl1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            commitPickerSmallControl1.Location = new Point(133, 3);
+            commitPickerSmallControl1.MinimumSize = new Size(100, 26);
+            commitPickerSmallControl1.Name = "commitPickerSmallControl1";
+            commitPickerSmallControl1.Size = new Size(156, 26);
+            commitPickerSmallControl1.TabIndex = 3;
             // 
             // FormCheckoutRevision
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(444, 106);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.MinimumSize = new System.Drawing.Size(460, 125);
-            this.Name = "FormCheckoutRevision";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Checkout revision";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.tableLayoutPanel2.PerformLayout();
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(444, 106);
+            Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(460, 125);
+            Name = "FormCheckoutRevision";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Checkout revision";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
