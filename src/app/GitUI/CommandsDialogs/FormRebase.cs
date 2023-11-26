2a9969a5b1c827f425f9b78c966abe95890b08f4
vovodroid <vovodroid@users.noreply.github.com>
2023-11-26 12:00:30 +0200

    feat(rebase): auto continue on rerere applied
    Co-authored by: mstv@gmx.net
    Refs: #13243

--- src/app/GitUI/CommandsDialogs/FormRebase.cs
+++ src/app/GitUI/CommandsDialogs/FormRebase.cs
@@ -244,7 +244,7 @@ public partial class FormRebase : GitExtensionsDialog
     {
         using (WaitCursorScope.Enter())
         {
-            FormProcess.ShowDialog(this, UICommands, arguments: Commands.ContinueRebase(), Module.WorkingDir, input: null, useDialogSettings: true);
+            FormProcess.ShowDialog(this, UICommands, arguments: Commands.ContinueRebase(), Module.WorkingDir, input: null, useDialogSettings: true, out string cmdOutput);
 
             if (!Module.InTheMiddleOfRebase())
             {
@@ -253,6 +253,10 @@ public partial class FormRebase : GitExtensionsDialog
 
             EnableButtons();
             PatchGrid.Initialize();
+            if (Module.CanContinueAction(cmdOutput))
+            {
+                BeginInvoke(btnContinueRebase.PerformClick);
+            }
         }
     }
 
@@ -373,6 +377,10 @@ public partial class FormRebase : GitExtensionsDialog
 
             EnableButtons();
             PatchGrid.Initialize();
+            if (Module.CanContinueAction(cmdOutput))
+            {
+                BeginInvoke(btnContinueRebase.PerformClick);
+            }
         }
     }
 
