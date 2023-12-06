a37a45b15f9f96463cdcde4e0abf3cb7898a4107
Philippe Miossec <pmiossec@gmail.com>
2023-12-06 02:35:02 +0100

    Plugins: Fail to load a plugin no more prevent loading of all others (#11412)
    When a plugin fail to load and an exception is raised,
    no plugin was added to the `Plugins` collection
    and so none was added to the menu in the UI.
    
    Instead display plugin loading failure in plugin Menu
    
    Will mitigate the case where external plugins not (yet) compatible
    won't prevent internal plugins to load.

---
+++ GitUI/Plugin/FailedPluginWrapper.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using GitUI.Properties;
+using GitUIPluginInterfaces;
+
+namespace GitUI;
+
+internal partial class FailedPluginWrapper : IGitPlugin
+{
+    [GeneratedRegex("\\\"GitExtensions.([^\"]+)\\\"")]
+    private static partial Regex PluginNameRegex();
+
+    private readonly string _exception;
+    private string _pluginName;
+
+    public FailedPluginWrapper(Exception loadingException)
+    {
+        ArgumentNullException.ThrowIfNull(loadingException);
+
+        _exception = loadingException.Demystify().ToString();
+        try
+        {
+            // Try to extract plugin name from exception
+            Match match = PluginNameRegex().Match(_exception);
+            if (match.Success)
+            {
+                _pluginName = match.Value;
+                Name = $"{TranslatedStrings.FailedToLoadPlugin}: {_pluginName.Substring(0, Math.Min(50, match.Value.Length))}";
+            }
+        }
+        catch (Exception)
+        {
+            // no-op: best effort
+        }
+    }
+
+    public Guid Id { get; } = Guid.NewGuid();
+    public string? Name { get; init; } = TranslatedStrings.FailedToLoadPlugin;
+    public string? Description { get; } = TranslatedStrings.FailedToLoadPlugin;
+    public Image? Icon { get; } = Resources.bug;
+    public IGitPluginSettingsContainer? SettingsContainer { get; set; }
+    public bool HasSettings { get; } = false;
+
+    public bool Execute(GitUIEventArgs args)
+    {
+        DialogResult result = MessageBox.Show(string.Format(TranslatedStrings.FailedToLoadPluginPopupText, _exception),
+            TranslatedStrings.FailedToLoadPlugin, MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+        if (result == DialogResult.OK)
+        {
+            Clipboard.SetText(_exception);
+        }
+
+        return false;
+    }
+
+    public IEnumerable<ISetting> GetSettings() => Array.Empty<ISetting>();
+
+    public void Register(IGitUICommands gitUiCommands)
+    {
+    }
+
+    public void Unregister(IGitUICommands gitUiCommands)
+    {
+    }
+}
