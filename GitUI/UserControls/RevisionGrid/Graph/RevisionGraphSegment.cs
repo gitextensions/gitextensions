de8297fb46927f4e9a180a420aafd8b1dbfad35a
Michael Seibt <mstv@gmx.net>
2023-12-17 01:33:56 +0100

    Calculate LaneSharing without previous row (#11456)

--- GitUI/UserControls/RevisionGrid/Graph/RevisionGraphSegment.cs
+++ GitUI/UserControls/RevisionGrid/Graph/RevisionGraphSegment.cs
@@ -26,6 +26,11 @@ namespace GitUI.UserControls.RevisionGrid.Graph
             Child = child;
         }
 
+        /// <summary>
+        ///  Used by <see cref="RevisionGraphRow.BuildSegmentLanes"/> in order to remember whether this segment has already shared a lane with another segment.
+        /// </summary>
+        internal int IsSecondarySharedLaneAtLeastSinceScore { get; set; } = int.MaxValue;
+
         public LaneInfo? LaneInfo { get; set; }
 
         public RevisionGraphRevision Parent { get; }
