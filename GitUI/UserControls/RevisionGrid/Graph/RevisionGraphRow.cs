f66f2776e955f36d043fb586db6e072207f617a7
Michael Seibt <mstv@gmx.net>
2023-12-17 01:32:38 +0100

    Revert "Deactivate very slow handling of lane sharing (#11294)"
    This reverts commit 89cbc43543e26077ff0ba5dd822c8889ada05de7.

--- GitUI/UserControls/RevisionGrid/Graph/RevisionGraphRow.cs
+++ GitUI/UserControls/RevisionGrid/Graph/RevisionGraphRow.cs
@@ -1,9 +1,4 @@
-﻿// Hotfix of #11292 for release 4.2: Due to heavy performance issues with the Linux repo, do not determine the kind of LaneSharing of secondary segments.
-// This deactivates #10915 which avoided the multiple drawing of shared graph segments.
-// This reactivates a minor hyperactivity of line-straightening over commits (#11059).
-#define ALL_PRIMARY_LANES
-
-using Microsoft;
+﻿using Microsoft;
 
 namespace GitUI.UserControls.RevisionGrid.Graph
 {
@@ -127,11 +122,7 @@ namespace GitUI.UserControls.RevisionGrid.Graph
                         }
                         else
                         {
-#if ALL_PRIMARY_LANES
-                            laneSharing = LaneSharing.ExclusiveOrPrimary;
-#else
                             laneSharing = LaneSharing.DifferentEnd;
-#endif
                         }
 
                         return new Lane(_revisionLane, laneSharing);
@@ -197,16 +188,12 @@ namespace GitUI.UserControls.RevisionGrid.Graph
 
                     LaneSharing GetSecondarySharingOfContinuedSegment()
                     {
-#if ALL_PRIMARY_LANES
-                        return LaneSharing.ExclusiveOrPrimary;
-#else
                         return _previousRow.GetLaneForSegment(segment).Sharing switch
                         {
                             LaneSharing.ExclusiveOrPrimary or LaneSharing.DifferentEnd => LaneSharing.DifferentStart,
                             LaneSharing.Entire or LaneSharing.DifferentStart => LaneSharing.Entire,
                             _ => throw new NotImplementedException()
                         };
-#endif
                     }
                 }
 
