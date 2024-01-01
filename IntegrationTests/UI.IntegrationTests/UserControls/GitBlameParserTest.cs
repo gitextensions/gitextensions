43d3bcf437d699024f4761604cd43b3af73f22f8
Gerhard Olsson <gerhardol@users.noreply.github.com>
2024-01-02 00:36:27 +0100

    amend! Blame: options to git-diff to get line in previous commit
    GetOriginalLineInPreviousCommit() (to get line number in the previous commit)
    must override the git diff option from git-config to follow the user preference
    and to give predictable options for tests.
    Similar, rename and whitespace options must be the same for git-diff
    and git-blame.
    
    * Include current section in checks
    Better matching.
    
    * BlameParserTest should not assume clone folder name

--- IntegrationTests/UI.IntegrationTests/UserControls/GitBlameParserTest.cs
+++ IntegrationTests/UI.IntegrationTests/UserControls/GitBlameParserTest.cs
@@ -9,18 +9,30 @@ public sealed class GitBlameParserTest
 {
     // Track the original setting value
     private bool _useHistogramDiffAlgorithm;
+    private bool _detectCopyInFileOnBlame;
+    private bool _detectCopyInAllOnBlame;
+    private bool _ignoreWhitespaceOnBlame;
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
         _useHistogramDiffAlgorithm = AppSettings.UseHistogramDiffAlgorithm;
+        _detectCopyInFileOnBlame = AppSettings.DetectCopyInFileOnBlame;
+        _detectCopyInAllOnBlame = AppSettings.DetectCopyInAllOnBlame;
+        _ignoreWhitespaceOnBlame = AppSettings.IgnoreWhitespaceOnBlame;
         AppSettings.UseHistogramDiffAlgorithm = false;
+        AppSettings.DetectCopyInFileOnBlame = false;
+        AppSettings.DetectCopyInAllOnBlame = false;
+        AppSettings.IgnoreWhitespaceOnBlame = true;
     }
 
     [OneTimeTearDown]
     public void OneTimeTearDown()
     {
         AppSettings.UseHistogramDiffAlgorithm = _useHistogramDiffAlgorithm;
+        AppSettings.DetectCopyInFileOnBlame = _detectCopyInFileOnBlame;
+        AppSettings.DetectCopyInAllOnBlame = _detectCopyInAllOnBlame;
+        AppSettings.IgnoreWhitespaceOnBlame = _ignoreWhitespaceOnBlame;
     }
 
     [Test]
@@ -40,14 +52,14 @@ public sealed class GitBlameParserTest
         // line content is still: internal bool ExecuteCommand(Command cmd)
         Assert.AreEqual(2192, GetOriginalLineInPreviousCommit(2281));
 
-        // line content from offset in diff
-        //   case Command.Stash: UICommands.StashSave(this, AppSettings.IncludeUntrackedFilesInManualStash); break;
-        //  ->
-        //   case Commands.QuickPull: UICommands.StartPullDialogAndPullImmediately(this); break;
-        Assert.AreEqual(2147, GetOriginalLineInPreviousCommit(2187));
-
         // line content is still around: internal enum Command -> internal enum Commands
         Assert.AreEqual(2085, GetOriginalLineInPreviousCommit(2090));
+
+        // line content from offset in diff
+        //   kGitToolStripMenuItem.ShortcutKeyDisplayString = GetShortcutKeys(Command.GitGitK).ToShortcutKeyDisplayString();
+        //  ->
+        //   kGitToolStripMenuItem.ShortcutKeyDisplayString = GetShortcutKeys(Commands.GitGitK).ToShortcutKeyDisplayString();
+        Assert.AreEqual(824, GetOriginalLineInPreviousCommit(824));
 
         int GetOriginalLineInPreviousCommit(int blamedLineNumber)
             => new GitBlameParser(() => gitModule)
