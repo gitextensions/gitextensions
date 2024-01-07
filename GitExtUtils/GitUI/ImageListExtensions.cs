7060ee7c2420253c78f909e83b8a81ed7311b6c7
Donatas Mačiūnas <mdonatas@gmail.com>
2024-01-08 00:19:50 +0200

    Review fixes

--- GitExtUtils/GitUI/ImageListExtensions.cs
+++ GitExtUtils/GitUI/ImageListExtensions.cs
@@ -1,23 +1,15 @@
-﻿using System.Runtime.InteropServices;
+﻿namespace GitUI;
 
-namespace GitUI
+public static class ImageListExtensions
 {
-    public static class ImageListExtensions
+    /// <summary>
+    /// A regression was introduced in .NET 8 which causes an incorrect background color to be set
+    /// which manifests when using transparent images. See https://github.com/dotnet/winforms/issues/10462
+    /// This method should be removed once the underlying issue is fixed.
+    /// </summary>
+    public static ImageList FixImageTransparencyRegression(this ImageList imageList)
     {
-        internal const int ComCtl32CLRNone = unchecked((int)0xFFFFFFFF);
-
-        [DllImport("comctl32.dll", EntryPoint = "ImageList_SetBkColor")]
-        internal static extern int ImageListSetBkColor(IntPtr himl, int clrBk);
-
-        /// <summary>
-        /// A regression was introduced in .net8 which causes an incorrect background color to be set
-        /// which manifests when using transparent images. See https://github.com/dotnet/winforms/issues/10462
-        /// This metod should be removed once the underlying issue is fixed.
-        /// </summary>
-        public static ImageList FixImageTransparencyRegression(this ImageList imageList)
-        {
-            ImageListSetBkColor(imageList.Handle, ComCtl32CLRNone);
-            return imageList;
-        }
+        NativeMethods.ImageListSetBkColor(imageList.Handle, NativeMethods.ComCtl32CLRNone);
+        return imageList;
     }
 }
