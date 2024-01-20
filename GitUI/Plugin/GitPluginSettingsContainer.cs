33845eb97bd2b9fb8497304a5849f896408685c7
Philippe Miossec <pmiossec@gmail.com>
2024-01-20 15:42:40 +0100

    refactor(GitPluginSettingsContainer): simplify logic (#11660)
    by putting the logic of backward compatibility when the container is created
    instead of every times a value is read

--- GitUI/Plugin/GitPluginSettingsContainer.cs
+++ GitUI/Plugin/GitPluginSettingsContainer.cs
@@ -1,67 +1,39 @@
 ﻿using GitCommands;
 using GitUIPluginInterfaces;
 
-namespace GitUI
+namespace GitUI;
+
+public class GitPluginSettingsContainer : SettingsSource, IGitPluginSettingsContainer
 {
-    public class GitPluginSettingsContainer : SettingsSource, IGitPluginSettingsContainer
+    private readonly string _pluginOldCompatibilityPrefix;
+    private readonly string _pluginPrefix;
+    private SettingsSource? _settingsSource;
+
+    public GitPluginSettingsContainer(Guid pluginId, string pluginName)
     {
-        private readonly Guid _pluginId;
-        private readonly string _pluginName;
-        private SettingsSource? _settingsSource;
-
-        public GitPluginSettingsContainer(Guid pluginId, string pluginName)
-        {
-            _pluginId = pluginId;
-            _pluginName = pluginName;
-        }
-
-        public SettingsSource GetSettingsSource()
-        {
-            return this;
-        }
-
-        public void SetSettingsSource(SettingsSource? settingsSource)
-        {
-            _settingsSource = settingsSource;
-        }
-
-        private SettingsSource ExternalSettings => _settingsSource ?? AppSettings.SettingsContainer;
-
-        public override SettingLevel SettingLevel
-        {
-            get => ExternalSettings.SettingLevel;
-            set => throw new InvalidOperationException(nameof(SettingLevel));
-        }
-
-        public override string? GetValue(string name)
-        {
-            // for old plugin setting processing
-            if (_pluginId == Guid.Empty)
-            {
-                return ExternalSettings.GetValue($"{_pluginName}{name}");
-            }
-
-            string value = ExternalSettings.GetValue($"{_pluginId}.{name}");
-
-            // for old plugin setting processing
-            if (value is null)
-            {
-                value = ExternalSettings.GetValue($"{_pluginName}{name}");
-            }
-
-            return value;
-        }
-
-        public override void SetValue(string name, string? value)
-        {
-            if (_pluginId == Guid.Empty)
-            {
-                ExternalSettings.SetValue($"{_pluginName}{name}", value);
-            }
-            else
-            {
-                ExternalSettings.SetValue($"{_pluginId}.{name}", value);
-            }
-        }
+        _pluginPrefix = pluginId == Guid.Empty ? pluginName : $"{pluginId}.";
+        _pluginOldCompatibilityPrefix = pluginName;
     }
+
+    public SettingsSource GetSettingsSource() => this;
+
+    public void SetSettingsSource(SettingsSource? settingsSource)
+    {
+        _settingsSource = settingsSource;
+    }
+
+    private SettingsSource ExternalSettings => _settingsSource ?? AppSettings.SettingsContainer;
+
+    public override SettingLevel SettingLevel
+    {
+        get => ExternalSettings.SettingLevel;
+        set => throw new InvalidOperationException(nameof(SettingLevel));
+    }
+
+    public override string? GetValue(string name)
+        => ExternalSettings.GetValue($"{_pluginPrefix}{name}")
+            ?? ExternalSettings.GetValue($"{_pluginOldCompatibilityPrefix}{name}");
+
+    public override void SetValue(string name, string? value)
+        => ExternalSettings.SetValue($"{_pluginPrefix}{name}", value);
 }
