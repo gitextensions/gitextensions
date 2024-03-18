gitextensions/gitextensions

commit 082b7fe0d183c81129ac4038c134fe659b27781a

fixup! git-grep review comments

GitUI/GitUIExtensions.cs
@@ -105,7 +105,7 @@ namespace GitUI
             {
                 GitCommandConfiguration commandConfiguration = GrepHighlightService.GetGitCommandConfiguration(fileViewer.Module, AppSettings.UseGitColoring.Value);
                 ExecutionResult result = await fileViewer.Module.GetGrepFileAsync(
-                        item.SecondRevision,
+                        item.SecondRevision.ObjectId,
                         item.Item.Name,
                         fileViewer.GetExtraGrepArguments(),
                         item.Item.GrepString,
@@ -121,7 +121,6 @@ namespace GitUI
                 }
 
                 await fileViewer.ViewGrepAsync(item, text: result.StandardOutput, useGitColoring: AppSettings.UseGitColoring.Value, item.Item.GrepString);
-                return;
             }
 
             if (firstId == ObjectId.CombinedDiffId)

GitCommands/Git/GitModule.cs
@@ -1440,7 +1440,7 @@ namespace GitCommands
 
             static bool DeletableItem(GitItemStatus item) => item.IsNew || item.IsRenamed;
 
-            // For normal commits: 'git-checkout <id> --' must be used for Unmerged but will not work on e.g. SkipWorktree.
+            // For normal commits: 'git-checkout <objectId> --' must be used for Unmerged but will not work on e.g. SkipWorktree.
             static bool UnmergedNotIndex(GitItemStatus item, Lazy<List<GitItemStatus>> status)
                 => item.IsUnmerged && !status.Value.Any(i => !i.IsDeleted && !i.IsNew && i.Name == item.Name);
 
@@ -2297,9 +2297,9 @@ namespace GitCommands
             });
         }
 
-        private ExecutionResult GetGrepFiles(ObjectId id, string grepString, CancellationToken cancellationToken = default)
+        private ExecutionResult GetGrepFiles(ObjectId objectId, string grepString, CancellationToken cancellationToken = default)
         {
-            bool noCache = id.IsArtificial;
+            bool noCache = objectId.IsArtificial;
 
             return _gitExecutable.Execute(
                 new GitArgumentBuilder("grep")
@@ -2310,7 +2310,7 @@ namespace GitCommands
                     { AppSettings.GitGrepIgnoreCase.Value, "--ignore-case" },
                     { AppSettings.GitGrepMatchWholeWord.Value, "--word-regexp" },
                     grepString,
-                    !id.IsArtificial ? id.ToString() : id == ObjectId.IndexId ? "--cached" : "",
+                    !objectId.IsArtificial ? objectId.ToString() : objectId == ObjectId.IndexId ? "--cached" : "",
                     "--"
                 },
                 cache: noCache ? null : GitCommandCache,
@@ -2318,35 +2318,34 @@ namespace GitCommands
                 cancellationToken: cancellationToken);
         }
 
-        public IReadOnlyList<GitItemStatus>? GetGrepFilesStatus(GitRevision revision, string grepString, CancellationToken cancellationToken = default)
+        public IReadOnlyList<GitItemStatus>? GetGrepFilesStatus(ObjectId objectId, string grepString, CancellationToken cancellationToken = default)
         {
             List<GitItemStatus> result = [];
-            ExecutionResult exec = GetGrepFiles(revision.ObjectId, grepString, cancellationToken);
+            ExecutionResult exec = GetGrepFiles(objectId, grepString, cancellationToken);
             if (!exec.ExitedSuccessfully)
             {
                 // Cannot see difference from error and no matches
-                return Array.Empty<GitItemStatus>();
+                return [];
             }
 
             foreach (string file in exec.StandardOutput.LazySplit('\0', StringSplitOptions.RemoveEmptyEntries))
             {
-                int colon = file.IndexOf(':') + 1;
-                result.Add(new GitItemStatus(file[colon..])
-                {
-                    GrepString = grepString,
+                int startIndex = file.IndexOf(':') + 1;
+                result.Add(new GitItemStatus(file[startIndex..])
+                    {
+                        GrepString = grepString,
 
-                    // Assume this file is handled by Git, may not be entirely correct for worktree
-                    IsTracked = true
-                });
+                        // Assume this file is handled by Git, may not be entirely correct for worktree
+                        IsTracked = true
+                    });
             }
 
             return result;
         }
 
-        public async Task<ExecutionResult> GetGrepFileAsync(GitRevision revision, string fileName, ArgumentString extraArgs, string grepString, bool useGitColoring, GitCommandConfiguration commandConfiguration, CancellationToken cancellationToken = default)
+        public async Task<ExecutionResult> GetGrepFileAsync(ObjectId objectId, string fileName, ArgumentString extraArgs, string grepString, bool useGitColoring, GitCommandConfiguration commandConfiguration, CancellationToken cancellationToken = default)
         {
-            ObjectId id = revision.ObjectId;
-            bool noCache = id.IsArtificial;
+            bool noCache = objectId.IsArtificial;
 
             GitArgumentBuilder args = new("grep", commandConfiguration: commandConfiguration)
             {
@@ -2358,18 +2357,17 @@ namespace GitCommands
                 { AppSettings.GitGrepIgnoreCase.Value, "--ignore-case" },
                 { AppSettings.GitGrepMatchWholeWord.Value, "--word-regexp" },
                 grepString,
-                !id.IsArtificial ? id.ToString() : id == ObjectId.IndexId ? "--cached" : "",
+                !objectId.IsArtificial ? objectId.ToString() : objectId == ObjectId.IndexId ? "--cached" : "",
                 "--",
                 fileName
             };
-            ExecutionResult result = await _gitExecutable.ExecuteAsync(
+
+            return await _gitExecutable.ExecuteAsync(
                 args,
                 cache: noCache ? null : GitCommandCache,
                 throwOnErrorExit: false,
                 stripAnsiEscapeCodes: !useGitColoring,
                 cancellationToken: cancellationToken);
-
-            return result;
         }
 
         public ExecutionResult GetDiffFiles(string? firstRevision, string? secondRevision, bool noCache = false, bool nullSeparated = false, CancellationToken cancellationToken = default)

GitCommands/Settings/AppSettings.cs
@@ -1342,11 +1342,7 @@ namespace GitCommands
             set => SetBool("showdiffforallparents", value);
         }
 
-        public static bool ShowSearchCommit
-        {
-            get => GetBool("showsearchcommit", false);
-            set => SetBool("showsearchcommit", value);
-        }
+        public static ISetting<bool> ShowSearchCommit { get; } = Setting.Create(AppearanceSettingsPath, nameof(ShowSearchCommit), false);
 
         public static bool ShowAvailableDiffTools
         {

GitUI/CommandsDialogs/RevisionDiffControl.Designer.cs
@@ -441,7 +441,7 @@ namespace GitUI.CommandsDialogs
             showSearchCommitToolStripMenuItem.Image = Properties.Images.ViewFile;
             showSearchCommitToolStripMenuItem.Name = "showSearchCommitToolStripMenuItem";
             showSearchCommitToolStripMenuItem.Size = new Size(262, 22);
-            showSearchCommitToolStripMenuItem.Text = "S&earch commit...";
+            showSearchCommitToolStripMenuItem.Text = "S&earch files in commit...";
             showSearchCommitToolStripMenuItem.Click += showSearchCommitToolStripMenuItem_Click;
             // 
             // DiffText

GitUI/Editor/Diff/GrepHighlightService.cs
@@ -97,8 +97,6 @@ public partial class GrepHighlightService : TextHighlightService
         {
             document.MarkerStrategy.AddMarker(tm);
         }
-
-        return;
     }
 
     private void SetText(ref string text, bool useGitColoring, string grepString)
@@ -139,7 +137,7 @@ public partial class GrepHighlightService : TextHighlightService
                     DebugHelpers.Fail($"Cannot parse lineNo for grep {line} ({sb.Length})");
                 }
 
-                // git-grep emitts an enpty line last, should not be displayed.
+                // git-grep emits an empty line last, should not be displayed.
                 // Other occurrences should not occur, just print them to debug.
                 sb.Append(line);
                 continue;
@@ -161,12 +159,12 @@ public partial class GrepHighlightService : TextHighlightService
                     Color color = AppColor.DiffAddedExtra.GetThemeColor();
                     Color forecolor = ColorHelper.GetForeColorForBackColor(color);
                     if (AnsiEscapeUtilities.TryGetTextMarker(new()
-                    {
-                        DocOffset = sb.Length + column - 1,
-                        Length = grepLength,
-                        BackColor = color,
-                        ForeColor = forecolor
-                    },
+                        {
+                            DocOffset = sb.Length + column - 1,
+                            Length = grepLength,
+                            BackColor = color,
+                            ForeColor = forecolor
+                        },
                         out TextMarker tm))
                     {
                         _textMarkers.Add(tm);

GitUI/Editor/FileViewer.cs
@@ -376,10 +376,11 @@ namespace GitUI.Editor
 
         public ArgumentString GetExtraGrepArguments()
         {
+            int numberOfContextLines = ShowEntireFile ? 100_000 : NumberOfContextLines;
             return new ArgumentBuilder
             {
                 "-h",
-                { ShowEntireFile, "--context=9000", $"--context={NumberOfContextLines}" },
+                $"--context={numberOfContextLines}",
                 { TreatAllFilesAsText, "--text" },
             };
         }

GitUI/Editor/SearchCommitForm.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using ICSharpCode.TextEditor.Actions;
+
 namespace GitUI
 {
 	partial class SearchCommitForm
@@ -24,7 +26,27 @@ namespace GitUI
             chkMatchCase = new CheckBox();
             btnSearch = new Button();
             chkShowSearchBox = new CheckBox();
+            MainPanel.SuspendLayout();
+            ControlsPanel.SuspendLayout();
             SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            MainPanel.Controls.Add(chkMatchCase);
+            MainPanel.Controls.Add(chkMatchWholeWord);
+            MainPanel.Controls.Add(txtOptions);
+            MainPanel.Controls.Add(txtSearchFor);
+            MainPanel.Controls.Add(lblOptions);
+            MainPanel.Controls.Add(label1);
+            MainPanel.Padding = new Padding(9);
+            MainPanel.Size = new Size(419, 103);
+            // 
+            // ControlsPanel
+            // 
+            ControlsPanel.Controls.Add(btnSearch);
+            ControlsPanel.Controls.Add(chkShowSearchBox);
+            ControlsPanel.Location = new Point(0, 50);
+            ControlsPanel.Size = new Size(419, 41);
             // 
             // label1
             // 
@@ -33,7 +55,7 @@ namespace GitUI
             label1.Name = "label1";
             label1.Size = new Size(45, 15);
             label1.TabIndex = 0;
-            label1.Text = "Search:";
+            label1.Text = "&Search:";
             // 
             // lblOptions
             // 
@@ -42,7 +64,7 @@ namespace GitUI
             lblOptions.Name = "lblOptions";
             lblOptions.Size = new Size(52, 15);
             lblOptions.TabIndex = 2;
-            lblOptions.Text = "Options:";
+            lblOptions.Text = "&Options:";
             // 
             // txtSearchFor
             // 
@@ -51,8 +73,8 @@ namespace GitUI
             txtSearchFor.Name = "txtSearchFor";
             txtSearchFor.Size = new Size(317, 23);
             txtSearchFor.TabIndex = 1;
-            txtSearchFor.KeyDown += txtSearchFor_KeyDown;
             txtSearchFor.SelectedIndexChanged += btnSearch_Click;
+            txtSearchFor.KeyDown += txtSearchFor_KeyDown;
             // 
             // txtOptions
             // 
@@ -63,17 +85,6 @@ namespace GitUI
             txtOptions.TabIndex = 2;
             txtOptions.TextChanged += txtOptions_TextChanged;
             txtOptions.KeyDown += txtSearchFor_KeyDown;
-            // 
-            // chkMatchCase
-            // 
-            chkMatchCase.AutoSize = true;
-            chkMatchCase.Location = new Point(90, 58);
-            chkMatchCase.Name = "chkMatchCase";
-            chkMatchCase.Size = new Size(86, 19);
-            chkMatchCase.TabIndex = 4;
-            chkMatchCase.Text = "Match &case";
-            chkMatchCase.UseVisualStyleBackColor = true;
-            chkMatchCase.CheckedChanged += chkMatchCase_CheckedChanged;
             // 
             // chkMatchWholeWord
             // 
@@ -86,10 +97,33 @@ namespace GitUI
             chkMatchWholeWord.UseVisualStyleBackColor = true;
             chkMatchWholeWord.CheckedChanged += chkMatchWholeWord_CheckedChanged;
             // 
+            // chkMatchCase
+            // 
+            chkMatchCase.AutoSize = true;
+            chkMatchCase.Location = new Point(90, 58);
+            chkMatchCase.Name = "chkMatchCase";
+            chkMatchCase.Size = new Size(86, 19);
+            chkMatchCase.TabIndex = 4;
+            chkMatchCase.Text = "Match &case";
+            chkMatchCase.UseVisualStyleBackColor = true;
+            chkMatchCase.CheckedChanged += chkMatchCase_CheckedChanged;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnSearch.DialogResult = DialogResult.Cancel;
+            btnSearch.Location = new Point(306, 8);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(100, 25);
+            btnSearch.TabIndex = 7;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
             // chkShowSearchBox
             // 
             chkShowSearchBox.AutoSize = true;
-            chkShowSearchBox.Location = new Point(90, 106);
+            chkShowSearchBox.Location = new Point(12, 10);
             chkShowSearchBox.Name = "chkShowSearchBox";
             chkShowSearchBox.Size = new Size(125, 19);
             chkShowSearchBox.TabIndex = 6;
@@ -97,40 +131,26 @@ namespace GitUI
             chkShowSearchBox.UseVisualStyleBackColor = true;
             chkShowSearchBox.CheckedChanged += chkShowSearchBox_CheckedChanged;
             // 
-            // btnSearch
-            // 
-            btnSearch.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            btnSearch.DialogResult = DialogResult.Cancel;
-            btnSearch.Location = new Point(307, 134);
-            btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(100, 25);
-            btnSearch.TabIndex = 7;
-            btnSearch.Text = "&Search";
-            btnSearch.UseVisualStyleBackColor = true;
-            btnSearch.Click += btnSearch_Click;
-            // 
             // SearchCommitForm
             // 
+            AcceptButton = btnSearch;
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            CancelButton = btnSearch;
-            ClientSize = new Size(419, 169);
-            Controls.Add(chkShowSearchBox);
-            Controls.Add(chkMatchCase);
-            Controls.Add(chkMatchWholeWord);
-            Controls.Add(btnSearch);
-            Controls.Add(txtOptions);
-            Controls.Add(txtSearchFor);
-            Controls.Add(lblOptions);
-            Controls.Add(label1);
+            ClientSize = new Size(419, 144);
             FormBorderStyle = FormBorderStyle.FixedDialog;
+            HelpButton = true;
+            ManualSectionAnchorName = "diff";
+            ManualSectionSubfolder = "browse_repository";
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "SearchCommitForm";
             StartPosition = FormStartPosition.Manual;
-            Text = "Search commit";
+            Text = "Search files in commit";
             FormClosing += SearchCommitForm_FormClosing;
-            Load += SearchCommitForm_Load;
+            MainPanel.ResumeLayout(false);
+            MainPanel.PerformLayout();
+            ControlsPanel.ResumeLayout(false);
+            ControlsPanel.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }

GitUI/Editor/SearchCommitForm.cs
@@ -2,11 +2,12 @@
 
 namespace GitUI;
 
-public partial class SearchCommitForm : GitExtensionsForm
+public partial class SearchCommitForm : GitExtensionsDialog
 {
     private bool _hasLoaded = false;
 
-    public SearchCommitForm()
+    public SearchCommitForm(GitUICommands commands)
+            : base(commands, enablePositionRestore: false)
     {
         InitializeComponent();
         InitializeComplete();
@@ -22,7 +23,16 @@ public partial class SearchCommitForm : GitExtensionsForm
 
     public Action<string, int> SearchFunc;
     public Action<bool> EnableSearchBoxFunc;
-    public ComboBox.ObjectCollection SearchItems { private get; set; }
+    public ComboBox.ObjectCollection SearchItems
+    {
+        set
+        {
+            foreach (object item in value)
+            {
+                txtSearchFor.Items.Add(item);
+            }
+        }
+    }
 
     protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
     {
@@ -52,10 +62,21 @@ public partial class SearchCommitForm : GitExtensionsForm
     private void SearchCommitForm_FormClosing(object sender, FormClosingEventArgs e)
     {
         // Close the search if search is not visible (or user has cleared input)
-        if (string.IsNullOrEmpty(SearchFor) || !AppSettings.ShowSearchCommit)
+        if (string.IsNullOrEmpty(SearchFor) || !AppSettings.ShowSearchCommit.Value)
         {
             SearchFunc?.Invoke("", 0);
         }
+    }
+
+    protected override void OnShown(EventArgs e)
+    {
+        base.OnShown(e);
+
+        txtOptions.Text = AppSettings.GitGrepUserArguments.Value;
+        chkMatchCase.Checked = !AppSettings.GitGrepIgnoreCase.Value;
+        chkMatchWholeWord.Checked = AppSettings.GitGrepMatchWholeWord.Value;
+        chkShowSearchBox.Checked = AppSettings.ShowSearchCommit.Value;
+        _hasLoaded = true;
     }
 
     private void Search()
@@ -93,20 +114,6 @@ public partial class SearchCommitForm : GitExtensionsForm
         Search();
     }
 
-    private void SearchCommitForm_Load(object sender, EventArgs e)
-    {
-        txtOptions.Text = AppSettings.GitGrepUserArguments.Value;
-        foreach (object item in SearchItems)
-        {
-            txtSearchFor.Items.Add(item);
-        }
-
-        chkMatchCase.Checked = !AppSettings.GitGrepIgnoreCase.Value;
-        chkMatchWholeWord.Checked = AppSettings.GitGrepMatchWholeWord.Value;
-        chkShowSearchBox.Checked = AppSettings.ShowSearchCommit;
-        _hasLoaded = true;
-    }
-
     private void txtSearchFor_KeyDown(object sender, KeyEventArgs e)
     {
         if (e.KeyCode == Keys.Enter)
@@ -132,7 +139,7 @@ public partial class SearchCommitForm : GitExtensionsForm
 
     private void chkShowSearchBox_CheckedChanged(object sender, EventArgs e)
     {
-        AppSettings.ShowSearchCommit = chkShowSearchBox.Checked;
+        AppSettings.ShowSearchCommit.Value = chkShowSearchBox.Checked;
         if (!_hasLoaded)
         {
             return;

commit 0285e797e0559b9c6663b115f48ab81faba56d30

fixup! regression from 082b7fe0d183c81129ac4038c134fe659b27781a

GitUI/GitUIExtensions.cs
@@ -121,6 +121,7 @@ namespace GitUI
                 }
 
                 await fileViewer.ViewGrepAsync(item, text: result.StandardOutput, useGitColoring: AppSettings.UseGitColoring.Value, item.Item.GrepString);
+                return;
             }
 
             if (firstId == ObjectId.CombinedDiffId)