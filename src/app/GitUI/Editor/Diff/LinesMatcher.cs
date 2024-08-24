2918d661c07ef954719d4f6c902a0384d03664b5
Michael Seibt <mstv@gmx.net>
2024-08-24 17:21:11 +0200

    feat(LinesMatcher): Then match by words
    Refs: #11851

--- src/app/GitUI/Editor/Diff/LinesMatcher.cs
+++ src/app/GitUI/Editor/Diff/LinesMatcher.cs
@@ -73,7 +73,64 @@ internal static class LinesMatcher
             return (Array.IndexOf(removed, longestMatchingRemoved), matchingAddedIndex);
         }
 
-        return (0, 0);
+        // then match lines whose common words have the maximum summed-up length
+        int removedMaxScoreIndex = 0;
+        int addedMaxScoreIndex = 0;
+        float maxScore = -1;
+        foreach ((int removedIndex, int addedIndex) in GetAllCombinations(removed.Length, added.Length))
+        {
+            float score = GetWordMatchScore(removed[removedIndex], added[addedIndex]);
+            if (maxScore < score)
+            {
+                maxScore = score;
+                removedMaxScoreIndex = removedIndex;
+                addedMaxScoreIndex = addedIndex;
+                if (maxScore == 1)
+                {
+                    return (removedMaxScoreIndex, addedMaxScoreIndex);
+                }
+            }
+        }
+
+        const float insignificantWordMatchScore = 0.1f;
+        return maxScore <= insignificantWordMatchScore ? (0, 0) : (removedMaxScoreIndex, addedMaxScoreIndex);
+
+        static float GetWordMatchScore(LineData r, LineData a)
+        {
+            if (r.Words.Count == 0 || a.Words.Count == 0)
+            {
+                return -1;
+            }
+
+            return (float)r.Words.Intersect(a.Words).Sum(w => w.Length) / Math.Max(r.WordsTotalLength, a.WordsTotalLength);
+        }
+    }
+
+    /// <summary>
+    ///  Iterates all combinations of indices - starting with (0,0), (1,0), (0,1), (2,0), (1,1), ...
+    /// </summary>
+    /// <returns>an enumeration of the index pairs</returns>
+    internal static IEnumerable<(int FirstIndex, int SecondIndex)> GetAllCombinations(int firstEnd, int secondEnd)
+    {
+        // upper left half including prinicipal diagonal
+        for (int diagonalIndex = 0; diagonalIndex < firstEnd; ++diagonalIndex)
+        {
+            int diagonalEnd = Math.Min(diagonalIndex + 1, secondEnd);
+            for (int secondIndex = 0; secondIndex < diagonalEnd; ++secondIndex)
+            {
+                yield return (FirstIndex: diagonalIndex - secondIndex, secondIndex);
+            }
+        }
+
+        // lower right half
+        for (int diagonalIndex = 1; diagonalIndex < secondEnd; ++diagonalIndex)
+        {
+            int diagonalEnd = Math.Min(firstEnd + diagonalIndex, secondEnd);
+            for (int secondIndex = diagonalIndex; secondIndex < diagonalEnd; ++secondIndex)
+            {
+                yield return (FirstIndex: firstEnd - 1 + diagonalIndex - secondIndex, secondIndex);
+            }
+        }
     }
 
     internal static IEnumerable<string> GetWords(string text, Func<char, bool> isWordChar)
@@ -118,6 +175,7 @@ internal static class LinesMatcher
         internal string Full { get; }
         internal string Trimmed { get; }
         internal IReadOnlySet<string> Words { get; }
+        internal int WordsTotalLength { get; }
 
         internal LineData(ISegment line, string text)
         {
@@ -125,6 +183,7 @@ internal static class LinesMatcher
             Full = text;
             Trimmed = text.Trim();
             Words = GetWords(Trimmed, TextUtilities.IsLetterDigitOrUnderscore).ToHashSet();
+            WordsTotalLength = Words.Sum(w => w.Length);
         }
     }
 }
---
+++ tests/app/UnitTests/GitUI.Tests/Editor/Diff/LinesMatcherTests.cs
@@ -0,0 +1,152 @@
+﻿using FluentAssertions;
+using GitUI.Editor.Diff;
+using ICSharpCode.TextEditor.Document;
+
+namespace GitUITests.Editor.Diff;
+
+[TestFixture]
+public class LinesMatcherTests
+{
+    [Test]
+    public void GetAllCombinations([Range(1, 4)] int firstEnd, [Range(1, 4)] int secondEnd)
+    {
+        bool[,] visited = new bool[firstEnd, secondEnd];
+        foreach ((int firstIndex, int secondIndex) in LinesMatcher.GetAllCombinations(firstEnd, secondEnd))
+        {
+            firstIndex.Should().BeLessThan(firstEnd);
+            secondIndex.Should().BeLessThan(secondEnd);
+            visited[firstIndex, secondIndex].Should().BeFalse();
+            visited[firstIndex, secondIndex] = true;
+        }
+
+        for (int i = 0; i < firstEnd; ++i)
+        {
+            for (int j = 0; j < secondEnd; ++j)
+            {
+                visited[i, j].Should().BeTrue($"everywhere including at {i}, {j}");
+            }
+        }
+    }
+
+    [Test]
+    [TestCase("", new string[] { })]
+    [TestCase("a", new string[] { "a" })]
+    [TestCase("a-", new string[] { "a" })]
+    [TestCase("-a", new string[] { "a" })]
+    [TestCase("a bc", new string[] { "a", "bc" })]
+    [TestCase("-a bc", new string[] { "a", "bc" })]
+    [TestCase("a bc-", new string[] { "a", "bc" })]
+    [TestCase("---abc---123---def_7---", new string[] { "abc", "123", "def_7" })]
+    public void GetWords(string text, string[] words)
+    {
+        LinesMatcher.GetWords(text, TextUtilities.IsLetterDigitOrUnderscore)
+            .Should().BeEquivalentTo(words);
+    }
+
+    [Test]
+    public void FindLinePairs_shall_not_search_in_too_large_blocks()
+    {
+        const int removedCount = 10;
+        const int maxCombinations = 100 * 100;
+        LineSegment[] removedLines = CreateLines(removedCount);
+        LineSegment[] addedLines = CreateLines((maxCombinations / removedCount) + 1);
+        Dictionary<ISegment, string> lineTexts = new();
+        for (int index = 0; index < removedCount; ++index)
+        {
+            string lineText = $"line{index}";
+            lineTexts[removedLines[index]] = lineText;
+            lineTexts[addedLines[index + removedCount]] = lineText;
+        }
+
+        IEnumerable<(ISegment RemovedLine, ISegment AddedLine)> pairs = LinesMatcher.FindLinePairs(GetText, removedLines, addedLines);
+
+        int pairIndex = 0;
+        foreach ((ISegment removedLine, ISegment addedLine) in pairs)
+        {
+            removedLine.Should().Be(removedLines[pairIndex]);
+            addedLine.Should().Be(addedLines[pairIndex]);
+            ++pairIndex;
+        }
+
+        return;
+
+        string GetText(ISegment line) => lineTexts.GetValueOrDefault(line, "other line");
+    }
+
+    [Test]
+    public void FindLinePairs_shall_match_trimmed_lines()
+    {
+        LineSegment[] removedLines = CreateLines(3);
+        LineSegment[] addedLines = CreateLines(5);
+        Dictionary<ISegment, string> lineTexts = new()
+        {
+            { removedLines[0], "r0" },
+            { removedLines[1], " trimmed line\t" },
+            { removedLines[2], "r2" },
+            { addedLines[0], "a0" },
+            { addedLines[1], "a1" },
+            { addedLines[2], "a2" },
+            { addedLines[3], "trimmed line" },
+            { addedLines[4], "a4" },
+        };
+
+        IEnumerable<(ISegment RemovedLine, ISegment AddedLine)> pairs = LinesMatcher.FindLinePairs(GetText, removedLines, addedLines);
+
+        (ISegment RemovedLine, ISegment AddedLine)[] expectedPairs =
+        [
+            (removedLines[0], addedLines[0]),
+            (removedLines[1], addedLines[3]),
+            (removedLines[2], addedLines[4]),
+        ];
+        pairs.Should().BeEquivalentTo(expectedPairs);
+
+        return;
+
+        string GetText(ISegment line) => lineTexts.GetValueOrDefault(line, "other line");
+    }
+
+    [Test]
+    public void FindLinePairs_shall_match_lines_whose_common_words_have_maximum_summedup_length()
+    {
+        LineSegment[] removedLines = CreateLines(4);
+        LineSegment[] addedLines = CreateLines(5);
+        Dictionary<ISegment, string> lineTexts = new()
+        {
+            { removedLines[0], "line 0 had some words" },
+            { removedLines[1], "line 1 had some more common words" },
+            { removedLines[2], " trimmed line\t" },
+            { removedLines[3], "r2" },
+            { addedLines[0], "some words" },
+            { addedLines[1], "line 1 has some words" },
+            { addedLines[2], "line 2 has some common words" },
+            { addedLines[3], "trimmed line" },
+            { addedLines[4], "a4" },
+        };
+
+        IEnumerable<(ISegment RemovedLine, ISegment AddedLine)> pairs = LinesMatcher.FindLinePairs(GetText, removedLines, addedLines);
+
+        (ISegment RemovedLine, ISegment AddedLine)[] expectedPairs =
+        [
+            (removedLines[0], addedLines[1]), // longer common words
+            (removedLines[1], addedLines[2]), // most long common words
+            (removedLines[2], addedLines[3]), // trimmed
+            (removedLines[3], addedLines[4]), // single remaining lines after best match
+        ];
+        pairs.Should().BeEquivalentTo(expectedPairs);
+
+        return;
+
+        string GetText(ISegment line) => lineTexts.GetValueOrDefault(line, "other line");
+    }
+
+    private static LineSegment[] CreateLines(int count)
+    {
+        LineSegment[] lines = new LineSegment[count];
+        for (int index = 0; index < count; ++index)
+        {
+            lines[index] = new LineSegment();
+        }
+
+        return lines;
+    }
+}
