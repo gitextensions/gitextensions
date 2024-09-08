686e405cb507efcdf7526d630ae556365501c70b
Donatas Mačiūnas <mdonatas@gmail.com>
2024-09-08 20:52:06 +0300

    Update src/app/GitExtUtils/GitUI/Interops/User32/GWL.cs
    Co-authored-by: Michael Seibt <36601201+mstv@users.noreply.github.com>

--- src/app/GitExtUtils/GitUI/Interops/User32/GWL.cs
+++ src/app/GitExtUtils/GitUI/Interops/User32/GWL.cs
@@ -5,12 +5,12 @@ internal static partial class NativeMethods
     /// <summary>
     ///  Window long values for <see cref="GetWindowLongPtrW(nint, GWL)"/>.
     /// </summary>
-    public enum GWL : int
+    internal enum GWL : int
     {
-        WNDPROC = (-4),
-        HWNDPARENT = (-8),
-        STYLE = (-16),
-        EXSTYLE = (-20),
-        ID = (-12),
+        WNDPROC = -4,
+        HWNDPARENT = -8,
+        STYLE = -16,
+        EXSTYLE = -20,
+        ID = -12,
     }
 }
