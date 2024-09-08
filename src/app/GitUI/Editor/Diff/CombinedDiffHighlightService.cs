a03ef8820a12e966a6222d09f0a692273e3ba922
Gerhard Olsson <gerhardol@users.noreply.github.com>
2024-09-08 13:23:33 +0200

    feat: Only parse diff files once
    Only parse the document for added/removed lines once, by using
    the DiffLineInfo info parsed for the linenumbering.
    Previously, the document was parsed many times every time the
    highlighting (including searching and marking) was changed.
    This simplifies the code.

--- src/app/GitUI/Editor/Diff/CombinedDiffHighlightService.cs
+++ src/app/GitUI/Editor/Diff/CombinedDiffHighlightService.cs
@@ -1,15 +1,10 @@
 using GitExtensions.Extensibility.Git;
-using GitExtUtils.GitUI.Theming;
-using GitUI.Theming;
-using ICSharpCode.TextEditor.Document;
 
 namespace GitUI.Editor.Diff;
 
 public class CombinedDiffHighlightService : DiffHighlightService
 {
     private static readonly string[] _diffFullPrefixes = ["  ", "++", "+ ", " +", "--", "- ", " -"];
-    private static readonly string[] _addedLinePrefixes = ["+", " +"];
-    private static readonly string[] _removedLinePrefixes = ["-", " -"];
 
     public CombinedDiffHighlightService(ref string text, bool useGitColoring, DiffViewerLineNumberControl lineNumbersControl)
         : base(ref text, useGitColoring)
@@ -22,21 +17,4 @@ public class CombinedDiffHighlightService : DiffHighlightService
         => GetGitCommandConfiguration(module, useGitColoring, "diff-tree");
 
     public override string[] GetFullDiffPrefixes() => _diffFullPrefixes;
-
-    protected override List<ISegment> GetAddedLines(IDocument document, ref int line, ref bool found)
-        => LinePrefixHelper.GetLinesStartingWith(document, _diffLinesInfo, DiffLineType.Plus, ref line, _addedLinePrefixes, ref found);
-
-    protected override List<ISegment> GetRemovedLines(IDocument document, ref int line, ref bool found)
-        => LinePrefixHelper.GetLinesStartingWith(document, _diffLinesInfo, DiffLineType.Minus, ref line, _removedLinePrefixes, ref found);
-
-    protected override int TryHighlightAddedAndDeletedLines(IDocument document, int line, LineSegment lineSegment)
-    {
-        ProcessLineSegment(document, ref line, lineSegment, "++", AppColor.AnsiTerminalGreenBackNormal.GetThemeColor());
-        ProcessLineSegment(document, ref line, lineSegment, "+ ", AppColor.AnsiTerminalGreenBackNormal.GetThemeColor());
-        ProcessLineSegment(document, ref line, lineSegment, " +", AppColor.AnsiTerminalGreenBackNormal.GetThemeColor());
-        ProcessLineSegment(document, ref line, lineSegment, "--", AppColor.AnsiTerminalRedBackNormal.GetThemeColor());
-        ProcessLineSegment(document, ref line, lineSegment, "- ", AppColor.AnsiTerminalRedBackNormal.GetThemeColor());
-        ProcessLineSegment(document, ref line, lineSegment, " -", AppColor.AnsiTerminalRedBackNormal.GetThemeColor());
-        return line;
-    }
 }
