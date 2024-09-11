118087e7f505ff13ec69f141c21740e1f8e610b0
Igor Velikorossov <RussKie@users.noreply.github.com>
2024-09-12 07:45:00 +1000

    Correctly escape paths (#11900)
    Fixes #11896
    Fixes #11897

--- src/plugins/FindLargeFiles/Properties/AssemblyInfo.cs
+++ src/plugins/FindLargeFiles/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDescription("GitExtensions plugin for finding large files")]
+
+[assembly:InternalsVisibleTo("FindLargeFiles.Tests")]
