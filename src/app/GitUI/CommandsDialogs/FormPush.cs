7c244e4a4628761c3c55f3faf247ba87d019a310
Philippe Miossec <pmiossec@gmail.com>
2024-09-23 10:19:20 +0200

    fix(push): fix tracking reference not well selected
    by retrieving the gitref corresponding to local branch combobox text
    
    fixes #11899

--- src/app/GitUI/CommandsDialogs/FormPush.cs
+++ src/app/GitUI/CommandsDialogs/FormPush.cs
@@ -780,24 +780,30 @@ namespace GitUI.CommandsDialogs
             {
                 if (PushToRemote.Checked)
                 {
-                    if (_NO_TRANSLATE_Branch.SelectedItem is GitRef branch)
+                    // Handle case where current branch is selected but SelectedItem is still null
+                    // because refs are lazy loaded until user interact with the control
+                    // (to improve performance on repos with a lot of local branches)
+                    IGitRef selectedBranchRef = _NO_TRANSLATE_Branch.SelectedItem as IGitRef;
+                    if (selectedBranchRef is null && !string.IsNullOrEmpty(_NO_TRANSLATE_Branch.Text))
                     {
-                        if (_selectedRemote is not null)
-                        {
-                            string? defaultRemote = _remotesManager.GetDefaultPushRemote(_selectedRemote, branch.Name);
-                            if (!string.IsNullOrEmpty(defaultRemote))
-                            {
-                                RemoteBranch.Text = defaultRemote;
-                                return;
-                            }
+                        selectedBranchRef = _gitRefs.FirstOrDefault(r => r.Name == _NO_TRANSLATE_Branch.Text);
+                    }
 
-                            if (branch.TrackingRemote.Equals(_selectedRemote.Name, StringComparison.OrdinalIgnoreCase))
+                    if (selectedBranchRef is not null && _selectedRemote is not null)
+                    {
+                        string? defaultRemote = _remotesManager.GetDefaultPushRemote(_selectedRemote, selectedBranchRef.Name);
+                        if (!string.IsNullOrEmpty(defaultRemote))
+                        {
+                            RemoteBranch.Text = defaultRemote;
+                            return;
+                        }
+
+                        if (selectedBranchRef.TrackingRemote.Equals(_selectedRemote.Name, StringComparison.OrdinalIgnoreCase))
+                        {
+                            RemoteBranch.Text = selectedBranchRef.MergeWith;
+                            if (!string.IsNullOrEmpty(RemoteBranch.Text))
                             {
-                                RemoteBranch.Text = branch.MergeWith;
-                                if (!string.IsNullOrEmpty(RemoteBranch.Text))
-                                {
-                                    return;
-                                }
+                                return;
                             }
                         }
                     }
