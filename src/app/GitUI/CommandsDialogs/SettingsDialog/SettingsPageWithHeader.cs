a6d047b17faf06b54e136c76817c9c1947b580b1
Philippe Miossec <pmiossec@gmail.com>
2024-09-25 11:50:08 +0200

    fix: Restrict settings to "global" when not in a repository
    and so when a setting could only be saved globally.
    
    Fixes #11907

--- src/app/GitUI/CommandsDialogs/SettingsDialog/SettingsPageWithHeader.cs
+++ src/app/GitUI/CommandsDialogs/SettingsDialog/SettingsPageWithHeader.cs
@@ -1,18 +1,25 @@
 ﻿using GitCommands;
+using GitExtensions.Extensibility.Git;
 using GitExtensions.Extensibility.Settings;
+using GitUIPluginInterfaces;
 
 namespace GitUI.CommandsDialogs.SettingsDialog
 {
     public partial class SettingsPageWithHeader : SettingsPageBase, IGlobalSettingsPage
     {
         private SettingsPageHeader? _header;
+        private bool _canSaveInsideRepo;
 
         public SettingsPageWithHeader(IServiceProvider serviceProvider)
             : base(serviceProvider)
         {
+            if (serviceProvider is IGitUICommands uiCommands)
+            {
+                _canSaveInsideRepo = uiCommands.Module.IsValidGitWorkingDir();
+            }
         }
 
-        public override Control GuiControl => _header ??= new SettingsPageHeader(this);
+        public override Control GuiControl => _header ??= new SettingsPageHeader(this, _canSaveInsideRepo);
 
         public virtual void SetGlobalSettings()
         {
