61c6b65391ed0c35cb20ff2338ddedc79e80b663
Michael Seibt <mstv@gmx.net>
2024-10-14 21:39:21 +0200

    fixup! feat: Fill OutputHistoryModel from FormStatus
    Split interface

--- src/app/GitUI/HelperDialogs/FormStatus.cs
+++ src/app/GitUI/HelperDialogs/FormStatus.cs
@@ -167,7 +167,7 @@ namespace GitUI.HelperDialogs
                 try
                 {
                     RunProcessInfo runProcessInfo = new(ProcessString, ProcessArguments, GetOutputString(), DateTime.Now);
-                    UICommands.GetRequiredService<IOutputHistoryModel>().RecordHistory(runProcessInfo);
+                    UICommands.GetRequiredService<IOutputHistoryRecorder>().RecordHistory(runProcessInfo);
                 }
                 catch (Exception exception)
                 {
