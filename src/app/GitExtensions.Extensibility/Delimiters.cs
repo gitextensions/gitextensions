000623e1898c158caf3a56453a309cdc0438a224
Michael Seibt <mstv@gmx.net>
2024-10-17 00:18:09 +0200

    chore: Sort Delimiters

--- src/app/GitExtensions.Extensibility/Delimiters.cs
+++ src/app/GitExtensions.Extensibility/Delimiters.cs
@@ -10,23 +10,24 @@ namespace GitExtensions.Extensibility;
 public static class Delimiters
 {
     public const char CarriageReturn = '\r';
-    public const char LineFeed = '\n';
-    public const char VerticalFeed = '\v';
-    public static readonly char[] LineAndVerticalFeed = [LineFeed, VerticalFeed];
-    public const char Space = ' ';
-    public const char Tab = '\t';
-    public const char Null = '\0';
-    public static readonly char[] TabAndSpace = [Tab, Space];
-    public static readonly char[] TabAndLineFeedAndCarriageReturn = [Tab, LineFeed, CarriageReturn];
-    public static readonly char[] NullAndLineFeed = [Null, LineFeed];
-    public static readonly char[] LineFeedAndCarriageReturn = [LineFeed, CarriageReturn];
-    public static readonly char[] LineFeedCarriageReturnAndNull = [LineFeed, CarriageReturn, Null];
-    public static readonly string[] NewLines = [$"{CarriageReturn}{LineFeed}", $"{LineFeed}"];
-    public static readonly char[] GitOutput = ['*', Space, LineFeed, CarriageReturn];
-    public const char ForwardSlash = '/';
     public const char Colon = ':';
     public const char Comma = ',';
-    public const char Period = '.';
+    public const char ForwardSlash = '/';
     public const char Hash = '#';
+    public const char LineFeed = '\n';
+    public const char Null = '\0';
+    public const char Period = '.';
+    public const char Space = ' ';
+    public const char Tab = '\t';
+    public const char VerticalFeed = '\v';
+
+    public static readonly char[] GitOutput = ['*', Space, LineFeed, CarriageReturn];
+    public static readonly char[] LineAndVerticalFeed = [LineFeed, VerticalFeed];
+    public static readonly char[] LineFeedAndCarriageReturn = [LineFeed, CarriageReturn];
+    public static readonly char[] LineFeedAndCarriageReturnAndNull = [LineFeed, CarriageReturn, Null];
+    public static readonly string[] NewLines = [$"{CarriageReturn}{LineFeed}", $"{LineFeed}"];
+    public static readonly char[] NullAndLineFeed = [Null, LineFeed];
     public static readonly char[] PathSeparators = [Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar];
+    public static readonly char[] TabAndLineFeedAndCarriageReturn = [Tab, LineFeed, CarriageReturn];
+    public static readonly char[] TabAndSpace = [Tab, Space];
 }
