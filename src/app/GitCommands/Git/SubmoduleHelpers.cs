4dc19c232b738f91f41d5e6aa9e987fc43f5a426
Michael Seibt <36601201+mstv@users.noreply.github.com>
2024-12-11 22:04:43 +0100

    fix(Submodules): Allow spaces in folder name
    - Fixup SubmoduleHelpers.DiffCommandRegex
    - Add test for CreatePatchFromString
    
    Refs: #12096

--- src/app/GitCommands/Git/SubmoduleHelpers.cs
+++ src/app/GitCommands/Git/SubmoduleHelpers.cs
@@ -7,7 +7,7 @@ namespace GitCommands.Git
 {
     public static partial class SubmoduleHelpers
     {
-        [GeneratedRegex(@"diff --git [^/\s]+/(?<filenamea>.+)\s[^/\s]+/(?<filenameb>.+)", RegexOptions.ExplicitCapture)]
+        [GeneratedRegex(@"diff --git\s+[ab]/(?<filenamea>.+)\s+[ba]/(?<filenameb>.+)", RegexOptions.ExplicitCapture)]
         private static partial Regex DiffCommandRegex();
         [GeneratedRegex(@"diff --cc (?<filenamea>.+)", RegexOptions.ExplicitCapture)]
         private static partial Regex CombinedDiffCommandRegex();
