9553799df49049957856a1abbef6439b5fb77228
Michael Seibt <mstv@gmx.net>
2024-12-31 00:59:50 +0100

    feat(FileStatusList): Add tree context menu items

---
+++ src/app/GitUI/UserControls/FileStatusList.TreeContextMenu.cs
@@ -0,0 +1,73 @@
+﻿using GitUI.Properties;
+using GitUI.UserControls;
+
+namespace GitUI;
+
+partial class FileStatusList
+{
+    private ToolStripMenuItem _collapse = new("&Collapse", Images.CollapseAll);
+    private ToolStripMenuItem _expand = new("&Expand", Images.ExpandAll);
+    private ToolStripMenuItem _selectAll = new("&Select all", Images.FileTree);
+    private ToolStripSeparator _treeContextMenuSeparator = new() { Name = nameof(_treeContextMenuSeparator) };
+
+    private void CreateTreeContextMenuItems()
+    {
+        _selectAll.Click += SelectAll_Click;
+        _collapse.Click += Collapse_Click;
+        _expand.Click += Expand_Click;
+    }
+
+    private void InsertTreeContextMenuItems(ToolStripItemCollection items, int index)
+    {
+        if (items.Find(_treeContextMenuSeparator.Name!, searchAllChildren: false).Length > 0)
+        {
+            return;
+        }
+
+        items.Insert(index++, _selectAll);
+        items.Insert(index++, _collapse);
+        items.Insert(index++, _expand);
+        items.Insert(index++, _treeContextMenuSeparator);
+    }
+
+    private void UpdateStatusOfTreeContextMenuItems()
+    {
+        bool hasSubnodes = FileStatusListView.FocusedNode?.Nodes.Count is > 0;
+
+        _collapse.Visible = hasSubnodes;
+        _expand.Visible = hasSubnodes;
+        _selectAll.Visible = hasSubnodes;
+        _treeContextMenuSeparator.Visible = hasSubnodes;
+
+        _collapse.Enabled = hasSubnodes && FileStatusListView.FocusedNode.IsExpanded;
+        _expand.Enabled = hasSubnodes;
+        _selectAll.Enabled = hasSubnodes;
+    }
+
+    private void Collapse_Click(object sender, EventArgs e)
+    {
+        FileStatusListView.FocusedNode?.Collapse(ignoreChildren: false);
+    }
+
+    private void Expand_Click(object sender, EventArgs e)
+    {
+        if (FileStatusListView.FocusedNode is not TreeNode node)
+        {
+            return;
+        }
+
+        node.ExpandAll();
+        FileStatusListView.FocusedNode = node;
+    }
+
+    private void SelectAll_Click(object sender, EventArgs e)
+    {
+        if (FileStatusListView.FocusedNode is not TreeNode node)
+        {
+            return;
+        }
+
+        node.ExpandAll();
+        FileStatusListView.SetSelectedNodes(node.Items().Where(node => node.Tag is FileStatusItem).ToHashSet(), focusedNode: node);
+    }
+}
