6a7d2dc963a78e1e8eeaf0d6b3446a8e4d3b61c5
Michael Seibt <mstv@gmx.net>
2025-01-31 20:37:34 +0100

    refactor: Extract IPersistentConfigValueStore
    from IConfigFileSettings
    Move implementation of GetValue<T> to ISettingsValueGetter
    Move implementation of SetPathValue to PathSetting.ConvertPathToGitSetting
    SettingsSource.SettingLevel must not be modified
    
    Refs: #12185

--- src/app/GitExtensions.Extensibility/Configurations/IConfigValueStore.cs
+++ src/app/GitExtensions.Extensibility/Configurations/IConfigValueStore.cs
@@ -1,9 +1,10 @@
+#nullable enable
+
 using GitExtensions.Extensibility.Settings;
 
 namespace GitExtensions.Extensibility.Configurations;
 
 public interface IConfigValueStore : ISettingsValueGetter
 {
-    void SetPathValue(string setting, string? value);
     void SetValue(string setting, string? value);
 }
