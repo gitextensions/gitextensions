91614d133dc44f6b3958de442c80c7ea75f8ddcc
Michael Seibt <mstv@gmx.net>
2025-02-05 18:57:38 +0100

    feat: Add GitConfigSettings
    and use it in GitConfigSettingsSet
    
    Refs: #12185

---
+++ src/app/GitExtensions.Extensibility/Git/GitSettingLevel.cs
@@ -0,0 +1,13 @@
+﻿namespace GitExtensions.Extensibility.Git;
+
+/// <summary>
+///  The scope of git config settings.
+/// </summary>
+public enum GitSettingLevel
+{
+    SystemWide,
+    Global,
+    Local,
+    ////WorkTree omitted because "git config --worktree" does not work without additional configuration
+    Effective
+}
