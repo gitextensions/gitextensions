76fcd0bb095d044a5cc29cd993d2fe727da2d4fa
Michael Seibt <mstv@gmx.net>
2025-02-13 00:48:13 +0100

    refactor: Replace ConfigFileSettings.CreateGlobal
    with GitConfigSettings
    
    Refs: #12201

--- src/app/GitCommands/Settings/AppSettings.cs
+++ src/app/GitCommands/Settings/AppSettings.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using GitCommands.Settings;
 using GitExtensions.Extensibility;
+using GitExtensions.Extensibility.Configurations;
 using GitExtensions.Extensibility.Git;
 using GitExtensions.Extensibility.Settings;
 using GitExtUtils.GitUI.Theming;
@@ -183,6 +184,8 @@ namespace GitCommands
                 SetString("AutoNormaliseSymbol", value);
             }
         }
+
+        public static string FileEditorCommand => @$"""{AppSettings.GetGitExtensionsFullPath()}"" fileeditor";
 
         public static bool RememberAmendCommitState
         {
@@ -747,9 +750,8 @@ namespace GitCommands
             }
 
             EnvironmentConfiguration.SetEnvironmentVariables();
-            ConfigFileSettings configFileGlobalSettings = ConfigFileSettings.CreateGlobal(useSharedCache: false);
-
-            string? path = configFileGlobalSettings.GetValue("core.editor");
+            IPersistentConfigValueStore globalSettings = new GitConfigSettings(new Executable(AppSettings.GitCommand), GitSettingLevel.Global);
+            string? path = globalSettings.GetValue("core.editor");
             if (path?.Contains("Program Files (x86)/GitExtensions", StringComparison.CurrentCultureIgnoreCase) is not true)
             {
                 return;
@@ -757,13 +759,10 @@ namespace GitCommands
 
 #if DEBUG
             // avoid setting, this may be in the debugger
-            throw new Exception($"Update the core.editor path {path} in {configFileGlobalSettings}");
+            throw new Exception($"Update the core.editor path {path} in global git settings");
 #else
-
-            // Similar in EditorHelper.FileEditorCommand
-            path = $"\"{AppSettings.GetGitExtensionsFullPath().ToPosixPath()}\" fileeditor";
-            configFileGlobalSettings.SetValue("core.editor", path);
-            configFileGlobalSettings.Save();
+            globalSettings.SetValue("core.editor", FileEditorCommand.ConvertPathToGitSetting());
+            globalSettings.Save();
 #endif
         }
 
