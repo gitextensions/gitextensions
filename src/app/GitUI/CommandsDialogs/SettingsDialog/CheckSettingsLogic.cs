40d3c71f7b20b27b31667940f933a45ae0dc4ded
Michael Seibt <mstv@gmx.net>
2025-02-13 00:48:13 +0100

    refactor: Replace ConfigFileSettings.CreateGlobal
    with GitConfigSettings

--- src/app/GitUI/CommandsDialogs/SettingsDialog/CheckSettingsLogic.cs
+++ src/app/GitUI/CommandsDialogs/SettingsDialog/CheckSettingsLogic.cs
@@ -1,7 +1,6 @@
 ﻿using GitCommands;
 using GitCommands.Utils;
 using GitExtensions.Extensibility.Git;
-using GitUI.CommandsDialogs.SettingsDialog.Pages;
 using Microsoft.Win32;
 
 namespace GitUI.CommandsDialogs.SettingsDialog
@@ -40,7 +39,7 @@ namespace GitUI.CommandsDialogs.SettingsDialog
 
             if (string.IsNullOrEmpty(editor))
             {
-                Environment.SetEnvironmentVariable(CommonLogic.AmbientGitEditorEnvVariableName, EditorHelper.FileEditorCommand);
+                Environment.SetEnvironmentVariable(CommonLogic.AmbientGitEditorEnvVariableName, AppSettings.FileEditorCommand);
             }
 
             return true;
