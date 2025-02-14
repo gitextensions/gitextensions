07670fccb62c482561fd7cbea0194240382b389c
Michael Seibt <mstv@gmx.net>
2025-02-14 22:40:21 +0100

    chore(GitConfigBaseSettingsPage): Unindent namespace

--- src/app/GitUI/CommandsDialogs/SettingsDialog/GitConfigBaseSettingsPage.cs
+++ src/app/GitUI/CommandsDialogs/SettingsDialog/GitConfigBaseSettingsPage.cs
@@ -3,67 +3,66 @@
 using GitExtensions.Extensibility.Settings;
 using Microsoft;
 
-namespace GitUI.CommandsDialogs.SettingsDialog
+namespace GitUI.CommandsDialogs.SettingsDialog;
+
+public partial class GitConfigBaseSettingsPage : SettingsPageWithHeader, IGitConfigSettingsPage
 {
-    public partial class GitConfigBaseSettingsPage : SettingsPageWithHeader, IGitConfigSettingsPage
+    public GitConfigBaseSettingsPage(IServiceProvider serviceProvider)
+        : base(serviceProvider)
     {
-        public GitConfigBaseSettingsPage(IServiceProvider serviceProvider)
-            : base(serviceProvider)
+    }
+
+    protected GitConfigSettingsSet GitConfigSettingsSet => CommonLogic.GitConfigSettingsSet;
+    protected SettingsSource? CurrentSettings { get; private set; }
+
+    protected override void Init(ISettingsPageHost pageHost)
+    {
+        base.Init(pageHost);
+
+        CurrentSettings = GitConfigSettingsSet.EffectiveSettings;
+    }
+
+    protected override SettingsSource GetCurrentSettings()
+    {
+        Validates.NotNull(CurrentSettings);
+        return CurrentSettings;
+    }
+
+    protected override void PageToSettings()
+    {
+        base.PageToSettings();
+        GitConfigSettingsSet.Save();
+    }
+
+    public void SetEffectiveSettings()
+    {
+        SetCurrentSettings(GitConfigSettingsSet.EffectiveSettings);
+    }
+
+    public void SetLocalSettings()
+    {
+        SetCurrentSettings(GitConfigSettingsSet.LocalSettings);
+    }
+
+    public override void SetGlobalSettings()
+    {
+        SetCurrentSettings(GitConfigSettingsSet.GlobalSettings);
+    }
+
+    public void SetSystemSettings()
+    {
+        SetCurrentSettings(GitConfigSettingsSet.SystemSettings);
+    }
+
+    private void SetCurrentSettings(SettingsSource settings)
+    {
+        if (CurrentSettings is not null)
         {
+            SaveSettings();
         }
 
-        protected GitConfigSettingsSet GitConfigSettingsSet => CommonLogic.GitConfigSettingsSet;
-        protected SettingsSource? CurrentSettings { get; private set; }
+        CurrentSettings = settings;
 
-        protected override void Init(ISettingsPageHost pageHost)
-        {
-            base.Init(pageHost);
-
-            CurrentSettings = GitConfigSettingsSet.EffectiveSettings;
-        }
-
-        protected override SettingsSource GetCurrentSettings()
-        {
-            Validates.NotNull(CurrentSettings);
-            return CurrentSettings;
-        }
-
-        protected override void PageToSettings()
-        {
-            base.PageToSettings();
-            GitConfigSettingsSet.Save();
-        }
-
-        public void SetEffectiveSettings()
-        {
-            SetCurrentSettings(GitConfigSettingsSet.EffectiveSettings);
-        }
-
-        public void SetLocalSettings()
-        {
-            SetCurrentSettings(GitConfigSettingsSet.LocalSettings);
-        }
-
-        public override void SetGlobalSettings()
-        {
-            SetCurrentSettings(GitConfigSettingsSet.GlobalSettings);
-        }
-
-        public void SetSystemSettings()
-        {
-            SetCurrentSettings(GitConfigSettingsSet.SystemSettings);
-        }
-
-        private void SetCurrentSettings(SettingsSource settings)
-        {
-            if (CurrentSettings is not null)
-            {
-                SaveSettings();
-            }
-
-            CurrentSettings = settings;
-
-            LoadSettings();
-        }
+        LoadSettings();
     }
 }
