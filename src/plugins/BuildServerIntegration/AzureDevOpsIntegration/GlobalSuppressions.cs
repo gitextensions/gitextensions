03e078e25160677669a632ce964eddfe497a77dc
Gerhard Olsson <6248932+gerhardol@users.noreply.github.com>
2025-02-20 22:34:06 +0100

    feat!: .net9 (#12064)
    suppress VSTHRD103,WFO1000
    
    Refs: #12064

---
+++ src/plugins/BuildServerIntegration/AzureDevOpsIntegration/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "VSTHRD103:Call async methods when in an async method", Justification = "<Pending>", Scope = "member", Target = "~M:AzureDevOpsIntegration.AzureDevOpsAdapter.EnsureBuildDefinitionsIsInitializedAsync(System.IObserver{GitExtensions.Extensibility.BuildServerIntegration.BuildInfo})~System.Threading.Tasks.Task{System.Boolean}")]
