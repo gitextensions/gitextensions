2af11903fc64bc01aa412a0a35bffb6fa4afcfe9
Michael Seibt <mstv@gmx.net>
2025-02-23 12:47:25 +0100

    chore(ContextMenuSelectionInfo): Move to own file
    Refs: #12149

---
+++ src/app/GitUI/CommandsDialogs/RevisionDiffContoller.ContextMenuSelectionInfo.cs
@@ -0,0 +1,21 @@
+﻿using GitExtensions.Extensibility.Git;
+using GitUIPluginInterfaces;
+
+namespace GitUI.CommandsDialogs;
+
+internal sealed record ContextMenuSelectionInfo(
+    GitRevision? SelectedRevision,
+    RelativePath? SelectedFolder,
+    bool IsDisplayOnlyDiff,
+    bool IsStatusOnly,
+    int SelectedGitItemCount,
+    bool IsAnyItemIndex,
+    bool IsAnyItemWorkTree,
+    bool IsBareRepository,
+    bool AllFilesExist,
+    bool AllDirectoriesExist,
+    bool AllFilesOrUntrackedDirectoriesExist,
+    bool IsAnyTracked,
+    bool SupportPatches,
+    bool IsDeleted,
+    bool IsAnySubmodule);
