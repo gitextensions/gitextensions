288cb118a78322081dfa5f7d4b588e30ce93b32d
Timwi <github@timwi.de>
2025-03-25 15:40:45 +0100

    Add keyboard mnemonics in “Remote Repositories” dialog, remote right-click menu, and “Push” dialog

--- src/app/GitUI/CommandsDialogs/FormPush.Designer.cs
+++ src/app/GitUI/CommandsDialogs/FormPush.Designer.cs
@@ -72,9 +72,9 @@
             groupBox2.SuspendLayout();
             menuPushSelection.SuspendLayout();
             SuspendLayout();
-            // 
+            //
             // ForcePushOptionPanel
-            // 
+            //
             ForcePushOptionPanel.AutoSize = true;
             ForcePushOptionPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             ForcePushOptionPanel.Controls.Add(ckForceWithLease);
@@ -84,9 +84,9 @@
             ForcePushOptionPanel.Name = "ForcePushOptionPanel";
             ForcePushOptionPanel.Size = new Size(194, 23);
             ForcePushOptionPanel.TabIndex = 0;
-            // 
+            //
             // ckForceWithLease
-            // 
+            //
             ckForceWithLease.AutoSize = true;
             ckForceWithLease.Location = new Point(0, 3);
             ckForceWithLease.Margin = new Padding(0, 3, 0, 3);
@@ -96,9 +96,9 @@
             ckForceWithLease.Text = "&Force with lease";
             ckForceWithLease.UseVisualStyleBackColor = true;
             ckForceWithLease.CheckedChanged += ForceWithLeaseCheckedChanged;
-            // 
+            //
             // ForcePushBranches
-            // 
+            //
             ForcePushBranches.AutoSize = true;
             ForcePushBranches.Location = new Point(112, 3);
             ForcePushBranches.Name = "ForcePushBranches";
@@ -107,9 +107,9 @@
             ForcePushBranches.Text = "F&orce push";
             ForcePushBranches.UseVisualStyleBackColor = true;
             ForcePushBranches.CheckedChanged += ForcePushBranchesCheckedChanged;
-            // 
+            //
             // PushToUrl
-            // 
+            //
             PushToUrl.AutoSize = true;
             PushToUrl.Location = new Point(14, 49);
             PushToUrl.Name = "PushToUrl";
@@ -119,9 +119,9 @@
             toolTip1.SetToolTip(PushToUrl, "Url to push to");
             PushToUrl.UseVisualStyleBackColor = true;
             PushToUrl.CheckedChanged += PushToUrlCheckedChanged;
-            // 
+            //
             // PushToRemote
-            // 
+            //
             PushToRemote.AutoSize = true;
             PushToRemote.Checked = true;
             PushToRemote.Location = new Point(14, 19);
@@ -132,9 +132,9 @@
             PushToRemote.Text = "&Remote";
             toolTip1.SetToolTip(PushToRemote, "Remote repository to push to");
             PushToRemote.UseVisualStyleBackColor = true;
-            // 
+            //
             // Push
-            // 
+            //
             Push.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             Push.DialogResult = DialogResult.OK;
             Push.Image = Properties.Images.ArrowUp;
@@ -145,9 +145,9 @@
             Push.TabIndex = 4;
             Push.UseVisualStyleBackColor = true;
             Push.Click += PushClick;
-            // 
+            //
             // TabControlTagBranch
-            // 
+            //
             TabControlTagBranch.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             TabControlTagBranch.Controls.Add(BranchTab);
             TabControlTagBranch.Controls.Add(TagTab);
@@ -159,9 +159,9 @@
             TabControlTagBranch.Size = new Size(560, 149);
             TabControlTagBranch.TabIndex = 1;
             TabControlTagBranch.Selected += TabControlTagBranch_Selected;
-            // 
+            //
             // BranchTab
-            // 
+            //
             BranchTab.Controls.Add(tableLayoutPanel1);
             BranchTab.Controls.Add(ShowOptions);
             BranchTab.Controls.Add(PushOptionsPanel);
@@ -174,9 +174,9 @@
             BranchTab.Text = "Push branches";
             BranchTab.ToolTipText = "Push branches and commits to remote repository.";
             BranchTab.UseVisualStyleBackColor = true;
-            // 
+            //
             // tableLayoutPanel1
-            // 
+            //
             tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             tableLayoutPanel1.AutoSize = true;
             tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
@@ -193,9 +193,9 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.Size = new Size(417, 22);
             tableLayoutPanel1.TabIndex = 1;
-            // 
+            //
             // _NO_TRANSLATE_Branch
-            // 
+            //
             _NO_TRANSLATE_Branch.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             _NO_TRANSLATE_Branch.AutoCompleteSource = AutoCompleteSource.ListItems;
             _NO_TRANSLATE_Branch.Dock = DockStyle.Fill;
@@ -208,9 +208,9 @@
             _NO_TRANSLATE_Branch.SelectedIndexChanged += _NO_TRANSLATE_Branch_SelectedIndexChanged;
             _NO_TRANSLATE_Branch.SelectedValueChanged += BranchSelectedValueChanged;
             _NO_TRANSLATE_Branch.Enter += _NO_TRANSLATE_Branch_Enter;
-            // 
+            //
             // labelTo
-            // 
+            //
             labelTo.AutoSize = true;
             labelTo.Location = new Point(200, 0);
             labelTo.Name = "labelTo";
@@ -219,9 +219,9 @@
             labelTo.TabIndex = 1;
             labelTo.Text = "&to";
             labelTo.TextAlign = ContentAlignment.MiddleCenter;
-            // 
+            //
             // RemoteBranch
-            // 
+            //
             RemoteBranch.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             RemoteBranch.AutoCompleteSource = AutoCompleteSource.ListItems;
             RemoteBranch.Dock = DockStyle.Fill;
@@ -231,9 +231,9 @@
             RemoteBranch.Name = "RemoteBranch";
             RemoteBranch.Size = new Size(197, 21);
             RemoteBranch.TabIndex = 2;
-            // 
+            //
             // ShowOptions
-            // 
+            //
             ShowOptions.AutoSize = true;
             ShowOptions.Location = new Point(6, 40);
             ShowOptions.Name = "ShowOptions";
@@ -242,9 +242,9 @@
             ShowOptions.TabStop = true;
             ShowOptions.Text = "Show options";
             ShowOptions.LinkClicked += ShowOptions_LinkClicked;
-            // 
+            //
             // flowLayoutPanel1
-            // 
+            //
             flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             flowLayoutPanel1.AutoSize = true;
             flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
@@ -256,9 +256,9 @@
             flowLayoutPanel1.Size = new Size(215, 25);
             flowLayoutPanel1.TabIndex = 2;
             flowLayoutPanel1.WrapContents = false;
-            // 
+            //
             // label2
-            // 
+            //
             label2.Anchor = AnchorStyles.Left;
             label2.AutoSize = true;
             label2.Location = new Point(0, 6);
@@ -267,9 +267,9 @@
             label2.Size = new Size(113, 13);
             label2.TabIndex = 0;
             label2.Text = "Recursive &submodules";
-            // 
+            //
             // RecursiveSubmodules
-            // 
+            //
             RecursiveSubmodules.Anchor = AnchorStyles.Left;
             RecursiveSubmodules.DropDownStyle = ComboBoxStyle.DropDownList;
             RecursiveSubmodules.Items.AddRange(new object[] {
@@ -281,9 +281,9 @@
             RecursiveSubmodules.Name = "RecursiveSubmodules";
             RecursiveSubmodules.Size = new Size(98, 21);
             RecursiveSubmodules.TabIndex = 1;
-            // 
+            //
             // ReplaceTrackingReference
-            // 
+            //
             ReplaceTrackingReference.AutoSize = true;
             ReplaceTrackingReference.Location = new Point(1, 29);
             ReplaceTrackingReference.Name = "ReplaceTrackingReference";
@@ -291,9 +291,9 @@
             ReplaceTrackingReference.TabIndex = 1;
             ReplaceTrackingReference.Text = "R&eplace tracking reference";
             ReplaceTrackingReference.UseVisualStyleBackColor = true;
-            // 
+            //
             // _createPullRequestCB
-            // 
+            //
             _createPullRequestCB.AutoSize = true;
             _createPullRequestCB.Location = new Point(1, 52);
             _createPullRequestCB.Name = "_createPullRequestCB";
@@ -301,18 +301,18 @@
             _createPullRequestCB.TabIndex = 3;
             _createPullRequestCB.Text = "&Create pull request after push";
             _createPullRequestCB.UseVisualStyleBackColor = true;
-            // 
+            //
             // labelFrom
-            // 
+            //
             labelFrom.AutoSize = true;
             labelFrom.Location = new Point(6, 15);
             labelFrom.Name = "labelFrom";
             labelFrom.Size = new Size(79, 13);
             labelFrom.TabIndex = 0;
             labelFrom.Text = "&Branch to push";
-            // 
+            //
             // TagTab
-            // 
+            //
             TagTab.Controls.Add(ForcePushTags);
             TagTab.Controls.Add(label1);
             TagTab.Controls.Add(TagComboBox);
@@ -324,9 +324,9 @@
             TagTab.Text = "Push tags";
             TagTab.ToolTipText = "Push tags to remote repository";
             TagTab.UseVisualStyleBackColor = true;
-            // 
+            //
             // ForcePushTags
-            // 
+            //
             ForcePushTags.AutoSize = true;
             ForcePushTags.Location = new Point(124, 43);
             ForcePushTags.Name = "ForcePushTags";
@@ -334,18 +334,18 @@
             ForcePushTags.TabIndex = 2;
             ForcePushTags.Text = "&Force push";
             ForcePushTags.UseVisualStyleBackColor = true;
-            // 
+            //
             // label1
-            // 
+            //
             label1.AutoSize = true;
             label1.Location = new Point(6, 15);
             label1.Name = "label1";
             label1.Size = new Size(64, 13);
             label1.TabIndex = 0;
             label1.Text = "&Tag to push";
-            // 
+            //
             // TagComboBox
-            // 
+            //
             TagComboBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             TagComboBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             TagComboBox.AutoCompleteSource = AutoCompleteSource.ListItems;
@@ -354,9 +354,9 @@
             TagComboBox.Name = "TagComboBox";
             TagComboBox.Size = new Size(262, 21);
             TagComboBox.TabIndex = 1;
-            // 
+            //
             // MultipleBranchTab
-            // 
+            //
             MultipleBranchTab.Controls.Add(BranchGrid);
             MultipleBranchTab.Location = new Point(4, 22);
             MultipleBranchTab.Name = "MultipleBranchTab";
@@ -365,9 +365,9 @@
             MultipleBranchTab.TabIndex = 2;
             MultipleBranchTab.Text = "Push multiple branches";
             MultipleBranchTab.UseVisualStyleBackColor = true;
-            // 
+            //
             // BranchGrid
-            // 
+            //
             BranchGrid.AllowUserToAddRows = false;
             BranchGrid.AllowUserToDeleteRows = false;
             BranchGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -389,50 +389,50 @@
             BranchGrid.CurrentCellDirtyStateChanged += BranchGrid_CurrentCellDirtyStateChanged;
             BranchGrid.DataBindingComplete += BranchGrid_DataBindingComplete;
             BranchGrid.CellPainting += BranchGrid_CellPainting;
-            // 
+            //
             // LocalColumn
-            // 
+            //
             LocalColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             LocalColumn.HeaderText = "Local Branch";
             LocalColumn.Name = "LocalColumn";
-            // 
+            //
             // RemoteColumn
-            // 
+            //
             RemoteColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             RemoteColumn.HeaderText = "Remote Branch";
             RemoteColumn.Name = "RemoteColumn";
-            // 
+            //
             // NewColumn
-            // 
+            //
             NewColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
             NewColumn.HeaderText = "Ahead/Behind";
             NewColumn.Name = "NewColumn";
             NewColumn.ReadOnly = true;
             NewColumn.Width = 97;
-            // 
+            //
             // PushColumn
-            // 
+            //
             PushColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
             PushColumn.HeaderText = "Push";
             PushColumn.Name = "PushColumn";
             PushColumn.Width = 36;
-            // 
+            //
             // ForceColumn
-            // 
+            //
             ForceColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
             ForceColumn.HeaderText = "Force";
             ForceColumn.Name = "ForceColumn";
             ForceColumn.Width = 101;
-            // 
+            //
             // DeleteColumn
-            // 
+            //
             DeleteColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
             DeleteColumn.HeaderText = "Delete Remote Branch";
             DeleteColumn.Name = "DeleteColumn";
             DeleteColumn.Width = 108;
-            // 
+            //
             // LoadSSHKey
-            // 
+            //
             LoadSSHKey.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             LoadSSHKey.Image = Properties.Images.Putty;
             LoadSSHKey.ImageAlign = ContentAlignment.MiddleLeft;
@@ -440,12 +440,12 @@
             LoadSSHKey.Name = "LoadSSHKey";
             LoadSSHKey.Size = new Size(137, 25);
             LoadSSHKey.TabIndex = 3;
-            LoadSSHKey.Text = "Load SSH key";
+            LoadSSHKey.Text = "&Load SSH key";
             LoadSSHKey.UseVisualStyleBackColor = true;
             LoadSSHKey.Click += LoadSshKeyClick;
-            // 
+            //
             // groupBox2
-            // 
+            //
             groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             groupBox2.Controls.Add(PushToRemote);
             groupBox2.Controls.Add(_NO_TRANSLATE_Remotes);
@@ -459,9 +459,9 @@
             groupBox2.TabIndex = 0;
             groupBox2.TabStop = false;
             groupBox2.Text = "Push to";
-            // 
+            //
             // _NO_TRANSLATE_Remotes
-            // 
+            //
             _NO_TRANSLATE_Remotes.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             _NO_TRANSLATE_Remotes.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             _NO_TRANSLATE_Remotes.AutoCompleteSource = AutoCompleteSource.ListItems;
@@ -474,9 +474,9 @@
             _NO_TRANSLATE_Remotes.SelectedIndexChanged += RemotesUpdated;
             _NO_TRANSLATE_Remotes.TextUpdate += RemotesUpdated;
             _NO_TRANSLATE_Remotes.Validated += RemotesValidated;
-            // 
+            //
             // AddRemote
-            // 
+            //
             AddRemote.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             AddRemote.Image = Properties.Images.Remotes;
             AddRemote.ImageAlign = ContentAlignment.MiddleLeft;
@@ -487,9 +487,9 @@
             AddRemote.Text = "&Manage remotes";
             AddRemote.UseVisualStyleBackColor = true;
             AddRemote.Click += AddRemoteClick;
-            // 
+            //
             // PushDestination
-            // 
+            //
             PushDestination.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             PushDestination.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             PushDestination.AutoCompleteSource = AutoCompleteSource.ListItems;
@@ -499,9 +499,9 @@
             PushDestination.Name = "PushDestination";
             PushDestination.Size = new Size(262, 21);
             PushDestination.TabIndex = 4;
-            // 
+            //
             // folderBrowserButton1
-            // 
+            //
             folderBrowserButton1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             folderBrowserButton1.AutoSize = true;
             folderBrowserButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
@@ -511,9 +511,9 @@
             folderBrowserButton1.PathShowingControl = PushDestination;
             folderBrowserButton1.Size = new Size(0, 0);
             folderBrowserButton1.TabIndex = 5;
-            // 
+            //
             // Pull
-            // 
+            //
             Pull.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             Pull.Location = new Point(11, 253);
             Pull.Name = "Pull";
@@ -522,9 +522,9 @@
             Pull.Text = "P&ull";
             Pull.UseVisualStyleBackColor = true;
             Pull.Click += PullClick;
-            // 
+            //
             // PushOptionsPanel
-            // 
+            //
             PushOptionsPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             PushOptionsPanel.Controls.Add(flowLayoutPanel1);
             PushOptionsPanel.Controls.Add(ReplaceTrackingReference);
@@ -535,39 +535,39 @@
             PushOptionsPanel.Size = new Size(537, 83);
             PushOptionsPanel.TabIndex = 3;
             PushOptionsPanel.Visible = false;
-            // 
+            //
             // contextMenuStripPush
-            // 
+            //
             menuPushSelection.Items.AddRange(new ToolStripItem[] {
             unselectAllToolStripMenuItem,
             selectTrackedToolStripMenuItem,
             selectAllToolStripMenuItem});
             menuPushSelection.Name = "menuPushSelection";
             menuPushSelection.Size = new Size(181, 92);
-            // 
+            //
             // unselectAllToolStripMenuItem
-            // 
+            //
             unselectAllToolStripMenuItem.Name = "unselectAllToolStripMenuItem";
             unselectAllToolStripMenuItem.Size = new Size(180, 22);
             unselectAllToolStripMenuItem.Text = "Unselect all";
             unselectAllToolStripMenuItem.Click += unselectAllToolStripMenuItem_Click;
-            // 
+            //
             // selectTrackedToolStripMenuItem
-            // 
+            //
             selectTrackedToolStripMenuItem.Name = "selectTrackedToolStripMenuItem";
             selectTrackedToolStripMenuItem.Size = new Size(180, 22);
             selectTrackedToolStripMenuItem.Text = "Select tracked";
             selectTrackedToolStripMenuItem.Click += selectTrackedToolStripMenuItem_Click;
-            // 
+            //
             // selectAllToolStripMenuItem
-            // 
+            //
             selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
             selectAllToolStripMenuItem.Size = new Size(180, 22);
             selectAllToolStripMenuItem.Text = "Select all";
             selectAllToolStripMenuItem.Click += selectAllToolStripMenuItem_Click;
-            // 
+            //
             // FormPush
-            // 
+            //
             AcceptButton = Push;
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
