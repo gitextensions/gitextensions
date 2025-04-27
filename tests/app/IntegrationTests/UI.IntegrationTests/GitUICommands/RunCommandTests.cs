2a0629e0b3ff2542ccd42845f9c0c06ac2c49080
Michael Seibt <mstv@gmx.net>
2025-04-27 14:36:33 +0200

    fix(RunCommandTests): Adapt expectation
    of tc RunCommandBasedOnArgument_settings and reactivate it
    
    Refs: #12338

--- tests/app/IntegrationTests/UI.IntegrationTests/GitUICommands/RunCommandTests.cs
+++ tests/app/IntegrationTests/UI.IntegrationTests/GitUICommands/RunCommandTests.cs
@@ -273,10 +273,9 @@ namespace GitUITests.GitUICommandsTests
         public void RunCommandBasedOnArgument_searchfile()
             => RunCommandBasedOnArgument<SearchWindow<string>>(new string[] { "ge.exe", "searchfile" }, expectedResult: false);
 
-        [Ignore("FormSettings throws unhandled exceptions when opened this way, works from real command line")]
         [Test]
         public void RunCommandBasedOnArgument_settings()
-            => RunCommandBasedOnArgument<FormSettings>(new string[] { "ge.exe", "settings" });
+            => RunCommandBasedOnArgument<FormSettings>(new string[] { "ge.exe", "settings" }, expectedResult: false /* because dialog is not closed using OK button */);
 
         [Test]
         public void RunCommandBasedOnArgument_stash()
