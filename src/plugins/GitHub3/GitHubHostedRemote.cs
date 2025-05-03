b49f5bd98a7079c4c20650cf37fd1b195f99f8a0
Muhammad Hamza Noor <hnalpha323@gmail.com>
2025-05-03 10:28:06 -0400

    ref : fix-foldername-GitHub3
    Fix folder name to match namespace, coding style and solution file.

---
+++ src/plugins/GitHub3/GitHubHostedRemote.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+using GitExtensions.Extensibility.Plugins;
+using GitUIPluginInterfaces.RepositoryHosts;
+
+namespace GitExtensions.Plugins.GitHub3
+{
+    [DebuggerDisplay("{Data}")]
+    internal class GitHubHostedRemote : IHostedRemote
+    {
+        private GitHubRepo? _repo;
+
+        public GitHubHostedRemote(string name, string owner, string remoteRepositoryName, string url)
+        {
+            Name = name;
+            Owner = owner;
+            RemoteRepositoryName = remoteRepositoryName;
+            RemoteUrl = url;
+            CloneProtocol = url.IsUrlUsingHttp() ? GitProtocol.Https : GitProtocol.Ssh;
+        }
+
+        public IHostedRepository GetHostedRepository()
+        {
+            return _repo ??= new GitHubRepo(GitHub3Plugin.GitHub.getRepository(Owner, RemoteRepositoryName))
+            {
+                CloneProtocol = CloneProtocol
+            };
+        }
+
+        /// <summary>
+        /// Local name of the remote, 'origin'
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Owner of the remote repository, in
+        /// git@github.com:mabako/Git.hub.git this is 'mabako'
+        /// </summary>
+        public string Owner { get; }
+
+        /// <summary>
+        /// Name of the remote repository, in
+        /// git@github.com:mabako/Git.hub.git this is 'Git.hub'
+        /// </summary>
+        public string RemoteRepositoryName { get; }
+
+        public string RemoteUrl { get; }
+
+        public GitProtocol CloneProtocol { get; }
+
+        public string Data => Owner + "/" + RemoteRepositoryName;
+        public string DisplayData => Data;
+        public bool IsOwnedByMe => GitHubLoginInfo.Username == Owner;
+
+        public string GetBlameUrl(string commitHash, string fileName, int lineIndex)
+        {
+            return $"{GitHub3Plugin.Instance.GitHubEndpoint}/{Data}/blame/{commitHash}/{fileName}#L{lineIndex}";
+        }
+    }
+}
---
+++ src/plugins/GitHub3/GitHubPullRequest.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Text;
+using Git.hub;
+using GitExtensions.Extensibility.Plugins;
+
+namespace GitExtensions.Plugins.GitHub3
+{
+    internal class GitHubPullRequest : IPullRequestInformation
+    {
+        private readonly PullRequest _pullRequest;
+
+        public GitHubPullRequest(PullRequest pullRequest)
+        {
+            _pullRequest = pullRequest;
+        }
+
+        public string Title => _pullRequest.Title;
+
+        public string Body => _pullRequest.Body;
+
+        public string Owner => _pullRequest.User.Login;
+
+        public DateTime Created => _pullRequest.CreatedAt;
+
+        private string? _diffData;
+
+        public async Task<string> GetDiffDataAsync()
+        {
+            if (_diffData is null)
+            {
+#pragma warning disable SYSLIB0014 // 'WebRequest.Create(string)' is obsolete
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_pullRequest.DiffUrl);
+#pragma warning restore SYSLIB0014 // 'WebRequest.Create(string)' is obsolete
+                using WebResponse response = await request.GetResponseAsync();
+                using StreamReader reader = new(response.GetResponseStream(), Encoding.UTF8);
+                _diffData = await reader.ReadToEndAsync();
+            }
+
+            return _diffData;
+        }
+
+        private IHostedRepository? _baseRepo;
+        public IHostedRepository BaseRepo => _baseRepo ??= new GitHubRepo(_pullRequest.Base.Repo);
+
+        private IHostedRepository? _headRepo;
+        public IHostedRepository HeadRepo => _headRepo ??= new GitHubRepo(_pullRequest.Head.Repo);
+
+        public string BaseSha => _pullRequest.Base.Sha;
+
+        public string HeadSha => _pullRequest.Head.Sha;
+
+        public string BaseRef => _pullRequest.Base.Ref;
+
+        public string HeadRef => _pullRequest.Head.Ref;
+
+        public string Id => _pullRequest.Number.ToString();
+
+        public string DetailedInfo => string.Format("Base repo owner: {0}\nHead repo owner: {1}", BaseRepo.Owner, HeadRepo.Owner);
+        public string FetchBranch => string.Format("pr/n{0}_{1}", Id, HeadRef);
+
+        public void Close()
+        {
+            _pullRequest.Close();
+        }
+
+        private IPullRequestDiscussion? _discussion;
+
+        public IPullRequestDiscussion GetDiscussion()
+        {
+            return _discussion ??= new GitHubPullRequestDiscussion(_pullRequest);
+        }
+    }
+}
