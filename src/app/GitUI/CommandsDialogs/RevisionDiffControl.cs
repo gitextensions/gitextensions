5fb847dda82f1652a96059df934908c1064b5193
Donatas Mačiūnas <mdonatas@gmail.com>
2025-06-15 13:38:23 +0300

    Remember blame splitter position
    and set provided default position in RestoreFromSettings only if the setting hasn't been saved before
    
    Refs: #12255

--- src/app/GitUI/CommandsDialogs/RevisionDiffControl.cs
+++ src/app/GitUI/CommandsDialogs/RevisionDiffControl.cs
@@ -335,6 +335,7 @@ namespace GitUI.CommandsDialogs
             NestedSplitterManager nested = new(splitterManager, Name);
             nested.AddSplitter(DiffSplitContainer);
             nested.AddSplitter(LeftSplitContainer);
+            BlameControl.InitSplitterManager(nested);
         }
 
         public SplitContainer HorizontalSplitter => DiffSplitContainer;
