2633c3cab5980d541a32d09bd57e1899ab88f1ce
Igor Velikorossov <russkie@gmail.com>
2025-07-20 20:32:55 +1000

    Fix namespaces

--- tests/app/UnitTests/GitCommands.Tests/Git/GitBranchNameNormaliserTest.cs
+++ tests/app/UnitTests/GitCommands.Tests/Git/GitBranchNameNormaliserTest.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+﻿using AwesomeAssertions;
 using GitCommands.Git;
 
 namespace GitCommandsTests.Git
--- tests/plugins/UnitTests/DeleteUnusedBranches.Tests/GitBranchOutputCommandParserTests.cs
+++ tests/plugins/UnitTests/DeleteUnusedBranches.Tests/GitBranchOutputCommandParserTests.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+﻿using AwesomeAssertions;
 using GitExtensions.Plugins.DeleteUnusedBranches;
 
 namespace DeleteUnusedBranchesTests
