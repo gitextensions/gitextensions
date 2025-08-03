2d21e98fb9f2bfaff41685b5f66ca0c27bde6af1
Gerhard Olsson <gerhardol@users.noreply.github.com>
2025-08-03 23:53:14 +0200

    fix: simplify submodule helpers
    GitSubmoduleStatus status handling was very complex,
    status was not always evaluated (as it requires CommitData).
    
    * Split SubmoduleResources from LocalizationHelpers
    * Clean up SubmoduleHelpers name
    * Avoid GetMergeBase() call when setting submodule status.
    * Set submodule status always after parsing. It was not set in all
    situations. This could cause commitdata to be evaluated twice if
    new/old commit was done in separate branches.
    * Prepare methods in IGitModule and GitSubmoduleStatus to be removed.

--- src/app/GitUI/LeftPanel/SubmoduleNode.cs
+++ src/app/GitUI/LeftPanel/SubmoduleNode.cs
@@ -166,7 +166,7 @@ namespace GitUI.LeftPanel
             {
                 // Prefer submodule status, shows ahead/behind
                 await TaskScheduler.Default;
-                toolTip = LocalizationHelpers.ProcessSubmoduleStatus(
+                toolTip = SubmoduleResources.GetSubmoduleStatusText(
                     new GitModule(Info.Path),
                     Info.Detailed.RawStatus,
                     moduleIsParent: false,
