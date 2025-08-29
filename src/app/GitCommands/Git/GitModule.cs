gitextensions/gitextensions

commit 1bb9dad31922f7557acddabaed5de1faac24d7df

fix: present removed submodules (#12436)
- Submodule presentation in the file tree and diff did not
present removed submodules.

- In diffs, detect if the path is a submodule also if the path
does not exist.

src/app/GitCommands/Git/GitModule.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Frozen;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
@@ -769,7 +768,7 @@ namespace GitCommands
             {
                 "-z",
                 "--unmerged",
-                { !string.IsNullOrWhiteSpace(filename), "--" },
+                "--",
                 filename.QuoteNE()
             };
 
@@ -837,7 +836,7 @@ namespace GitCommands
             {
                 // optimized codepath, default is "--format={_gitTreeParser.GitTreeFormat}"
                 refName.Quote(),
-                { !string.IsNullOrWhiteSpace(filename), "--" },
+                "--",
                 filename.QuoteNE()
             };
             string output = _gitExecutable.GetOutput(args);
@@ -962,7 +961,7 @@ namespace GitCommands
             GitArgumentBuilder args = new("mergetool")
             {
                 { string.IsNullOrWhiteSpace(customTool), gui, $"--tool={customTool}" },
-                { !string.IsNullOrWhiteSpace(fileName), "--" },
+                "--",
                 fileName.ToPosixPath().QuoteNE()
             };
 
@@ -2561,10 +2560,7 @@ namespace GitCommands
         }
 
         public IReadOnlyList<GitItemStatus> GetTreeFiles(ObjectId commitId, bool full, CancellationToken cancellationToken = default)
-        {
-            IEnumerable<GitItem> tree = GetGitItemTree(commitId, full, cancellationToken);
-
-            return tree
+            => GetTree(commitId, full, cancellationToken: cancellationToken)
                 .Select(file => new GitItemStatus(file.Name)
                 {
                     // IsTracked is always true, only tracked are reported
@@ -2578,7 +2574,6 @@ namespace GitCommands
                     IsSubmodule = file.ObjectType == GitObjectType.Commit
                 })
                 .ToList();
-        }
 
         public IReadOnlyList<GitItemStatus> GetAllChangedFiles(bool excludeIgnoredFiles = true,
             bool excludeAssumeUnchangedFiles = true, bool excludeSkipWorktreeFiles = true,
@@ -3184,15 +3179,17 @@ namespace GitCommands
                 .Split(Delimiters.NullAndLineFeed);
         }
 
-        public IEnumerable<INamedGitItem> GetTree(ObjectId? commitId, bool full, CancellationToken cancellationToken = default)
-            => GetGitItemTree(commitId, full, cancellationToken);
-
-        public IEnumerable<GitItem> GetGitItemTree(ObjectId? commitId, bool full, CancellationToken cancellationToken = default)
+        public IEnumerable<IObjectGitItem> GetTree(ObjectId? commitId, bool full, string fileName = "", CancellationToken cancellationToken = default)
         {
             bool isArtificial = commitId?.IsArtificial is true;
             if (isArtificial && !full)
             {
                 throw new ArgumentOutOfRangeException(nameof(full), "Artificial commit requires 'full'.");
+            }
+
+            if (commitId == ObjectId.CombinedDiffId)
+            {
+                throw new ArgumentOutOfRangeException(nameof(commitId), "Cannot use CombinedDiffId.");
             }
 
             GitArgumentBuilder args = isArtificial
@@ -3202,13 +3199,17 @@ namespace GitCommands
                     "-z",
                     { commitId == ObjectId.IndexId, "--cached", "--no-cached" },
                     @$"--format=""{_gitTreeParser.GitTreeFormat}""",
+                    "--",
+                    fileName.QuoteNE()
                 }
                 : new("ls-tree")
                 {
                     // optimized codepath, default is "--format={_gitTreeParser.GitTreeFormat}"
                     "-z",
                     { full, "-r" },
-                    commitId
+                    commitId,
+                    "--",
+                    fileName.QuoteNE()
                 };
 
             ExecutionResult result = _gitExecutable.Execute(args, cache: isArtificial ? null : GitCommandCache, cancellationToken: cancellationToken);
@@ -3479,7 +3480,7 @@ namespace GitCommands
             }
         }
 
-        public string GetFileText(ObjectId id, Encoding encoding, bool stripAnsiEscapeCodes)
+        public string? GetFileText(ObjectId id, Encoding encoding, bool stripAnsiEscapeCodes)
         {
             GitArgumentBuilder args = new("cat-file")
             {
@@ -3487,55 +3488,22 @@ namespace GitCommands
                 id.ToString().QuoteNE()
             };
 
-            return _gitExecutable.GetOutput(
-                args,
-                cache: GitCommandCache,
-                outputEncoding: encoding,
-                stripAnsiEscapeCodes: stripAnsiEscapeCodes);
+            ExecutionResult exec = _gitExecutable.Execute(args, throwOnErrorExit: false, cache: GitCommandCache);
+            if (!exec.ExitedSuccessfully)
+            {
+                // blob did not exist, this could be a submodule that is removed
+                return null;
+            }
+
+            return exec.StandardOutput;
         }
 
         public ObjectId? GetFileBlobHash(string fileName, ObjectId objectId)
         {
-            if (objectId == ObjectId.WorkTreeId || objectId == ObjectId.CombinedDiffId)
-            {
-                throw new ArgumentException($"Tried to get blob for unsupported revision: {objectId} and file: {fileName}");
-            }
-
-            // TODO use regex for parsing
-            if (objectId == ObjectId.IndexId)
-            {
-                GitArgumentBuilder args = new("ls-files")
-                {
-                    "-s",
-                    { !string.IsNullOrWhiteSpace(fileName), "--" },
-                    fileName.QuoteNE()
-                };
-
-                // index
-                string[] lines = _gitExecutable.GetOutput(args).Split(Delimiters.TabAndSpace);
-
-                if (lines.Length >= 2)
-                {
-                    return ObjectId.Parse(lines[1]);
-                }
-            }
-            else
-            {
-                GitArgumentBuilder args = new("ls-tree")
-                {
-                    "-r",
-                    objectId,
-                    { !string.IsNullOrWhiteSpace(fileName), "--" },
-                    fileName.QuoteNE()
-                };
-                string[] lines = _gitExecutable.GetOutput(args).Split(Delimiters.TabAndSpace);
-                if (lines.Length >= 3)
-                {
-                    return ObjectId.Parse(lines[2]);
-                }
-            }
-
-            return null;
+            IEnumerable<IObjectGitItem> items = GetTree(objectId, full: true, fileName);
+            return items.Count() == 1 && items.First() is IObjectGitItem i && i.ObjectType is GitObjectType.Blob
+                ? i.ObjectId
+                : null;
         }
 
         public Task<MemoryStream?> GetFileStreamAsync(string blob, CancellationToken cancellationToken)
@@ -3666,15 +3634,19 @@ namespace GitCommands
 
         public SubmoduleStatus CheckSubmoduleStatus(ObjectId? commit, ObjectId? oldCommit, CommitData? data, CommitData? oldData, bool loadData)
         {
-            // Submodule directory must exist to run commands, unknown otherwise
-            if (!IsValidGitWorkingDir() || oldCommit is null)
+            // Submodule directory must exist to run commands, just guess
+            if (oldCommit is null)
             {
                 return SubmoduleStatus.NewSubmodule;
             }
 
             if (commit is null)
             {
-                // Actually removed submodule, no special status for this uncommon status
+                return SubmoduleStatus.RemovedSubmodule;
+            }
+
+            if (!IsValidGitWorkingDir())
+            {
                 return SubmoduleStatus.Unknown;
             }
 

src/app/GitUI/Editor/FileViewer.cs
@@ -6,6 +6,7 @@ using System.Drawing.Imaging;
 using System.Text;
 using System.Text.RegularExpressions;
 using GitCommands;
+using GitCommands.Git;
 using GitCommands.Patches;
 using GitCommands.Settings;
 using GitCommands.Utils;
@@ -668,44 +669,43 @@ namespace GitUI.Editor
         /// <returns>Task to view the item</returns>
         private Task ViewGitItemAsync(GitItemStatus file, ObjectId? objectId, FileStatusItem? item, int? line, Action? openWithDifftool)
         {
-            if (objectId == ObjectId.WorkTreeId || file.Staged == StagedStatus.WorkTree)
-            {
-                // No blob exists for worktree, present contents from file system
-                return ViewFileAsync(file.Name, file.IsSubmodule, item, line, openWithDifftool);
-            }
-
-            file.TreeGuid ??= Module.GetFileBlobHash(file.Name, objectId);
-
             if (file.TreeGuid is null)
             {
-                return ViewTextAsync(file.Name, $"Cannot get treeId from Git for {file.Name} for commit {objectId}.");
-            }
-
-            string sha = file.TreeGuid.ToString();
-            bool isSubmodule = file.IsSubmodule;
-
-            if (!isSubmodule && file.IsNew && file.Staged == StagedStatus.Index)
-            {
-                // File system access for other than Worktree,
-                // to handle that git-status does not detect details for untracked (git-diff --no-index will not give info)
-                string fullPath = Path.Combine(Module.WorkingDir, file.Name);
-                if (Directory.Exists(fullPath) && GitModule.IsValidGitWorkingDir(fullPath))
+                IObjectGitItem[] items = Module.GetTree(objectId, full: true, file.Name).ToArray();
+                if (items.Count() == 1)
                 {
-                    isSubmodule = true;
+                    // set fields possibly not set from git-diff
+                    // (git-status does not report submodule, assume IsSubmodule is not set if not TreeGuid is)
+                    IObjectGitItem gitObject = items[0];
+                    file.IsSubmodule = gitObject.ObjectType == GitObjectType.Commit;
+                    file.TreeGuid ??= gitObject.ObjectId;
                 }
             }
 
+            if (file.TreeGuid is null)
+            {
+                string? fullPath = _fullPathResolver.Resolve(file.Name);
+                if (string.IsNullOrEmpty(fullPath))
+                {
+                    return ViewTextAsync(file.Name, $"Cannot get treeId from Git or path for {file.Name} for commit {objectId}.");
+                }
+
+                return ViewFileAsync(file.Name, file.IsSubmodule, item, line, openWithDifftool);
+            }
+
+            string sha = file.TreeGuid.ToString();
+
             return ViewItemAsync(
                 file.Name,
-                isSubmodule,
+                file.IsSubmodule,
                 getImage: () => ThreadHelper.JoinableTaskFactory.Run(GetImageAsync),
-                getFileText: GetFileTextIfBlobExists,
-                getSubmoduleText: () => LocalizationHelpers.GetSubmoduleText(Module, file.Name.TrimEnd('/'), sha, cache: true),
+                getFileText: GetFileText,
+                getSubmoduleText: () => LocalizationHelpers.GetSubmoduleText(Module, file.Name.TrimEnd('/'), sha, cache: objectId?.IsArtificial is false),
                 item: item,
                 line: line,
                 openWithDifftool: openWithDifftool);
 
-            string GetFileTextIfBlobExists()
+            string GetFileText()
             {
                 // If the file blob seem to be a diff file, get also escape sequences, that possibly are stored in the diff
                 // _viewMode is not set yet, similar check there
@@ -713,7 +713,7 @@ namespace GitUI.Editor
                     || (!file.Name.EndsWith(".diff", StringComparison.OrdinalIgnoreCase)
                        && !file.Name.EndsWith(".patch", StringComparison.OrdinalIgnoreCase));
                 FilePreamble = [];
-                return file.TreeGuid is not null ? Module.GetFileText(file.TreeGuid, Encoding, stripAnsiEscapeCodes) : string.Empty;
+                return Module.GetFileText(file.TreeGuid, Encoding, stripAnsiEscapeCodes) is string s ? s : "";
             }
 
             async Task<Image?> GetImageAsync()

src/app/GitCommands/Git/GitItem.cs
@@ -4,7 +4,7 @@ using GitExtensions.Extensibility.Git;
 namespace GitCommands.Git
 {
     [DebuggerDisplay("GitItem( {" + nameof(FileName) + "} )")]
-    public class GitItem : INamedGitItem
+    public class GitItem : IObjectGitItem
     {
         public GitItem(int mode, GitObjectType objectType, ObjectId objectId, string name)
         {
@@ -21,13 +21,5 @@ namespace GitCommands.Git
         public int Mode { get; }
 
         public string Guid => ObjectId.ToString();
-    }
-
-    public enum GitObjectType
-    {
-        None = 0,
-        Commit,
-        Tree,
-        Blob
     }
 }

src/app/GitExtensions.Extensibility/Git/GitObjectType.cs
@@ -1,0 +1,9 @@
+﻿namespace GitExtensions.Extensibility.Git;
+
+public enum GitObjectType
+{
+    None = 0,
+    Commit,
+    Tree,
+    Blob
+}

src/app/GitExtensions.Extensibility/Git/GitSubmoduleStatus.cs
@@ -33,7 +33,20 @@ public sealed class GitSubmoduleStatus
     {
         if (submodule is null)
         {
-            Status = SubmoduleStatus.NewSubmodule;
+            if (OldCommit is null)
+            {
+                // If there is no old commit, it is a new submodule.
+                Status = SubmoduleStatus.NewSubmodule;
+                return;
+            }
+
+            if (Commit is null)
+            {
+                Status = SubmoduleStatus.RemovedSubmodule;
+                return;
+            }
+
+            Status = SubmoduleStatus.Unknown;
             return;
         }
 

src/app/GitExtensions.Extensibility/Git/IGitModule.cs
@@ -21,7 +21,7 @@ public interface IGitModule
 
     IReadOnlyList<IGitRef> GetRefs(RefsFilter getRef);
     IEnumerable<string> GetSettings(string setting);
-    IEnumerable<INamedGitItem> GetTree(ObjectId? commitId, bool full, CancellationToken cancellationToken = default);
+    IEnumerable<IObjectGitItem> GetTree(ObjectId? commitId, bool full, string fileName = "", CancellationToken cancellationToken = default);
 
     /// <summary>
     ///  Loads the user-defined colors for the remote branches specific for the current repository.
@@ -436,7 +436,7 @@ public interface IGitModule
 
     bool ExistsMergeCommit(string? startRev, string? endRev);
 
-    string GetFileText(ObjectId id, Encoding encoding, bool stripAnsiEscapeCodes);
+    string? GetFileText(ObjectId id, Encoding encoding, bool stripAnsiEscapeCodes);
 
     Task<MemoryStream?> GetFileStreamAsync(string blob, CancellationToken cancellationToken);
 

src/app/GitExtensions.Extensibility/Git/IObjectGitItem.cs
@@ -1,0 +1,6 @@
+﻿namespace GitExtensions.Extensibility.Git;
+
+public interface IObjectGitItem : INamedGitItem
+{
+    GitObjectType ObjectType { get; }
+}

src/app/GitExtensions.Extensibility/Git/SubmoduleStatus.cs
@@ -4,6 +4,7 @@ public enum SubmoduleStatus
 {
     Unknown = 0,
     NewSubmodule,
+    RemovedSubmodule,
     FastForward,
     Rewind,
     NewerTime,

commit 53001072cadb7521e8a04a171d4cef0bb5b37882

amend! fix: read worktree files from the filesystem
fix: read worktree files from the filesystem

For index, the TreeGuid is mutable and must be refreshed every time it is used.
For worktree, the TreeGid is metadata but must not be used to to view files etc.
(if not dirty, the TreeGuid is for Index and could be OK,
that would be more complex than accessing the filesystem).

When viewing files, regression from 1bb9dad31922f7557acddabaed5de1faac24d7df
The TreeGuid is also used when linepatching, that usage was

src/app/GitCommands/Git/GitModule.cs
@@ -2568,13 +2568,15 @@ namespace GitCommands
                 .Select(file => new GitItemStatus(file.Name)
                 {
                     // IsTracked is always true, only tracked are reported
+                    // (all with TreeId are tracked)
+                    // TreeGuid for worktree reflects Index, just for reference
                     // New/Changed/Deleted are are just set
                     IsTracked = true,
                     IsNew = false,
                     IsChanged = false,
                     IsDeleted = false,
-                    TreeGuid = file.ObjectId,
                     Staged = StagedStatus.Unset,
+                    TreeGuid = file.ObjectId,
                     IsSubmodule = file.ObjectType == GitObjectType.Commit
                 })
                 .ToList();
@@ -3217,7 +3219,7 @@ namespace GitCommands
                     // optimized codepath, default is "--format={_gitTreeParser.GitTreeFormat}"
                     "-z",
                     { full, "-r" },
-                    commitId,
+                    { commitId is null, "HEAD", commitId.ToString() },
                     "--",
                     fileName.QuoteNE()
                 };
@@ -3510,9 +3512,9 @@ namespace GitCommands
 
         public ObjectId? GetFileBlobHash(string fileName, ObjectId objectId)
         {
-            IEnumerable<IObjectGitItem> items = GetTree(objectId, full: true, fileName);
-            return items.Count() == 1 && items.First() is IObjectGitItem i && i.ObjectType is GitObjectType.Blob
-                ? i.ObjectId
+            IObjectGitItem[] items = GetTree(objectId, full: true, fileName).ToArray();
+            return items.Length == 1 && items[0].ObjectType is GitObjectType.Blob
+                ? items[0].ObjectId
                 : null;
         }
 

src/app/GitUI/CommandsDialogs/FormFileHistory.cs
@@ -349,9 +349,10 @@ namespace GitUI.CommandsDialogs
                 GitItemStatus file = new(name: fileName)
                 {
                     IsTracked = true,
-                    IsSubmodule = GitModule.IsValidGitWorkingDir(_fullPathResolver.Resolve(fileName))
                 };
-                _ = View.ViewGitItemAsync(file, revision.ObjectId);
+                CancellationToken cancellationToken = _viewChangesSequence.Next();
+                View.TryUpdateTreeId(file, revision.ObjectId, cancellationToken);
+                _ = View.ViewGitItemAsync(file, revision.ObjectId, cancellationToken: cancellationToken);
             }
             else if (tabControl1.SelectedTab == DiffTab)
             {
@@ -359,13 +360,13 @@ namespace GitUI.CommandsDialogs
                 GitItemStatus file = new(name: fileName)
                 {
                     IsTracked = true,
-                    IsSubmodule = GitModule.IsValidGitWorkingDir(_fullPathResolver.Resolve(fileName))
                 };
+                CancellationToken cancellationToken = _viewChangesSequence.Next();
+                View.TryUpdateTreeId(file, revision.ObjectId, cancellationToken);
                 FileStatusItem item = new(firstRev: selectedRevisions.Count > 1 ? selectedRevisions[^1] : null,
                     secondRev: selectedRevisions.Count > 0 ? selectedRevisions[0] : null,
                     file);
-                _ = Diff.ViewChangesAsync(item, defaultText: TranslatedStrings.NoChanges,
-                    cancellationToken: _viewChangesSequence.Next());
+                _ = Diff.ViewChangesAsync(item, defaultText: TranslatedStrings.NoChanges, cancellationToken: cancellationToken);
             }
             else if (tabControl1.SelectedTab == CommitInfoTabPage)
             {

src/app/GitUI/Editor/FileViewer.cs
@@ -704,19 +704,12 @@ namespace GitUI.Editor
             // for worktree the TreeGuid is only valid if the file is not dirty, always get from file system
             bool useTreeId = objectId != ObjectId.WorkTreeId;
 
-            // for index the treeid is not immutable and must be evaluated
-            if (useTreeId && (file.TreeGuid is null || objectId == ObjectId.IndexId))
+            // for index the treeid is mutable and must be evaluated
+            if (useTreeId)
             {
-                cancellationToken.ThrowIfCancellationRequested();
-                IObjectGitItem[] items = Module.GetTree(objectId, full: true, file.Name, cancellationToken).ToArray();
-                if (items.Length == 1)
-                {
-                    // set fields possibly not set from git-diff
-                    // (git-status does not report submodule, assume IsSubmodule is not set if not TreeGuid is)
-                    IObjectGitItem gitObject = items[0];
-                    file.IsSubmodule = gitObject.ObjectType == GitObjectType.Commit;
-                    file.TreeGuid ??= gitObject.ObjectId;
-                }
+                // set fields possibly not set from git-diff
+                // (git-status does not report submodule, IsSubmodule is not set if not TreeGuid is)
+                TryUpdateTreeId(file, objectId, cancellationToken);
             }
 
             if (!useTreeId || file.TreeGuid is null)
@@ -730,14 +723,12 @@ namespace GitUI.Editor
                 return ViewFileAsync(file.Name, file.IsSubmodule, item, line, openWithDifftool, cancellationToken);
             }
 
-            string sha = file.TreeGuid.ToString();
-
             return ViewItemAsync(
                 file.Name,
                 file.IsSubmodule,
                 getImage: () => ThreadHelper.JoinableTaskFactory.Run(GetImageAsync),
                 getFileText: GetFileText,
-                getSubmoduleText: () => LocalizationHelpers.GetSubmoduleText(Module, file.Name.TrimEnd('/'), sha),
+                getSubmoduleText: () => LocalizationHelpers.GetSubmoduleText(Module, file.Name.TrimEnd('/'), file.TreeGuid.ToString()),
                 item: item,
                 line: line,
                 openWithDifftool: openWithDifftool,
@@ -758,7 +749,7 @@ namespace GitUI.Editor
             {
                 try
                 {
-                    using MemoryStream stream = await Module.GetFileStreamAsync(sha, cancellationToken: default);
+                    using MemoryStream stream = await Module.GetFileStreamAsync(file.TreeGuid.ToString(), cancellationToken: default);
                     if (stream is not null)
                     {
                         return CreateImage(file.Name, stream);
@@ -793,7 +784,8 @@ namespace GitUI.Editor
             Validates.NotNull(fullPath);
             DebugHelpers.Assert(Path.IsPathFullyQualified(fullPath), "Path must be resolved and fully qualified");
 
-            if (!isSubmodule)
+            // IsSubmodule may not be set, but if TreeGuid is set, IsSubmodule should also be set
+            if (!isSubmodule && item?.Item.TreeGuid is null)
             {
                 if (fileName.EndsWith("/") || Directory.Exists(fullPath))
                 {
@@ -1604,6 +1596,35 @@ namespace GitUI.Editor
         }
 
         /// <summary>
+        /// Update the current blob id for the GitItemStatus.
+        /// TreeId is immutable for normal commits, must always be updated before use for Index.
+        /// TreeId is irrelevant for worktree, but IsSubmodule is set.
+        /// TODO: add to GitModule, similar to GetFileBlobHash
+        /// </summary>
+        public ObjectId? TryUpdateTreeId(GitItemStatus file,
+            ObjectId? objectId,
+            CancellationToken cancellationToken = default)
+        {
+            if (file.TreeGuid is ObjectId treeId && objectId?.IsArtificial is false)
+            {
+                // current value is immutable
+                return file.TreeGuid;
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+            IObjectGitItem[] items = Module.GetTree(objectId, full: true, file.Name, cancellationToken).ToArray();
+            if (items.Length == 1)
+            {
+                IObjectGitItem gitItem = items[0];
+                file.IsSubmodule = gitItem.ObjectType == GitObjectType.Commit;
+                file.TreeGuid = gitItem.ObjectId;
+                return objectId == ObjectId.WorkTreeId ? null : file.TreeGuid;
+            }
+
+            return null;
+        }
+
+        /// <summary>
         /// Use implementation matching the current viewItem.
         /// </summary>
         private bool StageSelectedLines()
@@ -1657,6 +1678,7 @@ namespace GitUI.Editor
         /// <summary>
         /// Stage lines in WorkTree or Unstage lines in Index.
         /// </summary>
+        /// <param fileName="stage"><see langword="true"/> if current commit is worktree and the lines are to be staged, <see langword="false"/> otherwise.</param>
         public void StageSelectedLines(bool stage)
         {
             if (!AllowLinePatching || _viewItem is null)
@@ -1670,7 +1692,7 @@ namespace GitUI.Editor
             {
                 Validates.NotNull(FilePreamble);
 
-                string treeGuid = !stage ? _viewItem.Item.TreeGuid?.ToString() : null;
+                string? treeGuid = TryUpdateTreeId(_viewItem.Item, _viewItem.SecondRevision.ObjectId)?.ToString();
                 patch = PatchManager.GetSelectedLinesAsNewPatch(
                     Module,
                     _viewItem.Item.Name,
@@ -1733,7 +1755,7 @@ namespace GitUI.Editor
             {
                 Validates.NotNull(FilePreamble);
 
-                string treeGuid = currentItemStaged ? _viewItem.Item.TreeGuid?.ToString() : null;
+                string? treeGuid = TryUpdateTreeId(_viewItem.Item, _viewItem.SecondRevision.ObjectId)?.ToString();
                 patch = PatchManager.GetSelectedLinesAsNewPatch(
                     Module,
                     _viewItem.Item.Name,
@@ -1781,7 +1803,7 @@ namespace GitUI.Editor
         }
 
         /// <summary>
-        /// Cherry-pick/revert patches (not worktree or index).
+        /// Cherry-pick/revert patches (not worktree).
         /// </summary>
         /// <param name="reverse"><see langword="true"/> if patches is to be reversed; otherwise <see langword="false"/>.</param>.
         private void ApplySelectedLines(bool allFile, bool reverse)
@@ -1791,6 +1813,8 @@ namespace GitUI.Editor
                 // reload not completed
                 return;
             }
+
+            DebugHelpers.Assert(_viewItem.SecondRevision.ObjectId != ObjectId.WorkTreeId, "ApplySelectedLines() not supported for worktree");
 
             int selectionStart = allFile ? 0 : GetSelectionPosition();
             int selectionLength = allFile ? GetText().Length : GetSelectionLength();
@@ -1805,7 +1829,7 @@ namespace GitUI.Editor
             {
                 Validates.NotNull(FilePreamble);
 
-                string treeGuid = reverse ? _viewItem.Item.TreeGuid?.ToString() : null;
+                string? treeGuid = reverse ? TryUpdateTreeId(_viewItem.Item, _viewItem.SecondRevision.ObjectId)?.ToString() : null;
                 patch = PatchManager.GetSelectedLinesAsNewPatch(
                     Module,
                     _viewItem.Item.Name,

src/plugins/Statistics/GitStatistics/FormGitStatistics.cs
@@ -182,7 +182,7 @@ namespace GitExtensions.Plugins.GitStatistics
                 void LoadLinesOfCodeForModule(IGitModule module)
                 {
                     List<string> filesToCheck = module
-                        .GetTree(module.RevParse("HEAD"), full: true)
+                        .GetTree(commitId: null, full: true)
                         .Select(file => Path.Combine(module.WorkingDir, file.Name))
                         .ToList();
 