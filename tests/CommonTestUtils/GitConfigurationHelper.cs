59d9e5841abaa9f7ca009d369d1407ffe5cd5ce6
Jonathan Gilbert <logic@deltaq.org>
2025-10-07 12:32:57 -0500

    GitConfigurationHelper => namespace declaration instead of block

--- tests/CommonTestUtils/GitConfigurationHelper.cs
+++ tests/CommonTestUtils/GitConfigurationHelper.cs
@@ -1,49 +1,47 @@
-﻿using System.Diagnostics;
-using GitCommands;
+﻿using GitCommands;
 using GitCommands.Git;
 using GitExtensions.Extensibility;
 using GitExtensions.Extensibility.Git;
 
-namespace CommonTestUtils
+namespace CommonTestUtils;
+
+public sealed class GitConfigurationHelper
 {
-    public sealed class GitConfigurationHelper
+    private static Dictionary<string, string> _cachedConfiguration = new();
+
+    static GitConfigurationHelper()
     {
-        private static Dictionary<string, string> _cachedConfiguration = new();
-
-        static GitConfigurationHelper()
+        try
         {
-            try
+            Executable gitExecutable = new Executable(() => AppSettings.GitCommand, ".");
+
+            string packedSettings = gitExecutable.GetGitSettings(GitSettingLevel.Effective);
+
+            foreach (string line in packedSettings.LazySplit('\0'))
             {
-                Executable gitExecutable = new Executable(() => AppSettings.GitCommand, ".");
+                int separator = line.IndexOf('\n');
 
-                string packedSettings = gitExecutable.GetGitSettings(GitSettingLevel.Effective);
-
-                foreach (string line in packedSettings.LazySplit('\0'))
+                if (separator > 0)
                 {
-                    int separator = line.IndexOf('\n');
+                    string key = line.Substring(0, separator);
+                    string value = line.Substring(separator + 1);
 
-                    if (separator > 0)
-                    {
-                        string key = line.Substring(0, separator);
-                        string value = line.Substring(separator + 1);
-
-                        _cachedConfiguration[key] = value;
-                    }
+                    _cachedConfiguration[key] = value;
                 }
             }
-            catch
-            {
-            }
         }
-
-        public static string GetSetting(string key, string? defaultValue)
+        catch
         {
-            if (_cachedConfiguration.TryGetValue(key, out string assignedValue) && !string.IsNullOrWhiteSpace(assignedValue))
-            {
-                return assignedValue;
-            }
-
-            return defaultValue ?? "";
         }
+    }
+
+    public static string GetSetting(string key, string? defaultValue)
+    {
+        if (_cachedConfiguration.TryGetValue(key, out string assignedValue) && !string.IsNullOrWhiteSpace(assignedValue))
+        {
+            return assignedValue;
+        }
+
+        return defaultValue ?? "";
     }
 }
