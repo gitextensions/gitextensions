00da3398ab9a9b637bdfdee8ed9d43f05bee596f
Jonathan Gilbert <logic@deltaq.org>
2025-10-12 12:26:24 -0500

    Update Should_detect_CI_build to dump the environment to the test output.

--- tests/CommonTestUtils.Tests/GitModuleTestHelperTests.cs
+++ tests/CommonTestUtils.Tests/GitModuleTestHelperTests.cs
@@ -8,6 +8,11 @@ namespace CommonTestUtils.Tests
         [Test]
         public void Should_detect_CI_build()
         {
+            foreach (System.Collections.DictionaryEntry varName in Environment.GetEnvironmentVariables())
+            {
+                TestContext.WriteLine("ENV: {0}={1}", varName.Key, varName.Value);
+            }
+
             GitModuleTestHelper.IsCIBuild.Should().BeTrue();
         }
     }
