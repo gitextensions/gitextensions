8fca87f431d7f38c0ff190f392092c105a32798a
Gerhard Olsson <gerhardol@users.noreply.github.com>
2025-10-26 18:43:58 +0100

    fix: ls-files %objecttype Git version check
    Update the version check as %objecttype was introduced later than the
    --format option.
    Follow-up to #12501
    
    Note: The actual version in which %objecttype is supported
    is in 2.42, this reuses another constant
    to avoid breaking the Extensibility.Git version, so the fix is used in
    a few version not really needed.
    
    Update the minimal version that is somehow supported by
    usage to 2.43, used in Ubuntu 22.04.

--- src/app/GitCommands/Git/GitModule.cs
+++ src/app/GitCommands/Git/GitModule.cs
@@ -3246,7 +3246,7 @@ namespace GitCommands
                     { commitId == ObjectId.IndexId, "--cached", "--no-cached" },
 
                     // TODO change to GitVersion.SupportLsFilesFormat when Extensibility.Git can be updated
-                    { GitVersion.SupportUpdateRefs, @$"--format=""{_gitTreeParser.GitTreeFormat}""", "--stage" },
+                    { GitVersion.SupportNewGitConfigSyntax, @$"--format=""{_gitTreeParser.GitTreeFormat}""", "--stage" },
                     "--",
                     fileName.QuoteNE()
                 }
@@ -3262,7 +3262,7 @@ namespace GitCommands
 
             ExecutionResult result = _gitExecutable.Execute(args, cache: isArtificial ? null : GitCommandCache, cancellationToken: cancellationToken);
 
-            if (isArtificial && !GitVersion.SupportUpdateRefs)
+            if (isArtificial && !GitVersion.SupportNewGitConfigSyntax)
             {
                 return _gitTreeParser.ParseLsFiles(result.StandardOutput);
             }
