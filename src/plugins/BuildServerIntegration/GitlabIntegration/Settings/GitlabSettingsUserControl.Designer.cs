eaf8ac834973148f840663ed1a8af55e8ba19a5e
Jonathan Gilbert <logic@deltaq.org>
2025-11-01 17:33:26 -0500

    Switched a couple of "if (a != null) { a.Dispose(); }" calls to use the null-conditional operator. (#12619)

--- src/plugins/BuildServerIntegration/GitlabIntegration/Settings/GitlabSettingsUserControl.Designer.cs
+++ src/plugins/BuildServerIntegration/GitlabIntegration/Settings/GitlabSettingsUserControl.Designer.cs
@@ -13,9 +13,9 @@ partial class GitlabSettingsUserControl
     /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
     protected override void Dispose(bool disposing)
     {
-        if (disposing && (components != null))
+        if (disposing)
         {
-            components.Dispose();
+            components?.Dispose();
         }
         base.Dispose(disposing);
     }
