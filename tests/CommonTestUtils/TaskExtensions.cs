3a57529d53b9914d326f710ccea60a745b866462
Jonathan Gilbert <logic@deltaq.org>
2025-11-02 14:22:02 -0600

    Moved the other half of TaskExtensions.cs from GitExtUtils into CommonTestUtils.

--- tests/CommonTestUtils/TaskExtensions.cs
+++ tests/CommonTestUtils/TaskExtensions.cs
@@ -4,6 +4,17 @@ namespace GitExtUtils.Tasks;
 
 internal static class TaskExtensions
 {
+    // VSTHRD003: Avoid awaiting foreign Tasks. But, this extension method isn't
+    // really its own independent context, so these Tasks aren't foreign. This is
+    // just a tool for the callers to use to show intent.
+#pragma warning disable VSTHRD003
+    public static ConfiguredTaskAwaitable DetachFromSynchronizationContext(this Task task)
+        => task.ConfigureAwait(continueOnCapturedContext: false);
+
+    public static ConfiguredTaskAwaitable<TResult> DetachFromSynchronizationContext<TResult>(this Task<TResult> task)
+        => task.ConfigureAwait(continueOnCapturedContext: false);
+#pragma warning restore VSTHRD003
+
     // VSTHRD002: Synchronously waiting on tasks could cause deadlocks.
     // Specifically, if they're associated with a synchronization
     // context, and that synchronization context's message pump is the
