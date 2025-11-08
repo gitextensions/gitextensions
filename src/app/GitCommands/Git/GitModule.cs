4828c880211538fa5af08338845a53b15d9e8949
Gerhard Olsson <gerhardol@users.noreply.github.com>
2025-11-08 19:50:05 +0100

    fix: remove unused SubmoduleName

--- src/app/GitCommands/Git/GitModule.cs
+++ src/app/GitCommands/Git/GitModule.cs
@@ -108,15 +108,15 @@ public sealed partial class GitModule : IGitModule
 
         // If this is a submodule, populate relevant properties.
         // If this is not a submodule, these will all be null.
-        (SuperprojectModule, SubmodulePath, SubmoduleName) = InitialiseSubmoduleProperties();
+        (SuperprojectModule, SubmodulePath) = InitialiseSubmoduleProperties();
 
         return;
 
-        (GitModule? superprojectModule, string? submodulePath, string? submoduleName) InitialiseSubmoduleProperties()
+        (GitModule? superprojectModule, string? submodulePath) InitialiseSubmoduleProperties()
         {
             if (!IsValidGitWorkingDir())
             {
-                return (null, null, null);
+                return (null, null);
             }
 
             string currentPath = WorkingDir.RemoveTrailingPathSeparator();
@@ -139,7 +139,7 @@ public sealed partial class GitModule : IGitModule
                     // If we cannot read the .git file, assume it's not a submodule
                     // See also special handling of WSL .git symbolic links in PathUtil.IsWslLink()
                     // Symbolic links to submodule .git is not expected and not supported.
-                    return (null, null, null);
+                    return (null, null);
                 }
 
                 foreach (string line in lines)
@@ -163,26 +163,25 @@ public sealed partial class GitModule : IGitModule
                 }
             }
 
-            if (!string.IsNullOrEmpty(superprojectPath) && currentPath.ToPosixPath().StartsWith(superprojectPath.ToPosixPath()))
+            if (string.IsNullOrEmpty(superprojectPath) || !currentPath.ToPosixPath().StartsWith(superprojectPath.ToPosixPath()))
             {
-                string submodulePath = currentPath[superprojectPath.Length..].ToPosixPath();
-                ConfigFile configFile = new(Path.Combine(superprojectPath, ".gitmodules"));
-
-                foreach (IConfigSection configSection in configFile.ConfigSections)
-                {
-                    if (configSection.GetValue("path") == submodulePath.ToPosixPath())
-                    {
-                        string submoduleName = configSection.SubSection;
-                        GitModule superprojectModule = new(superprojectPath);
-
-                        return (superprojectModule, submodulePath, submoduleName);
-                    }
-                }
-
-                return (null, submodulePath, null);
+                return (null, null);
             }
 
-            return (null, null, null);
+            string submodulePath = currentPath[superprojectPath.Length..].ToPosixPath();
+            ConfigFile configFile = new(Path.Combine(superprojectPath, ".gitmodules"));
+
+            foreach (IConfigSection configSection in configFile.ConfigSections)
+            {
+                if (configSection.GetValue("path") == submodulePath.ToPosixPath())
+                {
+                    GitModule superprojectModule = new(superprojectPath);
+
+                    return (superprojectModule, submodulePath);
+                }
+            }
+
+            return (null, submodulePath);
 
             static bool HasGitModulesFile(string path)
                 => File.Exists(Path.Combine(path, ".gitmodules")) && IsValidGitWorkingDir(path);
@@ -213,12 +212,6 @@ public sealed partial class GitModule : IGitModule
 
     /// <inherit/>
     public string GetWindowsPath(string path) => PathUtil.GetWindowsPath(path, _wslDistro);
-
-    /// <summary>
-    /// If this module is a submodule, returns its name, otherwise <c>null</c>.
-    /// </summary>
-    // TODO: remove?
-    private string? SubmoduleName { get; }
 
     public string? SubmodulePath { get; }
 
