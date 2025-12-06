b804455ff81dd16af392867506b31dc352494114
Michael Seibt <mstv@gmx.net>
2025-12-06 11:17:39 +0100

    fix(PictureBox.OnPaint): Handle exceptions
    which can occur on broken images

---
+++ src/app/GitUI/UserControls/PictureBoxEx.cs
@@ -0,0 +1,22 @@
+﻿namespace GitUI.UserControls;
+
+/// <summary>
+///  A PictureBox which handles exceptions from OnPaint.
+/// </summary>
+public class PictureBoxEx : PictureBox
+{
+    public event EventHandler<Exception> PaintFailed;
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        try
+        {
+            base.OnPaint(e);
+        }
+        catch (Exception ex)
+        {
+            e.Graphics.Clear(SystemColors.Window);
+            PaintFailed?.Invoke(this, ex);
+        }
+    }
+}
