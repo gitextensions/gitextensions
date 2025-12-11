6ca344be9f52f60e12aae84d9ae0c5d4c74198bc
Michael Seibt <mstv@gmx.net>
2025-12-11 23:54:53 +0100

    fix(git status): Update after special rebase
    needed in case of autostash

--- src/app/GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
+++ src/app/GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
@@ -60,6 +60,7 @@ public sealed partial class RevisionGridControl : GitModuleControl, ICheckRefs, 
     public event EventHandler<DoubleClickRevisionEventArgs>? DoubleClickRevision;
     public event EventHandler<FilterChangedEventArgs>? FilterChanged;
     public event EventHandler? SelectionChanged;
+    public event EventHandler? ArtificialChanged;
 
     /// <summary>
     ///  Occurs whenever the revision graph has started loading the data.
@@ -3179,6 +3180,7 @@ public sealed partial class RevisionGridControl : GitModuleControl, ICheckRefs, 
 
         formProcess.ShowDialog(ParentForm);
         PerformRefreshRevisions();
+        ArtificialChanged?.Invoke(this, EventArgs.Empty);
     }
 
     #region Drag/drop patch files on revision grid
