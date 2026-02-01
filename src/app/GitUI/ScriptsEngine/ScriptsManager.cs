1fb76a28b51ee186ae5b6eebe1747bb893843ede
Gerhard Olsson <gerhardol@users.noreply.github.com>
2026-02-01 21:51:59 +0100

    feat: Add scripts lf/crlf conversion
    Added for new installations (when no scripts exists).
    Not enabled by default.
    
    The setup assumes that bash.exe is in the path.

--- src/app/GitUI/ScriptsEngine/ScriptsManager.cs
+++ src/app/GitUI/ScriptsEngine/ScriptsManager.cs
@@ -190,6 +190,30 @@ internal sealed partial class ScriptsManager : IScriptsManager, IScriptsRunner
                 OnEvent = 0,
                 AddToRevisionGridContextMenu = true,
                 Enabled = false
+            },
+            new ScriptInfo
+            {
+                HotkeyCommandIdentifier = 9006,
+                Name = "Convert workspace file to LF",
+                Command = "bash.exe",
+                Arguments = @"-c ""perl -pi -e 's%\r\n%\n%;' {SelectedRelativePaths}""",
+                RunInBackground = true,
+                AskConfirmation = false,
+                OnEvent = 0,
+                AddToRevisionGridContextMenu = false,
+                Enabled = false
+            },
+            new ScriptInfo
+            {
+                HotkeyCommandIdentifier = 9007,
+                Name = "Convert workspace file to CRLF",
+                Command = "bash.exe",
+                Arguments = @"-c ""perl -pi -e 's%\n%\r\n%;' {SelectedRelativePaths}""",
+                RunInBackground = true,
+                AskConfirmation = false,
+                OnEvent = 0,
+                AddToRevisionGridContextMenu = false,
+                Enabled = false
             }
         ];
 
