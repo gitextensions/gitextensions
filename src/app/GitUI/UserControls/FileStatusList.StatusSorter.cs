gitextensions/gitextensions

commit fd637fb4fe21e7378aefdc1d6a60b6296b02bb10

fix(PathFirstComparer): Distinguish folders having same start
and compare case-sensitively

Refs: #12598, fixes #12585

src/app/GitUI/UserControls/FileStatusList.StatusSorter.cs
@@ -142,15 +142,21 @@ partial class FileStatusList
                     ("", "") => 0,
                     (_, "") => -1,
                     ("", _) => 1,
-                    _ => StringComparer.InvariantCultureIgnoreCase.Compare(l.Path.Value, r.Path.Value)
+                    _ => StringComparer.InvariantCulture.Compare(l.Path.Value, r.Path.Value)
                 };
 
                 return pathComparison switch
                 {
-                    -1 => r.Path.Value.StartsWith(l.Path.Value, StringComparison.InvariantCultureIgnoreCase) ? 1 : -1,
-                    1 => l.Path.Value.StartsWith(r.Path.Value, StringComparison.InvariantCultureIgnoreCase) ? -1 : 1,
-                    _ => StringComparer.InvariantCultureIgnoreCase.Compare(l.Name, r.Name)
+                    -1 => StartsWith(r.Path, l.Path) ? 1 : -1,
+                    1 => StartsWith(l.Path, r.Path) ? -1 : 1,
+                    _ => StringComparer.InvariantCulture.Compare(l.Name, r.Name)
                 };
+
+                static bool StartsWith(RelativePath longPath, RelativePath shortPath)
+                {
+                    return longPath.Value.StartsWith(shortPath.Value)
+                        && longPath.Value[shortPath.Length] == '/';
+                }
             }
         }
 

tests/app/UnitTests/GitUI.Tests/UserControls/FileStatusListSorterTests.cs
@@ -27,8 +27,13 @@ public class FileStatusListSorterTests
             new("root_file"),
             new(".hidden_root_file"),
             new("ext/submodule/"),
+            new("ext/subfolder/filees"),
             new("ext/s/"),
+            new("ext/file"),
+            new("ext2/file"),
             new("1/file1"),
+            new("1/submodule_sorted_as_file/"),
+            new("1/subfolder/file1s"),
             new("1/2/file12"),
             new("1/3/file13"),
             new("1/3/4/file134"),

commit ea8eb1be664b5f3cd567294f5615e81290dbfbda

fix(PathFirstComparer): Use InvariantCulture
consistently

Refs: fixes #12856, broken in #12598

src/app/GitUI/UserControls/FileStatusList.StatusSorter.cs
@@ -154,7 +154,7 @@ partial class FileStatusList
 
                 static bool StartsWith(RelativePath longPath, RelativePath shortPath)
                 {
-                    return longPath.Value.StartsWith(shortPath.Value)
+                    return longPath.Value.StartsWith(shortPath.Value, StringComparison.InvariantCulture)
                         && longPath.Value[shortPath.Length] == '/';
                 }
             }