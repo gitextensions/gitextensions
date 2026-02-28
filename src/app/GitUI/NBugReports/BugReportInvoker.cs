b3a91448bcb439e53036ba322eacb36a1b0cf462
Igor Velikorossov <russkie@gmail.com>
2026-02-28 18:22:11 +1100

    Move GetExceptionInfo to ExceptionExtensions

--- src/app/GitUI/NBugReports/BugReportInvoker.cs
+++ src/app/GitUI/NBugReports/BugReportInvoker.cs
@@ -51,49 +51,6 @@ public static class BugReportInvoker
         return rootError;
     }
 
-    /// <summary>
-    /// Get the exception data.
-    /// </summary>
-    /// <param name="exception">An Exception to describe.</param>
-    internal static StringBuilder GetExceptionInfo(Exception exception)
-    {
-        StringBuilder text = new();
-
-        if (exception is UserExternalOperationException userExternalOperationException && !string.IsNullOrWhiteSpace(userExternalOperationException.Context))
-        {
-            // Context contains an error message as UserExternalOperationException is currently used. So append just "<context>"
-            text.AppendLine(userExternalOperationException.Context);
-        }
-
-        if (exception is ExternalOperationException externalOperationException)
-        {
-            // Exit code: <n>
-            if (externalOperationException.ExitCode is int exitCode)
-            {
-                AppendIfNotEmpty(ExecutionResult.FormatExitCode(exitCode), TranslatedStrings.ExitCode);
-            }
-
-            // Command: <command>
-            AppendIfNotEmpty(externalOperationException.Command, TranslatedStrings.Command);
-
-            // Arguments: <args>
-            AppendIfNotEmpty(externalOperationException.Arguments, TranslatedStrings.Arguments);
-
-            // Directory: <dir>
-            AppendIfNotEmpty(externalOperationException.WorkingDirectory, TranslatedStrings.WorkingDirectory);
-        }
-
-        return text;
-
-        void AppendIfNotEmpty(string? value, string designation)
-        {
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                text.Append(designation).Append(": ").AppendLine(value);
-            }
-        }
-    }
-
     public static void LogError(Exception exception, bool isTerminating = false)
     {
         string tempFolder = Path.GetTempPath();
@@ -179,7 +136,7 @@ public static class BugReportInvoker
                          or PathTooLongException
                          or Win32Exception;
 
-        StringBuilder text = GetExceptionInfo(exception);
+        StringBuilder text = exception.GetExceptionInfo();
         string rootError = GetRootError(exception);
 
         TaskDialogPage page = new()
