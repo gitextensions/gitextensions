4505d847fe8d253a8319ef04e6aed22056cd2738
Gerhard Olsson <6248932+gerhardol@users.noreply.github.com>
2026-03-01 09:31:43 +0100

    fix(dashboard): Focus search on start
    Refs: #12852

--- src/app/GitUI/CommandsDialogs/BrowseDialog/DashboardControl/UserRepositoriesList.cs
+++ src/app/GitUI/CommandsDialogs/BrowseDialog/DashboardControl/UserRepositoriesList.cs
@@ -749,6 +749,8 @@ public partial class UserRepositoriesList : GitExtensionsControl
         Validates.NotNull(menuStrip);
         ToolStripMenuItem dashboardMenu = (ToolStripMenuItem)menuStrip.Items.Cast<ToolStripItem>().SingleOrDefault(p => p.Name == "dashboardToolStripMenuItem");
         dashboardMenu?.DropDownItems.AddRange(menus);
+
+        BeginInvoke(textBoxSearch.Focus);
     }
 
     private void tsmiCategories_DropDownOpening(object sender, EventArgs e)
