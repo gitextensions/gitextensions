f49dc6738e61ee46abdd17028625ddb95d51f0c7
Igor Velikorossov <RussKie@users.noreply.github.com>
2026-03-01 12:20:38 +1100

    Update src/app/GitUI/NBugReports/BugReportInvoker.cs
    Co-authored-by: Michael Seibt <36601201+mstv@users.noreply.github.com>

--- src/app/GitUI/NBugReports/BugReportInvoker.cs
+++ src/app/GitUI/NBugReports/BugReportInvoker.cs
@@ -150,7 +150,7 @@ public static class BugReportInvoker
             exception,
             rootError,
             text,
-            new()
+            new OperationInfo()
             {
                 IsExternalOperation = isExternalOperation,
                 IsUserExternalOperation = isUserExternalOperation,
