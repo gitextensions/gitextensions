ad56da53756661142052016c1b33179e59ffa63b
Jonas Guss <44924601+jmg2k@users.noreply.github.com>
2026-03-25 20:01:42 +0100

    Implement Explorer shell extension supporting the modern context menu
    Windows 11 introduced a new context menu only accessible through a new API which is incompatible to the legacy one, resulting in GitExtensions no longer being accessible through the now default context menu. This PR aims to restore the 'lost' convenience by implementing the new API and packaging the extension in a way that Windows accepts it.
    
    Even for development enabled systems, the MSIX package must be signed with a trusted certificate before it can be installed. Some convenience scripts are provided and described in more detail in the README.md inside the source directory.
    
    Closes #9824

---
+++ src/native/GitExtensionsModernShellEx/resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by GitExtensionsModernShellEx.rc
+//
+
+// ReSharper disable IdentifierTypo
+// ReSharper disable CppClangTidyModernizeMacroToEnum
+// ReSharper disable CppClangTidyClangDiagnosticUnusedMacros
+
+#define IDI_GITEXTENSIONS               201
+#define IDI_ICONADDED                   202
+#define IDI_ICONBROWSEFILEEXPLORER      203
+#define IDI_ICONBRANCHCREATE            204
+#define IDI_ICONBRANCHCHECKOUT          205
+#define IDI_ICONREVISIONCHECKOUT        206
+#define IDI_ICONABOUT                   208
+#define IDI_ICONCLONEREPOGIT            209
+#define IDI_ICONCOMMIT                  210
+#define IDI_ICONFILEHISTORY             211
+#define IDI_ICONPULL                    212
+#define IDI_ICONPUSH                    213
+#define IDI_ICONTRESETFILETO            214
+#define IDI_ICONSETTINGS                215
+#define IDI_ICONVIEWCHANGES             216
+#define IDI_ICONSTASH                   217
+#define IDI_ICONCREATEREPOSITORY        218
+#define IDI_ICONPATCHAPPLY              219
+
+// ReSharper restore CppClangTidyClangDiagnosticUnusedMacros
+// ReSharper restore CppClangTidyModernizeMacroToEnum
+// ReSharper restore IdentifierTypo
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        220
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
