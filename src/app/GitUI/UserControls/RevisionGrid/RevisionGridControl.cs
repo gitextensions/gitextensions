gitextensions/gitextensions

commit 577f01711d9f57520b60007be15853e6330b98cd

feat(interactive ref labels): Improve UX
- use the same mnemonics as in the revision context menu
- force revision context menu by holding Shift
- force ref-specific context menu by holding Ctrl
- default to revision context menu if AlwaysShowAdvOpt
- filter revision context menu to clicked ref

Refs: #12991

src/app/GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
@@ -2088,13 +2088,21 @@ public sealed partial class RevisionGridControl : GitModuleControl, ICheckRefs, 
         }
 
         // If a ref label was right-clicked, show a focused context menu for that ref
-        if (_rightClickedHitInfo is { } hitInfo)
+        if (_rightClickedHitInfo is RefLabelHitInfo hitInfo && (ModifierKeys == Keys.Control || (ModifierKeys != Keys.Shift && !AppSettings.AlwaysShowAdvOpt)))
         {
             e.Cancel = true;
             ShowRefSpecificContextMenu(hitInfo.GitRef, hitInfo.StashReflogSelector);
             _rightClickedHitInfo = null;
             return;
         }
+
+        IGitRef? clickedRef = _rightClickedHitInfo?.GitRef;
+        Func<IEnumerable<IGitRef>, IEnumerable<IGitRef>> filterRefs = clickedRef is null
+            ? refs => refs
+            : refs => refs.Where(r => r == clickedRef);
+        copyToClipboardToolStripMenuItem.SetFilterRefsFunc(clickedRef is null
+            ? refNames => refNames
+            : refNames => refNames.Where(r => r == clickedRef.Name));
 
         bool inTheMiddleOfBisect = Module.InTheMiddleOfBisect();
         SetEnabled(markRevisionAsBadToolStripMenuItem, inTheMiddleOfBisect);
@@ -2115,7 +2123,7 @@ public sealed partial class RevisionGridControl : GitModuleControl, ICheckRefs, 
         GitRefListsForRevision gitRefListsForRevision = new(revision);
         _rebaseOnTopOf = null;
 
-        foreach (IGitRef head in gitRefListsForRevision.AllTags)
+        foreach (IGitRef head in filterRefs(gitRefListsForRevision.AllTags))
         {
             AddBranchMenuItem(deleteTagDropDown, head, delegate { UICommands.StartDeleteTagDialog(ParentForm, head.Name); });
             AddBranchMenuItem(selectInLeftPanelDropDown, head, SelectInLeftPanel_Click);
@@ -2128,10 +2136,9 @@ public sealed partial class RevisionGridControl : GitModuleControl, ICheckRefs, 
 
         // For now there is no action that could be done on currentBranch
         string currentBranchRef = GitRefName.RefsHeadsPrefix + CurrentBranch.Value;
-        IReadOnlyList<IGitRef> branchesWithNoIdenticalRemotes = gitRefListsForRevision.BranchesWithNoIdenticalRemotes;
 
         bool currentBranchPointsToRevision = false;
-        foreach (IGitRef head in branchesWithNoIdenticalRemotes)
+        foreach (IGitRef head in filterRefs(gitRefListsForRevision.BranchesWithNoIdenticalRemotes))
         {
             if (head.CompleteName == currentBranchRef)
             {
@@ -2161,7 +2168,7 @@ public sealed partial class RevisionGridControl : GitModuleControl, ICheckRefs, 
             _rebaseOnTopOf ??= toolStripItem.Tag as string;
         }
 
-        IReadOnlyList<IGitRef> allBranches = gitRefListsForRevision.AllBranches;
+        IReadOnlyList<IGitRef> allBranches = [.. filterRefs(gitRefListsForRevision.AllBranches)];
         bool isHeadOfCurrentBranch = false;
         bool firstRemoteBranchForCheckout = false;
         foreach (IGitRef head in allBranches)

src/app/GitUI/Translation/English.xlf
@@ -8561,7 +8561,7 @@ help</source>
   <file datatype="plaintext" original="LocalBranchContextMenuProvider" source-language="en">
     <body>
       <trans-unit id="_checkoutBranch.Text">
-        <source>&amp;Checkout this branch</source>
+        <source>Chec&amp;kout this branch</source>
         <target />
       </trans-unit>
       <trans-unit id="_deleteBranch.Text">
@@ -8573,7 +8573,7 @@ help</source>
         <target />
       </trans-unit>
       <trans-unit id="_pushBranch.Text">
-        <source>&amp;Push this branch</source>
+        <source>Pus&amp;h this branch</source>
         <target />
       </trans-unit>
       <trans-unit id="_rebaseOnto.Text">
@@ -8581,7 +8581,7 @@ help</source>
         <target />
       </trans-unit>
       <trans-unit id="_renameBranch.Text">
-        <source>Re&amp;name this branch</source>
+        <source>R&amp;ename this branch</source>
         <target />
       </trans-unit>
     </body>
@@ -8827,7 +8827,7 @@ This will just checkout on the submodule the commit determined by the superproje
   <file datatype="plaintext" original="RefContextMenuComposer" source-language="en">
     <body>
       <trans-unit id="_copyName.Text">
-        <source>Cop&amp;y name to clipboard</source>
+        <source>&amp;Copy name to clipboard</source>
         <target />
       </trans-unit>
     </body>
@@ -8835,7 +8835,7 @@ This will just checkout on the submodule the commit determined by the superproje
   <file datatype="plaintext" original="RemoteBranchContextMenuProvider" source-language="en">
     <body>
       <trans-unit id="_checkoutBranch.Text">
-        <source>&amp;Checkout this branch</source>
+        <source>Chec&amp;kout this branch</source>
         <target />
       </trans-unit>
       <trans-unit id="_deleteBranch.Text">
@@ -10107,15 +10107,15 @@ When OpenSSH is used, command line dialogs are shown!
   <file datatype="plaintext" original="StashRefContextMenuProvider" source-language="en">
     <body>
       <trans-unit id="_applyStash.Text">
-        <source>&amp;Apply stash</source>
+        <source>Appl&amp;y stash</source>
         <target />
       </trans-unit>
       <trans-unit id="_dropStash.Text">
-        <source>Dr&amp;op stash...</source>
+        <source>&amp;Drop stash...</source>
         <target />
       </trans-unit>
       <trans-unit id="_popStash.Text">
-        <source>P&amp;op stash</source>
+        <source>Pop &amp;stash</source>
         <target />
       </trans-unit>
     </body>
@@ -10123,15 +10123,15 @@ When OpenSSH is used, command line dialogs are shown!
   <file datatype="plaintext" original="StashSelectorContextMenuProvider" source-language="en">
     <body>
       <trans-unit id="_applyStash.Text">
-        <source>&amp;Apply stash</source>
+        <source>Appl&amp;y stash</source>
         <target />
       </trans-unit>
       <trans-unit id="_dropStash.Text">
-        <source>Dr&amp;op stash...</source>
+        <source>&amp;Drop stash...</source>
         <target />
       </trans-unit>
       <trans-unit id="_popStash.Text">
-        <source>P&amp;op stash</source>
+        <source>Pop &amp;stash</source>
         <target />
       </trans-unit>
     </body>

src/app/GitUI/UserControls/RevisionGrid/CopyContextMenuItem.cs
@@ -10,6 +10,7 @@ namespace GitUI.UserControls.RevisionGrid;
 public sealed class CopyContextMenuItem : ToolStripMenuItem
 {
     private readonly TranslationString _copyToClipboardText = new("&Copy to clipboard");
+    private Func<IEnumerable<string>, IEnumerable<string>> _filterRefsFunc = refs => refs;
     private Func<IReadOnlyList<GitRevision>>? _revisionFunc;
     private uint _itemNumber;
 
@@ -19,6 +20,11 @@ public sealed class CopyContextMenuItem : ToolStripMenuItem
         Text = _copyToClipboardText.Text;
 
         DropDownOpening += OnDropDownOpening;
+    }
+
+    public void SetFilterRefsFunc(Func<IEnumerable<string>, IEnumerable<string>> filterRefsFunc)
+    {
+        _filterRefsFunc = filterRefsFunc;
     }
 
     public void SetRevisionFunc(Func<IReadOnlyList<GitRevision>> revisionFunc)
@@ -104,8 +110,8 @@ public sealed class CopyContextMenuItem : ToolStripMenuItem
         foreach (GitRevision revision in revisions)
         {
             GitRefListsForRevision refLists = new(revision);
-            branchNames.AddRange(refLists.GetAllBranchNames());
-            tagNames.AddRange(refLists.GetAllTagNames());
+            branchNames.AddRange(_filterRefsFunc(refLists.GetAllBranchNames()));
+            tagNames.AddRange(_filterRefsFunc(refLists.GetAllTagNames()));
         }
 
         _itemNumber = 0;

src/app/GitUI/UserControls/RevisionGrid/RefContextMenus/LocalBranchContextMenuProvider.cs
@@ -10,12 +10,12 @@ namespace GitUI.UserControls.RevisionGrid.RefContextMenus;
 /// </summary>
 internal sealed class LocalBranchContextMenuProvider : Translate, IRefContextMenuProvider
 {
-    private readonly TranslationString _checkoutBranch = new("&Checkout this branch");
+    private readonly TranslationString _checkoutBranch = new("Chec&kout this branch");
     private readonly TranslationString _mergeIntoCurrent = new("&Merge into current branch");
     private readonly TranslationString _rebaseOnto = new("&Rebase current branch onto this");
-    private readonly TranslationString _renameBranch = new("Re&name this branch");
+    private readonly TranslationString _renameBranch = new("R&ename this branch");
     private readonly TranslationString _deleteBranch = new("&Delete this branch");
-    private readonly TranslationString _pushBranch = new("&Push this branch");
+    private readonly TranslationString _pushBranch = new("Pus&h this branch");
 
     public bool Handles(IGitRef? gitRef, string? stashReflogSelector) => gitRef?.IsHead is true;
 
@@ -48,7 +48,10 @@ internal sealed class LocalBranchContextMenuProvider : Translate, IRefContextMen
             menu.Items.Add(rebase);
         }
 
-        menu.Items.Add(new ToolStripSeparator());
+        if (menu.Items.Count > 0)
+        {
+            menu.Items.Add(new ToolStripSeparator());
+        }
 
         ToolStripMenuItem rename = new(_renameBranch.Text, Images.EditFile);
         rename.Click += (_, _) => context.UICommands.StartRenameDialog(context.ParentForm, gitRef.Name);

src/app/GitUI/UserControls/RevisionGrid/RefContextMenus/RefContextMenuComposer.cs
@@ -11,7 +11,7 @@ namespace GitUI.UserControls.RevisionGrid.RefContextMenus;
 /// </summary>
 internal sealed class RefContextMenuComposer : Translate
 {
-    private readonly TranslationString _copyName = new("Cop&y name to clipboard");
+    private readonly TranslationString _copyName = new("&Copy name to clipboard");
 
     private readonly IReadOnlyList<IRefContextMenuProvider> _providers;
 

src/app/GitUI/UserControls/RevisionGrid/RefContextMenus/RemoteBranchContextMenuProvider.cs
@@ -10,7 +10,7 @@ namespace GitUI.UserControls.RevisionGrid.RefContextMenus;
 /// </summary>
 internal sealed class RemoteBranchContextMenuProvider : Translate, IRefContextMenuProvider
 {
-    private readonly TranslationString _checkoutBranch = new("&Checkout this branch");
+    private readonly TranslationString _checkoutBranch = new("Chec&kout this branch");
     private readonly TranslationString _mergeIntoCurrent = new("&Merge into current branch");
     private readonly TranslationString _rebaseOnto = new("&Rebase current branch onto this");
     private readonly TranslationString _deleteBranch = new("&Delete this branch");

src/app/GitUI/UserControls/RevisionGrid/RefContextMenus/StashRefContextMenuProvider.cs
@@ -10,9 +10,9 @@ namespace GitUI.UserControls.RevisionGrid.RefContextMenus;
 /// </summary>
 internal sealed class StashRefContextMenuProvider : Translate, IRefContextMenuProvider
 {
-    private readonly TranslationString _applyStash = new("&Apply stash");
-    private readonly TranslationString _popStash = new("P&op stash");
-    private readonly TranslationString _dropStash = new("Dr&op stash...");
+    private readonly TranslationString _applyStash = new("Appl&y stash");
+    private readonly TranslationString _popStash = new("Pop &stash");
+    private readonly TranslationString _dropStash = new("&Drop stash...");
 
     public bool Handles(IGitRef? gitRef, string? stashReflogSelector) => gitRef?.IsStash is true;
 

src/app/GitUI/UserControls/RevisionGrid/RefContextMenus/StashSelectorContextMenuProvider.cs
@@ -13,9 +13,9 @@ namespace GitUI.UserControls.RevisionGrid.RefContextMenus;
 /// </summary>
 internal sealed class StashSelectorContextMenuProvider : Translate, IRefContextMenuProvider
 {
-    private readonly TranslationString _applyStash = new("&Apply stash");
-    private readonly TranslationString _popStash = new("P&op stash");
-    private readonly TranslationString _dropStash = new("Dr&op stash...");
+    private readonly TranslationString _applyStash = new("Appl&y stash");
+    private readonly TranslationString _popStash = new("Pop &stash");
+    private readonly TranslationString _dropStash = new("&Drop stash...");
 
     public bool Handles(IGitRef? gitRef, string? stashReflogSelector) => gitRef is null && stashReflogSelector is not null;
 

commit 93d416d48367434b479785939644ca2c18d063b2

fix(revision grid): always show ref-specific context menu by default
Right-clicking a ref label now always shows the focused ref-specific
context menu regardless of the AlwaysShowAdvOpt setting. Hold Shift to
see the full revision context menu instead.

This fixes a regression from #12991 where users with AlwaysShowAdvOpt
enabled had to hold Ctrl to get the ref-specific menu.

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

src/app/GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
@@ -2087,8 +2087,8 @@ public sealed partial class RevisionGridControl : GitModuleControl, ICheckRefs, 
             return;
         }
 
-        // If a ref label was right-clicked, show a focused context menu for that ref
-        if (_rightClickedHitInfo is RefLabelHitInfo hitInfo && (ModifierKeys == Keys.Control || (ModifierKeys != Keys.Shift && !AppSettings.AlwaysShowAdvOpt)))
+        // If a ref label was right-clicked, show a focused context menu for that ref (hold Shift for full menu)
+        if (_rightClickedHitInfo is RefLabelHitInfo hitInfo && !ModifierKeys.HasFlag(Keys.Shift))
         {
             e.Cancel = true;
             ShowRefSpecificContextMenu(hitInfo.GitRef, hitInfo.StashReflogSelector);