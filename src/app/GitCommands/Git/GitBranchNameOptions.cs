87080cb4c6fd0a24b23890d701e20d1c8b837514
Michael Seibt <36601201+mstv@users.noreply.github.com>
2026-05-22 12:22:54 +0200

    fix(remote branch prefix): Allow trailing slash
    by extending GitBranchNameNormaliser & GitBranchNameOptions
    
    Refs: #13074

--- src/app/GitCommands/Git/GitBranchNameOptions.cs
+++ src/app/GitCommands/Git/GitBranchNameOptions.cs
@@ -1,12 +1,20 @@
 ﻿namespace GitCommands.Git;
 
 /// <summary>
-/// Options used by <see cref="GitBranchNameNormaliser"/> to ensures compliance with the GIT branch naming conventions.
+///  Options used by <see cref="GitBranchNameNormaliser"/> to ensures compliance with the GIT branch naming conventions.
 /// </summary>
 public sealed class GitBranchNameOptions
 {
-    public GitBranchNameOptions(string? replacementToken)
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="GitBranchNameOptions"/> class.
+    /// </summary>
+    /// <param name="replacementToken">The character which will replace all invalid characters in git branch name.</param>
+    /// <param name="allowTrailingSlash">Indicates whether a trailing slash is allowed in the branch name, e.g. in case of a branch prefix.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the replacement token is invalid.</exception>
+    public GitBranchNameOptions(string? replacementToken, bool allowTrailingSlash = false)
     {
+        AllowTrailingSlash = allowTrailingSlash;
+
         if (!string.IsNullOrEmpty(replacementToken))
         {
             if (replacementToken.Length > 1)
@@ -24,7 +32,12 @@ public sealed class GitBranchNameOptions
     }
 
     /// <summary>
-    /// Gets the character which will replace all invalid characters in git branch name.
+    ///  Indicates whether a trailing slash is allowed in the branch name, e.g. in case of a branch prefix.
+    /// </summary>
+    public bool AllowTrailingSlash { get; }
+
+    /// <summary>
+    ///  Gets the character which will replace all invalid characters in git branch name.
     /// </summary>
     /// <seealso cref="GitBranchNameNormaliser"/>.
     public string ReplacementToken { get; }
