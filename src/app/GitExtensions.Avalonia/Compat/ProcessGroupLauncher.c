bb24da36918e6f1570a99a8f9e347c9bd69601bd
nikola.begovic <begota98@gmail.com>
2026-07-15 23:24:36 +0200

    feat(avalonia): isolate macOS process groups

---
+++ src/app/GitExtensions.Avalonia/Compat/ProcessGroupLauncher.c
@@ -0,0 +1,25 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fputs("Usage: GitExtensions.ProcessGroupLauncher <command> [arguments...]\n", stderr);
+        return 64;
+    }
+
+    if (setpgid(0, 0) == -1)
+    {
+        fprintf(stderr, "Unable to create a process group: %s\n", strerror(errno));
+        return 71;
+    }
+
+    execvp(argv[1], &argv[1]);
+    fprintf(stderr, "Unable to execute %s: %s\n", argv[1], strerror(errno));
+    return 127;
+}
