96c54021be5775e5043dd910171d65750401ac8b
nikola.begovic <begota98@gmail.com>
2026-07-23 01:00:55 +0200

    feat(avalonia): add build server settings host

--- tests/app/UnitTests/GitUI.Avalonia.Tests/BuildServerIntegrationTests.cs
+++ tests/app/UnitTests/GitUI.Avalonia.Tests/BuildServerIntegrationTests.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Headless;
 using Avalonia.Headless.NUnit;
 using Avalonia.Media;
 using Avalonia.Threading;
@@ -7,10 +8,14 @@ using GitCommands.Settings;
 using GitExtensions.Extensibility.BuildServerIntegration;
 using GitExtensions.Extensibility.Git;
 using GitExtensions.Extensibility.Settings;
+using GitExtensions.Extensibility.Translations;
 using GitUI.CommandsDialogs;
+using GitUI.CommandsDialogs.SettingsDialog;
+using GitUI.CommandsDialogs.SettingsDialog.Pages;
 using GitUI.HelperDialogs;
 using GitUI.UserControls.RevisionGrid.Columns;
 using GitUIPluginInterfaces;
+using Microsoft.VisualStudio.Threading;
 using NSubstitute;
 
 namespace GitExtensionsTests;
@@ -18,6 +23,97 @@ namespace GitExtensionsTests;
 [TestFixture]
 public sealed class BuildServerIntegrationTests
 {
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        ManagedExtensibility.Initialise();
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        GitUI.ThreadHelper.JoinableTaskContext = new JoinableTaskContext();
+    }
+
+    [AvaloniaTest]
+    public async Task Build_server_settings_page_should_be_registered_translated_and_populate_the_selector()
+    {
+        SettingsSource settings = AppSettings.SettingsContainer;
+        bool? originalEnabled = BuildServerSettings.IntegrationEnabled[settings];
+        bool? originalShowResult = BuildServerSettings.ShowBuildResultPage[settings];
+        string? originalServerName = BuildServerSettings.ServerName[settings];
+        BuildServerSettings.IntegrationEnabled[settings] = null;
+        BuildServerSettings.ShowBuildResultPage[settings] = true;
+        BuildServerSettings.ServerName[settings] = null;
+        FormSettings form = new();
+        try
+        {
+            FormSettings.TestAccessor formAccessor = form.GetTestAccessor();
+            formAccessor.InitializePages();
+            BuildServerIntegrationSettingsPage page = formAccessor.SettingsTreeView.SettingsPages
+                .OfType<BuildServerIntegrationSettingsPage>()
+                .Single();
+
+            form.GotoPage(page.PageReference);
+            SettingsPageHeader header = formAccessor.CurrentPage.Should().BeOfType<SettingsPageHeader>().Subject;
+            header.GetTestAccessor().Page.Should().BeSameAs(page);
+            page.GetTitle().Should().Be("Build server integration");
+
+            ITranslation translation = Substitute.For<ITranslation>();
+            page.AddTranslationItems(translation);
+            page.TranslateItems(translation);
+            translation.Received(1).AddTranslationItem(
+                nameof(BuildServerIntegrationSettingsPage), "$this", "Text", "Build server integration");
+            translation.Received(1).AddTranslationItem(
+                nameof(BuildServerIntegrationSettingsPage), "_noneItem", "Text", "None");
+            translation.Received(1).AddTranslationItem(
+                nameof(BuildServerIntegrationSettingsPage), "checkBoxEnableBuildServerIntegration", "Text", "Enable build server integration");
+            translation.Received(1).AddTranslationItem(
+                nameof(BuildServerIntegrationSettingsPage), "checkBoxShowBuildResultPage", "Text", "Show build result page");
+            translation.Received(1).AddTranslationItem(
+                nameof(BuildServerIntegrationSettingsPage), "labelBuildServerSettingsInfo", "Text", "Git Extensions can integrate with build servers to supply per-commit Continuous Integration information.");
+            translation.Received(1).AddTranslationItem(
+                nameof(BuildServerIntegrationSettingsPage), "labelBuildServerType", "Text", "Build server type");
+
+            page.LoadSettings();
+            BuildServerIntegrationSettingsPage.TestAccessor pageAccessor = page.GetTestAccessor();
+            await pageAccessor.PopulateBuildServerTypeTask!.JoinAsync();
+            Dispatcher.UIThread.RunJobs();
+
+            pageAccessor.checkBoxEnableBuildServerIntegration.IsEnabled.Should().BeTrue();
+            pageAccessor.checkBoxEnableBuildServerIntegration.IsThreeState.Should().BeTrue();
+            pageAccessor.checkBoxShowBuildResultPage.IsEnabled.Should().BeTrue();
+            pageAccessor.checkBoxShowBuildResultPage.IsThreeState.Should().BeTrue();
+            pageAccessor.BuildServerType.IsEnabled.Should().BeTrue();
+            pageAccessor.checkBoxEnableBuildServerIntegration.IsChecked.Should().BeNull();
+            pageAccessor.checkBoxShowBuildResultPage.IsChecked.Should().BeTrue();
+            pageAccessor.BuildServerType.SelectedIndex.Should().Be(0);
+            string[] buildServerTypes = [.. pageAccessor.BuildServerType.Items.Cast<string>()];
+            buildServerTypes.Should().StartWith("None");
+            pageAccessor.buildServerSettingsPanel.Content.Should().BeNull(
+                "plugin-owned settings controls are activated independently");
+
+            pageAccessor.checkBoxEnableBuildServerIntegration.IsChecked = true;
+            pageAccessor.checkBoxShowBuildResultPage.IsChecked = false;
+            page.SaveSettings();
+            BuildServerSettings.IntegrationEnabled[settings].Should().BeTrue();
+            BuildServerSettings.ShowBuildResultPage[settings].Should().BeFalse();
+            BuildServerSettings.ServerName[settings].Should().BeNull();
+
+            form.Show();
+            Dispatcher.UIThread.RunJobs();
+            form.CaptureRenderedFrame().Should().NotBeNull();
+        }
+        finally
+        {
+            BuildServerSettings.IntegrationEnabled[settings] = originalEnabled;
+            BuildServerSettings.ShowBuildResultPage[settings] = originalShowResult;
+            BuildServerSettings.ServerName[settings] = originalServerName;
+            form.GotoPage(new SettingsPageReferenceByType(typeof(SettingsPlaceholderPage)));
+            form.Close();
+        }
+    }
+
     [AvaloniaTest]
     public void Credentials_dialog_should_retain_the_original_authentication_controls()
     {
