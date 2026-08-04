93d69565c2e042cf8e00f58526efb801e95d085f
nikola.begovic <begota98@gmail.com>
2026-08-04 18:10:18 +0200

    feat(avalonia): restore FormBrowse partials

---
+++ src/app/GitUI.Avalonia/CommandsDialogs/FormBrowse.UpdateTargets.cs
@@ -0,0 +1,13 @@
+﻿namespace GitUI.CommandsDialogs;
+
+public sealed partial class FormBrowse
+{
+    [Flags]
+    private enum UpdateTargets
+    {
+        None = 1,
+        DiffList = 2,
+        FileTree = 4,
+        CommitInfo = 8
+    }
+}
