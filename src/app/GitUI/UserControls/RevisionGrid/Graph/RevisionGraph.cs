6b32a4ac1839925f656e3961c4bbe205828bac18
Gerhard Olsson <gerhardol@users.noreply.github.com>
2026-08-08 13:56:18 +0200

    fixup! remove unused

--- src/app/GitUI/UserControls/RevisionGrid/Graph/RevisionGraph.cs
+++ src/app/GitUI/UserControls/RevisionGrid/Graph/RevisionGraph.cs
@@ -196,9 +196,6 @@ public class RevisionGraph : IRevisionGraphRowProvider
     public bool TryGetNode(ObjectId objectId, [NotNullWhen(true)] out RevisionGraphRevision? revision)
         => _revisionByObjectId.TryGetValue(objectId, out revision);
 
-    /// <summary>Gets all revision nodes currently in the graph.</summary>
-    internal IEnumerable<RevisionGraphRevision> GetRevisions() => _revisionByObjectId.Values;
-
     public bool TryGetRowIndex(ObjectId objectId, out int index)
     {
         if (!TryGetNode(objectId, out RevisionGraphRevision? revision))
