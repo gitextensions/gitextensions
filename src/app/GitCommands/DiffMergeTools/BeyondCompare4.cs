349b93f56ae7d7e972377eb28a741144c9d6d54e
Michael Seibt <36601201+mstv@users.noreply.github.com>
2026-08-13 22:10:33 +0200

    feat(cross-platform): add names of diff/merge tools
    Refs: #13221

--- src/app/GitCommands/DiffMergeTools/BeyondCompare4.cs
+++ src/app/GitCommands/DiffMergeTools/BeyondCompare4.cs
@@ -1,9 +1,9 @@
-namespace GitCommands.DiffMergeTools;
+﻿namespace GitCommands.DiffMergeTools;
 
 internal sealed class BeyondCompare4 : DiffMergeTool
 {
     /// <inheritdoc />
-    public override string ExeFileName => "bcomp.exe";
+    public override string ExeFileName => OperatingSystem.IsWindows() ? "bcomp.exe" : "bcompare";
 
     /// <inheritdoc />
     public override string Name => "bc";
